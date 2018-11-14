cd7f40853408ebe27a553ab1aa803e6112049d1a
thekenu <thekenu@gmail.com>
2018-11-13 18:47:15 -0800

    Migrate PDM and move CAN into src/shared

---
+++ src/PDM/Inc/Debug.h
@@ -0,0 +1,12 @@
+/**
+    @file	   	Debug.h
+    @brief	  	PDM_2018
+    @author	 	UBC Formula Electric
+*/
+
+#ifndef DEBUG_H
+#define DEBUG_H
+
+#define DEBUG
+
+#endif
---
+++ src/PDM/Inc/ErrorHandling.h
@@ -0,0 +1,15 @@
+/**
+    @file       ErrorHandling.h
+    @brief      PDM 2018 Code
+    @author     UBC Formula Electric
+*/
+
+#ifndef ERRORHANDLING_H
+#define ERRORHANDLING_H
+
+#include "CANDefinitions.h"
+
+// Function Declarations
+void ServiceError(PdmError_Enum Error);
+
+#endif
