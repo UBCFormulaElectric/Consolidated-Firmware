f3ddbd3917d382ccbbbe9addae44717c74b44602
ahmadnav <ahmad.nav@outlook.com>
2018-11-13 16:20:41 -0800

    On branch ahmad/FSM_2018  Changes to be committed: 	modified:   .gitignore 	modified:   FSM_2018/.mxproject 	new file:   FSM_2018/MDK-ARM/DebugConfig/FSM2018_STM32F042K6Tx.dbgconf 	modified:   FSM_2018/MDK-ARM/FSM2018.uvprojx 	new file:   FSM_2018/MDK-ARM/RTE/_FSM2018/RTE_Components.h

---
+++ FSM_2018/MDK-ARM/RTE/_FSM2018/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'FSM2018' 
+ * Target:  'FSM2018' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f0xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
