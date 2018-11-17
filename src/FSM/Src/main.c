a559d3c53f6237aa5b5a3de14aac81c4bd2b6908
ahmadnav <ahmad.nav@outlook.com>
2018-11-16 19:15:17 -0800

    Updated CubeMX User Constants and .gitignore

--- src/FSM/Src/main.c
+++ src/FSM/Src/main.c
@@ -246,7 +246,7 @@ static void MX_CAN_Init(void)
 {
 
   hcan.Instance = CAN;
-  hcan.Init.Prescaler = CAN_Prescaler_Value_Cube;
+  hcan.Init.Prescaler = 8;
   hcan.Init.Mode = CAN_MODE_NORMAL;
   hcan.Init.SJW = CAN_SJW_2TQ;
   hcan.Init.BS1 = CAN_BS1_5TQ;
@@ -271,7 +271,7 @@ static void MX_IWDG_Init(void)
   hiwdg.Instance = IWDG;
   hiwdg.Init.Prescaler = IWDG_PRESCALER_32;
   hiwdg.Init.Window = 4095;
-  hiwdg.Init.Reload = WATCHDOG_TIMER_PERIOD_CUBE;
+  hiwdg.Init.Reload = 200;
   if (HAL_IWDG_Init(&hiwdg) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
