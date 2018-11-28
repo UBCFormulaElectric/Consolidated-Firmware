336fee0797e11551175e9bf05d3c4d9ce9915c64
unknown <ahmad.nav@outlook.com>
2018-11-27 20:22:40 -0800

    Updated values for ADC Sampling.

--- src/FSM/Src/main.c
+++ src/FSM/Src/main.c
@@ -227,7 +227,7 @@ static void MX_ADC_Init(void)
     */
   sConfig.Channel = ADC_CHANNEL_5;
   sConfig.Rank = ADC_RANK_CHANNEL_NUMBER;
-  sConfig.SamplingTime = ADC_SAMPLETIME_7CYCLES_5;
+  sConfig.SamplingTime = ADC_SAMPLETIME_55CYCLES_5;
   if (HAL_ADC_ConfigChannel(&hadc, &sConfig) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
