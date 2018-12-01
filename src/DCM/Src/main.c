71139903d565325627aaf317900162501127a2bb
Jason <jsn0716@outlook.com>
2018-11-30 23:40:23 -0800

    Use user constants for TIM6 (#86)

--- src/DCM/Src/main.c
+++ src/DCM/Src/main.c
@@ -264,9 +264,9 @@ static void MX_TIM6_Init(void)
   TIM_MasterConfigTypeDef sMasterConfig;
 
   htim6.Instance = TIM6;
-  htim6.Init.Prescaler = 15;
+  htim6.Init.Prescaler = TIM6_PRESCALER;
   htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim6.Init.Period = 44999;
+  htim6.Init.Period = (APB1_TIMER_CLOCK / ( (TIM6_PRESCALER + 1) * CONTROL_LOOP_FREQUENCY) ) - 1;
   htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
   {
