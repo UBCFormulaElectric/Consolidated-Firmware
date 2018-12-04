918e7691e45693d18bf764a1ef8a5e5c91f5c123
thekenu <thekenu@gmail.com>
2018-12-03 19:01:02 -0800

    Resolve leftover merge conflicts

--- src/PDM/MDK-ARM/RTE/_PDM/RTE_Components.h
+++ src/PDM/MDK-ARM/RTE/_PDM/RTE_Components.h
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:src/PDM/MDK-ARM/RTE/_PDM/RTE_Components.h
 
 /*
  * Auto generated Run-Time-Environment Component Configuration File
@@ -19,25 +18,3 @@
 
 
 #endif /* RTE_COMPONENTS_H */
-=======
-
-/*
- * Auto generated Run-Time-Environment Component Configuration File
- *      *** Do not modify ! ***
- *
- * Project: 'PDM_2018-Code' 
- * Target:  'PDM_2018-Code' 
- */
-
-#ifndef RTE_COMPONENTS_H
-#define RTE_COMPONENTS_H
-
-
-/*
- * Define the Device Header File: 
- */
-#define CMSIS_device_header "stm32f3xx.h"
-
-
-#endif /* RTE_COMPONENTS_H */
->>>>>>> master:src/PDM/MDK-ARM/RTE/_PDM_2018-Code/RTE_Components.h
