6082901ffd25eeb373ee596c6c7dca21a2a38ebf
thekenu <thekenu@gmail.com>
2018-12-03 19:09:28 -0800

    remove redundant PDM define and change to c11

--- src/PDM/Inc/main.h
+++ src/PDM/Inc/main.h
@@ -156,7 +156,6 @@ void Error_Handler(void);
 #define SWO_Pin GPIO_PIN_3
 #define SWO_GPIO_Port GPIOB
 /* USER CODE BEGIN Private defines */
-#define PDM
 /* USER CODE END Private defines */
 
 #ifdef __cplusplus
