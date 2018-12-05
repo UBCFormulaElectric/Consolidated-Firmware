beb3d69d95e8948ae9dfb58cc21fcb537defe597
SoleSun <joel.ahn@alumni.ubc.ca>
2018-12-04 21:37:39 -0800

    Created formatting scripts in python

--- clang-format/check_formatting_CI.py
+++ clang-format/check_formatting_CI.py
@@ -6,3 +6,11 @@ if __name__ == '__main__':
 		print("ERROR: Clang-Format encountered issues!")
 	else:
 		print("PASS: Clang-Format")
+
+	#If the git command exits with a 1
+	if os.system("git diff-index --quiet HEAD --;"):
+		#then there are differences and the user forgot to run formatting
+		print("Formatting check FAILED, see below for what changed!!")
+		os.system("git diff --color")
+	else:
+		print("Formatting check PASSED, great work!")
--- clang-format/fix_formatting.py
+++ clang-format/fix_formatting.py
@@ -1,16 +1,25 @@
 import os
 
-CLANG_FORMAT_VERSION = "4.0"
-CLANG_FORMAT_COMMAND = "clang-format-" + CLANG_FORMAT_VERSION
-
 def runClangFormat():
 	success = 0
+	
+	# Construct the command line input
+	CLANG_FORMAT_VERSION = "4.0"
+	CLANG_FORMAT_COMMAND = "clang-format-" + CLANG_FORMAT_VERSION
+	CLANG_FORMAT_OPTIONS = " -i --style=file "
 
-	for root, dirnames, filenames in os.walk('../src'):
+	#If the OS is Windows
+	if os.name == 'nt':
+		# Append the requisite .exe file ending
+		CLANG_FORMAT_COMMAND += ".exe" 
+
+	SOURCE_DIR = os.path.join("..", "src")
+
+	for root, dirnames, filenames in os.walk(SOURCE_DIR):
 	    for filename in filenames:
 	        if filename.endswith(('.c', '.cc', '.cpp', '.h', '.hpp', '.hh')):
-	        	print((CLANG_FORMAT_COMMAND + " -i --style=file " + os.path.join(root, filename)))
-	        	success |= os.system((os.path.abspath(CLANG_FORMAT_COMMAND) + " -i --style=file " + os.path.join(root, filename)))
+	        	print((CLANG_FORMAT_COMMAND + CLANG_FORMAT_OPTIONS + os.path.join(root, filename)))
+	        	success |= os.system((os.path.abspath(CLANG_FORMAT_COMMAND) + CLANG_FORMAT_OPTIONS + os.path.join(root, filename)))
 
 	return success
  
