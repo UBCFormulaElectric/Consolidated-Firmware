e72ea21dadcb5e00c37be7509ba46874bcc35493
Ken Lin <thekenu@gmail.com>
2018-12-05 18:40:52 -0800

    Add Constants.h with PI (#183)
    * Add Constants.h with PI
    
    * Remove unnecessary header sections
    
    * Remove typedef

---
+++ src/shared/Constants.h
@@ -0,0 +1,19 @@
+/**
+ * @file  Constants.h
+ * @brief A list of commonly used constants that may be shared between projects
+ */
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+/******************************************************************************
+* Includes
+*******************************************************************************/
+
+/******************************************************************************
+* Preprocessor Constants
+*******************************************************************************/
+// clang-format off
+#define PI          (float)(3.14159265359)
+// clang-format on
+
+#endif /* CONSTANTS_H */
