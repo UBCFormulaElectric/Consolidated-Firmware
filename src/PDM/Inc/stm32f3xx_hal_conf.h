ddcbd3f03435ba0dd25265529942cbceb39daaea
SoleSun <joel.ahn@alumni.ubc.ca>
2018-12-12 23:48:50 -0800

    Changed commentpragmas to work with ####### delimited comments

--- src/PDM/Inc/stm32f3xx_hal_conf.h
+++ src/PDM/Inc/stm32f3xx_hal_conf.h
@@ -47,8 +47,7 @@ extern "C"
     /* Exported constants
      * --------------------------------------------------------*/
 
-    /* ########################## Module Selection
-     * ############################## */
+    /* ########################## Module Selection ########################## */
     /**
      * @brief This is the list of modules to be used in the HAL driver
      */
@@ -175,8 +174,7 @@ extern "C"
        ===  you can define the HSE value in your toolchain compiler
        preprocessor. */
 
-    /* ########################### System Configuration
-     * ######################### */
+    /* ########################### System Configuration ##################### */
     /**
      * @brief This is the HAL system configuration section
      */
@@ -189,16 +187,14 @@ extern "C"
 #define INSTRUCTION_CACHE_ENABLE 0
 #define DATA_CACHE_ENABLE 0
 
-    /* ########################## Assert Selection
-     * ############################## */
+    /* ########################## Assert Selection ########################## */
     /**
      * @brief Uncomment the line below to expanse the "assert_param" macro in
      * the HAL drivers code
      */
     /* #define USE_FULL_ASSERT    1U */
 
-    /* ################## SPI peripheral configuration
-     * ########################## */
+    /* ################## SPI peripheral configuration ###################### */
 
     /* CRC FEATURE: Use to activate CRC feature inside HAL SPI Driver
      * Activated: CRC code is present inside driver
