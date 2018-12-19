9be6d2d0deab6c4d6011284481947089e3d448b0
thekenu <thekenu@gmail.com>
2018-12-18 21:50:01 -0500

    make local var camel_case and change fault_flag to be boolean type

--- src/FSM/Src/Apps.c
+++ src/FSM/Src/Apps.c
@@ -4,6 +4,7 @@
 #include "Apps.h"
 #include "main.h"
 #include "CanDefinitions.h"
+#include "stdbool.h"
 
 /******************************************************************************
  * Module Preprocessor Constants
@@ -22,6 +23,7 @@
  ******************************************************************************/
 // Used to send error CAN messages and track faults
 volatile uint32_t apps_fault_state = FSM_APPS_NORMAL_OPERATION;
+
 /******************************************************************************
 * Private Function Prototypes
 *******************************************************************************/
@@ -32,49 +34,49 @@ volatile uint32_t apps_fault_state = FSM_APPS_NORMAL_OPERATION;
 uint16_t getAcceleratorPedalPosition(APPS_Mode_Enum Mode)
 {
     static uint32_t apps_fault_counter = 0; 
-    float32_t RawPrimaryAPPSValue       = 0.0;
-    float32_t RawSecondaryAPPSValue     = 0.0;
-    float32_t PercentPrimaryAPPSValue   = 0.0;
-    float32_t PercentSecondaryAPPSValue = 0.0;
+    float32_t raw_primary_apps_value       = 0.0;
+    float32_t raw_secondary_apps_value     = 0.0;
+    float32_t percent_primary_apps_value   = 0.0;
+    float32_t percent_secdonary_apps_value = 0.0;
 
-    uint32_t TemporaryAPPSFaultCounter = 0;
-    uint8_t  FaultFlag                 = 0;
-    uint16_t AcceleratorPedalPosition  = 0;
+    uint32_t temporary_apps_fault_counter = 0;
+    bool fault_flag                       = false;
+    uint16_t accelerator_pedal_position   = 0;
 
     // Save apps_fault_counter in Mode = APPS_NORMAL_MODE
     if (Mode == APPS_NORMAL_MODE)
     {
-        TemporaryAPPSFaultCounter = apps_fault_counter;
+        temporary_apps_fault_counter = apps_fault_counter;
     }
 
     if (apps_fault_state == FSM_APPS_PRIMARY_SECONDARY_DIFFERENCE_ERROR)
     {
-        AcceleratorPedalPosition = 0;
+        accelerator_pedal_position = 0;
     }
     else
     {
         // Read latest APPS position values, from timer counter.
-        RawPrimaryAPPSValue   = (float32_t)TIM2->CNT;
-        RawSecondaryAPPSValue = 65535 - (float32_t)TIM3->CNT;
+        raw_primary_apps_value   = (float32_t)TIM2->CNT;
+        raw_secondary_apps_value = 65535 - (float32_t)TIM3->CNT;
 
         // Set a deadzone to handle underflow
-        if (((65535 - RawPrimaryAPPSValue) < PRIMARY_APPS_DEADZONE) ||
-            (RawPrimaryAPPSValue < PRIMARY_APPS_DEADZONE))
+        if (((65535 - raw_primary_apps_value) < PRIMARY_APPS_DEADZONE) ||
+            (raw_primary_apps_value < PRIMARY_APPS_DEADZONE))
         {
-            RawPrimaryAPPSValue = 0.0;
+            raw_primary_apps_value = 0.0;
         }
 
-        if (((65535 - RawPrimaryAPPSValue) < SECONDARY_APPS_DEADZONE) ||
-            (RawPrimaryAPPSValue < SECONDARY_APPS_DEADZONE))
+        if (((65535 - raw_primary_apps_value) < SECONDARY_APPS_DEADZONE) ||
+            (raw_primary_apps_value < SECONDARY_APPS_DEADZONE))
         {
-            RawSecondaryAPPSValue = 0.0;
+            raw_secondary_apps_value = 0.0;
         }
 
         // Calculate ratio between APPS position readings and calibrated maximum
         // values
-        PercentPrimaryAPPSValue = RawPrimaryAPPSValue / PRIMARY_APPS_MAX_VALUE;
-        PercentSecondaryAPPSValue =
-            RawSecondaryAPPSValue / SECONDARY_APPS_MAX_VALUE;
+        percent_primary_apps_value = raw_primary_apps_value / PRIMARY_APPS_MAX_VALUE;
+        percent_secdonary_apps_value =
+            raw_secondary_apps_value / SECONDARY_APPS_MAX_VALUE;
 
         // Set torque request to zero whenever brake pedal is pushed
         // This satisfies shutting off the motors whenever the brake pedal
@@ -82,35 +84,35 @@ uint16_t getAcceleratorPedalPosition(APPS_Mode_Enum Mode)
         // cube.
         if (HAL_GPIO_ReadPin(BSPD_BRAKE_THRES_GPIO_Port, BSPD_BRAKE_THRES_Pin))
         {
-            AcceleratorPedalPosition = 0;
+            accelerator_pedal_position = 0;
         }
         // Check for position readings outside bounds (when pedal is released OR
         // when pedal is pushed passed max rotation beyond calibrated pedal box)
         else if (
-            PercentPrimaryAPPSValue > 1.0 || PercentSecondaryAPPSValue > 1.0)
+            percent_primary_apps_value > 1.0 || percent_secdonary_apps_value > 1.0)
         {
             // Set pedal position to zero when encoder underflows OR when pedal
             // pushes passed max. pedal box rotation
-            AcceleratorPedalPosition = 0;
+            accelerator_pedal_position = 0;
         }
         else if (
-            PercentPrimaryAPPSValue > PEDAL_SATURATION_POINT &&
-            PercentPrimaryAPPSValue <= 1.0)
+            percent_primary_apps_value > PEDAL_SATURATION_POINT &&
+            percent_primary_apps_value <= 1.0)
         {
             // Saturate pedal position to 100% if close to being fully pressed
             // (accounts for deflection in pedal cluster)
-            AcceleratorPedalPosition = 1023;
+            accelerator_pedal_position = 1023;
         }
-        else if (PercentPrimaryAPPSValue < PEDAL_RELEASE_POINT)
+        else if (percent_primary_apps_value < PEDAL_RELEASE_POINT)
         {
             // Map to 0 when pedal is close to the release point
-            AcceleratorPedalPosition = 0;
+            accelerator_pedal_position = 0;
         }
         else
         {
             // Map accelerator pedal position to 10-bit value using primary APPS
             // reading
-            AcceleratorPedalPosition = PercentPrimaryAPPSValue * 1023;
+            accelerator_pedal_position = percent_primary_apps_value * 1023;
         }
 
         // Prevent FSM_APPS_Fault_State_1 and FSM_APPS_Fault_State_2 from
@@ -137,13 +139,13 @@ uint16_t getAcceleratorPedalPosition(APPS_Mode_Enum Mode)
                         FSM_APPS_PRIMARY_SECONDARY_DIFFERENCE_ERROR;
                 }
                 apps_fault_counter++;
-                FaultFlag = 1;
+                fault_flag = true;
             }
 
             // Check for implausibility between the primary and secondary APPS
             // position readings (>10% difference)
-            if (((PercentPrimaryAPPSValue - PercentSecondaryAPPSValue) > 0.1) ||
-                ((PercentSecondaryAPPSValue - PercentPrimaryAPPSValue) > 0.1))
+            if (((percent_primary_apps_value - percent_secdonary_apps_value) > 0.1) ||
+                ((percent_secdonary_apps_value - percent_primary_apps_value) > 0.1))
             {
                 // Check if implausibility occurs after 100msec
                 if (apps_fault_counter > MAX_APPS_FAULTS)
@@ -151,13 +153,13 @@ uint16_t getAcceleratorPedalPosition(APPS_Mode_Enum Mode)
                     apps_fault_state = FSM_APPS_BRAKE_PEDAL_PLAUSIBILITY_ERROR;
                 }
                 apps_fault_counter++;
-                FaultFlag = 1;
+                fault_flag = true;
             }
         }
 
         // Check for APPS/Brake Pedal Plausibility Check, where accelerator
         // pedal is pushed >25% while brake is pushed (EV2.5)
-        if ((PercentPrimaryAPPSValue > APPS_BPPC_THRESHOLD) &&
+        if ((percent_primary_apps_value > APPS_BPPC_THRESHOLD) &&
             HAL_GPIO_ReadPin(BSPD_BRAKE_THRES_GPIO_Port, BSPD_BRAKE_THRES_Pin))
         {
             // Check if implausibility occurs after 100msec
@@ -166,11 +168,11 @@ uint16_t getAcceleratorPedalPosition(APPS_Mode_Enum Mode)
                 apps_fault_state = FSM_APPS_BRAKE_PEDAL_PLAUSIBILITY_ERROR;
             }
             apps_fault_counter++;
-            FaultFlag = 1;
+            fault_flag = true;
         }
 
         // Check if pedal is at max. torque request
-        if (AcceleratorPedalPosition == 1023)
+        if (accelerator_pedal_position == 1023)
         {
             // Check if pedal is stuck at max. torque after 10 secs
             if (apps_fault_counter > MAX_SATURATION_FAULTS)
@@ -178,7 +180,7 @@ uint16_t getAcceleratorPedalPosition(APPS_Mode_Enum Mode)
                 apps_fault_state = FSM_APPS_MAX_TORQUE_ERROR;
             }
             apps_fault_counter++;
-            FaultFlag = 1;
+            fault_flag = true;
         }
 
         // Check for "APPS / Brake Pedal Plausibility Check" fault state and
@@ -186,8 +188,8 @@ uint16_t getAcceleratorPedalPosition(APPS_Mode_Enum Mode)
         if ((apps_fault_state == FSM_APPS_BRAKE_PEDAL_PLAUSIBILITY_ERROR) ||
             (apps_fault_state == FSM_APPS_MAX_TORQUE_ERROR))
         {
-            AcceleratorPedalPosition = 0;
-            if (PercentPrimaryAPPSValue < 0.05)
+            accelerator_pedal_position = 0;
+            if (percent_primary_apps_value < 0.05)
             {
                 // Reset fault variables to normal operational state
                 apps_fault_state   = FSM_APPS_NORMAL_OPERATION;
@@ -197,14 +199,14 @@ uint16_t getAcceleratorPedalPosition(APPS_Mode_Enum Mode)
         else if (
             (apps_fault_state == FSM_APPS_OPEN_CIRCUIT_SHORT_CIRCUIT_ERROR ||
              apps_fault_state == FSM_APPS_PRIMARY_SECONDARY_DIFFERENCE_ERROR) &&
-            (FaultFlag == 1))
+            (fault_flag == true))
         {
             // Set pedal position to zero if in error state AND fault flag is
             // triggered Need the fault flag check since FSM may latch onto
             // these error states
-            AcceleratorPedalPosition = 0;
+            accelerator_pedal_position = 0;
         }
-        else if (FaultFlag)
+        else if (fault_flag)
         {
             // Keep fault state at normal operation
             // BUT allow apps_fault_counter to increase
@@ -221,8 +223,8 @@ uint16_t getAcceleratorPedalPosition(APPS_Mode_Enum Mode)
     // Reload apps_fault_counter in Mode = 0
     if (Mode == 0)
     {
-        apps_fault_counter = TemporaryAPPSFaultCounter;
+        apps_fault_counter = temporary_apps_fault_counter;
     }
 
-    return AcceleratorPedalPosition;
+    return accelerator_pedal_position;
 }
