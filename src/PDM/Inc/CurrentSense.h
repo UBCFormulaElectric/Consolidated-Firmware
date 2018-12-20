c52d53b921792bc57dfe24a986eb37031f9867ad
thekenu <thekenu@gmail.com>
2018-12-19 22:32:27 -0600

    misc changes

--- src/PDM/Inc/CurrentSense.h
+++ src/PDM/Inc/CurrentSense.h
@@ -12,7 +12,6 @@
 #include "Gpio.h"
 #include "stm32f3xx_hal.h"
 #include "arm_math.h"
-#include "Constants.h"
 
 /******************************************************************************
  * Preprocessor Constants
@@ -175,8 +174,8 @@
 // into a struct?
 // TODO (Issue #191): Can this not be a static const? Or can it be in .c file
 // instead at least
-static const float32_t VOLTAGE_TO_CURRENT[NUM_ADC_CHANNELS * NUM_EFUSES_PER_PROFET2] =
-{
+static const float32_t 
+    VOLTAGE_TO_CURRENT[NUM_ADC_CHANNELS * NUM_EFUSES_PER_PROFET2] = {
     CURRENT_SCALING_AUX / SENSE_RESISTANCE,
     CURRENT_SCALING / SENSE_RESISTANCE,
     CURRENT_SCALING / SENSE_RESISTANCE,
@@ -199,7 +198,8 @@ static const float32_t VOLTAGE_TO_CURRENT[NUM_ADC_CHANNELS * NUM_EFUSES_PER_PROF
 // instead at least 3 retries for all outputs except FANS/COOLING which have 10
 // retries to account for inrush, and 1 retry for VICOR poweroff
 static const uint8_t MAX_FAULTS[NUM_ADC_CHANNELS * NUM_EFUSES_PER_PROFET2] = {
-    3, 10, 3, 10, 3, 3, 3, 1, 3, 3, 3, 10, 3, 3, 3, 1};
+    3, 10, 3, 10, 3, 3, 3, 1, 3, 3, 3, 10, 3, 3, 3, 1
+};
 
 /******************************************************************************
  * Function Prototypes
