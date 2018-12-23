7c91993de526487527d1785a7a5f02555ce75200
Gareth Ellis <gareth.ellis0@gmail.com>
2018-12-23 07:53:53 -0700

    Implemented CAN Testing Framework

---
+++ src/shared/CanTest/Inc/CanTest.h
@@ -0,0 +1,111 @@
+/**
+ * @file  CanTest.h
+ * @brief Our infrastructure for running tests over CAN
+ */
+#ifndef CAN_TEST_H
+#define CAN_TEST_H
+
+/******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include <stdbool.h>
+#include <stdint.h>
+#include "SharedCan.h"
+
+/******************************************************************************
+ * Preprocessor Constants
+ ******************************************************************************/
+// clang-format off
+
+/******************************************************************************
+ * Preprocessor Macros
+ ******************************************************************************/
+
+// Create a test with given ID
+// TODO: We should be explicitly checking the test_id is valid `uint16_t`
+#define CANTEST_TEST(TEST_ID) \ 
+    void CanTest_Test_##TEST_ID(CanTest_TestResult* test_status)
+
+// Assert a given condition within a CANTEST_TEST. If the condition is false,
+// then set the appropriate flags and exit the test
+#define CANTEST_ASSERT(COND) \
+    if (!( COND )) { \
+        test_status->test_passed = false; \
+        return; \
+    }
+
+// Set the extra values that any test can return.
+// TODO: explicitly cast these values to uint8_t?
+#define CANTEST_SET_EXTRA_VALUES(expr0, expr1, expr2, expr3) \
+    test_status->extra_values[0] = expr0; \
+    test_status->extra_values[1] = expr1; \
+    test_status->extra_values[2] = expr2; \
+    test_status->extra_values[3] = expr3;
+
+// Start a list of tests (usually within CanTest_runTest)
+#define CANTEST_START_TESTS \
+    CanTest_TestResult test_status = \
+    { \
+        .valid_test = true, \
+        .test_passed = true, \
+        .extra_values = { 0, 0, 0, 0 } \
+    }; \
+    switch(test_id) {
+
+// Add a test with the given ID to a list of tests
+#define CANTEST_ADD_TEST(TEST_ID) \
+        case TEST_ID: \
+            CanTest_Test_##TEST_ID(&test_status); \
+            break;
+
+// End a list of tests (usually within CanTest_runTest)
+#define CANTEST_END_TESTS \
+        default: \
+            test_status.valid_test = false; \
+    } \
+    return test_status;
+
+/******************************************************************************
+ * Typedefs
+ ******************************************************************************/
+// clang-format on
+
+typedef uint16_t CanTest_TestId;
+
+/** This struct holds the results of attempting to run a given test */
+typedef struct
+{
+    // This indicates if the test we tried to run is a valid test or not
+    bool valid_test;
+    // This indicates if the test we tried to run passed or not
+    bool test_passed;
+    // These 4 bytes are used to return custom values (if needed) from each test
+    uint8_t extra_values[4];
+} CanTest_TestResult;
+
+
+/******************************************************************************
+ * Global Variables
+ ******************************************************************************/
+
+/******************************************************************************
+ * Function Prototypes
+ ******************************************************************************/
+
+/**
+ * @brief Attempt to run the test with the given ID
+ * @param test_id The id of the test to run
+ * @return A CanTest_TestResult indicating the result of running the given test
+ */
+CanTest_TestResult CanTest_runTestWithId(CanTest_TestId test_id);
+
+// TODO: We _really_ should make this description a bit better
+/**
+ * @brief If the message given is a valid test message, attempts to run the 
+ *        test that it specifies. If the test is invalid, passes, or fails, 
+ *        it will broadcast a CAN message indicating as such. 
+ * @param rx_msg a CAN message that may or may not be a test message
+ */
+void CanTest_handleCanMsg(CanRxMsg_Struct rx_msg);
+
+#endif /* CAN_TEST_H */
---
+++ src/shared/CanTest/Src/CanTest.c
@@ -0,0 +1,116 @@
+/******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include "CanTest.h"
+
+/******************************************************************************
+ * Module Preprocessor Constants
+ ******************************************************************************/
+#ifdef PDM
+    // TODO
+#elif FSM
+    // TODO
+#elif BMS
+    #define CANTEST_START_MSG_STDID     TEST_START_BMS_STDID
+    #define CANTEST_RESULT_MSG_STDID    TEST_RESULT_BMS_STDID
+    #define CANTEST_START_MSG_DLC       TEST_START_BMS_DLC
+    #define CANTEST_RESULT_MSG_DLC      TEST_RESULT_BMS_DLC
+#elif FSM
+    // TODO
+#elif DCM
+    // TODO
+#else
+    #error "CanTest - No valid PCB name selected"
+#endif
+
+// For this library to work, we require that the start and result messsages
+// have the same format
+// TODO: We should specify the layout for the start and result messages (probably in the .sym file)
+_Static_assert(CANTEST_START_MSG_DLC == 2, 
+        "CanTest Start message data length != 2");
+_Static_assert(CANTEST_RESULT_MSG_DLC == 8, 
+        "CanTest Result message data length != 8");
+
+/******************************************************************************
+ * Module Preprocessor Macros
+ ******************************************************************************/
+
+/******************************************************************************
+ * Module Typedefs
+ ******************************************************************************/
+
+/******************************************************************************
+ * Module Variable Definitions
+ ******************************************************************************/
+
+/******************************************************************************
+* Private Function Prototypes
+*******************************************************************************/
+
+// TODO: Should private functions like this be prefixed with `CanTest_`?
+/**
+ * @brief Convert the given TestStatus into a 8 element 8-byte array for 
+ *        transmission over CAN
+ * @param test_id The id of the test that the given results are for
+ * @param test_result The TestStatus to convert into an array for a CAN message
+ * @param test_result_msg An 8-element array in which the CAN representation of
+ *                        the given test_result and test_id will be placed
+ */
+void testResultToCanMsg(CanTest_TestId test_id, CanTest_TestResult test_result, uint8_t* test_result_msg);
+
+/******************************************************************************
+ * Private Function Definitions
+ ******************************************************************************/
+
+void testResultToCanMsg(CanTest_TestId test_id, CanTest_TestResult test_result, uint8_t* test_result_msg){
+    // Pack the booleans values into the zero'th byte
+    test_result_msg[0] = test_result.valid_test + (test_result.test_passed << 1);
+
+    // Use the first and second bytes to store the test ID
+    // TODO: Is there a proper order (in terms of where the LSB shoud go, either 1 or 2)? Either way, should leave a more descriptive comment explaining this
+    test_result_msg[1] = (test_id >> 8);
+    test_result_msg[2] = (test_id & (0 << 8));
+
+    // NOTE: Byte 3 is being saved here for potential future use
+
+    // Use the last 4 bytes to store any extra data from the test
+    test_result_msg[4] = test_result.extra_values[0];
+    test_result_msg[5] = test_result.extra_values[1];
+    test_result_msg[6] = test_result.extra_values[2];
+    test_result_msg[7] = test_result.extra_values[3];
+}
+
+/******************************************************************************
+ * Function Definitions
+ ******************************************************************************/
+
+//CanTest_TestResult CanTest_runTest(uint16_t test_id)
+//{
+//    /* NOTE: This function Should not be modified, when the callback is needed,
+//              CanTest_runTest could be implemented in the CanTest.c file */
+//    CANTEST_START_TESTS;
+//    CANTEST_END_TESTS;
+//}
+
+void CanTest_handleCanMsg(CanRxMsg_Struct rx_msg) {
+    if (rx_msg.rx_header.StdId == CANTEST_START_MSG_STDID) {
+        // TODO: We should _really_ document the structure for the CAN msg somewhere...
+        
+        // Construct the test_id from the CAN message
+        // TODO: Should the order for the MSB and LSB be different? If so 
+        // we should probably switch it in the result as well
+        CanTest_TestId test_id = (rx_msg.data[0] << 8) + rx_msg.data[1];
+
+        // Attempt to run the given test
+        CanTest_TestResult test_result = CanTest_runTestWithId(test_id);
+
+        // Send the result of running the test over CAN
+        uint8_t test_result_msg[8];
+        testResultToCanMsg(test_id, test_result, test_result_msg);
+        SharedCan_TransmitDataCan(
+                CANTEST_RESULT_MSG_STDID, 
+                CANTEST_RESULT_MSG_DLC,
+                test_result_msg
+                );
+    }
+}
