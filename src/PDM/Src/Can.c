b3e460ae948ebb4b7a7780f3caf6624c109960ad
thekenu <thekenu@gmail.com>
2018-12-23 00:18:28 -0600

    clang-format

--- src/PDM/Src/Can.c
+++ src/PDM/Src/Can.c
@@ -46,7 +46,7 @@ static void Can_HeartbeatCallback(uint8_t *data)
 void Can_BroadcastAirShutdownError(void)
 {
     // TODO (#Issue 217): Is it ok for payload to be empty?
-    uint8_t data[CAN_PAYLOAD_BYTE_SIZE] = {0};
+    uint8_t data[CAN_PAYLOAD_BYTE_SIZE] = { 0 };
     SharedCan_TransmitDataCan(
         BMS_AIR_SHUTDOWN_ERROR_STDID, BMS_AIR_SHUTDOWN_ERROR_DLC, &data[0]);
 }
@@ -54,7 +54,7 @@ void Can_BroadcastAirShutdownError(void)
 void Can_BroadcastMotorShutdownError(void)
 {
     // TODO (#Issue 217): Is it ok for payload to be empty?
-    uint8_t data[CAN_PAYLOAD_BYTE_SIZE] = {0};
+    uint8_t data[CAN_PAYLOAD_BYTE_SIZE] = { 0 };
     SharedCan_TransmitDataCan(
         SHARED_MOTOR_SHUTDOWN_ERROR_STDID, SHARED_MOTOR_SHUTDOWN_ERROR_DLC,
         &data[0]);
@@ -76,7 +76,7 @@ void Can_RxCommonCallback(CAN_HandleTypeDef *hcan, uint32_t rx_fifo)
 
     switch (rx_msg.rx_header.StdId)
     {
-/* Disable heartbeat callback in DEBUG mode because breakpoints will stop 
+/* Disable heartbeat callback in DEBUG mode because breakpoints will stop
  * heartbeat broadcasting */
 #ifndef DEBUG
         case PDM_HEARTBEAT_STDID:
