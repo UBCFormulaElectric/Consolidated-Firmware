e6cac0221f55484e3720971f702ba29866d90d19
thekenu <thekenu@gmail.com>
2018-12-27 03:43:49 -0600

    Remove bloated comment

--- src/shared/STM32-CAN-Code/Inc/CanDefinitions.h
+++ src/shared/STM32-CAN-Code/Inc/CanDefinitions.h
@@ -166,21 +166,7 @@ typedef enum
     PCB_COUNT
 } Pcb_Enum;
 
-/** @brief FSM Errors
- *         APPS Fault States
- *         FSM_APPS_NORMAL_OPERATION  Operating normally
- *         FSM_APPS_NORMAL_OPERATION  Operating normally (Pedal deflects
- *         passed resting point and underflows) OR pedal pushed passed max
- *         rotation
- *         FSM_APPS_OPEN_CIRCUIT_SHORT_CIRCUIT_ERROR  Open/short circuit 
- *         on encoder differential lines 
- *         FSM_APPS_PRIMARY_SECONDARY_DIFFERENCE_ERROR  >10% difference 
- *         in primary and secondary APPS readings (EV2.3.6) 
- *         FSM_APPS_BRAKE_PEDAL_PLAUSIBILITY_ERROR  >25% pedal travel while
- *         activating brakes "APPS / Brake Pedal Plausibility Check" (EV2.5)
- *         FSM_APPS_MAX_TORQUE_ERROR  Pedal stuck at max torque for 
- *         greater than 10 secs
- */
+/** @brief FSM Errors */
 typedef enum
 {
     FSM_APPS_NORMAL_OPERATION,
