14aced80b760c10adcdbde7459dc0a075dc2ca85
thekenu <thekenu@gmail.com>
2018-12-29 04:06:19 -0600

    Update LPF function

--- src/shared/Filters/Src/SharedFilters.c
+++ src/shared/Filters/Src/SharedFilters.c
@@ -32,7 +32,12 @@
  ******************************************************************************/
 void SharedFilter_LowPassFilter(uint32_t *input, uint32_t *output, uint32_t smoothing_factor)
 {
-    for (uint32_t i = 0; i < sizeof(output)/sizeof(output[0]); i++)
+    // The pseudo-code for this LPF implementation is as follows:
+    // y[i] = y[i-1] + SmoothingFactor * ( x[i] - y[i-1] ), where y = 
+    // filter output, x = input. That is, the change from one filter output
+    // to the next is proportional to the difference between the previous 
+    // output and the next input.
+    for (uint32_t i = 0; i < sizeof(output) / sizeof(output[0]); i++)
     {
         output[i] = output[i] + smoothing_factor * (input[i] - output[i]);
     }
