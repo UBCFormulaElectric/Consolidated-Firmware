1fc6816e01cbd66cd9bd69b54f1500be03ce80de
thekenu <thekenu@gmail.com>
2019-01-03 21:20:09 -0800

    Add starting index for adc_readings[]

--- src/PDM/Inc/Adc.h
+++ src/PDM/Inc/Adc.h
@@ -17,6 +17,11 @@
  * Preprocessor Constants
  ******************************************************************************/
 // clang-format off
+// Element 0 to 4 of adc_readings[] are current sense values
+#define ADC_READINGS_CURRENT_START_INDEX 0
+
+// Element 5 to 7 of adc_readings[] are voltage sense values
+#define ADC_READINGS_VOLTAGE_START_INDEX 5
 
 /******************************************************************************
 * Preprocessor Macros
@@ -30,10 +35,10 @@
 /******************************************************************************
  * Global Variables
  ******************************************************************************/
-extern ADC_HandleTypeDef      hadc1;
-extern volatile float32_t     converted_readings[];
+extern ADC_HandleTypeDef  hadc1;
+extern volatile float32_t converted_readings[];
 extern volatile uint8_t
-    e_fuse_fault_states[NUM_ADC_CHANNELS * NUM_READINGS_PER_ADC_DMA_TRANSFER];
+                e_fuse_fault_states[NUM_ADC_CHANNELS * NUM_READINGS_PER_ADC_DMA_TRANSFER];
 extern uint32_t adc_readings[NUM_ADC_CHANNELS];
 
 /******************************************************************************
