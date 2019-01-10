69c2e12788ec86665eeba0753890753e1957d849
thekenu <thekenu@gmail.com>
2019-01-09 16:05:50 -0800

    Add reference link

--- src/shared/Filters/Inc/SharedFilters.h
+++ src/shared/Filters/Inc/SharedFilters.h
@@ -44,7 +44,9 @@
  * Function Prototypes
  ******************************************************************************/
 /**
- * @brief Apply low pass filter on a given array of values
+ * @brief Apply low pass filter on a given array of values based on the
+ *        implementation described here:
+ *        https://en.wikipedia.org/wiki/Low-pass_filter#Discrete-time_realization
  * @param input Pointer to an array of input samples
  * @param output Pointer to an array of output samples
  * @param sampling_time Sampling time interval for input and output samples
