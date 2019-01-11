f29a69a23c4f47d41137434920caf1683306fb65
Gareth Ellis <gareth.ellis0@gmail.com>
2019-01-11 14:30:56 -0800

    Sample of new code in FSM/Can.c

--- src/FSM/Src/Can.c
+++ src/FSM/Src/Can.c
@@ -1,8 +1,8 @@
 #include "SharedCan.h"
 #include "CanMsgs.h"
 
-void symbol1_handler(CanMsgs_symbol_t msg){
-
+void symbol1_callback(CanMsgs_symbol1_t msg) {
+    // TODO: Do something with the message here
 }
 
 void Can_RxCommonCallback(CAN_HandleTypeDef *hcan, uint32_t rx_fifo)
@@ -10,24 +10,7 @@ void Can_RxCommonCallback(CAN_HandleTypeDef *hcan, uint32_t rx_fifo)
     CanRxMsg_Struct rx_msg;
     SharedCan_ReceiveDataCan(hcan, rx_fifo, &rx_msg);
 
-    switch(rx_msg.rx_header.StdId) {
-        case CANMSGS_symbol1_FRAME_ID:
-            CanMsgs_symbol1_t msg;
-            if (CanMsgs_symbol1_unpack(&msg, rx_msg.data, rx_msg.rx_header.dlc) != 0) {
-                Error_Handler();
-            }
-            symbol1_handler(msg);
-            break;
-
-        case CANMSGS_SYMBOL2_FRAME_ID:
-            break;
+    SHAREDCAN_PASS_CAN_MSG_TO_CALLBACK(rx_msg.rx_header.StdId, rx_msg.data){
+        SHAREDCAN_CAN_MSG_CALLBACK(symbol1, symbol1_callback);
     }
-
-    // =========================================
-
-    START_CAN_MSG_CALLBACK_LIST();
-
-    DEFINE_CAN_MESSAGE_CALLBACK(symbol1, symbol1_handler);
-
-    END_CAN_MSG_CALLBACK_LIST();
 }
