ba200a890dcb06cd70bb854c9cc7039e52bbfc86
Ken Lin <thekenu@gmail.com>
2019-02-02 01:16:12 -0800

    Add Shared Watchdog module (#301)
    * Add sharedwatchdog module
    
    * Add header
    
    * Fix EOL
    
    * Address PR issues
    
    * Fix README
    
    *  Make function names more readable
    
    * Move hwidg
    
    * Refactor code by removing extern hiwdg
    
    * Fix include upper-case
    
    * Move stdbool to header
    
    * Fix formatting

---
+++ src/shared/Watchdog/Inc/SharedWatchdog.h
@@ -0,0 +1,66 @@
+/**
+ * @file  SharedWatchdog.h
+ * @brief Helper functions to unify the watchdog timer interface across boards
+ */
+#ifndef SHARED_WATCHDOG
+#define SHARED_WATCHDOG
+
+/******************************************************************************
+ * Includes
+ ******************************************************************************/
+// Check for STM32 microcontroller family
+#ifdef STM32F302x8
+// Used in DCM 2017, BMS 2017, and PDM 2018
+#include "stm32f3xx_hal.h"
+#elif STM32F042x6
+// Used in FSM 2017
+#include "stm32f0xx_hal.h"
+#else
+#error \
+    "No valid architecture selected - unable to determine what HAL library to use"
+#endif
+
+#include "stdbool.h"
+
+/******************************************************************************
+ * Preprocessor Constants
+ ******************************************************************************/
+// clang-format off
+
+/******************************************************************************
+ * Preprocessor Macros
+ ******************************************************************************/
+
+/******************************************************************************
+ * Typedefs
+ ******************************************************************************/
+// clang-format on
+
+/******************************************************************************
+ * Global Variables
+ ******************************************************************************/
+
+/******************************************************************************
+ * Function Prototypes
+ ******************************************************************************/
+/**
+ * @brief A wrapper for HAL_IWDG_Refresh that only refreshes the IWDG if the
+ *        handle has already been initialized
+ */
+void SharedWatchdog_RefreshIwdg(void);
+
+/**
+ * @brief Set the boolean flag to indicate that the IWDG handle is initialized
+ *        (To be used at the end of MX_IWDG_Init). And save a copy of the
+ *        pointer to the IWDG handle.
+ * @param hiwdg Pointer to the independent watchdog timer handle
+ */
+void SharedWatchdog_SetIwdgInitialized(IWDG_HandleTypeDef *hiwdg);
+
+/**
+ * @brief  Check if the IWDG handle is initialized yet
+ * @return The current IWDG initialization state
+ */
+bool SharedWatchdog_IsIwdgInitialized(void);
+
+#endif /* SHARED_WATCHDOG */
---
+++ src/shared/Watchdog/README.md
@@ -0,0 +1,49 @@
+## Overview
+The Independent Watchdog (IWDG) is an embedded watchdog peripheral needs to be periodically refreshed using `HAL_IWDG_Refresh(&hiwdg)` or else it will trigger a system reset when the counter reaches a given timeout value. Note that `hiwdg` is the **IWDG** handle. 
+
+It is possible to run `HAL_IWDG_Refresh()` when `hiwdg` is not yet initialized which would result in a Hard Fault. To get around this issue, this library proposes to use `SharedWatchdog_RefreshIwdg()` instead of `HAL_IWDG_Refresh()`, which only calls `HAL_IWDG_Refresh()` only if `hiwdg` has been properly initialized. This requires using `SharedWatchdog_SetIwdgInitialized()` as described below.
+
+
+In addition, we must use `__HAL_DBGMCU_FREEZE_IWDG()` to stop the **IWDG** clock when the CPU core is halted during **debugging**, or else **IWDG** could trigger an undesired system reset during a debug session.
+
+## How to Use This Library
+1. Insert `SharedWatchdog_SetIwdgInitialized(&hiwdg);`:
+```
+// main.c
+
+static void MX_IWDG_Init(void)
+{
+    /* Omitted some code */
+    if (HAL_IWDG_Init(&hiwdg) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN IWDG_Init 2 */
+    SharedWatchdog_SetIwdgInitialized(&hiwdg);
+    /* USER CODE END IWDG_Init 2 */
+}
+```
+1. Insert `__HAL_DBGMCU_FREEZE_IWDG();`:
+```
+// main.c
+
+{
+    /* USER CODE BEGIN 1 */
+    __HAL_DBGMCU_FREEZE_IWDG();
+    /* USER CODE END 1 */
+    
+    /* Omitted some code */
+}
+```
+
+1. Insert `SharedWatchdog_RefreshIwdg()` where needed (e.g. In a 100Hz Control Loop):
+
+```
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+    if (htim->Instance == ControlLoopTimer)
+    {
+        SharedWatchdog_RefreshIwdg();
+    }
+}
+```
