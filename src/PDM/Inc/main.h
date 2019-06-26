5a6917f323c5f488f1c86e584cb8903bab77ee68
Ken Lin <thekenu@gmail.com>
2019-06-26 00:08:16 -0700

    Get a minimal PDM project generated from CLion and STM32CubeMX
    - Initialize new PDM project (Code compiles and can be debugged)
    - Update .gitignore to ignore cmake artifacts
    - Remove cached files that are now untracked`

---
+++ src/PDM/Inc/main.h
@@ -0,0 +1,145 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.h
+ * @brief          : Header for main.c file.
+ *                   This file contains the common defines of the application.
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under Ultimate Liberty license
+ * SLA0044, the "License"; You may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at:
+ *                             www.st.com/SLA0044
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f3xx_hal.h"
+
+    /* Private includes
+     * ----------------------------------------------------------*/
+    /* USER CODE BEGIN Includes */
+
+    /* USER CODE END Includes */
+
+    /* Exported types
+     * ------------------------------------------------------------*/
+    /* USER CODE BEGIN ET */
+
+    /* USER CODE END ET */
+
+    /* Exported constants
+     * --------------------------------------------------------*/
+    /* USER CODE BEGIN EC */
+
+    /* USER CODE END EC */
+
+    /* Exported macro
+     * ------------------------------------------------------------*/
+    /* USER CODE BEGIN EM */
+
+    /* USER CODE END EM */
+
+    /* Exported functions prototypes
+     * ---------------------------------------------*/
+    void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define VBAT_SENSE_Pin GPIO_PIN_1
+#define VBAT_SENSE_GPIO_Port GPIOC
+#define _24V_AUX_SENSE_Pin GPIO_PIN_2
+#define _24V_AUX_SENSE_GPIO_Port GPIOC
+#define _24V_ACC_SENSE_Pin GPIO_PIN_3
+#define _24V_ACC_SENSE_GPIO_Port GPIOC
+#define CUR_SYNC_DI_L_DI_R_Pin GPIO_PIN_0
+#define CUR_SYNC_DI_L_DI_R_GPIO_Port GPIOA
+#define IS_DI_L_DI_R_Pin GPIO_PIN_1
+#define IS_DI_L_DI_R_GPIO_Port GPIOA
+#define PIN_DI_R_Pin GPIO_PIN_2
+#define PIN_DI_R_GPIO_Port GPIOA
+#define PIN_DI_L_Pin GPIO_PIN_3
+#define PIN_DI_L_GPIO_Port GPIOA
+#define CUR_SYNC_GLV_AIR_SHDN_Pin GPIO_PIN_4
+#define CUR_SYNC_GLV_AIR_SHDN_GPIO_Port GPIOA
+#define PIN_AIR_SHDN_Pin GPIO_PIN_5
+#define PIN_AIR_SHDN_GPIO_Port GPIOA
+#define PIN_GLV_Pin GPIO_PIN_6
+#define PIN_GLV_GPIO_Port GPIOA
+#define CSB_DI_L_DI_R_Pin GPIO_PIN_4
+#define CSB_DI_L_DI_R_GPIO_Port GPIOC
+#define CSB_GLV_AIR_SHDN_Pin GPIO_PIN_5
+#define CSB_GLV_AIR_SHDN_GPIO_Port GPIOC
+#define IS_GLV_AIR_SHDN_Pin GPIO_PIN_0
+#define IS_GLV_AIR_SHDN_GPIO_Port GPIOB
+#define IS_AUX1_AUX2_Pin GPIO_PIN_1
+#define IS_AUX1_AUX2_GPIO_Port GPIOB
+#define PIN_AUX2_Pin GPIO_PIN_2
+#define PIN_AUX2_GPIO_Port GPIOB
+#define OBSOLETE_IS_AUX1_AUX2_Pin GPIO_PIN_10
+#define OBSOLETE_IS_AUX1_AUX2_GPIO_Port GPIOB
+#define IS_EM_PUMP_Pin GPIO_PIN_11
+#define IS_EM_PUMP_GPIO_Port GPIOB
+#define PIN_AUX1_Pin GPIO_PIN_12
+#define PIN_AUX1_GPIO_Port GPIOB
+#define SCK_Pin GPIO_PIN_13
+#define SCK_GPIO_Port GPIOB
+#define MISO_Pin GPIO_PIN_14
+#define MISO_GPIO_Port GPIOB
+#define MOSI_Pin GPIO_PIN_15
+#define MOSI_GPIO_Port GPIOB
+#define CUR_SYNC_AUX1_AUX2_Pin GPIO_PIN_6
+#define CUR_SYNC_AUX1_AUX2_GPIO_Port GPIOC
+#define CSB_AUX1_AUX2_Pin GPIO_PIN_7
+#define CSB_AUX1_AUX2_GPIO_Port GPIOC
+#define OBSOLETE_IS_EM_PUMP_Pin GPIO_PIN_8
+#define OBSOLETE_IS_EM_PUMP_GPIO_Port GPIOC
+#define PIN_COOLING_Pin GPIO_PIN_9
+#define PIN_COOLING_GPIO_Port GPIOC
+#define PIN_EM_Pin GPIO_PIN_8
+#define PIN_EM_GPIO_Port GPIOA
+#define CSB_EM_PUMP_Pin GPIO_PIN_9
+#define CSB_EM_PUMP_GPIO_Port GPIOA
+#define CUR_SYNC_EM_PUMP_Pin GPIO_PIN_10
+#define CUR_SYNC_EM_PUMP_GPIO_Port GPIOA
+#define OBSOLETE_CHRG_FAULT_Pin GPIO_PIN_10
+#define OBSOLETE_CHRG_FAULT_GPIO_Port GPIOC
+#define CHRG_LED_Pin GPIO_PIN_11
+#define CHRG_LED_GPIO_Port GPIOC
+#define PGOOD_Pin GPIO_PIN_12
+#define PGOOD_GPIO_Port GPIOC
+#define OV_FAULT_MCU_Pin GPIO_PIN_2
+#define OV_FAULT_MCU_GPIO_Port GPIOD
+#define CHRG_FAULT_Pin GPIO_PIN_8
+#define CHRG_FAULT_GPIO_Port GPIOB
+#define UNUSED_GPIOD_1_Pin GPIO_PIN_9
+#define UNUSED_GPIOD_1_GPIO_Port GPIOB
+    /* USER CODE BEGIN Private defines */
+
+    /* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ src/PDM/Src/main.c
@@ -0,0 +1,535 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.c
+ * @brief          : Main program body
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under Ultimate Liberty license
+ * SLA0044, the "License"; You may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at:
+ *                             www.st.com/SLA0044
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
+
+CAN_HandleTypeDef hcan;
+
+SPI_HandleTypeDef hspi2;
+
+osThreadId          defaultTaskHandle;
+uint32_t            defaultTaskBuffer[128];
+osStaticThreadDef_t defaultTaskControlBlock;
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void        SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_CAN_Init(void);
+static void MX_SPI2_Init(void);
+static void MX_ADC1_Init(void);
+void        StartDefaultTask(void const *argument);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+ * @brief  The application entry point.
+ * @retval int
+ */
+int main(void)
+{
+    /* USER CODE BEGIN 1 */
+
+    /* USER CODE END 1 */
+
+    /* MCU
+     * Configuration--------------------------------------------------------*/
+
+    /* Reset of all peripherals, Initializes the Flash interface and the
+     * Systick. */
+    HAL_Init();
+
+    /* USER CODE BEGIN Init */
+
+    /* USER CODE END Init */
+
+    /* Configure the system clock */
+    SystemClock_Config();
+
+    /* USER CODE BEGIN SysInit */
+
+    /* USER CODE END SysInit */
+
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_CAN_Init();
+    MX_SPI2_Init();
+    MX_ADC1_Init();
+    /* USER CODE BEGIN 2 */
+
+    /* USER CODE END 2 */
+
+    /* USER CODE BEGIN RTOS_MUTEX */
+    /* add mutexes, ... */
+    /* USER CODE END RTOS_MUTEX */
+
+    /* USER CODE BEGIN RTOS_SEMAPHORES */
+    /* add semaphores, ... */
+    /* USER CODE END RTOS_SEMAPHORES */
+
+    /* USER CODE BEGIN RTOS_TIMERS */
+    /* start timers, add new ones, ... */
+    /* USER CODE END RTOS_TIMERS */
+
+    /* USER CODE BEGIN RTOS_QUEUES */
+    /* add queues, ... */
+    /* USER CODE END RTOS_QUEUES */
+
+    /* Create the thread(s) */
+    /* definition and creation of defaultTask */
+    osThreadStaticDef(
+        defaultTask, StartDefaultTask, osPriorityNormal, 0, 128,
+        defaultTaskBuffer, &defaultTaskControlBlock);
+    defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+
+    /* USER CODE BEGIN RTOS_THREADS */
+    /* add threads, ... */
+    /* USER CODE END RTOS_THREADS */
+
+    /* Start scheduler */
+    osKernelStart();
+
+    /* We should never get here as control is now taken by the scheduler */
+
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
+    while (1)
+    {
+        /* USER CODE END WHILE */
+
+        /* USER CODE BEGIN 3 */
+    }
+    /* USER CODE END 3 */
+}
+
+/**
+ * @brief System Clock Configuration
+ * @retval None
+ */
+void SystemClock_Config(void)
+{
+    RCC_OscInitTypeDef       RCC_OscInitStruct = { 0 };
+    RCC_ClkInitTypeDef       RCC_ClkInitStruct = { 0 };
+    RCC_PeriphCLKInitTypeDef PeriphClkInit     = { 0 };
+
+    /** Initializes the CPU, AHB and APB busses clocks
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
+    RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+    RCC_OscInitStruct.HSIState       = RCC_HSI_ON;
+    RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLMUL     = RCC_PLL_MUL9;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /** Initializes the CPU, AHB and APB busses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK |
+                                  RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    PeriphClkInit.PeriphClockSelection = RCC_PERIPHCLK_ADC1;
+    PeriphClkInit.Adc1ClockSelection   = RCC_ADC1PLLCLK_DIV1;
+
+    if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInit) != HAL_OK)
+    {
+        Error_Handler();
+    }
+}
+
+/**
+ * @brief ADC1 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_ADC1_Init(void)
+{
+    /* USER CODE BEGIN ADC1_Init 0 */
+
+    /* USER CODE END ADC1_Init 0 */
+
+    ADC_ChannelConfTypeDef sConfig = { 0 };
+
+    /* USER CODE BEGIN ADC1_Init 1 */
+
+    /* USER CODE END ADC1_Init 1 */
+    /** Common config
+     */
+    hadc1.Instance                   = ADC1;
+    hadc1.Init.ClockPrescaler        = ADC_CLOCK_ASYNC_DIV1;
+    hadc1.Init.Resolution            = ADC_RESOLUTION_12B;
+    hadc1.Init.ScanConvMode          = ADC_SCAN_DISABLE;
+    hadc1.Init.ContinuousConvMode    = DISABLE;
+    hadc1.Init.DiscontinuousConvMode = DISABLE;
+    hadc1.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    hadc1.Init.ExternalTrigConv      = ADC_SOFTWARE_START;
+    hadc1.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
+    hadc1.Init.NbrOfConversion       = 1;
+    hadc1.Init.DMAContinuousRequests = DISABLE;
+    hadc1.Init.EOCSelection          = ADC_EOC_SINGLE_CONV;
+    hadc1.Init.LowPowerAutoWait      = DISABLE;
+    hadc1.Init.Overrun               = ADC_OVR_DATA_OVERWRITTEN;
+    if (HAL_ADC_Init(&hadc1) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /** Configure Regular Channel
+     */
+    sConfig.Channel      = ADC_CHANNEL_11;
+    sConfig.Rank         = ADC_REGULAR_RANK_1;
+    sConfig.SingleDiff   = ADC_SINGLE_ENDED;
+    sConfig.SamplingTime = ADC_SAMPLETIME_1CYCLE_5;
+    sConfig.OffsetNumber = ADC_OFFSET_NONE;
+    sConfig.Offset       = 0;
+    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN ADC1_Init 2 */
+
+    /* USER CODE END ADC1_Init 2 */
+}
+
+/**
+ * @brief CAN Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_CAN_Init(void)
+{
+    /* USER CODE BEGIN CAN_Init 0 */
+
+    /* USER CODE END CAN_Init 0 */
+
+    /* USER CODE BEGIN CAN_Init 1 */
+
+    /* USER CODE END CAN_Init 1 */
+    hcan.Instance                  = CAN;
+    hcan.Init.Prescaler            = 16;
+    hcan.Init.Mode                 = CAN_MODE_NORMAL;
+    hcan.Init.SyncJumpWidth        = CAN_SJW_1TQ;
+    hcan.Init.TimeSeg1             = CAN_BS1_1TQ;
+    hcan.Init.TimeSeg2             = CAN_BS2_1TQ;
+    hcan.Init.TimeTriggeredMode    = DISABLE;
+    hcan.Init.AutoBusOff           = DISABLE;
+    hcan.Init.AutoWakeUp           = DISABLE;
+    hcan.Init.AutoRetransmission   = DISABLE;
+    hcan.Init.ReceiveFifoLocked    = DISABLE;
+    hcan.Init.TransmitFifoPriority = DISABLE;
+    if (HAL_CAN_Init(&hcan) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN CAN_Init 2 */
+
+    /* USER CODE END CAN_Init 2 */
+}
+
+/**
+ * @brief SPI2 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_SPI2_Init(void)
+{
+    /* USER CODE BEGIN SPI2_Init 0 */
+
+    /* USER CODE END SPI2_Init 0 */
+
+    /* USER CODE BEGIN SPI2_Init 1 */
+
+    /* USER CODE END SPI2_Init 1 */
+    /* SPI2 parameter configuration*/
+    hspi2.Instance               = SPI2;
+    hspi2.Init.Mode              = SPI_MODE_MASTER;
+    hspi2.Init.Direction         = SPI_DIRECTION_2LINES;
+    hspi2.Init.DataSize          = SPI_DATASIZE_4BIT;
+    hspi2.Init.CLKPolarity       = SPI_POLARITY_LOW;
+    hspi2.Init.CLKPhase          = SPI_PHASE_1EDGE;
+    hspi2.Init.NSS               = SPI_NSS_SOFT;
+    hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+    hspi2.Init.FirstBit          = SPI_FIRSTBIT_MSB;
+    hspi2.Init.TIMode            = SPI_TIMODE_DISABLE;
+    hspi2.Init.CRCCalculation    = SPI_CRCCALCULATION_DISABLE;
+    hspi2.Init.CRCPolynomial     = 7;
+    hspi2.Init.CRCLength         = SPI_CRC_LENGTH_DATASIZE;
+    hspi2.Init.NSSPMode          = SPI_NSS_PULSE_ENABLE;
+    if (HAL_SPI_Init(&hspi2) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN SPI2_Init 2 */
+
+    /* USER CODE END SPI2_Init 2 */
+}
+
+/**
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_GPIO_Init(void)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+
+    /* GPIO Ports Clock Enable */
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    __HAL_RCC_GPIOF_CLK_ENABLE();
+    __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_GPIOD_CLK_ENABLE();
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(
+        GPIOA,
+        PIN_DI_R_Pin | PIN_DI_L_Pin | PIN_AIR_SHDN_Pin | PIN_GLV_Pin |
+            PIN_EM_Pin,
+        GPIO_PIN_RESET);
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(
+        GPIOC, CSB_DI_L_DI_R_Pin | CSB_GLV_AIR_SHDN_Pin | CSB_AUX1_AUX2_Pin,
+        GPIO_PIN_SET);
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(GPIOB, PIN_AUX2_Pin | PIN_AUX1_Pin, GPIO_PIN_RESET);
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(PIN_COOLING_GPIO_Port, PIN_COOLING_Pin, GPIO_PIN_RESET);
+
+    /*Configure GPIO pin Output Level */
+    HAL_GPIO_WritePin(CSB_EM_PUMP_GPIO_Port, CSB_EM_PUMP_Pin, GPIO_PIN_SET);
+
+    /*Configure GPIO pins : PC13 PC14 PC15 PC0 */
+    GPIO_InitStruct.Pin  = GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15 | GPIO_PIN_0;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : CUR_SYNC_DI_L_DI_R_Pin CUR_SYNC_GLV_AIR_SHDN_Pin
+     * CUR_SYNC_EM_PUMP_Pin */
+    GPIO_InitStruct.Pin = CUR_SYNC_DI_L_DI_R_Pin | CUR_SYNC_GLV_AIR_SHDN_Pin |
+                          CUR_SYNC_EM_PUMP_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : PIN_DI_R_Pin PIN_DI_L_Pin PIN_AIR_SHDN_Pin
+       PIN_GLV_Pin PIN_EM_Pin */
+    GPIO_InitStruct.Pin = PIN_DI_R_Pin | PIN_DI_L_Pin | PIN_AIR_SHDN_Pin |
+                          PIN_GLV_Pin | PIN_EM_Pin;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_OD;
+    GPIO_InitStruct.Pull  = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : PA7 PA15 */
+    GPIO_InitStruct.Pin  = GPIO_PIN_7 | GPIO_PIN_15;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : CSB_DI_L_DI_R_Pin CSB_GLV_AIR_SHDN_Pin
+     * CSB_AUX1_AUX2_Pin */
+    GPIO_InitStruct.Pin =
+        CSB_DI_L_DI_R_Pin | CSB_GLV_AIR_SHDN_Pin | CSB_AUX1_AUX2_Pin;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull  = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : PIN_AUX2_Pin PIN_AUX1_Pin */
+    GPIO_InitStruct.Pin   = PIN_AUX2_Pin | PIN_AUX1_Pin;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_OD;
+    GPIO_InitStruct.Pull  = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : OBSOLETE_IS_AUX1_AUX2_Pin UNUSED_GPIOD_1_Pin */
+    GPIO_InitStruct.Pin  = OBSOLETE_IS_AUX1_AUX2_Pin | UNUSED_GPIOD_1_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : CUR_SYNC_AUX1_AUX2_Pin CHRG_LED_Pin PGOOD_Pin */
+    GPIO_InitStruct.Pin  = CUR_SYNC_AUX1_AUX2_Pin | CHRG_LED_Pin | PGOOD_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : OBSOLETE_IS_EM_PUMP_Pin OBSOLETE_CHRG_FAULT_Pin */
+    GPIO_InitStruct.Pin  = OBSOLETE_IS_EM_PUMP_Pin | OBSOLETE_CHRG_FAULT_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : PIN_COOLING_Pin */
+    GPIO_InitStruct.Pin   = PIN_COOLING_Pin;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_OD;
+    GPIO_InitStruct.Pull  = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(PIN_COOLING_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : CSB_EM_PUMP_Pin */
+    GPIO_InitStruct.Pin   = CSB_EM_PUMP_Pin;
+    GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
+    GPIO_InitStruct.Pull  = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+    HAL_GPIO_Init(CSB_EM_PUMP_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : OV_FAULT_MCU_Pin */
+    GPIO_InitStruct.Pin  = OV_FAULT_MCU_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(OV_FAULT_MCU_GPIO_Port, &GPIO_InitStruct);
+
+    /*Configure GPIO pins : PB4 PB5 PB6 PB7 */
+    GPIO_InitStruct.Pin  = GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7;
+    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : CHRG_FAULT_Pin */
+    GPIO_InitStruct.Pin  = CHRG_FAULT_Pin;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(CHRG_FAULT_GPIO_Port, &GPIO_InitStruct);
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+ * @brief  Function implementing the defaultTask thread.
+ * @param  argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void const *argument)
+{
+    /* USER CODE BEGIN 5 */
+    /* Infinite loop */
+    for (;;)
+    {
+        osDelay(1);
+    }
+    /* USER CODE END 5 */
+}
+
+/**
+ * @brief  Period elapsed callback in non blocking mode
+ * @note   This function is called  when TIM1 interrupt took place, inside
+ * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+ * a global variable "uwTick" used as application time base.
+ * @param  htim : TIM handle
+ * @retval None
+ */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+    /* USER CODE BEGIN Callback 0 */
+
+    /* USER CODE END Callback 0 */
+    if (htim->Instance == TIM1)
+    {
+        HAL_IncTick();
+    }
+    /* USER CODE BEGIN Callback 1 */
+
+    /* USER CODE END Callback 1 */
+}
+
+/**
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
+void Error_Handler(void)
+{
+    /* USER CODE BEGIN Error_Handler_Debug */
+    /* User can add his own implementation to report the HAL error return state
+     */
+
+    /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef USE_FULL_ASSERT
+/**
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
+void assert_failed(char *file, uint32_t line)
+{
+    /* USER CODE BEGIN 6 */
+    /* User can add his own implementation to report the file name and line
+       number, tex: printf("Wrong parameters value: file %s on line %d\r\n",
+       file, line) */
+    /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
