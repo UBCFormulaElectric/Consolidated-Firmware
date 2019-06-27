58219a2491eb125b98e43c36549e95d91cbfd052
Ken Lin <thekenu@gmail.com>
2019-06-26 20:42:30 -0700

    Get a minimal DCM project generated from CLion and STM32CubeMX
    - Initialize new DCM project (Code compiles and can be debugged)
    - Update .gitignore to ignore cmake artifacts
    - Remove cached files that are now untracked

---
+++ src/DCM/Inc/main.h
@@ -0,0 +1,117 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * @file           : main.h
+ * @brief          : Header for main.c file.
+ *                   This file contains the common defines of the application.
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under Ultimate Liberty license
+ * SLA0044, the "License"; You may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at:
+ *                             www.st.com/SLA0044
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f3xx_hal.h"
+
+    /* Private includes
+     * ----------------------------------------------------------*/
+    /* USER CODE BEGIN Includes */
+
+    /* USER CODE END Includes */
+
+    /* Exported types
+     * ------------------------------------------------------------*/
+    /* USER CODE BEGIN ET */
+
+    /* USER CODE END ET */
+
+    /* Exported constants
+     * --------------------------------------------------------*/
+    /* USER CODE BEGIN EC */
+
+    /* USER CODE END EC */
+
+    /* Exported macro
+     * ------------------------------------------------------------*/
+    /* USER CODE BEGIN EM */
+
+    /* USER CODE END EM */
+
+    /* Exported functions prototypes
+     * ---------------------------------------------*/
+    void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define UNUSED_ANALOG_IN1_Pin GPIO_PIN_0
+#define UNUSED_ANALOG_IN1_GPIO_Port GPIOA
+#define UNUSED_ANALOG_IN2_Pin GPIO_PIN_1
+#define UNUSED_ANALOG_IN2_GPIO_Port GPIOA
+#define UNUSED_ANALOG_IN3_Pin GPIO_PIN_2
+#define UNUSED_ANALOG_IN3_GPIO_Port GPIOA
+#define UNUSED_ANALOG_OUT_Pin GPIO_PIN_4
+#define UNUSED_ANALOG_OUT_GPIO_Port GPIOA
+#define UNUSED_GPIO1_Pin GPIO_PIN_5
+#define UNUSED_GPIO1_GPIO_Port GPIOA
+#define UNUSED_GPIO2_Pin GPIO_PIN_6
+#define UNUSED_GPIO2_GPIO_Port GPIOA
+#define UNUSED_GPIO3_Pin GPIO_PIN_7
+#define UNUSED_GPIO3_GPIO_Port GPIOA
+#define UNUSED_GPIO4_Pin GPIO_PIN_0
+#define UNUSED_GPIO4_GPIO_Port GPIOB
+#define UNUSED_GPIO5_Pin GPIO_PIN_1
+#define UNUSED_GPIO5_GPIO_Port GPIOB
+#define UNUSED_GPIO6_Pin GPIO_PIN_2
+#define UNUSED_GPIO6_GPIO_Port GPIOB
+#define INVERTER_L_RUN_Pin GPIO_PIN_10
+#define INVERTER_L_RUN_GPIO_Port GPIOB
+#define INVERTER_R_RUN_Pin GPIO_PIN_11
+#define INVERTER_R_RUN_GPIO_Port GPIOB
+#define IMON_Pin GPIO_PIN_13
+#define IMON_GPIO_Port GPIOB
+#define IMU_INT_Pin GPIO_PIN_14
+#define IMU_INT_GPIO_Port GPIOB
+#define STATUS_R_Pin GPIO_PIN_8
+#define STATUS_R_GPIO_Port GPIOA
+#define STATUS_G_Pin GPIO_PIN_9
+#define STATUS_G_GPIO_Port GPIOA
+#define STATUS_B_Pin GPIO_PIN_10
+#define STATUS_B_GPIO_Port GPIOA
+#define BSPD_BRAKE_THRES_Pin GPIO_PIN_15
+#define BSPD_BRAKE_THRES_GPIO_Port GPIOA
+#define BUZZER_EN_Pin GPIO_PIN_4
+#define BUZZER_EN_GPIO_Port GPIOB
+#define BRAKE_LIGHT_EN_Pin GPIO_PIN_5
+#define BRAKE_LIGHT_EN_GPIO_Port GPIOB
+    /* USER CODE BEGIN Private defines */
+
+    /* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ src/DCM/Src/stm32f3xx_hal_msp.c
@@ -0,0 +1,284 @@
+/* USER CODE BEGIN Header */
+/**
+ ******************************************************************************
+ * File Name          : stm32f3xx_hal_msp.c
+ * Description        : This file provides code for the MSP Initialization
+ *                      and de-Initialization codes.
+ ******************************************************************************
+ * @attention
+ *
+ * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+ * All rights reserved.</center></h2>
+ *
+ * This software component is licensed by ST under Ultimate Liberty license
+ * SLA0044, the "License"; You may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at:
+ *                             www.st.com/SLA0044
+ *
+ ******************************************************************************
+ */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN TD */
+
+/* USER CODE END TD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN Define */
+
+/* USER CODE END Define */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN Macro */
+
+/* USER CODE END Macro */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* External functions --------------------------------------------------------*/
+/* USER CODE BEGIN ExternalFunctions */
+
+/* USER CODE END ExternalFunctions */
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+/**
+ * Initializes the Global MSP.
+ */
+void HAL_MspInit(void)
+{
+    /* USER CODE BEGIN MspInit 0 */
+
+    /* USER CODE END MspInit 0 */
+
+    __HAL_RCC_SYSCFG_CLK_ENABLE();
+    __HAL_RCC_PWR_CLK_ENABLE();
+
+    /* System interrupt init*/
+    /* PendSV_IRQn interrupt configuration */
+    HAL_NVIC_SetPriority(PendSV_IRQn, 15, 0);
+
+    /* USER CODE BEGIN MspInit 1 */
+
+    /* USER CODE END MspInit 1 */
+}
+
+/**
+ * @brief ADC MSP Initialization
+ * This function configures the hardware resources used in this example
+ * @param hadc: ADC handle pointer
+ * @retval None
+ */
+void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+    if (hadc->Instance == ADC1)
+    {
+        /* USER CODE BEGIN ADC1_MspInit 0 */
+
+        /* USER CODE END ADC1_MspInit 0 */
+        /* Peripheral clock enable */
+        __HAL_RCC_ADC1_CLK_ENABLE();
+
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        __HAL_RCC_GPIOB_CLK_ENABLE();
+        /**ADC1 GPIO Configuration
+        PA0     ------> ADC1_IN1
+        PA1     ------> ADC1_IN2
+        PA2     ------> ADC1_IN3
+        PB13     ------> ADC1_IN13
+        */
+        GPIO_InitStruct.Pin = UNUSED_ANALOG_IN1_Pin | UNUSED_ANALOG_IN2_Pin |
+                              UNUSED_ANALOG_IN3_Pin;
+        GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+        GPIO_InitStruct.Pin  = IMON_Pin;
+        GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(IMON_GPIO_Port, &GPIO_InitStruct);
+
+        /* USER CODE BEGIN ADC1_MspInit 1 */
+
+        /* USER CODE END ADC1_MspInit 1 */
+    }
+}
+
+/**
+ * @brief ADC MSP De-Initialization
+ * This function freeze the hardware resources used in this example
+ * @param hadc: ADC handle pointer
+ * @retval None
+ */
+void HAL_ADC_MspDeInit(ADC_HandleTypeDef *hadc)
+{
+    if (hadc->Instance == ADC1)
+    {
+        /* USER CODE BEGIN ADC1_MspDeInit 0 */
+
+        /* USER CODE END ADC1_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_ADC1_CLK_DISABLE();
+
+        /**ADC1 GPIO Configuration
+        PA0     ------> ADC1_IN1
+        PA1     ------> ADC1_IN2
+        PA2     ------> ADC1_IN3
+        PB13     ------> ADC1_IN13
+        */
+        HAL_GPIO_DeInit(
+            GPIOA, UNUSED_ANALOG_IN1_Pin | UNUSED_ANALOG_IN2_Pin |
+                       UNUSED_ANALOG_IN3_Pin);
+
+        HAL_GPIO_DeInit(IMON_GPIO_Port, IMON_Pin);
+
+        /* USER CODE BEGIN ADC1_MspDeInit 1 */
+
+        /* USER CODE END ADC1_MspDeInit 1 */
+    }
+}
+
+/**
+ * @brief CAN MSP Initialization
+ * This function configures the hardware resources used in this example
+ * @param hcan: CAN handle pointer
+ * @retval None
+ */
+void HAL_CAN_MspInit(CAN_HandleTypeDef *hcan)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+    if (hcan->Instance == CAN)
+    {
+        /* USER CODE BEGIN CAN_MspInit 0 */
+
+        /* USER CODE END CAN_MspInit 0 */
+        /* Peripheral clock enable */
+        __HAL_RCC_CAN1_CLK_ENABLE();
+
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        /**CAN GPIO Configuration
+        PA11     ------> CAN_RX
+        PA12     ------> CAN_TX
+        */
+        GPIO_InitStruct.Pin       = GPIO_PIN_11 | GPIO_PIN_12;
+        GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull      = GPIO_NOPULL;
+        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_HIGH;
+        GPIO_InitStruct.Alternate = GPIO_AF9_TIM1;
+        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+        /* USER CODE BEGIN CAN_MspInit 1 */
+
+        /* USER CODE END CAN_MspInit 1 */
+    }
+}
+
+/**
+ * @brief CAN MSP De-Initialization
+ * This function freeze the hardware resources used in this example
+ * @param hcan: CAN handle pointer
+ * @retval None
+ */
+void HAL_CAN_MspDeInit(CAN_HandleTypeDef *hcan)
+{
+    if (hcan->Instance == CAN)
+    {
+        /* USER CODE BEGIN CAN_MspDeInit 0 */
+
+        /* USER CODE END CAN_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_CAN1_CLK_DISABLE();
+
+        /**CAN GPIO Configuration
+        PA11     ------> CAN_RX
+        PA12     ------> CAN_TX
+        */
+        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11 | GPIO_PIN_12);
+
+        /* USER CODE BEGIN CAN_MspDeInit 1 */
+
+        /* USER CODE END CAN_MspDeInit 1 */
+    }
+}
+
+/**
+ * @brief DAC MSP Initialization
+ * This function configures the hardware resources used in this example
+ * @param hdac: DAC handle pointer
+ * @retval None
+ */
+void HAL_DAC_MspInit(DAC_HandleTypeDef *hdac)
+{
+    GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+    if (hdac->Instance == DAC)
+    {
+        /* USER CODE BEGIN DAC_MspInit 0 */
+
+        /* USER CODE END DAC_MspInit 0 */
+        /* Peripheral clock enable */
+        __HAL_RCC_DAC1_CLK_ENABLE();
+
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        /**DAC GPIO Configuration
+        PA4     ------> DAC_OUT1
+        */
+        GPIO_InitStruct.Pin  = UNUSED_ANALOG_OUT_Pin;
+        GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(UNUSED_ANALOG_OUT_GPIO_Port, &GPIO_InitStruct);
+
+        /* USER CODE BEGIN DAC_MspInit 1 */
+
+        /* USER CODE END DAC_MspInit 1 */
+    }
+}
+
+/**
+ * @brief DAC MSP De-Initialization
+ * This function freeze the hardware resources used in this example
+ * @param hdac: DAC handle pointer
+ * @retval None
+ */
+void HAL_DAC_MspDeInit(DAC_HandleTypeDef *hdac)
+{
+    if (hdac->Instance == DAC)
+    {
+        /* USER CODE BEGIN DAC_MspDeInit 0 */
+
+        /* USER CODE END DAC_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_DAC1_CLK_DISABLE();
+
+        /**DAC GPIO Configuration
+        PA4     ------> DAC_OUT1
+        */
+        HAL_GPIO_DeInit(UNUSED_ANALOG_OUT_GPIO_Port, UNUSED_ANALOG_OUT_Pin);
+
+        /* USER CODE BEGIN DAC_MspDeInit 1 */
+
+        /* USER CODE END DAC_MspDeInit 1 */
+    }
+}
+
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
