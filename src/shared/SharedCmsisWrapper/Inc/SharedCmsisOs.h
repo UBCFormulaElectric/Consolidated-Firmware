f62669e5d43f7e7ca71cd94d7bf8237d9211526e
Ken Lin <thekenu@gmail.com>
2019-07-20 09:04:55 -0700

    Create two periodic tasks and refresh the IWDG so DCM doesn't keep rebooting

---
+++ src/shared/SharedCmsisWrapper/Inc/SharedCmsisOs.h
@@ -0,0 +1,46 @@
+/**
+ * @file  SharedCmsisOs.h
+ * @brief Shared library with wrapper functions for cmsis_os.h
+ */
+#ifndef SHARED_CMSIS_OS_H
+#define SHARED_CMSIS_OS_H
+
+/******************************************************************************
+ * Includes
+ ******************************************************************************/
+#include "cmsis_os.h"
+
+/******************************************************************************
+ * Preprocessor Constants
+ ******************************************************************************/
+// clang-format off
+
+/******************************************************************************
+ * Preprocessor Macros
+ ******************************************************************************/
+
+/******************************************************************************
+ * Typedefs
+ ******************************************************************************/
+// clang-format on
+
+/******************************************************************************
+ * Global Variables
+ ******************************************************************************/
+
+/******************************************************************************
+ * Function Prototypes
+ ******************************************************************************/
+/**
+ * @brief  Delay a task until a specified time
+ * @param   PreviousWakeTime   Pointer to a variable that holds the time at
+ * which the task was last unblocked. PreviousWakeTime must be initialised with
+ * the current time prior to its first use (PreviousWakeTime = osKernelSysTick()
+ * )
+ * @param   millisec    time delay value
+ * @retval  status code that indicates the execution status of the function.
+ */
+osStatus
+    SharedCmsisOs_osDelayUntilMs(uint32_t *PreviousWakeTime, uint32_t millisec);
+
+#endif // SHARED_CMSIS_OS_H
