05528209282d2c072f5c9201e1d841e1bceeaa2f
Ming Chen Lin <thekenu@gmail.com>
2019-10-01 14:17:32 -0700

    Check STM32CubeMX generated code as part of CI and CMakeLists.txt

--- scripts/travis_ci/travis_install_binaries.sh
+++ scripts/travis_ci/travis_install_binaries.sh
@@ -7,13 +7,18 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $SCRIPT_DIR/travis_shared.sh
 source $SCRIPT_DIR/../shared_funcs.sh
 
-check_installation_path_argument $#
+# Verify arguments
+num_arguments=$#
+if [ "$num_arguments" -ne 2 ]; then
+    echo "Please provide the installation path and STM32CubeMX installation path as arguments!"
+    exit 2
+fi
 
-if [ "$RUN_BUILD" = "true" ] || [ "$RUN_TESTS" = "true" ]; then
-  # Prepend bin path to $PATH in order to override the built-in CMake
-  INSTALL_DIR=$1
-  PATH=$INSTALL_DIR/bin:$PATH
-  # Install binaries
-  travis_run sudo scripts/environment_setup/install_cmake.sh $INSTALL_DIR
-  travis_run sudo scripts/environment_setup/install_gcc_arm_none_eabi.sh $INSTALL_DIR
+if [ "$RUN_BUILD" = "true" ] || [ "$RUN_TESTS" = "true" ] || [ "$RUN_CUBE_CODEGEN_CHECKS" = "true" ]; then
+    INSTALL_DIR=$1
+    CUBE_INSTALL_DIR=$2
+    # Install binaries
+    travis_run sudo scripts/environment_setup/install_cmake.sh $INSTALL_DIR
+    travis_run sudo scripts/environment_setup/install_gcc_arm_none_eabi.sh $INSTALL_DIR
+    travis_run pipenv run sudo python scripts/environment_setup/install_cube.py $CUBE_INSTALL_DIR ./tools/en.STM32CubeMX_v5-3-0.zip
 fi
