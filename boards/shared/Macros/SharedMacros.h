4c1b1e26c022f230bdd2047dbb3c1e5a582029eb
Ken Lin <thekenu@gmail.com>
2019-10-29 19:56:43 -0700

    Split HalHandler into Assert and HalErrorHandler for better abstraction (#465)

--- boards/shared/Macros/SharedMacros.h
+++ boards/shared/Macros/SharedMacros.h
@@ -14,5 +14,8 @@
 // clang-format off
 #define NUM_ELEMENTS_IN_ARRAY(array_pointer) \
     sizeof(array_pointer) / sizeof(array_pointer[0])
+/* @brief Extract the basename from a file path */
+#define __BASENAME__(path) \
+    (__builtin_strrchr(path, '/') ? __builtin_strrchr(path, '/') + 1 : path)
 
 #endif /* SHARED_MACROS_H */
