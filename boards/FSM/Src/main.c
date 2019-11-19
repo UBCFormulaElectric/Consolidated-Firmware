c6c08d2463c5919c4e151b12a754c23d3c4664fa
Gareth Ellis <gareth.ellis0@gmail.com>
2019-11-19 12:53:56 -0800

    Got Steering Angle ADC And Vrefint To Read At The Same Time

--- boards/FSM/Src/main.c
+++ boards/FSM/Src/main.c
@@ -252,13 +252,13 @@ static void MX_ADC1_Init(void)
     hadc1.Instance                   = ADC1;
     hadc1.Init.ClockPrescaler        = ADC_CLOCK_ASYNC_DIV1;
     hadc1.Init.Resolution            = ADC_RESOLUTION_12B;
-    hadc1.Init.ScanConvMode          = ADC_SCAN_DISABLE;
+    hadc1.Init.ScanConvMode          = ADC_SCAN_ENABLE;
     hadc1.Init.ContinuousConvMode    = DISABLE;
     hadc1.Init.DiscontinuousConvMode = DISABLE;
     hadc1.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_RISING;
     hadc1.Init.ExternalTrigConv      = ADC_EXTERNALTRIGCONV_T1_TRGO;
     hadc1.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
-    hadc1.Init.NbrOfConversion       = 1;
+    hadc1.Init.NbrOfConversion       = 2;
     hadc1.Init.DMAContinuousRequests = ENABLE;
     hadc1.Init.EOCSelection          = ADC_EOC_SINGLE_CONV;
     hadc1.Init.LowPowerAutoWait      = DISABLE;
@@ -275,6 +275,14 @@ static void MX_ADC1_Init(void)
     sConfig.SamplingTime = ADC_SAMPLETIME_601CYCLES_5;
     sConfig.OffsetNumber = ADC_OFFSET_NONE;
     sConfig.Offset       = 0;
+    if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /** Configure Regular Channel
+     */
+    sConfig.Channel = ADC_CHANNEL_VREFINT;
+    sConfig.Rank    = ADC_REGULAR_RANK_2;
     if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
     {
         Error_Handler();
@@ -494,8 +502,8 @@ void RunTask1Hz(void const *argument)
     UNUSED(argument);
     uint32_t PreviousWakeTime = osKernelSysTick();
 
-    volatile uint16_t adc_buffer[1] = {0};
-    HAL_ADC_Start_DMA(&hadc1, (uint32_t*)&adc_buffer, 1);
+    volatile uint16_t adc_buffer[2] = { 0 };
+    HAL_ADC_Start_DMA(&hadc1, (uint32_t *)&adc_buffer, 2);
 
     for (;;)
     {
