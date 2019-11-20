fc5ce438082b7c7ecf5c9a51e6f28a6c89db6675
Ming Chen Lin <thekenu@gmail.com>
2019-11-19 17:37:36 -0800

    Fix the incorrect pin mapping for CAN peripheral on BMS

--- boards/BMS/Src/stm32f3xx_hal_msp.c
+++ boards/BMS/Src/stm32f3xx_hal_msp.c
@@ -155,17 +155,17 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef *hcan)
         /* Peripheral clock enable */
         __HAL_RCC_CAN1_CLK_ENABLE();
 
-        __HAL_RCC_GPIOA_CLK_ENABLE();
+        __HAL_RCC_GPIOB_CLK_ENABLE();
         /**CAN GPIO Configuration
-        PA11     ------> CAN_RX
-        PA12     ------> CAN_TX
+        PB8     ------> CAN_RX
+        PB9     ------> CAN_TX
         */
-        GPIO_InitStruct.Pin       = GPIO_PIN_11 | GPIO_PIN_12;
+        GPIO_InitStruct.Pin       = GPIO_PIN_8 | GPIO_PIN_9;
         GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
         GPIO_InitStruct.Pull      = GPIO_NOPULL;
         GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_HIGH;
         GPIO_InitStruct.Alternate = GPIO_AF9_TIM1;
-        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
         /* CAN interrupt Init */
         HAL_NVIC_SetPriority(USB_HP_CAN_TX_IRQn, 5, 0);
@@ -197,10 +197,10 @@ void HAL_CAN_MspDeInit(CAN_HandleTypeDef *hcan)
         __HAL_RCC_CAN1_CLK_DISABLE();
 
         /**CAN GPIO Configuration
-        PA11     ------> CAN_RX
-        PA12     ------> CAN_TX
+        PB8     ------> CAN_RX
+        PB9     ------> CAN_TX
         */
-        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_11 | GPIO_PIN_12);
+        HAL_GPIO_DeInit(GPIOB, GPIO_PIN_8 | GPIO_PIN_9);
 
         /* CAN interrupt DeInit */
         HAL_NVIC_DisableIRQ(USB_HP_CAN_TX_IRQn);
