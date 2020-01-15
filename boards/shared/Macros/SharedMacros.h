352ab64f9a82d0ff4feed889f8a6f6f8def69f49
Ming Chen Lin <thekenu@gmail.com>
2020-01-15 00:16:54 -0800

    Set automatic breakpoints for hard faults and assert failures

--- boards/shared/Macros/SharedMacros.h
+++ boards/shared/Macros/SharedMacros.h
@@ -14,8 +14,16 @@
 // clang-format off
 #define NUM_ELEMENTS_IN_ARRAY(array_pointer) \
     sizeof(array_pointer) / sizeof(array_pointer[0])
+
 /* @brief Extract the basename from a file path */
 #define __BASENAME__(path) \
     (__builtin_strrchr(path, '/') ? __builtin_strrchr(path, '/') + 1 : path)
+
+/* @brief Set a software breakpoint if debugger is connected */
+#define BREAK_IF_DEBUGGER_CONNECTED() \
+if (CoreDebug->DHCSR & CoreDebug_DHCSR_C_DEBUGEN_Msk) \
+{ \
+    __asm__ __volatile__ ("bkpt #0"); \
+} \
 
 #endif /* SHARED_MACROS_H */
