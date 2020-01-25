d52a16597b92dc27ba447d8db128fe17a0729625
Gareth Ellis <gareth.ellis0@gmail.com>
2020-01-25 15:10:57 -0800

    Updated Travis Script To Build With CubeIDE Directly

--- scripts/travis_ci/travis_script.sh
+++ scripts/travis_ci/travis_script.sh
@@ -4,8 +4,17 @@
 # ------------------------------------------------------------------------------
 # Import shared bash scripts
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+PROJECT_ROOT="$SCRIPT_DIR/../.."
 source $SCRIPT_DIR/travis_shared.sh
 source $SCRIPT_DIR/../shared_funcs.sh
+
+# The user must specify the STM32CubeMX binary pth
+if [ "$#" -ne 1 ]; then
+    echo "Please provide the STM32CubeMX binary path as argument!"
+    exit 2
+else
+    STM32CUBEMX_BIN_PATH=$1
+fi
 
 # Currently supported boards
 BOARD_NAMES=(
@@ -16,12 +25,19 @@ BOARD_NAMES=(
 )
 
 if [ "$RUN_BUILD" = "true" ]; then
-    # Extract the CubeIDE directories so we have access to the executables in them
-    cd $SCRIPT_DIR/../..
-    travis_run ./tools/st-stm32cubeide_1.2.0_5034_20200108_0926_amd64.sh --noexec 
+    # Extract CubeIDE
+    cd $PROJECT_ROOT
+    mkdir cube_ide
+    cd cube_ide
+    travis_run ./../tools/st-stm32cubeide_1.2.0_5034_20200108_0926_amd64.sh --noexec 
     travis_run tar -zxf st-stm32cubeide_1.2.0_5034_20200108_0926_amd64.sh.root/st-stm32cubeide_1.2.0_5034_20200108_0926_amd64.tar.gz
-    # We assume this executable was extracted from the cube ide setup files
-    travis_run ./headless-build.sh -build all
+    cd $PROJECT_ROOT
+
+    # Build each project
+    for BOARD_NAME in "${BOARD_NAMES[@]}"
+    do
+        ./cube_ide/stm32cubeide --launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core.headlessbuild -data workspace -import boards/$BOARD_NAME -cleanBuild $BOARD_NAME
+    done
 fi
 
 if [ "$RUN_TESTS" = "true" ]; then
@@ -50,33 +66,32 @@ if [ "$RUN_FORMATTING_CHECKS" = "true" ]; then
     fi
 fi
 
-# TODO: re-add this, probably via stm32cubeide somehow?
-#if [ "$RUN_CUBE_CODEGEN_CHECKS" = "true" ]; then
-#    # Use .ioc to update STM32CubeMX auto-generated code
-#    for BOARD_NAME in "${BOARD_NAMES[@]}"
-#      do travis_run python scripts/utilities/generate_cube_code.py $BOARD_NAME boards/$BOARD_NAME/$BOARD_NAME.ioc boards/$BOARD_NAME $STM32CUBEMX_BIN_PATH
-#    done
-#    # Auto-generated STM32CubeMX code doesn't conform to our clang-format so we
-#    # need to manually format them
-#    travis_run python clang-format/fix_formatting.py
-#    # Check if there is any difference
-#    GIT_DIFF_RESULT="$(git --no-pager diff --color)"
-#    if [ "$GIT_DIFF_RESULT" ]; then
-#        echo ""
-#        echo_border
-#        echo "FAILED - Git diff was non-zero!"
-#        echo "STM32CubeMX auto-generated code is not up-to-date with .ioc "
-#        echo ""
-#        echo "$GIT_DIFF_RESULT"
-#        echo_border
-#        echo ""
-#        exit 1;
-#    else
-#        echo_border
-#        echo "PASSED - STM32CubeMX auto-generated code is up-to-date with .ioc"
-#        echo_border
-#    fi
-#fi
+if [ "$RUN_CUBE_CODEGEN_CHECKS" = "true" ]; then
+    # Use .ioc to update STM32CubeMX auto-generated code
+    for BOARD_NAME in "${BOARD_NAMES[@]}"
+      do travis_run python scripts/utilities/generate_cube_code.py $BOARD_NAME boards/$BOARD_NAME/$BOARD_NAME.ioc boards/$BOARD_NAME $STM32CUBEMX_BIN_PATH
+    done
+    # Auto-generated STM32CubeMX code doesn't conform to our clang-format so we
+    # need to manually format them
+    travis_run python clang-format/fix_formatting.py
+    # Check if there is any difference
+    GIT_DIFF_RESULT="$(git --no-pager diff --color)"
+    if [ "$GIT_DIFF_RESULT" ]; then
+        echo ""
+        echo_border
+        echo "FAILED - Git diff was non-zero!"
+        echo "STM32CubeMX auto-generated code is not up-to-date with .ioc "
+        echo ""
+        echo "$GIT_DIFF_RESULT"
+        echo_border
+        echo ""
+        exit 1;
+    else
+        echo_border
+        echo "PASSED - STM32CubeMX auto-generated code is up-to-date with .ioc"
+        echo_border
+    fi
+fi
 
 echo_border
 echo "Travis CI Script has finished successfully!"
