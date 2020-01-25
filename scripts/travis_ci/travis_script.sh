a66e6976fa8a3da402e0db7ee85653762aba21df
Gareth Ellis <gareth.ellis0@gmail.com>
2020-01-25 14:23:10 -0800

    updated CI Scripts To Use CubeIDE To Build

--- scripts/travis_ci/travis_script.sh
+++ scripts/travis_ci/travis_script.sh
@@ -7,14 +7,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source $SCRIPT_DIR/travis_shared.sh
 source $SCRIPT_DIR/../shared_funcs.sh
 
-# The user must specify the STM32CubeMX binary pth
-if [ "$#" -ne 1 ]; then
-    echo "Please provide the STM32CubeMX binary path as argument!"
-    exit 2
-else
-    STM32CUBEMX_BIN_PATH=$1
-fi
-
 # Currently supported boards
 BOARD_NAMES=(
     FSM
@@ -24,11 +16,12 @@ BOARD_NAMES=(
 )
 
 if [ "$RUN_BUILD" = "true" ]; then
-    # Build each project
-    for BOARD_NAME in "${BOARD_NAMES[@]}"
-    do
-        travis_run "cmake boards/$BOARD_NAME/CMakeLists.txt -DSTM32CUBEMX_BIN_PATH=$STM32CUBEMX_BIN_PATH && make -C boards/$BOARD_NAME"
-    done
+    # Extract the CubeIDE directories so we have access to the executables in them
+    cd $SCRIPT_DIR/../..
+    travis_run ./tools/st-stm32cubeide_1.2.0_5034_20200108_0926_amd64.sh --noexec 
+    travis_run tar -zxf st-stm32cubeide_1.2.0_5034_20200108_0926_amd64.sh.root/st-stm32cubeide_1.2.0_5034_20200108_0926_amd64.tar.gz
+    # We assume this executable was extracted from the cube ide setup files
+    travis_run ./headless-build.sh -build all
 fi
 
 if [ "$RUN_TESTS" = "true" ]; then
@@ -57,32 +50,33 @@ if [ "$RUN_FORMATTING_CHECKS" = "true" ]; then
     fi
 fi
 
-if [ "$RUN_CUBE_CODEGEN_CHECKS" = "true" ]; then
-    # Use .ioc to update STM32CubeMX auto-generated code
-    for BOARD_NAME in "${BOARD_NAMES[@]}"
-      do travis_run python scripts/utilities/generate_cube_code.py $BOARD_NAME boards/$BOARD_NAME/$BOARD_NAME.ioc boards/$BOARD_NAME $STM32CUBEMX_BIN_PATH
-    done
-    # Auto-generated STM32CubeMX code doesn't conform to our clang-format so we
-    # need to manually format them
-    travis_run python clang-format/fix_formatting.py
-    # Check if there is any difference
-    GIT_DIFF_RESULT="$(git --no-pager diff --color)"
-    if [ "$GIT_DIFF_RESULT" ]; then
-        echo ""
-        echo_border
-        echo "FAILED - Git diff was non-zero!"
-        echo "STM32CubeMX auto-generated code is not up-to-date with .ioc "
-        echo ""
-        echo "$GIT_DIFF_RESULT"
-        echo_border
-        echo ""
-        exit 1;
-    else
-        echo_border
-        echo "PASSED - STM32CubeMX auto-generated code is up-to-date with .ioc"
-        echo_border
-    fi
-fi
+# TODO: re-add this, probably via stm32cubeide somehow?
+#if [ "$RUN_CUBE_CODEGEN_CHECKS" = "true" ]; then
+#    # Use .ioc to update STM32CubeMX auto-generated code
+#    for BOARD_NAME in "${BOARD_NAMES[@]}"
+#      do travis_run python scripts/utilities/generate_cube_code.py $BOARD_NAME boards/$BOARD_NAME/$BOARD_NAME.ioc boards/$BOARD_NAME $STM32CUBEMX_BIN_PATH
+#    done
+#    # Auto-generated STM32CubeMX code doesn't conform to our clang-format so we
+#    # need to manually format them
+#    travis_run python clang-format/fix_formatting.py
+#    # Check if there is any difference
+#    GIT_DIFF_RESULT="$(git --no-pager diff --color)"
+#    if [ "$GIT_DIFF_RESULT" ]; then
+#        echo ""
+#        echo_border
+#        echo "FAILED - Git diff was non-zero!"
+#        echo "STM32CubeMX auto-generated code is not up-to-date with .ioc "
+#        echo ""
+#        echo "$GIT_DIFF_RESULT"
+#        echo_border
+#        echo ""
+#        exit 1;
+#    else
+#        echo_border
+#        echo "PASSED - STM32CubeMX auto-generated code is up-to-date with .ioc"
+#        echo_border
+#    fi
+#fi
 
 echo_border
 echo "Travis CI Script has finished successfully!"
