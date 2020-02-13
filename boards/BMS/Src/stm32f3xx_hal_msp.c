b73503fc7b8404e6941d08f23f7f2b0e72b3109f
Gareth Ellis <gareth.ellis0@gmail.com>
2020-02-12 16:58:42 -0800

    Fixed Formatting

--- boards/BMS/Src/stm32f3xx_hal_msp.c
+++ boards/BMS/Src/stm32f3xx_hal_msp.c
@@ -59,161 +59,157 @@
 
 /* USER CODE END 0 */
 /**
-  * Initializes the Global MSP.
-  */
+ * Initializes the Global MSP.
+ */
 void HAL_MspInit(void)
 {
-  /* USER CODE BEGIN MspInit 0 */
+    /* USER CODE BEGIN MspInit 0 */
 
-  /* USER CODE END MspInit 0 */
+    /* USER CODE END MspInit 0 */
 
-  __HAL_RCC_SYSCFG_CLK_ENABLE();
-  __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_RCC_SYSCFG_CLK_ENABLE();
+    __HAL_RCC_PWR_CLK_ENABLE();
 
-  /* System interrupt init*/
-  /* PendSV_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(PendSV_IRQn, 15, 0);
+    /* System interrupt init*/
+    /* PendSV_IRQn interrupt configuration */
+    HAL_NVIC_SetPriority(PendSV_IRQn, 15, 0);
 
-  /* USER CODE BEGIN MspInit 1 */
+    /* USER CODE BEGIN MspInit 1 */
 
-  /* USER CODE END MspInit 1 */
+    /* USER CODE END MspInit 1 */
 }
 
 /**
-* @brief ADC MSP Initialization
-* This function configures the hardware resources used in this example
-* @param hadc: ADC handle pointer
-* @retval None
-*/
-void HAL_ADC_MspInit(ADC_HandleTypeDef* hadc)
+ * @brief ADC MSP Initialization
+ * This function configures the hardware resources used in this example
+ * @param hadc: ADC handle pointer
+ * @retval None
+ */
+void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if(hadc->Instance==ADC1)
-  {
-  /* USER CODE BEGIN ADC1_MspInit 0 */
+    GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+    if (hadc->Instance == ADC1)
+    {
+        /* USER CODE BEGIN ADC1_MspInit 0 */
 
-  /* USER CODE END ADC1_MspInit 0 */
-    /* Peripheral clock enable */
-    __HAL_RCC_ADC1_CLK_ENABLE();
-  
-    __HAL_RCC_GPIOA_CLK_ENABLE();
-    /**ADC1 GPIO Configuration    
-    PA3     ------> ADC1_IN4 
-    */
-    GPIO_InitStruct.Pin = __24V_ISENSE_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    HAL_GPIO_Init(__24V_ISENSE_GPIO_Port, &GPIO_InitStruct);
+        /* USER CODE END ADC1_MspInit 0 */
+        /* Peripheral clock enable */
+        __HAL_RCC_ADC1_CLK_ENABLE();
 
-  /* USER CODE BEGIN ADC1_MspInit 1 */
+        __HAL_RCC_GPIOA_CLK_ENABLE();
+        /**ADC1 GPIO Configuration
+        PA3     ------> ADC1_IN4
+        */
+        GPIO_InitStruct.Pin  = __24V_ISENSE_Pin;
+        GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+        GPIO_InitStruct.Pull = GPIO_NOPULL;
+        HAL_GPIO_Init(__24V_ISENSE_GPIO_Port, &GPIO_InitStruct);
 
-  /* USER CODE END ADC1_MspInit 1 */
-  }
+        /* USER CODE BEGIN ADC1_MspInit 1 */
 
+        /* USER CODE END ADC1_MspInit 1 */
+    }
 }
 
 /**
-* @brief ADC MSP De-Initialization
-* This function freeze the hardware resources used in this example
-* @param hadc: ADC handle pointer
-* @retval None
-*/
-void HAL_ADC_MspDeInit(ADC_HandleTypeDef* hadc)
+ * @brief ADC MSP De-Initialization
+ * This function freeze the hardware resources used in this example
+ * @param hadc: ADC handle pointer
+ * @retval None
+ */
+void HAL_ADC_MspDeInit(ADC_HandleTypeDef *hadc)
 {
-  if(hadc->Instance==ADC1)
-  {
-  /* USER CODE BEGIN ADC1_MspDeInit 0 */
+    if (hadc->Instance == ADC1)
+    {
+        /* USER CODE BEGIN ADC1_MspDeInit 0 */
 
-  /* USER CODE END ADC1_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_ADC1_CLK_DISABLE();
-  
-    /**ADC1 GPIO Configuration    
-    PA3     ------> ADC1_IN4 
-    */
-    HAL_GPIO_DeInit(__24V_ISENSE_GPIO_Port, __24V_ISENSE_Pin);
+        /* USER CODE END ADC1_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_ADC1_CLK_DISABLE();
 
-  /* USER CODE BEGIN ADC1_MspDeInit 1 */
+        /**ADC1 GPIO Configuration
+        PA3     ------> ADC1_IN4
+        */
+        HAL_GPIO_DeInit(__24V_ISENSE_GPIO_Port, __24V_ISENSE_Pin);
 
-  /* USER CODE END ADC1_MspDeInit 1 */
-  }
+        /* USER CODE BEGIN ADC1_MspDeInit 1 */
 
+        /* USER CODE END ADC1_MspDeInit 1 */
+    }
 }
 
 /**
-* @brief CAN MSP Initialization
-* This function configures the hardware resources used in this example
-* @param hcan: CAN handle pointer
-* @retval None
-*/
-void HAL_CAN_MspInit(CAN_HandleTypeDef* hcan)
+ * @brief CAN MSP Initialization
+ * This function configures the hardware resources used in this example
+ * @param hcan: CAN handle pointer
+ * @retval None
+ */
+void HAL_CAN_MspInit(CAN_HandleTypeDef *hcan)
 {
-  GPIO_InitTypeDef GPIO_InitStruct = {0};
-  if(hcan->Instance==CAN)
-  {
-  /* USER CODE BEGIN CAN_MspInit 0 */
+    GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+    if (hcan->Instance == CAN)
+    {
+        /* USER CODE BEGIN CAN_MspInit 0 */
 
-  /* USER CODE END CAN_MspInit 0 */
-    /* Peripheral clock enable */
-    __HAL_RCC_CAN1_CLK_ENABLE();
-  
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-    /**CAN GPIO Configuration    
-    PB8     ------> CAN_RX
-    PB9     ------> CAN_TX 
-    */
-    GPIO_InitStruct.Pin = GPIO_PIN_8|GPIO_PIN_9;
-    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
-    GPIO_InitStruct.Alternate = GPIO_AF9_TIM1;
-    HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+        /* USER CODE END CAN_MspInit 0 */
+        /* Peripheral clock enable */
+        __HAL_RCC_CAN1_CLK_ENABLE();
 
-    /* CAN interrupt Init */
-    HAL_NVIC_SetPriority(USB_HP_CAN_TX_IRQn, 5, 0);
-    HAL_NVIC_EnableIRQ(USB_HP_CAN_TX_IRQn);
-    HAL_NVIC_SetPriority(USB_LP_CAN_RX0_IRQn, 5, 0);
-    HAL_NVIC_EnableIRQ(USB_LP_CAN_RX0_IRQn);
-    HAL_NVIC_SetPriority(CAN_RX1_IRQn, 5, 0);
-    HAL_NVIC_EnableIRQ(CAN_RX1_IRQn);
-  /* USER CODE BEGIN CAN_MspInit 1 */
+        __HAL_RCC_GPIOB_CLK_ENABLE();
+        /**CAN GPIO Configuration
+        PB8     ------> CAN_RX
+        PB9     ------> CAN_TX
+        */
+        GPIO_InitStruct.Pin       = GPIO_PIN_8 | GPIO_PIN_9;
+        GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull      = GPIO_NOPULL;
+        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_HIGH;
+        GPIO_InitStruct.Alternate = GPIO_AF9_TIM1;
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /* USER CODE END CAN_MspInit 1 */
-  }
+        /* CAN interrupt Init */
+        HAL_NVIC_SetPriority(USB_HP_CAN_TX_IRQn, 5, 0);
+        HAL_NVIC_EnableIRQ(USB_HP_CAN_TX_IRQn);
+        HAL_NVIC_SetPriority(USB_LP_CAN_RX0_IRQn, 5, 0);
+        HAL_NVIC_EnableIRQ(USB_LP_CAN_RX0_IRQn);
+        HAL_NVIC_SetPriority(CAN_RX1_IRQn, 5, 0);
+        HAL_NVIC_EnableIRQ(CAN_RX1_IRQn);
+        /* USER CODE BEGIN CAN_MspInit 1 */
 
+        /* USER CODE END CAN_MspInit 1 */
+    }
 }
 
 /**
-* @brief CAN MSP De-Initialization
-* This function freeze the hardware resources used in this example
-* @param hcan: CAN handle pointer
-* @retval None
-*/
-void HAL_CAN_MspDeInit(CAN_HandleTypeDef* hcan)
+ * @brief CAN MSP De-Initialization
+ * This function freeze the hardware resources used in this example
+ * @param hcan: CAN handle pointer
+ * @retval None
+ */
+void HAL_CAN_MspDeInit(CAN_HandleTypeDef *hcan)
 {
-  if(hcan->Instance==CAN)
-  {
-  /* USER CODE BEGIN CAN_MspDeInit 0 */
+    if (hcan->Instance == CAN)
+    {
+        /* USER CODE BEGIN CAN_MspDeInit 0 */
 
-  /* USER CODE END CAN_MspDeInit 0 */
-    /* Peripheral clock disable */
-    __HAL_RCC_CAN1_CLK_DISABLE();
-  
-    /**CAN GPIO Configuration    
-    PB8     ------> CAN_RX
-    PB9     ------> CAN_TX 
-    */
-    HAL_GPIO_DeInit(GPIOB, GPIO_PIN_8|GPIO_PIN_9);
+        /* USER CODE END CAN_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_CAN1_CLK_DISABLE();
 
-    /* CAN interrupt DeInit */
-    HAL_NVIC_DisableIRQ(USB_HP_CAN_TX_IRQn);
-    HAL_NVIC_DisableIRQ(USB_LP_CAN_RX0_IRQn);
-    HAL_NVIC_DisableIRQ(CAN_RX1_IRQn);
-  /* USER CODE BEGIN CAN_MspDeInit 1 */
+        /**CAN GPIO Configuration
+        PB8     ------> CAN_RX
+        PB9     ------> CAN_TX
+        */
+        HAL_GPIO_DeInit(GPIOB, GPIO_PIN_8 | GPIO_PIN_9);
 
-  /* USER CODE END CAN_MspDeInit 1 */
-  }
+        /* CAN interrupt DeInit */
+        HAL_NVIC_DisableIRQ(USB_HP_CAN_TX_IRQn);
+        HAL_NVIC_DisableIRQ(USB_LP_CAN_RX0_IRQn);
+        HAL_NVIC_DisableIRQ(CAN_RX1_IRQn);
+        /* USER CODE BEGIN CAN_MspDeInit 1 */
 
+        /* USER CODE END CAN_MspDeInit 1 */
+    }
 }
 
 /* USER CODE BEGIN 1 */
