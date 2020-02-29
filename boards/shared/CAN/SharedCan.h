f0397ef5b0955f2052366f1867f9fef5c8a68feb
Gareth Ellis <garethellis0@users.noreply.github.com>
2020-02-28 21:47:17 -0800

    Removed Hardware CAN Filters (#491)
    * Removed Hardware CAN Filters
    
    * Added __lib__ To .gitignore
    
    * Added Single Filter To Allow All Messages
    
    * Regenerate Cube code to update timestampe (i.e. 2019 -> 2020)
    
    * Add TODO comment
    
    Co-authored-by: Ken Lin <thekenu@gmail.com>

--- boards/shared/CAN/SharedCan.h
+++ boards/shared/CAN/SharedCan.h
@@ -24,10 +24,8 @@
  * Fixes to allow us to use this library with STM32F0xx Boards
  ******************************************************************************/
 #ifdef STM32F042x6
-typedef CanRxMsgTypeDef       CAN_RxHeaderTypeDef;
-typedef CanTxMsgTypeDef       CAN_TxHeaderTypeDef;
-typedef CAN_FilterConfTypeDef CAN_FilterTypeDef;
-#define CAN_FILTER_ENABLE ENABLE
+typedef CanRxMsgTypeDef CAN_RxHeaderTypeDef;
+typedef CanTxMsgTypeDef CAN_TxHeaderTypeDef;
 #endif
 
 /******************************************************************************
@@ -38,128 +36,9 @@ typedef CAN_FilterConfTypeDef CAN_FilterTypeDef;
 #define CAN_ExtID_NULL 0 // Set CAN Extended ID to 0 because we are not using it
 
 /******************************************************************************
- * Preprocessor Macros
- ******************************************************************************/
-/**
- * @brief Used to initialize an element in mask_filters[]
- * @param id Can be MASKMODE_16BIT_ID_XXX, where XXX is the PCB name
- * @param mask Can be MASKMODE_16BIT_MASK_XXX, where XXX is the PCB name
- */
-#define INIT_MASK_FILTER(filter_id, filter_mask) {.id = filter_id, .mask = filter_mask}
-
-// The following filter IDs/masks must be used with 16-bit Filter Scale
-// (FSCx = 0) and Identifier Mask Mode (FBMx = 0). In this mode, the identifier
-// registers are associated with mask registers specifying which bits of the
-// identifier are handled as "don't care" or as "must match". For each bit in
-// the mask registers, 0 = Don't Care and 1 = Must Match.
-//
-// Bit mapping of a 16-bit identifier register and mask register:
-// Standard CAN ID [15:5] RTR[4] IDE[3] Extended CAN ID [2:0]
-//
-// For example, with the following filter IDs/mask:
-// =======================================================
-// Identifier Register:    [000 0000 0000] [0] [0] [000]
-// Mask Register:          [111 1110 0000] [1] [1] [000]
-// =======================================================
-// The filter will accept incoming messages that match the following criteria:
-// [000 000x xxxx]    [0]    [0]         [xxx]
-// Standard CAN ID    RTR    IDE     Extended CAN ID
-
-// Bit definition for FxR1 and FxR2 Registers
-#define MASKMODE_16BIT_STDID_Pos  (5U)
-#define MASKMODE_16BIT_STDID_Mask (0x7FFU << MASKMODE_16BIT_STDID_Pos) /** 0xFFE0 */
-#define MASKMODE_16BIT_RTR_Pos    (4U)
-#define MASKMODE_16BIT_RTR_Mask   (0x1U   << MASKMODE_16BIT_RTR_Pos)   /** 0x0010 */
-#define MASKMODE_16BIT_IDE_Pos    (3U)
-#define MASKMODE_16BIT_IDE_Mask   (0x1U   << MASKMODE_16BIT_IDE_Pos)   /** 0x0008 */
-#define MASKMODE_16BIT_EXTID_Pos  (0U)
-#define MASKMODE_16BIT_EXTID_Mask (0x7U   << MASKMODE_16BIT_EXTID_Pos) /** 0x0007 */
-
-/** @brief Helper macro to initialize FiRx register in 16-bit mode */
-#define INIT_MASKMODE_16BIT_FiRx(std_id, rtr, ide, ext_id) \
-      ( (((uint32_t)(std_id) << MASKMODE_16BIT_STDID_Pos) & MASKMODE_16BIT_STDID_Mask) | \
-        (((uint32_t)(rtr)    << MASKMODE_16BIT_RTR_Pos)   & MASKMODE_16BIT_RTR_Mask)   | \
-        (((uint32_t)(ide)    << MASKMODE_16BIT_IDE_Pos)   & MASKMODE_16BIT_IDE_Mask)   | \
-        (((uint32_t)(ext_id) << MASKMODE_16BIT_EXTID_Pos) & MASKMODE_16BIT_EXTID_Mask) )
-
-/** BMS filter - CAN ID Range: 0x00 to 0x1F, RTR: Data Frame, IDE: Standard ID */
-// ID:   [000 0000 0000] [0] [0] [000] or 0x0000
-// Mask: [111 1110 0000] [1] [1] [000] or 0xFC18
-#define MASKMODE_16BIT_ID_BMS   INIT_MASKMODE_16BIT_FiRx(0x0,           \
-                                                         CAN_ID_STD,    \
-                                                         CAN_RTR_DATA,  \
-                                                         CAN_ExtID_NULL)
-#define MASKMODE_16BIT_MASK_BMS INIT_MASKMODE_16BIT_FiRx(0x7E0, 0x1, 0x1, 0x0)
-
-/** DCM filter - CAN ID Range: 0x20 - 0x3F, RTR: Data Frame, IDE: Standard ID */
-// ID:   [000 0010 0000] [0] [0] [000] or 0x0400
-// Mask: [111 1110 0000] [1] [1] [000] or 0xFC18
-#define MASKMODE_16BIT_ID_DCM   INIT_MASKMODE_16BIT_FiRx(0x20,           \
-                                                         CAN_ID_STD,     \
-                                                         CAN_RTR_DATA,   \
-                                                         CAN_ExtID_NULL)
-#define MASKMODE_16BIT_MASK_DCM INIT_MASKMODE_16BIT_FiRx(0x7E0, 0x1, 0x1, 0x0)
-
-/** FSM filter - CAN ID Range: 0x40 - 0x5F, RTR: Data Frame, IDE: Standard ID */
-// ID:   [000 0100 0000] [0] [0] [000] or 0x0800
-// Mask: [111 1110 0000] [1] [1] [000] or 0xFC18
-#define MASKMODE_16BIT_ID_FSM   INIT_MASKMODE_16BIT_FiRx(0x40,           \
-                                                         CAN_ID_STD,     \
-                                                         CAN_RTR_DATA,   \
-                                                         CAN_ExtID_NULL)
-#define MASKMODE_16BIT_MASK_FSM INIT_MASKMODE_16BIT_FiRx(0x7E0, 0x1, 0x1, 0x0)
-
-/** PDM filter - CAN ID Range: 0x60 - 0x7F, RTR: Data Frame, IDE: Standard ID */
-// ID:   [000 0110 0000] [0] [0] [000] or 0x0C00
-// Mask: [111 1110 0000] [1] [1] [000] or 0xFC18
-#define MASKMODE_16BIT_ID_PDM   INIT_MASKMODE_16BIT_FiRx(0x60,           \
-                                                         CAN_ID_STD,     \
-                                                         CAN_RTR_DATA,   \
-                                                         CAN_ExtID_NULL)
-#define MASKMODE_16BIT_MASK_PDM INIT_MASKMODE_16BIT_FiRx(0x7E0, 0x1, 0x1, 0x0)
-
-/** Shared filter - CAN ID Range: 0x80 - 0x9F, RTR: Data Frame, IDE: Standard ID */
-// ID:   [000 1000 0000] [0] [0] [000] or 0x1000
-// Mask: [111 1110 0000] [1] [1] [000] or 0xFC18
-#define MASKMODE_16BIT_ID_SHARED INIT_MASKMODE_16BIT_FiRx(0x80,          \
-                                                         CAN_ID_STD,     \
-                                                         CAN_RTR_DATA,   \
-                                                         CAN_ExtID_NULL)
-#define MASKMODE_16BIT_MASK_SHARED INIT_MASKMODE_16BIT_FiRx(0x7E0, 0x1, 0x1, 0x0)
-
-/** BAMOCAR Tx Filter - ID: 0x190 - 0x19F, RTR: Data Frame, IDE: Standard ID */
-// ID:   [001 1001 0000] [0] [0] [000] or 0x3200
-// Mask: [111 1111 0000] [1] [1] [000] or 0xFE18
-#define MASKMODE_16BIT_ID_BAMOCAR_TX INIT_MASKMODE_16BIT_FiRx(0x190,     \
-                                                         CAN_ID_STD,     \
-                                                         CAN_RTR_DATA,   \
-                                                         CAN_ExtID_NULL)
-#define MASKMODE_16BIT_MASK_BAMOCAR_TX INIT_MASKMODE_16BIT_FiRx(0x7F0, 0x1, 0x1, 0x0)
-
-/** BAMOCAR Rx Filter - ID: 0x210- 0x21F, RTR: Data Frame, IDE: Standard ID */
-// ID:   [010 0001 0000] [0] [0] [000] or 0x4200
-// Mask: [111 1111 0000] [1] [1] [000] or 0xFE18
-#define MASKMODE_16BIT_ID_BAMOCAR_RX INIT_MASKMODE_16BIT_FiRx(0x210,     \
-                                                         CAN_ID_STD,     \
-                                                         CAN_RTR_DATA,   \
-                                                         CAN_ExtID_NULL)
-#define MASKMODE_16BIT_MASK_BAMOCAR_RX INIT_MASKMODE_16BIT_FiRx(0x7F0, 0x1, 0x1, 0x0)
-
-/******************************************************************************
  * Typedefs
  ******************************************************************************/
 // clang-format on
-/** @brief Struct to help initialize CAN filters */
-typedef struct
-{
-    // Used to initialize CAN_FilterTypeDef.FilterIdLow and
-    // CAN_FilterTypeDef.FilterIdHigh.
-    const uint32_t id;
-
-    // Used to initialize CAN_FilterTypeDef.FilterMaskIdLow and
-    // CAN_FilterTypeDef.FilterMaskIdHigh.
-    const uint32_t mask;
-} CanMaskFilterConfig_Struct;
 
 /**
  * @brief The HAL library represents a CAN message using at least two structs:
@@ -183,19 +62,14 @@ struct CanMsg
  * Function Prototypes
  ******************************************************************************/
 /**
- * @brief  Initialize CAN interrupts and CAN filters before starting the CAN
+ * @brief  Initialize CAN interrupts before starting the CAN
  *         module. After this, the node is active on the bus: it receive
  *         messages, and can send messages. This should be placed inside
  *         MX_CAN_Init() and in the USER CODE BLOCK after HAL_CAN_Init().
  * @param hcan Pointer to a CAN_HandleTypeDef structure that contains
  *         the configuration information for the specified CAN.
- * @param filters Array CAN filters.
- * @param num_of_filters Number of CAN filters in the array.
  */
-void SharedCan_Init(
-    CAN_HandleTypeDef *        hcan,
-    CanMaskFilterConfig_Struct filters[],
-    uint32_t                   num_of_filers);
+void SharedCan_Init(CAN_HandleTypeDef *hcan);
 
 /**
  * @brief Send a message to the back of the CAN TX queue
