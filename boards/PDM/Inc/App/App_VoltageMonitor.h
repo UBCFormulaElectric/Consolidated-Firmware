b0ac827efa57bd58a0f968744555194b409b1070
Ming Chen Lin <thekenu@gmail.com>
2020-04-04 17:56:18 -0700

    refactor to voltage monitor

---
+++ boards/PDM/Inc/App/App_VoltageMonitor.h
@@ -0,0 +1,43 @@
+#pragma once
+
+struct VoltageMonitor;
+
+enum VoltageMonitor_Status
+{
+    VOLTAGEMONITOR_NORMAL,
+    VOLTAGEMONITOR_UNDERVOLTAGE,
+    VOLTAGEMONITOR_OVERVOLTAGE
+};
+
+/**
+ * Allocate and initialize a voltage monitor
+ * @get_voltage A function that can be called to get the voltage
+ * @get_min_voltage A function that can be called to get the minimum voltage
+ * @get_max_voltage A function that can be called to get the maximum voltage
+ * @return A pointer to the created voltage monitor, whose ownership is given to
+ * the caller
+ */
+struct VoltageMonitor *App_VoltageMonitor_Create(
+    float (*get_voltage)(void),
+    float (*get_min_voltage)(void),
+    float (*get_max_voltage)(void));
+
+/**
+ * Update the status for the given voltage monitor
+ * @param voltage_monitor The voltage monitor to update for
+ */
+void App_VoltageMonitor_Tick(struct VoltageMonitor *voltage_monitor);
+
+/**
+ * Get the status for the given voltage monitor
+ * @param voltage_monitor The voltage monitor to get status for
+ * @return The status for the given voltage monitor
+ */
+enum VoltageMonitor_Status
+    App_VoltageMonitor_GetStatus(struct VoltageMonitor *voltage_monitor);
+
+/**
+ * Destroy the given voltage monitor
+ * @param voltage_monitor The voltage monitor to destroy
+ */
+void App_VoltageMonitor_Destroy(struct VoltageMonitor *voltage_check);
---
+++ boards/PDM/Src/App/App_VoltageMonitor.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+
+#include "App_VoltageMonitor.h"
+#include "App_SharedAssert.h"
+
+struct VoltageMonitor
+{
+    float (*get_voltage)(void);
+    float (*get_min_voltage)(void);
+    float (*get_max_voltage)(void);
+    enum VoltageMonitor_Status status;
+};
+
+struct VoltageMonitor *App_VoltageMonitor_Create(
+    float (*get_voltage)(void),
+    float (*get_min_voltage)(void),
+    float (*get_max_voltage)(void))
+{
+    struct VoltageMonitor *voltage_monitor =
+        malloc(sizeof(struct VoltageMonitor));
+
+    shared_assert(voltage_monitor != NULL);
+
+    voltage_monitor->get_voltage     = get_voltage;
+    voltage_monitor->get_min_voltage = get_min_voltage;
+    voltage_monitor->get_max_voltage = get_max_voltage;
+    voltage_monitor->status          = VOLTAGEMONITOR_NORMAL;
+
+    return voltage_monitor;
+}
+
+void App_VoltageMonitor_Tick(struct VoltageMonitor *voltage_monitor)
+{
+    const float voltage     = voltage_monitor->get_voltage();
+    const float min_voltage = voltage_monitor->get_min_voltage();
+    const float max_voltage = voltage_monitor->get_max_voltage();
+
+    if (voltage < min_voltage)
+    {
+        voltage_monitor->status = VOLTAGEMONITOR_UNDERVOLTAGE;
+    }
+    else if (voltage > max_voltage)
+    {
+        voltage_monitor->status = VOLTAGEMONITOR_OVERVOLTAGE;
+    }
+    else
+    {
+        voltage_monitor->status = VOLTAGEMONITOR_NORMAL;
+    }
+}
+
+enum VoltageMonitor_Status
+    App_VoltageMonitor_GetStatus(struct VoltageMonitor *voltage_monitor)
+{
+    return voltage_monitor->status;
+}
+
+void App_VoltageMonitor_Destroy(struct VoltageMonitor *voltage_monitor)
+{
+    free(voltage_monitor);
+}
