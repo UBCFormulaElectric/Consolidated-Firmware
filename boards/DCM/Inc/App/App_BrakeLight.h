58af594622a75e5df64a58500b8142485ff943fc
Ming Chen Lin <thekenu@gmail.com>
2020-04-05 14:25:27 -0700

    javadoc

--- boards/DCM/Inc/App/App_BrakeLight.h
+++ boards/DCM/Inc/App/App_BrakeLight.h
@@ -8,18 +8,40 @@ enum BrakeLight_Status
     BRAKE_LIGHT_OFF,
 };
 
-// TODO: Fill in javadoc once the review looks good
+/**
+ * Allocate and initialize a brake light
+ * @param is_brake_actuated A function that can be called to check if the brake
+ *                          is actuated
+ * @param is_regen_active A function that can be called to check if the regen
+ *                        braking is active
+ * @param turn_on_brake_light A function that can be called to turn on the brake
+ *                            light
+ * @param turn_off_brake_light A function that can be called to turn off the
+ *                             brake light
+ * @return A pointer to the created brake light, whose ownership is given to
+ * the caller
+ */
 struct BrakeLight *App_BrakeLight_Create(
     bool (*const is_brake_actuated)(void),
     bool (*const is_regen_active)(void),
     void (*const turn_on_brake_light)(void),
     void (*const turn_off_brake_light)(void));
 
-// TODO: Fill in javadoc once the review looks good
+/**
+ * Destroy the given brake light
+ * @param brake_light The brake light to destroy
+ */
+
+void App_BrakeLight_Destroy(struct BrakeLight *brake_light);
+/**
+ * Update the status for the given brake light
+ * @param brake_light The brake light to update for
+ */
 void App_BrakeLight_Tick(struct BrakeLight *brake_light);
 
-// TODO: Fill in javadoc once the review looks good
+/**
+ * Get the status for the given brake light
+ * @param brake_light The brake light to get status for
+ * @return The status for the given brake light
+ */
 enum BrakeLight_Status App_BrakeLight_GetStatus(struct BrakeLight *brake_light);
-
-// TODO: Fill in javadoc once the review looks good
-void App_BrakeLight_Destroy(struct BrakeLight *brake_light);
