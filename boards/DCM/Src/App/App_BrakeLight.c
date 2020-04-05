7f8e03a445df90ec8d0df74a0532a5e88dfa6766
Ming Chen Lin <thekenu@gmail.com>
2020-04-05 16:23:59 -0700

    null

--- boards/DCM/Src/App/App_BrakeLight.c
+++ boards/DCM/Src/App/App_BrakeLight.c
@@ -18,6 +18,11 @@ struct BrakeLight *App_BrakeLight_Create(
     void (*const turn_on_brake_light)(void),
     void (*const turn_off_brake_light)(void))
 {
+    shared_assert(is_brake_actuated != NULL);
+    shared_assert(is_regen_active != NULL);
+    shared_assert(turn_on_brake_light != NULL);
+    shared_assert(turn_off_brake_light != NULL);
+
     struct BrakeLight *brake_light = malloc(sizeof(struct BrakeLight));
 
     shared_assert(brake_light != NULL);
@@ -35,11 +40,14 @@ struct BrakeLight *App_BrakeLight_Create(
 
 void App_BrakeLight_Destroy(struct BrakeLight *const brake_light)
 {
+    shared_assert(brake_light != NULL);
     free(brake_light);
 }
 
 void App_BrakeLight_Tick(struct BrakeLight *const brake_light)
 {
+    shared_assert(brake_light != NULL);
+
     if (brake_light->is_brake_actuated() == true ||
         brake_light->is_regen_active() == true)
     {
@@ -56,5 +64,6 @@ void App_BrakeLight_Tick(struct BrakeLight *const brake_light)
 enum BrakeLight_Status
     App_BrakeLight_GetStatus(const struct BrakeLight *const brake_light)
 {
+    shared_assert(brake_light != NULL);
     return brake_light->status;
 }
