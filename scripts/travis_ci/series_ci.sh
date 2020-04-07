9cec1f14beb5abb7e7d111afc6192196f5de06b1
Ming Chen Lin <thekenu@gmail.com>
2020-04-07 10:47:27 -0700

    change 7 to 8

--- scripts/travis_ci/series_ci.sh
+++ scripts/travis_ci/series_ci.sh
@@ -1,5 +1,5 @@
 curl \
   --header "Authorization: Token 5e287d4c-a391-41e9-b2f5-8cfa38525052" \
   --header "Content-Type: application/json" \
-  --data "{\"value\":\"7\",\"sha\":\"${TRAVIS_COMMIT}\"}" \
+  --data "{\"value\":\"8\",\"sha\":\"${TRAVIS_COMMIT}\"}" \
   https://seriesci.com/api/thekenu/Consolidated-Firmware/:series/one
