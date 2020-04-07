0624c7ee9c951aadecb982408338cfbe8e5c4aa8
Ming Chen Lin <thekenu@gmail.com>
2020-04-07 12:32:54 -0700

    can't hover over data point, create new series entirely

--- scripts/travis_ci/series_ci.sh
+++ scripts/travis_ci/series_ci.sh
@@ -8,7 +8,7 @@ curl \
   --header "Authorization: Token 5e287d4c-a391-41e9-b2f5-8cfa38525052" \
   --header "Content-Type: application/json" \
   --data "{\"values\":[{\"value\":\"9\",\"line\":\"a\"}],\"sha\":\"${TRAVIS_COMMIT}\"}" \
-  https://seriesci.com/api/thekenu/Consolidated-Firmware/data/many
+  https://seriesci.com/api/thekenu/Consolidated-Firmware/data2/many
 
 curl \
   --header "Authorization: Token 5e287d4c-a391-41e9-b2f5-8cfa38525052" \
