67fd36b755471664d7604c67107216cdaab4513f
Ming Chen Lin <thekenu@gmail.com>
2020-04-07 13:07:58 -0700

    data3

--- scripts/travis_ci/series_ci.sh
+++ scripts/travis_ci/series_ci.sh
@@ -8,7 +8,7 @@ curl \
   --header "Authorization: Token 5e287d4c-a391-41e9-b2f5-8cfa38525052" \
   --header "Content-Type: application/json" \
   --data "{\"values\":[{\"value\":\"9\",\"line\":\"a\"},{\"value\":\"19\",\"line\":\"b\"}],\"sha\":\"${TRAVIS_COMMIT}\"}" \
-  https://seriesci.com/api/thekenu/Consolidated-Firmware/data2/many
+  https://seriesci.com/api/thekenu/Consolidated-Firmware/data3/many
 
 curl \
   --header "Authorization: Token 5e287d4c-a391-41e9-b2f5-8cfa38525052" \
