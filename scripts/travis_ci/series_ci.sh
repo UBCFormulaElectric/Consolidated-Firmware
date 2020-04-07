30d78b03b2b9aad7a0d6c51969daaead2e0972f8
Ming Chen Lin <thekenu@gmail.com>
2020-04-07 13:26:23 -0700

    remove one line

--- scripts/travis_ci/series_ci.sh
+++ scripts/travis_ci/series_ci.sh
@@ -7,7 +7,7 @@ curl \
 curl \
   --header "Authorization: Token 5e287d4c-a391-41e9-b2f5-8cfa38525052" \
   --header "Content-Type: application/json" \
-  --data "{\"values\":[{\"value\":\"9\",\"line\":\"a\"},{\"value\":\"19\",\"line\":\"b\"}],\"sha\":\"${TRAVIS_COMMIT}\"}" \
+  --data "{\"values\":[{\"value\":\"9\",\"line\":\"a\"}],\"sha\":\"${TRAVIS_COMMIT}\"}" \
   https://seriesci.com/api/thekenu/Consolidated-Firmware/data3/many
 
 curl \
