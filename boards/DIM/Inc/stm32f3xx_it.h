c8e56ab3d8195865d3503a2a77224283cad46fd7
Ken Lin <thekenu@gmail.com>
2020-04-09 08:42:13 -0700

    Configure CAN settings in Cube for DIM (#559)

--- boards/DIM/Inc/stm32f3xx_it.h
+++ boards/DIM/Inc/stm32f3xx_it.h
@@ -59,6 +59,9 @@ extern "C"
     void BusFault_Handler(void);
     void UsageFault_Handler(void);
     void DebugMon_Handler(void);
+    void USB_HP_CAN_TX_IRQHandler(void);
+    void USB_LP_CAN_RX0_IRQHandler(void);
+    void CAN_RX1_IRQHandler(void);
     void TIM6_DAC_IRQHandler(void);
     /* USER CODE BEGIN EFP */
 
