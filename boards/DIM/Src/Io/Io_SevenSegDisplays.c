83a760cb261dc0a13cc0af603e0ff61d585e1d08
Ming Chen Lin <thekenu@gmail.com>
2020-04-08 18:45:28 -0700

    static assert

--- boards/DIM/Src/Io/Io_SevenSegDisplays.c
+++ boards/DIM/Src/Io/Io_SevenSegDisplays.c
@@ -3,6 +3,23 @@
 
 #include "Io_SevenSegDisplays.h"
 
+static_assert(HEX_DIGIT_0 == 0, "Hex enum must match its numeric value.");
+static_assert(HEX_DIGIT_1 == 1, "Hex enum must match its numeric value.");
+static_assert(HEX_DIGIT_2 == 2, "Hex enum must match its numeric value.");
+static_assert(HEX_DIGIT_3 == 3, "Hex enum must match its numeric value.");
+static_assert(HEX_DIGIT_4 == 4, "Hex enum must match its numeric value.");
+static_assert(HEX_DIGIT_5 == 5, "Hex enum must match its numeric value.");
+static_assert(HEX_DIGIT_6 == 6, "Hex enum must match its numeric value.");
+static_assert(HEX_DIGIT_7 == 7, "Hex enum must match its numeric value.");
+static_assert(HEX_DIGIT_8 == 8, "Hex enum must match its numeric value.");
+static_assert(HEX_DIGIT_9 == 9, "Hex enum must match its numeric value.");
+static_assert(HEX_DIGIT_A == 10, "Hex enum must match its numeric value.");
+static_assert(HEX_DIGIT_B == 11, "Hex enum must match its numeric value.");
+static_assert(HEX_DIGIT_C == 12, "Hex enum must match its numeric value.");
+static_assert(HEX_DIGIT_D == 13, "Hex enum must match its numeric value.");
+static_assert(HEX_DIGIT_E == 14, "Hex enum must match its numeric value.");
+static_assert(HEX_DIGIT_F == 15, "Hex enum must match its numeric value.");
+
 float Io_SevenSegDisplay_GetStateOfCharge(void)
 {
     // TODO: Read the state of charge from CAN
@@ -54,6 +71,30 @@ void Io_SevenSegDisplay_SetLeftHexDigit(uint8_t hex_digit)
         }
         break;
         case HEX_DIGIT_9:
+        {
+        }
+        break;
+        case HEX_DIGIT_A:
+        {
+        }
+        break;
+        case HEX_DIGIT_B:
+        {
+        }
+        break;
+        case HEX_DIGIT_C:
+        {
+        }
+        break;
+        case HEX_DIGIT_D:
+        {
+        }
+        break;
+        case HEX_DIGIT_E:
+        {
+        }
+        break;
+        case HEX_DIGIT_F:
         {
         }
         break;
@@ -112,6 +153,30 @@ void Io_SevenSegDisplay_SetMiddleHexDigit(uint8_t hex_digit)
         {
         }
         break;
+        case HEX_DIGIT_A:
+        {
+        }
+        break;
+        case HEX_DIGIT_B:
+        {
+        }
+        break;
+        case HEX_DIGIT_C:
+        {
+        }
+        break;
+        case HEX_DIGIT_D:
+        {
+        }
+        break;
+        case HEX_DIGIT_E:
+        {
+        }
+        break;
+        case HEX_DIGIT_F:
+        {
+        }
+        break;
         default:
         {
         }
@@ -167,6 +232,31 @@ void Io_SevenSegDisplay_SetRightHexDigit(uint8_t hex_digit)
         {
         }
         break;
+        case HEX_DIGIT_A:
+        {
+        }
+        break;
+        case HEX_DIGIT_B:
+        {
+        }
+        break;
+        case HEX_DIGIT_C:
+        {
+        }
+        break;
+        case HEX_DIGIT_D:
+        {
+        }
+        break;
+        case HEX_DIGIT_E:
+        {
+        }
+        break;
+        case HEX_DIGIT_F:
+        {
+        }
+        break;
+
         default:
         {
         }
