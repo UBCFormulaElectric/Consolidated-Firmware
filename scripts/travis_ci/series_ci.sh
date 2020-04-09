9676c84cc627ddcbdd27b11bb7e65f67385d5d5c
Ming Chen Lin <thekenu@gmail.com>
2020-04-08 20:21:12 -0700

    add another line to the same series

--- scripts/travis_ci/series_ci.sh
+++ scripts/travis_ci/series_ci.sh
@@ -1,5 +1,5 @@
 curl \
   --header "Authorization: Token 92cb0c5d-2b14-41c0-9cfc-06fa089aa108" \
   --header "Content-Type: application/json" \
-  --data "{\"values\":[{\"value\":\"9\",\"line\":\"text\"}],\"sha\":\"${TRAVIS_COMMIT}\"}" \
+  --data "{\"values\":[{\"value\":\"9\",\"line\":\"text\"},{\"value\":\"19\",\"line\":\"data\"}],\"sha\":\"${TRAVIS_COMMIT}\"}" \
   https://seriesci.com/api/thekenu/Consolidated-Firmware/codesize/many
