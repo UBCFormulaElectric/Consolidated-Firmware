d0808001cfac7837600f09f5f6d96ee7aeae0ae2
Ming Chen Lin <thekenu@gmail.com>
2020-04-12 19:12:31 -0700

    nit

--- boards/BMS/Test/Inc/imd_fff.h
+++ boards/BMS/Test/Inc/imd_fff.h
@@ -2,7 +2,6 @@
 
 extern "C"
 {
-#include <stdint.h>
     FAKE_VALUE_FUNC(float, get_pwm_frequency);
     FAKE_VALUE_FUNC(float, get_pwm_frequency_tolerance);
     FAKE_VALUE_FUNC(float, get_pwm_duty_cycle);
