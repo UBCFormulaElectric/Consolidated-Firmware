12bb5f7290aa3febf1aee71630b06d22210f5711
Ming Chen Lin <thekenu@gmail.com>
2020-04-14 19:34:10 -0700

    set imd cond in state machine .cpp

--- boards/BMS/Test/Src/state_machine_test.cpp
+++ boards/BMS/Test/Src/state_machine_test.cpp
@@ -69,6 +69,19 @@ class BmsStateMachineTest : public testing::Test
         };
     }
 
+    void SetImdCondition(enum Imd_Condition condition)
+    {
+        float mapping[NUM_OF_IMD_CONDITIONS] = {
+            [IMD_SHORT_CIRCUIT] = 0.0f,          [IMD_NORMAL] = 10.0f,
+            [IMD_UNDERVOLTAGE_DETECTED] = 20.0f, [IMD_SST] = 30.0f,
+            [IMD_DEVICE_ERROR] = 40.0f,          [IMD_EARTH_FAULT] = 50.0f,
+        };
+
+        get_pwm_frequency_fake.return_val = mapping[condition];
+        App_Imd_Tick(imd);
+        ASSERT_EQ(condition, App_Imd_GetCondition(imd));
+    }
+
     void TearDown() override
     {
         ASSERT_TRUE(world != NULL);
@@ -186,8 +199,7 @@ TEST_F(
     for (auto &state : GetAllStates())
     {
         SetInitialState(state);
-
-        get_pwm_frequency_fake.return_val = 10.0f;
+        SetImdCondition(IMD_NORMAL);
 
         // Test an arbitrarily chosen valid resistance
         get_pwm_duty_cycle_fake.return_val = 50.0f;
@@ -221,8 +233,7 @@ TEST_F(
     for (auto &state : GetAllStates())
     {
         SetInitialState(state);
-
-        get_pwm_frequency_fake.return_val = 20.0f;
+        SetImdCondition(IMD_UNDERVOLTAGE_DETECTED);
 
         // Test an arbitrarily chosen valid resistance
         get_pwm_duty_cycle_fake.return_val = 50.0f;
@@ -256,8 +267,7 @@ TEST_F(
     for (auto &state : GetAllStates())
     {
         SetInitialState(state);
-
-        get_pwm_frequency_fake.return_val = 30.0f;
+        SetImdCondition(IMD_SST);
 
         // Test an arbitrarily chosen SST_GOOD
         get_pwm_duty_cycle_fake.return_val = 7.5f;
