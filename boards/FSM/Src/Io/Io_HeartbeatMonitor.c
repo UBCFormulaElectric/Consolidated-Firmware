6ad8f5fa315eb30b739333c931e47358bb2aa153
Ming Chen Lin <thekenu@gmail.com>
2020-04-18 13:11:36 -0700

    nit clean ups

--- boards/FSM/Src/Io/Io_HeartbeatMonitor.c
+++ boards/FSM/Src/Io/Io_HeartbeatMonitor.c
@@ -1,5 +1,4 @@
 #include <cmsis_os.h>
-
 #include "Io_HeartbeatMonitor.h"
 
 uint32_t Io_HeartbeatMonitor_GetCurrentMs(void)
--- boards/shared/Test/Inc/Test_Shared.h
+++ boards/shared/Test/Inc/Test_Shared.h
@@ -7,3 +7,4 @@ class SharedTest : public testing::Test
 {
     // TODO: Put any shared code between tests here
 };
+
