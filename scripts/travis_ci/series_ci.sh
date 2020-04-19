d656649b116c2d4ab7c3c9a7edf66290f5560a5b
Ming Chen Lin <thekenu@gmail.com>
2020-04-19 14:32:38 -0700

    Update series CI script

--- scripts/travis_ci/series_ci.sh
+++ scripts/travis_ci/series_ci.sh
@@ -24,7 +24,7 @@ echo $data
 echo $bss
 
 curl \
-  --header "Authorization: Token 92cb0c5d-2b14-41c0-9cfc-06fa089aa108" \
+  --header "Authorization: Token 27d62d1f-71c6-4075-95b5-f7aec56d0204" \
   --header "Content-Type: application/json" \
   --data "{\"values\":[{\"value\":\"$text\",\"line\":\"text\"},{\"value\":\"$data\",\"line\":\"data\"},{\"value\":\"$bss\",\"line\":\"bss\"}],\"sha\":\"${TRAVIS_COMMIT}\"}" \
-  https://seriesci.com/api/thekenu/Consolidated-Firmware/$SERIES_NAME/many
+  https://seriesci.com/api/UBCFormulaElectric/Consolidated-Firmware/$SERIES_NAME/many
