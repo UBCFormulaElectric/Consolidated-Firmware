e1f9f50730a93623f737f21094923dfa2ac415f3
Ming Chen Lin <thekenu@gmail.com>
2020-04-20 18:38:32 -0700

    javadoc nit

--- boards/DIM/Inc/App/App_SocDigits.h
+++ boards/DIM/Inc/App/App_SocDigits.h
@@ -11,16 +11,13 @@ struct SocDigits
 
 /**
  * Round the given state of charge to an integer value by dropping the decimal
- * values, and then convert it to individual digits. For example, 67.857%
- * state of charge will be converted to:
- *
- *     digits[0] = 7
- *     digits[1] = 6
- *     digits[2] = (undefined because there are only 2 digits)
- *     num_digits = 2
- *
+ * values, and then convert it to individual digits.
  * @note Only the first num_digits values in digits[] are defined, the rest are
- *       undefined.
+ *       undefined. For example, 67.857% state of charge will be converted to:
+ *           digits[0] = 7
+ *           digits[1] = 6
+ *           digits[2] = (undefined because there are only 2 digits)
+ *           num_digits = 2
  * @param soc The state of charge to convert
  * @param soc_digits The buffer for storing the individual digits
  * @return ERROR_CODE_INVALID_ARGS if the given state of charge is out-of-bound
