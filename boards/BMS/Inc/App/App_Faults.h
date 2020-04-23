71aeee9bef57ae81d196b4cbd77b12b1b911569b
Ming Chen Lin <thekenu@gmail.com>
2020-04-23 14:57:40 -0700

    func declar

--- boards/BMS/Inc/App/App_Faults.h
+++ boards/BMS/Inc/App/App_Faults.h
@@ -5,4 +5,5 @@
 bool has_fsm_critical_fault(void* can_rx);
 bool has_dcm_critical_fault(void* can_rx);
 bool has_pdm_critical_fault(void* can_rx);
+bool has_bms_critical_fault(void* can_rx);
 bool has_critical_fault(void *can_rx);
