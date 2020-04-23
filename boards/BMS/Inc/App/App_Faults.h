1f79ba30b7a04ac5eb37ae2c7394a44c42ed084c
Ming Chen Lin <thekenu@gmail.com>
2020-04-23 14:51:29 -0700

    First draft

---
+++ boards/BMS/Inc/App/App_Faults.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <stdbool.h>
+
+bool has_fsm_critical_fault(void* can_rx);
+bool has_dcm_critical_fault(void* can_rx);
+bool has_pdm_critical_fault(void* can_rx);
+bool has_critical_fault(void *can_rx);
