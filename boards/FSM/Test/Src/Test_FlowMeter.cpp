d7cf85301a4b89a742f66ae47b75e4db88e25e00
Pasit Laothamatas <32230980+pasitlaothamatas@users.noreply.github.com>
2020-04-23 16:00:19 -0700

    Update boards/FSM/Test/Src/Test_FlowMeter.cpp
    Co-Authored-By: Ken Lin <thekenu@gmail.com>

--- boards/FSM/Test/Src/Test_FlowMeter.cpp
+++ boards/FSM/Test/Src/Test_FlowMeter.cpp
@@ -23,8 +23,7 @@ class FlowMeterTest : public FsmTest
 
 TEST_F(FlowMeterTest, check_if_flow_rate_is_updated_after_tick)
 {
-    constexpr float PWM_CAPTURED_FREQUENCY = 1.0f;
-    get_flow_rate_fake.return_val          = PWM_CAPTURED_FREQUENCY;
+    get_flow_rate_fake.return_val          = 1.0f;
     App_FlowMeter_Tick(flow_meter);
     ASSERT_EQ(
         get_flow_rate_fake.return_val, App_FlowMeter_GetFlowRate(flow_meter));
