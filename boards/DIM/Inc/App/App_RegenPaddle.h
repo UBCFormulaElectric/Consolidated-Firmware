65af3a5bfd9f49de1e8b81e96acb16dfecc220a4
Ming Chen Lin <thekenu@gmail.com>
2020-04-23 12:44:29 -0700

    update javadoc

--- boards/DIM/Inc/App/App_RegenPaddle.h
+++ boards/DIM/Inc/App/App_RegenPaddle.h
@@ -6,17 +6,27 @@
 /**
  * Allocate and initialize a regen paddle
  * @param get_paddle_position A function that can be called to get the paddle
- *                            position as a percentage, where 0 means not
- *                            pressed at all and 100 means pressed all the way
- * @param lower_deadzone The paddle position threshold of lower deadzone
- * @param upper_deadzone The paddle position threshold of upper deadzone
+ *                            position, where 0 means not pressed at all and
+ *                            100 means pressed all the way. Any paddle position
+ *                            outside of this range is considered invalid.
+ * @param lower_deadzone Any paddle position equal to or less than this will map
+ *                       to a regen value of 0
+ * @param upper_deadzone Any paddle position equal to or greater than this will
+ *                       map to a regen value of 100
  * @return The created regen paddle, whose ownership is given to the caller
  */
 struct RegenPaddle *App_RegenPaddle_Create(
     uint32_t (*get_paddle_position)(void),
     uint32_t lower_deadzone,
     uint32_t upper_deadzone);
-void      App_RegenPaddle_Destroy(struct RegenPaddle *regen_paddle);
+void App_RegenPaddle_Destroy(struct RegenPaddle *regen_paddle);
+
+/**
+ * Update the paddle position and regen value for the given regen paddle
+ * @param regen_paddle The regen paddle to update
+ * @return ERROR_CODE_OUT_OF_RANGE If we read a paddle position not within the
+ *                                 range 0 to 100 inclusive
+ */
 ErrorCode App_RegenPaddle_Tick(struct RegenPaddle *regen_paddle);
 uint32_t  App_RegenPaddle_GetRegen(const struct RegenPaddle *regen_paddle);
 uint32_t  App_RegenPaddle_GetPosition(const struct RegenPaddle *regen_paddle);
