af391651b104dc0f81e7f2ec9db80142b56b5557
Ming Chen Lin <thekenu@gmail.com>
2020-04-24 15:34:40 -0700

    rename voltage check

--- boards/PDM/Test/Src/Test_InRangeCheck.cpp
+++ boards/PDM/Test/Src/Test_InRangeCheck.cpp
@@ -14,7 +14,7 @@ class InRangeCheckTest : public PdmTest
   protected:
     virtual void SetUp()
     {
-        voltage_check = App_InRangeCheck_Create(
+        in_range_check = App_InRangeCheck_Create(
             get_value, DEFAULT_MIN_VALUE, DEFAULT_MAX_VAVLUE, error_callback);
 
         RESET_FAKE(get_value);
@@ -25,12 +25,12 @@ class InRangeCheckTest : public PdmTest
 
     virtual void TearDown()
     {
-        ASSERT_TRUE(voltage_check != NULL);
-        App_InRangeCheck_Destroy(voltage_check);
-        voltage_check = NULL;
+        ASSERT_TRUE(in_range_check != NULL);
+        App_InRangeCheck_Destroy(in_range_check);
+        in_range_check = NULL;
     }
 
-    struct InRangeCheck *voltage_check;
+    struct InRangeCheck *in_range_check;
 
     const float DEFAULT_MIN_VALUE  = 5.0f;
     const float DEFAULT_MAX_VAVLUE = 6.0f;
@@ -39,34 +39,34 @@ class InRangeCheckTest : public PdmTest
 TEST_F(InRangeCheckTest, value_in_range)
 {
     get_value_fake.return_val = (DEFAULT_MIN_VALUE + DEFAULT_MAX_VAVLUE) / 2.0f;
-    App_InRangeCheck_Tick(voltage_check);
-    ASSERT_EQ(App_InRangeCheck_GetStatus(voltage_check), VALUE_IN_RANGE);
+    App_InRangeCheck_Tick(in_range_check);
+    ASSERT_EQ(App_InRangeCheck_GetStatus(in_range_check), VALUE_IN_RANGE);
 }
 
 TEST_F(InRangeCheckTest, value_undervalue)
 {
     get_value_fake.return_val = DEFAULT_MIN_VALUE;
-    App_InRangeCheck_Tick(voltage_check);
-    ASSERT_EQ(App_InRangeCheck_GetStatus(voltage_check), VALUE_IN_RANGE);
+    App_InRangeCheck_Tick(in_range_check);
+    ASSERT_EQ(App_InRangeCheck_GetStatus(in_range_check), VALUE_IN_RANGE);
     ASSERT_EQ(error_callback_fake.call_count, 0);
 
     get_value_fake.return_val =
         std::nextafter(DEFAULT_MIN_VALUE, std::numeric_limits<float>::min());
-    App_InRangeCheck_Tick(voltage_check);
-    ASSERT_EQ(App_InRangeCheck_GetStatus(voltage_check), VALUE_UNDERFLOW);
+    App_InRangeCheck_Tick(in_range_check);
+    ASSERT_EQ(App_InRangeCheck_GetStatus(in_range_check), VALUE_UNDERFLOW);
     ASSERT_EQ(error_callback_fake.call_count, 1);
 }
 
 TEST_F(InRangeCheckTest, value_overflow)
 {
     get_value_fake.return_val = DEFAULT_MAX_VAVLUE;
-    App_InRangeCheck_Tick(voltage_check);
-    ASSERT_EQ(App_InRangeCheck_GetStatus(voltage_check), VALUE_IN_RANGE);
+    App_InRangeCheck_Tick(in_range_check);
+    ASSERT_EQ(App_InRangeCheck_GetStatus(in_range_check), VALUE_IN_RANGE);
     ASSERT_EQ(error_callback_fake.call_count, 0);
 
     get_value_fake.return_val =
         std::nextafter(DEFAULT_MAX_VAVLUE, std::numeric_limits<float>::max());
-    App_InRangeCheck_Tick(voltage_check);
-    ASSERT_EQ(App_InRangeCheck_GetStatus(voltage_check), VALUE_OVERFLOW);
+    App_InRangeCheck_Tick(in_range_check);
+    ASSERT_EQ(App_InRangeCheck_GetStatus(in_range_check), VALUE_OVERFLOW);
     ASSERT_EQ(error_callback_fake.call_count, 1);
 }
