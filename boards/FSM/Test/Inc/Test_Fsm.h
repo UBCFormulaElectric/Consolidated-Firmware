8cb93cc5c8e4c6e6a8c0e4f306bd062f78192fbd
Pasit Laothamatas <32230980+pasitlaothamatas@users.noreply.github.com>
2020-04-25 15:18:27 -0700

    Update boards/FSM/Test/Inc/Test_Fsm.h
    Co-Authored-By: Ken Lin <thekenu@gmail.com>

--- boards/FSM/Test/Inc/Test_Fsm.h
+++ boards/FSM/Test/Inc/Test_Fsm.h
@@ -2,15 +2,3 @@
 
 #include <fff.h>
 #include <gtest/gtest.h>
-
-extern "C"
-{
-#include "App_FlowMeter.h"
-}
-
-class FsmTest : public testing::Test
-{
-  protected:
-    struct FlowMeter *primary_flow_meter;
-    struct FlowMeter *secondary_flow_meter;
-};
