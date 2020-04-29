e4de346bb847b46a8654aa5dcebc616f5b7fcd82
Ming Chen Lin <thekenu@gmail.com>
2020-04-29 14:15:28 -0700

    fix pin mapping

--- boards/PDM/Src/Io/Io_RgbLedSequence.c
+++ boards/PDM/Src/Io/Io_RgbLedSequence.c
@@ -3,21 +3,15 @@
 
 void Io_RgbLedSequence_TurnOnRedLed(void)
 {
-    HAL_GPIO_WritePin(DIM_RED_GPIO_Port, DIM_RED_Pin, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(DIM_GREEN_GPIO_Port, DIM_GREEN_Pin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(DIM_BLUE_GPIO_Port, DIM_BLUE_Pin, GPIO_PIN_SET);
+    // TODO: Fill this in once the new PDM hardware with RGB LED is made
 }
 
 void Io_RgbLedSequence_TurnOnGreenLed(void)
 {
-    HAL_GPIO_WritePin(DIM_RED_GPIO_Port, DIM_RED_Pin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(DIM_GREEN_GPIO_Port, DIM_GREEN_Pin, GPIO_PIN_RESET);
-    HAL_GPIO_WritePin(DIM_BLUE_GPIO_Port, DIM_BLUE_Pin, GPIO_PIN_SET);
+    // TODO: Fill this in once the new PDM hardware with RGB LED is made
 }
 
 void Io_RgbLedSequence_TurnOnBlueLed(void)
 {
-    HAL_GPIO_WritePin(DIM_RED_GPIO_Port, DIM_RED_Pin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(DIM_GREEN_GPIO_Port, DIM_GREEN_Pin, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(DIM_BLUE_GPIO_Port, DIM_BLUE_Pin, GPIO_PIN_RESET);
+    // TODO: Fill this in once the new PDM hardware with RGB LED is made
 }
