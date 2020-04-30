b2279b0de3545775471bbcad14194aba31ce9f9c
Ming Chen Lin <thekenu@gmail.com>
2020-04-29 17:25:14 -0700

    nit

--- boards/DIM/Inc/App/App_BinarySwitch.h
+++ boards/DIM/Inc/App/App_BinarySwitch.h
@@ -5,5 +5,5 @@
 struct BinarySwitch;
 
 struct BinarySwitch *App_BinarySwitch_Create(bool (*is_turned_on)(void));
-bool App_BinarySwitch_IsTurnedOn(const struct BinarySwitch *binary_switch);
 void App_BinarySwitch_Destroy(struct BinarySwitch *binary_switch);
+bool App_BinarySwitch_IsTurnedOn(const struct BinarySwitch *binary_switch);
