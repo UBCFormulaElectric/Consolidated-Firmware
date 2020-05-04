41c7c73e8946642bcf2e68cfb28bdc9ba1e39f6e
Ming Chen Lin <thekenu@gmail.com>
2020-05-04 11:51:35 -0700

    World.h comment

--- boards/PDM/Inc/App/App_PdmWorld.h
+++ boards/PDM/Inc/App/App_PdmWorld.h
@@ -33,20 +33,61 @@ struct PdmWorld *App_PdmWorld_Create(
     struct RgbLedSequence *   rgb_led_sequence);
 
 /**
- * Destroy the given PDM world, freeing the memory associated with it
- * @param world The PDM world to destroy
+ * Deallocate the memory used by the given world
+ * @param world The world to deallocate
  */
 void App_PdmWorld_Destroy(struct PdmWorld *world);
 
+/**
+ * Get the CAN TX interface for the given world
+ * @param world The world to get CAN TX interface for
+ * @return The CAN TX interface for the given world
+ */
 struct PdmCanTxInterface *App_PdmWorld_GetCanTx(const struct PdmWorld *world);
+
+/**
+ * Get the CAN RX interface for the given world
+ * @param world The world to get CAN RX interface for
+ * @return The CAN RX interface for the given world
+ */
 struct PdmCanRxInterface *App_PdmWorld_GetCanRx(const struct PdmWorld *world);
+
+/**
+ * Get the VBAT voltage monitor for the given world
+ * @param world The world to get VBAT voltage monitor for
+ * @return The CAN VBAT voltage monitor for the given world
+ */
 struct VoltageMonitor *
     App_PdmWorld_GetVbatVoltageMonitor(const struct PdmWorld *world);
+
+/**
+ * Get the 24V AUX voltage monitor for the given world
+ * @param world The world to get 24V AUX voltage monitor for
+ * @return The CAN 24V AUX voltage monitor for the given world
+ */
 struct VoltageMonitor *
     App_PdmWorld_Get24vAuxVoltageMonitor(const struct PdmWorld *world);
+
+/**
+ * Get the 24V ACC voltage monitor for the given world
+ * @param world The world to get 24V ACC voltage monitor for
+ * @return The CAN 24V ACC voltage monitor for the given world
+ */
 struct VoltageMonitor *
     App_PdmWorld_Get24vAccVoltageMonitor(const struct PdmWorld *world);
+
+/**
+ * Get the heartbeat monitor for the given world
+ * @param world The world to get heartbeat monitor for
+ * @return The heartbeat monitor for the given world
+ */
 struct HeartbeatMonitor *
     App_PdmWorld_GetHeartbeatMonitor(const struct PdmWorld *world);
+
+/**
+ * Get the RGB LED sequence for the given world
+ * @param world The world to get RGB LED sequence for
+ * @return The RGB LED sequence for the given world
+ */
 struct RgbLedSequence *
     App_PdmWorld_GetRgbLedSequence(const struct PdmWorld *world);
