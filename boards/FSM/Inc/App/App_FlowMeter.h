24bf0a39ccfb9d7d655565d10d1001d6868f26d4
Ming Chen Lin <thekenu@gmail.com>
2020-05-06 15:34:07 -0700

    nit

--- boards/FSM/Inc/App/App_FlowMeter.h
+++ boards/FSM/Inc/App/App_FlowMeter.h
@@ -4,7 +4,7 @@ struct FlowMeter;
 
 /**
  * Create the flow meter to read flow rate from
- * @param get_flow_rate : A function pointer to a function that
+ * @param get_flow_rate A function pointer to a function that
  * acquires the flow rate from the flow meter
  * @return A pointer to the created flow meter, whose ownership is given to the
  * caller
@@ -19,7 +19,7 @@ void App_FlowMeter_Destroy(struct FlowMeter *flow_meter);
 
 /**
  * Get the flow rate from a flow meter
- * @param flow_meter: The flow meter to get the flow rate from
+ * @param flow_meter The flow meter to get the flow rate from
  * @return The flow rate of the flow meter (L/min)
  */
 float App_FlowMeter_GetFlowRate(const struct FlowMeter *flow_meter);
