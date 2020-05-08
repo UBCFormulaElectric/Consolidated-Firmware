6d90a87f54f2fcf81381134ae9de7b3ab167480e
Ming Chen Lin <thekenu@gmail.com>
2020-05-07 22:43:55 -0700

    Test utils

--- boards/PDM/Test/Inc/Test_Pdm.h
+++ boards/PDM/Test/Inc/Test_Pdm.h
@@ -3,10 +3,4 @@
 #include <fff.h>
 #include <gtest/gtest.h>
 
-template <typename T>
-void TearDownObject(T *&object, void (*destroy_func)(T *object))
-{
-    ASSERT_TRUE(object != NULL);
-    destroy_func(object);
-    object = NULL;
-}
+#include "Test_Utils.h"
