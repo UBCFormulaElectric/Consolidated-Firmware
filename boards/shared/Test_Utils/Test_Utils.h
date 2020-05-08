943f2718326eb0d95614f50069d6d8d26501a03a
Ming Chen Lin <thekenu@gmail.com>
2020-05-08 10:37:25 -0700

    format

--- boards/shared/Test_Utils/Test_Utils.h
+++ boards/shared/Test_Utils/Test_Utils.h
@@ -3,7 +3,7 @@
 template <typename T>
 void TearDownObject(T *&object, void (*destroy_func)(T *object))
 {
-ASSERT_TRUE(object != NULL);
-destroy_func(object);
-object = NULL;
+    ASSERT_TRUE(object != NULL);
+    destroy_func(object);
+    object = NULL;
 }
