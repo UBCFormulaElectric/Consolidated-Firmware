204f75915f727f5db39c763b67312e9c3ab2e822
markomarkusi <marko.markusi@hotmail.com>
2020-05-10 13:54:41 -0700

    Fix styling

--- boards/PDM/Inc/Io/Io_Efuse.h
+++ boards/PDM/Inc/Io/Io_Efuse.h
@@ -11,11 +11,11 @@
 #include <stdbool.h>
 #include "main.h"
 
-/* Current Sense Ratios */
+// Current Sense Ratios
 #define HIGH_CURRENT_SENSE_RATIO (1.0f / 1500.0f)
 #define LOW_CURRENT_SENSE_RATIO (1.0f / 500.0f)
 
-/* Current Sense Channels */
+// Current Sense Channels
 #define AUX1_CURRENT_SENSE_CHANNEL CSNS_FUNCTION_CURRENT_CH0
 #define AUX2_CURRENT_SENSE_CHANNEL CSNS_FUNCTION_CURRENT_CH1
 #define EM_CURRENT_SENSE_CHANNEL CSNS_FUNCTION_CURRENT_CH0
@@ -25,17 +25,9 @@
 #define AIR_SHDN_CURRENT_SENSE_CHANNEL CSNS_FUNCTION_CURRENT_CH0
 #define GLV_CURRENT_SENSE_CHANNEL CSNS_FUNCTION_CURRENT_CH1
 
-#define EFUSE_ADDR_MASK 0xFU
-#define EFUSE_ADDR_SHIFT 0x0AU
-#define EFUSE_SI_DATA_MASK 0x1FFU
-#define EFUSE_SO_DATA_MASK 0xFF // Ignore Normal-Mode status bit (bit 9)
+//! Serial input register addresses
 
-#define WATCH_DOG_BIT (1U << 15U)
-#define PARITY_BIT_SHIFT 0x0FU
-
-/* Serial input register addresses */
-
-/* STATR_s input address */
+// STATR_s input address
 #define SI_STATR_0_ADDR 0x00U
 #define SI_STATR_1_ADDR 0x08U
 
@@ -49,7 +41,7 @@
 #define SOA2_SHIFT 0x02U
 #define SOA3_SHIFT 0x0DU
 
-/* PWMR_s input address */
+// PWMR_s input address
 #define SI_PWMR_0_ADDR 0x01U
 #define SI_PWMR_1_ADDR 0x09U
 
@@ -73,7 +65,7 @@
 #define PWM7_S_SHIFT 0x07U
 #define ON_S_SHIFT 0x08U
 
-/* CONFR_s input address */
+// CONFR_s input address
 #define SI_CONFR_0_ADDR 0x02U
 #define SI_CONFR_1_ADDR 0x0AU
 
@@ -97,7 +89,7 @@
 #define OLON_DIS_S_SHIFT 0x07U
 #define OS_DIS_S_SHIFT 0x08U
 
-/* OCR_s input address */
+// OCR_s input address
 #define SI_OCR_0_ADDR 0x04U
 #define SI_OCR_1_ADDR 0x0CU
 
@@ -121,7 +113,7 @@
 #define PR_S_SHIFT 0x07U
 #define HOCR_S_SHIFT 0x08U
 
-/* RETRY_s input address */
+// RETRY_s input address
 #define SI_RETRY_0_ADDR 0x05U
 #define SI_RETRY_1_ADDR 0x0DU
 
@@ -137,7 +129,7 @@
 #define AUTO_PERIOD_1_S_SHIFT 0x03U
 #define OFP_S_SHIFT 0x08U
 
-/* GCR_s input address */
+// GCR_s input address
 #define SI_GCR_ADDR 0x06U
 
 #define OV_DIS_MASK 0x01U
@@ -160,14 +152,14 @@
 #define PWM_EN_0_SHIFT 0x07U
 #define PWM_EN_1_SHIFT 0x08U
 
-/* CALR_s input address */
+// CALR_s input address
 #define SI_CALR_0_ADDR 0x07U
 #define SI_CALR_1_ADDR 0x0FU
 #define CALR_MASK 0x15BU
 
-/* Serial output register addresses */
+//! Serial output register addresses
 
-/* STATR output address */
+// STATR output address
 #define SO_STATR_ADDR 0x00U
 
 #define SO_OUT0_MASK 0x01U
@@ -190,7 +182,7 @@
 #define SO_UV_SHIFT 0x07U
 #define SO_OV_SHIFT 0x08U
 
-/* FAULTR_r output address */
+// FAULTR_r output address
 #define SO_FAULTR_0_ADDR 0x01U
 #define SO_FAULTR_1_ADDR 0x09U
 
@@ -210,7 +202,7 @@
 #define SO_OLON_SHIFT 0x06U
 #define SO_OTW_SHIFT 0x08U
 
-/* PWMR_s output address */
+// PWMR_s output address
 #define SO_PWMR_0_ADDR 0x02U
 #define SO_PWMR_1_ADDR 0x0AU
 
@@ -220,7 +212,7 @@
 #define SO_PWM_S_SHIFT 0x00U
 #define SO_ON_S_SHIFT 0x08U
 
-/* CONFR_s output address */
+// CONFR_s output address
 #define SO_CONFR_0_ADDR 0x03U
 #define SO_CONFR_1_ADDR 0x0BU
 
@@ -238,7 +230,7 @@
 #define SO_OLON_DIS_S_SHIFT 0x07U
 #define SO_OS_DIS_S_SHIFT 0x08U
 
-/* OCR_s output address */
+// OCR_s output address
 #define SO_OCR_0_ADDR 0x04U
 #define SO_OCR_1_ADDR 0x0CU
 
@@ -262,7 +254,7 @@
 #define SO_PR_S_SHIFT 0x07U
 #define SO_HOCR_S_SHIFT 0x08U
 
-/* RETRYR_S output address */
+// RETRYR_S output address
 #define SO_RETRYR_0_ADDR 0x05U
 #define SO_RETRYR_1_ADDR 0x0DU
 
@@ -278,7 +270,7 @@
 #define SO_R_SHIFT 0x04U
 #define SO_OFP_SHIFT 0x08U
 
-/* GCR output address */
+// GCR output address
 #define SO_GCR_ADDR 0x06U
 
 #define SO_OV_DIS_MASK 0x01U
@@ -299,7 +291,7 @@
 #define SO_PWM_EN_0_SHIFT 0x07U
 #define SO_PWM_EN_1_SHIFT 0x08U
 
-/* DIAGR output address */
+// DIAGR output address
 #define SO_DIAGR_ADDR 0x07U
 
 #define SO_CAL_FAIL0_MASK 0x01U
@@ -322,7 +314,7 @@
 #define SO_CONF0_SHIFT 0x07U
 #define SO_CONF1_SHIFT 0x08U
 
-/* Feature definitions in CONFR_s register */
+// Feature definitions in CONFR_s register
 #define SWITCH_ON_DELAY_NO_DELAY 0x00U
 #define SWITCH_ON_DELAY_32_PWM_CLOCKS 0x01U
 #define SWITCH_ON_DELAY_64_PWM_CLOCKS 0x02U
@@ -349,7 +341,7 @@
 #define SHORT_CIRCUIT_DETECTION_ENABLE (0x00 << OS_DIS_S_SHIFT)
 #define SHORT_CIRCUIT_DETECTION_DISABLE (0x01 << OS_DIS_S_SHIFT)
 
-/* Feature definitions in OCR_s register */
+// Feature definitions in OCR_s register
 #define LOW_CURRENT_THRESHOLD_SELECT_IOCL1 \
     ((0x00 << HOCR_S_SHIFT) | (0x01 << OCL_S_SHIFT))
 #define LOW_CURRENT_THRESHOLD_SELECT_IOCL2 \
@@ -390,7 +382,7 @@
 #define PWM_EXTERNAL_CLOCK_DIVIDER_256 (0x00 << PR_S_SHIFT)
 #define PWM_EXTERNAL_CLOCK_DIVIDER_512 (0x01 << PR_S_SHIFT)
 
-/* Feature definitions in RETRY_s register */
+// Feature definitions in RETRY_s register
 #define AUTO_RETRY_PERIOD_T_AUTO_00 \
     ((0x00 << AUTO_PERIOD_1_S_SHIFT) | (0x00 << AUTO_PERIOD_0_S_SHIFT))
 #define AUTO_RETRY_PERIOD_tAUTO_01 \
@@ -402,7 +394,6 @@
 
 #define AUTO_RETRY_NUMBER_16 (0x00 << RETRY_UNLIMITED_S_SHIFT)
 #define AUTO_RETRY_NUMBER_INFINITE (0x01 << RETRY_UNLIMITED_S_SHIFT)
-// Overcurrent profile specific. Dependent on CONF[x] pin state.
 #define AUTO_RETRY_BULB_ENABLED (0x00 << RETRY_S_SHIFT)
 #define AUTO_RETRY_BULB_DISABLED (0x01 << RETRY_S_SHIFT)
 #define AUTO_RETRY_DC_MOTOR_ENABLED (0x01 << RETRY_S_SHIFT)
@@ -411,7 +402,7 @@
 #define RANDOM_CURRENT_OFFSET_ADD (0x01 << OFP_S_SHIFT)
 #define RANDOM_CURRENT_OFFSET_SUBTRACT (0x00 << OFP_S_SHIFT)
 
-/* Feature definitions in GCR register */
+// Feature definitions in GCR register
 #define OVERVOLTAGE_PROTECTION_ENABLE (0x00 << OV_DIS_SHIFT)
 #define OVERVOLTAGE_PROTECTION_DISABLE (0x01 << OV_DIS_SHIFT)
 
@@ -444,46 +435,46 @@
 #define PWM_CHANNEL_1_ENABLE (0x01 << PWM_EN_1_SHIFT)
 #define PWM_CHANNEL_1_DISABLE (0x00 << PWM_EN_1_SHIFT)
 
-/* Configuration values used for initializing the registers */
+//! Configuration values used for initializing the registers
 
-/* Global Configuration */
-#define GCR_CONFIG                                                  \
+// Global Configuration
+#define GCR_CONFIG                                                           \
     (PWM_CHANNEL_0_DISABLE | PWM_CHANNEL_1_DISABLE | PARALLEL_MODE_DISABLE | \
      TRACK_AND_HOLD_CURRENT_DISABLE | WATCHDOG_DISABLE |                     \
      VDD_FAILURE_DETECTION_DISABLE | CSNS_FUNCTION_DISABLED |                \
      OVERVOLTAGE_PROTECTION_ENABLE)
 
-/* Channel 0 Configurations */
-#define RETRY_0_CONFIG                                    \
+// Channel 0 Configurations
+#define RETRY_0_CONFIG                                             \
     (RANDOM_CURRENT_OFFSET_SUBTRACT | AUTO_RETRY_NUMBER_INFINITE | \
      AUTO_RETRY_PERIOD_T_AUTO_10 | AUTO_RETRY_DC_MOTOR_DISABLED)
-#define CONFR_0_CONFIG                                         \
+#define CONFR_0_CONFIG                                                  \
     (SHORT_CIRCUIT_DETECTION_DISABLE | OPEN_LOAD_DETECTION_ON_DISABLE | \
      OPEN_LOAD_DETECTION_OFF_DISABLE | DIRECT_CONTROL_ENABLE |          \
      SWITCH_ON_DELAY_NO_DELAY | SLEW_RATE_MEDIUM)
-#define OCR_0_CONFIG                                     \
+#define OCR_0_CONFIG                                              \
     (PWM_EXTERNAL_CLOCK_DIVIDER_256 | PWM_CLOCK_SOURCE_EXTERNAL | \
      THRESHOLD_ACTIVATION_TIMES_T_OCH1_T_OCM1_L |                 \
      MEDIUM_CURRENT_THRESHOLD_SELECT_IOCM1 |                      \
      LOW_CURRENT_THRESHOLD_SELECT_IOCL3 |                         \
      HIGH_CURRENT_THRESHOLD_SELECT_IOCH1 | CURRENT_SENSE_RATIO_LOW)
 
-/* Channel 1 Configurations */
-#define RETRY_1_CONFIG                                    \
+// Channel 1 Configurations
+#define RETRY_1_CONFIG                                             \
     (RANDOM_CURRENT_OFFSET_SUBTRACT | AUTO_RETRY_NUMBER_INFINITE | \
      AUTO_RETRY_PERIOD_T_AUTO_10 | AUTO_RETRY_DC_MOTOR_DISABLED)
-#define CONFR_1_CONFIG                                         \
+#define CONFR_1_CONFIG                                                  \
     (SHORT_CIRCUIT_DETECTION_DISABLE | OPEN_LOAD_DETECTION_ON_DISABLE | \
      OPEN_LOAD_DETECTION_OFF_DISABLE | DIRECT_CONTROL_ENABLE |          \
      SWITCH_ON_DELAY_NO_DELAY | SLEW_RATE_MEDIUM)
-#define OCR_1_CONFIG                                     \
+#define OCR_1_CONFIG                                              \
     (PWM_EXTERNAL_CLOCK_DIVIDER_256 | PWM_CLOCK_SOURCE_EXTERNAL | \
      THRESHOLD_ACTIVATION_TIMES_T_OCH1_T_OCM1_L |                 \
      MEDIUM_CURRENT_THRESHOLD_SELECT_IOCM1 |                      \
      LOW_CURRENT_THRESHOLD_SELECT_IOCL3 |                         \
      HIGH_CURRENT_THRESHOLD_SELECT_IOCH1 | CURRENT_SENSE_RATIO_LOW)
 
-/* Serial Output Registers Enums */
+//! Serial Output Registers Enums
 
 //! Enumeration for the status types
 typedef enum
@@ -530,18 +521,94 @@ typedef enum
 //! Enumeration for the channel configurations types
 typedef enum
 {
-    ChannelConfigType_Delay_0              = (1 << 0), //!< Switch-on Delay bit 0
-    ChannelConfigType_Delay_1              = (1 << 1), //!< Switch-on Delay bit 1
-    ChannelConfigType_Delay_2              = (1 << 2), //!< Switch-on Delay bit 2
-    ChannelConfigType_SlewRate_0           = (1 << 3), //!< Slew Rate bit 3
-    ChannelConfigType_SlewRate_1           = (1 << 4), //!< Slew Rate bit 4
-    ChannelConfigType_DirecControl_Disable = (1 << 5), //!< Direct Control Disable
-    ChannelConfigType_OpenLoad_InOffState  = (1 << 6), //!< Open Load Detection in Off-state
-    ChannelConfigType_OpenLoad_InOnState   = (1 << 7), //!< Open Load Detection in On-state
-    ChannelConfigType_ShortCircuitDetection_Disable = (1 << 8) //!< Short-circuit to Vpwr Detection Disable
+    ChannelConfigType_Delay_0    = (1 << 0), //!< Switch-on Delay Bit 0
+    ChannelConfigType_Delay_1    = (1 << 1), //!< Switch-on Delay Bit 1
+    ChannelConfigType_Delay_2    = (1 << 2), //!< Switch-on Delay Bit 2
+    ChannelConfigType_SlewRate_0 = (1 << 3), //!< Slew Rate Bit 3
+    ChannelConfigType_SlewRate_1 = (1 << 4), //!< Slew Rate Bit 4
+    ChannelConfigType_DIR_Dis    = (1 << 5), //!< Direct Control Disable
+    ChannelConfigType_OL_OFF_Dis =
+        (1 << 6), //!< Open Load Detection in Off-state Disable
+    ChannelConfigType_OL_ON_Dis =
+        (1 << 7), //!< Open Load Detection in On-state Disable
+    ChannelConfigType_OS_Dis =
+        (1 << 8) //!< Short-circuit to Vpwr Detection Disable
 } ChannelConfigType_e;
 
-struct E_Fuse
+//! Enumeration for the overcurrent protection types
+typedef enum
+{
+    OverCurrentConfigType_OCL = (1 << 0), //!< Lowest Over-current Profile
+    OverCurrentConfigType_OCM = (1 << 1), //!< Over-current Medium Threshold
+    OverCurrentConfigType_OCH = (1 << 2), //!< Over-current High Threshold
+    OverCurrentConfigType_tOCM =
+        (1 << 3), //!< Over-current Medium Threshold Window
+    OverCurrentConfigType_tOCH =
+        (1 << 4), //!< Over-current High Threshold Window
+    OverCurrentConfigType_CurrentSense_Ratio =
+        (1 << 5), //!< Low-current Sense Ratio
+    OverCurrentConfigType_Clock_Internal =
+        (1 << 6),                          //!< PWM Internal Clock Enable
+    OverCurrentConfigType_PR   = (1 << 7), //!< PWM Frequency 2X Prescaler
+    OverCurrentConfigType_HOCR = (1 << 8)  //!< Highest Over-current Profile
+} OverCurrentConfigType_e;
+
+//! Enumeration for the retry configurations types
+typedef enum
+{
+    RetryType_AutoRetry = (1 << 0), //!< Auto Retry Disable for Lighting Profile
+    RetryType_UnlimitedRetries = (1 << 1), //!< Unlimited Retries
+    RetryType_Auto_Period0     = (1 << 2), //!< Auto-retry Period Bit 0
+    RetryType_Auto_Period1     = (1 << 3), //!< Auto-retry Period Bit 1
+    RetryType_ReservedBit_0    = (1 << 4), //!< Unused Bit 0
+    RetryType_ReservedBit_1    = (1 << 5), //!< Unused Bit 0
+    RetryType_ReservedBit_2    = (1 << 6), //!< Unused Bit 0
+    RetryType_ReservedBit_3    = (1 << 7), //!< Unused Bit 0
+    RetryType_Add_Offset_Current =
+        (1 << 8) //!< Random Offset Current Added to Sensed Current
+} RetryType_e;
+
+//! Enumeration for the global configurations types
+typedef enum
+{
+    GlobalConfigType_OvervoltageProtection_Disable =
+        (1 << 0), //!< Over-voltage Protection Disable
+    GlobalConfigType_CSNS0_Enable =
+        (1 << 1), //!< Current Sensing on Channel 0 Enable
+    GlobalConfigType_CSNS1_Enable =
+        (1 << 2), //!< Current Sensing on Channel 1 Enable
+    GlobalConfigType_Temperature_Sense_Enable =
+        ((1 << 1) | (1 << 2)), //!< Temperature Sensing Enable
+    GlobalConfigType_VDD_Fail_Enable =
+        (1 << 3), //!< VDD Failure Detection Enable
+    GlobalConfigType_Watchdog_Disable = (1 << 4), //!< SPI Watchdog Disable
+    GlobalConfigType_TrackHold_Sense_Enable =
+        (1 << 5), //!< Track and Hold Current Sensing Enable
+    GlobalConfigType_Parallel_Mode_Enable =
+        (1 << 6), //!< Parallel Channels Mode Enable
+    GlobalConfigType_PWM_Channel0_Enable =
+        (1 << 7), //!< Channel 0 PWM Function Enable
+    GlobalConfigType_PWM_Channel1_Enable =
+        (1 << 8) //!< Channel 1 PWM Function Enable
+} GlobalConfigType_e;
+
+//! Enumeration for the diagnostic configurations types
+typedef enum
+{
+    DiagnosticType_Cal0_Failure =
+        (1 << 0), //!< Channel 0 Internal Clock Calibration Failure
+    DiagnosticType_Cal1_Failure =
+        (1 << 1), //!< Channel 1 Internal Clock Calibration Failure
+    DiagnosticType_ExtClock_Failure = (1 << 2), //!< External Clock Failure
+    DiagnosticType_Input0_On        = (1 << 3), //!< Input 0 Pin On
+    DiagnosticType_Input1_On        = (1 << 4), //!< Input 1 Pin On
+    DiagnosticType_ID0              = (1 << 5), //!< Product ID Number Bit 0
+    DiagnosticType_ID1              = (1 << 6), //!< Product ID Number Bit 1
+    DiagnosticType_CONF0            = (1 << 7), //!< Channel 0 Config DC Motor
+    DiagnosticType_CONF1            = (1 << 8)  //!< Channel 1 Config DC Motor
+} DiagnosticType_e;
+
+struct Efuse
 {
     bool         watch_dog_state;
     StatusType_e status;
@@ -550,72 +617,78 @@ struct E_Fuse
         FaultType_e channel_0_faults;
         FaultType_e channel_1_faults;
     } faults;
-    struct E_FuseChipSelect
+    struct EfuseChipSelect
     {
         GPIO_TypeDef *GPIO_Port;
         uint16_t      GPIO_Pin;
     } chip_select;
-    struct E_FuseCurrentSenseSync
+    struct EfuseCurrentSenseSync
     {
         GPIO_TypeDef *GPIO_Port;
         uint16_t      GPIO_Pin;
     } current_sense_sync;
 };
 
-/* Function Definitions */
-
 /**
  * Initializes the SPI handle for the Efuses
  * @param hspi Handle to the SPI peripheral used for the Efuses
  */
-void Io_Efuse_22XS4200_Init(SPI_HandleTypeDef *const hspi);
+void Io_Efuse_Init(SPI_HandleTypeDef *const hspi);
 
 /**
- * Configures the Efuse's registers using the values defined in: REGNAME_CONFIG
- * @param *e_fuse Pointer to E_Fuse structure being configured
+ * Configures the Efuse's registers using the values defined in: REGISTERNAME_CONFIG
+ * @param e_fuse Pointer to Efuse structure being configured
  */
-void Io_Efuse_22XS4200_configureEfuse(struct E_Fuse *e_fuse);
+void Io_Efuse_ConfigureEfuse(struct Efuse *e_fuse);
 
 /**
  * Sets current/temperature monitoring option of CSNS pin
- * @param selection The desired monitoring function which can be selected by specifying the following values:
- *                  CSNS_FUNCTION_DISABLED - Current sensing disabled
- *                  CSNS_FUNCTION_CURRENT_CH0 - Current sensing for channel 0
+ * @param selection The desired monitoring function which can be selected by
+ * specifying the following values: CSNS_FUNCTION_DISABLED - Current sensing
+ * disabled CSNS_FUNCTION_CURRENT_CH0 - Current sensing for channel 0
  *                  CSNS_FUNCTION_CURRENT_CH1 - Current sensing for channel 1
  *                  CSNS_FUNCTION_TEMPERATURE - Ffuse temperature sensing
- *                  CSNS_FUNCTION_CURRENT_SUM - Current sensing for summed channels (for Parallel mode)
- * @param *e_fuse Pointer to E_Fuse structure being configured
+ *                  CSNS_FUNCTION_CURRENT_SUM - Current sensing for summed
+ * channels (for Parallel mode)
+ * @param e_fuse Pointer to Efuse structure being configured
  */
-void Io_Efuse_22XS4200_configure_channel_monitoring(uint8_t selection, struct E_Fuse *e_fuse);
+void Io_Efuse_Configure_Channel_Monitoring(
+    uint8_t        selection,
+    struct Efuse *e_fuse);
 
 /**
- * Gets the value of the Efuse's Status Register over SPI and updates the Efuse handle's operating status
- * @param *e_fuse Pointer to E_Fuse structure being checked
+ * Gets the value of the Efuse's Status Register over SPI and updates the Efuse
+ * handle's operating status
+ * @param e_fuse Pointer to Efuse structure being checked
  */
-void Io_Efuse_22XS4200_UpdateStatus(struct E_Fuse *e_fuse);
+void Io_Efuse_UpdateStatus(struct Efuse *e_fuse);
 
 /**
- * DGets the value of the Efuse's Fault Register over SPI and updates the Efuse handle's fault status
- * @param *e_fuse Pointer to E_Fuse structure being checked
+ * Get the value of the Efuse's Fault Register over SPI and updates the Efuse handle's fault status
+ * handle's fault status
+ * @param e_fuse Pointer to Efuse structure being checked
  */
-void Io_Efuse_22XS4200_UpdateFaults(struct E_Fuse *e_fuse);
-
+void Io_Efuse_UpdateFaults(struct Efuse *e_fuse);
 
 /**
  * Write data to a specific Serial Input register on the Efuse
  * @param register_address Serial Input register being written to
  * @param register_value Value being written to the Serial Input register
- * @param *e_fuse Pointer to E_Fuse structure being written to
+ * @param e_fuse Pointer to Efuse structure being written to
  */
-void Io_Efuse_22XS4200_WriteReg(uint8_t register_address, uint16_t register_value, struct E_Fuse *e_fuse);
+void Io_Efuse_WriteReg(
+    uint8_t        register_address,
+    uint16_t       register_value,
+    struct Efuse *e_fuse);
 
 /**
  * Read data from a specific Serial Output register on the Efuse
  * @param register_address Serial Output register being read from
- * @param *e_fuse Pointer to E_Fuse structure being read from
+ * @param e_fuse Pointer to Efuse structure being read from
  * @return Contents of the addressed register
  */
-uint16_t Io_Efuse_22XS4200_ReadReg(uint8_t register_address, struct E_Fuse *e_fuse);
+uint16_t
+    Io_Efuse_ReadReg(uint8_t register_address, struct Efuse *e_fuse);
 
 /**
  * Calculates the parity of the SPI command to be sent to the Efuse.
@@ -627,20 +700,27 @@ uint16_t Io_Efuse_22XS4200_ReadReg(uint8_t register_address, struct E_Fuse *e_fu
 uint8_t Io_Efuse_Cal_Parity_Bit(uint16_t spi_command);
 
 /**
- * Write SPI data to Efuse
+ * Write SPI data to Efuse in blocking mode
  * @param *TxData Pointer to data being sent to the Efuse
  * @param *GPIO_Port Handle to Efuse's chip-select GPIO port
  * @param GPIO_Pin Efuse's chip-select GPIO pin
  * @return Status of the SPI transfer
  */
-HAL_StatusTypeDef Io_Efuse_22XS4200_WriteToEfuse(uint16_t *TxData, GPIO_TypeDef *GPIO_Port, uint16_t GPIO_Pin);
+HAL_StatusTypeDef Io_Efuse_WriteToEfuse(
+    uint16_t *    TxData,
+    GPIO_TypeDef *GPIO_Port,
+    uint16_t      GPIO_Pin);
 
 /**
- * Read SPI data from Efuse
+ * Read SPI data from Efuse in blocking mode
  * @param *TxData Pointer to data being sent to the Efuse
  * @param *RxData Pointer to data being received from the Efuse
  * @param *GPIO_Port Handle to Efuse's chip-select GPIO port
  * @param GPIO_Pin Efuse's chip-select GPIO pin
  * @return Status of the SPI transfer
  */
-HAL_StatusTypeDef Io_Efuse_22XS4200_ReadFromEfuse(uint16_t *TxData, uint16_t *RxData, GPIO_TypeDef *GPIO_Port, uint16_t GPIO_Pin);
+HAL_StatusTypeDef Io_Efuse_ReadFromEfuse(
+    uint16_t *    TxData,
+    uint16_t *    RxData,
+    GPIO_TypeDef *GPIO_Port,
+    uint16_t      GPIO_Pin);
---
+++ boards/PDM/Src/Io/Io_Efuse.c
@@ -0,0 +1,202 @@
+#include "Io_Efuse.h"
+
+#define EFUSE_ADDR_MASK 0xFU
+#define EFUSE_ADDR_SHIFT 0x0AU
+#define EFUSE_SI_DATA_MASK 0x1FFU
+#define EFUSE_SO_DATA_MASK 0xFF // Ignore Normal-Mode status bit (bit 9)
+
+#define WATCH_DOG_BIT (1U << 15U)
+#define PARITY_BIT_SHIFT 0x0EU
+
+// The SPI handle for the SPI device the E-Fuses are connected to
+static SPI_HandleTypeDef *efuse_spi_handle;
+
+void Io_Efuse_Init(SPI_HandleTypeDef *const hspi)
+{
+    efuse_spi_handle = hspi;
+}
+
+void Io_Efuse_ConfigureEfuse(struct Efuse *e_fuse)
+{
+    // Global config register
+    Io_Efuse_WriteReg(SI_GCR_ADDR, GCR_CONFIG, e_fuse);
+
+    // Channel 0 config registers
+    Io_Efuse_WriteReg(SI_RETRY_0_ADDR, RETRY_0_CONFIG, e_fuse);
+    Io_Efuse_WriteReg(SI_CONFR_0_ADDR, CONFR_0_CONFIG, e_fuse);
+    Io_Efuse_WriteReg(SI_OCR_0_ADDR, OCR_0_CONFIG, e_fuse);
+
+    // Channel 1 config registers
+    Io_Efuse_WriteReg(SI_RETRY_1_ADDR, RETRY_1_CONFIG, e_fuse);
+    Io_Efuse_WriteReg(SI_CONFR_1_ADDR, CONFR_1_CONFIG, e_fuse);
+    Io_Efuse_WriteReg(SI_OCR_1_ADDR, OCR_1_CONFIG, e_fuse);
+}
+
+void Io_Efuse_Configure_Channel_Monitoring(
+    uint8_t        selection,
+    struct Efuse *e_fuse)
+{
+    uint16_t reg_val = 0x0000;
+
+    // Read original content of GCR Register
+    reg_val = Io_Efuse_ReadReg(SI_GCR_ADDR, e_fuse);
+
+    reg_val &= ~(CSNS1_EN_MASK | CSNS0_EN_MASK);
+    reg_val |= (selection & (CSNS1_EN_MASK | CSNS0_EN_MASK));
+
+    Io_Efuse_WriteReg(SI_GCR_ADDR, reg_val, e_fuse);
+}
+
+void Io_Efuse_UpdateStatus(struct Efuse *e_fuse)
+{
+    uint16_t status = StatusType_NoFault;
+
+    status = Io_Efuse_ReadReg(SO_STATR_ADDR, e_fuse);
+
+    // Clear other bits
+    status &= EFUSE_SO_DATA_MASK;
+
+    e_fuse->status = (StatusType_e)status;
+}
+
+void Io_Efuse_UpdateFaults(struct Efuse *e_fuse)
+{
+    uint16_t channel_0_faults = FaultType_NoFault;
+    uint16_t channel_1_faults = FaultType_NoFault;
+
+    channel_0_faults = Io_Efuse_ReadReg(SO_FAULTR_0_ADDR, e_fuse);
+    channel_1_faults = Io_Efuse_ReadReg(SO_FAULTR_1_ADDR, e_fuse);
+
+    // Clear other bits
+    channel_0_faults &= EFUSE_SO_DATA_MASK;
+    channel_1_faults &= EFUSE_SO_DATA_MASK;
+
+    e_fuse->faults.channel_0_faults = (FaultType_e)channel_0_faults;
+    e_fuse->faults.channel_1_faults = (FaultType_e)channel_1_faults;
+}
+
+void Io_Efuse_WriteReg(
+    uint8_t        register_address,
+    uint16_t       register_value,
+    struct Efuse *e_fuse)
+{
+    HAL_StatusTypeDef status;
+    uint16_t          command    = 0x0000U;
+    uint8_t           parity_bit = 0;
+
+    // Place the register address into bits 10->13
+    command =
+        (uint16_t)((register_address & EFUSE_ADDR_MASK) << EFUSE_ADDR_SHIFT);
+    // Place register value to be written into bits 0->8
+    command = (uint16_t)(command | (register_value & EFUSE_SI_DATA_MASK));
+    // Invert watchdog bit state
+    if (e_fuse->watch_dog_state)
+    {
+        command |= WATCH_DOG_BIT;
+    }
+    else
+    {
+        command &= ~(WATCH_DOG_BIT);
+    }
+    // Invert watchdog bit state for next write
+    e_fuse->watch_dog_state = !e_fuse->watch_dog_state;
+    // Compute parity
+    parity_bit = Io_Efuse_Cal_Parity_Bit(command);
+    // Set parity value
+    command = (uint16_t)(command | ((parity_bit & 1) << 14));
+    status  = Io_Efuse_WriteToEfuse(
+        &command, e_fuse->chip_select.GPIO_Port, e_fuse->chip_select.GPIO_Pin);
+    UNUSED(status);
+}
+
+uint16_t
+    Io_Efuse_ReadReg(uint8_t register_address, struct Efuse *e_fuse)
+{
+    HAL_StatusTypeDef status;
+    uint16_t          command    = 0x0000U;
+    uint8_t           parity_bit = 0;
+    uint16_t          tx_data[]  = { 0x0000U };
+    uint16_t          rx_data[]  = { 0x0000U };
+
+    // Place the Status Register address into bits 10->13
+    command =
+        (uint16_t)((SI_STATR_0_ADDR & EFUSE_ADDR_MASK) << EFUSE_ADDR_SHIFT);
+    // Shift bit 3 of the address (SOA3: the channel number) to the 13th bit
+    command = (uint16_t)(
+        command | ((register_address & SOA3_MASK) << EFUSE_ADDR_SHIFT) |
+        (register_address & (SOA2_MASK | SOA1_MASK | SOA0_MASK)));
+    // Invert watchdog bit state
+    if (e_fuse->watch_dog_state)
+    {
+        command |= WATCH_DOG_BIT;
+    }
+    else
+    {
+        command &= ~(WATCH_DOG_BIT);
+    }
+    // Invert watchdog bit state for next write
+    e_fuse->watch_dog_state = !e_fuse->watch_dog_state;
+    // Compute parity
+    parity_bit = Io_Efuse_Cal_Parity_Bit(command);
+    // Set parity value
+    command    = (uint16_t)command | ((parity_bit & 1U) << PARITY_BIT_SHIFT);
+    tx_data[0] = command;
+    status     = Io_Efuse_ReadFromEfuse(
+        tx_data, rx_data, e_fuse->chip_select.GPIO_Port,
+        e_fuse->chip_select.GPIO_Pin);
+    UNUSED(status);
+    return rx_data[0];
+}
+
+uint8_t Io_Efuse_Cal_Parity_Bit(uint16_t spi_command)
+{
+    uint8_t parity_bit;
+    // Calculate parity for bits in spi command
+    for (parity_bit = 0; spi_command > 0; spi_command >>= 1)
+    {
+        parity_bit ^= (spi_command & 1);
+    }
+    return parity_bit;
+}
+
+HAL_StatusTypeDef Io_Efuse_WriteToEfuse(
+    uint16_t *    TxData,
+    GPIO_TypeDef *GPIO_Port,
+    uint16_t      GPIO_Pin)
+{
+    HAL_StatusTypeDef status;
+
+    HAL_GPIO_WritePin(GPIO_Port, GPIO_Pin, GPIO_PIN_RESET);
+    status = HAL_SPI_Transmit(efuse_spi_handle, (uint8_t *)TxData, 1U, 100U);
+    while (efuse_spi_handle->State != HAL_SPI_STATE_READY)
+    {
+    }
+    HAL_GPIO_WritePin(GPIO_Port, GPIO_Pin, GPIO_PIN_SET);
+
+    return status;
+}
+
+HAL_StatusTypeDef Io_Efuse_ReadFromEfuse(
+    uint16_t *    TxData,
+    uint16_t *    RxData,
+    GPIO_TypeDef *GPIO_Port,
+    uint16_t      GPIO_Pin)
+{
+    HAL_StatusTypeDef status;
+    // Send command to read from status register
+    // Data is returned on the following SPI transfer
+    HAL_GPIO_WritePin(GPIO_Port, GPIO_Pin, GPIO_PIN_RESET);
+    status = HAL_SPI_Transmit(efuse_spi_handle, (uint8_t *)TxData, 1U, 100U);
+    while (efuse_spi_handle->State != HAL_SPI_STATE_READY)
+    {
+    }
+    HAL_GPIO_WritePin(GPIO_Port, GPIO_Pin, GPIO_PIN_SET);
+    // Receive data from E-fuse
+    HAL_GPIO_WritePin(GPIO_Port, GPIO_Pin, GPIO_PIN_RESET);
+    status &= HAL_SPI_Receive(efuse_spi_handle, (uint8_t *)RxData, 1U, 100U);
+    while (efuse_spi_handle->State != HAL_SPI_STATE_READY)
+    {
+    }
+    HAL_GPIO_WritePin(GPIO_Port, GPIO_Pin, GPIO_PIN_SET);
+    return status;
+}
