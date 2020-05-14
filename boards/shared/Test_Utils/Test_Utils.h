e5d50fce120328c13f04768ff7120de87be38cac
Ming Chen Lin <thekenu@gmail.com>
2020-05-13 17:37:41 -0700

    fix formatting

--- boards/shared/Test_Utils/Test_Utils.h
+++ boards/shared/Test_Utils/Test_Utils.h
@@ -8,8 +8,7 @@ void TearDownObject(T *&object, void (*destroy_func)(T *object))
     object = NULL;
 }
 
-template <typename T>
-__inline__ T HZ_TO_MS(T hz)
+template <typename T> __inline__ T HZ_TO_MS(T hz)
 {
     return (T)1000 / hz;
 }
