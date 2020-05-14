c8c6c8c1e494726d5166582e7e7f861e2f84abce
Ming Chen Lin <thekenu@gmail.com>
2020-05-14 12:04:41 -0700

    add tests for other boards

---
+++ boards/FSM/Test/Src/Test_CanMsgs.cpp
@@ -0,0 +1,31 @@
+#include "Test_Fsm.h"
+
+extern "C"
+{
+#include "App_CanMsgs.h"
+}
+
+// FSM-10
+TEST(CanMsgsTest, state_machine_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(100), CANMSGS_FSM_STATE_MACHINE_CYCLE_TIME_MS);
+}
+
+// FSM-1
+TEST(CanMsgsTest, heartbeat_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(10), CANMSGS_FSM_HEARTBEAT_CYCLE_TIME_MS);
+}
+
+// FSM-17
+TEST(CanMsgsTest, brake_actuation_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(100), CANMSGS_FSM_BRAKE_CYCLE_TIME_MS);
+}
+
+// FSM-14
+TEST(CanMsgsTest, flow_meter_message_frequency)
+{
+    // This includes the primary and secondary flow meter
+    ASSERT_GE(HZ_TO_MS(1), CANMSGS_FSM_FLOW_METER_CYCLE_TIME_MS);
+}
