6d1971d0fc15884b7766b54c564e4621b7867cae
Ming Chen Lin <thekenu@gmail.com>
2020-05-13 17:28:20 -0700

    shared utils

--- boards/DIM/Test/Src/Test_CanMsgs.cpp
+++ boards/DIM/Test/Src/Test_CanMsgs.cpp
@@ -5,41 +5,32 @@ extern "C"
 #include "App_CanMsgs.h"
 }
 
-class CanMsgsTest : public testing::Test
-{
-  protected:
-    __inline__ uint32_t HZ_TO_MS(uint32_t hz)
-    {
-        return 1000 / hz;
-    }
-};
-
 // DIM-12
-TEST_F(CanMsgsTest, state_machine_message_frequency)
+TEST(CanMsgsTest, state_machine_message_frequency)
 {
     ASSERT_EQ(HZ_TO_MS(100), CANMSGS_DIM_STATE_MACHINE_CYCLE_TIME_MS);
 }
 
 // DIM-10
-TEST_F(CanMsgsTest, heartbeat_message_frequency)
+TEST(CanMsgsTest, heartbeat_message_frequency)
 {
     ASSERT_EQ(HZ_TO_MS(10), CANMSGS_DIM_HEARTBEAT_CYCLE_TIME_MS);
 }
 
 // DIM-3
-TEST_F(CanMsgsTest, drive_mode_switch_message_frequency)
+TEST(CanMsgsTest, drive_mode_switch_message_frequency)
 {
     ASSERT_EQ(HZ_TO_MS(100), CANMSGS_DIM_DRIVE_MODE_SWITCH_CYCLE_TIME_MS);
 }
 
 // DIM-4
-TEST_F(CanMsgsTest, start_and_traction_control_and_torque_vectoring_switches_message_frequency)
+TEST(CanMsgsTest, start_and_traction_control_and_torque_vectoring_switches_message_frequency)
 {
     ASSERT_EQ(HZ_TO_MS(100), CANMSGS_DIM_SWITCHES_CYCLE_TIME_MS);
 }
 
 // DIM-7
-TEST_F(CanMsgsTest, mapped_regen_paddle_percentage_message_frequency)
+TEST(CanMsgsTest, mapped_regen_paddle_percentage_message_frequency)
 {
     ASSERT_EQ(HZ_TO_MS(100), CANMSGS_DIM_REGEN_PADDLE_CYCLE_TIME_MS);
 }
--- boards/shared/Test_Utils/Test_Utils.h
+++ boards/shared/Test_Utils/Test_Utils.h
@@ -7,3 +7,9 @@ void TearDownObject(T *&object, void (*destroy_func)(T *object))
     destroy_func(object);
     object = NULL;
 }
+
+template <typename T>
+__inline__ T HZ_TO_MS(T hz)
+{
+    return (T)1000 / hz;
+}
