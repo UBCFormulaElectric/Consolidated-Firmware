e4eabb111a53a6b2db0ab2f8a7ef1d1506ab21b3
Ken Lin <thekenu@gmail.com>
2020-05-15 19:19:14 -0700

    Add test to check message transmit frequencies against specs.md (#612)

---
+++ boards/BMS/Test/Src/Test_CanMsgs.cpp
@@ -0,0 +1,24 @@
+#include "Test_Bms.h"
+
+extern "C"
+{
+#include "App_CanMsgs.h"
+}
+
+// BMS-31
+TEST(CanMsgsTest, state_machine_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(100), CANMSGS_BMS_STATE_MACHINE_CYCLE_TIME_MS);
+}
+
+// BMS-1
+TEST(CanMsgsTest, heartbeat_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(10), CANMSGS_BMS_HEARTBEAT_CYCLE_TIME_MS);
+}
+
+// BMS-34
+TEST(CanMsgsTest, state_of_charge_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(100), CANMSGS_BMS_STATE_OF_CHARGE_CYCLE_TIME_MS);
+}
---
+++ boards/DCM/Test/Src/Test_CanMsgs.cpp
@@ -0,0 +1,24 @@
+#include "Test_Dcm.h"
+
+extern "C"
+{
+#include "App_CanMsgs.h"
+}
+
+// DCM-21
+TEST(CanMsgsTest, state_machine_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(100), CANMSGS_DCM_STATE_MACHINE_CYCLE_TIME_MS);
+}
+
+// DCM-2
+TEST(CanMsgsTest, heartbeat_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(10), CANMSGS_DCM_HEARTBEAT_CYCLE_TIME_MS);
+}
+
+// DCM-19
+TEST(CanMsgsTest, torque_request_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(100), CANMSGS_DCM_TORQUE_REQUEST_CYCLE_TIME_MS);
+}
