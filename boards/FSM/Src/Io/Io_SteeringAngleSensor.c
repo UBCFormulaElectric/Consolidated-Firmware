2e14ecc7f3340216975bf44dd97b4e76fdee8ba2
Pasit Laothamatas <morp.laothamatas@gmail.com>
2020-05-24 22:55:40 -0700

    wip

--- boards/FSM/Src/Io/Io_SteeringAngleSensor.c
+++ boards/FSM/Src/Io/Io_SteeringAngleSensor.c
@@ -3,15 +3,10 @@
 
 float Io_SteeringAngleSensor_GetAngleDegree(void)
 {
-    // Subtract the measured voltage by the steering angle sensor voltage
-    // offset. Multiply the result with VOLTAGE_TO_DEGREES to calculate the
-    // steering angle in degrees.
-    //
-    // The scale factor used to convert the voltage to an angle is given by 360
-    // deg / VDDA (3.3V)
+    // Steering angle = (ADC Voltage - Voltage Offset) * Degree Per Volt
 
     const float STEERING_ANGLE_VOLTAGE_OFFSET = 1.9f;
-    const float VOLTAGE_TO_DEGREES            = 360.0f / 3.3f;
-    return VOLTAGE_TO_DEGREES *
+    const float DEGREE_PER_VOLT               = 360.0f / 3.3f;
+    return DEGREE_PER_VOLT *
            (Io_Adc_GetChannel1Voltage() - STEERING_ANGLE_VOLTAGE_OFFSET);
 }
