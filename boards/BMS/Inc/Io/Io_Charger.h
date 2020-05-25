eb1bc6891f369b54ca5a1d00ef127554cee223ef
Ming Chen Lin <thekenu@gmail.com>
2020-05-25 01:55:05 -0700

    fix io jdocs

--- boards/BMS/Inc/Io/Io_Charger.h
+++ boards/BMS/Inc/Io/Io_Charger.h
@@ -3,17 +3,17 @@
 #include <stdbool.h>
 
 /**
- * Enable the charger
+ * Enable the PON pin of the BRUSA NLG513 charger
  */
 void Io_Charger_Enable(void);
 
 /**
- * Disable the charger
+ * Disable the PON pin of the BRUSA NLG513 charger
  */
 void Io_Charger_Disable(void);
 
 /**
- * Check if the charger is connected
- * @return true if the charger is connected, else false
+ * Check if the BRUSA NLG513 charger is connected
+ * @return true if the BRUSA NLG513 charger is connected, else false
  */
 bool Io_Charger_IsConnected(void);
