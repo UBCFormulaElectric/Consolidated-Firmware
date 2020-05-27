36558d90abd696b247e088dd43f0e7b53ed52b50
thekenu <thekenu@gmail.com>
2020-05-27 05:33:38 -0700

    Make AIRs message periodic

--- boards/BMS/Test/Src/Test_CanMsgs.cpp
+++ boards/BMS/Test/Src/Test_CanMsgs.cpp
@@ -34,3 +34,9 @@ TEST(CanMsgsTest, ok_status_message_frequency)
 {
     ASSERT_GE(HZ_TO_MS(100), CANMSGS_BMS_OK_STATUSES_CYCLE_TIME_MS);
 }
+
+// BMS-38
+TEST(CanMsgsTest, airs_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(100), CANMSGS_BMS_AIRS_CYCLE_TIME_MS);
+}
