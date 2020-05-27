2255384e43cf6985f45c12e1dac72ee25f0a5c5a
thekenu <thekenu@gmail.com>
2020-05-27 05:53:04 -0700

    Fix test

--- boards/BMS/Test/Src/Test_CanMsgs.cpp
+++ boards/BMS/Test/Src/Test_CanMsgs.cpp
@@ -36,7 +36,7 @@ TEST(CanMsgsTest, ok_status_message_frequency)
 }
 
 // BMS-38
-TEST(CanMsgsTest, airs_message_frequency)
+TEST(CanMsgsTest, air_states_message_frequency)
 {
     ASSERT_GE(HZ_TO_MS(100), CANMSGS_BMS_AIR_STATES_CYCLE_TIME_MS);
 }
