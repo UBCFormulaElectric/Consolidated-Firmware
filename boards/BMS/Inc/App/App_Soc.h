c3fd51e8566b6ea07d52171ee47e364e0252b152
thekenu <thekenu@gmail.com>
2020-05-28 05:00:37 -0700

    WIP

--- boards/BMS/Inc/App/App_Soc.h
+++ boards/BMS/Inc/App/App_Soc.h
@@ -3,23 +3,43 @@
 #include "App_SharedExitCode.h"
 
 /**
- * Given three state-of-charge (SoCs), check whether the difference between any
- * two SoCs is less than or equal to the specified threshold. If any two SoCs
- * pass the check, return the average value of those two SoCs.
- * @param max_difference The maximum tolerable difference between any two given
- *                       state-of-charge values
- * @param soc_1 The first SoC
- * @param soc_2 The second SoC
- * @param soc_3 The third SoC
- * @param result The average value of two SoCs whose difference is less than or
- *               equal to max_value, else NaN. In the event of a tiebreak (i.e.
- *               All three SoCs pass the check), th first two SoCs are used to
- *               calculate the average value.
- * @return EXIT_CODE_INVALID_ARGS if the max difference or any of the SoCs is
- *                                less than 0 or greater than 100
+ * Given three state-of-charge (SoCs), check whether the absolute difference
+ * between any two SoCs is less than or equal to the specified maximum absolute
+ * difference. If any two SoCs pass the check, return the average value of those
+ * two SoCs.
+ * @param max_abs_difference The maximum allowable absolute difference between
+ *                           any two given SoCs, must be between 0 and 100
+ *                           inclusive
+ * @param soc_1 The first SoC, must be between 0 and 100 inclusive
+ * @param soc_2 The second SoC, must be between 0 and 100 inclusive
+ * @param soc_3 The third SoC, must be between 0 and 100 inclusive
+ * @param result This will be set to the average value of the two SoCs whose
+ *               difference is less than or equal to max_difference. Multiple
+ *               SoC pairs may have their difference less than or equal to
+ *               max_difference. In this scenario, we prioritize as follows:
+ *
+ *               Highest Priority: soc_1 / soc_2
+ *                                 soc_2 / soc_3
+ *               Lowest Priority:  soc_1 / soc_3
+ *
+ *               If no two suitable SoCs can be found, this will be set to NaN.
+ * @note An example of a tiebreaker:
+ *
+ *         max_difference = 5
+ *         soc_1 = 0
+ *         soc_2 = 50
+ *         soc_3 = 100
+ *
+ *       In this scenario, the difference between soc_1 and soc_2 is the same as
+ *       the difference between soc_2 and soc_3, but we prioritize soc_1 / soc_2
+ *       ovr soc_2 / soc_3. Thus, the average value to calculate,
+ *
+ *       (soc_1 + soc_2) / 2 = 25
+ * @return EXIT_CODE_INVALID_ARGS if max_difference, soc_1, soc_2, or soc_3 is
+ *                                not between 0 and 100 inclusive
  */
 ExitCode App_Soc_Vote(
-    float  max_difference,
+    float  max_abs_difference,
     float  soc_1,
     float  soc_2,
     float  soc_3,
--- boards/BMS/Src/App/App_Soc.c
+++ boards/BMS/Src/App/App_Soc.c
@@ -2,42 +2,37 @@
 #include "App_Soc.h"
 
 ExitCode App_Soc_Vote(
-    float  max_difference,
+    float  max_abs_difference,
     float  soc_1,
     float  soc_2,
     float  soc_3,
     float *result)
 {
-    if (max_difference < 0.0f || max_difference > 100.0f)
+    if (max_abs_difference < 0.0f || max_abs_difference > 100.0f ||
+        soc_1 < 0.0f || soc_1 > 100.0f || soc_2 < 0.0f || soc_2 > 100.0f ||
+        soc_3 < 0.0f || soc_3 > 100.0f)
+    {
         return EXIT_CODE_INVALID_ARGS;
-
-    if (soc_1 < 0.0f || soc_1 > 100.0f)
-        return EXIT_CODE_INVALID_ARGS;
-
-    if (soc_2 < 0.0f || soc_2 > 100.0f)
-        return EXIT_CODE_INVALID_ARGS;
-
-    if (soc_3 < 0.0f || soc_3 > 100.0f)
-        return EXIT_CODE_INVALID_ARGS;
+    }
 
     float diff = 0.0f;
 
     diff = fabsf(soc_1 - soc_2);
-    if (diff <= max_difference)
+    if (diff <= max_abs_difference)
     {
         *result = (soc_1 + soc_2) / 2.0f;
         return EXIT_CODE_OK;
     }
 
     diff = fabsf(soc_2 - soc_3);
-    if (diff <= max_difference)
+    if (diff <= max_abs_difference)
     {
         *result = (soc_2 + soc_3) / 2.0f;
         return EXIT_CODE_OK;
     }
 
     diff = fabsf(soc_1 - soc_3);
-    if (diff <= max_difference)
+    if (diff <= max_abs_difference)
     {
         *result = (soc_1 + soc_3) / 2.0f;
         return EXIT_CODE_OK;
