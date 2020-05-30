75075fb45272432a2a3d2f08a7bc2d38e72493b8
Pasit Laothamatas <morp.laothamatas@gmail.com>
2020-05-30 14:31:05 -0700

    Add brake pressure sensor id

--- boards/FSM/Inc/Io/Io_BrakePressureSensor.h
+++ boards/FSM/Inc/Io/Io_BrakePressureSensor.h
@@ -1,7 +1,7 @@
 #pragma once
 
 /**
- * Get the pressure in psi from the brake pressure sensor
+ * Get the pressure in psi from the MSP-300-2K5-P-3-N-1 brake pressure sensor
  * @return The pressure of the brake pressure sensor in psi
  */
 float Io_BrakePressureSensor_GetPressurePsi(void);
