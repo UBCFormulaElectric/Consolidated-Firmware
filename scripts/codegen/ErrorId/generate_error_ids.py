d692a4e987300b32cca1ee047885473f129e8538
Ming Chen Lin <thekenu@gmail.com>
2020-06-05 18:53:05 -0700

    simplify codegen

--- scripts/codegen/ErrorId/generate_error_ids.py
+++ scripts/codegen/ErrorId/generate_error_ids.py
@@ -6,6 +6,8 @@ from scripts.utilities.supported_boards import get_board_names
 
 
 ERRORID_ENUM_TEMPLATE = '''\
+#pragma once
+
 enum ErrorId
 {{
     // BMS non-critical errors
@@ -42,12 +44,6 @@ enum ErrorId
 }};
 '''
 
-def format_errors(errors):
-    """
-    Add indentation and trailing comma to each error in the given list of errors
-    """
-    return ['    {},'.format(error) for error in errors]
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--dbc', help='Path to the DBC file')
@@ -56,41 +52,38 @@ if __name__ == "__main__":
 
     database = cantools.database.load_file(args.dbc, database_format='dbc')
 
-    critical_errors = {}
-    non_critical_errors = {}
+    enum_members = {
+        'critical': {},
+        'non_critical': {}
+    }
 
     # Find non-critical and critical error CAN messages for each board
     for board in get_board_names():
         try:
             can_msg = database.get_message_by_name(board + '_CRITICAL_ERRORS')
-            signals = []
-            for signal in can_msg.signals:
-                signals.append(board + '_CRITICAL_' + signal.name.upper())
-            critical_errors[board] = signals
-
+            enum_members['critical'][board] = \
+                ['    %s_CRITICAL_%s,' %(board, signal.name.upper()) for signal in can_msg.signals]
         except KeyError:
-            raise KeyError('Could not find critical error message for {}'.format(board))
+            raise KeyError('Could not find critical error message for %s' % board)
 
         try:
             can_msg = database.get_message_by_name(board + '_NON_CRITICAL_ERRORS')
-            signals = []
-            for signal in can_msg.signals:
-                signals.append(board + '_NON_CRITICAL_' + signal.name.upper())
-            non_critical_errors[board] = signals
+            enum_members['non_critical'][board] = \
+                ['    %s_NON_CRITICAL_%s,' %(board, signal.name.upper()) for signal in can_msg.signals]
         except KeyError:
-            raise KeyError('Could not find critical error message for {}'.format(board))
+            raise KeyError('Could not find critical error message for %s' % board)
 
     enum = ERRORID_ENUM_TEMPLATE.format(
-        bms_non_critical_errors = '\n'.join(format_errors(non_critical_errors['BMS'])),
-        dcm_non_critical_errors = '\n'.join(format_errors(non_critical_errors['DCM'])),
-        dim_non_critical_errors = '\n'.join(format_errors(non_critical_errors['DIM'])),
-        fsm_non_critical_errors = '\n'.join(format_errors(non_critical_errors['FSM'])),
-        pdm_non_critical_errors = '\n'.join(format_errors(non_critical_errors['PDM'])),
-        bms_critical_errors  = '\n'.join(format_errors(critical_errors['BMS'])),
-        dcm_critical_errors  = '\n'.join(format_errors(critical_errors['DCM'])),
-        dim_critical_errors  = '\n'.join(format_errors(critical_errors['DIM'])),
-        fsm_critical_errors  = '\n'.join(format_errors(critical_errors['FSM'])),
-        pdm_critical_errors  = '\n'.join(format_errors(critical_errors['PDM'])))
+        bms_non_critical_errors = '\n'.join(enum_members['non_critical']['BMS']),
+        dcm_non_critical_errors = '\n'.join(enum_members['non_critical']['DCM']),
+        dim_non_critical_errors = '\n'.join(enum_members['non_critical']['DIM']),
+        fsm_non_critical_errors = '\n'.join(enum_members['non_critical']['FSM']),
+        pdm_non_critical_errors = '\n'.join(enum_members['non_critical']['PDM']),
+        bms_critical_errors  = '\n'.join(enum_members['critical']['BMS']),
+        dcm_critical_errors  = '\n'.join(enum_members['critical']['DCM']),
+        dim_critical_errors  = '\n'.join(enum_members['critical']['DIM']),
+        fsm_critical_errors  = '\n'.join(enum_members['critical']['FSM']),
+        pdm_critical_errors  = '\n'.join(enum_members['critical']['PDM']))
 
     # Generate output folder if it doesn't exist yet
     output_dir = os.path.dirname(args.output_path)
