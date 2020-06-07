c41dac292b9eb14fa6016439f0117eceb38a3d96
Pasit Laothamatas <32230980+pasitlaothamatas@users.noreply.github.com>
2020-06-07 14:48:46 -0700

    Update boards/FSM/Inc/Io/Io_MSP3002K5P3N1.h
    Co-authored-by: Ken Lin <thekenu@gmail.com>

--- boards/FSM/Inc/Io/Io_MSP3002K5P3N1.h
+++ boards/FSM/Inc/Io/Io_MSP3002K5P3N1.h
@@ -5,7 +5,7 @@
 /**
  * Check if the pressure from MSP-300-2K5-P-3-N-1 exceeds the brake pressure
  * threshold. If the pressure exceeds threshold, the brake is said to be
- * actuated
+ * actuated.
  * @return True if the brake is actuated, false if the brake is not actuated
  */
 bool Io_MSP3002K5P3N1_IsBrakeActuated(void);
