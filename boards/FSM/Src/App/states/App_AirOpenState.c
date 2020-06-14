547915de46d57c214c8a32d9f0ea15fbda2aa4fd
Pasit Laothamatas <morp.laothamatas@gmail.com>
2020-06-13 22:14:48 -0700

    wip

--- boards/FSM/Src/App/states/App_AirOpenState.c
+++ boards/FSM/Src/App/states/App_AirOpenState.c
@@ -25,9 +25,6 @@ static void AirOpenStateRunOnTick100Hz(struct StateMachine *const state_machine)
     App_SetPeriodicSignals_FlowRateInRangeChecks(world);
     App_SetPeriodicSignals_WheelSpeedInRangeChecks(world);
     App_SetPeriodicSignals_SteeringAngleInRangeCheck(world);
-    // App_SetPeriodicSignals_BrakePressureInRangeCheck(world);
-    // App_SetPeriodicSignals_BrakeActuationStatus(world);
-    // App_SetPeriodicSignals_BrakeSensorOpenOrShortCircuitStatus(world);
     App_SetPeriodicSignals_Brake(world);
 
     if (App_CanRx_BMS_AIR_STATES_GetSignal_AIR_POSITIVE(can_rx) ==
