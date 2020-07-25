f8b1c4a2cb5991f0bdbba8522d4008c5b983fdd8
marko <marko.markusi@hotmail.com>
2020-07-24 20:50:53 -0700

    Update boards/PDM/Src/Io/Io_Efuse.c
    Co-authored-by: Pasit Laothamatas <32230980+pasitlaothamatas@users.noreply.github.com>

--- boards/PDM/Src/Io/Io_Efuse.c
+++ boards/PDM/Src/Io/Io_Efuse.c
@@ -851,7 +851,7 @@ static ExitCode Io_Efuse_ReadRegister(
 
     tx_data[0] = command;
     exit_code  = Io_Efuse_ReadFromEfuse(
-        tx_data, register_value, chip_select_port, chip_select_pin);
+        &command, register_value, chip_select_port, chip_select_pin);
     // Only return register contents and clear bits 9->15
     CLEAR_BIT(*register_value, ~EFUSE_SO_DATA_MASK);
 
