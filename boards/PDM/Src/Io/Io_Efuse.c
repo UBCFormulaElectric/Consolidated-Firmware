b94357ab4bd7e850972d794dad2509f43d2387e8
marko <marko.markusi@hotmail.com>
2020-07-26 17:45:30 -0700

    Update boards/PDM/Src/Io/Io_Efuse.c
    Co-authored-by: Pasit Laothamatas <32230980+pasitlaothamatas@users.noreply.github.com>

--- boards/PDM/Src/Io/Io_Efuse.c
+++ boards/PDM/Src/Io/Io_Efuse.c
@@ -824,6 +824,7 @@ static ExitCode Io_Efuse_ReadRegister(
     // Place the Status Register address into bits 10->13
     command =
         (uint16_t)((SI_STATR_0_ADDR & EFUSE_ADDR_MASK) << EFUSE_ADDR_SHIFT);
+        
     // Shift bit 3 of the address (SOA3: the channel number) to the 13th bit
     command = (uint16_t)(
         command | ((register_address & SOA3_MASK) << EFUSE_ADDR_SHIFT) |
