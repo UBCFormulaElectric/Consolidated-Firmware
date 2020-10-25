5d6f697cdd806e07d9ee55f68a22c4e919dfd634
Pasit Laothamatas <morp.laothamatas@gmail.com>
2020-10-24 17:39:35 -0700

    wip

--- boards/BMS/Src/App/states/App_ChargeState.c
+++ boards/BMS/Src/App/states/App_ChargeState.c
@@ -23,13 +23,13 @@ static void ChargeStateRunOnTick1Hz(struct StateMachine *const state_machine)
 
     App_SetPeriodicSignals_CellMonitorInRangeChecks(can_tx, cell_monitor);
 
-    if (App_CanTx_GetPeriodicSignal_MAX_CELL_VOLTAGE_OUT_OF_RANGE(can_tx) !=
-            CANMSGS_BMS_AIR_SHUTDOWN_ERRORS_MAX_CELL_VOLTAGE_OUT_OF_RANGE_OK_CHOICE ||
-        App_CanTx_GetPeriodicSignal_MIN_CELL_VOLTAGE_OUT_OF_RANGE(can_tx) !=
-            CANMSGS_BMS_AIR_SHUTDOWN_ERRORS_MIN_CELL_VOLTAGE_OUT_OF_RANGE_OK_CHOICE)
-    {
-        App_SharedStateMachine_SetNextState(state_machine, App_GetFaultState());
-    }
+    //if (App_CanTx_GetPeriodicSignal_MAX_CELL_VOLTAGE_OUT_OF_RANGE(can_tx) !=
+    //        CANMSGS_BMS_AIR_SHUTDOWN_ERRORS_MAX_CELL_VOLTAGE_OUT_OF_RANGE_OK_CHOICE ||
+    //    App_CanTx_GetPeriodicSignal_MIN_CELL_VOLTAGE_OUT_OF_RANGE(can_tx) !=
+    //        CANMSGS_BMS_AIR_SHUTDOWN_ERRORS_MIN_CELL_VOLTAGE_OUT_OF_RANGE_OK_CHOICE)
+    //{
+    //    App_SharedStateMachine_SetNextState(state_machine, App_GetFaultState());
+    //}
 }
 
 static void ChargeStateRunOnTick100Hz(struct StateMachine *const state_machine)
