6e9af56b9e1a2c1d90aa6b6a3fd391642c36b056
alxshev <ashevchuk2001@gmail.com>
2020-11-06 10:27:28 -0800

    pre STM32CubeMX modifications

---
+++ boards/DCM/Inc/Io/Io_IsrHalCallback.h
@@ -0,0 +1,7 @@
+#include "main.h"
+
+/**
+ * HAL callback, called when MCU detects interrupt
+ * @param GPIO_Pin the pin which detected the interrupt
+ */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin);
---
+++ boards/DCM/Src/Io/Io_IsrHalCallback.c
@@ -0,0 +1,14 @@
+#include <stm32f3xx_hal.h>
+#include "main.h"
+#include "Io_LSM6DS33.h"
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+    switch (GPIO_Pin)
+    {
+        case IMU_PIN_2_Pin:
+            Io_LSM6DS33_UpdateSensorData();
+            break;
+        default:
+    }
+}
