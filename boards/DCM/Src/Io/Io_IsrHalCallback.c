bdf9685429aa27ef2071aa1b1bae0536aee9a510
alxshev <ashevchuk2001@gmail.com>
2020-11-14 05:36:43 -0800

    bug fix

--- boards/DCM/Src/Io/Io_IsrHalCallback.c
+++ boards/DCM/Src/Io/Io_IsrHalCallback.c
@@ -6,8 +6,10 @@ void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
     switch (GPIO_Pin)
     {
         case IMU_PIN_2_Pin:
-            Io_LSM6DS33_ConfigureImu();
+            Io_LSM6DS33_ExtiCallback();
             break;
         default:
+            break;
     }
+
 }
