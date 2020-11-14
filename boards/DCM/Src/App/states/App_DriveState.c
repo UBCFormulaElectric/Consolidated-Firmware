3d736b97870d9897f75692a7f09156d2cdd8286b
Alex Shevchuk <ashevchuk2001@gmail.com>
2020-11-14 12:09:34 -0800

    Update boards/DCM/Src/App/states/App_DriveState.c
    Co-authored-by: Pasit Laothamatas <32230980+pasitlaothamatas@users.noreply.github.com>

--- boards/DCM/Src/App/states/App_DriveState.c
+++ boards/DCM/Src/App/states/App_DriveState.c
@@ -35,6 +35,7 @@ static void DriveStateRunOnTick100Hz(struct StateMachine *const state_machine)
         App_SharedStateMachine_SetNextState(state_machine, App_GetInitState());
     }
 
+
     App_SetPeriodicCanSignals_Imu(world);
     App_SetPeriodicCanSignals_TorqueRequests(world);
 }
