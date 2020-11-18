6f16c07fb6d542e5d22ed41f2bd3094578adccc5
Pasit Laothamatas <morp.laothamatas@gmail.com>
2020-11-17 23:53:49 -0800

    Add app and io code, and tests for AIRs

---
+++ boards/BMS/Src/Io/Io_Airs.c
@@ -0,0 +1,29 @@
+#include "Io_Airs.h"
+#include "main.h"
+
+// Since there are no pins to determine the AIRs' statuses directly, the AIRs'
+// statuses can be determined by the LUT below:
+//
+// +------------------+------------------+-------------------+
+// | AIR POWER STATUS | AIR POWER ISENSE |   AIRs statuses   |
+// +------------------+------------------+-------------------+
+// |                0 |                0 | AIR- ON, AIR+ OFF |
+// |                0 |                1 | NOT POSSIBLE      |
+// |                1 |                0 | AIR- ON, AIR+ OFF |
+// |                1 |                1 | AIR- ON, AIR+ ON  |
+// +------------------+------------------+-------------------+
+
+bool Io_Airs_IsAirPositiveOn(void)
+{
+    return HAL_GPIO_ReadPin(AIR_TOTAL_ISENSE_GPIO_Port, AIR_TOTAL_ISENSE_Pin) &&
+           HAL_GPIO_ReadPin(AIR_POWER_STATUS_GPIO_Port, AIR_POWER_STATUS_Pin);
+}
+
+bool Io_Airs_IsAirNegativeOn(void)
+{
+    return Io_Airs_IsAirPositiveOn() ||
+           (HAL_GPIO_ReadPin(
+                AIR_POWER_STATUS_GPIO_Port, AIR_POWER_STATUS_Pin) &&
+            !HAL_GPIO_ReadPin(
+                AIR_TOTAL_ISENSE_GPIO_Port, AIR_TOTAL_ISENSE_Pin));
+}
