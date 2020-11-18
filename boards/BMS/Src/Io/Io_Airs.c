9037b2539bd04910b9f9911356deed1b07f28700
Pasit Laothamatas <morp.laothamatas@gmail.com>
2020-11-18 14:02:58 -0800

    fix lut

--- boards/BMS/Src/Io/Io_Airs.c
+++ boards/BMS/Src/Io/Io_Airs.c
@@ -4,14 +4,14 @@
 // Since there are no pins to determine the AIRs' statuses directly, the AIRs'
 // statuses can be determined by the LUT below:
 //
-// +------------------+------------------+-------------------+
-// | AIR POWER STATUS | AIR POWER ISENSE |   AIRs statuses   |
-// +------------------+------------------+-------------------+
-// |                0 |                0 | AIR- ON, AIR+ OFF |
-// |                0 |                1 | NOT POSSIBLE      |
-// |                1 |                0 | AIR- ON, AIR+ OFF |
-// |                1 |                1 | AIR- ON, AIR+ ON  |
-// +------------------+------------------+-------------------+
+// +------------------+------------------+--------------------+
+// | AIR POWER STATUS | AIR POWER ISENSE |   AIRs statuses    |
+// +------------------+------------------+--------------------+
+// |                0 |                0 | AIR- OFF, AIR+ OFF |
+// |                0 |                1 | NOT POSSIBLE       |
+// |                1 |                0 | AIR- ON, AIR+ OFF  |
+// |                1 |                1 | AIR- ON, AIR+ ON   |
+// +------------------+------------------+--------------------+
 
 bool Io_Airs_IsAirPositiveOn(void)
 {
