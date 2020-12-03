556dc15a1f6e0bb1e2cd53036cce75a6158c0552
Pasit Laothamatas <morp.laothamatas@gmail.com>
2020-12-02 23:38:51 -0800

    Remove exit code and null checking

--- boards/BMS/Src/Io/Io_VoltageSense.c
+++ boards/BMS/Src/Io/Io_VoltageSense.c
@@ -1,24 +1,21 @@
-#include <stddef.h>
+#include <math.h>
 #include "Io_VoltageSense.h"
 
-ExitCode Io_VoltageSense_GetTractiveSystemVoltage(
-    float  adc_voltage,
-    float *tractive_system_voltage)
+float Io_VoltageSense_GetTractiveSystemVoltage(float adc_voltage)
 {
     if (adc_voltage < 0.0f)
-        return EXIT_CODE_OUT_OF_RANGE;
-
-    if (tractive_system_voltage == NULL)
-        return EXIT_CODE_INVALID_ARGS;
+    {
+        return NAN;
+    }
 
     // The tractive system voltage is divided down by several resistors, then
     // fed through an amplifier.
     //
     // TS Voltage
     //   -+-
-    //    |                                     +----------+  +-------------+
-    //    +--<499k>--<499k>--<499k>--<499k>--+--| Amplifier|--| ADC Channel |
-    //                                       |  +----------+  +-------------+
+    //    |                                     +-----------+  +-------------+
+    //    +--<499k>--<499k>--<499k>--<499k>--+--| Amplifier |--| ADC Channel |
+    //                                       |  +-----------+  +-------------+
     //                                       |
     //                                    <1.24k>
     //                                       |
@@ -43,7 +40,5 @@ ExitCode Io_VoltageSense_GetTractiveSystemVoltage(
         1.024e+3f / (499e+3f + 499e+3f + 499e+3f + 499e+3f + 1.024e+3f);
     const float amplifier_gain = 8.0f;
 
-    *tractive_system_voltage = adc_voltage / (voltage_ratio * amplifier_gain);
-
-    return EXIT_CODE_OK;
+    return adc_voltage / (voltage_ratio * amplifier_gain);
 }
--- boards/BMS/Test/Src/Test_VoltageSense.cpp
+++ boards/BMS/Test/Src/Test_VoltageSense.cpp
@@ -11,29 +11,16 @@ TEST(VoltageSenseTest, tractive_system_voltage_calculation)
     // Negative ADC voltage
     float adc_voltage =
         std::nextafter(0.0f, std::numeric_limits<float>::lowest());
-    float tractive_system_voltage = 0.0f;
-    ASSERT_EQ(
-        EXIT_CODE_OUT_OF_RANGE, Io_VoltageSense_GetTractiveSystemVoltage(
-                                    adc_voltage, &tractive_system_voltage));
+    ASSERT_TRUE(isnanf(Io_VoltageSense_GetTractiveSystemVoltage(adc_voltage)));
 
     // Zero tractive system voltage (0V)
     adc_voltage = 0.0f;
     ASSERT_EQ(
-        EXIT_CODE_OK, Io_VoltageSense_GetTractiveSystemVoltage(
-                          adc_voltage, &tractive_system_voltage));
-    ASSERT_EQ(0.0f, tractive_system_voltage);
+        adc_voltage, Io_VoltageSense_GetTractiveSystemVoltage(adc_voltage));
 
     // Nominal tractive system voltage (400V)
     adc_voltage =
         400.0f * 8.0f *
         (1.024e+3f / (1.024e+3f + 499e+3f + 499e+3f + 499e+3f + 499e+3f));
-    ASSERT_EQ(
-        EXIT_CODE_OK, Io_VoltageSense_GetTractiveSystemVoltage(
-                          adc_voltage, &tractive_system_voltage));
-    ASSERT_EQ(400.0f, tractive_system_voltage);
-
-    // Null pointer
-    ASSERT_EQ(
-        EXIT_CODE_INVALID_ARGS,
-        Io_VoltageSense_GetTractiveSystemVoltage(adc_voltage, NULL));
+    ASSERT_EQ(400.0f, Io_VoltageSense_GetTractiveSystemVoltage(adc_voltage));
 }
