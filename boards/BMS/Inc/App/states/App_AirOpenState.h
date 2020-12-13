e48e78cac5b90510487234c0447acbe94254f924
Pasit Laothamatas <morp.laothamatas@gmail.com>
2020-12-12 16:35:10 -0800

    fix javadoc

--- boards/BMS/Inc/App/states/App_AirOpenState.h
+++ boards/BMS/Inc/App/states/App_AirOpenState.h
@@ -3,7 +3,7 @@
 #include "App_SharedStateMachine.h"
 
 /**
- * Get a pointer to the Air Open State.
- * @return A pointer to the Air Open State. THIS SHOULD NOT BE MODIFIED.
+ * Get a pointer to the AIR Open State.
+ * @return A pointer to the AIR Open State. THIS SHOULD NOT BE MODIFIED.
  */
 const struct State *App_GetAirOpenState(void);
