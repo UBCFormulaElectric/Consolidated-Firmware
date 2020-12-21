42502ed9d70e68cd8cf4d5a5ddc32bcad94d4c10
Pasit Laothamatas <morp.laothamatas@gmail.com>
2020-12-20 21:02:07 -0800

    Add state transition logic and tests

--- boards/BMS/Src/App/states/App_AirOpenState.c
+++ boards/BMS/Src/App/states/App_AirOpenState.c
@@ -1,4 +1,5 @@
 #include "states/App_AllStates.h"
+#include "states/App_PreChargeState.h"
 
 #include "App_SharedMacros.h"
 
@@ -18,6 +19,16 @@ static void AirOpenStateRunOnTick1Hz(struct StateMachine *const state_machine)
 static void AirOpenStateRunOnTick100Hz(struct StateMachine *const state_machine)
 {
     App_AllStatesRunOnTick100Hz(state_machine);
+
+    struct BmsWorld *world = App_SharedStateMachine_GetWorld(state_machine);
+    struct Airs *    airs  = App_BmsWorld_GetAirs(world);
+
+    // Begin the precharge sequence if AIR- is closed
+    if (App_SharedBinaryStatus_IsActive(App_Airs_GetAirNegative(airs)))
+    {
+        App_SharedStateMachine_SetNextState(
+            state_machine, App_GetPreChargeState());
+    }
 }
 
 static void AirOpenStateRunOnExit(struct StateMachine *const state_machine)
