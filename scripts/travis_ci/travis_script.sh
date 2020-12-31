ff903f682293be20870fcbf7bad2a4781e4fe228
Pasit Laothamatas <32230980+pasitlaothamatas@users.noreply.github.com>
2020-12-31 03:09:12 -0800

    Update travis_script.sh

--- scripts/travis_ci/travis_script.sh
+++ scripts/travis_ci/travis_script.sh
@@ -31,8 +31,8 @@ if [ "$RUN_ARM_BUILD" = "true" ]; then
         #    SHA=${TRAVIS_PULL_REQUEST_SHA}
         #fi
         #SHA=${GITHUB_REF}
-        #SHA=${TRAVIS_COMMIT}
-        SHA=${TRAVIS_PULL_REQUEST_SHA}
+        SHA=${TRAVIS_COMMIT}
+        #SHA=${TRAVIS_PULL_REQUEST_SHA}
         # For now, the maximum RAM and FLASH size are the same for every board.
         MAX_RAM_SIZE=40960 
         MAX_FLASH_SIZE=262144
