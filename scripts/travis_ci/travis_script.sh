c818f40c45c4b95d6a9ee0b2b7f53882be167302
Pasit Laothamatas <32230980+pasitlaothamatas@users.noreply.github.com>
2021-01-01 08:16:28 +0100

    Update travis_script.sh

--- scripts/travis_ci/travis_script.sh
+++ scripts/travis_ci/travis_script.sh
@@ -30,7 +30,7 @@ if [ "$RUN_ARM_BUILD" = "true" ]; then
             # Travis CI Pull Request build
             SHA=${TRAVIS_PULL_REQUEST_SHA}
         fi
-        #SHA=${GITHUB_SHA}
+        SHA=${GITHUB_SHA}
         #SHA=${TRAVIS_COMMIT}
         #SHA=${TRAVIS_PULL_REQUEST_SHA}
         # For now, the maximum RAM and FLASH size are the same for every board.
