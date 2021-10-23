26b25b50f1269d4e71e7f383568bd44ac06218a5
Alex Krantz <alex@krantz.dev>
2021-10-23 15:27:43 -0700

    Add order-independent checks

--- boards/DCM/Src/App/states/App_InitState.c
+++ boards/DCM/Src/App/states/App_InitState.c
@@ -23,12 +23,19 @@ static void InitStateRunOnTick100Hz(struct StateMachine *const state_machine)
     App_AllStatesRunOnTick100Hz(state_machine);
 
     struct DcmWorld *world = App_SharedStateMachine_GetWorld(state_machine);
+    struct DcmCanRxInterface *can_rx_interface = App_DcmWorld_GetCanRx(world);
     struct DcmCanTxInterface *can_tx_interface = App_DcmWorld_GetCanTx(world);
+    struct ErrorTable *error_table = App_DcmWorld_GetErrorTable(world);
 
     App_CanTx_SetPeriodicSignal_TORQUE_REQUEST(can_tx_interface, 0.0f);
 
-    // No need for any safety checks, just run! (this is a demo)
-    App_SharedStateMachine_SetNextState(state_machine, App_GetDriveState());
+    bool any_critical_errors = App_SharedErrorTable_HasAnyCriticalErrorSet(error_table);
+    bool bms_positive_air_closed = App_CanRx_BMS_AIR_STATES_GetSignal_AIR_POSITIVE(can_rx_interface) == 1;
+    bool bms_negative_air_closed = App_CanRx_BMS_AIR_STATES_GetSignal_AIR_NEGATIVE(can_rx_interface) == 1;
+
+    if (!any_critical_errors && bms_positive_air_closed && bms_negative_air_closed) {
+        App_SharedStateMachine_SetNextState(state_machine, App_GetDriveState());
+    }
 }
 
 static void InitStateRunOnExit(struct StateMachine *const state_machine)
