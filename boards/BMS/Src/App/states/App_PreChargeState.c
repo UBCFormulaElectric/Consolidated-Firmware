a3f9c1d96244acaa7d011117381b88ace9441681
ashevc01 <ashevchuk2001@gmail.com>
2021-10-30 12:37:40 -0700

    Cleaned up precharge state logic and added todos

--- boards/BMS/Src/App/states/App_PreChargeState.c
+++ boards/BMS/Src/App/states/App_PreChargeState.c
@@ -33,78 +33,96 @@ static void PreChargeStateRunOnTick1Hz(struct StateMachine *const state_machine)
     App_AllStatesRunOnTick1Hz(state_machine);
 }
 
+static float _getPrechargeVoltageThreshold(const float max_pack_voltage) {
+    // todo: maybe convert to constant?
+    return max_pack_voltage * 0.98f;
+}
+
+static const struct State *_prechargeInDurationRunOnTick100Hz(struct BmsWorld *world) {
+    struct BmsCanTxInterface *can_tx      = App_BmsWorld_GetCanTx(world);
+    struct PreCharge *        pre_charge  = App_BmsWorld_GetPreCharge(world);
+    struct Accumulator *      accumulator = App_BmsWorld_GetAccumulator(world);
+
+    const float max_pack_voltage =
+            App_Accumulator_GetMaxPackVoltage(accumulator);
+    const float tractive_system_voltage =
+            App_PreCharge_GetTractiveSystemVoltage(pre_charge);
+    const float pre_charge_voltage_threshold = _getPrechargeVoltageThreshold(max_pack_voltage);
+
+    // Check for overvoltage
+    if (tractive_system_voltage > pre_charge_voltage_threshold)
+    {
+        // todo: ask Prom about 0.93f code
+        App_CanTx_SetPeriodicSignal_PRE_CHARGE_FAULT(
+                can_tx,
+                CANMSGS_BMS_NON_CRITICAL_ERRORS_PRE_CHARGE_FAULT_OVERVOLTAGE_CHOICE);
+        return App_GetInitState();
+    }
+
+    return App_GetPreChargeState();
+}
+
+static const struct State *_prechargeOutOfDurationRunOnTick100Hz(struct BmsWorld *world) {
+    struct BmsCanTxInterface *can_tx      = App_BmsWorld_GetCanTx(world);
+    struct PreCharge *        pre_charge  = App_BmsWorld_GetPreCharge(world);
+    struct Accumulator *      accumulator = App_BmsWorld_GetAccumulator(world);
+    struct Charger *          charger     = App_BmsWorld_GetCharger(world);
+
+    const float max_pack_voltage =
+            App_Accumulator_GetMaxPackVoltage(accumulator);
+    const float tractive_system_voltage =
+            App_PreCharge_GetTractiveSystemVoltage(pre_charge);
+
+    const float pre_charge_voltage_threshold = max_pack_voltage * 0.98f;
+
+    // Check precharge failure
+    if (tractive_system_voltage < pre_charge_voltage_threshold) {
+        // BMS-43, BMS-46
+        // todo: reset the AIR shutdown latch when AIR- is opened.
+        App_CanTx_SetPeriodicSignal_PRE_CHARGE_FAULT(
+                can_tx,
+                CANMSGS_BMS_NON_CRITICAL_ERRORS_PRE_CHARGE_FAULT_UNDERVOLTAGE_CHOICE);
+        // todo: BMS-46: reset the AIR shutdown latch when AIR- is opened
+        // todo: ask why we enter init state instead of fault state in case of precharge failure
+        return App_GetInitState();
+    }
+
+    // Precharge successful
+
+    // todo: BMS-42: Open AIR+ after successful precharge
+    // BMS-44: Precharge success with charger
+    if (App_Charger_IsConnected(charger))
+    {
+        return App_GetChargeState();
+    }
+
+    // todo: add ErrorTable_HasAnyMotorShutdownErrorSet checks to drive state
+    // BMS-45: Precharge success without charger
+    return App_GetDriveState();
+
+}
+
 static void
     PreChargeStateRunOnTick100Hz(struct StateMachine *const state_machine)
 {
     App_AllStatesRunOnTick100Hz(state_machine);
 
     struct BmsWorld *world = App_SharedStateMachine_GetWorld(state_machine);
-    struct BmsCanTxInterface *can_tx      = App_BmsWorld_GetCanTx(world);
     struct PreCharge *        pre_charge  = App_BmsWorld_GetPreCharge(world);
-    struct Accumulator *      accumulator = App_BmsWorld_GetAccumulator(world);
-    struct Charger *          charger     = App_BmsWorld_GetCharger(world);
-    struct ErrorTable *       error_table = App_BmsWorld_GetErrorTable(world);
     struct Clock *            clock       = App_BmsWorld_GetClock(world);
 
     const uint32_t pre_charge_duration =
         App_SharedClock_GetCurrentTimeInMilliseconds(clock) -
         App_SharedClock_GetPreviousTimeInMilliseconds(clock);
-    const float max_pack_voltage =
-        App_Accumulator_GetMaxPackVoltage(accumulator);
-    const float tractive_system_voltage =
-        App_PreCharge_GetTractiveSystemVoltage(pre_charge);
-    const struct State *next_state = App_GetPreChargeState();
+    const struct State *next_state;
 
     if (pre_charge_duration <= App_PreCharge_GetMinDurationMs(pre_charge))
     {
-        if (tractive_system_voltage > (max_pack_voltage * 0.93f))
-        {
-            // Check that the tractive system voltage is less than 93% of the
-            // accumulator voltage while pre-charging the inverter and capacitor
-            // to 90% of the accumulator's max voltage.
-            next_state = App_GetInitState();
-        }
+        next_state = _prechargeInDurationRunOnTick100Hz(world);
     }
     else
     {
-        const float pre_charge_voltage_threshold = max_pack_voltage * 0.98f;
-        if (pre_charge_duration >
-            App_PreCharge_GetMinCompleteDurationMs(pre_charge))
-        {
-            if (tractive_system_voltage >= pre_charge_voltage_threshold)
-            {
-                if (App_Charger_IsConnected(charger))
-                {
-                    next_state = App_GetChargeState();
-                }
-                else if (App_SharedErrorTable_HasAnyMotorShutdownErrorSet(
-                             error_table))
-                {
-                    next_state = App_GetDriveState();
-                }
-            }
-            else
-            {
-                if (pre_charge_duration >
-                    App_PreCharge_GetMaxCompleteDurationMs(pre_charge))
-                {
-                    App_CanTx_SetPeriodicSignal_PRE_CHARGE_FAULT(
-                        can_tx,
-                        CANMSGS_BMS_NON_CRITICAL_ERRORS_PRE_CHARGE_FAULT_UNDERVOLTAGE_CHOICE);
-                    next_state = App_GetInitState();
-                }
-            }
-        }
-        else
-        {
-            if (tractive_system_voltage > pre_charge_voltage_threshold)
-            {
-                App_CanTx_SetPeriodicSignal_PRE_CHARGE_FAULT(
-                    can_tx,
-                    CANMSGS_BMS_NON_CRITICAL_ERRORS_PRE_CHARGE_FAULT_OVERVOLTAGE_CHOICE);
-                next_state = App_GetInitState();
-            }
-        }
+        next_state = _prechargeOutOfDurationRunOnTick100Hz(world);
     }
 
     App_SharedStateMachine_SetNextState(state_machine, next_state);
