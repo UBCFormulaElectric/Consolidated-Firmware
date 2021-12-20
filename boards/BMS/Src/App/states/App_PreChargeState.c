c82073f72b8f634badbb4342ab2eadbe38ac364d
ashevc01 <ashevchuk2001@gmail.com>
2021-12-20 00:15:07 -0800

    refactored precharge code

--- boards/BMS/Src/App/states/App_PreChargeState.c
+++ boards/BMS/Src/App/states/App_PreChargeState.c
@@ -33,59 +33,70 @@ static void PreChargeStateRunOnTick1Hz(struct StateMachine *const state_machine)
     App_AllStatesRunOnTick1Hz(state_machine);
 }
 
-static float _getPrechargeVoltageThreshold(const float max_pack_voltage) {
+static float _getPrechargeVoltageThreshold(const float max_pack_voltage)
+{
     // todo: maybe convert to constant?
     return max_pack_voltage * 0.98f;
 }
 
-static const struct State *_prechargeInDurationRunOnTick100Hz(struct BmsWorld *world) {
+static void
+    _prechargeInDurationRunOnTick100Hz(struct StateMachine *const state_machine)
+{
+    struct BmsWorld *world = App_SharedStateMachine_GetWorld(state_machine);
     struct BmsCanTxInterface *can_tx      = App_BmsWorld_GetCanTx(world);
     struct PreCharge *        pre_charge  = App_BmsWorld_GetPreCharge(world);
     struct Accumulator *      accumulator = App_BmsWorld_GetAccumulator(world);
 
     const float max_pack_voltage =
-            App_Accumulator_GetMaxPackVoltage(accumulator);
+        App_Accumulator_GetMaxPackVoltage(accumulator);
     const float tractive_system_voltage =
-            App_PreCharge_GetTractiveSystemVoltage(pre_charge);
-    const float pre_charge_voltage_threshold = _getPrechargeVoltageThreshold(max_pack_voltage);
+        App_PreCharge_GetTractiveSystemVoltage(pre_charge);
+    const float pre_charge_voltage_threshold =
+        _getPrechargeVoltageThreshold(max_pack_voltage);
 
     // Check for overvoltage
     if (tractive_system_voltage > pre_charge_voltage_threshold)
     {
         // todo: ask Prom about 0.93f code
         App_CanTx_SetPeriodicSignal_PRE_CHARGE_FAULT(
-                can_tx,
-                CANMSGS_BMS_NON_CRITICAL_ERRORS_PRE_CHARGE_FAULT_OVERVOLTAGE_CHOICE);
-        return App_GetInitState();
+            can_tx,
+            CANMSGS_BMS_NON_CRITICAL_ERRORS_PRE_CHARGE_FAULT_OVERVOLTAGE_CHOICE);
+        App_SharedStateMachine_SetNextState(state_machine, App_GetInitState());
+        return;
     }
-
-    return App_GetPreChargeState();
+    App_SharedStateMachine_SetNextState(state_machine, App_GetPreChargeState());
 }
 
-static const struct State *_prechargeOutOfDurationRunOnTick100Hz(struct BmsWorld *world) {
+static void _prechargeOutOfDurationRunOnTick100Hz(
+    struct StateMachine *const state_machine)
+{
+    struct BmsWorld *world = App_SharedStateMachine_GetWorld(state_machine);
     struct BmsCanTxInterface *can_tx      = App_BmsWorld_GetCanTx(world);
     struct PreCharge *        pre_charge  = App_BmsWorld_GetPreCharge(world);
     struct Accumulator *      accumulator = App_BmsWorld_GetAccumulator(world);
     struct Charger *          charger     = App_BmsWorld_GetCharger(world);
-    struct Airs * airs = App_BmsWorld_GetAirs(world);
+    struct Airs *             airs        = App_BmsWorld_GetAirs(world);
 
     const float max_pack_voltage =
-            App_Accumulator_GetMaxPackVoltage(accumulator);
+        App_Accumulator_GetMaxPackVoltage(accumulator);
     const float tractive_system_voltage =
-            App_PreCharge_GetTractiveSystemVoltage(pre_charge);
+        App_PreCharge_GetTractiveSystemVoltage(pre_charge);
 
     const float pre_charge_voltage_threshold = max_pack_voltage * 0.98f;
 
     // Check precharge failure
-    if (tractive_system_voltage < pre_charge_voltage_threshold) {
+    if (tractive_system_voltage < pre_charge_voltage_threshold)
+    {
         // BMS-43, BMS-46
         App_CanTx_SetPeriodicSignal_PRE_CHARGE_FAULT(
-                can_tx,
-                CANMSGS_BMS_NON_CRITICAL_ERRORS_PRE_CHARGE_FAULT_UNDERVOLTAGE_CHOICE);
+            can_tx,
+            CANMSGS_BMS_NON_CRITICAL_ERRORS_PRE_CHARGE_FAULT_UNDERVOLTAGE_CHOICE);
         // todo: BMS-46: reset the AIR shutdown latch when AIR- is opened
 
-        // todo: ask why we enter init state instead of fault state in case of precharge failure
-        return App_GetInitState();
+        // todo: ask why we enter init state instead of fault state in case of
+        // precharge failure
+        App_SharedStateMachine_SetNextState(state_machine, App_GetInitState());
+        return;
     }
 
     // Precharge successful
@@ -96,13 +107,14 @@ static const struct State *_prechargeOutOfDurationRunOnTick100Hz(struct BmsWorld
     // BMS-44: Precharge success with charger
     if (App_Charger_IsConnected(charger))
     {
-        return App_GetChargeState();
+        App_SharedStateMachine_SetNextState(
+            state_machine, App_GetChargeState());
+        return;
     }
 
     // todo: add ErrorTable_HasAnyMotorShutdownErrorSet checks to drive state
     // BMS-45: Precharge success without charger
-    return App_GetDriveState();
-
+    App_SharedStateMachine_SetNextState(state_machine, App_GetDriveState());
 }
 
 static void
@@ -110,25 +122,20 @@ static void
 {
     App_AllStatesRunOnTick100Hz(state_machine);
 
-    struct BmsWorld *world = App_SharedStateMachine_GetWorld(state_machine);
-    struct PreCharge *        pre_charge  = App_BmsWorld_GetPreCharge(world);
-    struct Clock *            clock       = App_BmsWorld_GetClock(world);
+    struct BmsWorld * world = App_SharedStateMachine_GetWorld(state_machine);
+    struct PreCharge *pre_charge = App_BmsWorld_GetPreCharge(world);
+    struct Clock *    clock      = App_BmsWorld_GetClock(world);
 
-    const uint32_t pre_charge_duration =
-        App_SharedClock_GetCurrentTimeInMilliseconds(clock) -
-        App_SharedClock_GetPreviousTimeInMilliseconds(clock);
-    const struct State *next_state;
-
-    if (pre_charge_duration <= App_PreCharge_GetMinDurationMs(pre_charge))
+    if (App_SharedClock_GetCurrentTimeInMilliseconds(clock) -
+            App_SharedClock_GetPreviousTimeInMilliseconds(clock) <=
+        App_PreCharge_GetMinDurationMs(pre_charge))
     {
-        next_state = _prechargeInDurationRunOnTick100Hz(world);
+        _prechargeInDurationRunOnTick100Hz(state_machine);
     }
     else
     {
-        next_state = _prechargeOutOfDurationRunOnTick100Hz(world);
+        _prechargeOutOfDurationRunOnTick100Hz(state_machine);
     }
-
-    App_SharedStateMachine_SetNextState(state_machine, next_state);
 }
 
 static void PreChargeStateRunOnExit(struct StateMachine *const state_machine)
