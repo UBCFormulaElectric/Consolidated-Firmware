2bef49d564f9c0d0662ba3befef98175f0e11040
Pasit Laothamatas <32230980+pasitlaothamatas@users.noreply.github.com>
2022-03-10 21:35:27 -0800

    Add support for differential ADC mode (#735)

--- boards/DIM/Src/Io/Io_Adc.c
+++ boards/DIM/Src/Io/Io_Adc.c
@@ -36,7 +36,7 @@ static float    adc_voltages[NUM_ADC_CHANNELS];
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
 {
     adc_voltages[CHANNEL_12] = Io_SharedAdc_ConvertRawAdcValueToVoltage(
-        hadc, raw_adc_values[CHANNEL_12]);
+        hadc, false, raw_adc_values[CHANNEL_12]);
 }
 
 uint16_t *Io_Adc_GetRawAdcValues(void)
--- boards/shared/Src/Io/Io_SharedAdc.c
+++ boards/shared/Src/Io/Io_SharedAdc.c
@@ -1,47 +1,63 @@
-#include "App_SharedConstants.h"
 #include "Io_SharedAdc.h"
+#include "App_SharedConstants.h"
+
+#define SINGLE_ENDED_ADC_V_SCALE (3.3f)
+#define DIFFERENTIAL_ADC_V_SCALE (6.6f)
 
 float Io_SharedAdc_ConvertRawAdcValueToVoltage(
     ADC_HandleTypeDef *hadc,
+    bool               is_differential,
     uint16_t           raw_adc_value)
 {
-    uint32_t full_scale;
+    uint16_t full_scale = MAX_12_BITS_VALUE;
 
     switch (hadc->Init.Resolution)
     {
         case ADC_RESOLUTION_6B:
-        {
             full_scale = MAX_6_BITS_VALUE;
-        }
-        break;
+            break;
+
         case ADC_RESOLUTION_8B:
-        {
             full_scale = MAX_8_BITS_VALUE;
-        }
-        break;
+            break;
+
         case ADC_RESOLUTION_10B:
-        {
             full_scale = MAX_10_BITS_VALUE;
-        }
-        break;
+            break;
+
         case ADC_RESOLUTION_12B:
-        {
             full_scale = MAX_12_BITS_VALUE;
-        }
-        break;
+            break;
+
+        default:
+            full_scale = MAX_12_BITS_VALUE;
+            break;
     }
 
     // Taken from the STM32 manual, the formula to convert the raw ADC
     // measurements to an absolute voltage value is as follows:
     //
-    //                   VDDA (3.3V) x ADC_DATAx
-    //    V_CHANNELx = ----------------------------
-    //                         FULL_SCALE
+    //                   V_SCALE (VDDA or 2VDDA) x ADC_DATAx
+    //    V_CHANNELx = ---------------------------------------
+    //                               FULL_SCALE
     // where:
+    // - V_SCALE depends on whether the ADC is configured as a single ended or
+    // differential input
     // - ADC_DATAx is the value measured by the ADC on channel x (right-aligned)
     // - FULL_SCALE is the maximum digital value of the ADC output. For example
     //   with 12-bit resolution, it will be 2^12 -1 = 4095 or with 8-bit
     //   resolution, 2^8 - 1 = 255.
 
-    return (3.3f * raw_adc_value) / (float)full_scale;
+    float voltage = 0.0f;
+    if (is_differential)
+    {
+        voltage = DIFFERENTIAL_ADC_V_SCALE * (int16_t)raw_adc_value /
+                  (float)full_scale;
+    }
+    else
+    {
+        voltage = SINGLE_ENDED_ADC_V_SCALE * raw_adc_value / (float)full_scale;
+    }
+
+    return voltage;
 }
