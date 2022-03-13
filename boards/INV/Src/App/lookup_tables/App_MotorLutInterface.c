c91e4d1f4fa36928d27bb894140311c93c97f926
Auhv <geoff-gw@hotmail.com>
2022-03-13 01:35:37 -0800

    got lookup tables and derating working ( i think)

--- boards/INV/Src/App/lookup_tables/App_MotorLutInterface.c
+++ boards/INV/Src/App/lookup_tables/App_MotorLutInterface.c
@@ -35,8 +35,8 @@ void look_up_dqs_current_ref(
 
 float intp_values_from_lut(uint8_t voltage_index_high, uint8_t voltage_index_low, float voltage_inter_percentage, uint8_t torque_index, uint8_t speed_index, uint8_t table_number)
 {
-    float intp1_val = 0.0f;
-    float intp2_val = 0.0f;
+    float intp1_high_volt = 0.0f;
+    float intp2_low_volt = 0.0f;
     float intp2p1_val = 0.0f;
 
     speed_index = speed_index > LUT_NUM_COLUMNS-1 ? LUT_NUM_COLUMNS-1 : (speed_index < 0 ? 0 : speed_index);
@@ -44,21 +44,24 @@ float intp_values_from_lut(uint8_t voltage_index_high, uint8_t voltage_index_low
 
     if(table_number == ID_PEAK)
     {
-        intp1_val = (float)(id_peak_lut_times100[voltage_index_high][speed_index][torque_index])/100.0f;
-        intp2_val = (float)(id_peak_lut_times100[voltage_index_low][speed_index][torque_index])/100.0f;
-        intp2p1_val = (intp2_val - intp1_val)*voltage_inter_percentage + intp1_val;
+        intp1_high_volt = (float)(id_peak_lut_times100[voltage_index_high][speed_index][torque_index])/100.0f;
+        intp2_low_volt = (float)(id_peak_lut_times100[voltage_index_low][speed_index][torque_index])/100.0f;
+        intp2p1_val = intp1_high_volt > intp2_low_volt ? (intp2_low_volt + fabsf(intp2_low_volt - intp1_high_volt)*voltage_inter_percentage)
+                                            : (intp2_low_volt - fabsf(intp2_low_volt - intp1_high_volt)*voltage_inter_percentage);
     }
     else if(table_number == IQ_PEAK)
     {
-        intp1_val = (float)(iq_peak_lut_times100[voltage_index_high][speed_index][torque_index])/100.0f;
-        intp2_val = (float)(iq_peak_lut_times100[voltage_index_low][speed_index][torque_index])/100.0f;
-        intp2p1_val = (intp2_val - intp1_val)*voltage_inter_percentage + intp1_val;
+        intp1_high_volt = (float)(iq_peak_lut_times100[voltage_index_high][speed_index][torque_index])/100.0f;
+        intp2_low_volt = (float)(iq_peak_lut_times100[voltage_index_low][speed_index][torque_index])/100.0f;
+        intp2p1_val = intp1_high_volt > intp2_low_volt ? (intp2_low_volt + fabsf(intp2_low_volt - intp1_high_volt)*voltage_inter_percentage)
+                                            : (intp2_low_volt - fabsf(intp2_low_volt - intp1_high_volt)*voltage_inter_percentage);
     }
     else if(table_number == IS_PEAK)
     {
-        intp1_val = (float)(is_peak_lut_times100[voltage_index_high][speed_index][torque_index])/100.0f;
-        intp2_val = (float)(is_peak_lut_times100[voltage_index_low][speed_index][torque_index])/100.0f;
-        intp2p1_val = (intp2_val - intp1_val)*voltage_inter_percentage + intp1_val;
+        intp1_high_volt = (float)(is_peak_lut_times100[voltage_index_high][speed_index][torque_index])/100.0f;
+        intp2_low_volt = (float)(is_peak_lut_times100[voltage_index_low][speed_index][torque_index])/100.0f;
+        intp2p1_val = intp1_high_volt > intp2_low_volt ? (intp2_low_volt + fabsf(intp2_low_volt - intp1_high_volt)*voltage_inter_percentage)
+                                            : (intp2_low_volt - fabsf(intp2_low_volt - intp1_high_volt)*voltage_inter_percentage);
     }
     //Invalid table number
     else
@@ -76,30 +79,32 @@ uint8_t calculate_derated_torque_index(float derated_stator_current, uint8_t vol
     float stator_current_guess = intp_values_from_lut(voltage_index_high, voltage_index_low, voltage_inter_percentage, derated_torque_index_guess, speed_index, IS_PEAK);
 
     //Use the fact that stator current is monotonic with the torque index to find an appropriate torque index for the current limit.
-//    if(stator_current_guess >= derated_stator_current)
-//    {
-//        while(stator_current_guess > derated_stator_current && derated_torque_index_guess > 0)
-//        {
-//            derated_torque_index_guess -= 1;
-//            stator_current_guess = intp_values_from_lut(voltage_index_high, voltage_index_low, voltage_inter_percentage, derated_torque_index_guess, speed_index, IS_PEAK);
-//        }
-//    }
-//    else
-//    {
-//        int times_ran = 0;
-//        float last_stator_current_guess = -1;
-//        //Keep going up in stator current until it stops increasing or we surpass the limit
-//        while(stator_current_guess < derated_stator_current && derated_torque_index_guess < LUT_NUM_ROWS && stator_current_guess != last_stator_current_guess)
-//        {
-//            times_ran += 1;
-//            derated_torque_index_guess += 1;
-//            last_stator_current_guess = stator_current_guess;
-//            //stator_current_guess = intp_values_from_lut(voltage_index_high, voltage_index_low, voltage_inter_percentage, derated_torque_index_guess, speed_index, IS_PEAK);
-//
-//        }
-//        //We're using one torque index too high since we want to be lower than the limit, so subtract one
-//        derated_torque_index_guess -= 1;
-//    }
+    if(stator_current_guess >= derated_stator_current)
+    {
+        while(stator_current_guess > derated_stator_current && derated_torque_index_guess > 0)
+        {
+            derated_torque_index_guess -= 1;
+            stator_current_guess = intp_values_from_lut(voltage_index_high, voltage_index_low, voltage_inter_percentage, derated_torque_index_guess, speed_index, IS_PEAK);
+        }
+    }
+    else
+    {
+        int times_ran = 0;
+        float last_stator_current_guess = -1;
+        //Keep going up in stator current until it stops increasing or we surpass the limit
+        while(stator_current_guess < derated_stator_current && derated_torque_index_guess < LUT_NUM_ROWS && stator_current_guess != last_stator_current_guess)
+        {
+            times_ran += 1;
+            derated_torque_index_guess += 1;
+            last_stator_current_guess = stator_current_guess;
+            //stator_current_guess = intp_values_from_lut(voltage_index_high, voltage_index_low, voltage_inter_percentage, derated_torque_index_guess, speed_index, IS_PEAK);
+            //TODO derate on 600V value for now - doesn't show much error, but will cause things to get hotter. The derating system should prevent this from getting out of control
+            //Note: not quite enough cycles to compute full interpolated values between voltages
+            stator_current_guess = (float)(is_peak_lut_times100[BUS_VOLTAGE_600V][speed_index][derated_torque_index_guess])/100.0f;
+        }
+        //We're using one torque index too high since we want to be lower than the limit, so subtract one
+        derated_torque_index_guess -= 1;
+    }
 
-    return 100;//derated_torque_index_guess;
+    return derated_torque_index_guess;
 }
