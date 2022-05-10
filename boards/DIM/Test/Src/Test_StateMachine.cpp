0a657e3d70e4743704a809fb1e12751ea4379182
Pasit Laothamatas <32230980+pasitlaothamatas@users.noreply.github.com>
2022-05-10 16:10:37 -0700

    Implement missing hb callback FSM (#773)

--- boards/DIM/Test/Src/Test_StateMachine.cpp
+++ boards/DIM/Test/Src/Test_StateMachine.cpp
@@ -588,7 +588,7 @@ TEST_F(DimStateMachineTest, dcm_board_status_led_control_with_multiple_errors)
 TEST_F(DimStateMachineTest, fsm_board_status_led_control_with_critical_error)
 {
     // Set any critical error and check that the FSM LED turns red
-    App_SharedErrorTable_SetError(error_table, FSM_AIR_SHUTDOWN_DUMMY_AIR_SHUTDOWN, true);
+    App_SharedErrorTable_SetError(error_table, FSM_AIR_SHUTDOWN_MISSING_HEARTBEAT, true);
     LetTimePass(state_machine, 10);
     ASSERT_EQ(1, turn_fsm_status_led_red_fake.call_count);
 }
@@ -616,7 +616,7 @@ TEST_F(DimStateMachineTest, fsm_board_status_led_control_with_multiple_errors)
     // If the error table contains critical and non-critical errors
     // simultaneously, the critical error should take precedence and turn the
     // FSM LED red rather than blue
-    App_SharedErrorTable_SetError(error_table, FSM_AIR_SHUTDOWN_DUMMY_AIR_SHUTDOWN, true);
+    App_SharedErrorTable_SetError(error_table, FSM_AIR_SHUTDOWN_MISSING_HEARTBEAT, true);
     App_SharedErrorTable_SetError(error_table, FSM_NON_CRITICAL_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
     LetTimePass(state_machine, 10);
     ASSERT_EQ(1, turn_fsm_status_led_red_fake.call_count);
