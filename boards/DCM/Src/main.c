b2931c83e6c216f10958ed3957781b1f1ccd9e53
timbitss <timothyng00@gmail.com>
2022-05-29 12:48:21 -0700

    Add left resolver calibration, update inverter msg cycle times
    - HAL_Delay() at start-up band-aid for not receiving messages from DIM

--- boards/DCM/Src/main.c
+++ boards/DCM/Src/main.c
@@ -157,7 +157,7 @@ int main(void)
     SystemClock_Config();
 
     /* USER CODE BEGIN SysInit */
-
+    HAL_Delay(5000); // For DIM to send CAN msgs properly
     /* USER CODE END SysInit */
 
     /* Initialize all configured peripherals */
