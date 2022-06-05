644f2a42ecaa531525d0d03165cb720e7ff407b8
Pasit Laothamatas <morp.laothamatas@gmail.com>
2022-06-05 11:56:14 -0700

    minor DCM changes

--- boards/DCM/Src/App/states/App_DriveState.c
+++ boards/DCM/Src/App/states/App_DriveState.c
@@ -4,35 +4,35 @@
 #include "states/App_AllStates.h"
 #include "states/App_InitState.h"
 #include "App_SetPeriodicCanSignals.h"
+#include "configs/App_MotorEfficiencyLut.h"
 
 #define MAX_TORQUE_REQUEST_NM (90.0f)
 #define EFFICIENCY_ESTIMATE (0.80f)
 #define RPM_TO_RADS(rpm) ((rpm) * (float)M_PI / 30.0f)
-#define IS_EQUAL_FLOAT(a, b) (fabsf((a) - (b)) < 0.005f)
 
 void App_SetPeriodicCanSignals_TorqueRequests(struct DcmCanTxInterface *can_tx, struct DcmCanRxInterface *can_rx)
 {
-    float max_torque_request, torque_request;
-    float bms_available_power   = App_CanRx_BMS_AVAILABLE_POWER_GetSignal_AVAILABLE_POWER(can_rx);
-    float right_motor_speed_rpm = (float)abs(App_CanRx_INVR_MOTOR_POSITION_INFO_GetSignal_D2_MOTOR_SPEED_INVR(can_rx));
-    float left_motor_speed_rpm  = (float)abs(App_CanRx_INVL_MOTOR_POSITION_INFO_GetSignal_D2_MOTOR_SPEED_INVL(can_rx));
-
     // Estimate the maximum torque request to draw the maximum power available from the BMS
     // Note that the motors can not exceed a torque of MAX_TORQUE_REQUEST_NM
-    if (IS_EQUAL_FLOAT(right_motor_speed_rpm + left_motor_speed_rpm, 0.0f))
+    const float bms_available_power = App_CanRx_BMS_AVAILABLE_POWER_GetSignal_AVAILABLE_POWER(can_rx);
+    const float right_motor_speed_rpm =
+        (float)abs(App_CanRx_INVR_MOTOR_POSITION_INFO_GetSignal_D2_MOTOR_SPEED_INVR(can_rx));
+    const float left_motor_speed_rpm =
+        (float)abs(App_CanRx_INVL_MOTOR_POSITION_INFO_GetSignal_D2_MOTOR_SPEED_INVL(can_rx));
+
+    float bms_torque_limit = MAX_TORQUE_REQUEST_NM;
+    if ((right_motor_speed_rpm + left_motor_speed_rpm) > 0.0f)
     {
-        max_torque_request = MAX_TORQUE_REQUEST_NM;
-    }
-    else
-    {
-        float bms_torque_limit = bms_available_power * EFFICIENCY_ESTIMATE /
-                                 (RPM_TO_RADS(right_motor_speed_rpm) + RPM_TO_RADS(left_motor_speed_rpm));
-        max_torque_request = min(bms_torque_limit, MAX_TORQUE_REQUEST_NM);
+        bms_torque_limit = bms_available_power * EFFICIENCY_ESTIMATE /
+                           (RPM_TO_RADS(right_motor_speed_rpm) + RPM_TO_RADS(left_motor_speed_rpm));
     }
 
+    const float max_torque_request = min(bms_torque_limit, MAX_TORQUE_REQUEST_NM);
+
     // Calculate the actual torque request to transmit
-    torque_request =
+    const float torque_request =
         0.01f * App_CanRx_FSM_PEDAL_POSITION_GetSignal_MAPPED_PEDAL_PERCENTAGE(can_rx) * max_torque_request;
+    App_CanRx_FSM_PEDAL_POSITION_SetSignal_MAPPED_PEDAL_PERCENTAGE(can_rx, 0.0f);
 
     // Transmit torque command to both inverters
     App_CanTx_SetPeriodicSignal_TORQUE_COMMAND_INVL(
