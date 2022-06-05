8efcba0102d1707b584dde2bfdfaae7198de1d0f
Pasit Laothamatas <32230980+pasitlaothamatas@users.noreply.github.com>
2022-06-05 12:55:22 -0700

    Reset pedal position in CAN table + minor changes (#802)

---
+++ boards/DCM/Inc/App/configs/App_MotorEfficiencyLut.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define MOTOR_EFFICIENCY_LUT_INTERVAL (500U)
+#define MOTOR_EFFICIENCY_LUT_SIZE (14U)
+
+// Motor efficiency LUT with interval of 500rpm
+static const float motor_efficiency_lut[MOTOR_EFFICIENCY_LUT_SIZE] = { 0.8f, 0.8f, 0.8f, 0.8f, 0.8f, 0.8f, 0.8f,
+                                                                       0.8f, 0.8f, 0.8f, 0.8f, 0.8f, 0.8f, 0.8f };
