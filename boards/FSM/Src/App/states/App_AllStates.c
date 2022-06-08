751617644d62f20346957d5898834d324e47d524
Timothy Nguyen <timothyng00@gmail.com>
2022-06-07 19:59:53 -0700

    Add FSM torque limit and plausibility check (#803)
    * Add FSM torque limit and plausibility check
    
    - Throw motor shutdown error if plausibility check failed
    - Adjustable FSM torque limit upper offset
    
    * Add error count to torque plausibility
    
    - FSM torque plausibility error thrown if fails for > 200 ms
    - DCM does not scale pedal percentage by FSM torque limit

--- boards/FSM/Src/App/states/App_AllStates.c
+++ boards/FSM/Src/App/states/App_AllStates.c
@@ -1,4 +1,8 @@
 #include "states/App_AllStates.h"
+#include "App_SharedConstants.h"
+
+#define TORQUE_LIMIT_OFFSET_NM (5.0f)
+#define MAX_TORQUE_PLAUSIBILITY_ERR_CNT (25) // 250 ms window
 
 void App_AllStatesRunOnTick1Hz(struct StateMachine *const state_machine)
 {
@@ -15,10 +19,11 @@ void App_AllStatesRunOnTick1Hz(struct StateMachine *const state_machine)
 
 void App_AllStatesRunOnTick100Hz(struct StateMachine *const state_machine)
 {
-    struct FsmWorld *         world      = App_SharedStateMachine_GetWorld(state_machine);
-    struct FsmCanTxInterface *can_tx     = App_FsmWorld_GetCanTx(world);
-    struct FsmCanRxInterface *can_rx     = App_FsmWorld_GetCanRx(world);
-    struct HeartbeatMonitor * hb_monitor = App_FsmWorld_GetHeartbeatMonitor(world);
+    struct FsmWorld *         world       = App_SharedStateMachine_GetWorld(state_machine);
+    struct FsmCanTxInterface *can_tx      = App_FsmWorld_GetCanTx(world);
+    struct FsmCanRxInterface *can_rx      = App_FsmWorld_GetCanRx(world);
+    struct HeartbeatMonitor * hb_monitor  = App_FsmWorld_GetHeartbeatMonitor(world);
+    static uint8_t            error_count = 0;
 
     App_CanTx_SetPeriodicSignal_HEARTBEAT(can_tx, true);
 
@@ -27,6 +32,31 @@ void App_AllStatesRunOnTick100Hz(struct StateMachine *const state_machine)
         App_SharedHeartbeatMonitor_CheckIn(hb_monitor, BMS_HEARTBEAT_ONE_HOT);
         App_CanRx_BMS_VITALS_SetSignal_HEARTBEAT(can_rx, false);
     }
+
+    // Check for torque plausibility
+    float left_torque_req = App_CanMsgs_dcm_invl_command_message_torque_command_invl_decode(
+        App_CanRx_DCM_INVL_COMMAND_MESSAGE_GetSignal_TORQUE_COMMAND_INVL(can_rx));
+    float right_torque_req = App_CanMsgs_dcm_invr_command_message_torque_command_invr_decode(
+        App_CanRx_DCM_INVR_COMMAND_MESSAGE_GetSignal_TORQUE_COMMAND_INVR(can_rx));
+    float fsm_torque_limit = App_CanTx_GetPeriodicSignal_FSM_TORQUE_LIMIT(can_tx);
+    if (left_torque_req > fsm_torque_limit || right_torque_req > fsm_torque_limit)
+    {
+        error_count++;
+    }
+    else
+    {
+        error_count = 0;
+    }
+
+    if (error_count == MAX_TORQUE_PLAUSIBILITY_ERR_CNT)
+    {
+        App_CanTx_SetPeriodicSignal_TORQUE_PLAUSIBILITY_CHECK_FAILED(can_tx, true);
+    }
+
+    // Broadcast a new FSM torque limit based on pedal percentage
+    fsm_torque_limit = 0.01f * App_CanTx_GetPeriodicSignal_MAPPED_PEDAL_PERCENTAGE(can_tx) * MAX_TORQUE_REQUEST_NM +
+                       TORQUE_LIMIT_OFFSET_NM;
+    App_CanTx_SetPeriodicSignal_FSM_TORQUE_LIMIT(can_tx, fsm_torque_limit);
 
     App_CanTx_SetPeriodicSignal_MISSING_HEARTBEAT(can_tx, !App_SharedHeartbeatMonitor_Tick(hb_monitor));
 }
