6544e08abe0bd4d5278bc3ee0c66ec2703ab7350
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2022-10-21 19:58:50 -0700

    ELEC-242 Bad Flow Range Check

--- boards/FSM/Src/App/App_FlowMeterSignals.c
+++ boards/FSM/Src/App/App_FlowMeterSignals.c
@@ -1,16 +1,16 @@
 #include "App_FlowMeterSignals.h"
 
-bool App_FlowMetersSignals_IsPrimaryFlowRateBelowThreshold(struct FsmWorld *const world)
-{
-    struct InRangeCheck *flow_rate_in_range_check = App_FsmWorld_GetFlowRateInRangeCheck(world);
+bool App_FlowMetersSignals_IsPrimaryFlowRateBelowThreshold(struct FsmWorld *const world){
+    struct Coolant *coolant = App_FsmWorld_GetCoolant(world);
+    struct InRangeCheck *flow_rate_in_range_check = App_Coolant_GetFlowInRangeCheck(coolant);
 
     float flow_rate;
     return App_InRangeCheck_GetValue(flow_rate_in_range_check, &flow_rate) == VALUE_UNDERFLOW;
 }
 
-bool App_FlowMetersSignals_IsFlowRateInRange(struct FsmWorld *const world)
-{
-    struct InRangeCheck *flow_rate_in_range_check = App_FsmWorld_GetFlowRateInRangeCheck(world);
+bool App_FlowMetersSignals_IsFlowRateInRange(struct FsmWorld *const world){
+    struct Coolant *coolant = App_FsmWorld_GetCoolant(world);
+    struct InRangeCheck *flow_rate_in_range_check = App_Coolant_GetFlowInRangeCheck(coolant);
 
     float flow_rate;
     return App_InRangeCheck_GetValue(flow_rate_in_range_check, &flow_rate) == VALUE_IN_RANGE;
