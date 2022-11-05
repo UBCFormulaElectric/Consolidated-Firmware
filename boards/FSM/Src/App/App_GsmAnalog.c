aa9acc845549c84c65bf7904f52aeb32ecbf2031
Ivan Cheung <ivancheung455@gmail.com>
2022-11-05 11:23:40 -0700

    GSM App Code prototype

---
+++ boards/FSM/Src/App/App_GsmAnalog.c
@@ -0,0 +1,58 @@
+//
+// Created by formulae on 20/10/22.
+//
+#include <stddef.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <linkedlist.h>
+#include "/home/formulae/Documents/Consolidated-Firmware/boards/FSM/Inc/App/App_GsmAnalog.h"
+struct GSMAnalog {
+    struct GSMAnalog *GSMAnalog_create;
+    float slope;
+    float y_int;
+    float (*get_sensorAnalogVal_voltage)(void);
+    float (*get_output)(void);
+    char* units;
+
+};
+struct GSMAnalog *GSMAnalog_create (float slope,
+        float y_int,
+        float (*get_sensorAnalogVal_voltage)(void),
+        float (*get_output)(void),
+        char* units)
+{
+    struct GSMAnalog *gsmAnalog = malloc(sizeof (struct GSMAnalog));
+    assert(gsmAnalog != NULL);
+    gsmAnalog ->slope = slope;
+    gsmAnalog -> y_int = y_int;
+    gsmAnalog -> get_sensorAnalogVal_voltage = get_sensorAnalogVal_voltage;
+    gsmAnalog -> get_output = get_output;
+    gsmAnalog -> units = units;
+    return gsmAnalog;
+
+}
+void App_GSMAnalog_Destroy (struct GSMAnalog *gsmAnalog)
+{
+    free(gsmAnalog);
+}
+float get_output(const struct GSMAnalog *gsmAnalog)
+        {
+    return gsmAnalog->slope *  gsmAnalog->get_sensorAnalogVal_voltage() + gsmAnalog->y_int;
+        }
+float get_sensorAnalogVal_voltage (const struct GSMAnalog *gsmAnalog){
+    return gsmAnalog->get_sensorAnalogVal_voltage();
+}
+/*
+ * Create IO code to interface with the 8 channels
+
+Channels must have ability to tune to different transfer functions
+
+Possibly pass first order function as argument to IO code?
+
+Explore options in this regard
+ Provide CAN Bus support for 8 generic channels.
+ Include configurability in the units, name, etc so that the board can
+ easily switch to reporting different sensors
+Functions:
+ - function to label
+ */
---
+++ boards/FSM/Src/Io/Io_GSMDigital.h
@@ -0,0 +1,14 @@
+//
+// Created by formulae on 05/11/22.
+//
+
+#ifndef FORMULA_ELECTRIC_FIRMWARE_IO_GSMDIGITAL_H
+#define FORMULA_ELECTRIC_FIRMWARE_IO_GSMDIGITAL_H
+
+#endif //FORMULA_ELECTRIC_FIRMWARE_IO_GSMDIGITAL_H
+
+#include "Io_SharedFreqOnlyPwmInput.h"
+float Io_GetVoltage (void)
+{
+    return
+}
