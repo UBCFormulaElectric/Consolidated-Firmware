3f400dfe8339a82ff286276fd467487d71eb9302
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2022-11-12 14:56:39 -0800

    state changes

--- boards/FSM/Src/App/states/App_FaultState.c
+++ boards/FSM/Src/App/states/App_FaultState.c
@@ -4,7 +4,9 @@
 
 void App_FaultStateRunOnEntry(struct StateMachine *const state_machine)
 {
-    return;
+    struct FsmWorld *         world            = App_SharedStateMachine_GetWorld(state_machine);
+    struct FsmCanTxInterface *can_tx_interface = App_FsmWorld_GetCanTx(world);
+    App_CanTx_SetPeriodicSignal_STATE(can_tx_interface, CANMSGS_FSM_STATE_MACHINE_STATE_FAULT_CHOICE);
 }
 
 void App_FaultStateRunOnTick1Hz(struct StateMachine *state_machine)
