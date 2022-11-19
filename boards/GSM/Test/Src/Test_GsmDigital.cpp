51cd5ec1ef27200cb803ccfbe091f307062ba6de
Ivan Cheung <ivancheung455@gmail.com>
2022-11-19 11:19:45 -0800

    started analog testing

---
+++ boards/GSM/Test/Src/Test_GsmDigital.cpp
@@ -0,0 +1,21 @@
+//
+// Created by formulae on 19/11/22.
+//
+#include "Test_Gsm.h"
+
+extern "C"
+{
+#include "App_GSMDigital.h"
+}
+
+//FAKE_VALUE_FUNC(float, get_sensorDigitalVal_voltage);
+
+class GsmDigitalTest : public testing :: Test
+{
+protected:
+    void SetUp() override {
+
+    }
+};
+
+
