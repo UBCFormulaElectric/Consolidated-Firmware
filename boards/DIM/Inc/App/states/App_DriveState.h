abfc0046d41f38f067581b24e5142ed816ba33b5
will-chaba <wchaba@alumni.ubc.ca>
2022-11-26 10:52:11 -0800

    move bms fault + warning checks to function

--- boards/DIM/Inc/App/states/App_DriveState.h
+++ boards/DIM/Inc/App/states/App_DriveState.h
@@ -7,3 +7,17 @@
  * @return A pointer to the Drive State. THIS SHOULD NOT BE MODIFIED.
  */
 const struct State *App_GetDriveState(void);
+
+/**
+ * Check CAN bus for BMS Fault messages
+ * @param can_rx Dim CAN RX interface for recieving messages
+ * @return True if any BMS Fault is present
+ */
+const bool has_bms_fault(const struct DimCanRxInterface *can_rx);
+
+/**
+ * Check CAN bus for BMS Warning messages
+ * @param can_rx Dim CAN RX interface for recieving messages
+ * @return True if any BMS Warning is present
+ */
+const bool has_bms_warning(const struct DimCanRxInterface *can_rx);
