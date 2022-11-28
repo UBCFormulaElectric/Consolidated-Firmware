9159609005cd45e24e3039ccde61e396da5f7638
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2022-11-27 16:58:35 -0800

    Timer now works

--- boards/FSM/Src/main.c
+++ boards/FSM/Src/main.c
@@ -28,6 +28,7 @@
 // shared
 #include "App_SharedMacros.h"
 #include "App_SharedStateMachine.h"
+#include "App_Timer.h"
 
 // IO functions exposing
 #include "Io_CanTx.h"
@@ -898,7 +899,7 @@ void RunTask1kHz(void const *argument)
         Io_SharedSoftwareWatchdog_CheckForTimeouts();
         const uint32_t task_start_ms = TICK_TO_MS(osKernelSysTick());
 
-        // App_SharedClock_SetCurrentTimeInMilliseconds(clock, task_start_ms);
+        App_Timer_SetCurrentTimeMS(task_start_ms);
         Io_CanTx_EnqueuePeriodicMsgs(can_tx, task_start_ms);
 
         // Watchdog check-in must be the last function called before putting the
