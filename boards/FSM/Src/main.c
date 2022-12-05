db8cd24e31204979e8d869824a76e24f21ab48f9
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2022-12-04 21:36:43 -0800

    fix formatting

--- boards/FSM/Src/main.c
+++ boards/FSM/Src/main.c
@@ -228,8 +228,8 @@ int main(void)
     // Coolants
     Io_FlowMeter_Init(&htim4);
     coolant = App_Coolant_Create(
-        Io_FlowMeter_GetFlowRate, Io_Coolant_GetTemperatureA,
-        Io_Coolant_GetTemperatureB, Io_Coolant_GetPressureA, Io_Coolant_GetPressureB);
+        Io_FlowMeter_GetFlowRate, Io_Coolant_GetTemperatureA, Io_Coolant_GetTemperatureB, Io_Coolant_GetPressureA,
+        Io_Coolant_GetPressureB);
 
     // steering
     steering = App_Steering_Create(Io_SteeringAngleSensor_GetAngleDegree, Io_SteeringSensorOCSC);
