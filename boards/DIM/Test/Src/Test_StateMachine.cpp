103652d5d2045258194d54c75ef62810641c98ca
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2023-01-11 16:37:54 -0800

    test 2

--- boards/DIM/Test/Src/Test_StateMachine.cpp
+++ boards/DIM/Test/Src/Test_StateMachine.cpp
@@ -118,9 +118,9 @@ class DimStateMachineTest : public BaseStateMachineTest
         clock = App_SharedClock_Create();
 
         world = App_DimWorld_Create(
-            seven_seg_displays, heartbeat_monitor, rgb_led_sequence,
-            drive_mode_switch, imd_led, bspd_led, start_switch, traction_control_switch, torque_vectoring_switch,
-            bms_status_led, dcm_status_led, dim_status_led, fsm_status_led, pdm_status_led, clock);
+            seven_seg_displays, heartbeat_monitor, rgb_led_sequence, drive_mode_switch, imd_led, bspd_led, start_switch,
+            traction_control_switch, torque_vectoring_switch, bms_status_led, dcm_status_led, dim_status_led,
+            fsm_status_led, pdm_status_led, clock);
 
         // Default to starting the state machine in the `Drive` state
         state_machine = App_SharedStateMachine_Create(world, App_GetDriveState());
@@ -239,7 +239,7 @@ TEST_F(DimStateMachineTest, check_drive_state_is_broadcasted_over_can)
 {
     SetInitialState(App_GetDriveState());
 
-//    EXPECT_EQ(CANMSGS_DIM_STATE_MACHINE_STATE_DRIVE_CHOICE, App_CanTx_GetPeriodicSignal_STATE(can_tx_interface));
+    //    EXPECT_EQ(CANMSGS_DIM_STATE_MACHINE_STATE_DRIVE_CHOICE, App_CanTx_GetPeriodicSignal_STATE(can_tx_interface));
 }
 
 // DIM-9
@@ -276,7 +276,7 @@ TEST_F(DimStateMachineTest, check_7_seg_displays_show_error_id_in_drive_state_if
     // Set error for some made-up error ID, the value 10 was chosen because
     // it will be value as valid as we have more than 10 errors, which should
     // always be true.
-//    App_SharedErrorTable_SetError(error_table, (enum ErrorId)(10), true);
+    //    App_SharedErrorTable_SetError(error_table, (enum ErrorId)(10), true);
 
     LetTimePass(state_machine, 10);
 
@@ -297,8 +297,8 @@ TEST_F(DimStateMachineTest, check_7_seg_displays_cycle_through_two_error_ids_in_
     // Set errors for some made-up error IDs, the value 10 and 11 were chosen
     // because it will be valid as long as we have more than 11 errors, which
     // should always be true.
-//    App_SharedErrorTable_SetError(error_table, (enum ErrorId)(10), true);
-//    App_SharedErrorTable_SetError(error_table, (enum ErrorId)(11), true);
+    //    App_SharedErrorTable_SetError(error_table, (enum ErrorId)(10), true);
+    //    App_SharedErrorTable_SetError(error_table, (enum ErrorId)(11), true);
 
     // When an error ID shows up on the 7-segment displays, it will have an
     // offset of 500 added to it. This is why we are asserting for the value
@@ -341,11 +341,13 @@ TEST_F(DimStateMachineTest, check_start_switch_is_broadcasted_over_can_in_drive_
 {
     start_switch_is_turned_on_fake.return_val = false;
     LetTimePass(state_machine, 10);
-//    ASSERT_EQ(CANMSGS_DIM_SWITCHES_START_SWITCH_OFF_CHOICE, App_CanTx_GetPeriodicSignal_START_SWITCH(can_tx_interface));
+    //    ASSERT_EQ(CANMSGS_DIM_SWITCHES_START_SWITCH_OFF_CHOICE,
+    //    App_CanTx_GetPeriodicSignal_START_SWITCH(can_tx_interface));
 
     start_switch_is_turned_on_fake.return_val = true;
     LetTimePass(state_machine, 10);
-//    ASSERT_EQ(CANMSGS_DIM_SWITCHES_START_SWITCH_ON_CHOICE, App_CanTx_GetPeriodicSignal_START_SWITCH(can_tx_interface));
+    //    ASSERT_EQ(CANMSGS_DIM_SWITCHES_START_SWITCH_ON_CHOICE,
+    //    App_CanTx_GetPeriodicSignal_START_SWITCH(can_tx_interface));
 }
 
 // DIM-4
@@ -353,15 +355,15 @@ TEST_F(DimStateMachineTest, check_traction_control_switch_is_broadcasted_over_ca
 {
     traction_control_switch_is_turned_on_fake.return_val = false;
     LetTimePass(state_machine, 10);
-//    ASSERT_EQ(
-//        CANMSGS_DIM_SWITCHES_START_SWITCH_OFF_CHOICE,
-//        App_CanTx_GetPeriodicSignal_TRACTION_CONTROL_SWITCH(can_tx_interface));
+    //    ASSERT_EQ(
+    //        CANMSGS_DIM_SWITCHES_START_SWITCH_OFF_CHOICE,
+    //        App_CanTx_GetPeriodicSignal_TRACTION_CONTROL_SWITCH(can_tx_interface));
 
     traction_control_switch_is_turned_on_fake.return_val = true;
     LetTimePass(state_machine, 10);
-//    ASSERT_EQ(
-//        CANMSGS_DIM_SWITCHES_START_SWITCH_ON_CHOICE,
-//        App_CanTx_GetPeriodicSignal_TRACTION_CONTROL_SWITCH(can_tx_interface));
+    //    ASSERT_EQ(
+    //        CANMSGS_DIM_SWITCHES_START_SWITCH_ON_CHOICE,
+    //        App_CanTx_GetPeriodicSignal_TRACTION_CONTROL_SWITCH(can_tx_interface));
 }
 
 // DIM-4
@@ -369,15 +371,15 @@ TEST_F(DimStateMachineTest, check_torque_vectoring_switch_is_broadcasted_over_ca
 {
     torque_vectoring_switch_is_turned_on_fake.return_val = false;
     LetTimePass(state_machine, 10);
-//    ASSERT_EQ(
-//        CANMSGS_DIM_SWITCHES_START_SWITCH_OFF_CHOICE,
-//        App_CanTx_GetPeriodicSignal_TORQUE_VECTORING_SWITCH(can_tx_interface));
+    //    ASSERT_EQ(
+    //        CANMSGS_DIM_SWITCHES_START_SWITCH_OFF_CHOICE,
+    //        App_CanTx_GetPeriodicSignal_TORQUE_VECTORING_SWITCH(can_tx_interface));
 
     torque_vectoring_switch_is_turned_on_fake.return_val = true;
     LetTimePass(state_machine, 10);
-//    ASSERT_EQ(
-//        CANMSGS_DIM_SWITCHES_START_SWITCH_ON_CHOICE,
-//        App_CanTx_GetPeriodicSignal_TORQUE_VECTORING_SWITCH(can_tx_interface));
+    //    ASSERT_EQ(
+    //        CANMSGS_DIM_SWITCHES_START_SWITCH_ON_CHOICE,
+    //        App_CanTx_GetPeriodicSignal_TORQUE_VECTORING_SWITCH(can_tx_interface));
 }
 
 // DIM-5
@@ -438,7 +440,7 @@ TEST_F(DimStateMachineTest, rgb_led_sequence_in_drive_state)
 TEST_F(DimStateMachineTest, dim_board_status_led_control_with_critical_error)
 {
     // Set any critical error and check that the DIM LED turns red
-//    App_SharedErrorTable_SetError(error_table, DIM_AIR_SHUTDOWN_DUMMY_AIR_SHUTDOWN, true);
+    //    App_SharedErrorTable_SetError(error_table, DIM_AIR_SHUTDOWN_DUMMY_AIR_SHUTDOWN, true);
     LetTimePass(state_machine, 10);
     ASSERT_EQ(1, turn_dim_status_led_red_fake.call_count);
 }
@@ -447,7 +449,7 @@ TEST_F(DimStateMachineTest, dim_board_status_led_control_with_critical_error)
 TEST_F(DimStateMachineTest, dim_board_status_led_control_with_warning)
 {
     // Set any non-critical error and check that the DIM LED turns blue
-//    App_SharedErrorTable_SetError(error_table, DIM_WARNING_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
+    //    App_SharedErrorTable_SetError(error_table, DIM_WARNING_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
     LetTimePass(state_machine, 10);
     ASSERT_EQ(1, turn_dim_status_led_blue_fake.call_count);
 }
@@ -465,8 +467,8 @@ TEST_F(DimStateMachineTest, dim_board_status_led_control_with_multiple_errors)
     // If the error table contains critical and non-critical errors
     // simultaneously, the critical error should take precedence and turn the
     // DIM LED red rather than blue
-//    App_SharedErrorTable_SetError(error_table, DIM_AIR_SHUTDOWN_DUMMY_AIR_SHUTDOWN, true);
-//    App_SharedErrorTable_SetError(error_table, DIM_WARNING_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
+    //    App_SharedErrorTable_SetError(error_table, DIM_AIR_SHUTDOWN_DUMMY_AIR_SHUTDOWN, true);
+    //    App_SharedErrorTable_SetError(error_table, DIM_WARNING_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
     LetTimePass(state_machine, 10);
     ASSERT_EQ(1, turn_dim_status_led_red_fake.call_count);
     ASSERT_EQ(0, turn_dim_status_led_blue_fake.call_count);
@@ -476,7 +478,7 @@ TEST_F(DimStateMachineTest, dim_board_status_led_control_with_multiple_errors)
 TEST_F(DimStateMachineTest, dcm_board_status_led_control_with_critical_error)
 {
     // Set any critical error and check that the DCM LED turns red
-//    App_SharedErrorTable_SetError(error_table, DCM_AIR_SHUTDOWN_MISSING_HEARTBEAT, true);
+    //    App_SharedErrorTable_SetError(error_table, DCM_AIR_SHUTDOWN_MISSING_HEARTBEAT, true);
     LetTimePass(state_machine, 10);
     ASSERT_EQ(1, turn_dcm_status_led_red_fake.call_count);
 }
@@ -485,7 +487,7 @@ TEST_F(DimStateMachineTest, dcm_board_status_led_control_with_critical_error)
 TEST_F(DimStateMachineTest, dcm_board_status_led_control_with_warning)
 {
     // Set any warning and check that the DCM LED turns blue
-//    App_SharedErrorTable_SetError(error_table, DCM_WARNING_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
+    //    App_SharedErrorTable_SetError(error_table, DCM_WARNING_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
     LetTimePass(state_machine, 10);
     ASSERT_EQ(1, turn_dcm_status_led_blue_fake.call_count);
 }
@@ -504,8 +506,8 @@ TEST_F(DimStateMachineTest, dcm_board_status_led_control_with_multiple_errors)
     // If the error table contains critical and non-critical errors
     // simultaneously, the critical error should take precedence and turn the
     // DCM LED red rather than blue
-//    App_SharedErrorTable_SetError(error_table, DCM_AIR_SHUTDOWN_MISSING_HEARTBEAT, true);
-//    App_SharedErrorTable_SetError(error_table, DCM_WARNING_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
+    //    App_SharedErrorTable_SetError(error_table, DCM_AIR_SHUTDOWN_MISSING_HEARTBEAT, true);
+    //    App_SharedErrorTable_SetError(error_table, DCM_WARNING_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
     LetTimePass(state_machine, 10);
     ASSERT_EQ(1, turn_dcm_status_led_red_fake.call_count);
     ASSERT_EQ(0, turn_dcm_status_led_blue_fake.call_count);
@@ -515,7 +517,7 @@ TEST_F(DimStateMachineTest, dcm_board_status_led_control_with_multiple_errors)
 TEST_F(DimStateMachineTest, fsm_board_status_led_control_with_critical_error)
 {
     // Set any critical error and check that the FSM LED turns red
-//    App_SharedErrorTable_SetError(error_table, FSM_AIR_SHUTDOWN_MISSING_HEARTBEAT, true);
+    //    App_SharedErrorTable_SetError(error_table, FSM_AIR_SHUTDOWN_MISSING_HEARTBEAT, true);
     LetTimePass(state_machine, 10);
     ASSERT_EQ(1, turn_fsm_status_led_red_fake.call_count);
 }
@@ -524,7 +526,7 @@ TEST_F(DimStateMachineTest, fsm_board_status_led_control_with_critical_error)
 TEST_F(DimStateMachineTest, fsm_board_status_led_control_with_warning)
 {
     // Set any warning and check that the FSM LED turns blue
-//    App_SharedErrorTable_SetError(error_table, FSM_WARNING_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
+    //    App_SharedErrorTable_SetError(error_table, FSM_WARNING_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
     LetTimePass(state_machine, 10);
     ASSERT_EQ(1, turn_fsm_status_led_blue_fake.call_count);
 }
@@ -543,8 +545,8 @@ TEST_F(DimStateMachineTest, fsm_board_status_led_control_with_multiple_errors)
     // If the error table contains critical and non-critical errors
     // simultaneously, the critical error should take precedence and turn the
     // FSM LED red rather than blue
-//    App_SharedErrorTable_SetError(error_table, FSM_AIR_SHUTDOWN_MISSING_HEARTBEAT, true);
-//    App_SharedErrorTable_SetError(error_table, FSM_WARNING_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
+    //    App_SharedErrorTable_SetError(error_table, FSM_AIR_SHUTDOWN_MISSING_HEARTBEAT, true);
+    //    App_SharedErrorTable_SetError(error_table, FSM_WARNING_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
     LetTimePass(state_machine, 10);
     ASSERT_EQ(1, turn_fsm_status_led_red_fake.call_count);
     ASSERT_EQ(0, turn_fsm_status_led_blue_fake.call_count);
@@ -570,7 +572,7 @@ TEST_F(DimStateMachineTest, bms_board_status_led_control_with_critical_error)
 TEST_F(DimStateMachineTest, pdm_board_status_led_control_with_critical_error)
 {
     // Set any critical error and check that the PDM LED turns red
-//    App_SharedErrorTable_SetError(error_table, PDM_AIR_SHUTDOWN_DUMMY_AIR_SHUTDOWN, true);
+    //    App_SharedErrorTable_SetError(error_table, PDM_AIR_SHUTDOWN_DUMMY_AIR_SHUTDOWN, true);
     LetTimePass(state_machine, 10);
     ASSERT_EQ(1, turn_pdm_status_led_red_fake.call_count);
 }
@@ -579,7 +581,7 @@ TEST_F(DimStateMachineTest, pdm_board_status_led_control_with_critical_error)
 TEST_F(DimStateMachineTest, pdm_board_status_led_control_with_warning)
 {
     // Set any warning and check that the PDM LED turns blue
-//    App_SharedErrorTable_SetError(error_table, PDM_WARNING_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
+    //    App_SharedErrorTable_SetError(error_table, PDM_WARNING_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
     LetTimePass(state_machine, 10);
     ASSERT_EQ(1, turn_pdm_status_led_blue_fake.call_count);
 }
@@ -598,8 +600,8 @@ TEST_F(DimStateMachineTest, pdm_board_status_led_control_with_multiple_errors)
     // If the error table contains critical and non-critical errors
     // simultaneously, the critical error should take precedence and turn the
     // PDM LED red rather than blue
-//    App_SharedErrorTable_SetError(error_table, PDM_AIR_SHUTDOWN_DUMMY_AIR_SHUTDOWN, true);
-//    App_SharedErrorTable_SetError(error_table, PDM_WARNING_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
+    //    App_SharedErrorTable_SetError(error_table, PDM_AIR_SHUTDOWN_DUMMY_AIR_SHUTDOWN, true);
+    //    App_SharedErrorTable_SetError(error_table, PDM_WARNING_STACK_WATERMARK_ABOVE_THRESHOLD_TASK1HZ, true);
     LetTimePass(state_machine, 10);
     ASSERT_EQ(1, turn_pdm_status_led_red_fake.call_count);
     ASSERT_EQ(0, turn_pdm_status_led_blue_fake.call_count);
@@ -608,9 +610,9 @@ TEST_F(DimStateMachineTest, pdm_board_status_led_control_with_multiple_errors)
 TEST_F(DimStateMachineTest, check_enum_ordering)
 {
     // Test to ensure BoardLeds enum matches Boards enum for use in DIM drive state
-//    ASSERT_EQ(DCM_LED, DCM);
-//    ASSERT_EQ(DIM_LED, DIM);
-//    ASSERT_EQ(FSM_LED, FSM);
-//    ASSERT_EQ(PDM_LED, PDM);
+    //    ASSERT_EQ(DCM_LED, DCM);
+    //    ASSERT_EQ(DIM_LED, DIM);
+    //    ASSERT_EQ(FSM_LED, FSM);
+    //    ASSERT_EQ(PDM_LED, PDM);
 }
 } // namespace StateMachineTest
