d67eb478abcc17cf346bfd221da0c6f249d4417a
Tomaz Zlindra <tomz0840@gmail.com>
2023-01-21 10:35:13 -0800

    PDM Efuse Jan 21st

---
+++ boards/PDM/Inc/App/App_RailMonitoring.h
@@ -0,0 +1,66 @@
+
+struct RailMonitoring;
+
+/**
+ *
+ * @param get_VBAT_voltage VBAT Voltage Value
+ * @param get__24V_ACC_voltage Accumulator Voltage Value
+ * @param get__22V_AUX_voltage Boost Voltage Value
+ * @return rail_monitor structure
+ */
+struct RailMonitoring *App_RailMonitoring_Create(
+        float (*get_VBAT_voltage)(void),
+        float (*get__24V_ACC_voltage)(void),
+        float (*get__22V_AUX_voltage)(void));
+
+void App_RailMonitoring_Destroy(struct RailMonitoring *rail_monitor);
+
+/**
+ *
+ * @param rail_monitor
+ * @return VBAT Voltage
+ */
+float App_RailMonitoring_Get_VBAT_Voltage(struct RailMonitoring *rail_monitor);
+
+/**
+ *
+ * @param rail_monitor
+ * @return ACC Voltage
+ */
+float App_RailMonitoring_Get__24V_ACC_Voltage(struct RailMonitoring *rail_monitor);
+
+/**
+ *
+ * @param rail_monitor
+ * @return AUX Voltage
+ */
+float App_RailMonitoring_Get__22V_AUX_Voltage(struct RailMonitoring *rail_monitor);
+
+/**
+ * Local function checking if voltage is too low
+ * @param value
+ * @param min_value
+ * @return true if too low, false if not
+ */
+bool App_RailMonitoring_VoltageLowCheck(float value, float min_value);
+
+/**
+ * Local function checking if voltage is too high
+ * @param value
+ * @param max_value
+ * @return true if too high, false if not
+ */
+bool App_RailMonitoring_VoltageHighCheck(float value, float max_value);
+
+// Functions below check if each voltage is too low or too high
+bool App_RailMonitoring_VBAT_VoltageLowCheck(struct RailMonitoring *rail_monitor);
+bool App_RailMonitoring_VBAT_VoltageHighCheck(struct RailMonitoring *rail_monitor);
+bool App_RailMonitoring__24V_ACC_VoltageLowCheck(struct RailMonitoring *rail_monitor);
+bool App_RailMonitoring__24V_ACC_VoltageHighCheck(struct RailMonitoring *rail_monitor);
+bool App_RailMonitoring__22V_AUX_VoltageLowCheck(struct RailMonitoring *rail_monitor);
+bool App_RailMonitoring__22V_AUX_VoltageHighCheck(struct RailMonitoring *rail_monitor);
+
+// Functions below check if each voltage is critical or not
+bool App_RailMonitoring_VBAT_VoltageCriticalCheck(struct RailMonitoring *rail_monitor);
+bool App_RailMonitoring__24V_ACC_VoltageCriticalCheck(struct RailMonitoring *rail_monitor);
+bool App_RailMonitoring__22V_AUX_VoltageCriticalCheck(struct RailMonitoring *rail_monitor);
--- boards/PDM/Src/App/App_Efuse.c
+++ boards/PDM/Src/App/App_Efuse.c
@@ -2,16 +2,147 @@
 
 struct Efuse
 {
-    void (*enable_channel_0)(void);
-    void (*disable_channel_0)(void);
-    void (*enable_channel_1)(void);
-    void (*disable_channel_1)(void);
-    ExitCode (*get_status)(enum Efuse_Status *status);
-    ExitCode (*get_channel_0_faults)(enum Efuse_Fault *fault);
-    ExitCode (*get_channel_1_faults)(enum Efuse_Fault *fault);
-    bool (*is_in_fault_mode)(void);
-    bool (*is_in_failsafe_mode)(void);
-    void (*delatch_faults)(void);
-    float (*get_channel_0_current)(void);
-    float (*get_channel_1_current)(void);
+    struct Io_Efuse (*io_efuse);
+    void (*enable_channel_0)(struct Io_Efuse);
+    void (*disable_channel_0)(struct Io_Efuse);
+    void (*enable_channel_1)(struct Io_Efuse);
+    void (*disable_channel_1)(struct Io_Efuse);
+    bool (*is_channel_0_enabled)(struct Io_Efuse);
+    bool (*is_channel_1_enabled)(struct Io_Efuse);
+    void (*stdby_reset)(struct Io_Efuse);
+    float (*get_channel_0_current)(struct Io_Efuse);
+    float (*get_channel_1_current)(struct Io_Efuse);
+    float (*channel_0_min_current);
+    float (*channel_0_max_current);
+    float (*channel_1_min_current);
+    float (*channel_1_max_current);
 };
+
+struct Efuse *App_Efuse_Create(
+        struct Io_Efuse (*io_efuse),
+        void (*enable_channel_0)(struct Io_Efuse),
+        void (*disable_channel_0)(struct Io_Efuse),
+        void (*enable_channel_1)(struct Io_Efuse),
+        void (*disable_channel_1)(struct Io_Efuse),
+        bool (*is_channel_0_enabled)(struct Io_Efuse),
+        bool (*is_channel_1_enabled)(struct Io_Efuse),
+        void (*stdby_reset)(struct Io_Efuse),
+        float (*get_channel_0_current)(struct Io_Efuse),
+        float (*get_channel_1_current)(struct Io_Efuse),
+        float (*channel_0_min_current),
+        float (*channel_0_max_current),
+        float (*channel_1_min_current),
+        float (*channel_1_max_current))
+{
+    struct Efuse *efuse = malloc(sizeof(struct Efuse));
+    assert(efuse != NULL);
+
+    efuse->io_efuse              = io_efuse;
+    efuse->enable_channel_0      = enable_channel_0;
+    efuse->disable_channel_0     = disable_channel_0;
+    efuse->enable_channel_1      = enable_channel_1;
+    efuse->disable_channel_1     = disable_channel_1;
+    efuse->is_channel_0_enabled  = is_channel_0_enabled;
+    efuse->is_channel_1_enabled  = is_channel_1_enabled;
+    efuse->stdby_reset           = stdby_reset;
+    efuse->get_channel_0_current = get_channel_0_current;
+    efuse->get_channel_1_current = get_channel_1_current;
+    efuse->channel_0_min_current = channel_0_min_current;
+    efuse->channel_0_max_current = channel_0_max_current;
+    efuse->channel_1_min_current = channel_1_min_current;
+    efuse->channel_1_max_current = channel_1_max_current;
+
+    return efuse;
+}
+
+void App_Efuse_Destroy(struct Efuse *efuse)
+{
+    free(efuse);
+}
+
+void App_Efuse_EnableChannel0(struct Efuse *efuse)
+{
+    efuse->enable_channel_0(*efuse->io_efuse);
+}
+
+void App_Efuse_DisableChannel0(struct Efuse *efuse)
+{
+    efuse->disable_channel_0(*efuse->io_efuse);
+}
+
+void App_Efuse_EnableChannel1(struct Efuse *efuse)
+{
+    efuse->enable_channel_1(*efuse->io_efuse);
+}
+
+void App_Efuse_DisableChannel1(struct Efuse *efuse)
+{
+    efuse->disable_channel_1(*efuse->io_efuse);
+}
+
+bool App_Efuse_IsChannel0Enabled(struct Efuse *efuse)
+{
+    return efuse->is_channel_0_enabled(*efuse->io_efuse);
+}
+
+bool App_Efuse_IsChannel1Enabled(struct Efuse *efuse)
+{
+    return efuse->is_channel_1_enabled(*efuse->io_efuse);
+}
+
+void App_Efuse_StandbyReset(struct Efuse *efuse)
+{
+    efuse->stdby_reset(*efuse->io_efuse);
+}
+
+float App_Efuse_GetChannel0Current(struct Efuse *efuse)
+{
+    return efuse->get_channel_0_current(*efuse->io_efuse);
+}
+
+float App_Efuse_GetChannel1Current(struct Efuse *efuse)
+{
+    return efuse->get_channel_1_current(*efuse->io_efuse);
+}
+
+bool App_Efuse_CurrentLowCheck(float value, float min_value)
+{
+    if (value >= min_value)
+        return false;
+    return true;
+}
+
+bool App_Efuse_CurrentHighCheck(float value, float max_value)
+{
+    if (value <= max_value)
+        return false;
+    return true;
+}
+
+bool App_Efuse_Channel0_CurrentLowCheck(struct Efuse *efuse)
+{
+    if (App_Efuse_CurrentLowCheck(efuse->get_channel_0_current(*efuse->io_efuse), efuse->channel_0_min_current) == true)
+        return true;
+    return false;
+}
+
+bool App_Efuse_Channel0_CurrentHighCheck(struct Efuse *efuse)
+{
+    if (App_Efuse_CurrentHighCheck(efuse->get_channel_0_current(*efuse->io_efuse), efuse->channel_0_max_current) == true)
+        return true;
+    return false;
+}
+
+bool App_Efuse_Channel1_CurrentLowCheck(struct Efuse *efuse)
+{
+    if (App_Efuse_CurrentLowCheck(efuse->get_channel_1_current(*efuse->io_efuse), efuse->channel_1_min_current) == true)
+        return true;
+    return false;
+}
+
+bool App_Efuse_Channel1_CurrentHighCheck(struct Efuse *efuse)
+{
+    if (App_Efuse_CurrentHighCheck(efuse->get_channel_1_current(*efuse->io_efuse), efuse->channel_1_max_current) == true)
+        return true;
+    return false;
+}
