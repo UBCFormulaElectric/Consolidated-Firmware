6d538cf918be4037032bf740eb73e25e3cf82e7e
Tomaz Zlindra <tomz0840@gmail.com>
2023-01-21 10:48:14 -0800

    Jan 21st 2.0

--- boards/PDM/Inc/Io/Io_Efuse.h
+++ boards/PDM/Inc/Io/Io_Efuse.h
@@ -1,38 +1,72 @@
-
- 
 #pragma once
 
-#include <stdint.h>
-#include <stdbool.h>
-#include "main.h"
-#include "App_SharedExitCode.h"
-#include "../../Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_spi.h"
-#include "../../Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f302xc.h"
-
 #include <assert.h>
-#include <math.h>
-#include <stdlib.h>
+#include <stdbool.h>
 #include "Io_Efuse.h"
-#include "configs/Io_EfuseConfig.h"
 
 struct Io_Efuse;
-
+/**
+ *
+ * @return io_efuse structure
+ */
 struct Io_Efuse *Io_Efuse_Create(
 
 );
 
+/**
+ * Enables Channel_0
+ * @param io_efuse
+ */
 void Io_Efuse_EnableChannel0(struct Io_Efuse *io_efuse);
 
+/**
+ * Disables Channel_0
+ * @param io_efuse
+ */
 void Io_Efuse_DisableChannel0(struct Io_Efuse *io_efuse);
 
+/**
+ * Enables Channel_1
+ * @param io_efuse
+ */
 void Io_Efuse_EnableChannel1(struct Io_Efuse *io_efuse);
 
+/**
+ * Disables Channel_1
+ * @param io_efuse
+ */
 void Io_Efuse_DisableChannel1(struct Io_Efuse *io_efuse);
-void Io_Efuse_IsChannel0Enabled(struct Io_Efuse *io_efuse);
 
-void Io_Efuse_IsChannel1Enabled(struct Io_Efuse *io_efuse);
+/**
+ * Checks if Channel_0 is enabled or not
+ * @param io_efuse
+ * return true if enabled, false if not
+ */
+bool Io_Efuse_IsChannel0Enabled(struct Io_Efuse *io_efuse);
+
+/**
+ * Checks if Channel_1 is enabled or not
+ * @param io_efuse
+ * @return true if enabled, false if not
+ */
+bool Io_Efuse_IsChannel1Enabled(struct Io_Efuse *io_efuse);
+
+/**
+ * Puts a current through the STBY_RST pin to attempt to reset the Efuse after a failure
+ * @param io_efuse
+ */
 void Io_Efuse_StandbyReset(struct Io_Efuse *io_efuse);
 
+/**
+ *
+ * @param io_efuse
+ * @return Channel_0 Current Value
+ */
 float Io_Efuse_GetChannel0Current(struct Io_Efuse *io_efuse);
 
+/**
+ *
+ * @param io_efuse
+ * @return Channel_1 Current Value
+ */
 float Io_Efuse_GetChannel1Current(struct Io_Efuse *io_efuse);
