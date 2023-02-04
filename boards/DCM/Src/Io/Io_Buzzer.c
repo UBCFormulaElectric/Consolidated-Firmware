88e79937a117d384718a6ce6acaa2162991c79dc
philam2001 <lamyenphi14@gmail.com>
2023-02-04 11:13:27 -0800

    Change F3 to F4 in CMakeLists.txt and change io to match gpio names

--- boards/DCM/Src/Io/Io_Buzzer.c
+++ boards/DCM/Src/Io/Io_Buzzer.c
@@ -4,10 +4,10 @@
 
 void Io_Buzzer_TurnOn(void)
 {
-    HAL_GPIO_WritePin(BUZZER_EN_GPIO_Port, BUZZER_EN_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(BUZZER_EN_3V3_GPIO_Port, BUZZER_EN_3V3_Pin, GPIO_PIN_SET);
 }
 
 void Io_Buzzer_TurnOff(void)
 {
-    HAL_GPIO_WritePin(BUZZER_EN_GPIO_Port, BUZZER_EN_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(BUZZER_EN_3V3_GPIO_Port, BUZZER_EN_3V3_Pin, GPIO_PIN_RESET);
 }
