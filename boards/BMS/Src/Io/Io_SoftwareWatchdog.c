ccc9c1eed424e6e2a9f071b1465ff3511abc7d69
will-chaba <wchaba@alumni.ubc.ca>
2023-02-11 11:27:00 -0800

    Move Warnings to Aperiodic Messages + add IMD condition message

--- boards/BMS/Src/Io/Io_SoftwareWatchdog.c
+++ boards/BMS/Src/Io/Io_SoftwareWatchdog.c
@@ -1,6 +1,8 @@
 #include "Io_Hal.h"
 #include <string.h>
 #include "App_CanTx.h"
+#include "Io_CanTx.h"
+#include "App_CanAlerts.h"
 #include "Io_SoftwareWatchdog.h"
 #include "Io_SharedMacros.h"
 
@@ -15,11 +17,8 @@ void Io_SoftwareWatchdog_TimeoutCallback(SoftwareWatchdogHandle_t watchdog)
 {
     BREAK_IF_DEBUGGER_CONNECTED();
 
-    App_CanTx_BMS_Warnings_WatchdogTimeout_Set(true);
-
-    // TODO: JSON2CAN APERIODIC MSGS
-    // struct CanMsgs_bms_watchdog_timeout_t payload;
-    // memcpy(&payload.task_name, Io_SharedSoftwareWatchdog_GetName(watchdog), sizeof(payload.task_name));
-
-    // App_CanTx_SendNonPeriodicMsg_BMS_WATCHDOG_TIMEOUT(_can_tx, &payload);
+    const uint8_t watchdog_id = Io_SharedSoftwareWatchdog_GetTaskId(watchdog);
+    App_CanAlerts_SetAlert(BMS_ALERT_WATCHDOG_TIMEOUT, true);
+    App_CanTx_BMS_WatchdogTimeout_TaskName_Set((RtosTaskName)watchdog_id);
+    App_CanTx_BMS_WatchdogTimeout_SendAperiodic();
 }
