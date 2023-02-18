89ac90854f2fd2f424150f6d216c598068c94d86
halab1t <laura.romer@gmail.com>
2023-02-18 11:12:39 -0800

    new folder and constants moved

--- boards/DCM/Inc/App/TorqueVectoring/TorqueVectoring_constants.h
+++ boards/DCM/Inc/App/TorqueVectoring/TorqueVectoring_constants.h
@@ -9,5 +9,9 @@
 #define FRONT_AXLE_LENGTH_m
 #define POWER_TO_TORQUE_CONVERSION_FACTOR  9550 //as specified in the manual for the emrax 188 motors
 #define HARD_POWER_LIMIT_kW 80
+#define MOTOR_TEMP_CUTOFF_c 90 //in degrees celsius
+#define MOTOR_TEMP_POWER_DECREMENTING_RATIO 80/30 //amount to decrease max power
+                                                // by for every degree celsius over motor cutoff temp (we got this number by looking at the emrax 188 manual)
+#define SMALL_EPSILON .000001 //to avoid divide by zero error
 
 #endif //FORMULA_ELECTRIC_FIRMWARE_TORQUEVECTORING_CONSTANTS_H
