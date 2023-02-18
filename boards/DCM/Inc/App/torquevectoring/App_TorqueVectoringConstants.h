1be1b0a02fa670956fbbd2d9e4ccd3b3f01c9184
halab1t <laura.romer@gmail.com>
2023-02-18 14:07:31 -0800

    changes

--- boards/DCM/Inc/App/torquevectoring/App_TorqueVectoringConstants.h
+++ boards/DCM/Inc/App/torquevectoring/App_TorqueVectoringConstants.h
@@ -8,9 +8,9 @@
 #define WHEEL_RADIUS_m
 #define FRONT_AXLE_LENGTH_m
 #define POWER_TO_TORQUE_CONVERSION_FACTOR  9550 //as specified in the manual for the emrax 188 motors
-#define HARD_POWER_LIMIT_kW 80
+#define POWER_LIMIT_CAR_kW 80 //max power we can use as specifiec by FSAE rules
 #define MOTOR_TEMP_CUTOFF_c 90 //in degrees celsius
-#define MOTOR_TEMP_POWER_DECREMENTING_RATIO 80/30 //amount to decrease max power
+#define MOTOR_TEMP_POWER_DECREMENTING_RATIO 80.0/30.0 //amount to decrease max power
                                                 // by for every degree celsius over motor cutoff temp (we got this number by looking at the emrax 188 manual)
 #define SMALL_EPSILON .000001 //to avoid divide by zero error
 
