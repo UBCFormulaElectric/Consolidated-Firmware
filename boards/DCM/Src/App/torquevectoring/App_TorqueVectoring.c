02aa8997047560f2a5ef9232ac2915cfcc37976c
halab1t <laura.romer@gmail.com>
2023-02-18 15:01:09 -0800

    can_rx

--- boards/DCM/Src/App/torquevectoring/App_TorqueVectoring.c
+++ boards/DCM/Src/App/torquevectoring/App_TorqueVectoring.c
@@ -2,8 +2,20 @@
 // Created by formulae on 18/02/23.
 //
 #include "torquevectoring/App_TorqueVectoringConstants.h"
+#include "torquevectoring//App_TorqueVectoring.h"
+#include "App_CanRx.h"
 
 
-int x = 1;
+static PowerLimiting_Inputs power_limiting_inputs;
+static float estimated_power_limit;
+static float actual_power_limit;
+static ActiveDifferential_Inputs active_differential_inputs;
+static ActiveDifferential_Outputs active_differential_outputs;
+static TractionControl_Inputs traction_control_inputs;
+static TractionControl_Outputs traction_control_outputs;
+
+void App_TorqueVectoring_Run(void){
+    const float bms_available_power = App_CanRx_BMS_AvailablePower_AvailablePower_Get();
+}
 
 
