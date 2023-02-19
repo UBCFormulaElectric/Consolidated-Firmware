bb753a196e6b3c5f6477393e021f0ae53e7ac95d
Alex Koen <akoen@mailbox.org>
2023-02-18 16:54:30 -0800

    Fix build errors casting

--- boards/DCM/Src/App/torquevectoring/App_TorqueVectoring.c
+++ boards/DCM/Src/App/torquevectoring/App_TorqueVectoring.c
@@ -2,17 +2,15 @@
 #include "torquevectoring/App_TorqueVectoringConstants.h"
 #include "App_CanRx.h"
 
-
-static PowerLimiting_Inputs power_limiting_inputs;
-static float estimated_power_limit;
-static float actual_power_limit;
-static ActiveDifferential_Inputs active_differential_inputs;
+static PowerLimiting_Inputs       power_limiting_inputs;
+static float                      estimated_power_limit;
+static float                      actual_power_limit;
+static ActiveDifferential_Inputs  active_differential_inputs;
 static ActiveDifferential_Outputs active_differential_outputs;
-static TractionControl_Inputs traction_control_inputs;
-static TractionControl_Outputs traction_control_outputs;
+static TractionControl_Inputs     traction_control_inputs;
+static TractionControl_Outputs    traction_control_outputs;
 
-void App_TorqueVectoring_Run(void){
+void App_TorqueVectoring_Run(void)
+{
     const float bms_available_power = App_CanRx_BMS_AvailablePower_AvailablePower_Get();
 }
-
-
