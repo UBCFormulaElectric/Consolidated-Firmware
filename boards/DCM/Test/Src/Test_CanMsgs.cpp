26cee18bb9fea2ab85021ab93579ab05eac7427f
philam2001 <lamyenphi14@gmail.com>
2023-02-21 15:12:59 -0800

    Change time to 1 Hz

--- boards/DCM/Test/Src/Test_CanMsgs.cpp
+++ boards/DCM/Test/Src/Test_CanMsgs.cpp
@@ -8,11 +8,11 @@ extern "C"
 // DCM-21
 TEST(CanMsgsTest, state_machine_message_frequency)
 {
-    ASSERT_GE(HZ_TO_MS(10), CAN_MSG_DCM_VITALS_CYCLE_TIME_MS);
+    ASSERT_GE(HZ_TO_MS(1), CAN_MSG_DCM_VITALS_CYCLE_TIME_MS);
 }
 
 // DCM-2
 TEST(CanMsgsTest, heartbeat_message_frequency)
 {
-    ASSERT_GE(HZ_TO_MS(10), CAN_MSG_DCM_VITALS_CYCLE_TIME_MS);
+    ASSERT_GE(HZ_TO_MS(1), CAN_MSG_DCM_VITALS_CYCLE_TIME_MS);
 }
