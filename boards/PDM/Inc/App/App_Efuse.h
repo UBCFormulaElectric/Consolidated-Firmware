6b7c55dcf8782fc756612d952e5a6e954ce43a6e
Tomaz Zlindra <tomz0840@gmail.com>
2023-03-04 13:17:16 -0800

    March 4th, 13:17

--- boards/PDM/Inc/App/App_Efuse.h
+++ boards/PDM/Inc/App/App_Efuse.h
@@ -42,38 +42,38 @@ struct Efuse *App_Efuse_Create(
 void App_Efuse_Destroy(struct Efuse *efuse);
 
 /**
- * Function to enable channel_0
+ * Function to enable channel_0 of the PDM Efuse
  * @param efuse App_Efuse structure
  */
 void App_Efuse_EnableChannel0(struct Efuse *efuse);
 
 /**
- * Function to disable channel_0
+ * Function to disable channel_0 of the PDM Efuse
  * @param efuse App_Efuse structure
  */
 void App_Efuse_DisableChannel0(struct Efuse *efuse);
 
 /**
- * Function to enable channel_1
+ * Function to enable channel_1 of the PDM Efuse
  * @param efuse App_Efuse structure
  */
 void App_Efuse_EnableChannel1(struct Efuse *efuse);
 
 /**
- * Function to disable channel_1
+ * Function to disable channel_1 of the PDM Efuse
  * @param efuse App_Efuse structure
  */
 void App_Efuse_DisableChannel1(struct Efuse *efuse);
 
 /**
- * Function that checks if channel_0 is enabled
+ * Function that checks if channel_0 is enabled of the PDM Efuse
  * @param efuse
  * @return true if channel_0 is enabled, false otherwise
  */
 bool App_Efuse_IsChannel0Enabled(struct Efuse *efuse);
 
 /**
- * Function that checks if channel_1 is enabled
+ * Function that checks if channel_1 is enabled of the PDM Efuse
  * @param efuse
  * @return true if channel_1 is enabled, false otherwise
  */
@@ -86,57 +86,59 @@ bool App_Efuse_IsChannel1Enabled(struct Efuse *efuse);
 void App_Efuse_StandbyReset(struct Efuse *efuse);
 
 /**
+ * Gets the current value of channel_0 of the PDM Efuse in Amps
  * @param efuse
  * @return The current value of channel_0
  */
 float App_Efuse_GetChannel0Current(struct Efuse *efuse);
 
 /**
+ * Gets the current value of channel_1 of the PDM Efuse in Amps
  * @param efuse App_Efuse structure
  * @return The current value of channel_1
  */
 float App_Efuse_GetChannel1Current(struct Efuse *efuse);
 
 /**
- *
+ * Checks if the current of the channel_0 Efuse is too low
  * @param efuse
  * @return true if current too low, false if good (channel0)
  */
 bool App_Efuse_Channel0_CurrentLowCheck(struct Efuse *efuse);
 
 /**
- *
+ * Checks if the current of the channel_0 Efuse is too high
  * @param efuse
  * @return true if current too high, false if good (channel0)
  */
 bool App_Efuse_Channel0_CurrentHighCheck(struct Efuse *efuse);
 
 /**
- *
+ * Checks if the current of the channel_1 Efuse is too low
  * @param efuse
  * @return true if current too high, false if good (channel0)
  */
 bool App_Efuse_Channel1_CurrentLowCheck(struct Efuse *efuse);
 
 /**
- *
+ * Checks if the current of the channel_1 Efuse is too high
  * @param efuse
  * @return true if current too high, false if good (channel1)
  */
 bool App_Efuse_Channel1_CurrentHighCheck(struct Efuse *efuse);
 
 /**
- * If current == 0 or current > max current, then it will go through this fault procedure function (channel0)
+ * If current too low & enabled or current > max current, then it will go through this fault procedure function (channel0)
  * @param efuse
  * @param max_attempts max number of attempts allowed before faulting
- * @return 0 if fine, 1 if timer is still running, 2 if channel failed
+ * @return 0 if fault, 1 if fine
  */
-int App_Efuse_FaultProcedure_Channel0(struct Efuse *efuse, int max_attempts);
+int App_Efuse_FaultProcedureChannel0(struct Efuse *efuse, int max_attempts);
 
 /**
- * If current == 0 or current > max current, then it will go through this fault procedure function (channel1)
+ * If current too low & enabled or current > max current, then it will go through this fault procedure function (channel1)
  * @param efuse
  * @param max_attempts max number of attempts allowed before faulting
- * @return 0 if fine, 1 if timer is still running, 2 if channel failed
+ * @return 0 if fault, 1 if fine
  */
-int App_Efuse_FaultProcedure_Channel1(struct Efuse *efuse, int max_attempts);
+int App_Efuse_FaultProcedureChannel1(struct Efuse *efuse, int max_attempts);
