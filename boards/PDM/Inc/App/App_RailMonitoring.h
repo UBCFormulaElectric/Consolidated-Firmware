a730cb37705dc7588706a53696b015ac4500575f
Tomaz Zlindra <tomz0840@gmail.com>
2023-03-04 13:17:16 -0800

    March 4th, 13:17

--- boards/PDM/Inc/App/App_RailMonitoring.h
+++ boards/PDM/Inc/App/App_RailMonitoring.h
@@ -13,88 +13,65 @@ struct RailMonitoring *App_RailMonitoring_Create(
     float (*get__24V_ACC_voltage)(void),
     float (*get__22V_AUX_voltage)(void));
 
-void App_RailMonitoring_Destroy(struct RailMonitoring *rail_monitor);
+/**
+ * Gets the VBAT Voltage from the PDM
+ * @param rail_monitor
+ * @return VBAT Voltage Value from the PDM
+ */
+float App_RailMonitoring_GetVbatVoltage(struct RailMonitoring *rail_monitor);
 
 /**
- *
+ * Gets the Accumulator Voltage from the PDM
  * @param rail_monitor
- * @return VBAT Voltage
+ * @return Accumulator Voltage Value from the PDM
  */
-float App_RailMonitoring_Get_VBAT_Voltage(struct RailMonitoring *rail_monitor);
+float App_RailMonitoring_Get24VAccumulatorVoltage(struct RailMonitoring *rail_monitor);
 
 /**
- *
+ * Gets the AUX Voltage from the PDM
  * @param rail_monitor
- * @return ACC Voltage
+ * @return AUX Voltage Value from the PDM
  */
-float App_RailMonitoring_Get__24V_ACC_Voltage(struct RailMonitoring *rail_monitor);
+float App_RailMonitoring_Get22VAuxiliaryVoltage(struct RailMonitoring *rail_monitor);
 
 /**
- *
+ * Checks if the VBAT Voltage from the PDM is too low
  * @param rail_monitor
- * @return AUX Voltage
+ * @return true if voltage too low, false otherwise
  */
-float App_RailMonitoring_Get__22V_AUX_Voltage(struct RailMonitoring *rail_monitor);
+bool App_RailMonitoring_VbatVoltageLowCheck(struct RailMonitoring *rail_monitor);
 
 /**
- *
+ * Checks if the VBAT Voltage from the PDM is too high
  * @param rail_monitor
- * @return true if VBAT voltage too low
+ * @return true if voltage too high, false otherwise
  */
-bool App_RailMonitoring_VBAT_VoltageLowCheck(struct RailMonitoring *rail_monitor);
+bool App_RailMonitoring_VbatVoltageHighCheck(struct RailMonitoring *rail_monitor);
 
 /**
- *
+ * Checks if the Accumulator Voltage from the PDM is too low
  * @param rail_monitor
- * @return true if VBAT voltage too high
+ * @return true if voltage too low, false otherwise
  */
-bool App_RailMonitoring_VBAT_VoltageHighCheck(struct RailMonitoring *rail_monitor);
+bool App_RailMonitoring_24VAccumulatorVoltageLowCheck(struct RailMonitoring *rail_monitor);
 
 /**
- *
+ * Checks if the Accumulator Voltage from the PDM is too high
  * @param rail_monitor
- * @return true if VBAT voltage critically low
+ * @return true if voltage too high, false otherwise
  */
-bool App_RailMonitoring_VBAT_VoltageCriticalCheck(struct RailMonitoring *rail_monitor);
+bool App_RailMonitoring_24VAccumulatorVoltageHighCheck(struct RailMonitoring *rail_monitor);
 
 /**
- *
+ * Checks if the AUX Voltage from the PDM is too low
  * @param rail_monitor
- * @return true if accumulator voltage too low
+ * @return true if voltage too low, false otherwise
  */
-bool App_RailMonitoring__24V_ACC_VoltageLowCheck(struct RailMonitoring *rail_monitor);
+bool App_RailMonitoring_22VAuxiliaryVoltageLowCheck(struct RailMonitoring *rail_monitor);
 
 /**
- *
+ * Checks if the AUX Voltage from the PDM is too high
  * @param rail_monitor
- * @return true if accumulator voltage too high
+ * @return true if voltage too high, false otherwise
  */
-bool App_RailMonitoring__24V_ACC_VoltageHighCheck(struct RailMonitoring *rail_monitor);
-
-/**
- *
- * @param rail_monitor
- * @return true if accumulator voltage critically low
- */
-bool App_RailMonitoring__24V_ACC_VoltageCriticalCheck(struct RailMonitoring *rail_monitor);
-
-/**
- *
- * @param rail_monitor
- * @return true if auxiliary voltage too low
- */
-bool App_RailMonitoring__22V_AUX_VoltageLowCheck(struct RailMonitoring *rail_monitor);
-
-/**
- *
- * @param rail_monitor
- * @return true if auxiliary voltage too high
- */
-bool App_RailMonitoring__22V_AUX_VoltageHighCheck(struct RailMonitoring *rail_monitor);
-
-/**
- *
- * @param rail_monitor
- * @return true if auxiliary voltage critically low
- */
-bool App_RailMonitoring__22V_AUX_VoltageCriticalCheck(struct RailMonitoring *rail_monitor);
+bool App_RailMonitoring_22VAuxiliaryVoltageHighCheck(struct RailMonitoring *rail_monitor);
--- boards/PDM/Src/App/App_Efuse.c
+++ boards/PDM/Src/App/App_Efuse.c
@@ -1,8 +1,6 @@
 #include "App_Efuse.h"
 #include "App_Timer.h"
 
-#define EFUSE_CURRENT_THRESHOLD (0.1f)
-
 struct Efuse
 {
     struct Io_Efuse *io_efuse;
@@ -19,6 +17,13 @@ struct Efuse
     float channel_0_max_current;
     float channel_1_min_current;
     float channel_1_max_current;
+    TimerChannel efuse_timer_channel_0;
+    TimerChannel efuse_timer_channel_1;
+    int num_attempts_channel_0;
+    int num_attempts_channel_1;
+    bool has_timer_started_channel_0;
+    bool has_timer_started_channel_1;
+
 };
 
 struct Efuse *App_Efuse_Create(
@@ -54,6 +59,15 @@ struct Efuse *App_Efuse_Create(
     efuse->channel_0_max_current = channel_0_max_current;
     efuse->channel_1_min_current = channel_1_min_current;
     efuse->channel_1_max_current = channel_1_max_current;
+
+    efuse->num_attempts_channel_0 = 0;
+    efuse->num_attempts_channel_1 = 0;
+    efuse->has_timer_started_channel_0 = false;
+    efuse->has_timer_started_channel_1 = false;
+
+    App_Timer_InitTimer(&efuse->efuse_timer_channel_0, 1000);
+    App_Timer_InitTimer(&efuse->efuse_timer_channel_1, 1000);
+
 
     return efuse;
 }
@@ -128,80 +142,69 @@ bool App_Efuse_Channel1_CurrentHighCheck(struct Efuse *efuse)
     return efuse->get_channel_1_current(efuse->io_efuse) > efuse->channel_1_max_current;
 }
 
-int App_Efuse_FaultProcedure_Channel0(struct Efuse *efuse, int max_attempts)
+int App_Efuse_FaultProcedureChannel0(struct Efuse *efuse, int max_attempts)
 {
-    static TimerChannel *efuse_timer;
-    static int           num_attempts      = 0;
-    static bool          has_timer_started = false;
-    App_Timer_InitTimer(efuse_timer, 1000);
-
-    if ((App_Efuse_GetChannel0Current(efuse) <= EFUSE_CURRENT_THRESHOLD && App_Efuse_IsChannel0Enabled(efuse)) ||
+    if ((App_Efuse_Channel0_CurrentLowCheck(efuse) && App_Efuse_IsChannel0Enabled(efuse)) ||
         App_Efuse_Channel0_CurrentHighCheck(efuse))
     {
-        if (!has_timer_started)
+        if (!efuse->has_timer_started_channel_0)
         {
-            App_Timer_Restart(efuse_timer);
-            has_timer_started = true;
+            App_Timer_Restart(&efuse->efuse_timer_channel_0);
+            efuse->has_timer_started_channel_0 = true;
         }
     }
     else
     {
-        App_Timer_Stop(efuse_timer);
-        has_timer_started = false;
-        num_attempts      = 0;
-        return 0; // fine
+        App_Timer_Stop(&efuse->efuse_timer_channel_0);
+        efuse->has_timer_started_channel_0 = false;
+        efuse->num_attempts_channel_0      = 0;
     }
 
-    if (has_timer_started && App_Timer_UpdateAndGetState((efuse_timer)) == TIMER_STATE_EXPIRED)
+    if (efuse->has_timer_started_channel_0 && App_Timer_UpdateAndGetState(&efuse->efuse_timer_channel_0) == TIMER_STATE_EXPIRED)
     {
         App_Efuse_StandbyReset(efuse);
-        num_attempts++;
+        efuse->num_attempts_channel_0++;
     }
-    if (num_attempts >= max_attempts)
+    if (efuse->num_attempts_channel_0 >= max_attempts)
     {
-        App_Timer_Stop(efuse_timer);
-        has_timer_started = false;
-        return 2; // channel failed
+        App_Timer_Stop(&efuse->efuse_timer_channel_0);
+        efuse->has_timer_started_channel_0 = false;
+        return 0; // channel failed
     }
 
-    return 1; // unknown status
+    return 1; // channel fine
 }
 
-int App_Efuse_FaultProcedure_Channel1(struct Efuse *efuse, int max_attempts)
+int App_Efuse_FaultProcedureChannel1(struct Efuse *efuse, int max_attempts)
 {
-    static TimerChannel *efuse_timer;
-    static int           num_attempts      = 0;
-    static bool          has_timer_started = false;
-    App_Timer_InitTimer(efuse_timer, 1000);
 
-    if ((App_Efuse_GetChannel1Current(efuse) <= EFUSE_CURRENT_THRESHOLD && App_Efuse_IsChannel1Enabled(efuse)) ||
+    if ((App_Efuse_Channel1_CurrentLowCheck(efuse) && App_Efuse_IsChannel1Enabled(efuse)) ||
         App_Efuse_Channel1_CurrentHighCheck(efuse))
     {
-        if (!has_timer_started)
+        if (!efuse->has_timer_started_channel_1)
         {
-            App_Timer_Restart(efuse_timer);
-            has_timer_started = true;
+            App_Timer_Restart(&efuse->efuse_timer_channel_1);
+            efuse->has_timer_started_channel_1 = true;
         }
     }
     else
     {
-        App_Timer_Stop(efuse_timer);
-        has_timer_started = false;
-        num_attempts      = 0;
-        return 0;
+        App_Timer_Stop(&efuse->efuse_timer_channel_1);
+        efuse->has_timer_started_channel_1 = false;
+        efuse->num_attempts_channel_1      = 0;
     }
 
-    if (has_timer_started && App_Timer_UpdateAndGetState((efuse_timer)) == TIMER_STATE_EXPIRED)
+    if (efuse->has_timer_started_channel_1 && App_Timer_UpdateAndGetState(&efuse->efuse_timer_channel_1) == TIMER_STATE_EXPIRED)
     {
         App_Efuse_StandbyReset(efuse);
-        num_attempts++;
+        efuse->num_attempts_channel_1++;
     }
-    if (num_attempts >= max_attempts)
+    if (efuse->num_attempts_channel_1 >= max_attempts)
     {
-        App_Timer_Stop(efuse_timer);
-        has_timer_started = false;
-        return 2;
+        App_Timer_Stop(&efuse->efuse_timer_channel_1);
+        efuse->has_timer_started_channel_1 = false;
+        return 0; // channel failed
     }
 
-    return 1; // unknown status
+    return 1; // fine
 }
