3aaafda3cb5f8d040b189ad44a3c7b5cd02f0395
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2023-03-06 12:04:12 -0800

    BMS changes for first flash (#886)

--- boards/BMS/Inc/stm32f4xx_hal_conf.h
+++ boards/BMS/Inc/stm32f4xx_hal_conf.h
@@ -94,8 +94,8 @@ extern "C"
  *        (when HSE is used as system clock source, directly or through the PLL).
  */
 #if !defined(HSE_VALUE)
-#define HSE_VALUE ((uint32_t)25000000U) /*!< Value of the External oscillator in Hz */
-#endif                                  /* HSE_VALUE */
+#define HSE_VALUE ((uint32_t)8000000U) /*!< Value of the External oscillator in Hz */
+#endif                                 /* HSE_VALUE */
 
 #if !defined(HSE_STARTUP_TIMEOUT)
 #define HSE_STARTUP_TIMEOUT ((uint32_t)100U) /*!< Time out for HSE start up, in ms */
