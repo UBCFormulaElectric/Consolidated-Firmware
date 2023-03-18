dee069e59c75b61cace33c27d3126ea62b66743c
halab1t <laura.romer@gmail.com>
2023-03-18 15:38:09 -0700

    changes

--- boards/DCM/Test/Src/torquevectoring/Test_ActiveDifferential.cpp
+++ boards/DCM/Test/Src/torquevectoring/Test_ActiveDifferential.cpp
@@ -15,3 +15,19 @@ extern "C"
 #include "torquevectoring/App_TorqueVectoringConstants.h"
 #include "math.h"
 }
+/**
+ * Define SetUp and TearDown to be called before every TEST_F call
+*/
+class PowerLimitingTest : public testing::Test
+{
+protected:
+    //TODO: Find out if this is necessary if no IO functions are called.
+    void SetUp() override
+    {
+        return;
+    }
+    void TearDown() override
+    {
+        return;
+    }
+};
