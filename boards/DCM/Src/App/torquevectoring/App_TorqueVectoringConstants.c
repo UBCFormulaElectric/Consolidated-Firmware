2576a5d542f33f33145db271d8c4695fbf845a61
Alex Koen <akoen@mailbox.org>
2023-03-18 15:04:47 -0700

    Fix formatting

--- boards/DCM/Src/App/torquevectoring/App_TorqueVectoringConstants.c
+++ boards/DCM/Src/App/torquevectoring/App_TorqueVectoringConstants.c
@@ -1,6 +1,14 @@
 #include "torquevectoring/App_TorqueVectoringConstants.h"
 
-const PID_Config PID_POWER_CORRECTION_CONFIG = { .Kp = 0.01f, .Ki = 0.0f, .Kd = 0.0f, .out_max = 1.0f, .out_min = -1.0f };
+const PID_Config PID_POWER_CORRECTION_CONFIG = { .Kp      = 0.01f,
+                                                 .Ki      = 0.0f,
+                                                 .Kd      = 0.0f,
+                                                 .out_max = 1.0f,
+                                                 .out_min = -1.0f };
 
 // TODO(akoen): Determine default PID parameters for traction control
-const PID_Config PID_TRACTION_CONTROL_CONFIG = { .Kp = 0.001f, .Ki = 0.0f, .Kd = 0.0f, .out_max = 0.0f, .out_min = -1.0f };
+const PID_Config PID_TRACTION_CONTROL_CONFIG = { .Kp      = 0.001f,
+                                                 .Ki      = 0.0f,
+                                                 .Kd      = 0.0f,
+                                                 .out_max = 0.0f,
+                                                 .out_min = -1.0f };
