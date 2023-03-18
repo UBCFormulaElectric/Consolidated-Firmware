e429bb19e49c4677b9c5813bd9208e33714de07d
Kyle Mackenzie <1mackenziekyle@gmail.com>
2023-03-18 15:06:33 -0700

    Modified App_TestPowerLimiting

---
+++ boards/DCM/Test/Src/torquevectoring/Test_PID.cpp
@@ -0,0 +1,29 @@
+
+
+#include "Test_Dcm.h"
+
+extern "C"
+{
+    #include "App_PID.h"
+    #include "App_TorqueVectoring.h"
+}
+
+class PIDTest : public testing::Test
+{
+    protected:
+        void SetUp() override
+        {
+            return;
+        }
+        void TearDown() override
+        {
+            return;
+        }
+};
+
+PID_Config* pid_config_test1;
+pid_config_test1->Kp = 1.0;
+pid_config_test1->Ki = 0.0;
+pid_config_test1->Kd = 0.0;
+pid_config_test1->out_min = 10.0;
+pid_config_test1->out_min = 0.0;
