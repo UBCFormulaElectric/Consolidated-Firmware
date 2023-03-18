697350561e577c639f25c9c80fb75b13e549787a
Kyle Mackenzie <1mackenziekyle@gmail.com>
2023-03-18 15:35:04 -0700

    fixed syntax errors in power limiting

--- boards/DCM/Test/Src/torquevectoring/Test_PowerLimiting.cpp
+++ boards/DCM/Test/Src/torquevectoring/Test_PowerLimiting.cpp
@@ -10,9 +10,9 @@
 
 extern "C"
 {
-#include "App_PowerLimiting.h"
-#include "App_TorqueVectoring.h"
-#include "App_TorqueVectoringConstants.h"
+#include "torquevectoring/App_PowerLimiting.h"
+#include "torquevectoring/App_TorqueVectoring.h"
+#include "torquevectoring/App_TorqueVectoringConstants.h"
 #include "math.h"
 }
 
@@ -35,15 +35,12 @@ class PowerLimitingTest : public testing::Test
 
 
 // Test1: Limit on max motor temps
-PowerLimiting_Inputs* power_limiting_inputs_test1 = {
-    100.0,
-    100.0,
-    POWER_LIMIT_CAR_kW,
-    1.00
-};
 
-TEST_F(PowerLimitingTest, test_motor_temp_limits_power)
+TEST_F(PowerLimitingTest, motor_temps_limit_power)
 {
+    struct PowerLimiting_Inputs* power_limiting_inputs_test1;
+    *power_limiting_inputs_test1 = {100.0, 100.0, POWER_LIMIT_CAR_kW, 1.00}; 
+
     float expected_power_limit_test1 = POWER_LIMIT_CAR_kW - (fmaxf(power_limiting_inputs_test1->left_motor_temp_C, power_limiting_inputs_test1->right_motor_temp_C)- MOTOR_TEMP_CUTOFF_c) * MOTOR_TEMP_POWER_DECREMENTING_RATIO;
     float actual_power_limit_test1 = App_PowerLimiting_ComputeMaxPower(power_limiting_inputs_test1);
     ASSERT_FLOAT_EQ(actual_power_limit_test1, expected_power_limit_test1);
@@ -51,15 +48,11 @@ TEST_F(PowerLimitingTest, test_motor_temp_limits_power)
 
 
 // Test2: Limit on BMS available power
-PowerLimiting_Inputs* power_limiting_inputs_test2 = {
-    10.0,
-    10.0,
-    50.0,
-    1.00
-};
 
-TEST_F(PowerLimitingTest, test_bms_limits_power)
+TEST_F(PowerLimitingTest, bms_limits_power)
 {
+    struct PowerLimiting_Inputs* power_limiting_inputs_test2;
+    *power_limiting_inputs_test2 = {10.0, 10.0, 50.0, 1.00};
     float expected_power_limit_test2 = 50.0;
     float actual_power_limit_test2 = App_PowerLimiting_ComputeMaxPower(power_limiting_inputs_test2);
     ASSERT_FLOAT_EQ(actual_power_limit_test2, expected_power_limit_test2);
@@ -67,29 +60,24 @@ TEST_F(PowerLimitingTest, test_bms_limits_power)
 
 
 // Test3: Limit on pedal percentage
-PowerLimiting_Inputs* power_limiting_inputs_test3 = {
-    10.0,
-    10.0,
-    POWER_LIMIT_CAR_kW,
-    0.50
-};
 
-TEST_F(PowerLimitingTest, test_pedal_limits_power)
+TEST_F(PowerLimitingTest, pedal_limits_power)
 {
+    struct PowerLimiting_Inputs* power_limiting_inputs_test3;
+    *power_limiting_inputs_test3 = {10.0, 10.0, POWER_LIMIT_CAR_kW, 0.50};
     float expected_power_limit_test3 = POWER_LIMIT_CAR_kW * power_limiting_inputs_test3->accelerator_pedal_percent;
-    ASSERT_FLOAT_EQ(App_PowerLimiting_ComputeMaxPower(power_limiting_inputs_test3), expected_power_limit_test3);
+    float actual_power_limit_test3 = App_PowerLimiting_ComputeMaxPower(power_limiting_inputs_test3);
+    ASSERT_FLOAT_EQ(actual_power_limit_test3, expected_power_limit_test3);
 }
 
 
 // Test4 Pedal not pressed
-PowerLimiting_Inputs* power_limiting_inputs_test4 = {
-    10.0,
-    10.0,
-    POWER_LIMIT_CAR_kW,
-    0.00
-}
 
-TEST_F(PowerLimitingTest, test_pedal_not_pressed)
+TEST_F(PowerLimitingTest, no_power_when_pedal_not_pressed)
 {
-    ASSERT_FLOAT_EQ(App_PowerLimiting_ComputeMaxPower(power_limiting_inputs_test4), 0.0f);
+    struct PowerLimiting_Inputs* power_limiting_inputs_test4;
+    *power_limiting_inputs_test4 = {10.0, 10.0, POWER_LIMIT_CAR_kW, 0.00};
+    float expected_power_limit_test4 = 0.0;
+    float actual_power_limit_test4 = App_PowerLimiting_ComputeMaxPower(power_limiting_inputs_test4);
+    ASSERT_FLOAT_EQ(actual_power_limit_test4, expected_power_limit_test4);
 }
