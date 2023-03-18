00bd5d1f84a5e84c890d1323382c590112aa70a2
Kyle Mackenzie <1mackenziekyle@gmail.com>
2023-03-18 15:06:33 -0700

    Modified App_TestPowerLimiting

--- boards/DCM/Test/Src/torquevectoring/Test_PowerLimiting.cpp
+++ boards/DCM/Test/Src/torquevectoring/Test_PowerLimiting.cpp
@@ -35,13 +35,14 @@ class PowerLimitingTest : public testing::Test
 
 
 // Test1: Limit on max motor temps
-PowerLimiting_Inputs* power_limiting_inputs_test1;
-power_limiting_inputs_test1->left_motor_temp_C = 100.0;
-power_limiting_inputs_test1->right_motor_temp_C =  100.0;
-power_limiting_inputs_test1->available_power_kW = POWER_LIMIT_CAR_kW
-power_limiting_inputs_test1->accelerator_pedal_percent = 1.00; 
+PowerLimiting_Inputs* power_limiting_inputs_test1 = {
+    100.0,
+    100.0,
+    POWER_LIMIT_CAR_kW,
+    1.00
+};
 
-TEST_F(PowerLimitingTest, power_limiting_inputs_test1)
+TEST_F(PowerLimitingTest, test_motor_temp_limits_power)
 {
     float expected_power_limit_test1 = POWER_LIMIT_CAR_kW - (fmaxf(power_limiting_inputs_test1->left_motor_temp_C, power_limiting_inputs_test1->right_motor_temp_C)- MOTOR_TEMP_CUTOFF_c) * MOTOR_TEMP_POWER_DECREMENTING_RATIO;
     float actual_power_limit_test1 = App_PowerLimiting_ComputeMaxPower(power_limiting_inputs_test1);
@@ -50,14 +51,14 @@ TEST_F(PowerLimitingTest, power_limiting_inputs_test1)
 
 
 // Test2: Limit on BMS available power
-PowerLimiting_Inputs* power_limiting_inputs_test2;
-power_limiting_inputs_test2->left_motor_temp_C = 10.0;
-power_limiting_inputs_test2->right_motor_temp_C =  10.0;
-power_limiting_inputs_test2->available_power_kW = 50.0;
-power_limiting_inputs_test2->accelerator_pedal_percent = 1.00; 
+PowerLimiting_Inputs* power_limiting_inputs_test2 = {
+    10.0,
+    10.0,
+    50.0,
+    1.00
+};
 
-
-TEST_F(PowerLimitingTest, power_limiting_inputs_test2)
+TEST_F(PowerLimitingTest, test_bms_limits_power)
 {
     float expected_power_limit_test2 = 50.0;
     float actual_power_limit_test2 = App_PowerLimiting_ComputeMaxPower(power_limiting_inputs_test2);
@@ -66,30 +67,29 @@ TEST_F(PowerLimitingTest, power_limiting_inputs_test2)
 
 
 // Test3: Limit on pedal percentage
-PowerLimiting_Inputs* power_limiting_inputs_test3;
-power_limiting_inputs_test3->left_motor_temp_C = 10.0;
-power_limiting_inputs_test3->right_motor_temp_C =  10.0;
-power_limiting_inputs_test3->available_power_kW = POWER_LIMIT_CAR_kW;
-power_limiting_inputs_test3->accelerator_pedal_percent = 0.50; 
+PowerLimiting_Inputs* power_limiting_inputs_test3 = {
+    10.0,
+    10.0,
+    POWER_LIMIT_CAR_kW,
+    0.50
+};
 
-TEST_F(PowerLimitingTest, power_limiting_inputs_test3)
+TEST_F(PowerLimitingTest, test_pedal_limits_power)
 {
     float expected_power_limit_test3 = POWER_LIMIT_CAR_kW * power_limiting_inputs_test3->accelerator_pedal_percent;
-    float actual_power_limit_test3 = App_PowerLimiting_ComputeMaxPower(power_limiting_inputs_test3);
-    ASSERT_FLOAT_EQ(actual_power_limit_test3, expected_power_limit_test3);
+    ASSERT_FLOAT_EQ(App_PowerLimiting_ComputeMaxPower(power_limiting_inputs_test3), expected_power_limit_test3);
 }
 
 
 // Test4 Pedal not pressed
-PowerLimiting_Inputs* power_limiting_inputs_test4;
-power_limiting_inputs_test4->left_motor_temp_C = 10.0;
-power_limiting_inputs_test4->right_motor_temp_C =  10.0;
-power_limiting_inputs_test4->available_power_kW = POWER_LIMIT_CAR_kW;
-power_limiting_inputs_test4->accelerator_pedal_percent = 0.00; 
+PowerLimiting_Inputs* power_limiting_inputs_test4 = {
+    10.0,
+    10.0,
+    POWER_LIMIT_CAR_kW,
+    0.00
+}
 
-TEST_F(PowerLimitingTest, power_limiting_inputs_test4)
+TEST_F(PowerLimitingTest, test_pedal_not_pressed)
 {
-    float expected_power_limit_test4 = 0.0;
-    float actual_power_limit_test4 = App_PowerLimiting_ComputeMaxPower(power_limiting_inputs_test4);
-    ASSERT_FLOAT_EQ(actual_power_limit_test4, expected_power_limit_test4);
+    ASSERT_FLOAT_EQ(App_PowerLimiting_ComputeMaxPower(power_limiting_inputs_test4), 0.0f);
 }
