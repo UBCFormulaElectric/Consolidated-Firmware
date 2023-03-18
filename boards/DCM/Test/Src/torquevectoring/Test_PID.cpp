461870964d15fffc6bbc7573fe4e4de0604f1f36
Kyle Mackenzie <1mackenziekyle@gmail.com>
2023-03-18 15:56:13 -0700

    fixed include paths in Test_PID

--- boards/DCM/Test/Src/torquevectoring/Test_PID.cpp
+++ boards/DCM/Test/Src/torquevectoring/Test_PID.cpp
@@ -4,8 +4,8 @@
 
 extern "C"
 {
-    #include "App_PID.h"
-    #include "App_TorqueVectoring.h"
+    #include "torquevectoring/App_PID.h"
+    #include "torquevectoring/App_TorqueVectoring.h"
 }
 
 class PIDTest : public testing::Test
@@ -22,10 +22,8 @@ class PIDTest : public testing::Test
 };
 
 PID_Config* pid_config_test1;
-pid_config_test1->Kp = 1.0;
-pid_config_test1->Ki = 0.0;
-pid_config_test1->Kd = 0.0;
-pid_config_test1->out_min = 10.0;
-pid_config_test1->out_min = 0.0;
 
-TEST_F(PIDTest, pid_config_test1)
+
+TEST_F(PIDTest, pid_config_test1){
+    
+}
