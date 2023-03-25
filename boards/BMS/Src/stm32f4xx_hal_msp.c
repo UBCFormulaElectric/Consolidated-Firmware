c2635954d689e78d93e922ac313f52315c2c6822
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2023-03-24 18:16:16 -0700

    New BMS reading from module (#894)
    Changes for BMS to read from a module

--- boards/BMS/Src/stm32f4xx_hal_msp.c
+++ boards/BMS/Src/stm32f4xx_hal_msp.c
@@ -133,7 +133,7 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
         hdma_adc1.Init.MemInc              = DMA_MINC_ENABLE;
         hdma_adc1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
         hdma_adc1.Init.MemDataAlignment    = DMA_MDATAALIGN_HALFWORD;
-        hdma_adc1.Init.Mode                = DMA_NORMAL;
+        hdma_adc1.Init.Mode                = DMA_CIRCULAR;
         hdma_adc1.Init.Priority            = DMA_PRIORITY_LOW;
         hdma_adc1.Init.FIFOMode            = DMA_FIFOMODE_DISABLE;
         if (HAL_DMA_Init(&hdma_adc1) != HAL_OK)
@@ -143,9 +143,6 @@ void HAL_ADC_MspInit(ADC_HandleTypeDef *hadc)
 
         __HAL_LINKDMA(hadc, DMA_Handle, hdma_adc1);
 
-        /* ADC1 interrupt Init */
-        HAL_NVIC_SetPriority(ADC_IRQn, 5, 0);
-        HAL_NVIC_EnableIRQ(ADC_IRQn);
         /* USER CODE BEGIN ADC1_MspInit 1 */
 
         /* USER CODE END ADC1_MspInit 1 */
@@ -185,9 +182,6 @@ void HAL_ADC_MspDeInit(ADC_HandleTypeDef *hadc)
 
         /* ADC1 DMA DeInit */
         HAL_DMA_DeInit(hadc->DMA_Handle);
-
-        /* ADC1 interrupt DeInit */
-        HAL_NVIC_DisableIRQ(ADC_IRQn);
         /* USER CODE BEGIN ADC1_MspDeInit 1 */
 
         /* USER CODE END ADC1_MspDeInit 1 */
@@ -283,14 +277,21 @@ void HAL_SPI_MspInit(SPI_HandleTypeDef *hspi)
         */
         GPIO_InitStruct.Pin       = GPIO_PIN_5;
         GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull      = GPIO_NOPULL;
+        GPIO_InitStruct.Pull      = GPIO_PULLDOWN;
         GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_HIGH;
         GPIO_InitStruct.Alternate = GPIO_AF5_SPI1;
         HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
-        GPIO_InitStruct.Pin       = GPIO_PIN_4 | GPIO_PIN_5;
+        GPIO_InitStruct.Pin       = GPIO_PIN_4;
         GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull      = GPIO_NOPULL;
+        GPIO_InitStruct.Pull      = GPIO_PULLUP;
+        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_HIGH;
+        GPIO_InitStruct.Alternate = GPIO_AF5_SPI1;
+        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+        GPIO_InitStruct.Pin       = GPIO_PIN_5;
+        GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+        GPIO_InitStruct.Pull      = GPIO_PULLDOWN;
         GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_HIGH;
         GPIO_InitStruct.Alternate = GPIO_AF5_SPI1;
         HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
@@ -364,6 +365,17 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim_base)
 
         /* USER CODE END TIM1_MspInit 1 */
     }
+    else if (htim_base->Instance == TIM3)
+    {
+        /* USER CODE BEGIN TIM3_MspInit 0 */
+
+        /* USER CODE END TIM3_MspInit 0 */
+        /* Peripheral clock enable */
+        __HAL_RCC_TIM3_CLK_ENABLE();
+        /* USER CODE BEGIN TIM3_MspInit 1 */
+
+        /* USER CODE END TIM3_MspInit 1 */
+    }
     else if (htim_base->Instance == TIM13)
     {
         /* USER CODE BEGIN TIM13_MspInit 0 */
@@ -413,6 +425,17 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef *htim_base)
         /* USER CODE BEGIN TIM1_MspDeInit 1 */
 
         /* USER CODE END TIM1_MspDeInit 1 */
+    }
+    else if (htim_base->Instance == TIM3)
+    {
+        /* USER CODE BEGIN TIM3_MspDeInit 0 */
+
+        /* USER CODE END TIM3_MspDeInit 0 */
+        /* Peripheral clock disable */
+        __HAL_RCC_TIM3_CLK_DISABLE();
+        /* USER CODE BEGIN TIM3_MspDeInit 1 */
+
+        /* USER CODE END TIM3_MspDeInit 1 */
     }
     else if (htim_base->Instance == TIM13)
     {
