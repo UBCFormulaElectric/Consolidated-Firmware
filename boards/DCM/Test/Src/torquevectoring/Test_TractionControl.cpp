fb1164d74d8e4f5848041199ad086dac5ef2c43d
Kyle Mackenzie <1mackenziekyle@gmail.com>
2023-03-25 15:26:26 -0700

    First TV test passed

--- boards/DCM/Test/Src/torquevectoring/Test_TractionControl.cpp
+++ boards/DCM/Test/Src/torquevectoring/Test_TractionControl.cpp
@@ -4,6 +4,7 @@
 
 extern "C"
 {
+    #include "torquevectoring/App_TorqueVectoring.h"
     #include "torquevectoring/App_TractionControl.h"
 }
 
@@ -18,4 +19,16 @@ class TractionControlTest : public testing::Test
         {
             return;
         }
-};
+};  
+
+TEST(TractionControlTest, TractionControl_init)
+{
+    PID_Config tc_pid_config = { 0.0, 0.0, 0.0, 0.0, 10.0};
+    PID tc_pid;
+    App_PID_Init(&tc_pid, &tc_pid_config);
+    TractionControl_Inputs test_inputs = { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, &tc_pid };
+    TractionControl_Outputs test_outputs = { 0.0, 0.0 };
+    App_TractionControl_ComputeTorque(&test_inputs, &test_outputs);
+    ASSERT_NEAR(test_outputs.torque_left_final_Nm, 0.0, 0.000001);
+    ASSERT_NEAR(test_outputs.torque_right_final_Nm, 0.0, 0.000001);
+}
