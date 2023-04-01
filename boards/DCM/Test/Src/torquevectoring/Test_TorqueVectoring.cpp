21a2fcc253a9f4e783cc0b7afbb2a51f377fa66e
halab1t <laura.romer@gmail.com>
2023-04-01 11:18:41 -0700

    wip: torquevestoring tests

--- boards/DCM/Test/Src/torquevectoring/Test_TorqueVectoring.cpp
+++ boards/DCM/Test/Src/torquevectoring/Test_TorqueVectoring.cpp
@@ -12,6 +12,7 @@
 #include "Test_Dcm.h"
 #include "App_SharedMacros.h"
 #include "App_CanRx.h"
+#include "App_CanTx.h"
 
 extern "C"
 {
@@ -38,6 +39,29 @@ protected:
     }
 };
 
+TEST_F(TorqueVectoringTest, no_BMS_available_power){
+    App_CanRx_FSM_Apps_PappsMappedPedalPercentage_Update(100.0);
+    App_CanRx_FSM_Wheels_LeftWheelSpeed_Update(50.0);
+    App_CanRx_FSM_Wheels_RightWheelSpeed_Update(50.0);
+    App_CanRx_INVL_MotorPositionInfo_MotorSpeed_Update(135);
+    App_CanRx_INVR_MotorPositionInfo_MotorSpeed_Update(135);
+    App_CanRx_BMS_TractiveSystem_TsCurrent_Update(100);
+    App_CanRx_BMS_TractiveSystem_TsVoltage_Update(390);
+    App_CanRx_INVL_Temperatures3_MotorTemperature_Update(50);
+    App_CanRx_INVR_Temperatures3_MotorTemperature_Update(50);
+    App_CanRx_BMS_AvailablePower_AvailablePower_Update(0);
+    App_CanRx_FSM_Steering_SteeringAngle_Update(30);
+    App_TorqueVectoring_Run();
+    float expected_torque_left_nM = 0.0;
+    float expected_torque_right_nM = 0.0;
+    float actual_torque_left_nM = App_CanTx_DCM_LeftInverterCommand_TorqueCommand_Get();
+    float actual_torque_right_nM = App_CanTx_DCM_RightInverterCommand_TorqueCommand_Get();
+    ASSERT_FLOAT_EQ(expected_torque_left_nM, actual_torque_left_nM);
+    ASSERT_FLOAT_EQ(expected_torque_right_nM, actual_torque_right_nM);
+
+//check the IO functions are being called the proper amount of times
+
+}
 TEST_F(TorqueVectoringTest, accelerator_pedal_not_pressed){
     App_CanRx_FSM_Apps_PappsMappedPedalPercentage_Update(0.0);
     App_CanRx_FSM_Wheels_LeftWheelSpeed_Update(50.0);
@@ -50,8 +74,60 @@ TEST_F(TorqueVectoringTest, accelerator_pedal_not_pressed){
     App_CanRx_INVR_Temperatures3_MotorTemperature_Update(50);
     App_CanRx_BMS_AvailablePower_AvailablePower_Update(50);
     App_CanRx_FSM_Steering_SteeringAngle_Update(30);
-
     App_TorqueVectoring_Run();
+    float expected_torque_left_nM = 0.0;
+    float expected_torque_right_nM = 0.0;
+    float actual_torque_left_nM = App_CanTx_DCM_LeftInverterCommand_TorqueCommand_Get();
+    float actual_torque_right_nM = App_CanTx_DCM_RightInverterCommand_TorqueCommand_Get();
+    ASSERT_FLOAT_EQ(expected_torque_left_nM, actual_torque_left_nM);
+    ASSERT_FLOAT_EQ(expected_torque_right_nM, actual_torque_right_nM);
+
+//check the IO functions are being called the proper amount of times
+
+}
+TEST_F(TorqueVectoringTest, left_motor_too_hot){
+    App_CanRx_FSM_Apps_PappsMappedPedalPercentage_Update(100.0);
+    App_CanRx_FSM_Wheels_LeftWheelSpeed_Update(50.0);
+    App_CanRx_FSM_Wheels_RightWheelSpeed_Update(50.0);
+    App_CanRx_INVL_MotorPositionInfo_MotorSpeed_Update(135);
+    App_CanRx_INVR_MotorPositionInfo_MotorSpeed_Update(135);
+    App_CanRx_BMS_TractiveSystem_TsCurrent_Update(100);
+    App_CanRx_BMS_TractiveSystem_TsVoltage_Update(390);
+    App_CanRx_INVL_Temperatures3_MotorTemperature_Update(130);
+    App_CanRx_INVR_Temperatures3_MotorTemperature_Update(100);
+    App_CanRx_BMS_AvailablePower_AvailablePower_Update(50);
+    App_CanRx_FSM_Steering_SteeringAngle_Update(30);
+    App_TorqueVectoring_Run();
+    float expected_torque_left_nM = 0.0;
+    float expected_torque_right_nM = 0.0;
+    float actual_torque_left_nM = App_CanTx_DCM_LeftInverterCommand_TorqueCommand_Get();
+    float actual_torque_right_nM = App_CanTx_DCM_RightInverterCommand_TorqueCommand_Get();
+    ASSERT_FLOAT_EQ(expected_torque_left_nM, actual_torque_left_nM);
+    ASSERT_FLOAT_EQ(expected_torque_right_nM, actual_torque_right_nM);
+
+//check the IO functions are being called the proper amount of times
+
+}
+TEST_F(TorqueVectoringTest, right_motor_too_hot){
+    App_CanRx_FSM_Apps_PappsMappedPedalPercentage_Update(100.0);
+    App_CanRx_FSM_Wheels_LeftWheelSpeed_Update(50.0);
+    App_CanRx_FSM_Wheels_RightWheelSpeed_Update(50.0);
+    App_CanRx_INVL_MotorPositionInfo_MotorSpeed_Update(135);
+    App_CanRx_INVR_MotorPositionInfo_MotorSpeed_Update(135);
+    App_CanRx_BMS_TractiveSystem_TsCurrent_Update(100);
+    App_CanRx_BMS_TractiveSystem_TsVoltage_Update(390);
+    App_CanRx_INVL_Temperatures3_MotorTemperature_Update(100);
+    App_CanRx_INVR_Temperatures3_MotorTemperature_Update(130);
+    App_CanRx_BMS_AvailablePower_AvailablePower_Update(50);
+    App_CanRx_FSM_Steering_SteeringAngle_Update(30);
+    App_TorqueVectoring_Run();
+    float expected_torque_left_nM = 0.0;
+    float expected_torque_right_nM = 0.0;
+    float actual_torque_left_nM = App_CanTx_DCM_LeftInverterCommand_TorqueCommand_Get();
+    float actual_torque_right_nM = App_CanTx_DCM_RightInverterCommand_TorqueCommand_Get();
+    ASSERT_FLOAT_EQ(expected_torque_left_nM, actual_torque_left_nM);
+    ASSERT_FLOAT_EQ(expected_torque_right_nM, actual_torque_right_nM);
+
 //check the IO functions are being called the proper amount of times
 
 }
