a3f1d528b3ed22f89b28d94a7f60b44a8a07f03b
formulae/bms/charging/1 <lkevin2003@gmail.com>
2023-04-08 17:00:33 -0700

    Finishing touches with formatting and cleared Test_Bms.cpp file

--- boards/BMS/Inc/App/states/App_ChargeState.h
+++ boards/BMS/Inc/App/states/App_ChargeState.h
@@ -2,10 +2,10 @@
 
 #include "App_SharedStateMachine.h"
 #include "App_Accumulator.h"
+// 0.05C is standard for a boundary to consider full charge
+#define CURRENT_AT_MAX_CHARGE (0.05f * C_RATE_TO_AMPS);
 /**
  * Get a pointer to the Charge State.
  * @return A pointer to the Charge State. THIS SHOULD NOT BE MODIFIED.
  */
 const struct State *App_GetChargeState(void);
-// 0.05C is standard for a boundary to consider full charge
-#define CURRENT_AT_MAX_CHARGE (0.05f * C_RATE_TO_AMPS);
