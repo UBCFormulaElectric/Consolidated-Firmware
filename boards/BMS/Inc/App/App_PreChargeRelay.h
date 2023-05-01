30451e9a7a00d968c54396ae98730d435921a9ad
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2023-05-01 11:17:39 -0700

    add test for precharge behavior

--- boards/BMS/Inc/App/App_PreChargeRelay.h
+++ boards/BMS/Inc/App/App_PreChargeRelay.h
@@ -6,6 +6,20 @@
 #include <stdlib.h>
 #include "App_CanTx.h"
 
+// clang-format off
+#define NUM_OF_INVERTERS        (2U)
+#define PRECHARGE_RESISTOR_OHMS (500U)
+#define INV_CAPACITANCE_F       (0.280e-3f)
+#define PRECHARGE_CAPACITANCE_F (INV_CAPACITANCE_F * NUM_OF_INVERTERS)
+#define PRECHARGE_RC_MS         (S_TO_MS(PRECHARGE_RESISTOR_OHMS * PRECHARGE_CAPACITANCE_F))
+
+// 2.7RC corresponds to time to reach ~93% charged
+#define PRECHARGE_COMPLETION_MS          ((float)PRECHARGE_RC_MS * 2.7f)
+#define PRECHARGE_COMPLETION_LOWER_BOUND ((uint32_t)(PRECHARGE_COMPLETION_MS * 0.5f))
+#define PRECHARGE_COMPLETION_UPPER_BOUND ((uint32_t)(PRECHARGE_COMPLETION_MS * 2.0f))
+
+// clang-format on
+
 struct PrechargeRelay;
 
 /**
