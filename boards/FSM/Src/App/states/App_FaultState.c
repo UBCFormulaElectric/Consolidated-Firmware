36a4a872dcd5419e5839b459348e0588afbea9a0
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2023-05-07 20:21:11 -0700

    Improve FSM faults test coverage (#910)
    Improve test coverage for FSM faults

--- boards/FSM/Src/App/states/App_FaultState.c
+++ boards/FSM/Src/App/states/App_FaultState.c
@@ -7,6 +7,7 @@ void App_FaultStateRunOnEntry(struct StateMachine *const state_machine)
 {
     UNUSED(state_machine);
     App_CanTx_FSM_Vitals_State_Set(FSM_STATE_FAULT);
+    App_CanAlerts_SetFault(FSM_FAULT_STATE_FAULT, true);
 }
 
 void App_FaultStateRunOnTick1Hz(struct StateMachine *state_machine)
@@ -17,12 +18,20 @@ void App_FaultStateRunOnTick100Hz(struct StateMachine *state_machine)
 {
     App_AllStatesRunOnTick100Hz(state_machine);
 
-    // SUPRESS CERTAIN MESSAGES
-    App_CanTx_FSM_Apps_PappsMappedPedalPercentage_Set(0.0f);
+    // Suppress accelerator pedal percentages (set to 0%)
+    App_CanTx_FSM_Apps_PappsMappedPedalPercentage_Set(0);
+    App_CanTx_FSM_Apps_SappsMappedPedalPercentage_Set(0);
+
+    const bool hb_ok = !App_CanAlerts_GetFault(FSM_FAULT_MISSING_HEARTBEAT);
+    if (hb_ok)
+    {
+        App_SharedStateMachine_SetNextState(state_machine, App_GetDriveState());
+    }
 }
 void App_FaultStateRunOnExit(struct StateMachine *const state_machine)
 {
     UNUSED(state_machine);
+    App_CanAlerts_SetFault(FSM_FAULT_STATE_FAULT, false);
 }
 
 const struct State *App_GetFaultState(void)
