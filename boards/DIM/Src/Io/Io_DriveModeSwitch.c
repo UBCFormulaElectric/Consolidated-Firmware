f6d8258fab983de0e0e25213acc50e6f02a9e1ed
lehroatic <wchaba@alumni.ubc.ca>
2023-06-04 17:26:37 -0700

    firstone

--- boards/DIM/Src/Io/Io_DriveModeSwitch.c
+++ boards/DIM/Src/Io/Io_DriveModeSwitch.c
@@ -1,29 +1,37 @@
 #include <limits.h>
 #include "main.h"
 #include "Io_DriveModeSwitch.h"
-
+#include "stdbool.h"
 // decode the binary pins back to a value from 1-12
 uint32_t Io_DriveModeSwitch_GetPosition(void)
 {
     // TODO: This is untested
-    uint32_t position = UINT_MAX;
-    position          = 0;
+    uint32_t position = 0;
     // drive mode gpio port is a 4 bit binary number
+
+    bool pins_set[4];
+    pins_set[0] = HAL_GPIO_ReadPin(DRIVE_MODE_0_GPIO_Port, DRIVE_MODE_0_Pin) == GPIO_PIN_RESET;
+    pins_set[1] = HAL_GPIO_ReadPin(DRIVE_MODE_1_GPIO_Port, DRIVE_MODE_1_Pin) == GPIO_PIN_RESET;
+    pins_set[2] = HAL_GPIO_ReadPin(DRIVE_MODE_2_GPIO_Port, DRIVE_MODE_2_Pin) == GPIO_PIN_RESET;
+    pins_set[3] = HAL_GPIO_ReadPin(DRIVE_MODE_3_GPIO_Port, DRIVE_MODE_3_Pin) == GPIO_PIN_RESET;
+
+    UNUSED(pins_set);
+
     if (HAL_GPIO_ReadPin(DRIVE_MODE_0_GPIO_Port, DRIVE_MODE_0_Pin) == GPIO_PIN_RESET)
     {
-        position += 8;
+        position += 1;
     }
     if (HAL_GPIO_ReadPin(DRIVE_MODE_1_GPIO_Port, DRIVE_MODE_1_Pin) == GPIO_PIN_RESET)
     {
-        position += 4;
+        position += 2;
     }
     if (HAL_GPIO_ReadPin(DRIVE_MODE_2_GPIO_Port, DRIVE_MODE_2_Pin) == GPIO_PIN_RESET)
     {
-        position += 2;
+        position += 4;
     }
     if (HAL_GPIO_ReadPin(DRIVE_MODE_3_GPIO_Port, DRIVE_MODE_3_Pin) == GPIO_PIN_RESET)
     {
-        position += 1;
+        position += 8;
     }
 
     return position;
