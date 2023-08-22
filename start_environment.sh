7bef529b28818c6e1d3e32aade6741eed998c859
gtaharaedmonds <gtaharaedmonds@gmail.com>
2023-08-22 13:37:40 -0700

    improve VS code config

--- start_environment.sh
+++ start_environment.sh
@@ -1,2 +1,9 @@
+set -e
+
+# Set `HOST_IP` environment variable, which is required to connect to the debug server.
+# In Unix-like systems, the system's IP address is shown after the "nameserver" field
+# in /etc/resolve.conf.
 export HOST_IP=$(cat /etc/resolv.conf | grep '^nameserver' | awk '{print $2}')
+
+# Start Docker service.
 docker compose up -d
