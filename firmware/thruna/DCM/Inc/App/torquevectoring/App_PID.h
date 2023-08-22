3793cc99346c15b75e483fe36098999beba005a1
gtaharaedmonds <gtaharaedmonds@gmail.com>
2023-08-21 23:54:55 -0700

    Squashed docker-related changes
    Added switch reading

---
+++ firmware/thruna/DCM/Inc/App/torquevectoring/App_PID.h
@@ -0,0 +1,29 @@
+#pragma once
+
+typedef struct
+{
+    float Kp;
+    float Ki;
+    float Kd;
+    float error;
+    float derivative;
+    float integral;
+    float prev_input;
+    float out_min;
+    float out_max;
+} PID;
+
+typedef struct
+{
+    const float Kp;
+    const float Ki;
+    const float Kd;
+    const float out_min;
+    const float out_max;
+} PID_Config;
+
+void App_PID_Init(PID *pid, const PID_Config *conf);
+
+float App_PID_Compute(PID *pid, float setpoint, float input);
+
+void App_PID_RequestReset(PID *pid);
