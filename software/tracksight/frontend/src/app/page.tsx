03ac7cc93fbd8a120593c0e4306079082018a1f5
Evan Liu <liuyievan@gmail.com>
2023-09-14 14:21:12 -0700

    Update navbar

--- software/tracksight/frontend/src/app/page.tsx
+++ software/tracksight/frontend/src/app/page.tsx
@@ -1,9 +1,33 @@
+'use client';
+
+import {useState, React} from 'react';
 import styles from './page.module.css'
+import NavBar from './components/navbar.tsx'
+
+import { Layout } from 'antd';
+const { Header, Content } = Layout;
 
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-    </main>
-  )
+const Home = () => {
+    const [componentToDisplay, setComponentToDisplay] = useState("visualize");
+
+    let componentToRender = null;
+    if (componentToDisplay === "visualize") {
+        componentToRender = (<p>Hi</p>);
+    } else {
+        componentToRender = (<p>Hello</p>);
+    }
+
+    return (
+        <Layout className={styles.main}>
+            <Header className={styles.header}>
+                <NavBar updateFunction={setComponentToDisplay}/>
+            </Header>
+            <Content>
+                {componentToRender}
+            </Content>
+        </Layout>
+    )
 }
+
+export default Home;
