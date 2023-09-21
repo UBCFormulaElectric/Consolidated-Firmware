61bdd49c12124ec036afba15364302f665a83cb9
Evan <liuyievan@gmail.com>
2023-09-21 12:36:16 -0700

    Get sample signals workign (#986)
    ### Summary
    <!-- Quick summary of changes, optional -->
    
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    ### Resolved Issues
    <!-- Link any issues that this PR resolved like so: `Resolves #1, #2,
    and #5` (Note: Using this format, Github will automatically close the
    issue(s) when this PR is merged in). -->
    
    ### Checklist
    *Please change `[ ]` to `[x]` when you are ready.*
    - [ ] I have read and followed the code conventions detailed in
    [README.md](../README.md) (*This will save time for both you and the
    reviewer!*).
    - [ ] If this pull request is longer then **500** lines, I have provided
    *explicit* justification in the summary above explaining why I *cannot*
    break this up into multiple pull requests (*Small PR's are faster and
    less painful for everyone involved!*).

--- software/tracksight/backend/app/process/SignalUtil.py
+++ software/tracksight/backend/app/process/SignalUtil.py
@@ -9,19 +9,19 @@ import Definitions
 MOCK_DATA_PATH = Path(Definitions.ROOT_DIR) / "mock_data" / "data" / "sample_data.csv"
 
 class SignalUtil:
-	def __init__(self, is_mock = True):
-		if is_mock:
-			try:
-				self.df = pd.read_csv(MOCK_DATA_PATH)
-				self.df = self.df.set_index("date_time")
-			except:
-				print("Error reading mock data file")
-		else:
-			pass
+    def __init__(self, is_mock = True):
+        if is_mock:
+            try:
+                self.df = pd.read_csv(MOCK_DATA_PATH)
+                self.df = self.df.set_index("date_time")
+            except:
+                print("Error reading mock data file")
+        else:
+            pass
 
-	def get_signals(self, ids):
-		valid_ids = list(filter(lambda s_id : s_id in self.df.columns, ids))
-		return self.df[valid_ids].to_json()
+    def get_signals(self, ids):
+        valid_ids = list(filter(lambda s_id : s_id in self.df.columns, ids))
+        return self.df[valid_ids].to_json()
 
-	def get_signal(self, s_id):
-		return self.df[s_id]
+    def get_signal(self, s_id):
+        return self.df[s_id]
