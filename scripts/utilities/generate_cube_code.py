6f09b341de62ec9dd392a175403add0a5308c151
gtaharaedmonds <gtaharaedmonds@gmail.com>
2023-09-24 02:32:28 +0000

    day 1 fixes

--- scripts/utilities/generate_cube_code.py
+++ scripts/utilities/generate_cube_code.py
@@ -9,7 +9,7 @@ import argparse
 
 from supported_boards import get_board_names
 
-CUBE_SCRIPT = '''\
+CUBE_SCRIPT = """\
 ###############################################################################
 # This file is auto-generated. DO NOT MODIFY!
 #
@@ -27,9 +27,9 @@ generate code {codegen_dir}
 
 # Exit the program
 exit
-'''
+"""
 
-LOG4J_PROPERTIES = '''\
+LOG4J_PROPERTIES = """\
 ###############################################################################
 # This file is auto-generated. DO NOT MODIFY!
 ###############################################################################
@@ -40,19 +40,21 @@ log4j.rootLogger=ERROR, theConsoleAppender
 log4j.appender.theConsoleAppender=org.apache.log4j.ConsoleAppender
 log4j.appender.theConsoleAppender.layout=org.apache.log4j.PatternLayout
 log4j.appender.theConsoleAppender.layout.ConversionPattern=%-4r [%t] %-5p %c %x - %m%n
-'''
+"""
 
-def generate_cubemx_code(board, ioc, codegen_dir, cubemx, log4j_properties):
+
+def generate_cubemx_code(board, ioc, codegen_dir, cubemx, log4j_properties, no_gui):
     """
     Generate STM32CubeMX code
 
-    @param board: Name of the board
+
+    board: Name of the board
     @param ioc: Path to .ioc file
     @param codegen_dir: Directory in which STM32CubeMX code is to be generated
     @param cubemx: Path to STM32CubeMX binary
     """
     # Generate output folders if they don't exist already
-    cube_script_dir = os.path.join(codegen_dir, 'auto_generated')
+    cube_script_dir = os.path.join(codegen_dir, "auto_generated")
     if not os.path.exists(cube_script_dir):
         os.makedirs(cube_script_dir)
     log4j_properties_dir = os.path.dirname(log4j_properties)
@@ -60,18 +62,23 @@ def generate_cubemx_code(board, ioc, codegen_dir, cubemx, log4j_properties):
         os.makedirs(log4j_properties_dir)
 
     # Generate a temporary STM32CubeMX configuration file
-    cube_script = os.path.join(cube_script_dir, board + '.stm32cubemx.script')
-    cube_script_f = open(cube_script, 'w+')
+    cube_script = os.path.join(cube_script_dir, board + ".stm32cubemx.script")
+    cube_script_f = open(cube_script, "w+")
     cube_script_f.write(CUBE_SCRIPT.format(ioc=ioc, codegen_dir=codegen_dir))
     cube_script_f.close()
 
     # Generate a config file to make STM32CubeMX logging output less verbose
-    log4j_properties_f = open(log4j_properties, 'w+')
+    log4j_properties_f = open(log4j_properties, "w+")
     log4j_properties_f.write(LOG4J_PROPERTIES)
     log4j_properties_f.close()
 
     # Generate STM32CubeMX code
-    proc = subprocess.Popen(['java', '-jar', cubemx, '-q', cube_script_f.name])
+    cmd = ["java", "-jar", cubemx, "-q", cube_script_f.name]
+    if not no_gui:
+        cmd.insert("xvfb-run")
+
+    proc = subprocess.Popen(cmd)
+
     # Note: If the STM32CubeMX script encounters an exception (e.g. It can't
     # find the a valid script), the process may never exit so there is no status
     # code to check at all. Account for this by setting a time out.
@@ -79,22 +86,38 @@ def generate_cubemx_code(board, ioc, codegen_dir, cubemx, log4j_properties):
     try:
         proc.wait(timeout_sec)
     except subprocess.TimeoutExpired:
-        raise Exception('STM32CubeMX execution has timed out after {} seconds.'.format(str(timeout_sec)))
-    if proc.returncode != 0:
-        raise Exception('An error occured while executing STM32CubeMX.')
+        raise Exception(
+            f"STM32CubeMX execution has timed out after {timeout_sec} seconds."
+        )
 
-if __name__ == '__main__':
+    if proc.returncode != 0:
+        raise Exception("An error occured while executing STM32CubeMX.")
+
+
+if __name__ == "__main__":
     # Parse arguments
     parser = argparse.ArgumentParser()
     supported_boards = get_board_names()
-    parser.add_argument('--board', help='Choose one of the following: ' + ' '.join(supported_boards))
-    parser.add_argument('--log4j_properties_output', help='Path to output file storing log4j properties')
-    parser.add_argument('--ioc', help='STM32CubeMX .ioc file')
-    parser.add_argument('--codegen_output_dir', help='Code generation output folder')
-    parser.add_argument('--cube_bin', help='STM32CubeMX binary')
+    parser.add_argument(
+        "--board", help="Choose one of the following: " + " ".join(supported_boards)
+    )
+    parser.add_argument(
+        "--log4j_properties_output", help="Path to output file storing log4j properties"
+    )
+    parser.add_argument("--ioc", help="STM32CubeMX .ioc file")
+    parser.add_argument("--codegen_output_dir", help="Code generation output folder")
+    parser.add_argument("--cube_bin", help="STM32CubeMX binary")
+    parser.add_argument("--no-gui", action="store_true", help="Run without a GUI")
     args = parser.parse_args()
     if args.board not in supported_boards:
-        print('Error: Invalid board name. Valid options: ' + ' '.join(supported_boards))
+        print("Error: Invalid board name. Valid options: " + " ".join(supported_boards))
         sys.exit(1)
 
-    generate_cubemx_code(args.board, args.ioc, args.codegen_output_dir, args.cube_bin, args.log4j_properties_output)
+    generate_cubemx_code(
+        args.board,
+        args.ioc,
+        args.codegen_output_dir,
+        args.cube_bin,
+        args.log4j_properties_output,
+        args.no_gui,
+    )
