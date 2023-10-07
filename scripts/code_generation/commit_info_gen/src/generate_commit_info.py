aab383aa584a54bc145e46c28b2553c56f02c89b
Liam Ilan <snowboardsheep@gmail.com>
2023-10-07 12:59:33 -0700

    Implemented --output_header option for commit_info_gen

--- scripts/code_generation/commit_info_gen/src/generate_commit_info.py
+++ scripts/code_generation/commit_info_gen/src/generate_commit_info.py
@@ -1,25 +1,35 @@
 import os
+from argparse import ArgumentParser
 import jinja2
 import git
 
-# get template for rendering
-module_dir = os.path.dirname(os.path.relpath(__file__))
-template_dir = os.path.join(module_dir, "templates")
-env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
-template = env.get_template("./app_commit_info.h.j2")
+if __name__ == "__main__":
 
-# get commit data
-repo = git.Repo(search_parent_directories=True)
-commit = repo.head.commit
-clean = not repo.is_dirty(untracked_files=True)
+  # set up path arg
+  parser = ArgumentParser()
+  parser.add_argument("--output-header", type=str, required=True)
+  args = parser.parse_args() 
+  output_path = ""
 
-# short hash is the first 7 chars of the long hash
-short_hash = commit.hexsha[0:7]
+  # get template for rendering
+  module_dir = os.path.dirname(os.path.relpath(__file__))
+  template_dir = os.path.join(module_dir, "templates")
+  env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
+  template = env.get_template("./App_CommitInfo.h.j2")
 
-data = {
-  "hash": short_hash,
-  "clean": "1" if clean else "0"
-}
+  # get commit data
+  repo = git.Repo(search_parent_directories=True)
+  commit = repo.head.commit
+  clean = not repo.is_dirty(untracked_files=True)
 
-with open("test.h", "w") as file:
-  file.write(template.render(data))
+  # short hash is the first 7 chars of the long hash
+  short_hash = commit.hexsha[0:7]
+
+  data = {
+    "hash": short_hash,
+    "clean": "1" if clean else "0"
+  }
+
+  with open(args.output_header, "w") as file:
+    file.write(template.render(data))
+    
