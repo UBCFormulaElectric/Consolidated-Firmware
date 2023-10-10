5f6a82b2714509a129c4e02f666c0dcbea8053ea
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2023-10-10 00:06:58 -0700

    python nits

--- scripts/utilities/supported_boards.py
+++ scripts/utilities/supported_boards.py
@@ -2,23 +2,23 @@ import os.path
 from os import path, listdir
 
 
-def pathIsFolderAndHasIOCFile(pathName: str):
-    isFolder = not path.isfile(pathName)
-    if not isFolder:
+def path_is_folder_and_has_ioc_file(pathName: str):
+    is_folder = not path.isfile(pathName)
+    if not is_folder:
         return False
-    hasIOC = any(i.endswith(".ioc") for i in listdir(pathName)) or any(
-        [i.endswith(".ioc") for i in listdir(path.join(pathName, "src/cubemx"))])
-    return hasIOC
+    has_ioc = any(file.endswith(".ioc") for file in listdir(pathName)
+                  or any(file.endswith(".ioc") for file in listdir(path.join(pathName, "src/cubemx"))))
+    return has_ioc
 
 
 def get_board_names() -> tuple[str]:
     """
     Return a tuple of board names supported in our current build, whose STM code can be generated (only usage)
     """
-    pathToBoards = path.join(path.dirname(__file__), '..', '..', 'firmware', 'thruna')
-    SUPPORTED_BOARD_NAMES: tuple[str, ...] = tuple(
-        filter(lambda x: pathIsFolderAndHasIOCFile(os.path.join(pathToBoards, x)), listdir(pathToBoards)))
-    return SUPPORTED_BOARD_NAMES
+    path_to_boards = path.join(path.dirname(__file__), '..', '..', 'firmware', 'thruna')
+    supported_board_names: tuple[str, ...] = tuple(
+        filter(lambda x: path_is_folder_and_has_ioc_file(os.path.join(path_to_boards, x)), listdir(path_to_boards)))
+    return supported_board_names
 
 
 def print_space_delimited_board_names():
@@ -29,4 +29,4 @@ def print_space_delimited_board_names():
 
 
 if __name__ == '__main__':
-    print(get_board_names())
+    print(print_space_delimited_board_names())
