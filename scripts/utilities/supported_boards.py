16d15007b183508e5286d51b04a40b9bcf34b6df
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2023-10-09 20:15:08 -0700

    supported boards change python

--- scripts/utilities/supported_boards.py
+++ scripts/utilities/supported_boards.py
@@ -1,19 +1,32 @@
-def get_board_names():
+import os.path
+from os import path, listdir
+
+
+def pathIsFolderAndHasIOCFile(pathName: str):
+    isFolder = not path.isfile(pathName)
+    if not isFolder:
+        return False
+    hasIOC = any(i.endswith(".ioc") for i in listdir(pathName)) or any(
+        [i.endswith(".ioc") for i in listdir(path.join(pathName, "src/cubemx"))])
+    return hasIOC
+
+
+def get_board_names() -> tuple[str]:
     """
-    Return a tuple of board names supported in our current build
+    Return a tuple of board names supported in our current build, whose STM code can be generated (only usage)
     """
-    SUPPORTED_BOARD_NAMES = (
-        'FSM',
-        'PDM',
-        'BMS',
-        'DCM',
-        'DIM',
-        'GSM',
-    )
+    pathToBoards = path.join(path.dirname(__file__), '..', '..', 'firmware', 'thruna')
+    SUPPORTED_BOARD_NAMES: tuple[str, ...] = tuple(
+        filter(lambda x: pathIsFolderAndHasIOCFile(os.path.join(pathToBoards, x)), listdir(pathToBoards)))
     return SUPPORTED_BOARD_NAMES
+
 
 def print_space_delimited_board_names():
     """
     Print a space-delimited string which can be parsed as an array in bash
     """
     print(' '.join(get_board_names()))
+
+
+if __name__ == '__main__':
+    print(get_board_names())
