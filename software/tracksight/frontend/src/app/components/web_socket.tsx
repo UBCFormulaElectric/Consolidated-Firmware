4d42839b19e7313cb0a8858ca29a5fa4d7468e1e
Matthew Yung <117509016+myung03@users.noreply.github.com>
2023-10-12 19:45:47 -0700

    Matthew yung/telemetry dashboards (#1020)
    ### Summary
    
    Basic signal displaying functionality for telemetry MVP. Multiple
    signals can now be displayed on a single graph, and multiple graphs can
    be created.
    
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    - set up websocket server/client connection, allowing CSV data from
    server to be displayed
    - refactored previous component hierarchy; websocket is now only passed
    into Graph.tsx for simplicity
    - see Graph.tsx, page.tsx, websocket.tsx, dropdown_menu.tsx,
    telemetry.py, and SignalUtil.py for main changes
    - 
    ### Testing Done
    
    - added additional data sheets to test graph functionality for large
    data files vs. small data files
    - 
    ### Checklist
    
    - [x] I have read and followed the code conventions detailed in
    [README.md](../README.md) (*This will save time for both you and the
    reviewer!*).
    - [x] If this pull request is longer then **500** lines, I have provided
    *explicit* justification in the summary above explaining why I *cannot*
    break this up into multiple pull requests (*Small PR's are faster and
    less painful for everyone involved!*).

--- software/tracksight/frontend/src/app/components/web_socket.tsx
+++ software/tracksight/frontend/src/app/components/web_socket.tsx
@@ -1,29 +1,38 @@
 'use client';
 
+
 import { useEffect, useState, React } from 'react';
+import { Button, Space } from 'antd';
+//  WebSockets provide a full-duplex (two-way) communication channel over a single, 
+// long-lived connection, making it ideal for real-time data transfer between the client and server.
 
 const WebSocketComponent = (props) => {
+const [currId, setCurrId] = useState('');
 
-    useEffect(() => {
-        props.socket.on("available_signals_response", (data) => {
-            props.set_available_signals(JSON.parse(data));
-        });
-    }, [props.socket]);
 
-    const handleSubmit = () => {
-        props.socket.emit("signals", {"ids": ["Signal1"]});
-    };
+
+const handleSubmit = () => {
+  setCurrId(props.id);
+  console.log(props.signals)
+  props.socket.emit("signal", { "ids": props.signals });  
+};
+
 
     useEffect(() => {
         props.socket.on("signal_response", (data) => {
-            props.setData(JSON.parse(data));
+            console.log(data);
+            if (typeof props.setData === 'function') {
+            props.setData(data);
+            } else {
+                console.error('setData is not a function');
+            }
         });
-    }, [props.socket]);
+    }, []);
 
     return (
         <div>
           <h2>WebSocket Communication</h2>
-          <button onClick={handleSubmit}>submit</button>
+          <Button onClick={handleSubmit}>submit</Button>
         </div>
     );
 }
