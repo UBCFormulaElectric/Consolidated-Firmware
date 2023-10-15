d7d767f51c531227fa884296d0ab445822ecfad5
gtaharaedmonds <gtaharaedmonds@gmail.com>
2023-10-14 17:26:49 -0700

    refactor fsm

---
+++ firmware/thruna/FSM/test/test_canMsgs.cpp
@@ -0,0 +1,28 @@
+#include <gtest/gtest.h>
+#include "Test_Utils.h"
+
+extern "C"
+{
+#include "App_CanUtils.h"
+}
+
+TEST(CanMsgsTest, state_machine_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(1), CAN_MSG_FSM_VITALS_CYCLE_TIME_MS);
+}
+
+TEST(CanMsgsTest, heartbeat_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(1), CAN_MSG_FSM_VITALS_CYCLE_TIME_MS);
+}
+
+TEST(CanMsgsTest, brake_actuation_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(10), CAN_MSG_FSM_BRAKE_CYCLE_TIME_MS);
+}
+
+TEST(CanMsgsTest, flow_meter_message_frequency)
+{
+    // This includes the primary and secondary flow meter
+    ASSERT_GE(HZ_TO_MS(1), CAN_MSG_FSM_COOLANT_CYCLE_TIME_MS);
+}
