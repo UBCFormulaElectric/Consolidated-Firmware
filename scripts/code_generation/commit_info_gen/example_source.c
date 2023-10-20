3d822f6f7f4f5164b3fb5e16b2ed3308c9386a95
Liam Ilan <snowboardsheep@gmail.com>
2023-10-19 18:07:29 -0700

    Added extern int to commit_info_gen src file to make pedantic compilation happy.

--- scripts/code_generation/commit_info_gen/example_source.c
+++ scripts/code_generation/commit_info_gen/example_source.c
@@ -3,3 +3,6 @@
   This file is intentionally empty,
   it only exists to tell cmake to scan 
 */
+
+// to make pedantic compilation happy (avoid empty translation unit error)
+extern int commit_info_happy_pedantic_compilation;
