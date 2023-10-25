070ae65bf584a9e48b0261aae9bbf75df48114a0
Shayan Ajmal <shayanajmaluni@gmail.com>
2023-10-25 16:51:27 -0700

    fixed function description

--- firmware/thruna/FSM/Inc/Io/Io_AcceleratorPedals.h
+++ firmware/thruna/FSM/Inc/Io/Io_AcceleratorPedals.h
@@ -3,8 +3,8 @@
 #include <stdbool.h>
 
 /**
- * Calculates pedal percentage by comparing the current angle to the max angle relative to the rest angle 
- * @return pedal percentage based on pqrimary accelerator pedal sensor  
+ * Calculates pedal percentage by comparing the current angle to the max angle relative to the rest angle
+ * @return pedal percentage based on primary accelerator pedal sensor
  */
 float Io_AcceleratorPedals_GetPapps(void);
 /**
@@ -13,8 +13,8 @@ float Io_AcceleratorPedals_GetPapps(void);
 bool Io_AcceleratorPedals_PappsOCSC(void);
 
 /**
- * Calculates pedal percentage by comparing the current angle to the max angle relative to the rest angle 
- * @return pedal percentage based on secondary accelerator pedal sensor  
+ * Calculates pedal percentage by comparing the current angle to the max angle relative to the rest angle
+ * @return pedal percentage based on secondary accelerator pedal sensor
  */
 float Io_AcceleratorPedals_GetSapps(void);
 
