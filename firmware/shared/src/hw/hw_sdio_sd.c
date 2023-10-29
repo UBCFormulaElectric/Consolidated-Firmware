9b0606aea9c60613c191e572af2334a4ac610aa5
Jiajun-Huang <jhuang430@qq.com>
2023-10-29 03:22:43 -0700

    fix  hw_sd_read_offset method

--- firmware/shared/src/hw/hw_sdio_sd.c
+++ firmware/shared/src/hw/hw_sdio_sd.c
@@ -17,7 +17,13 @@ SdCardStatus hw_sd_read(SdCard *sd, uint8_t *pdata, uint32_t block_addr, uint32_
 SdCardStatus hw_sd_read_offset(SdCard *sd, uint8_t *pdata, uint32_t block_addr, uint32_t offset, uint32_t size)
 {
     uint32_t     block_size = sd->hsd->SdCard.BlockSize;
-    SdCardStatus status;
+    SdCardStatus status = SD_CARD_OK;
+
+    if(size == 0)
+    {
+        return SD_CARD_OK;
+    }
+
     if (offset == 0 && (size % block_size) == 0) // easy case
     {
         status = hw_sd_read(sd, pdata, block_addr, size / block_size);
@@ -26,18 +32,23 @@ SdCardStatus hw_sd_read_offset(SdCard *sd, uint8_t *pdata, uint32_t block_addr, 
 
     // not easy case, data is in between blockes
     uint32_t end          = offset + size;
-    uint32_t total_size   = block_size + size + size % block_size; // around up the blockes
+    uint32_t total_size   = end + (abs(end - block_size)) % block_size; // around up the blockes
     uint8_t *local_buffer = malloc(total_size);                    // temp buffer
     if (local_buffer == NULL)
-        return SD_CARD_ERROR;
+    {
+        
+        status = SD_CARD_ERROR;
+        goto free_done;
+    }
 
     status = (SdCardStatus)hw_sd_read(sd, local_buffer, block_addr, total_size / block_size);
     if (status != SD_CARD_OK)
     {
-        return status;
+        goto free_done;
     }
 
     memcpy(pdata, local_buffer + offset, size);
+free_done:    
     free(local_buffer);
     return status;
 }
@@ -55,37 +66,51 @@ SdCardStatus hw_sd_write(SdCard *sd, uint8_t *pdata, uint32_t block_addr, uint32
 SdCardStatus hw_sd_write_offset(SdCard *sd, uint8_t *pdata, uint32_t block_addr, uint32_t offset, uint32_t size)
 {
     uint32_t     block_size = sd->hsd->SdCard.BlockSize;
-    SdCardStatus status;
+    SdCardStatus status = SD_CARD_OK;
+    if(size == 0)
+    {
+        return status;
+    }
+
     if (offset == 0 && (size % block_size) == 0) // easy case
     {
         status = hw_sd_write(sd, pdata, block_addr, size / block_size);
-        return (SdCardStatus)status;
+        return status;
     }
 
     // not easy case, the data is in between blocks
     uint32_t end = offset + size;
 
     // read first block and last block, put it together with write data, write to the sd card
-    uint32_t total_size   = block_size + size + size % block_size;
+    uint32_t total_size   =  end + (abs(end - block_size)) % block_size;
     uint8_t *local_buffer = malloc(total_size);
     if (local_buffer == NULL)
         return SD_CARD_ERROR;
 
-    uint32_t end_block_addr = block_addr + total_size / block_size;
+    if(total_size == block_size) // only one block
+    {  
+        status = hw_sd_read(sd, local_buffer, block_addr, 1);
+        memcpy(local_buffer + offset, pdata, size);
+        status = hw_sd_write(sd, local_buffer, block_addr, 1);
+        goto free_done;
+    }
+
+    uint32_t last_block_addr = block_addr + total_size / block_size - 1; // last 
 
     // read first block and last block
-    status = (SdCardStatus)hw_sd_read(sd, local_buffer, block_addr, 1);
+    status = hw_sd_read(sd, local_buffer, block_addr, 1); // first block
     if (status != SD_CARD_OK)
-        return status;
-    status = (SdCardStatus)hw_sd_read(sd, local_buffer + (total_size / block_size), end_block_addr, 1);
+        goto free_done;
+    status = hw_sd_read(sd, local_buffer + (last_block_addr - block_addr) * block_size, last_block_addr, 1); // last block
     if (status != SD_CARD_OK)
-        return status;
+        goto free_done;
     // copy the middle block
     memcpy(local_buffer + offset, pdata, size);
+    status = hw_sd_write(sd, local_buffer, block_addr, total_size/block_size);
 
+free_done:
     free(local_buffer);
-
-    return (SdCardStatus)status;
+    return status;
 }
 
 SdCardStatus hw_sd_erase(SdCard *sd, uint32_t start_addr, uint32_t end_addr)
