5e847217aea9db938cb6b0f015f510744f2a999a
Jiajun-Huang <jhuang430@qq.com>
2023-11-02 18:27:51 -0700

    update doc

--- firmware/shared/src/hw/hw_sdio_sd.h
+++ firmware/shared/src/hw/hw_sdio_sd.h
@@ -42,39 +42,39 @@ typedef enum
 SdCardStatus hw_sd_read(SdCard *sd, uint8_t *pdata, uint32_t block_addr, uint32_t num_blocks);
 
 /**
- * @brief   Read from sd card. The data size will be num_blocks * BlockSize
+ * @brief   write to sd card. The data size will be num_blocks * BlockSize
  * @param   sd the state struct of sd card
- * @param   pdata the base addr where the read data store to;
+ * @param   pdata the base addr where the write data from
  *                should reserve [pdata, pdata+num_blocks * BlockSize] of memory space
  * @param   block_addr the index of the block on sd card must greater then 0
  * @param   num_blocks number of block you want to read
  * @return  SD_card_status the status of the opeation
  *
  */
-SdCardStatus hw_sd_read(SdCard *sd, uint8_t *pdata, uint32_t block_addr, uint32_t num_blocks);
+SdCardStatus hw_sd_write(SdCard *sd, uint8_t *pdata, uint32_t block_addr, uint32_t num_blocks);
 
 /**
- * @brief   Write from the sd card
+ * @brief   Read within the 1 block from sd card, provided offset and size
  * @param   sd the state struct of sd card
  * @param   pdata the base addr where data write to;
  *                the data in the address space [pdata, pdata + size] will be copy to sd card
  * @param   block_addr the index of the block on sd card must greater then 0
  * @param   offset offset within a block value range [0, block_size)
- * @param   size   bytes  of the data read from
+ * @param   size   bytes of the data read (size + offset) < blocksize
  * @return SD_card_status the status of the opeation
  *
  */
-SdCardStatus hw_sd_write_offset(SdCard *sd, uint8_t *pdata, uint32_t block_addr, uint32_t offset, uint32_t size);
+SdCardStatus hw_sd_read_offset(SdCard *sd, uint8_t *pdata, uint32_t block_addr, uint32_t offset, uint32_t size);
 
 /**
- * @brief   Write from the sd card with offset and size
+ * @brief   write within the 1 block from sd card, provided offset and size
  *
  * @param   sd the state struct of sd card
  * @param   pdata the base addr where data write to;
  *                the data in the address space [pdata, pdata + size] will be copy to sd card
  * @param   block_addr the index of the block on sd card must greater then 0
  * @param   offset offset within a block [0, block_size)
- * @param   size   bytes of the data write to
+ * @param   size   bytes of the data write to (size + offset) < blocksize
  * @return SD_card_status the status of the opeation
  *
  */
