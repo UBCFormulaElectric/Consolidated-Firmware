66ee33aa95a9f058208abef004e1e0c74f7e841a
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2023-11-08 15:56:12 -0800

    remove can broadcast functions for now

--- software/dimos/src/ui/main.cpp
+++ software/dimos/src/ui/main.cpp
@@ -9,31 +9,6 @@ extern "C"
 }
 
 int main(int argc, char *argv[]) {
-	//	Io_SharedCan_Init(); // firmware specific i believe
-
-	Io_CanTx_Init(reinterpret_cast<void (*)(const CanMsg *)>(Can_Write));
-	//	Io_CanTx_EnableMode(CAN_MODE_DEFAULT, true);
-	//	App_CanTx_Init();
-	//	App_CanRx_Init();
-
-	// TODO commit hash
-	// App_CanTx_FSM_Hash_Set(GIT_COMMIT_HASH);
-	// App_CanTx_FSM_Clean_Set(GIT_COMMIT_CLEAN);
-
-	// ON REPETITION
-	// Io_CanTx_EnqueueOtherPeriodicMsgs
-	// Io_CanTx_Enqueue100HzMsgs
-	// const bool debug_mode_enabled = App_CanRx_Debug_EnableDebugMode_Get();
-	// Io_CanTx_Enqueue1HzMsgs();
-
-	// CANRX Task
-	// Io_SharedCan_DequeueCanRxMessage(&message);
-	// Io_CanRx_UpdateRxTableWithMessage(&message);
-
-	// CANTX TASK
-	// Io_SharedCan_TransmitEnqueuedCanTxMessagesFromTask();
-
-
 	QApplication a(argc, argv);
 	MainWindow w;
 	w.show();
