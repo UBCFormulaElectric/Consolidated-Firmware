a9978dd1e981f2e04a6cf049eed8c8206bb18e1b
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2023-11-11 11:23:38 -0800

    first draft

--- software/dimos/src/ui/main.cpp
+++ software/dimos/src/ui/main.cpp
@@ -6,9 +6,24 @@ extern "C"
 {
 #include "App_CanTx.h"
 #include "App_CanRx.h"
+#include "Io_CanRx.h"
+#include "Io_CanTx.h"
 }
 
+const bool GIT_COMMIT_HASH = true;
+const bool GIT_COMMIT_CLEAN = false;
+
 int main(int argc, char *argv[]) {
+	// IO init
+	Io_CanTx_Init(reinterpret_cast<void (*)(const CanMsg *)>(Can_Write));
+	Io_CanTx_EnableMode(CAN_MODE_DEFAULT, true);
+	// clear tables
+	App_CanTx_Init();
+	App_CanRx_Init();
+	// TODO commit hash
+	App_CanTx_dimos_Hash_Set(GIT_COMMIT_HASH);
+	App_CanTx_dimos_Clean_Set(GIT_COMMIT_CLEAN);
+
 	QApplication a(argc, argv);
 	MainWindow w;
 	w.show();
