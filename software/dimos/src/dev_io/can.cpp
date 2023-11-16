6f2a5ac9c5821e0c14d8e2aec4e5f14d751d37b1
Edwin <20777515+Lucien950@users.noreply.github.com>
2023-11-15 21:58:11 -0800

    Dimos CAN (#1084)
    ### Summary
    Dimos should now broadcast CAN properly.
    
    ### Changelist 
    - Main Window CAN broadcasting in time
    - Mutex locking for CAN tables
    
    ### Testing Done
    - DevIO console logging
    
    ### Resolved Issues
    
    ### Checklist
    *Please change `[ ]` to `[x]` when you are ready.*
    - [x] I have read and followed the code conventions detailed in
    [README.md](../README.md) (*This will save time for both you and the
    reviewer!*).
    - [x] If this pull request is longer then **500** lines, I have provided
    *explicit* justification in the summary above explaining why I *cannot*
    break this up into multiple pull requests (*Small PR's are faster and
    less painful for everyone involved!*).
    
    ---------
    
    Co-authored-by: peterjinweigu <petergujinwei@gmail.com>

--- software/dimos/src/dev_io/can.cpp
+++ software/dimos/src/dev_io/can.cpp
@@ -1,14 +1,22 @@
 #include "can.h"
+#include <iostream>
+#include <thread>
+#include <chrono>
 
+using std::cout, std::endl;
 
 Result<std::monostate, CanConnectionError> Can_Init() {
+	cout << "Can Initialized" << endl;
 	return std::monostate{};
 }
 
 Result<CanMsg, CanReadError> Can_Read() {
-	return CanMsg{};
+	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+	cout << "Can Read Requested" << endl;
+	return CanMsg{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 }
 
 Result<std::monostate, CanWriteError> Can_Write(const CanMsg *msg) {
+	cout << "Can with id " << msg->std_id << " Written" << endl;
 	return std::monostate{};
 }
--- software/dimos/src/ui/mainwindow/mainwindow.cpp
+++ software/dimos/src/ui/mainwindow/mainwindow.cpp
@@ -1,13 +1,63 @@
-#include <iostream>
+#include <QThread>
+#include <chrono>
 
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include "can.h"
 
-using std::cout, std::endl;
+extern "C" {
+#include "Io_CanTx.h"
+#include "Io_CanRx.h"
+}
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
+	// CANTX TASK
+	tx100Hz.setInterval(10);
+	tx100Hz.setSingleShot(false);
+	QTimer::connect(&tx100Hz, &QTimer::timeout, Io_CanTx_Enqueue100HzMsgs);
+	tx100Hz.start();
+
+	tx1Hz.setInterval(1000);
+	tx1Hz.setSingleShot(false);
+	QTimer::connect(&tx1Hz, &QTimer::timeout, Io_CanTx_Enqueue1HzMsgs);
+	tx1Hz.start();
+
+	CanRxTaskThread = QThread::create(&MainWindow::CanRXTask);
+	CanTxPeriodicTaskThread = QThread::create(&MainWindow::CanPeriodicTXTask);
+	CanRxTaskThread->start();
+	CanTxPeriodicTaskThread->start();
+
 	ui->setupUi(this);
 }
 
-MainWindow::~MainWindow() = default;
+[[noreturn]] void MainWindow::CanRXTask() {
+	while (true) {
+		Result<CanMsg, CanReadError> res = Can_Read();
+		if (res.index() == 1) {
+			switch (get<CanReadError>(res)) {
+				case ReadInterfaceNotCreated:
+					break;
+				case SocketReadError:
+				case IncompleteCanFrame:
+					continue;
+			}
+			continue;
+		}
 
+		// success
+		CanMsg message = get<CanMsg>(res);
+		// acquire lock
+		Io_CanRx_UpdateRxTableWithMessage(&message);
+		// release lock
+	}
+}
+
+[[noreturn]] void MainWindow::CanPeriodicTXTask() {
+	using namespace std::chrono;
+	while (true) {
+		auto ms = duration_cast<milliseconds>(system_clock::now().time_since_epoch());
+		Io_CanTx_EnqueueOtherPeriodicMsgs(ms.count());
+	}
+}
+
+MainWindow::~MainWindow() = default;
