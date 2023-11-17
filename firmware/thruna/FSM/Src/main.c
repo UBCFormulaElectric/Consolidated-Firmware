d6d7bfa538405803dabf0552472f9738f338ad03
Shayan Ajmal <101350779+shayana18@users.noreply.github.com>
2023-11-17 14:35:26 -0800

    Shayan/heartbeat fault fix (#1082)
    ### Summary
    Removed heartbeat delays, fixed start up settle fault count and added
    some safety measures for pedal angle calculation
    
    ### Changelist 
    - Removed Hal delays from BMS, PDM and FSM as they were causing missing
    heart beat faults at start up
    - moved accumulator and traction system fault counting to after the
    settle period (this way faults in the settle period will not be
    recorded)
    - added a clamping function to the input of acos in the calc angle
    function of Io_AcceleratorPedal.C as a safety measure
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    ### Resolved Issues
    Extensive testing was done, if you would like to see the results please
    contact me and I will send you the CAN result screenshots (Will not
    include here because there are more than 10)
    
    ### Checklist
    - syntax
    - Clamp implementation

--- firmware/thruna/FSM/Src/main.c
+++ firmware/thruna/FSM/Src/main.c
@@ -227,7 +227,7 @@ int main(void)
     SystemClock_Config();
 
     /* USER CODE BEGIN SysInit */
-    HAL_Delay(1000U);
+
     /* USER CODE END SysInit */
 
     /* Initialize all configured peripherals */
