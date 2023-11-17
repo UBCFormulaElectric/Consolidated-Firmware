4e3a1e58b9f7f02fe6f1d5cdbe97a01f98c2d79d
gtaharaedmonds <gtaharaedmonds@gmail.com>
2023-11-16 16:15:02 -0800

    track ioc checksums

--- firmware/dev/h7dev/src/cubemx/Src/stm32h7xx_it.c
+++ firmware/dev/h7dev/src/cubemx/Src/stm32h7xx_it.c
@@ -22,7 +22,7 @@
 #include "stm32h7xx_it.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "Io_SharedHardFaultHandler.h"
+#include "hw_hardFaultHandler.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -86,7 +86,7 @@ void NMI_Handler(void)
 void HardFault_Handler(void)
 {
     /* USER CODE BEGIN HardFault_IRQn 0 */
-    Io_SharedHardFaultHandler_HandleHardFault();
+    hw_hardFaultHandler_handleFault();
     /* USER CODE END HardFault_IRQn 0 */
     while (1)
     {
