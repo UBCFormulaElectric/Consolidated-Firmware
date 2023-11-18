398e8052a289ca04dec338fae8a3e512ae005542
gtaharaedmonds <gtaharaedmonds@gmail.com>
2023-11-17 18:55:02 -0800

    undo

--- firmware/thruna/FSM/Src/main.c
+++ firmware/thruna/FSM/Src/main.c
@@ -227,7 +227,7 @@ int main(void)
     SystemClock_Config();
 
     /* USER CODE BEGIN SysInit */
-
+    HAL_Delay(1000U);
     /* USER CODE END SysInit */
 
     /* Initialize all configured peripherals */
@@ -748,14 +748,14 @@ static void MX_GPIO_Init(void)
     __HAL_RCC_GPIOB_CLK_ENABLE();
 
     /*Configure GPIO pin Output Level */
-    HAL_GPIO_WritePin(GPIOC, LED_Pin | GPIO_PIN_2, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_RESET);
 
-    /*Configure GPIO pins : LED_Pin PC2 */
-    GPIO_InitStruct.Pin   = LED_Pin | GPIO_PIN_2;
+    /*Configure GPIO pin : LED_Pin */
+    GPIO_InitStruct.Pin   = LED_Pin;
     GPIO_InitStruct.Mode  = GPIO_MODE_OUTPUT_PP;
     GPIO_InitStruct.Pull  = GPIO_NOPULL;
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+    HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
 
     /*Configure GPIO pins : BSPD_BRAKE_PRESSED_5V_Pin BRAKE_OCSC_OK_5V_Pin */
     GPIO_InitStruct.Pin  = BSPD_BRAKE_PRESSED_5V_Pin | BRAKE_OCSC_OK_5V_Pin;
