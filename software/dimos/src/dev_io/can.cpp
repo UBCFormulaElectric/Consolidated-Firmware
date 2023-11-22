1eb1f472839826e73892b2636b15f23b02312a6f
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2023-11-21 21:28:00 -0800

    Refactor CAN driver (#944)
    ### Summary
    
    The current CAN driver implementation has a few problems:
    1. Tightly coupled with FreeRTOS primitives which makes it difficult to
    read and understand.
    2. Not much documentation on how it works.
    
    I've tried to fix both these problems by: 
    1. Using CMSIS OS v2 API instead.
    2. Added some basic documentation on how the driver works (see
    `io_can.h`).
    3. General refactoring: Remove an unnecessary semaphore, comply with new
    coding standard, etc.
    
    All in all this should simplify our CAN driver a lot, without
    significantly changing functionality.
    
    (only fixed the DIM for new driver)
    
    ### Testing Done
    
    - [x] Test on the car

--- software/dimos/src/dev_io/can.cpp
+++ software/dimos/src/dev_io/can.cpp
@@ -10,13 +10,13 @@ Result<std::monostate, CanConnectionError> Can_Init() {
 	return std::monostate{};
 }
 
-Result<CanMsg, CanReadError> Can_Read() {
+Result<JsonCanMsg, CanReadError> Can_Read() {
 	std::this_thread::sleep_for(std::chrono::milliseconds(1000));
 	cout << "Can Read Requested" << endl;
-	return CanMsg{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+	return JsonCanMsg{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 }
 
-Result<std::monostate, CanWriteError> Can_Write(const CanMsg *msg) {
+Result<std::monostate, CanWriteError> Can_Write(const JsonCanMsg *msg) {
 	cout << "Can with id " << msg->std_id << " Written" << endl;
 	return std::monostate{};
 }
