f2bb495ac1886624511e7bd01bd8040f88bf0f30
Jiajun Huang <jhuang430@qq.com>
2023-11-25 10:55:27 -0800

    temp save

--- firmware/dev/f4dev/src/cubemx/Src/main.c
+++ firmware/dev/f4dev/src/cubemx/Src/main.c
@@ -25,10 +25,10 @@
 /* USER CODE BEGIN Includes */
 #include "hw_sd.h"
 #include "hw_bootup.h"
+// #define LFS_NO_MALLOC
 #include "lfs.h"
-#define LFS_NO_MALLOC
-#define LFS_CACHE_SIZE 16
-#define LFS_LOOKAHEAD_SIZE 16
+#define LFS_CACHE_SIZE 256
+#define LFS_LOOKAHEAD_SIZE 256
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -43,7 +43,7 @@
 
 /* Private macro -------------------------------------------------------------*/
 /* USER CODE BEGIN PM */
-SdCard  sd;
+SdCard sd;
 uint8_t read_buffer[LFS_CACHE_SIZE];
 uint8_t prog_buffer[LFS_CACHE_SIZE];
 uint8_t lookahead_buffer[LFS_LOOKAHEAD_SIZE];
@@ -58,23 +58,23 @@ CAN_HandleTypeDef hcan2;
 SD_HandleTypeDef hsd;
 
 /* Definitions for defaultTask */
-osThreadId_t         defaultTaskHandle;
+osThreadId_t defaultTaskHandle;
 const osThreadAttr_t defaultTask_attributes = {
-    .name       = "defaultTask",
+    .name = "defaultTask",
     .stack_size = 128 * 4,
-    .priority   = (osPriority_t)osPriorityNormal,
+    .priority = (osPriority_t)osPriorityNormal,
 };
 /* USER CODE BEGIN PV */
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
-void        SystemClock_Config(void);
+void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_SDIO_SD_Init(void);
 static void MX_CAN1_Init(void);
 static void MX_ADC1_Init(void);
 static void MX_CAN2_Init(void);
-void        StartDefaultTask(void *argument);
+void StartDefaultTask(void *argument);
 
 /* USER CODE BEGIN PFP */
 
@@ -91,19 +91,19 @@ int lfs_read(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, void 
 {
     SdCardStatus status = hw_sd_readOffset(&sd, (uint8_t *)buffer, (uint32_t)block, (uint32_t)off, (uint32_t)size);
 
-    return 0;
+    return status;
 }
 
 int lfs_prog(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size)
 {
     SdCardStatus status = hw_sd_writeOffset(&sd, (uint8_t *)buffer, (uint32_t)block, (uint32_t)off, (uint32_t)size);
-    return 0;
+    return status;
 }
 
 int lfs_erase(const struct lfs_config *c, lfs_block_t block)
 {
     SdCardStatus status = hw_sd_erase(&sd, (uint32_t)block, (uint32_t)block);
-    return 0;
+    return status;
 }
 
 int lfs_sync(const struct lfs_config *c)
@@ -113,26 +113,26 @@ int lfs_sync(const struct lfs_config *c)
 
 struct lfs_config cfg = {
     // block device operations
-    .read  = lfs_read,
-    .prog  = lfs_prog,
+    .read = lfs_read,
+    .prog = lfs_prog,
     .erase = lfs_erase,
-    .sync  = lfs_sync,
+    .sync = lfs_sync,
 
     // block device configuration
-    .read_size        = 16,
-    .prog_size        = 16,
-    .block_size       = 4096,
-    .block_count      = 128,
-    .cache_size       = LFS_CACHE_SIZE,
-    .lookahead_size   = LFS_LOOKAHEAD_SIZE,
-    .block_cycles     = 500,
-    .read_buffer      = (void *)read_buffer,
-    .prog_buffer      = (void *)prog_buffer,
+    .read_size = 512,
+    .prog_size = 512,
+    .block_size = 512,
+    .block_count = 1024,
+    .cache_size = LFS_CACHE_SIZE,
+    .lookahead_size = LFS_LOOKAHEAD_SIZE,
+    .block_cycles = 500,
+    .read_buffer = (void *)read_buffer,
+    .prog_buffer = (void *)prog_buffer,
     .lookahead_buffer = (void *)lookahead_buffer,
 
 };
 
-lfs_t      lfs;
+lfs_t lfs;
 lfs_file_t file;
 /* USER CODE END 0 */
 
@@ -168,14 +168,24 @@ int main(void)
     MX_ADC1_Init();
     MX_CAN2_Init();
     /* USER CODE BEGIN 2 */
-    sd.hsd     = &hsd;
-    sd.timeout = 100;
+    sd.hsd = &hsd;
+    sd.timeout = osWaitForever;
+
+    HAL_SD_CardStateTypeDef sd_state = HAL_SD_GetCardState(&hsd);
+
+    // uint8_t write_buffer[512];
+    // memset(write_buffer, 0U, sizeof(write_buffer));
+    // write_buffer[0] = 7;
+    // uint8_t buffer[512];
+
+    // SdCardStatus status_write = hw_sd_read(&sd, (uint8_t *)write_buffer, 0, 1);
+    // SdCardStatus status_read = hw_sd_read(&sd, (uint8_t *)buffer, 0, 1);
 
     // config littlefs
-    cfg.block_size  = sd.hsd->SdCard.BlockSize;
-    cfg.block_count = sd.hsd->SdCard.BlockNbr;
+    cfg.block_size = sd.hsd->SdCard.BlockSize;
+    // cfg.block_count = sd.hsd->SdCard.BlockNbr;
 
-    // mount the filesystem
+
     int err = lfs_mount(&lfs, &cfg);
     // write the hello world
     if (err)
@@ -188,22 +198,44 @@ int main(void)
 
     // read current count
     uint32_t boot_count = 0;
-    // mkdir
-    err = lfs_mkdir(&lfs, "helloworld_dir");
+    uint32_t boot_count1 = 0;
+    char buffer[512];
+    err = lfs_file_open(&lfs, &file, "boot_count.txt", LFS_O_RDWR | LFS_O_CREAT);
+    err = lfs_file_read(&lfs, &file, &boot_count, sizeof(boot_count));
+    err = lfs_file_rewind(&lfs, &file);
+    err = lfs_file_close(&lfs, &file);
 
-    err = lfs_file_open(&lfs, &file, "boot_count", LFS_O_RDWR | LFS_O_CREAT);
-    if (!err)
-    {
-        err = lfs_file_read(&lfs, &file, &boot_count, sizeof(boot_count));
-    }
+    err = lfs_mkdir(&lfs, "j");
+    err = lfs_file_open(&lfs, &file, "j/boot_count.txt", LFS_O_RDWR | LFS_O_CREAT);
+    err = lfs_file_read(&lfs, &file, &boot_count1, sizeof(boot_count));
+    err = lfs_file_rewind(&lfs, &file);
+    err = lfs_file_close(&lfs, &file);
+
+    // err = lfs_file_open(&lfs, &file, "first-file.txt", LFS_O_RDWR);
+    // err = lfs_file_read(&lfs, &file, &buffer, sizeof(boot_count));
+    // err = lfs_file_rewind(&lfs, &file);
+    // err = lfs_file_close(&lfs, &file);
 
     // update boot count
     boot_count += 1;
+    boot_count1 += 1;
+
+    err = lfs_file_open(&lfs, &file, "boot_count.txt", LFS_O_RDWR);
+    err = lfs_file_write(&lfs, &file, &boot_count, sizeof(boot_count));
     err = lfs_file_rewind(&lfs, &file);
-    if (!err)
-    {
-        err = lfs_file_write(&lfs, &file, &boot_count, sizeof(boot_count));
-    }
+    err = lfs_file_close(&lfs, &file);
+
+    err = lfs_file_open(&lfs, &file, "j/boot_count.txt", LFS_O_RDWR);
+    err = lfs_file_write(&lfs, &file, &boot_count1, sizeof(boot_count));
+    err = lfs_file_rewind(&lfs, &file);
+    err = lfs_file_close(&lfs, &file);
+
+    // remember the storage is not updated until the file is closed successfully
+    // release any resources we were using
+    lfs_unmount(&lfs);
+
+    // print the boot count
+    // printf("boot_count: %d\n", boot_count);
 
     /* USER CODE END 2 */
 
@@ -259,8 +291,8 @@ int main(void)
  */
 void SystemClock_Config(void)
 {
-    RCC_OscInitTypeDef RCC_OscInitStruct = { 0 };
-    RCC_ClkInitTypeDef RCC_ClkInitStruct = { 0 };
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
     /** Configure the main internal regulator output voltage
      */
@@ -271,14 +303,14 @@ void SystemClock_Config(void)
      * in the RCC_OscInitTypeDef structure.
      */
     RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-    RCC_OscInitStruct.HSEState       = RCC_HSE_ON;
-    RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
-    RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
-    RCC_OscInitStruct.PLL.PLLM       = 8;
-    RCC_OscInitStruct.PLL.PLLN       = 192;
-    RCC_OscInitStruct.PLL.PLLP       = RCC_PLLP_DIV2;
-    RCC_OscInitStruct.PLL.PLLQ       = 15;
-    RCC_OscInitStruct.PLL.PLLR       = 2;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = 8;
+    RCC_OscInitStruct.PLL.PLLN = 192;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = 12;
+    RCC_OscInitStruct.PLL.PLLR = 2;
     if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
     {
         Error_Handler();
@@ -287,8 +319,8 @@ void SystemClock_Config(void)
     /** Initializes the CPU, AHB and APB buses clocks
      */
     RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
-    RCC_ClkInitStruct.SYSCLKSource   = RCC_SYSCLKSOURCE_PLLCLK;
-    RCC_ClkInitStruct.AHBCLKDivider  = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
     RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
     RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
@@ -315,18 +347,18 @@ static void MX_ADC1_Init(void)
 
     /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion)
      */
-    hadc1.Instance                   = ADC1;
-    hadc1.Init.ClockPrescaler        = ADC_CLOCK_SYNC_PCLK_DIV4;
-    hadc1.Init.Resolution            = ADC_RESOLUTION_8B;
-    hadc1.Init.ScanConvMode          = DISABLE;
-    hadc1.Init.ContinuousConvMode    = DISABLE;
+    hadc1.Instance = ADC1;
+    hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV4;
+    hadc1.Init.Resolution = ADC_RESOLUTION_8B;
+    hadc1.Init.ScanConvMode = DISABLE;
+    hadc1.Init.ContinuousConvMode = DISABLE;
     hadc1.Init.DiscontinuousConvMode = DISABLE;
-    hadc1.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_NONE;
-    hadc1.Init.ExternalTrigConv      = ADC_SOFTWARE_START;
-    hadc1.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
-    hadc1.Init.NbrOfConversion       = 1;
+    hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+    hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+    hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+    hadc1.Init.NbrOfConversion = 1;
     hadc1.Init.DMAContinuousRequests = DISABLE;
-    hadc1.Init.EOCSelection          = ADC_EOC_SINGLE_CONV;
+    hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
     if (HAL_ADC_Init(&hadc1) != HAL_OK)
     {
         Error_Handler();
@@ -350,17 +382,17 @@ static void MX_CAN1_Init(void)
     /* USER CODE BEGIN CAN1_Init 1 */
 
     /* USER CODE END CAN1_Init 1 */
-    hcan1.Instance                  = CAN1;
-    hcan1.Init.Prescaler            = 16;
-    hcan1.Init.Mode                 = CAN_MODE_NORMAL;
-    hcan1.Init.SyncJumpWidth        = CAN_SJW_1TQ;
-    hcan1.Init.TimeSeg1             = CAN_BS1_1TQ;
-    hcan1.Init.TimeSeg2             = CAN_BS2_1TQ;
-    hcan1.Init.TimeTriggeredMode    = DISABLE;
-    hcan1.Init.AutoBusOff           = DISABLE;
-    hcan1.Init.AutoWakeUp           = DISABLE;
-    hcan1.Init.AutoRetransmission   = DISABLE;
-    hcan1.Init.ReceiveFifoLocked    = DISABLE;
+    hcan1.Instance = CAN1;
+    hcan1.Init.Prescaler = 16;
+    hcan1.Init.Mode = CAN_MODE_NORMAL;
+    hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
+    hcan1.Init.TimeSeg1 = CAN_BS1_1TQ;
+    hcan1.Init.TimeSeg2 = CAN_BS2_1TQ;
+    hcan1.Init.TimeTriggeredMode = DISABLE;
+    hcan1.Init.AutoBusOff = DISABLE;
+    hcan1.Init.AutoWakeUp = DISABLE;
+    hcan1.Init.AutoRetransmission = DISABLE;
+    hcan1.Init.ReceiveFifoLocked = DISABLE;
     hcan1.Init.TransmitFifoPriority = DISABLE;
     if (HAL_CAN_Init(&hcan1) != HAL_OK)
     {
@@ -385,17 +417,17 @@ static void MX_CAN2_Init(void)
     /* USER CODE BEGIN CAN2_Init 1 */
 
     /* USER CODE END CAN2_Init 1 */
-    hcan2.Instance                  = CAN2;
-    hcan2.Init.Prescaler            = 16;
-    hcan2.Init.Mode                 = CAN_MODE_NORMAL;
-    hcan2.Init.SyncJumpWidth        = CAN_SJW_1TQ;
-    hcan2.Init.TimeSeg1             = CAN_BS1_1TQ;
-    hcan2.Init.TimeSeg2             = CAN_BS2_1TQ;
-    hcan2.Init.TimeTriggeredMode    = DISABLE;
-    hcan2.Init.AutoBusOff           = DISABLE;
-    hcan2.Init.AutoWakeUp           = DISABLE;
-    hcan2.Init.AutoRetransmission   = DISABLE;
-    hcan2.Init.ReceiveFifoLocked    = DISABLE;
+    hcan2.Instance = CAN2;
+    hcan2.Init.Prescaler = 16;
+    hcan2.Init.Mode = CAN_MODE_NORMAL;
+    hcan2.Init.SyncJumpWidth = CAN_SJW_1TQ;
+    hcan2.Init.TimeSeg1 = CAN_BS1_1TQ;
+    hcan2.Init.TimeSeg2 = CAN_BS2_1TQ;
+    hcan2.Init.TimeTriggeredMode = DISABLE;
+    hcan2.Init.AutoBusOff = DISABLE;
+    hcan2.Init.AutoWakeUp = DISABLE;
+    hcan2.Init.AutoRetransmission = DISABLE;
+    hcan2.Init.ReceiveFifoLocked = DISABLE;
     hcan2.Init.TransmitFifoPriority = DISABLE;
     if (HAL_CAN_Init(&hcan2) != HAL_OK)
     {
@@ -420,18 +452,14 @@ static void MX_SDIO_SD_Init(void)
     /* USER CODE BEGIN SDIO_Init 1 */
 
     /* USER CODE END SDIO_Init 1 */
-    hsd.Instance                 = SDIO;
-    hsd.Init.ClockEdge           = SDIO_CLOCK_EDGE_RISING;
-    hsd.Init.ClockBypass         = SDIO_CLOCK_BYPASS_DISABLE;
-    hsd.Init.ClockPowerSave      = SDIO_CLOCK_POWER_SAVE_DISABLE;
-    hsd.Init.BusWide             = SDIO_BUS_WIDE_4B;
-    hsd.Init.HardwareFlowControl = SDIO_HARDWARE_FLOW_CONTROL_DISABLE;
-    hsd.Init.ClockDiv            = 0;
+    hsd.Instance = SDIO;
+    hsd.Init.ClockEdge = SDIO_CLOCK_EDGE_RISING;
+    hsd.Init.ClockBypass = SDIO_CLOCK_BYPASS_DISABLE;
+    hsd.Init.ClockPowerSave = SDIO_CLOCK_POWER_SAVE_DISABLE;
+    hsd.Init.BusWide = SDIO_BUS_WIDE_1B;
+    hsd.Init.HardwareFlowControl = SDIO_HARDWARE_FLOW_CONTROL_ENABLE;
+    hsd.Init.ClockDiv = 32;
     if (HAL_SD_Init(&hsd) != HAL_OK)
-    {
-        Error_Handler();
-    }
-    if (HAL_SD_ConfigWideBusOperation(&hsd, SDIO_BUS_WIDE_4B) != HAL_OK)
     {
         Error_Handler();
     }
@@ -447,7 +475,7 @@ static void MX_SDIO_SD_Init(void)
  */
 static void MX_GPIO_Init(void)
 {
-    GPIO_InitTypeDef GPIO_InitStruct = { 0 };
+    GPIO_InitTypeDef GPIO_InitStruct = {0};
     /* USER CODE BEGIN MX_GPIO_Init_1 */
     /* USER CODE END MX_GPIO_Init_1 */
 
@@ -459,44 +487,50 @@ static void MX_GPIO_Init(void)
     __HAL_RCC_GPIOD_CLK_ENABLE();
 
     /*Configure GPIO pins : PA2 PA3 */
-    GPIO_InitStruct.Pin       = GPIO_PIN_2 | GPIO_PIN_3;
-    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull      = GPIO_NOPULL;
-    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Pin = GPIO_PIN_2 | GPIO_PIN_3;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
     /*Configure GPIO pins : PA5 PA6 PA7 */
-    GPIO_InitStruct.Pin       = GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7;
-    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull      = GPIO_NOPULL;
-    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Pin = GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF5_SPI1;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
     /*Configure GPIO pins : PB0 PB1 */
-    GPIO_InitStruct.Pin       = GPIO_PIN_0 | GPIO_PIN_1;
-    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull      = GPIO_NOPULL;
-    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Pin = GPIO_PIN_0 | GPIO_PIN_1;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
     HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
     /*Configure GPIO pins : PC6 PC7 */
-    GPIO_InitStruct.Pin       = GPIO_PIN_6 | GPIO_PIN_7;
-    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull      = GPIO_NOPULL;
-    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
+    GPIO_InitStruct.Pin = GPIO_PIN_6 | GPIO_PIN_7;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     GPIO_InitStruct.Alternate = GPIO_AF2_TIM3;
     HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
     /*Configure GPIO pins : PA11 PA12 */
-    GPIO_InitStruct.Pin       = GPIO_PIN_11 | GPIO_PIN_12;
-    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-    GPIO_InitStruct.Pull      = GPIO_NOPULL;
-    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Pin = GPIO_PIN_11 | GPIO_PIN_12;
+    GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
     GPIO_InitStruct.Alternate = GPIO_AF10_OTG_FS;
     HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+    /*Configure GPIO pin : PC12 */
+    GPIO_InitStruct.Pin = GPIO_PIN_12;
+    GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+    GPIO_InitStruct.Pull = GPIO_NOPULL;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
     /* USER CODE BEGIN MX_GPIO_Init_2 */
     /* USER CODE END MX_GPIO_Init_2 */
