2b631f6f2598aadb3e9c71fd2fa1c598743fa6df
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2023-11-25 10:08:17 -0800

    Refactor PDM (#1043)
    ### Summary
    <!-- Quick summary of changes, optional -->
    
    Refactor the PDM to be more inline with the new coding structure (like
    we have on the DIM).
    
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    PDM app and IO layer folows the new structure.
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    - [x] Builds and tests pass
    - [x] Testing on car
    
    ### Resolved Issues
    <!-- Link any issues that this PR resolved like so: `Resolves #1, #2,
    and #5` (Note: Using this format, Github will automatically close the
    issue(s) when this PR is merged in). -->
    
    ### Checklist
    *Please change `[ ]` to `[x]` when you are ready.*
    - [x] I have read and followed the code conventions detailed in
    [README.md](../README.md) (*This will save time for both you and the
    reviewer!*).
    - [x] If this pull request is longer then **500** lines, I have provided
    *explicit* justification in the summary above explaining why I *cannot*
    break this up into multiple pull requests (*Small PR's are faster and
    less painful for everyone involved!*).

--- firmware/thruna/PDM/Inc/App/App_SetPeriodicCanSignals.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-#include "App_PdmWorld.h"
-
-void App_SetPeriodicCanSignals_CurrentInRangeChecks(const struct PdmWorld *world);
-void App_SetPeriodicCanSignals_VoltageInRangeChecks(const struct PdmWorld *world);
--- firmware/thruna/PDM/Inc/App/configs/App_VoltageLimits.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#define VBAT_MIN_VOLTAGE 6.0f
-#define VBAT_MAX_VOLTAGE 8.5f
-#define _24V_AUX_MIN_VOLTAGE 22.0f
-#define _24V_AUX_MAX_VOLTAGE 26.0f
-#define _24V_ACC_MIN_VOLTAGE 22.0f
-#define _24V_ACC_MAX_VOLTAGE 26.0f
