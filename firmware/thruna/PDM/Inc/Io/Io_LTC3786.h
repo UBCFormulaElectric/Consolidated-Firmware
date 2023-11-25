f6af4af96eb77ae717e2a0e0cf2141122727272a
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2023-11-25 10:08:17 -0800

    Refactor PDM (#1043)
    ### Summary
    <!-- Quick summary of changes, optional -->
    
    Refactor the PDM to be more inline with the new coding structure (like
    we have on the DIM).
    
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    PDM app and IO layer folows the new structure.
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    - [x] Builds and tests pass
    - [x] Testing on car
    
    ### Resolved Issues
    <!-- Link any issues that this PR resolved like so: `Resolves #1, #2,
    and #5` (Note: Using this format, Github will automatically close the
    issue(s) when this PR is merged in). -->
    
    ### Checklist
    *Please change `[ ]` to `[x]` when you are ready.*
    - [x] I have read and followed the code conventions detailed in
    [README.md](../README.md) (*This will save time for both you and the
    reviewer!*).
    - [x] If this pull request is longer then **500** lines, I have provided
    *explicit* justification in the summary above explaining why I *cannot*
    break this up into multiple pull requests (*Small PR's are faster and
    less painful for everyone involved!*).

--- firmware/thruna/PDM/Inc/Io/Io_LTC3786.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdbool.h>
-
-/**
- * Check if the LTC3786 has a fault
- * @return true if the LTC3786 has a fault, else false
- */
-bool Io_LTC3786_HasFault(void);
---
+++ firmware/thruna/PDM/src/hw/hw_adc.c
@@ -0,0 +1,44 @@
+#include "hw_adc.h"
+#include <assert.h>
+#include "hw_hal.h"
+#include "hw_adcConversions.h"
+
+static uint16_t raw_adc_values[NUM_ADC_CHANNELS];
+static float    adc_voltages[NUM_ADC_CHANNELS];
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
+{
+    adc_voltages[ADC_1_CHANNEL_4] =
+        hw_adcConversions_rawAdcValueToVoltage(hadc, false, raw_adc_values[ADC_1_CHANNEL_4]);
+    adc_voltages[ADC_1_CHANNEL_5] =
+        hw_adcConversions_rawAdcValueToVoltage(hadc, false, raw_adc_values[ADC_1_CHANNEL_5]);
+    adc_voltages[ADC_1_CHANNEL_6] =
+        hw_adcConversions_rawAdcValueToVoltage(hadc, false, raw_adc_values[ADC_1_CHANNEL_6]);
+    adc_voltages[ADC_1_CHANNEL_7] =
+        hw_adcConversions_rawAdcValueToVoltage(hadc, false, raw_adc_values[ADC_1_CHANNEL_7]);
+    adc_voltages[ADC_1_CHANNEL_8] =
+        hw_adcConversions_rawAdcValueToVoltage(hadc, false, raw_adc_values[ADC_1_CHANNEL_8]);
+    adc_voltages[ADC_1_CHANNEL_9] =
+        hw_adcConversions_rawAdcValueToVoltage(hadc, false, raw_adc_values[ADC_1_CHANNEL_9]);
+    adc_voltages[ADC_1_CHANNEL_10] =
+        hw_adcConversions_rawAdcValueToVoltage(hadc, false, raw_adc_values[ADC_1_CHANNEL_10]);
+    adc_voltages[ADC_1_CHANNEL_11] =
+        hw_adcConversions_rawAdcValueToVoltage(hadc, false, raw_adc_values[ADC_1_CHANNEL_11]);
+    adc_voltages[ADC_1_CHANNEL_12] =
+        hw_adcConversions_rawAdcValueToVoltage(hadc, false, raw_adc_values[ADC_1_CHANNEL_12]);
+    adc_voltages[ADC_1_CHANNEL_14] =
+        hw_adcConversions_rawAdcValueToVoltage(hadc, false, raw_adc_values[ADC_1_CHANNEL_14]);
+    adc_voltages[ADC_1_CHANNEL_15] =
+        hw_adcConversions_rawAdcValueToVoltage(hadc, false, raw_adc_values[ADC_1_CHANNEL_15]);
+}
+
+uint16_t *hw_adc_getRawValuesBuffer(void)
+{
+    return raw_adc_values;
+}
+
+float hw_adc_getVoltage(AdcChannel channel)
+{
+    assert(channel < NUM_ADC_CHANNELS);
+    return adc_voltages[channel];
+}
