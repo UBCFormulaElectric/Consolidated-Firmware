5f1b0b201b9d578cd93e4ad043d5950d9bebc9bb
Amir Tajaddoditalab <96522772+Mighty199@users.noreply.github.com>
2023-12-03 21:03:52 -0800

    Delete build_x86 directory

--- build_x86/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amirt/Consolidated-Firmware
-# Build directory: /home/amirt/Consolidated-Firmware/build_x86
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("_deps/googletest-build")
-subdirs("firmware")
-subdirs("scripts/code_generation/fakegen")
