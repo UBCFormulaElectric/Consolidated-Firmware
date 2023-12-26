dc4b5065e5efdb24f654cd47bd650a38a7108daf
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2023-12-25 22:32:26 -0800

    FDCAN support on H7 (#1102)
    ### Summary
    <!-- Quick summary of changes, optional -->
    
    The STM32H733VGTx that we are using on the vehicle controller and the
    BMS support FDCAN, which means they use a different HAL driver. This PR
    adds a new `hw_fdcan.c` file which is functionally equivalent to
    `hw_can.c`, except for the FDCAN peripheral.
    
    Note: Although we are using the FDCAN peripheral, this only adds support
    for standard CAN (FDCAN is a superset of standard CAN).
    
    Note 2: @Jugveer-Sandher wrote about 99% of this, I ended up taking over
    due to suspected hardware issue, after many hours of debugging it was
    just a dead transceiver :(
    
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    - Add shared `hw_fdcan.c` which adds driver-level CAN support.
    - Setup IO-level CAN queues on the H7 dev board as a demo.
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    - [x] Testing on bench with H7 dev board
    
    ### Resolved Issues
    <!-- Link any issues that this PR resolved like so: `Resolves #1, #2,
    and #5` (Note: Using this format, Github will automatically close the
    issue(s) when this PR is merged in). -->
    
    ### Checklist
    *Please change `[ ]` to `[x]` when you are ready.*
    - [x] I have read and followed the code conventions detailed in
    [README.md](../README.md) (*This will save time for both you and the
    reviewer!*).
    - [x] If this pull request is longer then **500** lines, I have provided
    *explicit* justification in the summary above explaining why I *cannot*
    break this up into multiple pull requests (*Small PR's are faster and
    less painful for everyone involved!*).

--- firmware/dev/h7dev/src/cubemx/Src/main.c
+++ firmware/dev/h7dev/src/cubemx/Src/main.c
@@ -24,6 +24,9 @@
 /* USER CODE BEGIN Includes */
 #include "string.h"
 #include "hw_hardFaultHandler.h"
+#include "hw_can.h"
+
+#include "io_can.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -44,9 +47,11 @@ typedef StaticTask_t osStaticThreadDef_t;
 
 /* Private variables ---------------------------------------------------------*/
 
+FDCAN_HandleTypeDef hfdcan2;
+
 /* Definitions for defaultTask */
 osThreadId_t         defaultTaskHandle;
-uint32_t             defaultTaskBuffer[128];
+uint32_t             defaultTaskBuffer[512];
 osStaticThreadDef_t  defaultTaskControlBlock;
 const osThreadAttr_t defaultTask_attributes = {
     .name       = "defaultTask",
@@ -56,14 +61,45 @@ const osThreadAttr_t defaultTask_attributes = {
     .stack_size = sizeof(defaultTaskBuffer),
     .priority   = (osPriority_t)osPriorityNormal,
 };
+/* Definitions for canTxTask */
+osThreadId_t         canTxTaskHandle;
+uint32_t             canTxTaskBuffer[512];
+osStaticThreadDef_t  canTxTaskControlBlock;
+const osThreadAttr_t canTxTask_attributes = {
+    .name       = "canTxTask",
+    .cb_mem     = &canTxTaskControlBlock,
+    .cb_size    = sizeof(canTxTaskControlBlock),
+    .stack_mem  = &canTxTaskBuffer[0],
+    .stack_size = sizeof(canTxTaskBuffer),
+    .priority   = (osPriority_t)osPriorityBelowNormal,
+};
+/* Definitions for canRxTask */
+osThreadId_t         canRxTaskHandle;
+uint32_t             canRxTaskBuffer[512];
+osStaticThreadDef_t  canRxTaskControlBlock;
+const osThreadAttr_t canRxTask_attributes = {
+    .name       = "canRxTask",
+    .cb_mem     = &canRxTaskControlBlock,
+    .cb_size    = sizeof(canRxTaskControlBlock),
+    .stack_mem  = &canRxTaskBuffer[0],
+    .stack_size = sizeof(canRxTaskBuffer),
+    .priority   = (osPriority_t)osPriorityBelowNormal,
+};
 /* USER CODE BEGIN PV */
-
+static CanConfig can_config = {
+    .rx_msg_filter        = NULL,
+    .tx_overflow_callback = NULL,
+    .rx_overflow_callback = NULL,
+};
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void        SystemClock_Config(void);
 static void MX_GPIO_Init(void);
-void        StartDefaultTask(void *argument);
+static void MX_FDCAN2_Init(void);
+void        runDefaultTask(void *argument);
+void        runCanTxTask(void *argument);
+void        runCanRxTask(void *argument);
 
 /* USER CODE BEGIN PFP */
 
@@ -107,10 +143,14 @@ int main(void)
 
     /* Initialize all configured peripherals */
     MX_GPIO_Init();
+    MX_FDCAN2_Init();
     /* USER CODE BEGIN 2 */
     // __HAL_DBGMCU_FREEZE_IWDG();
 
-    hw_hardFaultHandler_handleFault();
+    hw_hardFaultHandler_init();
+    hw_can_init(&hfdcan2);
+
+    io_can_init(&can_config);
 
     // Configure and initialize SEGGER SystemView.
     SEGGER_SYSVIEW_Conf();
@@ -137,7 +177,13 @@ int main(void)
 
     /* Create the thread(s) */
     /* creation of defaultTask */
-    defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
+    defaultTaskHandle = osThreadNew(runDefaultTask, NULL, &defaultTask_attributes);
+
+    /* creation of canTxTask */
+    canTxTaskHandle = osThreadNew(runCanTxTask, NULL, &canTxTask_attributes);
+
+    /* creation of canRxTask */
+    canRxTaskHandle = osThreadNew(runCanRxTask, NULL, &canRxTask_attributes);
 
     /* USER CODE BEGIN RTOS_THREADS */
     /* add threads, ... */
@@ -191,13 +237,13 @@ void SystemClock_Config(void)
     RCC_OscInitStruct.PLL.PLLState   = RCC_PLL_ON;
     RCC_OscInitStruct.PLL.PLLSource  = RCC_PLLSOURCE_HSE;
     RCC_OscInitStruct.PLL.PLLM       = 1;
-    RCC_OscInitStruct.PLL.PLLN       = 62;
+    RCC_OscInitStruct.PLL.PLLN       = 64;
     RCC_OscInitStruct.PLL.PLLP       = 1;
     RCC_OscInitStruct.PLL.PLLQ       = 4;
     RCC_OscInitStruct.PLL.PLLR       = 2;
     RCC_OscInitStruct.PLL.PLLRGE     = RCC_PLL1VCIRANGE_3;
     RCC_OscInitStruct.PLL.PLLVCOSEL  = RCC_PLL1VCOWIDE;
-    RCC_OscInitStruct.PLL.PLLFRACN   = 4096;
+    RCC_OscInitStruct.PLL.PLLFRACN   = 0;
     if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
     {
         Error_Handler();
@@ -222,6 +268,57 @@ void SystemClock_Config(void)
 }
 
 /**
+ * @brief FDCAN2 Initialization Function
+ * @param None
+ * @retval None
+ */
+static void MX_FDCAN2_Init(void)
+{
+    /* USER CODE BEGIN FDCAN2_Init 0 */
+
+    /* USER CODE END FDCAN2_Init 0 */
+
+    /* USER CODE BEGIN FDCAN2_Init 1 */
+
+    /* USER CODE END FDCAN2_Init 1 */
+    hfdcan2.Instance                  = FDCAN2;
+    hfdcan2.Init.FrameFormat          = FDCAN_FRAME_CLASSIC;
+    hfdcan2.Init.Mode                 = FDCAN_MODE_NORMAL;
+    hfdcan2.Init.AutoRetransmission   = ENABLE;
+    hfdcan2.Init.TransmitPause        = DISABLE;
+    hfdcan2.Init.ProtocolException    = DISABLE;
+    hfdcan2.Init.NominalPrescaler     = 16;
+    hfdcan2.Init.NominalSyncJumpWidth = 4;
+    hfdcan2.Init.NominalTimeSeg1      = 13;
+    hfdcan2.Init.NominalTimeSeg2      = 2;
+    hfdcan2.Init.DataPrescaler        = 1;
+    hfdcan2.Init.DataSyncJumpWidth    = 1;
+    hfdcan2.Init.DataTimeSeg1         = 1;
+    hfdcan2.Init.DataTimeSeg2         = 1;
+    hfdcan2.Init.MessageRAMOffset     = 0;
+    hfdcan2.Init.StdFiltersNbr        = 1;
+    hfdcan2.Init.ExtFiltersNbr        = 0;
+    hfdcan2.Init.RxFifo0ElmtsNbr      = 1;
+    hfdcan2.Init.RxFifo0ElmtSize      = FDCAN_DATA_BYTES_8;
+    hfdcan2.Init.RxFifo1ElmtsNbr      = 0;
+    hfdcan2.Init.RxFifo1ElmtSize      = FDCAN_DATA_BYTES_8;
+    hfdcan2.Init.RxBuffersNbr         = 0;
+    hfdcan2.Init.RxBufferSize         = FDCAN_DATA_BYTES_8;
+    hfdcan2.Init.TxEventsNbr          = 0;
+    hfdcan2.Init.TxBuffersNbr         = 0;
+    hfdcan2.Init.TxFifoQueueElmtsNbr  = 1;
+    hfdcan2.Init.TxFifoQueueMode      = FDCAN_TX_FIFO_OPERATION;
+    hfdcan2.Init.TxElmtSize           = FDCAN_DATA_BYTES_8;
+    if (HAL_FDCAN_Init(&hfdcan2) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    /* USER CODE BEGIN FDCAN2_Init 2 */
+
+    /* USER CODE END FDCAN2_Init 2 */
+}
+
+/**
  * @brief GPIO Initialization Function
  * @param None
  * @retval None
@@ -236,6 +333,7 @@ static void MX_GPIO_Init(void)
     __HAL_RCC_GPIOE_CLK_ENABLE();
     __HAL_RCC_GPIOH_CLK_ENABLE();
     __HAL_RCC_GPIOA_CLK_ENABLE();
+    __HAL_RCC_GPIOD_CLK_ENABLE();
     __HAL_RCC_GPIOB_CLK_ENABLE();
 
     /*Configure GPIO pin Output Level */
@@ -248,6 +346,14 @@ static void MX_GPIO_Init(void)
     GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
     HAL_GPIO_Init(LED_GPIO_Port, &GPIO_InitStruct);
 
+    /*Configure GPIO pin : PD0 */
+    GPIO_InitStruct.Pin       = GPIO_PIN_0;
+    GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
+    GPIO_InitStruct.Pull      = GPIO_NOPULL;
+    GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
+    GPIO_InitStruct.Alternate = GPIO_AF9_FDCAN1;
+    HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
     /* USER CODE BEGIN MX_GPIO_Init_2 */
     /* USER CODE END MX_GPIO_Init_2 */
 }
@@ -256,26 +362,67 @@ static void MX_GPIO_Init(void)
 
 /* USER CODE END 4 */
 
-/* USER CODE BEGIN Header_StartDefaultTask */
+/* USER CODE BEGIN Header_runDefaultTask */
 /**
  * @brief  Function implementing the defaultTask thread.
  * @param  argument: Not used
  * @retval None
  */
-/* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void *argument)
+/* USER CODE END Header_runDefaultTask */
+void runDefaultTask(void *argument)
 {
     /* USER CODE BEGIN 5 */
     /* Infinite loop */
     for (;;)
     {
         // Just blinky for now
-        HAL_GPIO_WritePin(GPIOE, GPIO_PIN_6, GPIO_PIN_SET);
-        osDelay(500);
-        HAL_GPIO_WritePin(GPIOE, GPIO_PIN_6, GPIO_PIN_RESET);
-        osDelay(500);
+        HAL_GPIO_TogglePin(GPIOE, GPIO_PIN_6);
+        osDelay(1000);
+
+        CanMsg msg = {
+            .std_id = 100,
+            .dlc    = 0,
+        };
+        io_can_pushTxMsgToQueue(&msg);
     }
     /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_runCanTxTask */
+/**
+ * @brief Function implementing the canTxTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_runCanTxTask */
+void runCanTxTask(void *argument)
+{
+    /* USER CODE BEGIN runCanTxTask */
+    /* Infinite loop */
+    for (;;)
+    {
+        io_can_transmitMsgFromQueue();
+    }
+    /* USER CODE END runCanTxTask */
+}
+
+/* USER CODE BEGIN Header_runCanRxTask */
+/**
+ * @brief Function implementing the canRxTask thread.
+ * @param argument: Not used
+ * @retval None
+ */
+/* USER CODE END Header_runCanRxTask */
+void runCanRxTask(void *argument)
+{
+    /* USER CODE BEGIN runCanRxTask */
+    /* Infinite loop */
+    for (;;)
+    {
+        CanMsg rx_msg;
+        io_can_popRxMsgFromQueue(&rx_msg);
+    }
+    /* USER CODE END runCanRxTask */
 }
 
 /**
