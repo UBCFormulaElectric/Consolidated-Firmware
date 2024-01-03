ada7692d03ffdd3c683b285e10693ed4c9c2a3e1
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2024-01-03 12:04:58 -0500

    formatting

--- software/dimos/src/qml_ui/constants/constants.h
+++ software/dimos/src/qml_ui/constants/constants.h
@@ -2,18 +2,18 @@
 
 #include <QtQml>
 
-#define SHARED_CONSTANT(type, name, value) \
-static inline constexpr type name = value; \
-Q_PROPERTY(type name MEMBER name CONSTANT)
+#define SHARED_CONSTANT(type, name, value)     \
+    static inline constexpr type name = value; \
+    Q_PROPERTY(type name MEMBER name CONSTANT)
 
 class Constants : public QObject
 {
-  Q_OBJECT
-  QML_UNCREATABLE("Class only exposes constants to QML.")
-  QML_ELEMENT
-  QML_SINGLETON
+    Q_OBJECT
+    QML_UNCREATABLE("Class only exposes constants to QML.")
+    QML_ELEMENT
+    QML_SINGLETON
 
-public:
-  SHARED_CONSTANT(int, SCREEN_WIDTH, 1024);
-  SHARED_CONSTANT(int, SCREEN_HEIGHT, 600);
+  public:
+    SHARED_CONSTANT(int, SCREEN_WIDTH, 1024);
+    SHARED_CONSTANT(int, SCREEN_HEIGHT, 600);
 };
