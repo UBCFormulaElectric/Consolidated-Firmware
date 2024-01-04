8ebd6220618cf7c812bc9ca3fda31ffbc734aa47
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2024-01-03 23:21:09 -0500

    formatting :(

--- software/dimos/src/qml_ui/constants/constants.h
+++ software/dimos/src/qml_ui/constants/constants.h
@@ -3,9 +3,9 @@
 #include <QtQml>
 #include <QString>
 
-#define SHARED_CONSTANT(type_quali, type, name, value)                         \
-  static inline type_quali type name = value;                                  \
-  Q_PROPERTY(type name MEMBER name CONSTANT)
+#define SHARED_CONSTANT(type_quali, type, name, value) \
+    static inline type_quali type name = value;        \
+    Q_PROPERTY(type name MEMBER name CONSTANT)
 
 // ReSharper disable once CppClassCanBeFinal
 class Constants : public QObject
@@ -20,15 +20,16 @@ class Constants : public QObject
     SHARED_CONSTANT(constexpr, int, SCREEN_WIDTH, 1024);
     SHARED_CONSTANT(constexpr, int, SCREEN_HEIGHT, 600);
 
-    static QString getWindowTitle() {
-      QString out;
+    static QString getWindowTitle()
+    {
+        QString out;
 #ifdef USING_dimos
         out = "Dimos";
 #endif
 #ifdef USING_dimos_dev
         out = "[Development] Dimos";
 #endif
-      return out;
+        return out;
     }
     Q_PROPERTY(QString WINDOW_TITLE READ getWindowTitle CONSTANT)
 };
