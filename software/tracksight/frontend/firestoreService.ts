59d89c45fe241844beda72eafbfac7095b19472b
Matthew <matthewyungisworking@gmail.com>
2024-01-06 22:18:31 -0800

    added data persistence for dashboards

---
+++ software/tracksight/frontend/firestoreService.ts
@@ -0,0 +1,46 @@
+import database from './firebase'; 
+import { ref, get, set } from "firebase/database";
+
+
+// function for getting  data from firebase
+const getRealtimeData = async (path) => {
+    try {
+        const dataRef = ref(database, path);
+        const snapshot = await get(dataRef);
+        if (snapshot.exists()) {
+            return snapshot.val();
+        } else {
+            console.log("No data available");
+            return null;
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
+// function for saving data to firebase
+const saveDashboardData = async (path, data) => {
+    try {
+        const dataRef = ref(database, path);
+        await set(dataRef, data);
+        return true; 
+    } catch (error) {
+        console.error("Error saving dashboard data: ", error);
+        return false; 
+    }
+};
+
+
+// function for deleting data from firebase
+const deleteDashboardData = async (path) => {
+    try {
+        const dataRef = ref(database, path);
+        await set(dataRef, null); 
+        return true;
+    } catch (error) {
+        console.error("Error deleting dashboard data: ", error);
+        return false;
+    }
+};
+
+export { getRealtimeData, saveDashboardData, deleteDashboardData };
--- software/tracksight/frontend/src/app/components/visualize.tsx
+++ software/tracksight/frontend/src/app/components/visualize.tsx
@@ -1,10 +1,11 @@
 import React, { useState, ReactElement } from 'react';
-import { Divider, Button, Switch, Space } from 'antd';
+import { Divider, Button, Switch, Space, Modal, Input, message } from 'antd';
 import Graph from './db/graph';
 import LiveGraph from './live/livegraph';
 import { PlotRelayoutEvent } from 'plotly.js';
 import { Socket } from "socket.io-client";
 import { MessageInstance } from 'antd/es/message/interface';
+import { saveDashboardData } from '../../../firestoreService'
 
 export interface VisualizeProps {
     addGraph: (live: boolean) => void;
@@ -19,10 +20,53 @@ export interface VisualizeProps {
 const Visualize = (props: VisualizeProps) => {
     const [sync, setSync] = useState<boolean>(false);
     const [zoomData, setZoomData] = useState<PlotRelayoutEvent>({});
+    const [graphSignals, setGraphSignals] = useState({});
+    const [dbName, setDbName] = useState<string>("test");
+    const [modalOpen, setModalOpen] = useState<boolean>(false);
+
+    const updateGraphSignals = (graphId, signals) => {
+        setGraphSignals(prev => ({ ...prev, [graphId]: signals }));
+    };
+
+    const handleInputChange = (event) => {
+        setDbName(event.target.value);
+    }
 
     const changeSync = (checked: boolean) => {
         setSync(checked);
     };
+
+    const saveDashboard = async () => {
+        //error handling for cases where these goblins try to save dashboard without a name or without any data
+        if (!dbName.trim()) {
+            message.error('name your dashboard before saving brother');
+            return;
+        }
+        const hasData = Object.keys(graphSignals).some(graphId => graphSignals[graphId].length > 0);
+        if (!hasData) {
+            message.warning('stop trying to save a dashboard without any graphs brother');
+            return;
+        }
+
+        const data = {
+            dbname: dbName,
+            graphs: {}
+        }
+        for (let graphId in graphSignals) {
+            data.graphs[graphId] = graphSignals[graphId];
+    };
+    
+    const path = `dashboards/${dbName}`; 
+        const success = await saveDashboardData(path, data);
+        if (success) {
+            message.success('Dashboard saved successfully!');
+            setModalOpen(false);
+        } else {
+            message.error('Error saving the dashboard.');
+        }
+    setModalOpen(false);
+    
+};
 
     return (
         <div className="layout">
@@ -38,6 +82,12 @@ const Visualize = (props: VisualizeProps) => {
                     </Space>
                     <Button onClick={() => props.addGraph(false)}>Add Influx Graph</Button>
                     <Button onClick={() => props.addGraph(true)}>Add Live Graph</Button>
+                    <Button onClick={() => setModalOpen(true)}>Save Current Loadout</Button>
+                    <Modal open={modalOpen} closeIcon={false} title='Save Dashboard'
+                    cancelButtonProps={{onClick:() =>setModalOpen(false)}}
+                    okButtonProps={{onClick:()=>saveDashboard()}}>
+                        <Input placeholder='Name of dashboard...' onChange={handleInputChange}></Input>
+                    </Modal>
                     <Divider />
                 </Space>
             </Space>
@@ -58,6 +108,7 @@ const Visualize = (props: VisualizeProps) => {
                     <LiveGraph
                         key={graphId}
                         id={graphId}
+                        updateGraphSignals={updateGraphSignals}
                         onDelete={() => props.deleteGraph(graphId, true)}
                         socket={props.socket}
                     />
