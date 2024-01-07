a41a1e5ff57443d174671c799a0da15da057bc09
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2024-01-07 00:43:49 -0500

    CommitInfo correctness

--- scripts/code_generation/commit_info_gen/commit_info.cmake
+++ scripts/code_generation/commit_info_gen/commit_info.cmake
@@ -1,12 +1,25 @@
-# WWW
-function(commit_info_register_library src_location header_location)
-    set(GENERATE_COMMIT_INFO_SCRIPT_PY
-            ${SCRIPTS_DIR}/code_generation/commit_info_gen/src/generate_commit_info.py)
+function(commit_info_register_library bind_target src_location header_location)
+    option(USE_COMMIT_INFO "Use commit info" ON)
+    if (NOT USE_COMMIT_INFO)
+        return()
+    endif()
+    file(RELATIVE_PATH header_location_relative ${CMAKE_SOURCE_DIR} ${header_location})
+    message("📚 Registering commit info library ${bind_target} with header at ${header_location_relative}")
+    set(GENERATE_COMMIT_INFO_SCRIPT_PY ${SCRIPTS_DIR}/code_generation/commit_info_gen/src/generate_commit_info.py)
     add_custom_command(
-            OUTPUT ${header_location} ${src_location}
-            COMMAND ${PYTHON_COMMAND} ${GENERATE_COMMIT_INFO_SCRIPT_PY}
-            --output-header ${header_location}
-            --output-source ${src_location}
-            WORKING_DIRECTORY ${REPO_ROOT_DIR}
+        TARGET ${bind_target}
+        COMMAND ${PYTHON_COMMAND} ${GENERATE_COMMIT_INFO_SCRIPT_PY}
+        --output-header ${header_location}
+        --output-source ${src_location}
+        WORKING_DIRECTORY ${REPO_ROOT_DIR}
     )
+
+    # ORIGINAL
+    # add_custom_command(
+    #     OUTPUT ${header_location} ${src_location}
+    #     COMMAND ${PYTHON_COMMAND} ${GENERATE_COMMIT_INFO_SCRIPT_PY}
+    #     --output-header ${header_location}
+    #     --output-source ${src_location}
+    #     WORKING_DIRECTORY ${REPO_ROOT_DIR}
+    # )
 endfunction()
--- software/dimos/src/find_libs.cmake
+++ software/dimos/src/find_libs.cmake
@@ -1,16 +1,6 @@
 # OUTPUTS
 # - REQUIRED_LIBRARIES -> list of libraries to link
 
-# qt
-find_package(QT NAMES Qt6 REQUIRED COMPONENTS Core Gui Widgets Qml Svg Quick)
-if(${QT_VERSION_MAJOR} LESS 6)
-    message("❌ Qt6 is required" FATAL_ERROR)
-endif()
-find_package(Qt6 REQUIRED COMPONENTS Core Gui Widgets Qml Svg Quick)
-find_package(Qt${QT_VERSION_MAJOR} REQUIRED COMPONENTS Core Gui Widgets Qml Svg Quick)
-list(APPEND REQUIRED_LIBRARIES Qt6::Core Qt6::Gui Qt6::Widgets Qt6::Qml Qt6::Svg Qt6::Quick)
-message("📚 Added Qt6 libraries")
-
 #gpiod
 if(${LINUX})
     find_library(GPIOD_LIBRARY NAMES libgpiod gpiod)
@@ -20,7 +10,7 @@ if(${LINUX})
     endif()
     list(APPEND REQUIRED_LIBRARIES "gpiod")
     list(APPEND REQUIRED_LIBRARIES "gpiodcxx")
-    message("📚 Added gpiod library")
+    message("📚✔️ Added gpiod library")
 endif ()
 
 # jsoncan
@@ -29,13 +19,13 @@ add_library(dimos_can STATIC ${CAN_SRCS})
 target_include_directories(dimos_can PUBLIC ${CAN_INCLUDE_DIRS})
 target_compile_definitions(dimos_can PRIVATE -DTHREAD_SAFE_CAN_PACKING)
 list(APPEND REQUIRED_LIBRARIES "dimos_can")
-message("📚 Added JsonCAN library")
+message("📚✔️ Added JsonCAN library")
 
 # commit_info
 set(COMMIT_INFO_SRC "${CMAKE_CURRENT_BINARY_DIR}/commit_info/App_CommitInfo.c")
 set(COMMIT_INFO_HEADER "${CMAKE_CURRENT_BINARY_DIR}/commit_info/App_CommitInfo.h")
-commit_info_register_library(${COMMIT_INFO_SRC} ${COMMIT_INFO_HEADER})
+commit_info_register_library(${EXECUTABLE_NAME} ${COMMIT_INFO_SRC} ${COMMIT_INFO_HEADER})
 add_library(dimos_commit_info STATIC ${COMMIT_INFO_SRC})
 target_include_directories(dimos_commit_info PUBLIC "${CMAKE_CURRENT_BINARY_DIR}/commit_info")
 list(APPEND REQUIRED_LIBRARIES "dimos_commit_info")
-message("📚 Added commit_info library")
+message("📚✔️ Added commit_info library")
