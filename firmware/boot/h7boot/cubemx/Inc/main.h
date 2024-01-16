4b27f8952e9f30bc2e0b364af5da163f3ecc7a2e
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2024-01-16 12:14:15 -0800

    Bootloader on H7 (#1105)
    ### Summary
    <!-- Quick summary of changes, optional -->
    
    Port our CAN bootloader to the STM32H733xx microcontroller, so it can be
    used with the BMS and VC on Quadruna.
    
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    - STM32H7 bootloader
    - Python changes to `canup` subrepo
    - Rework CMake to make shared files "opt-in" vs. "opt-out" across the
    entire repo
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    - Can update the binary on the H7 dev board on the bench
    - CI
    
    ### Resolved Issues
    <!-- Link any issues that this PR resolved like so: `Resolves #1, #2,
    and #5` (Note: Using this format, Github will automatically close the
    issue(s) when this PR is merged in). -->
    
    ### Checklist
    *Please change `[ ]` to `[x]` when you are ready.*
    - [x] I have read and followed the code conventions detailed in
    [README.md](../README.md) (*This will save time for both you and the
    reviewer!*).
    - [x] If this pull request is longer then **500** lines, I have provided
    *explicit* justification in the summary above explaining why I *cannot*
    break this up into multiple pull requests (*Small PR's are faster and
    less painful for everyone involved!*).
    
    ---------
    
    Co-authored-by: Gus Tahara-Edmonds <gtaharaedmonds@Guss-Laptop.local>
    Co-authored-by: Gus Tahara-Edmonds <gtaharaedmonds@dhcp-206-12-41-148.ubcsecure.wireless.ubc.ca>

--- firmware/boot/h7boot/cubemx/Inc/main.h
+++ firmware/boot/h7boot/cubemx/Inc/main.h
@@ -1,31 +1,34 @@
 /* USER CODE BEGIN Header */
 /**
  ******************************************************************************
- * @file    stm32f4xx_it.h
- * @brief   This file contains the headers of the interrupt handlers.
+ * @file           : main.h
+ * @brief          : Header for main.c file.
+ *                   This file contains the common defines of the application.
  ******************************************************************************
  * @attention
  *
- * <h2><center>&copy; Copyright (c) 2023 STMicroelectronics.
- * All rights reserved.</center></h2>
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
  *
- * This software component is licensed by ST under Ultimate Liberty license
- * SLA0044, the "License"; You may not use this file except in compliance with
- * the License. You may obtain a copy of the License at:
- *                             www.st.com/SLA0044
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
  *
  ******************************************************************************
  */
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __STM32F4xx_IT_H
-#define __STM32F4xx_IT_H
+#ifndef __MAIN_H
+#define __MAIN_H
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32h7xx_hal.h"
 
     /* Private includes ----------------------------------------------------------*/
     /* USER CODE BEGIN Includes */
@@ -48,21 +51,20 @@ extern "C"
     /* USER CODE END EM */
 
     /* Exported functions prototypes ---------------------------------------------*/
-    void NMI_Handler(void);
-    void HardFault_Handler(void);
-    void MemManage_Handler(void);
-    void BusFault_Handler(void);
-    void UsageFault_Handler(void);
-    void DebugMon_Handler(void);
-    void CAN1_RX0_IRQHandler(void);
-    void CAN1_RX1_IRQHandler(void);
-    void TIM6_IRQHandler(void);
+    void Error_Handler(void);
+
     /* USER CODE BEGIN EFP */
 
     /* USER CODE END EFP */
+
+    /* Private defines -----------------------------------------------------------*/
+
+    /* USER CODE BEGIN Private defines */
+
+    /* USER CODE END Private defines */
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __STM32F4xx_IT_H */
+#endif /* __MAIN_H */
