ecbc7b15a16f3db81b3a763a015a553d29547423
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2024-01-26 14:46:08 -0800

    delete untracked

--- firmware/thruna/DCM/src/App/states/app_initState.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "App_SharedStateMachine.h"
-
-/**
- * Get a pointer to the Init State.
- * @return A pointer to the Init State. THIS SHOULD NOT BE MODIFIED.
- */
-const struct State *app_initState_get(void);
