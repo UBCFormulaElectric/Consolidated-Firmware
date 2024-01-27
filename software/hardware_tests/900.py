83b101d8cfa37cef74f1bd5f9dca4e42ab706e7c
Ashli <forbesashli@gmail.com>
2024-01-27 11:53:44 -0800

    serial push

---
+++ software/hardware_tests/900.py
@@ -0,0 +1,21 @@
+# Imports
+import serial
+ser = serial.Serial('/dev/ttyUSB0', baudrate=57600, stopbits=1, timeout=100) 
+
+def send_msg(ser, command):
+        ser.write(command.encode())
+
+def main():     
+    try:
+        while True:
+            msg = "hello lara\n"
+            print(msg)
+            send_msg(ser,msg)
+
+
+    except KeyboardInterrupt:
+        # Handle Ctrl+C to exit gracefully
+        print("\nScript terminated by user.")
+
+if __name__ == "__main__":
+    main()
