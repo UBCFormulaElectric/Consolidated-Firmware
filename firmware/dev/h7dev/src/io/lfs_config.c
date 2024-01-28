b4919fb5a3b3a3f4491682da529ae4ad3f9d8df0
Jiajun-Huang <jhuang430@qq.com>
2024-01-28 02:49:51 -0800

    some cleanup

--- firmware/dev/h7dev/src/io/lfs_config.c
+++ firmware/dev/h7dev/src/io/lfs_config.c
@@ -2,54 +2,76 @@
 #include "hw_sd.h"
 #include "hw_gpio.h"
 
-#define LFS_CACHE_SIZE 512
-#define LFS_LOOKAHEAD_SIZE 512
-
-#define LFS_READ_SIZE 512
-#define LFS_PROG_SIZE 512
-#define LFS_CHACHE_SIZE 512
-#define LFS_LOOKAHEAD_SIZE 512
+// those factor will be multiple with the block size
+#define LFS_READ_SIZE_FACTOR 1
+#define LFS_PROG_SIZE_FACTOR 1
+#define LFS_LOOKAHEAD_SIZE_FACTOR 1
+#define LFS_CACHE_SIZE_FACTOR 8 // a multiple of the read and program sizes
+#define LFS_FILE_CACHE_SIZE_FACTOR 8
 #define LFS_BLOCK_CYCLES 500
 
-char lfs_read_buffer[LFS_READ_SIZE];
-char lfs_prog_buffer[LFS_PROG_SIZE];
+#define LFS_BLOCK_SIZE 512
+#define LFS_READ_SIZE LFS_BLOCK_SIZE *LFS_READ_SIZE_FACTOR
+#define LFS_PROG_SIZE LFS_BLOCK_SIZE *LFS_PROG_SIZE_FACTOR
+#define LFS_LOOKAHEAD_SIZE LFS_BLOCK_SIZE *LFS_LOOKAHEAD_SIZE_FACTOR
+#define LFS_CACHE_SIZE LFS_BLOCK_SIZE *LFS_CACHE_SIZE_FACTOR
+#define LFS_FILE_CACHE_SIZE LFS_BLOCK_SIZE *LFS_FILE_CACHE_SIZE_FACTOR
 
-SdCard sd;
-extern Gpio sd_enable;
+char *lfs_read_buffer;
+char *lfs_prog_buffer;
+
+extern SdCard      sd;
+extern Gpio sd_present;
+extern bool sd_inited;
+
+static bool sd_card_ready()
+{
+    return sd_inited && !hw_gpio_readPin(&sd_present);
+}
+
 int lfs_read(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size)
 {
-    if (hw_gpio_readPin(&sd_enable))
+    if (!sd_card_ready())
     {
         return LFS_ERR_IO;
     }
-    SdCardStatus status = hw_sd_readOffset(&sd, (uint8_t *)buffer, (uint32_t)block, (uint32_t)off, (uint32_t)size);
+    if (hw_sd_readOffset(&sd, (uint8_t *)buffer, (uint32_t)block, (uint32_t)off, (uint32_t)size) != SD_CARD_OK)
+    {
+        return LFS_ERR_IO;
+    }
 
-    return status;
+    return LFS_ERR_OK;
 }
 
 int lfs_prog(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size)
 {
-    if (hw_gpio_readPin(&sd_enable))
+    if (!sd_card_ready())
     {
         return LFS_ERR_IO;
     }
-    SdCardStatus status = hw_sd_writeOffset(&sd, (uint8_t *)buffer, (uint32_t)block, (uint32_t)off, (uint32_t)size);
-    return status;
+    if (hw_sd_writeOffset(&sd, (uint8_t *)buffer, (uint32_t)block, (uint32_t)off, (uint32_t)size) != SD_CARD_OK)
+    {
+        return LFS_ERR_IO;
+    }
+    return LFS_ERR_OK;
 }
 
 int lfs_erase(const struct lfs_config *c, lfs_block_t block)
 {
-    if (hw_gpio_readPin(&sd_enable))
+    if (!sd_card_ready())
     {
         return LFS_ERR_IO;
     }
-    SdCardStatus status = hw_sd_erase(&sd, (uint32_t)block, (uint32_t)block);
-    return status;
+    if (hw_sd_erase(&sd, (uint32_t)block, (uint32_t)block))
+    {
+        return LFS_ERR_IO;
+    }
+    return LFS_ERR_OK;
 }
 
 int lfs_sync(const struct lfs_config *c)
 {
-    if (hw_gpio_readPin(&sd_enable))
+    if (!sd_card_ready())
     {
         return LFS_ERR_IO;
     }
@@ -59,35 +81,23 @@ int lfs_sync(const struct lfs_config *c)
 int lfs_config_object(uint32_t block_size, uint32_t block_number, struct lfs_config *cfg)
 {
     // the function
-    cfg->read = lfs_read;
-    cfg->prog = lfs_prog;
+    cfg->read  = lfs_read;
+    cfg->prog  = lfs_prog;
     cfg->erase = lfs_erase;
-    cfg->sync = lfs_sync;
+    cfg->sync  = lfs_sync;
 
-    cfg->read_size = LFS_READ_SIZE;
-    cfg->prog_size = LFS_PROG_SIZE;
-    cfg->block_size = LFS_CHACHE_SIZE;
+    assert(LFS_BLOCK_SIZE == block_size);
+
+    cfg->read_size      = LFS_READ_SIZE;
+    cfg->prog_size      = LFS_PROG_SIZE;
+    cfg->block_size     = LFS_BLOCK_SIZE;
     cfg->lookahead_size = LFS_LOOKAHEAD_SIZE;
-
-    cfg->block_size = block_size;
-    cfg->block_count = block_number;
-
-    cfg->block_cycles = 500;
-    cfg->cache_size = LFS_CACHE_SIZE;
-    cfg->lookahead_size = LFS_LOOKAHEAD_SIZE;
-
-    cfg->read_buffer = lfs_read_buffer;
-    cfg->prog_buffer = lfs_prog_buffer;
-
-    // #ifdef LFS_THREADSAFE
-    //     // Lock the underlying block device. Negative error codes
-    //     // are propagated to the user.
-    //     // int (*lock)(const struct lfs_config *c);
-
-    //     // // Unlock the underlying block device. Negative error codes
-    //     // // are propagated to the user.
-    //     // int (*unlock)(const struct lfs_config *c);
-    // #endif
+    cfg->block_size     = BLOCKSIZE;
+    cfg->block_count    = block_number;
+    cfg->block_cycles   = LFS_BLOCK_CYCLES;
+    cfg->cache_size     = LFS_CACHE_SIZE;
+    cfg->read_buffer    = lfs_read_buffer;
+    cfg->prog_buffer    = lfs_prog_buffer;
 
     return 0;
 }
--- firmware/shared/src/io/io_canLogging.c
+++ firmware/shared/src/io/io_canLogging.c
@@ -5,6 +5,7 @@
 #include "cmsis_os.h"
 #include "queue.h"
 #include "hw_gpio.h"
+#include "lfs_config.h"
 
 // Private globals.
 static const CanConfig *config;
@@ -12,106 +13,73 @@ static const CanConfig *config;
 #define QUEUE_BYTES sizeof(CanMsg) * QUEUE_SIZE
 #define PATH_LENGTH 10
 static osMessageQueueId_t message_queue_id;
-static StaticQueue_t queue_control_block;
-static uint8_t queue_buf[QUEUE_BYTES];
+static StaticQueue_t      queue_control_block;
+static uint8_t            queue_buf[QUEUE_BYTES];
 
 #define QUEUE_SIZE 20
 static uint32_t current_bootcount;
 
-static lfs_t lfs;
+static lfs_t      lfs;
 static lfs_file_t file;
 
-extern Gpio sd_enable;
-extern bool sd_inited;
+extern SdCard sd;
+extern Gpio   sd_present;
+extern bool   sd_inited;
 
 static char current_path[10];
 
 static const osMessageQueueAttr_t queue_attr = {
-    .name = "CAN Logging Queue",
+    .name      = "CAN Logging Queue",
     .attr_bits = 0,
-    .cb_mem = &queue_control_block,
-    .cb_size = sizeof(StaticQueue_t),
-    .mq_mem = queue_buf,
-    .mq_size = QUEUE_BYTES,
+    .cb_mem    = &queue_control_block,
+    .cb_size   = sizeof(StaticQueue_t),
+    .mq_mem    = queue_buf,
+    .mq_size   = QUEUE_BYTES,
 };
 
-static char buffer[512];
+static char                  buffer[512];
 const struct lfs_file_config fcfg = {
     .buffer = buffer,
 };
 
-static void generateCANMsg(void)
-{
-    lfs_file_opencfg(&lfs, &file, "canMsg", LFS_O_RDWR | LFS_O_CREAT, &fcfg);
-    CanMsg msg;
-    uint32_t i = 0;
-    msg.dlc = 1;
-
-    // write 0 to 99
-    for (i = 0; i < 100; i++)
-    {
-        msg.std_id = i;
-        msg.data[0] = (uint8_t)i;
-        lfs_file_write(&lfs, &file, &msg, sizeof(msg));
-    }
-
-    msg.dlc = 5;
-    memcpy(&msg.data, "hello", 5);
-    // write hello
-    for (i = 0; i < 100; i++)
-    {
-        msg.std_id = i + 100;
-        lfs_file_write(&lfs, &file, &msg, sizeof(msg));
-    }
-    lfs_file_close(&lfs, &file);
-}
-
 // assume the lfs is already mounted
-static void createFolder(struct lfs_config *cfg)
+static void init_logging_file_system()
 {
     // early return
-    if (!sd_inited && hw_gpio_readPin(&sd_enable))
+    if (!sd_inited || hw_gpio_readPin(&sd_present))
     {
         return;
     }
 
+    // config the file system
+    struct lfs_config cfg;
+    lfs_config_object(sd.hsd->SdCard.BlockSize, sd.hsd->SdCard.BlockNbr, &cfg);
+
     uint32_t bootcount = 0;
-    int err = lfs_mount(&lfs, cfg);
+    int      err       = lfs_mount(&lfs, &cfg);
     if (err)
     {
-        lfs_format(&lfs, cfg);
-        err = lfs_mount(&lfs, cfg);
+        lfs_format(&lfs, &cfg);
+        err = lfs_mount(&lfs, &cfg);
     }
 
+    // get bootcount value from the file; use this to create new file for logging
     lfs_file_opencfg(&lfs, &file, "bootcount", LFS_O_RDWR | LFS_O_CREAT, &fcfg);
     lfs_file_read(&lfs, &file, &bootcount, sizeof(bootcount));
 
-    // update bootcount
+    // update bootcount for next boot
     bootcount += 1;
     lfs_file_rewind(&lfs, &file);
     lfs_file_write(&lfs, &file, &bootcount, sizeof(bootcount));
     current_bootcount = bootcount;
-
-    // close the bootcount file
     lfs_file_close(&lfs, &file);
-    generateCANMsg();
 
-    // create new folder on root based on the bootcount
-
+    // create new file on root based on the bootcount
     sprintf((char *)current_path, "%lu", bootcount);
-
-    // open a file for logging can message
-    lfs_file_opencfg(&lfs, &file, current_path, LFS_O_RDWR | LFS_O_CREAT, &fcfg);
-    CanMsg msg = {
-        .dlc = 12,
-        .std_id = 12,
-    };
-    msg.data[0] = 0xff;
-    lfs_file_write(&lfs, &file, &msg, sizeof(msg));
-    lfs_file_close(&lfs, &file);
+    lfs_file_opencfg(&lfs, &file, current_path, LFS_O_RDWR | LFS_O_CREAT, &fcfg); // this file opens forever
 }
 
-void io_canLogging_init(const CanConfig *can_config, struct lfs_config *cfg)
+void io_canLogging_init(const CanConfig *can_config)
 {
     assert(can_config != NULL);
     config = can_config;
@@ -121,7 +89,7 @@ void io_canLogging_init(const CanConfig *can_config, struct lfs_config *cfg)
     message_queue_id = osMessageQueueNew(QUEUE_SIZE, sizeof(CanMsg), &queue_attr);
 
     // create new folder for this boot
-    createFolder(cfg);
+    init_logging_file_system();
 }
 
 void io_canLogging_pushTxMsgToQueue(const CanMsg *msg)
@@ -137,31 +105,35 @@ void io_canLogging_pushTxMsgToQueue(const CanMsg *msg)
 
 void io_canLogging_recordMsgFromQueue(void)
 {
-    if (!sd_inited && hw_gpio_readPin(&sd_enable))
+    if (!sd_inited && hw_gpio_readPin(&sd_present))
     {
         return;
     }
     CanMsg tx_msg;
     osMessageQueueGet(message_queue_id, &tx_msg, NULL, osWaitForever);
-    lfs_file_opencfg(&lfs, &file, current_path, LFS_O_RDWR | LFS_O_CREAT, &fcfg);
-    lfs_file_seek(&lfs, &file, 0, SEEK_END);
+
     lfs_ssize_t size = lfs_file_write(&lfs, &file, &tx_msg, sizeof(tx_msg));
-    lfs_file_close(&lfs, &file);
-    if (size)
+    if (size != sizeof(tx_msg))
     {
+        // something happend
     }
-    // write the message to the file system
+    static uint32_t message_written = 0;
+    message_written++;
+    if (message_written > 128)
+    {
+        lfs_file_sync(&lfs, &file);
+    }
 }
 
 void io_canLogging_msgReceivedCallback(uint32_t rx_fifo, CanMsg *rx_msg)
 {
     static uint32_t rx_overflow_count = 0;
 
-    // if (config->rx_msg_filter != NULL && !config->rx_msg_filter(rx_msg->std_id))
-    // {
-    //     // Early return if we don't care about this msg via configured filter func.
-    //     return;
-    // }
+    if (config->rx_msg_filter != NULL && !config->rx_msg_filter(rx_msg->std_id))
+    {
+        // Early return if we don't care about this msg via configured filter func.
+        return;
+    }
 
     // We defer reading the CAN RX message to another task by storing the
     // message on the CAN RX queue.
