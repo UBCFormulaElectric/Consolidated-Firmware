3fcd6741a2c976ee07b571a26257519e8eb31d53
Liam Ilan <snowboardsheep@gmail.com>
2024-01-29 17:05:18 -0800

    Added hard fault handler to quadruna fsm.

--- firmware/quadruna/FSM/src/cubemx/Src/stm32f4xx_it.c
+++ firmware/quadruna/FSM/src/cubemx/Src/stm32f4xx_it.c
@@ -22,6 +22,7 @@
 #include "stm32f4xx_it.h"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
+#include "hw_hardFaultHandler.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -87,7 +88,7 @@ void NMI_Handler(void)
 void HardFault_Handler(void)
 {
     /* USER CODE BEGIN HardFault_IRQn 0 */
-
+    hw_hardFaultHandler_handleFault();
     /* USER CODE END HardFault_IRQn 0 */
     while (1)
     {
