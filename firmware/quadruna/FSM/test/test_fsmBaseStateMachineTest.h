23fa17735e2f726ce4fcb9a63c181a85b0b7de79
Liam Ilan <snowboardsheep@gmail.com>
2024-01-29 18:27:36 -0800

    Fixed base state machine for quadruna fsm, and added temporary emmpty test file to ensure state machine compilation.

--- firmware/quadruna/FSM/test/test_fsmBaseStateMachineTest.h
+++ firmware/quadruna/FSM/test/test_fsmBaseStateMachineTest.h
@@ -1,14 +1,17 @@
-#pragma once
 #include <gtest/gtest.h>
-#include "fake_io_time.hpp"
+#include "test_baseStateMachineTest.h"
+
+// TODO: include fakes
 
 extern "C"
 {
 #include "app_canTx.h"
 #include "app_canRx.h"
-#include "app_mainState.h"
+#include "app_canAlerts.h"
 #include "app_heartbeatMonitor.h"
-#include "app_globals.h"
+#include "app_stateMachine.h"
+#include "app_canUtils.h"
+#include "app_utils.h"
 }
 
 // Test fixture definition for any test requiring the state machine. Can also be used for non-state machine related
@@ -28,15 +31,18 @@ class FsmBaseStateMachineTest : public BaseStateMachineTest
             HEARTBEAT_MONITOR_TIMEOUT_PERIOD_MS, heartbeatMonitorChecklist, heartbeatGetters, heartbeatUpdaters,
             &app_canTx_FSM_Heartbeat_set, heartbeatFaultSetters, heartbeatFaultGetters);
 
-        app_stateMachine_init(app_mainState_get());
-
         // Disable heartbeat monitor in the nominal case. To use representative heartbeat behavior,
         // re-enable the heartbeat monitor.
         app_heartbeatMonitor_blockFaults(true);
     }
 
+    void TearDown() override
+    {
+        // TODO: reset fakes
+    }
+
     // config to forward can functions to shared heartbeat
-    // FSM rellies on TBD
+    // FSM rellies on BMS
     bool heartbeatMonitorChecklist[HEARTBEAT_BOARD_COUNT] = { [BMS_HEARTBEAT_BOARD] = false,
                                                               [DCM_HEARTBEAT_BOARD] = false,
                                                               [PDM_HEARTBEAT_BOARD] = false,
@@ -44,11 +50,11 @@ class FsmBaseStateMachineTest : public BaseStateMachineTest
                                                               [DIM_HEARTBEAT_BOARD] = false };
 
     // heartbeatGetters - get heartbeat signals from other boards
-    bool (*heartbeatGetters[HEARTBEAT_BOARD_COUNT])() = { [BMS_HEARTBEAT_BOARD] = false,
-                                                          [DCM_HEARTBEAT_BOARD] = false,
-                                                          [PDM_HEARTBEAT_BOARD] = false,
-                                                          [FSM_HEARTBEAT_BOARD] = false,
-                                                          [DIM_HEARTBEAT_BOARD] = false };
+    bool (*heartbeatGetters[HEARTBEAT_BOARD_COUNT])() = { [BMS_HEARTBEAT_BOARD] = NULL,
+                                                          [DCM_HEARTBEAT_BOARD] = NULL,
+                                                          [PDM_HEARTBEAT_BOARD] = NULL,
+                                                          [FSM_HEARTBEAT_BOARD] = NULL,
+                                                          [DIM_HEARTBEAT_BOARD] = NULL };
 
     // heartbeatUpdaters - update local CAN table with heartbeat status
     void (*heartbeatUpdaters[HEARTBEAT_BOARD_COUNT])(bool) = { [BMS_HEARTBEAT_BOARD] = NULL,
@@ -65,9 +71,9 @@ class FsmBaseStateMachineTest : public BaseStateMachineTest
                                                                    [DIM_HEARTBEAT_BOARD] = NULL };
 
     // heartbeatFaultGetters - gets fault statuses over CAN
-    bool (*heartbeatFaultGetters[HEARTBEAT_BOARD_COUNT])() = { [BMS_HEARTBEAT_BOARD] = false,
-                                                               [DCM_HEARTBEAT_BOARD] = false,
-                                                               [PDM_HEARTBEAT_BOARD] = false,
-                                                               [FSM_HEARTBEAT_BOARD] = false,
-                                                               [DIM_HEARTBEAT_BOARD] = false };
+    bool (*heartbeatFaultGetters[HEARTBEAT_BOARD_COUNT])() = { [BMS_HEARTBEAT_BOARD] = NULL,
+                                                               [DCM_HEARTBEAT_BOARD] = NULL,
+                                                               [PDM_HEARTBEAT_BOARD] = NULL,
+                                                               [FSM_HEARTBEAT_BOARD] = NULL,
+                                                               [DIM_HEARTBEAT_BOARD] = NULL };
 };
