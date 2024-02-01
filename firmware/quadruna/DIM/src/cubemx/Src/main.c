e85cac4f724ff56fe234722aa0b5abca9e5941bc
Leah Fernandez <leahmf88@students.cs.ubc.ca>
2024-01-31 19:31:56 -0800

    cmake and cubemx inc/src

---
+++ firmware/quadruna/DIM/src/cubemx/Src/main.c
@@ -0,0 +1,582 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2024 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+typedef StaticTask_t osStaticThreadDef_t;
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+CAN_HandleTypeDef hcan1;
+
+IWDG_HandleTypeDef hiwdg;
+
+/* Definitions for TaskCanRx */
+osThreadId_t TaskCanRxHandle;
+uint32_t TaskCanRxBuffer[ 512 ];
+osStaticThreadDef_t TaskCanRxControlBlock;
+const osThreadAttr_t TaskCanRx_attributes = {
+  .name = "TaskCanRx",
+  .cb_mem = &TaskCanRxControlBlock,
+  .cb_size = sizeof(TaskCanRxControlBlock),
+  .stack_mem = &TaskCanRxBuffer[0],
+  .stack_size = sizeof(TaskCanRxBuffer),
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for Task100Hz */
+osThreadId_t Task100HzHandle;
+uint32_t Task100HzBuffer[ 512 ];
+osStaticThreadDef_t Task100HzControlBlock;
+const osThreadAttr_t Task100Hz_attributes = {
+  .name = "Task100Hz",
+  .cb_mem = &Task100HzControlBlock,
+  .cb_size = sizeof(Task100HzControlBlock),
+  .stack_mem = &Task100HzBuffer[0],
+  .stack_size = sizeof(Task100HzBuffer),
+  .priority = (osPriority_t) osPriorityHigh,
+};
+/* Definitions for TaskCanTx */
+osThreadId_t TaskCanTxHandle;
+uint32_t TaskCanTxBuffer[ 512 ];
+osStaticThreadDef_t TaskCanTxControlBlock;
+const osThreadAttr_t TaskCanTx_attributes = {
+  .name = "TaskCanTx",
+  .cb_mem = &TaskCanTxControlBlock,
+  .cb_size = sizeof(TaskCanTxControlBlock),
+  .stack_mem = &TaskCanTxBuffer[0],
+  .stack_size = sizeof(TaskCanTxBuffer),
+  .priority = (osPriority_t) osPriorityNormal,
+};
+/* Definitions for Task1kHz */
+osThreadId_t Task1kHzHandle;
+uint32_t Task1kHzBuffer[ 512 ];
+osStaticThreadDef_t Task1kHzControlBlock;
+const osThreadAttr_t Task1kHz_attributes = {
+  .name = "Task1kHz",
+  .cb_mem = &Task1kHzControlBlock,
+  .cb_size = sizeof(Task1kHzControlBlock),
+  .stack_mem = &Task1kHzBuffer[0],
+  .stack_size = sizeof(Task1kHzBuffer),
+  .priority = (osPriority_t) osPriorityRealtime,
+};
+/* Definitions for Task1Hz */
+osThreadId_t Task1HzHandle;
+uint32_t Task1HzBuffer[ 512 ];
+osStaticThreadDef_t Task1HzControlBlock;
+const osThreadAttr_t Task1Hz_attributes = {
+  .name = "Task1Hz",
+  .cb_mem = &Task1HzControlBlock,
+  .cb_size = sizeof(Task1HzControlBlock),
+  .stack_mem = &Task1HzBuffer[0],
+  .stack_size = sizeof(Task1HzBuffer),
+  .priority = (osPriority_t) osPriorityAboveNormal,
+};
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_CAN1_Init(void);
+static void MX_IWDG_Init(void);
+void RunTaskCanRx(void *argument);
+void RunTask100Hz(void *argument);
+void RunTaskCanTx(void *argument);
+void RunTask1kHz(void *argument);
+void RunTask1Hz(void *argument);
+
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_CAN1_Init();
+  MX_IWDG_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Init scheduler */
+  osKernelInitialize();
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* creation of TaskCanRx */
+  TaskCanRxHandle = osThreadNew(RunTaskCanRx, NULL, &TaskCanRx_attributes);
+
+  /* creation of Task100Hz */
+  Task100HzHandle = osThreadNew(RunTask100Hz, NULL, &Task100Hz_attributes);
+
+  /* creation of TaskCanTx */
+  TaskCanTxHandle = osThreadNew(RunTaskCanTx, NULL, &TaskCanTx_attributes);
+
+  /* creation of Task1kHz */
+  Task1kHzHandle = osThreadNew(RunTask1kHz, NULL, &Task1kHz_attributes);
+
+  /* creation of Task1Hz */
+  Task1HzHandle = osThreadNew(RunTask1Hz, NULL, &Task1Hz_attributes);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_EVENTS */
+  /* add events, ... */
+  /* USER CODE END RTOS_EVENTS */
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Configure the main internal regulator output voltage
+  */
+  __HAL_RCC_PWR_CLK_ENABLE();
+  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI|RCC_OSCILLATORTYPE_LSI;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
+  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief CAN1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_CAN1_Init(void)
+{
+
+  /* USER CODE BEGIN CAN1_Init 0 */
+
+  /* USER CODE END CAN1_Init 0 */
+
+  /* USER CODE BEGIN CAN1_Init 1 */
+
+  /* USER CODE END CAN1_Init 1 */
+  hcan1.Instance = CAN1;
+  hcan1.Init.Prescaler = 16;
+  hcan1.Init.Mode = CAN_MODE_NORMAL;
+  hcan1.Init.SyncJumpWidth = CAN_SJW_1TQ;
+  hcan1.Init.TimeSeg1 = CAN_BS1_1TQ;
+  hcan1.Init.TimeSeg2 = CAN_BS2_1TQ;
+  hcan1.Init.TimeTriggeredMode = DISABLE;
+  hcan1.Init.AutoBusOff = DISABLE;
+  hcan1.Init.AutoWakeUp = DISABLE;
+  hcan1.Init.AutoRetransmission = DISABLE;
+  hcan1.Init.ReceiveFifoLocked = DISABLE;
+  hcan1.Init.TransmitFifoPriority = DISABLE;
+  if (HAL_CAN_Init(&hcan1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN CAN1_Init 2 */
+
+  /* USER CODE END CAN1_Init 2 */
+
+}
+
+/**
+  * @brief IWDG Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_IWDG_Init(void)
+{
+
+  /* USER CODE BEGIN IWDG_Init 0 */
+
+  /* USER CODE END IWDG_Init 0 */
+
+  /* USER CODE BEGIN IWDG_Init 1 */
+
+  /* USER CODE END IWDG_Init 1 */
+  hiwdg.Instance = IWDG;
+  hiwdg.Init.Prescaler = IWDG_PRESCALER_4;
+  hiwdg.Init.Reload = LSI_FREQUENCY / IWDG_PRESCALER / IWDG_RESET_FREQUENCY;
+  if (HAL_IWDG_Init(&hiwdg) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN IWDG_Init 2 */
+
+  /* USER CODE END IWDG_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, BMS_B_Pin|BMS_G_Pin|BSPD_R_Pin|BMS_R_Pin
+                          |FMS_R_Pin|VC_B_Pin|LED_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOC, VC_G_Pin|RSM_G_Pin|RSM_R_Pin|FSM_B_Pin
+                          |FSM_G_Pin|IMD_R_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, VC_R_Pin|AUX_DB_B_Pin|AUX_DB_G_Pin|AUX_DB_R_Pin
+                          |CRIT_DB_B_Pin|CRIT_DB_G_Pin|CRIT_DB_R_Pin|RSM_B_Pin
+                          |REGEN_LED_Pin|START_SIG_Pin|TORQUE_VECTORING_LED_Pin|REGEN_SIG_Pin
+                          |TORQUE_VECTORING_SIG_Pin|START_LED_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(AMS_R_GPIO_Port, AMS_R_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : DRIVE_MODE_1B_Pin NCHIMERA_Pin DRIVE_MODE_0B_Pin DRIVE_MODE_3B_Pin
+                           DRIVE_MODE_2B_Pin SHDN_G_Pin INERTIA_SEN_Pin SHDN_SEN_Pin */
+  GPIO_InitStruct.Pin = DRIVE_MODE_1B_Pin|NCHIMERA_Pin|DRIVE_MODE_0B_Pin|DRIVE_MODE_3B_Pin
+                          |DRIVE_MODE_2B_Pin|SHDN_G_Pin|INERTIA_SEN_Pin|SHDN_SEN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SHDN_R_Pin NPROGRAM_3V3_Pin */
+  GPIO_InitStruct.Pin = SHDN_R_Pin|NPROGRAM_3V3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : BMS_B_Pin BMS_G_Pin BSPD_R_Pin BMS_R_Pin
+                           FMS_R_Pin VC_B_Pin LED_Pin */
+  GPIO_InitStruct.Pin = BMS_B_Pin|BMS_G_Pin|BSPD_R_Pin|BMS_R_Pin
+                          |FMS_R_Pin|VC_B_Pin|LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PA2 PA3 */
+  GPIO_InitStruct.Pin = GPIO_PIN_2|GPIO_PIN_3;
+  GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.Alternate = GPIO_AF7_USART2;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : REGEN_3V3_Pin */
+  GPIO_InitStruct.Pin = REGEN_3V3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_ANALOG;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(REGEN_3V3_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : VC_G_Pin RSM_G_Pin RSM_R_Pin FSM_B_Pin
+                           FSM_G_Pin IMD_R_Pin */
+  GPIO_InitStruct.Pin = VC_G_Pin|RSM_G_Pin|RSM_R_Pin|FSM_B_Pin
+                          |FSM_G_Pin|IMD_R_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : VC_R_Pin AUX_DB_B_Pin AUX_DB_G_Pin AUX_DB_R_Pin
+                           CRIT_DB_B_Pin CRIT_DB_G_Pin CRIT_DB_R_Pin RSM_B_Pin
+                           REGEN_LED_Pin START_SIG_Pin TORQUE_VECTORING_LED_Pin REGEN_SIG_Pin
+                           TORQUE_VECTORING_SIG_Pin START_LED_Pin */
+  GPIO_InitStruct.Pin = VC_R_Pin|AUX_DB_B_Pin|AUX_DB_G_Pin|AUX_DB_R_Pin
+                          |CRIT_DB_B_Pin|CRIT_DB_G_Pin|CRIT_DB_R_Pin|RSM_B_Pin
+                          |REGEN_LED_Pin|START_SIG_Pin|TORQUE_VECTORING_LED_Pin|REGEN_SIG_Pin
+                          |TORQUE_VECTORING_SIG_Pin|START_LED_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : AMS_R_Pin */
+  GPIO_InitStruct.Pin = AMS_R_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(AMS_R_GPIO_Port, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/* USER CODE BEGIN Header_RunTaskCanRx */
+/**
+  * @brief  Function implementing the TaskCanRx thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_RunTaskCanRx */
+void RunTaskCanRx(void *argument)
+{
+  /* USER CODE BEGIN 5 */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END 5 */
+}
+
+/* USER CODE BEGIN Header_RunTask100Hz */
+/**
+* @brief Function implementing the Task100Hz thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_RunTask100Hz */
+void RunTask100Hz(void *argument)
+{
+  /* USER CODE BEGIN RunTask100Hz */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END RunTask100Hz */
+}
+
+/* USER CODE BEGIN Header_RunTaskCanTx */
+/**
+* @brief Function implementing the TaskCanTx thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_RunTaskCanTx */
+void RunTaskCanTx(void *argument)
+{
+  /* USER CODE BEGIN RunTaskCanTx */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END RunTaskCanTx */
+}
+
+/* USER CODE BEGIN Header_RunTask1kHz */
+/**
+* @brief Function implementing the Task1kHz thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_RunTask1kHz */
+void RunTask1kHz(void *argument)
+{
+  /* USER CODE BEGIN RunTask1kHz */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END RunTask1kHz */
+}
+
+/* USER CODE BEGIN Header_RunTask1Hz */
+/**
+* @brief Function implementing the Task1Hz thread.
+* @param argument: Not used
+* @retval None
+*/
+/* USER CODE END Header_RunTask1Hz */
+void RunTask1Hz(void *argument)
+{
+  /* USER CODE BEGIN RunTask1Hz */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END RunTask1Hz */
+}
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM6 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM6) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
