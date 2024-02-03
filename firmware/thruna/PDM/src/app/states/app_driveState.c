4add42d8a120486a0531ac174113abed02b49b63
Jugveer-Sandher <62398344+Jugveer-Sandher@users.noreply.github.com>
2024-02-03 21:54:44 +0000

    Power Manager for PDM (#1099)
    ### Summary
    Created a power manager for the PDM sets the efuses.
    
    ### Testing Done
    Tested on car
    
    ### Checklist
    *Please change `[ ]` to `[x]` when you are ready.*
    - [X] I have read and followed the code conventions detailed in
    [README.md](../README.md) (*This will save time for both you and the
    reviewer!*).
    - [X] If this pull request is longer then **500** lines, I have provided
    *explicit* justification in the summary above explaining why I *cannot*
    break this up into multiple pull requests (*Small PR's are faster and
    less painful for everyone involved!*).

---
+++ firmware/thruna/PDM/src/app/states/app_driveState.c
@@ -0,0 +1,33 @@
+#include <stddef.h>
+
+#include "states/app_driveState.h"
+#include "states/app_allStates.h"
+#include "app_canUtils.h"
+#include "app_powerManager.h"
+#include "app_canTx.h"
+#include "app_canRx.h"
+#include "states/app_initState.h"
+
+static void driveStateRunOnEntry(void)
+{
+    app_canTx_PDM_State_set(PDM_DRIVE_STATE);
+    app_powerManager_setState(POWER_MANAGER_DRIVE);
+}
+
+static void driveStateRunOnTick100Hz(void)
+{
+    app_allStates_runOnTick100Hz();
+}
+
+const State *app_driveState_get(void)
+{
+    static State drive_state = {
+        .name              = "DRIVE",
+        .run_on_entry      = driveStateRunOnEntry,
+        .run_on_tick_1Hz   = NULL,
+        .run_on_tick_100Hz = driveStateRunOnTick100Hz,
+        .run_on_exit       = NULL,
+    };
+
+    return &drive_state;
+}
