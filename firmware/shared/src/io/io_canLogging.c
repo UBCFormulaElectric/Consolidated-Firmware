25ceee7e42d5c9209412ba3856fee06ecad9e02d
Jiajun-Huang <jhuang430@qq.com>
2024-02-03 17:03:18 -0800

    little fix

--- firmware/shared/src/io/io_canLogging.c
+++ firmware/shared/src/io/io_canLogging.c
@@ -9,7 +9,7 @@
 
 // Private globals.
 static const CanConfig *config;
-#define QUEUE_SIZE 20
+#define QUEUE_SIZE 50
 #define QUEUE_BYTES sizeof(CanMsg) * QUEUE_SIZE
 #define PATH_LENGTH 10
 static osMessageQueueId_t message_queue_id;
