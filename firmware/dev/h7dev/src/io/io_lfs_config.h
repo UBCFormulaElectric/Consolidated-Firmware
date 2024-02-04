dd7e570259d55b3dcc06f1cc861ada97e051c98b
Jiajun-Huang <jhuang430@qq.com>
2024-02-03 17:03:18 -0800

    little fix

--- firmware/dev/h7dev/src/io/io_lfs_config.h
+++ firmware/dev/h7dev/src/io/io_lfs_config.h
@@ -3,17 +3,18 @@
 #include <stdint.h>
 
 // those factor will be multiple with the block size
+#define IO_LFS_BLOCK_SIZE_FACTOR 1
 #define IO_LFS_READ_SIZE_FACTOR 1
 #define IO_LFS_PROG_SIZE_FACTOR 1
 #define IO_LFS_LOOKAHEAD_SIZE_FACTOR 1
 #define IO_LFS_CACHE_SIZE_FACTOR 1 // a multiple of the read and program sizes
 #define IO_LFS_BLOCK_CYCLES 500
 
-#define IO_LFS_BLOCK_SIZE 512
-#define IO_LFS_READ_SIZE IO_LFS_BLOCK_SIZE *IO_LFS_READ_SIZE_FACTOR
-#define IO_LFS_PROG_SIZE IO_LFS_BLOCK_SIZE *IO_LFS_PROG_SIZE_FACTOR
-#define IO_LFS_LOOKAHEAD_SIZE IO_LFS_BLOCK_SIZE *IO_LFS_LOOKAHEAD_SIZE_FACTOR
-#define IO_LFS_CACHE_SIZE IO_LFS_BLOCK_SIZE *IO_LFS_CACHE_SIZE_FACTOR
+#define IO_LFS_BLOCK_SIZE HW_DEVICE_SECTOR_SIZE *IO_LFS_BLOCK_SIZE_FACTOR
+#define IO_LFS_READ_SIZE HW_DEVICE_SECTOR_SIZE *IO_LFS_READ_SIZE_FACTOR
+#define IO_LFS_PROG_SIZE HW_DEVICE_SECTOR_SIZE *IO_LFS_PROG_SIZE_FACTOR
+#define IO_LFS_LOOKAHEAD_SIZE HW_DEVICE_SECTOR_SIZE *IO_LFS_LOOKAHEAD_SIZE_FACTOR
+#define IO_LFS_CACHE_SIZE HW_DEVICE_SECTOR_SIZE *IO_LFS_CACHE_SIZE_FACTOR
 
 int io_lfs_read(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, void *buffer, lfs_size_t size);
 int io_lfs_prog(const struct lfs_config *c, lfs_block_t block, lfs_off_t off, const void *buffer, lfs_size_t size);
