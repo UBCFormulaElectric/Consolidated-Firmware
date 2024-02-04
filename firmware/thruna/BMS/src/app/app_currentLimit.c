26619fb1a6ecbabc031b8f090831e2550aae16a6
DJ90864 <dikshitjain664@gmail.com>
2024-02-03 16:16:26 -0800

    Blinky

---
+++ firmware/thruna/BMS/src/app/app_currentLimit.c
@@ -0,0 +1,6 @@
+// #include <math.h>
+// #include <stdint.h>
+// #include <float.h>
+
+// #include "app_math.h"
+
---
+++ firmware/thruna/BMS/src/app/app_currentLimit.h
@@ -0,0 +1,7 @@
+// #pragma once
+
+// #include "app_utils.h"
+// #include "app_accumulator.h"
+// #include "app_timer.h"
+// #include "app_eeprom.h"
+
