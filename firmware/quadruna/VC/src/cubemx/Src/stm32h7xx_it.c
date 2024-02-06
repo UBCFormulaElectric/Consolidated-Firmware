b696e1e69fcb7bb36fe646a752295d710beaa932
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2024-02-05 17:58:19 -0800

    Add bootloader to VC (#1137)
    ### Summary
    <!-- Quick summary of changes, optional -->
    
    Add bootloader to the VC. Had to enable a second CAN peripheral since
    the VC uses FDCAN1, whereas the H7 dev board used for testing uses
    FDCAN2.
    
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    - Add bootloader to the VC
    - Add blinky to VC
    - Fix outdated commit info on FSM
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    - Testing on bench, with real VC
    
    ### Resolved Issues
    <!-- Link any issues that this PR resolved like so: `Resolves #1, #2,
    and #5` (Note: Using this format, Github will automatically close the
    issue(s) when this PR is merged in). -->
    
    ### Checklist
    *Please change `[ ]` to `[x]` when you are ready.*
    - [x] I have read and followed the code conventions detailed in
    [README.md](../README.md) (*This will save time for both you and the
    reviewer!*).
    - [x] If this pull request is longer then **500** lines, I have provided
    *explicit* justification in the summary above explaining why I *cannot*
    break this up into multiple pull requests (*Small PR's are faster and
    less painful for everyone involved!*).

--- firmware/quadruna/VC/src/cubemx/Src/stm32h7xx_it.c
+++ firmware/quadruna/VC/src/cubemx/Src/stm32h7xx_it.c
@@ -56,7 +56,8 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
-extern TIM_HandleTypeDef htim6;
+extern FDCAN_HandleTypeDef hfdcan1;
+extern TIM_HandleTypeDef   htim6;
 
 /* USER CODE BEGIN EV */
 
@@ -159,6 +160,34 @@ void DebugMon_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32h7xx.s).                    */
 /******************************************************************************/
+
+/**
+ * @brief This function handles FDCAN1 interrupt 0.
+ */
+void FDCAN1_IT0_IRQHandler(void)
+{
+    /* USER CODE BEGIN FDCAN1_IT0_IRQn 0 */
+
+    /* USER CODE END FDCAN1_IT0_IRQn 0 */
+    HAL_FDCAN_IRQHandler(&hfdcan1);
+    /* USER CODE BEGIN FDCAN1_IT0_IRQn 1 */
+
+    /* USER CODE END FDCAN1_IT0_IRQn 1 */
+}
+
+/**
+ * @brief This function handles FDCAN1 interrupt 1.
+ */
+void FDCAN1_IT1_IRQHandler(void)
+{
+    /* USER CODE BEGIN FDCAN1_IT1_IRQn 0 */
+
+    /* USER CODE END FDCAN1_IT1_IRQn 0 */
+    HAL_FDCAN_IRQHandler(&hfdcan1);
+    /* USER CODE BEGIN FDCAN1_IT1_IRQn 1 */
+
+    /* USER CODE END FDCAN1_IT1_IRQn 1 */
+}
 
 /**
  * @brief This function handles TIM6 global interrupt, DAC1_CH1 and DAC1_CH2 underrun error interrupts.
