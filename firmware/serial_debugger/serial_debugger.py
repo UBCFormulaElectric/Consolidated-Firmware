58d6380782f155639946c764ad317365bc57367e
Steven141 <steven.brown1620@gmail.com>
2024-02-22 13:49:32 -0800

    add board classes

--- firmware/serial_debugger/serial_debugger.py
+++ firmware/serial_debugger/serial_debugger.py
@@ -5,6 +5,7 @@ API for the serial debugger library.
 import subprocess
 import serial
 from importlib import reload
+
 import board_libs.VC_pb2 as VC_lib
 import board_libs.BMS_pb2 as BMS_lib
 import board_libs.CRIT_pb2 as CRIT_lib
@@ -33,15 +34,9 @@ class Board():
 
     def get_incoming_packet_size(self) -> int:
         return int.from_bytes(self.ser.read(DEBUG_SIZE_MSG_BUF_SIZE), byteorder='little')
-
-
-class VC(Board):
-    def __init__(self, com_port: str) -> None:
-        super().__init__(com_port)
-        self.debug_msg = VC_lib.DebugMessage()
     
     def gpio_read(self, net_label: str) -> int:
-        self.debug_msg.gpio_read.net_name = VC_lib.GpioNetName.Value(net_label)
+        self.debug_msg.gpio_read.net_name = self.board_lib.GpioNetName.Value(net_label)
 
         serialized_gpio = self.debug_msg.SerializeToString()
         packet_size = len(serialized_gpio)
@@ -54,7 +49,7 @@ class VC(Board):
         return self.debug_msg.gpio_read.value - 1  # subtract one for enum scale offset
 
     def gpio_write(self, net_label: str, value: bool) -> None:
-        self.debug_msg.gpio_write.net_name = VC_lib.GpioNetName.Value(net_label)
+        self.debug_msg.gpio_write.net_name = self.board_lib.GpioNetName.Value(net_label)
         self.debug_msg.gpio_write.value = value + 1  # add one for enum scale offset
 
         serialized_gpio = self.debug_msg.SerializeToString()
@@ -67,7 +62,7 @@ class VC(Board):
         assert self.ser.read(packet_size) == serialized_gpio, "Debug Message Not Received"
 
     def adc_read(self, net_label: str) -> float:
-        self.debug_msg.adc.net_name = VC_lib.AdcNetName.Value(net_label)
+        self.debug_msg.adc.net_name = self.board_lib.AdcNetName.Value(net_label)
 
         serialized_adc = self.debug_msg.SerializeToString()
         packet_size = len(serialized_adc)
@@ -78,3 +73,34 @@ class VC(Board):
 
         self.debug_msg.ParseFromString(self.ser.read(packet_size))
         return self.debug_msg.adc.value
+
+
+class VC(Board):
+    def __init__(self, com_port: str) -> None:
+        super().__init__(com_port)
+        self.debug_msg = VC_lib.DebugMessage()
+        self.board_lib = VC_lib
+
+class BMS(Board):
+    def __init__(self, com_port: str) -> None:
+        super().__init__(com_port)
+        self.debug_msg = BMS_lib.DebugMessage()
+        self.board_lib = BMS_lib
+
+class FSM(Board):
+    def __init__(self, com_port: str) -> None:
+        super().__init__(com_port)
+        self.debug_msg = FSM_lib.DebugMessage()
+        self.board_lib = FSM_lib
+
+class RSM(Board):
+    def __init__(self, com_port: str) -> None:
+        super().__init__(com_port)
+        self.debug_msg = RSM_lib.DebugMessage()
+        self.board_lib = RSM_lib
+
+class CRIT(Board):
+    def __init__(self, com_port: str) -> None:
+        super().__init__(com_port)
+        self.debug_msg = CRIT_lib.DebugMessage()
+        self.board_lib = CRIT_lib
