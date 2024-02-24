252acdf632b1e160ba1b5461ead6a925d9873163
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2024-02-24 14:55:16 -0800

    BMS ADC hotfix

--- firmware/quadruna/BMS/src/cubemx/Src/main.c
+++ firmware/quadruna/BMS/src/cubemx/Src/main.c
@@ -356,7 +356,7 @@ static void MX_ADC1_Init(void)
      */
     sConfig.Channel                = ADC_CHANNEL_4;
     sConfig.Rank                   = ADC_REGULAR_RANK_1;
-    sConfig.SamplingTime           = ADC_SAMPLETIME_2CYCLES_5;
+    sConfig.SamplingTime           = ADC_SAMPLETIME_1CYCLE_5;
     sConfig.SingleDiff             = ADC_SINGLE_ENDED;
     sConfig.OffsetNumber           = ADC_OFFSET_NONE;
     sConfig.Offset                 = 0;
@@ -386,9 +386,10 @@ static void MX_ADC1_Init(void)
 
     /** Configure Regular Channel
      */
-    sConfig.Channel    = ADC_CHANNEL_10;
-    sConfig.Rank       = ADC_REGULAR_RANK_4;
-    sConfig.SingleDiff = ADC_DIFFERENTIAL_ENDED;
+    sConfig.Channel      = ADC_CHANNEL_10;
+    sConfig.Rank         = ADC_REGULAR_RANK_4;
+    sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
+    sConfig.SingleDiff   = ADC_DIFFERENTIAL_ENDED;
     if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
     {
         Error_Handler();
