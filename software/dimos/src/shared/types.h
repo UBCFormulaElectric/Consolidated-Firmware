bc785dbf181e534d8abbd0925959e26c287b3532
Edwin <20777515+Lucien950@users.noreply.github.com>
2024-02-26 18:33:48 -0800

    proper port

--- software/dimos/src/shared/types.h
+++ software/dimos/src/shared/types.h
@@ -1,9 +1,10 @@
+// ReSharper disable CppNonExplicitConvertingConstructor
 #pragma once
 
 #include "variant"
 
 template <typename Data, typename Error>
-class Result: std::variant<Data, Error>
+class Result: public std::variant<Data, Error>
 {
     /**
      * \brief Unwraps the result, throwing an exception if it is an error
@@ -15,4 +16,8 @@ class Result: std::variant<Data, Error>
             throw std::get<Error>(*this);
         return std::get<Data>(*this);
     }
+
+public:
+    Result(const Data& data): std::variant<Data, Error>(data) {} // NOLINT(*-explicit-constructor)
+    Result(const Error& error): std::variant<Data, Error>(error) {} // NOLINT(*-explicit-constructor)
 };
