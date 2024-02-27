4127c5950bc7c2230f3b32d181b6790d2bc925c7
will-chaba <willchabaubc@gmail.com>
2024-02-26 20:23:24 -0800

    test charging

--- firmware/quadruna/BMS/src/app/states/app_initState.c
+++ firmware/quadruna/BMS/src/app/states/app_initState.c
@@ -55,12 +55,6 @@ static void initStateRunOnTick100Hz(void)
             // precharge when preparing to charge
             const bool precharge_for_driving = !charger_connected && !cell_balancing_enabled;
 
-// TODO: Re-implement precharge after remaining testing completed
-#ifdef TARGET_EMBEDDED
-            (void)precharge_for_charging;
-            (void)precharge_for_driving;
-            (void)cell_balancing_enabled;
-#else
             if (precharge_for_charging)
             {
                 app_stateMachine_setNextState(app_prechargeState_get());
@@ -73,7 +67,6 @@ static void initStateRunOnTick100Hz(void)
             {
                 app_stateMachine_setNextState(app_balancingState_get());
             }
-#endif
         }
     }
 }
