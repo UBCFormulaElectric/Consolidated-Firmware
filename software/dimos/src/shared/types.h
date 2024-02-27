699dd1a6011c945d32396867f549185a01005337
Edwin <20777515+Lucien950@users.noreply.github.com>
2024-02-26 18:31:23 -0800

    new result enum

--- software/dimos/src/shared/types.h
+++ software/dimos/src/shared/types.h
@@ -2,4 +2,17 @@
 
 #include "variant"
 
-template <typename Data, typename Error> using Result = std::variant<Data, Error>;
+template <typename Data, typename Error>
+class Result: std::variant<Data, Error>
+{
+    /**
+     * \brief Unwraps the result, throwing an exception if it is an error
+     * \return The unwrapped data
+     */
+    Data unwrap()
+    {
+        if(std::holds_alternative<Error>(*this))
+            throw std::get<Error>(*this);
+        return std::get<Data>(*this);
+    }
+};
