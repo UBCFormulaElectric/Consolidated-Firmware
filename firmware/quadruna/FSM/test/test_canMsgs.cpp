ea423ff4ff644224e8d13bc991f07a7681604d0e
liam-ilan <snowboardsheep@gmail.com>
2024-03-02 16:24:12 -0800

    Added can msgs tests to quadruna fsm.

--- firmware/quadruna/FSM/test/test_canMsgs.cpp
+++ firmware/quadruna/FSM/test/test_canMsgs.cpp
@@ -6,12 +6,57 @@ extern "C"
 #include "app_canUtils.h"
 }
 
-TEST(CanMsgsTest, state_machine_message_frequency)
+TEST(CanMsgsTest, vitals_message_frequency)
 {
     ASSERT_GE(HZ_TO_MS(1), CAN_MSG_FSM_VITALS_CYCLE_TIME_MS);
 }
 
-TEST(CanMsgsTest, heartbeat_message_frequency)
+TEST(CanMsgsTest, alert_message_frequency)
 {
-    ASSERT_GE(HZ_TO_MS(1), CAN_MSG_FSM_VITALS_CYCLE_TIME_MS);
+    ASSERT_GE(HZ_TO_MS(1), CAN_MSG_FSM_ALERTS_CONTEXT_CYCLE_TIME_MS);
+}
+
+TEST(CanMsgsTest, commit_info_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(10), CAN_MSG_FSM_COMMIT_INFO_CYCLE_TIME_MS);
+}
+
+TEST(CanMsgsTest, wheels_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(10), CAN_MSG_FSM_WHEELS_CYCLE_TIME_MS);
+}
+
+TEST(CanMsgsTest, apps_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(10), CAN_MSG_FSM_APPS_CYCLE_TIME_MS);
+}
+
+TEST(CanMsgsTest, brake_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(10), CAN_MSG_FSM_BRAKE_CYCLE_TIME_MS);
+}
+
+TEST(CanMsgsTest, suspension_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(10), CAN_MSG_FSM_SUSPENSION_CYCLE_TIME_MS);
+}
+
+TEST(CanMsgsTest, warnings_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(1), CAN_MSG_FSM_WARNINGS_CYCLE_TIME_MS);
+}
+
+TEST(CanMsgsTest, faults_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(1), CAN_MSG_FSM_FAULTS_CYCLE_TIME_MS);
+}
+
+TEST(CanMsgsTest, warnings_counts_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(1), CAN_MSG_FSM_WARNINGS_COUNTS_CYCLE_TIME_MS);
+}
+
+TEST(CanMsgsTest, faults_counts_message_frequency)
+{
+    ASSERT_GE(HZ_TO_MS(1), CAN_MSG_FSM_FAULTS_COUNTS_CYCLE_TIME_MS);
 }
