b95e8154f4c9be96f1425477308e4818c3f3ee15
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2024-03-04 17:04:52 -0800

    update chimera python proto lib generator bash script

--- validationtools/validationtools/chimera/generate_libs.sh
+++ validationtools/validationtools/chimera/generate_libs.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-for file in proto/*.proto
-do
-    protoc -I proto --python_out ../../firmware/chimera/proto_libs $file
+rootDir=$(realpath "$(dirname "$BASH_SOURCE")/../../../")
+
+# Create the directory, ignoring the error if it already exists
+mkdir -p "$rootDir/validationtools/validationtools/chimera/proto_libs" 2> /dev/null
+
+# Iterate over all .proto files in the specified directory
+find "$rootDir/firmware/chimera/proto" -name "*.proto" | while read -r file; do
+    protoc \
+        -I "$rootDir/firmware/chimera/proto" \
+        --python_out="$rootDir/validationtools/validationtools/chimera/proto_libs" "$file"
 
     protol \
-    --in-place \
-    --python-out ../../firmware/chimera/proto_libs \
-    protoc --proto-path=proto $file
+        --in-place \
+        --python-out "$rootDir/validationtools/validationtools/chimera/proto_libs" \
+        protoc --proto-path="$rootDir/firmware/chimera/proto" $file
 done
-
