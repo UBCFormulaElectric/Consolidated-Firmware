32a7a6b95a1cd684f9a5703dfa09501d8354b66e
Jiajun-Huang <jhuang430@qq.com>
2024-03-09 15:57:43 -0800

    6k 0 loss

--- firmware/dev/h7dev/src/io/io_lfs_config.h
+++ firmware/dev/h7dev/src/io/io_lfs_config.h
@@ -4,11 +4,11 @@
 
 #define LFS_NO_MALLOC 1
 // those factor will be multiple with the block size
-#define IO_LFS_BLOCK_SIZE_FACTOR 4
-#define IO_LFS_READ_SIZE_FACTOR 4
-#define IO_LFS_PROG_SIZE_FACTOR 4
-#define IO_LFS_LOOKAHEAD_SIZE_FACTOR 4
-#define IO_LFS_CACHE_SIZE_FACTOR 4 // a multiple of the read and program sizes
+#define IO_LFS_BLOCK_SIZE_FACTOR 2
+#define IO_LFS_READ_SIZE_FACTOR 2
+#define IO_LFS_PROG_SIZE_FACTOR 2
+#define IO_LFS_LOOKAHEAD_SIZE_FACTOR 2
+#define IO_LFS_CACHE_SIZE_FACTOR 2 // a multiple of the read and program sizes
 #define IO_LFS_BLOCK_CYCLES 500
 
 #define IO_LFS_BLOCK_SIZE HW_DEVICE_SECTOR_SIZE *IO_LFS_BLOCK_SIZE_FACTOR
