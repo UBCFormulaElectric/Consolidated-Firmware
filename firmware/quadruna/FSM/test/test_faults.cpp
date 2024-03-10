7471fa6805353e7552c27ac08aa17fb11b2f031e
liam-ilan <snowboardsheep@gmail.com>
2024-03-09 16:43:40 -0800

    Added rest of FSM quadruna tests, still working on fixing failing one.

--- firmware/quadruna/FSM/test/test_faults.cpp
+++ firmware/quadruna/FSM/test/test_faults.cpp
@@ -172,54 +172,52 @@ TEST_F(FsmFaultsTest, apps_disagreement_sets_mapped_pedal_percentage_to_zero_and
         float papps_percentage;
         float sapps_percentage;
         bool  expect_fault;
-    } test_params[] = {
+    } test_params[7] = {
         {
             // Papps not greater than sapps
             .papps_percentage = 10,
             .sapps_percentage = 0,
             .expect_fault     = false,
         },
-        // {
-        //     // Papps just greater than sapps
-        //     .papps_percentage = 11,
-        //     .sapps_percentage = 0,
-        //     .expect_fault     = true,
-        // },
-        // {
-        //     // Papps much greater than sapps
-        //     .papps_percentage = 75,
-        //     .sapps_percentage = 25,
-        //     .expect_fault     = true,
-        // },
+        {
+            // Papps just greater than sapps
+            .papps_percentage = 11,
+            .sapps_percentage = 0,
+            .expect_fault     = true,
+        },
+        {
+            // Papps much greater than sapps
+            .papps_percentage = 75,
+            .sapps_percentage = 25,
+            .expect_fault     = true,
+        },
         {
             // Sapps not greater than papps
             .papps_percentage = 0,
             .sapps_percentage = 10,
             .expect_fault     = false,
         },
-        // {
-        //     // Sapps just greater than papps
-        //     .papps_percentage = 0,
-        //     .sapps_percentage = 11,
-        //     .expect_fault     = true,
-        // },
-        // {
-        //     // Sapps much greater than sapps
-        //     .papps_percentage = 25,
-        //     .sapps_percentage = 75,
-        //     .expect_fault     = true,
-        // },
+        {
+            // Sapps just greater than papps
+            .papps_percentage = 0,
+            .sapps_percentage = 11,
+            .expect_fault     = true,
+        },
+        {
+            // Sapps much greater than sapps
+            .papps_percentage = 25,
+            .sapps_percentage = 75,
+            .expect_fault     = true,
+        },
         {
             // Control: papps = sapps
             .papps_percentage = 50,
             .sapps_percentage = 50,
             .expect_fault     = false,
-        }
+        },
     };
 
-    int test_params_len = sizeof(test_params) / sizeof(test_params[0]);
-
-    for (int i = 0; i < test_params_len; i++)
+    for (int i = 0; i < 5; i++)
     {
         TearDown();
         SetUp();
@@ -270,4 +268,92 @@ TEST_F(FsmFaultsTest, apps_disagreement_sets_mapped_pedal_percentage_to_zero_and
             ASSERT_FALSE(app_canAlerts_FSM_Warning_AppsDisagreement_get());
         }
     }
+}
+
+TEST_F(FsmFaultsTest, brake_actuated_sets_mapped_pedal_percentage_to_zero_and_sets_fault_if_papps_pressed)
+{
+    struct
+    {
+        float apps_percentage;
+        bool  brake_actuated;
+        bool  expect_fault;
+    } test_params[5] = {
+        { 0, false, false },  // Papps not pressed and brake not actuated, no fault
+        { 24, true, false },  // Papps not pressed enough, no fault
+        { 26, false, false }, // Brake not actuated, no fault
+        { 26, true, true },   // Papps pressed enough + brake actuated, fault
+        { 75, true, true },   // Papps pressed significantly + brake actuated, fault
+    };
+
+    for (int i = 0; i < 5; i++)
+    {
+        TearDown();
+        SetUp();
+
+        // Actuate brake, and set peddal percentage
+        fake_io_apps_getPrimary_returns(test_params[i].apps_percentage);
+        fake_io_apps_getSecondary_returns(test_params[i].apps_percentage);
+        fake_io_brake_isActuated_returns(test_params[i].brake_actuated);
+        LetTimePass(10 + APP_BRAKE_TIME_TO_FAULT - 1);
+        ASSERT_NEAR(test_params[i].apps_percentage, app_canTx_FSM_PappsMappedPedalPercentage_get(), 0.5f);
+        ASSERT_NEAR(test_params[i].apps_percentage, app_canTx_FSM_SappsMappedPedalPercentage_get(), 0.5f);
+        ASSERT_FALSE(app_canAlerts_FSM_Warning_BrakeAppsDisagreement_get());
+
+        if (test_params[i].expect_fault)
+        {
+            // Signal debounce has elapsed, expect fault
+            LetTimePass(1);
+            ASSERT_EQ(0, app_canTx_FSM_PappsMappedPedalPercentage_get());
+            ASSERT_EQ(0, app_canTx_FSM_SappsMappedPedalPercentage_get());
+            ASSERT_TRUE(app_canAlerts_FSM_Warning_BrakeAppsDisagreement_get());
+
+            // Check faulted indefinitely
+            LetTimePass(1000);
+            ASSERT_FLOAT_EQ(0, app_canTx_FSM_PappsMappedPedalPercentage_get());
+            ASSERT_FLOAT_EQ(0, app_canTx_FSM_SappsMappedPedalPercentage_get());
+            ASSERT_TRUE(app_canAlerts_FSM_Warning_BrakeAppsDisagreement_get());
+
+            // Clear condition, confirm fault resets
+            fake_io_apps_getPrimary_returns(4); // Primary must be <5% to clear
+            fake_io_apps_getSecondary_returns(4);
+
+            LetTimePass(10 + APP_BRAKE_TIME_TO_CLEAR - 1);
+            ASSERT_FLOAT_EQ(0, app_canTx_FSM_PappsMappedPedalPercentage_get());
+            ASSERT_FLOAT_EQ(0, app_canTx_FSM_SappsMappedPedalPercentage_get());
+            ASSERT_TRUE(app_canAlerts_FSM_Warning_BrakeAppsDisagreement_get());
+
+            LetTimePass(1);
+            ASSERT_NEAR(4, app_canTx_FSM_PappsMappedPedalPercentage_get(), 0.5f);
+            ASSERT_NEAR(4, app_canTx_FSM_SappsMappedPedalPercentage_get(), 0.5f);
+            ASSERT_FALSE(app_canAlerts_FSM_Warning_BrakeAppsDisagreement_get());
+        }
+        else
+        {
+            // No fault condition, confirm no fault is set
+            LetTimePass(1000);
+            ASSERT_NEAR(test_params[i].apps_percentage, app_canTx_FSM_PappsMappedPedalPercentage_get(), 0.5f);
+            ASSERT_NEAR(test_params[i].apps_percentage, app_canTx_FSM_SappsMappedPedalPercentage_get(), 0.5f);
+            ASSERT_FALSE(app_canAlerts_FSM_Warning_BrakeAppsDisagreement_get());
+        }
+    }
+}
+
+TEST_F(FsmFaultsTest, steering_sensor_ocsc_sets_warning)
+{
+    LetTimePass(10);
+    ASSERT_FALSE(app_canAlerts_FSM_Warning_SteeringAngleOCSC_get());
+
+    // Set steering wheel OCSC
+    fake_io_steering_sensorOCSC_returns(true);
+    LetTimePass(10);
+    ASSERT_TRUE(app_canAlerts_FSM_Warning_SteeringAngleOCSC_get());
+
+    // Confirm set indefinitely
+    LetTimePass(1000);
+    ASSERT_TRUE(app_canAlerts_FSM_Warning_SteeringAngleOCSC_get());
+
+    // Clear condition, confirm warning resets
+    fake_io_steering_sensorOCSC_returns(false);
+    LetTimePass(10);
+    ASSERT_FALSE(app_canAlerts_FSM_Warning_SteeringAngleOCSC_get());
 }
