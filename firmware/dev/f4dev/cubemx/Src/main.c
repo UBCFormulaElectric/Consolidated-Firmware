ca30a7ff1e4fada774994b3da88a185139302d7a
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2024-03-09 18:02:03 -0800

    Align CAN timings with FDCAN supported values (#1177)
    ### Summary
    <!-- Quick summary of changes, optional -->
    
    Our current CAN timings are:
    - Time quantum: 250ns
    - Time Seg1: 6 quanta
    - Time Seg2: 1 quanta
    
    These are not supported by the FDCAN peripheral on the STM32H733 MCU!
    The closest supported values are:
    - Time quantum: 125ns
    - Time Seg1: 13 quanta
    - Time Seg2: 2 quanta
    
    As far as I can tell, the most important parameter (besides bitrate) in
    CAN timing configuration is sample point, which is the percentage of a
    single bit's period on which the bus is sampled. This is given by:
    ```
    SP = (t_seg1 + 1)/(t_seg1 + t_seg2 + 1)
    ```
    
    Both configurations yield the same sample point (87.5%). So, both should
    be identical in practice! Regardless, I've gone and updated all the
    boards in our repo to use the new timings since I feel like they should
    be consistent across all boards. Also, hopefully if there are any latent
    CAN unreliabilities with the new timings, they will be caught early.
    
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    Updated CAN bit timings as per above for all boards in our repo.
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    - [x] Tested on Thruna + spun wheels
    
    ### Resolved Issues
    <!-- Link any issues that this PR resolved like so: `Resolves #1, #2,
    and #5` (Note: Using this format, Github will automatically close the
    issue(s) when this PR is merged in). -->
    
    ### Checklist
    *Please change `[ ]` to `[x]` when you are ready.*
    - [x] I have read and followed the code conventions detailed in
    [README.md](../README.md) (*This will save time for both you and the
    reviewer!*).
    - [x] If this pull request is longer then **500** lines, I have provided
    *explicit* justification in the summary above explaining why I *cannot*
    break this up into multiple pull requests (*Small PR's are faster and
    less painful for everyone involved!*).

--- firmware/dev/f4dev/cubemx/Src/main.c
+++ firmware/dev/f4dev/cubemx/Src/main.c
@@ -356,17 +356,17 @@ static void MX_CAN1_Init(void)
 
     /* USER CODE END CAN1_Init 1 */
     hcan1.Instance                  = CAN1;
-    hcan1.Init.Prescaler            = 16;
+    hcan1.Init.Prescaler            = 6;
     hcan1.Init.Mode                 = CAN_MODE_NORMAL;
-    hcan1.Init.SyncJumpWidth        = CAN_SJW_1TQ;
-    hcan1.Init.TimeSeg1             = CAN_BS1_1TQ;
-    hcan1.Init.TimeSeg2             = CAN_BS2_1TQ;
+    hcan1.Init.SyncJumpWidth        = CAN_SJW_4TQ;
+    hcan1.Init.TimeSeg1             = CAN_BS1_13TQ;
+    hcan1.Init.TimeSeg2             = CAN_BS2_2TQ;
     hcan1.Init.TimeTriggeredMode    = DISABLE;
-    hcan1.Init.AutoBusOff           = DISABLE;
+    hcan1.Init.AutoBusOff           = ENABLE;
     hcan1.Init.AutoWakeUp           = DISABLE;
-    hcan1.Init.AutoRetransmission   = DISABLE;
-    hcan1.Init.ReceiveFifoLocked    = DISABLE;
-    hcan1.Init.TransmitFifoPriority = DISABLE;
+    hcan1.Init.AutoRetransmission   = ENABLE;
+    hcan1.Init.ReceiveFifoLocked    = ENABLE;
+    hcan1.Init.TransmitFifoPriority = ENABLE;
     if (HAL_CAN_Init(&hcan1) != HAL_OK)
     {
         Error_Handler();
@@ -391,17 +391,17 @@ static void MX_CAN2_Init(void)
 
     /* USER CODE END CAN2_Init 1 */
     hcan2.Instance                  = CAN2;
-    hcan2.Init.Prescaler            = 16;
+    hcan2.Init.Prescaler            = 6;
     hcan2.Init.Mode                 = CAN_MODE_NORMAL;
     hcan2.Init.SyncJumpWidth        = CAN_SJW_1TQ;
-    hcan2.Init.TimeSeg1             = CAN_BS1_1TQ;
-    hcan2.Init.TimeSeg2             = CAN_BS2_1TQ;
+    hcan2.Init.TimeSeg1             = CAN_BS1_13TQ;
+    hcan2.Init.TimeSeg2             = CAN_BS2_2TQ;
     hcan2.Init.TimeTriggeredMode    = DISABLE;
-    hcan2.Init.AutoBusOff           = DISABLE;
+    hcan2.Init.AutoBusOff           = ENABLE;
     hcan2.Init.AutoWakeUp           = DISABLE;
-    hcan2.Init.AutoRetransmission   = DISABLE;
-    hcan2.Init.ReceiveFifoLocked    = DISABLE;
-    hcan2.Init.TransmitFifoPriority = DISABLE;
+    hcan2.Init.AutoRetransmission   = ENABLE;
+    hcan2.Init.ReceiveFifoLocked    = ENABLE;
+    hcan2.Init.TransmitFifoPriority = ENABLE;
     if (HAL_CAN_Init(&hcan2) != HAL_OK)
     {
         Error_Handler();
