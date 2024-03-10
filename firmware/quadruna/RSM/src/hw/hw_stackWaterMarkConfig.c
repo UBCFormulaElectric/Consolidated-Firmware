a3194c042534cec2130ae8dea814585fd33cfed8
Shayan Ajmal <shayanajmaluni@gmail.com>
2024-03-09 16:16:18 -0800

    Stack water mark has been configured

--- firmware/quadruna/RSM/src/hw/hw_stackWaterMarkConfig.c
+++ firmware/quadruna/RSM/src/hw/hw_stackWaterMarkConfig.c
@@ -6,6 +6,7 @@
 #include "app_utils.h"
 #include "hw_stackWaterMarkConfig.h"
 #include "app_canAlerts.h"
+#include "app_canAlerts.h"
 
 extern TaskHandle_t Task1HzHandle;
 extern TaskHandle_t Task100HzHandle;
@@ -39,6 +40,69 @@ static void logWaterMarkAboveThresholdTaskCanRx(uint8_t error)
 static void logWaterMarkAboveThresholdTaskCanTx(uint8_t error)
 {
     app_canAlerts_RSM_Warning_StackWaterMarkHighTaskCanTx_set(true);
+}
+
+/** @brief Iterate through this table to check stack watermarks for each task */
+static StackWaterMark stack_watermarks[] = {
+    {
+        .handle              = &Task1HzHandle,
+        .stack_size          = 512,
+        .watermark_threshold = STACK_HIGH_WATERMARK_THRESHOLD,
+        .log_error           = logWaterMarkAboveThresholdTask1Hz,
+    },
+    {
+        .handle              = &Task100HzHandle,
+        .stack_size          = 512,
+        .watermark_threshold = STACK_HIGH_WATERMARK_THRESHOLD,
+        .log_error           = logWaterMarkAboveThresholdTask100Hz,
+    },
+    {
+        .handle              = &Task1kHzHandle,
+        .stack_size          = 512,
+        .watermark_threshold = STACK_HIGH_WATERMARK_THRESHOLD,
+        .log_error           = logWaterMarkAboveThresholdTask1kHz,
+    },
+    {
+        .handle              = &TaskCanRxHandle,
+        .stack_size          = 512,
+        .watermark_threshold = STACK_HIGH_WATERMARK_THRESHOLD,
+        .log_error           = logWaterMarkAboveThresholdTaskCanRx,
+    },
+    {
+        .handle              = &TaskCanTxHandle,
+        .stack_size          = 512,
+        .watermark_threshold = STACK_HIGH_WATERMARK_THRESHOLD,
+        .log_error           = logWaterMarkAboveThresholdTaskCanTx,
+    },
+};
+
+void hw_stackWaterMarkConfig_check(void)
+{
+    hw_stackWaterMark_check(stack_watermarks, NUM_ELEMENTS_IN_ARRAY(stack_watermarks));
+}
+static void logWaterMarkAboveThresholdTask1kHz(uint8_t error)
+{
+    app_canAlerts_RSM_Warning_StackWaterMarkHighTask1kHz_set(true);
+}
+
+static void logWaterMarkAboveThresholdTask100Hz(uint8_t error)
+{
+    app_canAlerts_RSM_Warning_StackWaterMarkHighTask100kHz_set(true);
+}
+
+static void logWaterMarkAboveThresholdTask1Hz(uint8_t error)
+{
+    app_canAlerts_RSM_Warning_StackWaterMarkHighTask1Hz_set(true);
+}
+
+static void logWaterMarkAboveThresholdTaskCanRx(uint8_t error)
+{
+    app_canAlerts_DCM_Warning_StackWaterMarkHighTaskCanRx_set(true);
+}
+
+static void logWaterMarkAboveThresholdTaskCanTx(uint8_t error)
+{
+    app_canAlerts_DCM_Warning_StackWaterMarkHighTaskCanTx_set(true);
 }
 
 /** @brief Iterate through this table to check stack watermarks for each task */
