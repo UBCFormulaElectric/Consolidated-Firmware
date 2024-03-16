718cc15c1443fa2e96c6ca47406619c60bfe27c2
liam-ilan <snowboardsheep@gmail.com>
2024-03-16 02:23:02 -0700

    VC now builds.

--- firmware/quadruna/VC/src/app/states/app_allStates.c
+++ firmware/quadruna/VC/src/app/states/app_allStates.c
@@ -1,4 +1,10 @@
+#include <stddef.h>
 #include "states/app_allStates.h"
+#include "app_canTx.h"
+#include "app_canRx.h"
+#include "app_utils.h"
+#include "app_heartbeatMonitor.h"
+#include "io_lowVoltageBattery.h"
 
 void app_allStates_runOnTick1Hz(void) {}
 
