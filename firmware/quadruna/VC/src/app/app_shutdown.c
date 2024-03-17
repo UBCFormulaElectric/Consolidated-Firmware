5ecd33c66abd82dfea135e0a481b87822b621a7d
liam-ilan <snowboardsheep@gmail.com>
2024-03-16 18:05:01 -0700

    Added shutdown app layer.

---
+++ firmware/quadruna/VC/src/app/app_shutdown.c
@@ -0,0 +1,14 @@
+#include "app_shutdown.h"
+#include <stdbool.h>
+#include "io_shutdown.h"
+#include "app_canRx.h"
+#include "app_canTx.h"
+#include "app_canAlerts.h"
+
+
+void app_shutdown_broadcast(void) {
+    app_canAlerts_VC_Fault_LEStopFault_set(io_shutdown_hasLEStopFault());
+    app_canAlerts_VC_Fault_REStopFault_set(io_shutdown_hasREStopFault());
+    app_canAlerts_VC_Fault_PCMCInterlockFault_set(io_shutdown_hasPcmcFault());
+    app_canAlerts_VC_Fault_TSMSFault_set(io_shutdown_hasTsmsFault());
+}
