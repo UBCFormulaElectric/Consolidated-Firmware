2066169daeb31e67f61e3476c6e2422fecb31fa7
Edwin <20777515+Lucien950@users.noreply.github.com>
2024-03-17 23:11:23 -0700

    CircleProgressBar Improvements

--- software/dimos/src/ui/components/CircleProgressBar.cpp
+++ software/dimos/src/ui/components/CircleProgressBar.cpp
@@ -9,24 +9,25 @@ CircleProgressBar::CircleProgressBar(QQuickItem *parent) // NOLINT(*-pro-type-me
 
 void CircleProgressBar::paint(QPainter *p)
 {
-    QPen bg_pen(m_bg_color, m_stroke_width, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
-    QPen bar_pen(m_bar_color, m_stroke_width, Qt::SolidLine, Qt::RoundCap, Qt::RoundJoin);
-    if (!m_round_tip)
-    {
-        bg_pen.setCapStyle(Qt::FlatCap);
-        bar_pen.setCapStyle(Qt::FlatCap);
-    }
+    const QPen bg_pen(
+        m_bg_color, m_stroke_width, Qt::SolidLine, m_round_tip ? Qt::RoundCap : Qt::FlatCap, Qt::RoundJoin);
+    const QPen bar_pen(
+        m_bar_color, m_bar_stroke_width != -1 ? m_bar_stroke_width : m_stroke_width, Qt::SolidLine,
+        m_round_tip ? Qt::RoundCap : Qt::FlatCap, Qt::RoundJoin);
 
     p->setRenderHint(QPainter::Antialiasing);
 
     const int delta_theta = m_end_angle - m_start_angle;
 
-    const QRectF boundRect = this->boundingRect().marginsRemoved(
-        QMargins(m_stroke_width / 2, m_stroke_width / 2, m_stroke_width / 2, m_stroke_width / 2));
+    const double remove_margin = static_cast<double>(std::max(m_bar_stroke_width, m_stroke_width)) / 2.0;
+    const QRectF boundRect =
+        this->boundingRect().marginsRemoved(QMarginsF(remove_margin, remove_margin, remove_margin, remove_margin));
     p->setCompositionMode(QPainter::CompositionMode_SourceOver);
     p->setPen(bg_pen);
-    p->drawArc(boundRect, m_start_angle * 16, delta_theta * 16);
+    p->drawEllipse(boundRect);
 
     p->setPen(bar_pen);
-    p->drawArc(boundRect, m_start_angle * 16, static_cast<int>(round(delta_theta * 16.0 * m_percentage)));
+    p->drawArc(
+        boundRect, m_start_angle * 16,
+        static_cast<int>(round(delta_theta * 16.0 * m_percentage)) * (m_turn_right ? -1 : 1));
 }
--- software/dimos/src/ui/components/CircleProgressBar.h
+++ software/dimos/src/ui/components/CircleProgressBar.h
@@ -6,20 +6,22 @@ class CircleProgressBar : public QQuickPaintedItem
 {
     Q_OBJECT
     QML_ELEMENT
-    Q_PROPERTY(float percentage MEMBER m_percentage FINAL REQUIRED)
-    Q_PROPERTY(int stroke_width MEMBER m_stroke_width FINAL REQUIRED)
-    Q_PROPERTY(int start_angle MEMBER m_start_angle FINAL REQUIRED)
-    Q_PROPERTY(int end_angle MEMBER m_end_angle FINAL REQUIRED)
-    Q_PROPERTY(bool round_tip MEMBER m_round_tip FINAL REQUIRED)
-    Q_PROPERTY(QColor bar_color MEMBER m_bar_color FINAL REQUIRED)
-    Q_PROPERTY(QColor bg_color MEMBER m_bg_color FINAL REQUIRED)
+    Q_PROPERTY(float percentage MEMBER m_percentage REQUIRED)
+    Q_PROPERTY(int stroke_width MEMBER m_stroke_width REQUIRED)
+    Q_PROPERTY(int bar_stroke_width MEMBER m_bar_stroke_width)
+    Q_PROPERTY(int start_angle MEMBER m_start_angle REQUIRED)
+    Q_PROPERTY(int end_angle MEMBER m_end_angle REQUIRED)
+    Q_PROPERTY(bool round_tip MEMBER m_round_tip REQUIRED)
+    Q_PROPERTY(QColor bar_color MEMBER m_bar_color REQUIRED)
+    Q_PROPERTY(QColor bg_color MEMBER m_bg_color REQUIRED)
+    Q_PROPERTY(bool turn_right MEMBER m_turn_right)
   public:
     explicit CircleProgressBar(QQuickItem *parent = nullptr);
-    void paint(QPainter *p) override;
+    void     paint(QPainter *p) override;
 
   private:
     /**
-     * @brief 0 -> 1 value
+     * @brief -1 -> 1 value
      */
     float m_percentage;
 
@@ -27,6 +29,7 @@ class CircleProgressBar : public QQuickPaintedItem
      * @brief The stroke_width` of the circle stroke
      */
     int m_stroke_width;
+    int m_bar_stroke_width = -1;
 
     /**
      * @brief ON ANGLES
@@ -43,4 +46,6 @@ class CircleProgressBar : public QQuickPaintedItem
 
     QColor m_bar_color;
     QColor m_bg_color;
+
+    bool m_turn_right = false;
 };
