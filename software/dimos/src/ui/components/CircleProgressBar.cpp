fe6dba3f3139d991fbf563b1e795602cf6edcbd7
Edwin <20777515+Lucien950@users.noreply.github.com>
2024-03-18 20:35:10 -0700

    newest circleprogressbar

--- software/dimos/src/ui/components/CircleProgressBar.cpp
+++ software/dimos/src/ui/components/CircleProgressBar.cpp
@@ -11,7 +11,8 @@ void CircleProgressBar::paint(QPainter *p)
 {
     p->setRenderHint(QPainter::Antialiasing);
     p->setCompositionMode(QPainter::CompositionMode_SourceOver);
-    const Qt::PenCapStyle tipShape = m_round_tip ? Qt::RoundCap : Qt::FlatCap;
+    const Qt::PenCapStyle tipShape             = m_round_tip ? Qt::RoundCap : Qt::FlatCap;
+    const int             direction_multiplier = m_turn_right ? -1 : 1;
 
     const double remove_margin = static_cast<double>(std::max(m_bg_stroke_width, m_stroke_width)) / 2.0;
     const QRectF boundRect =
@@ -19,15 +20,29 @@ void CircleProgressBar::paint(QPainter *p)
 
     if (m_background)
     {
-        p->setPen(QPen(
-            m_bg_color, m_bg_stroke_width != -1 ? m_bg_stroke_width : m_stroke_width, Qt::SolidLine, tipShape,
-            Qt::RoundJoin));
-        p->drawEllipse(boundRect);
+        if (m_bg_start_angle == -1 || m_bg_end_angle == -1)
+        {
+            m_bg_start_angle = m_start_angle;
+            m_bg_end_angle   = m_end_angle;
+        }
+        if (m_bg_stroke_width == -1)
+        {
+            m_bg_stroke_width = m_stroke_width;
+        }
+        if (m_bg_color == -1)
+        {
+            qCritical() << "Background color is not set";
+        }
+        const int delta_theta = m_bg_end_angle - m_bg_start_angle;
+        p->setPen(QPen(m_bg_color, m_bg_stroke_width, Qt::SolidLine, tipShape, Qt::RoundJoin));
+        p->drawArc(
+            boundRect, m_start_angle * 16,
+            static_cast<int>(round(delta_theta * 16.0 * m_percentage)) * direction_multiplier);
     }
 
     const int delta_theta = m_end_angle - m_start_angle;
     p->setPen(QPen(m_bar_color, m_stroke_width, Qt::SolidLine, tipShape, Qt::RoundJoin));
     p->drawArc(
         boundRect, m_start_angle * 16,
-        static_cast<int>(round(delta_theta * 16.0 * m_percentage)) * (m_turn_right ? -1 : 1));
+        static_cast<int>(round(delta_theta * 16.0 * m_percentage)) * direction_multiplier);
 }
--- software/dimos/src/ui/components/CircleProgressBar.h
+++ software/dimos/src/ui/components/CircleProgressBar.h
@@ -9,16 +9,19 @@ class CircleProgressBar : public QQuickPaintedItem
 
     Q_PROPERTY(bool round_tip MEMBER m_round_tip REQUIRED)
     // bar properties
-    Q_PROPERTY(QColor bar_color MEMBER m_bar_color REQUIRED)
     Q_PROPERTY(int stroke_width MEMBER m_stroke_width REQUIRED)
-    // background properties
-    Q_PROPERTY(bool background MEMBER m_background)
-    Q_PROPERTY(int bg_stroke_width MEMBER m_bg_stroke_width) // only for override
-    Q_PROPERTY(QColor bg_color MEMBER m_bg_color)
-    // angles
-    Q_PROPERTY(float percentage MEMBER m_percentage REQUIRED)
+    Q_PROPERTY(QColor bar_color MEMBER m_bar_color REQUIRED)
     Q_PROPERTY(int start_angle MEMBER m_start_angle REQUIRED)
     Q_PROPERTY(int end_angle MEMBER m_end_angle REQUIRED)
+    // background on
+    Q_PROPERTY(bool background MEMBER m_background)
+    // background properties
+    Q_PROPERTY(int bg_stroke_width MEMBER m_bg_stroke_width) // only for override
+    Q_PROPERTY(QColor bg_color MEMBER m_bg_color)
+    Q_PROPERTY(int bg_start_angle MEMBER m_bg_start_angle)
+    Q_PROPERTY(int bg_end_angle MEMBER m_bg_end_angle)
+    // angles
+    Q_PROPERTY(float percentage MEMBER m_percentage REQUIRED)
     Q_PROPERTY(bool turn_right MEMBER m_turn_right)
   public:
     explicit CircleProgressBar(QQuickItem *parent = nullptr);
@@ -43,6 +46,8 @@ class CircleProgressBar : public QQuickPaintedItem
      */
     int m_start_angle;
     int m_end_angle;
+    int m_bg_start_angle = -1;
+    int m_bg_end_angle   = -1;
 
     /**
      * @brief Whether or not the stroke is round
@@ -50,7 +55,7 @@ class CircleProgressBar : public QQuickPaintedItem
     bool m_round_tip;
 
     QColor m_bar_color;
-    QColor m_bg_color;
+    QColor m_bg_color = -1;
 
     bool m_turn_right = false;
     bool m_background = true;
