b61dabcd868e3b20c4280d3ae211e038f6eb34f4
Jiajun-Huang <jhuang430@qq.com>
2024-03-19 15:13:29 -0700

    reenable the tick

--- firmware/dev/h7dev/cubemx/Src/main.c
+++ firmware/dev/h7dev/cubemx/Src/main.c
@@ -166,7 +166,7 @@ int main(void)
     SystemClock_Config();
 
     /* USER CODE BEGIN SysInit */
-
+    HAL_InitTick(0);
     /* USER CODE END SysInit */
 
     /* Initialize all configured peripherals */
