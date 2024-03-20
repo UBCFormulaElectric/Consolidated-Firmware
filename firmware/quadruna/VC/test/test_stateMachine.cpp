a5f94a987259579a69d6528ae592780b071c8c93
AmirTajaddodi <amir.tajadodi@gmail.com>
2024-03-20 13:35:31 -0700

    Commented our VC unit testing

--- firmware/quadruna/VC/test/test_stateMachine.cpp
+++ firmware/quadruna/VC/test/test_stateMachine.cpp
@@ -1,22 +1,17 @@
-#include "test_vcBaseStateMachineTest.h"
+// #include "test_vcBaseStateMachineTest.h"
 
-class StateMachineTest : public VcBaseStateMachineTest
-{
-};
+//Finish up unit testing once. Crit is done and we have all boards ready. Needs to be done before we flash anything onto VC
 
-TEST_F(StateMachineTest, check_init_state_is_broadcasted_over_can)
-{
-    SetInitialState(app_initState_get());
-    EXPECT_EQ(VC_INIT_STATE, app_canTx_VC_State_get());
-    EXPECT_EQ(app_powerManager_getState(), POWER_MANAGER_SHUTDOWN);
-}
+// class StateMachineTest : public VcBaseStateMachineTest
+// {
+// };
 
-TEST_F(StateMachineTest, check_drive_state_is_broadcasted_over_can)
-{
-    SetInitialState(app_driveState_get());
-    EXPECT_EQ(VC_DRIVE_STATE, app_canTx_VC_State_get());
-    EXPECT_EQ(app_powerManager_getState(), POWER_MANAGER_DRIVE);
-}
+// TEST_F(StateMachineTest, check_init_state_is_broadcasted_over_can)
+// {
+//     SetInitialState(app_initState_get());
+//     EXPECT_EQ(VC_INIT_STATE, app_canTx_VC_State_get());
+//     EXPECT_EQ(app_powerManager_getState(), POWER_MANAGER_SHUTDOWN);
+// }
 
 TEST_F(StateMachineTest, check_state_transition_from_init_to_inverter_on)
 {
@@ -28,3 +23,18 @@ TEST_F(StateMachineTest, check_state_transition_from_init_to_inverter_on)
 }
 
 // TODO: add inverter_on to drive state test
+// TEST_F(StateMachineTest, check_drive_state_is_broadcasted_over_can)
+// {
+//     SetInitialState(app_driveState_get());
+//     EXPECT_EQ(VC_DRIVE_STATE, app_canTx_VC_State_get());
+//     EXPECT_EQ(app_powerManager_getState(), POWER_MANAGER_DRIVE);
+// }
+
+// TEST_F(StateMachineTest, check_state_transition_from_init_to_drive)
+// {
+//     SetInitialState(app_initState_get());
+//     app_canRx_BMS_State_update(BMS_INVERTER_ON_STATE);
+//     LetTimePass(1000);
+//     EXPECT_EQ(VC_DRIVE_STATE, app_canTx_VC_State_get());
+//     EXPECT_EQ(app_powerManager_getState(), POWER_MANAGER_DRIVE);
+// }
