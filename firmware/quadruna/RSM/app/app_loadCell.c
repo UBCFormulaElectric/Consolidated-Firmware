7798ae9c23e9a9004a14662e465a17e4ee9c3347
Shayan Ajmal <shayanajmaluni@gmail.com>
2024-03-23 16:52:46 -0700

    implemented load cell on RSM

---
+++ firmware/quadruna/RSM/app/app_loadCell.c
@@ -0,0 +1,18 @@
+#include "ic_loadCell.h"
+#include <stdlib.h>
+#include <assert.h>
+#include "app_canTx.h"
+#include "app_canAlerts.h"
+#include "app_loadCell.h"
+
+void loadcell_broadcast()
+{
+    app_canTx_RSM_LoadCell3_set();
+    app_canTx_RSM_LoadCell4_set();
+
+    const bool loadcell3_OCSC = io_loadCell3_OCSC();
+    app_canAlerts_RSM_Warning_LoadCell3OCSC_set(loadcell3_OCSC);
+
+    const bool loadcell4_OCSC = io_loadCell4_OCSC();
+    app_canAlerts_RSM_Warning_LoadCell3OCSC_set(loadcell4_OCSC);
+}
