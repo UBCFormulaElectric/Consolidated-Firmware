a581e4e13f823a14985b7dabff05f1c94b0b3688
Jugveer-Sandher <jugveersandher@gmail.com>
2024-03-23 11:38:00 -0700

    added comments

--- firmware/thruna/PDM/src/app/app_powerManager.h
+++ firmware/thruna/PDM/src/app/app_powerManager.h
@@ -14,10 +14,25 @@ typedef enum
 
 static PowerManagerState current_power_state;
 
+/**
+ * initalizes the power manager
+ */
 void app_powerManager_init();
 
+/**
+ * sets the efuses for a specific PowerManagerState
+ * @param state the state of the car shutdown/init or drive
+ */
 void app_powerManager_setState(PowerManagerState state);
 
+/**
+ * @return the current PowerManagerState
+ */
 PowerManagerState app_powerManager_getState();
 
+/**
+ * @note runs every 100Hz
+ * @param state the current PowerManagerState's efuses being checked
+ * @returns if the state machine should go to init state
+ */
 bool app_powerManager_check_efuses(PowerManagerState state);
