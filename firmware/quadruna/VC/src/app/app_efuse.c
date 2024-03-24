637f712cd7f00f5627d26889b233ad6020f590d7
liam-ilan <snowboardsheep@gmail.com>
2024-03-23 17:16:12 -0700

    VC_tx updated with efuses.

--- firmware/quadruna/VC/src/app/app_efuse.c
+++ firmware/quadruna/VC/src/app/app_efuse.c
@@ -1,10 +1,12 @@
 #include "app_efuse.h"
+#include <stdbool.h>
 #include "io_efuse.h"
 
-
-void app_efuse_broadcast(void) {
-    for (int efuse = 0; efuse < NUM_EFUSE_CHANNELS; efuse += 1) {
+void app_efuse_broadcast(void)
+{
+    for (int efuse = 0; efuse < NUM_EFUSE_CHANNELS; efuse += 1)
+    {
         float current = io_efuse_getChannelCurrent(efuse);
-        bool enabled = io_efuse_isChannelEnabled(efuse);
+        bool  enabled = io_efuse_isChannelEnabled(efuse);
     }
 }
