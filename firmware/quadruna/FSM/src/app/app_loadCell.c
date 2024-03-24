185d74a89aae4642afd156a21b16686bde467dbe
liam-ilan <snowboardsheep@gmail.com>
2024-03-24 14:28:54 -0700

    Added load cell app layer to quadruna FSM.

---
+++ firmware/quadruna/FSM/src/app/app_loadCell.c
@@ -0,0 +1,12 @@
+#include "app_loadCell.h"
+#include "io_loadCell.h"
+#include "app_canTx.h"
+#include "app_canAlerts.h"
+
+void app_loadCell_broadcast() {
+    app_canTx_FSM_LoadCell1_set(io_loadCell_getMechanicalLoad1());
+    app_canTx_FSM_LoadCell2_set(io_loadCell_getMechanicalLoad2());
+
+    app_canAlerts_FSM_Warning_LoadCell1OCSC_set(io_loadCell_sensor1OCSC());
+    app_canAlerts_FSM_Warning_LoadCell2OCSC_set(io_loadCell_sensor2OCSC());
+}
