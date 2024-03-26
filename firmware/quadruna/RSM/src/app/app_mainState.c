25d08c8cb3de490137b5bc6cb43ba6f07cfe21ca
Shayan Ajmal <shayanajmaluni@gmail.com>
2024-03-25 19:55:06 -0700

    implemented all changes from PR. Transfer functions will be added promptly

--- firmware/quadruna/RSM/src/app/app_mainState.c
+++ firmware/quadruna/RSM/src/app/app_mainState.c
@@ -1,5 +1,6 @@
 #include "app_mainState.h"
 #include <stddef.h>
+#include "io_led.h"
 #include "app_utils.h"
 #include "app_canTx.h"
 #include "app_canRx.h"
@@ -7,6 +8,7 @@
 #include "app_coolant.h"
 #include "app_loadCell.h"
 #include "app_suspension.h"
+#include "app_globals.h"
 
 #include "app_heartbeatMonitor.h"
 
@@ -15,10 +17,14 @@ void mainStateRunOnTick100Hz(void)
     app_coolant_broadcast();
     app_loadcell_broadcast();
     app_suspension_broadcast();
-
     app_heartbeatMonitor_checkIn();
     app_heartbeatMonitor_tick();
     app_heartbeatMonitor_broadcastFaults();
+
+    const bool brake_actuated = app_canRx_FSM_BrakeActuated_get();
+    io_led_enable(globals->config->brake_light, brake_actuated);
+    hw_gpio_writePin(globals->config->acc_fan, true);
+    hw_gpio_writePin(globals->config->rad_fan, true);
 
     bool missing_hb = app_heartbeatMonitor_checkFaults();
 }
