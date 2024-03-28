97b46e7f9b0734bc681ee90cbe952e4f5d1014d3
Jugveer-Sandher <jugveersandher@gmail.com>
2024-03-27 19:47:21 -0700

    fixed some bugs

--- firmware/quadruna/VC/src/io/io_imu.h
+++ firmware/quadruna/VC/src/io/io_imu.h
@@ -7,15 +7,15 @@ bool io_imu_init();
 
 /**
  * @brief gets the linear acceleration in the x direction
-*/
+ */
 double io_imu_get_linear_acceleration_x();
 
 /**
  * @brief gets the linear acceleration in the y direction
-*/
+ */
 double io_imu_get_linear_acceleration_y();
 
 /**
  * @brief gets the linear acceleration in the y direction
-*/
+ */
 double io_imu_get_linear_acceleration_z();
