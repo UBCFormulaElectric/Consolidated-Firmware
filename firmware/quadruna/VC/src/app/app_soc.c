3505690415ccaf26f344560e9a294b1434d818c8
liam-ilan <snowboardsheep@gmail.com>
2024-03-30 16:00:25 -0700

    Added ocv lookup.

--- firmware/quadruna/VC/src/app/app_soc.c
+++ firmware/quadruna/VC/src/app/app_soc.c
@@ -2,6 +2,7 @@
 #include "io_currentSensing.h"
 #include "app_math.h"
 #include "app_canTx.h"
+#include "io_lowVoltageBattery.h"
 
 static SocStats stats;
 
@@ -11,7 +12,8 @@ static SocStats stats;
 void app_soc_init(void) 
 {
     stats.prev_current_A = 0.0f;
-    stats.charge_c = 0; // TODO: init from ocv lookup
+
+    stats.charge_c = app_soc_getChargeFromOcv();
 
     app_timer_init(&stats.soc_timer, SOC_TIMER_DURATION_MS);
 }
@@ -31,9 +33,27 @@ void app_soc_update(void)
 }
 
 float app_soc_getPercent() {
-    return (float) (stats.charge_c / MAX_CHARGE_C * 100.0);
+    return (float) stats.charge_c / MAX_CHARGE_C * 100.0f;
 }
 
 void app_soc_broadcast() {
     app_canTx_VC_Soc_set(app_soc_getPercent());
+}
+
+float app_soc_getChargeFromOcv(void) {
+    float battery_voltage = io_lowVoltageBattery_getBatVoltage();
+    uint8_t lut_index = 0;
+
+    while ((battery_voltage > ocv_soc_lut[lut_index]) && (lut_index < OCV_SOC_LUT_SIZE))
+    {
+        lut_index++;
+    }
+
+    if (lut_index == OCV_SOC_LUT_SIZE)
+    {
+        // Ensures that the index is in the LUT range
+        lut_index--;
+    }
+
+    return MAX_CHARGE_C * battery_voltage;
 }
