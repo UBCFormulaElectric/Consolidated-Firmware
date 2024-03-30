c0acc0bc6fee685f76a18bb22870d1e430b069a7
SoroushRohanizadeh <rohanisoroush@gmail.com>
2024-03-30 16:07:59 -0700

    implemented app_PumpControl

--- firmware/quadruna/VC/src/io/io_i2cPotentiometer.h
+++ firmware/quadruna/VC/src/io/io_i2cPotentiometer.h
@@ -6,16 +6,16 @@
  * Initializes the i2c interface with the potentiometer.
  * @return True if i2c potentiometer is ready, else false.
  */
-bool io_i2c_potentiometerInit();
+bool io_i2cPotentiometer_Init();
 
 /**
  * Sets potentiometer wiper.
  * @param position Wiper position, 0 (min) to 127 (full).
  */
-void io_i2c_setWiper(uint8_t position);
+void io_i2cPotentiometer_setWiper(uint8_t position);
 
 /**
  * Reads the current state of the wiper.
  * @return Current state of the wiper, 0 to 127.
  */
-uint8_t io_i2c_readWiper();
+uint8_t io_i2cPotentiometer_readWiper();
