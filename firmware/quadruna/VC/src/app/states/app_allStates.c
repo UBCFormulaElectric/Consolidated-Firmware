1837251ed53e18744256b77a9b677cbe057209d4
AmirTajaddodi <amir.tajadodi@gmail.com>
2024-04-03 13:42:01 -0700

    Moved fault checks from all states into their own file

--- firmware/quadruna/VC/src/app/states/app_allStates.c
+++ firmware/quadruna/VC/src/app/states/app_allStates.c
@@ -2,6 +2,7 @@
 #include "states/app_allStates.h"
 
 #include "app_sbgEllipse.h"
+#include "app_faultCheck.h"
 #include "io_led.h"
 #include "app_canTx.h"
 #include "app_canRx.h"
@@ -45,25 +46,16 @@ bool app_allStates_runOnTick100Hz(void)
     {
         app_heartbeatMonitor_broadcastFaults();
     }
+    // check if either inverter faulted
+    const bool inverter_fault = app_inverterFaultCheck();
 
-    const bool left_inverter_fault  = app_canRx_INVL_VsmState_get() == INVERTER_VSM_BLINK_FAULT_CODE_STATE;
-    const bool right_inverter_fault = app_canRx_INVR_VsmState_get() == INVERTER_VSM_BLINK_FAULT_CODE_STATE;
-    app_canAlerts_VC_Fault_LeftInverterFault_set(left_inverter_fault);
-    app_canAlerts_VC_Fault_RightInverterFault_set(right_inverter_fault);
-
-    const bool bms_fault  = app_canAlerts_BoardHasFault(BMS_ALERT_BOARD);
-    const bool vc_fault   = app_canAlerts_BoardHasFault(VC_ALERT_BOARD);
-    const bool fsm_fault  = app_canAlerts_BoardHasFault(FSM_ALERT_BOARD);
-    const bool crit_fault = app_canAlerts_BoardHasFault(CRIT_ALERT_BOARD);
-
-    // once crit dim is done add dim_fault
-    const bool any_board_has_fault = bms_fault || vc_fault || fsm_fault || crit_fault;
+    const bool any_board_has_fault = app_boardFaultCheck();
 
     io_sbgEllipse_handleLogs();
     app_sbgEllipse_broadcast();
 
     // Set status to false (which blocks drive) if either inverter is faulted, or another board has set a fault.
-    return !(left_inverter_fault || right_inverter_fault || any_board_has_fault);
+    return !(inverter_fault || any_board_has_fault);
 }
 
 void app_allStates_runOnTick1Hz() {}
