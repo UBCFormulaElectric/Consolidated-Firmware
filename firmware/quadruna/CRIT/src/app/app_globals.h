18b642d8549b4bf88854ae50d35b5c6e97fe5962
Leah Fernandez <leahmf88@students.cs.ubc.ca>
2024-04-07 00:08:41 -0700

    removed Gpio from GlobalConfig

--- firmware/quadruna/CRIT/src/app/app_globals.h
+++ firmware/quadruna/CRIT/src/app/app_globals.h
@@ -6,14 +6,14 @@
 #include "io_shutdownSensor.h"
 #include "io_driveMode.h"
 
-#ifdef TARGET_EMBEDDED
-#include "hw_gpio.h"
-#else
-// dummy version of Gpio when not compiling for the embedded target
-typedef struct {
-    bool unused; // Just a placeholder, doesn't need to be used
-} Gpio;
-#endif
+// #ifdef TARGET_EMBEDDED
+// #include "hw_gpio.h"
+// #else
+// // dummy version of Gpio when not compiling for the embedded target
+// typedef struct {
+//     bool unused; // Just a placeholder, doesn't need to be used
+// } Gpio;
+// #endif
 
 typedef struct
 {
