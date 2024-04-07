2d65758a61592d744d78260b9373e90e34c9d970
liam-ilan <snowboardsheep@gmail.com>
2024-04-06 17:31:17 -0700

    Load cells now set to 0 on ocsc.

--- firmware/quadruna/FSM/src/app/app_loadCell.c
+++ firmware/quadruna/FSM/src/app/app_loadCell.c
@@ -8,6 +8,19 @@ void app_loadCell_broadcast()
     app_canTx_FSM_LoadCell1_set(io_loadCell_getMechanicalLoad1());
     app_canTx_FSM_LoadCell2_set(io_loadCell_getMechanicalLoad2());
 
-    app_canAlerts_FSM_Warning_LoadCell1OCSC_set(io_loadCell_sensor1OCSC());
-    app_canAlerts_FSM_Warning_LoadCell2OCSC_set(io_loadCell_sensor2OCSC());
+    bool load_cell_1_ocsc = io_loadCell_sensor1OCSC();
+    bool load_cell_2_ocsc = io_loadCell_sensor2OCSC();
+
+    if (load_cell_1_ocsc)
+    {
+        app_canTx_FSM_LoadCell1_set(0);
+    }
+
+    if (load_cell_2_ocsc)
+    {
+        app_canTx_FSM_LoadCell2_set(0);
+    }
+
+    app_canAlerts_FSM_Warning_LoadCell1OCSC_set(load_cell_1_ocsc);
+    app_canAlerts_FSM_Warning_LoadCell2OCSC_set(load_cell_2_ocsc);
 }
