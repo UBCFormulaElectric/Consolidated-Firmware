b358bf0777b28f09dd88c5b330b896960723d2b2
liam-ilan <snowboardsheep@gmail.com>
2024-04-08 16:54:00 -0700

    Fix formatting.

--- firmware/quadruna/FSM/src/app/app_loadCell.c
+++ firmware/quadruna/FSM/src/app/app_loadCell.c
@@ -7,7 +7,7 @@ void app_loadCell_broadcast()
 {
     app_canTx_FSM_LoadCell1_set(io_loadCell_getMechanicalLoad1());
     app_canTx_FSM_LoadCell2_set(io_loadCell_getMechanicalLoad2());
-    
+
     app_canAlerts_FSM_Warning_LoadCell1OCSC_set(io_loadCell_sensor1OCSC());
     app_canAlerts_FSM_Warning_LoadCell2OCSC_set(io_loadCell_sensor2OCSC());
 }
