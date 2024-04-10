fe27f722c7a3b432fe1c160f8f01f5e60d257a0d
Leah Fernandez <leahmf88@students.cs.ubc.ca>
2024-04-09 18:25:20 -0700

    shutdown sensor tests

--- firmware/quadruna/CRIT/test/test_shutdownSensor.cpp
+++ firmware/quadruna/CRIT/test/test_shutdownSensor.cpp
@@ -1,16 +1,14 @@
 #include <gtest/gtest.h>
 #include "fake_io_shutdownSensor.hpp"
+#include "test_critBaseStateMachineTest.h"
 
 extern "C"
 {
 #include "io_shutdownSensor.h"
 }
 
-class ShutdownSensorTest : public testing::Test
+class ShutdownSensorTest : public CritBaseStateMachineTest
 {
-  protected:
-    ShutdownSensor sensor;
-
     void SetUp() override { fake_io_shutdownSensor_readPin_reset(); }
 };
 
@@ -18,18 +16,40 @@ TEST_F(ShutdownSensorTest, VerifyReturnValue)
 {
     // Set the fake to return true and verify the return value.
     fake_io_shutdownSensor_readPin_returnsForAnyArgs(true);
-    ASSERT_TRUE(io_shutdownSensor_readPin(&sensor));
+    ASSERT_TRUE(io_shutdownSensor_readPin(&shdn_sen));
 
     // Set the fake to return false and verify the return value.
     fake_io_shutdownSensor_readPin_reset(); // Reset to clear previous state.
     fake_io_shutdownSensor_readPin_returnsForAnyArgs(false);
-    ASSERT_FALSE(io_shutdownSensor_readPin(&sensor));
+    ASSERT_FALSE(io_shutdownSensor_readPin(&shdn_sen));
+}
+
+TEST_F(ShutdownSensorTest, ShutdownSensorActiveWhenImdLatchedFaultIsFalse)
+{
+    app_canRx_BMS_ImdLatchedFault_update(false);
+
+    fake_io_shutdownSensor_readPin_returnsForArgs(&shdn_sen, true);
+    
+    // Verify that the shutdown sensor is indeed active.
+    ASSERT_TRUE(io_shutdownSensor_readPin(&shdn_sen));
+
+}
+
+TEST_F(ShutdownSensorTest, ShutdownSensorActiveWhenImdLatchedFaultIsTrue)
+{
+    app_canRx_BMS_ImdLatchedFault_update(true);
+
+    fake_io_shutdownSensor_readPin_returnsForArgs(&shdn_sen, false);
+    
+    // Verify that the shutdown sensor is indeed not active.
+    ASSERT_FALSE(io_shutdownSensor_readPin(&shdn_sen));
+
 }
 
 TEST_F(ShutdownSensorTest, VerifyCallCount)
 {
     // Call the function without setting a specific return value.
-    io_shutdownSensor_readPin(&sensor);
+    io_shutdownSensor_readPin(&shdn_sen);
 
     // Verify that the call count is as expected.
     ASSERT_EQ(1, fake_io_shutdownSensor_readPin_callCount());
@@ -38,11 +58,11 @@ TEST_F(ShutdownSensorTest, VerifyCallCount)
 TEST_F(ShutdownSensorTest, VerifyCallCountForSpecificArgs)
 {
     // Set the function to return a specific value when called with a specific sensor.
-    fake_io_shutdownSensor_readPin_returnsForArgs(&sensor, true);
+    fake_io_shutdownSensor_readPin_returnsForAnyArgs(true);
 
     // Call the function with the sensor.
-    io_shutdownSensor_readPin(&sensor);
+    io_shutdownSensor_readPin(&shdn_sen);
 
     // Verify that the function was called with the specific arguments.
-    ASSERT_EQ(1, fake_io_shutdownSensor_readPin_callCountForArgs(&sensor));
+    ASSERT_EQ(1, fake_io_shutdownSensor_readPin_callCountForArgs(&shdn_sen));
 }
