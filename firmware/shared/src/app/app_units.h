ded3c85d4833b38008f6a8ba1a173b183516e120
Edwin <20777515+Lucien950@users.noreply.github.com>
2024-04-11 00:56:08 -0700

    Prefer math.h header files for PI constant

--- firmware/shared/src/app/app_units.h
+++ firmware/shared/src/app/app_units.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <math.h>
 
 // Time conversions.
 #define S_TO_MS(time_s) ((uint32_t)((time_s) * 1000U))
@@ -11,13 +12,12 @@
 #define IN_TO_M (0.0254f)
 
 // Radial units/conversions.
-#define PI (3.1415926f)
-#define RAD_TO_DEG(r) (r * 180.0f / PI)
+#define RAD_TO_DEG(r) (r * 180.0f / M_PI)
 #define DEG_TO_RAD(degrees) ((degrees) * (float)M_PI / 180.0f)
 #define RPM_TO_RADS(rpm) ((rpm) * (float)M_PI / 30.0f)
 
 #define WHEEL_DIAMETER_IN (15.0f)
 #define GEAR_RATIO (4.124f)
-#define MOTOR_RPM_TO_KMH(rpm) ((rpm) * (float)WHEEL_DIAMETER_IN * PI * INCH_TO_KM * MIN_TO_HOUR / GEAR_RATIO)
-#define MOTOR_KMH_TO_RPM(kmh) ((int)((kmh) / ((float)WHEEL_DIAMETER_IN * PI * INCH_TO_KM * MIN_TO_HOUR / GEAR_RATIO)))
-#define WHEEL_KMH_TO_RPM(kmh) ((int)((kmh) / ((float)WHEEL_DIAMETER_IN * PI * INCH_TO_KM * MIN_TO_HOUR)))
+#define MOTOR_RPM_TO_KMH(rpm) ((rpm) * (float)WHEEL_DIAMETER_IN * M_PI * INCH_TO_KM * MIN_TO_HOUR / GEAR_RATIO)
+#define MOTOR_KMH_TO_RPM(kmh) ((int)((kmh) / ((float)WHEEL_DIAMETER_IN * M_PI * INCH_TO_KM * MIN_TO_HOUR / GEAR_RATIO)))
+#define WHEEL_KMH_TO_RPM(kmh) ((int)((kmh) / ((float)WHEEL_DIAMETER_IN * M_PI * INCH_TO_KM * MIN_TO_HOUR)))
