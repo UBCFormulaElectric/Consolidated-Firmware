5b2de9992ba2c03e8e8ba9293bd3130c5756daa3
Jugveer-Sandher <jugveersandher@gmail.com>
2024-04-30 12:31:54 -0700

    code review changes

--- firmware/quadruna/VC/src/app/app_retryHandler.c
+++ firmware/quadruna/VC/src/app/app_retryHandler.c
@@ -1,7 +1,6 @@
 #include <stdbool.h>
 
 #include "app_retryHandler.h"
-#include "io_efuse.h"
 #include "app_stateMachine.h"
 #include "states/app_initState.h"
 #include "app_canRx.h"
@@ -9,53 +8,51 @@
 #include "app_retryHandler.h"
 #include "app_powerManager.h"
 
-bool app_retryHandler_start(RetryProtocol protocol, const RetryConfig retry_configs[], RetryData retry_data[])
+bool app_retryHandler_start(EfuseChannel efuse, const RetryConfig retry_configs[], RetryData retry_data[])
 {
     bool go_to_init = false;
 
-    switch (protocol)
+    switch (efuse)
     {
-        case RETRY_PROTOCOL_SHDN:
+        case EFUSE_CHANNEL_SHDN:
             io_efuse_setChannel(EFUSE_CHANNEL_SHDN, false);
 
             retry_data[EFUSE_CHANNEL_SHDN].protocol_state = PROTOCOL_STATE_DEBOUNCE;
             break;
-        case RETRY_PROTOCOL_LV:
-            for (int efuse = 0; efuse < NUM_EFUSE_CHANNELS; efuse++)
+        case EFUSE_CHANNEL_LV:
+            for (int efuse_channel = 0; efuse_channel < NUM_EFUSE_CHANNELS; efuse_channel++)
             {
-                io_efuse_setChannel(efuse, false);
-                retry_data[efuse].protocol_state = PROTOCOL_STATE_DEPENDENCY_WAITING;
+                io_efuse_setChannel(efuse_channel, false);
+                retry_data[efuse_channel].protocol_state = PROTOCOL_STATE_DEPENDENCY_WAITING;
             }
 
             go_to_init = true;
 
             retry_data[EFUSE_CHANNEL_LV].protocol_state = PROTOCOL_STATE_DEBOUNCE;
             break;
-        case RETRY_PROTOCOL_PUMP:
+        case EFUSE_CHANNEL_PUMP:
             io_efuse_setChannel(EFUSE_CHANNEL_PUMP, false);
             retry_data[EFUSE_CHANNEL_PUMP].protocol_state = PROTOCOL_STATE_DEBOUNCE;
             break;
-        case RETRY_PROTOCOL_INV_R:
+        case EFUSE_CHANNEL_INV_R:
+        case EFUSE_CHANNEL_INV_L:
             io_efuse_setChannel(EFUSE_CHANNEL_INV_R, false);
             io_efuse_setChannel(EFUSE_CHANNEL_INV_L, false);
             io_efuse_setChannel(EFUSE_CHANNEL_SHDN, false);
 
             go_to_init = true;
 
-            retry_data[EFUSE_CHANNEL_INV_R].protocol_state = PROTOCOL_STATE_DEBOUNCE;
-            retry_data[EFUSE_CHANNEL_INV_L].protocol_state = PROTOCOL_STATE_DEPENDENCY_WAITING;
-            retry_data[EFUSE_CHANNEL_SHDN].protocol_state  = PROTOCOL_STATE_DEPENDENCY_WAITING;
-            break;
-        case RETRY_PROTOCOL_INV_L:
-            io_efuse_setChannel(EFUSE_CHANNEL_INV_R, false);
-            io_efuse_setChannel(EFUSE_CHANNEL_INV_L, false);
-            io_efuse_setChannel(EFUSE_CHANNEL_SHDN, false);
-
-            go_to_init = true;
-
-            retry_data[EFUSE_CHANNEL_INV_L].protocol_state = PROTOCOL_STATE_DEBOUNCE;
-            retry_data[EFUSE_CHANNEL_INV_R].protocol_state = PROTOCOL_STATE_DEPENDENCY_WAITING;
-            retry_data[EFUSE_CHANNEL_SHDN].protocol_state  = PROTOCOL_STATE_DEPENDENCY_WAITING;
+            if (efuse == EFUSE_CHANNEL_INV_L)
+            {
+                retry_data[EFUSE_CHANNEL_INV_L].protocol_state = PROTOCOL_STATE_DEBOUNCE;
+                retry_data[EFUSE_CHANNEL_INV_R].protocol_state = PROTOCOL_STATE_DEPENDENCY_WAITING;
+            }
+            else
+            {
+                retry_data[EFUSE_CHANNEL_INV_R].protocol_state = PROTOCOL_STATE_DEBOUNCE;
+                retry_data[EFUSE_CHANNEL_INV_L].protocol_state = PROTOCOL_STATE_DEPENDENCY_WAITING;
+            }
+            retry_data[EFUSE_CHANNEL_SHDN].protocol_state = PROTOCOL_STATE_DEPENDENCY_WAITING;
             break;
         default:
             break;
@@ -63,25 +60,25 @@ bool app_retryHandler_start(RetryProtocol protocol, const RetryConfig retry_conf
     return go_to_init;
 }
 
-void app_retryHandler_success(RetryProtocol protocol, const RetryConfig retry_configs[], RetryData retry_data[])
+void app_retryHandler_success(EfuseChannel efuse, const RetryConfig retry_configs[], RetryData retry_data[])
 {
-    switch (protocol)
+    switch (efuse)
     {
-        case RETRY_PROTOCOL_SHDN:
+        case EFUSE_CHANNEL_SHDN:
             retry_data[EFUSE_CHANNEL_SHDN].protocol_state = PROTOCOL_STATE_OFF;
             break;
-        case RETRY_PROTOCOL_LV:
-            for (int efuse = 0; efuse < NUM_EFUSE_CHANNELS; efuse++)
+        case EFUSE_CHANNEL_LV:
+            for (int efuse_channel = 0; efuse_channel < NUM_EFUSE_CHANNELS; efuse_channel++)
             {
-                io_efuse_setChannel(efuse, true);
-                retry_data[efuse].protocol_state = PROTOCOL_STATE_OFF;
+                io_efuse_setChannel(efuse_channel, true);
+                retry_data[efuse_channel].protocol_state = PROTOCOL_STATE_OFF;
             }
             break;
-        case RETRY_PROTOCOL_PUMP:
+        case EFUSE_CHANNEL_PUMP:
             retry_data[EFUSE_CHANNEL_PUMP].protocol_state = PROTOCOL_STATE_OFF;
             break;
-        case RETRY_PROTOCOL_INV_R:
-        case RETRY_PROTOCOL_INV_L:
+        case EFUSE_CHANNEL_INV_R:
+        case EFUSE_CHANNEL_INV_L:
             io_efuse_setChannel(EFUSE_CHANNEL_INV_L, true);
             io_efuse_setChannel(EFUSE_CHANNEL_INV_R, true);
             io_efuse_setChannel(EFUSE_CHANNEL_SHDN, true);
--- firmware/quadruna/VC/src/app/app_retryHandler.h
+++ firmware/quadruna/VC/src/app/app_retryHandler.h
@@ -1,16 +1,6 @@
 #include <stdbool.h>
+#include "io_efuse.h"
 #pragma once
-
-typedef enum
-{
-    RETRY_PROTOCOL_SHDN,
-    RETRY_PROTOCOL_LV,
-    RETRY_PROTOCOL_PUMP,
-    RETRY_PROTOCOL_INV_R,
-    RETRY_PROTOCOL_INV_L,
-    RETRY_PROTOCOL_NONE,
-    NUM_RETRY_PROTOCOL
-} RetryProtocol;
 
 typedef enum
 {
@@ -18,13 +8,13 @@ typedef enum
     PROTOCOL_STATE_CALC_AVG,           // Efuse is calculating the average current
     PROTOCOL_STATE_CALC_DONE,          // Efuse has finished calculating the average and needs further action
     PROTOCOL_STATE_DEPENDENCY_WAITING, // Efuse is waiting for another efuse to finish its retry protocol
-    PROTOCOL_STATE_DEBOUNCE            // Efuse is waiting for debounce time before turning on again
+    PROTOCOL_STATE_DEBOUNCE,           // Efuse is waiting for debounce time before turning on again
+    PROTOCOL_STATE_FAULTED             // Efuse went through the protocol and is still faulting
 } ProtocolState;
 
 typedef struct
 {
-    bool          efuse_state;          // State of the efuse (on/off)
-    RetryProtocol retry_protocol;       // Protocol for efuse retry handling
+    bool          is_efuse_on;          // State of the efuse (on/off)
     int           retry_attempts_limit; // Limit for retry protocol attempts
     float         min_needed_current;   // Minimum current required for the efuse
 } RetryConfig;
@@ -39,18 +29,18 @@ typedef struct
 /**
  * @brief Starts the retry protocol for efuses.
  *
- * @param protocol Which efuse protocol to run.
+ * @param efuse Which efuse protocol to run.
  * @param retry_configs The configurations for each efuse (efuse_state).
  * @param retry_data The data for each efuse (ProtocolState).
- * @return If VC should go to the init state.
+ * @return If car should reset (VC go to init state).
  */
-bool app_retryHandler_start(RetryProtocol protocol, const RetryConfig retry_configs[], RetryData retry_data[]);
+bool app_retryHandler_start(EfuseChannel efuse, const RetryConfig retry_configs[], RetryData retry_data[]);
 
 /**
  * @brief Recovers from the protocol, sets the efuses that app_retry_handler_start turned off to on.
  *
- * @param protocol Which efuse protocol to run.
+ * @param efuse Which efuse protocol to run.
  * @param retry_configs The configurations for each efuse (efuse_state).
  * @param retry_data The data for each efuse (ProtocolState).
  */
-void app_retryHandler_success(RetryProtocol protocol, const RetryConfig retry_configs[], RetryData retry_data[]);
+void app_retryHandler_success(EfuseChannel efuse, const RetryConfig retry_configs[], RetryData retry_data[]);
