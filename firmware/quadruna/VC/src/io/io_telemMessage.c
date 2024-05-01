c9a119aa360aca6bc2857239617d84a3a8f36fdb
Ashli Forbes <44144720+forbesashli@users.noreply.github.com>
2024-05-01 14:39:20 -0700

    Minimum driving code with static message (#1240)
    ### Changelist 
    Changed the vc ioc and added some extra files to utilize the telem proto
    messages. Gives others platforms to do integration tests for their
    projects
    
    ### Testing Done
    -  testing on hardware and works
    
    ### Resolved Tickets
    none
    
    ---------
    
    Co-authored-by: Edwin <20777515+Lucien950@users.noreply.github.com>

---
+++ firmware/quadruna/VC/src/io/io_telemMessage.c
@@ -0,0 +1,54 @@
+#include "io_telemMessage.h"
+#include "telem.pb.h"
+#include "pb_encode.h"
+#include "pb_decode.h"
+#include "cmsis_os.h"
+#include "queue.h"
+
+// create the truth table for now to decide which amount of things to use
+// create or grab the constants for the different modem and pins and such
+
+// Private Globals
+static bool   modem_900_choice;
+static Modem *modem;
+
+#define QUEUE_SIZE 12
+#define QUEUE_BYTES 4 * QUEUE_SIZE // this is all temp
+
+static bool    proto_status;
+static uint8_t proto_msg_length;
+static uint8_t proto_buffer[QUEUE_SIZE]; // TODO: verify that this is the needed size (most likely can be smaller)
+TelemMessage   message = TelemMessage_init_zero;
+
+void io_telemMessage_init(Modem *m)
+{
+    modem_900_choice = true; // if false, then using the 2.4GHz,
+    modem            = m;
+}
+
+bool io_telemMessage_broadcast()
+{
+    // send it over the correct UART functionality
+    pb_ostream_t stream = pb_ostream_from_buffer(proto_buffer, sizeof(proto_buffer));
+
+    // filling in fields
+    message.can_id     = 53;
+    message.data       = 23;
+    message.time_stamp = 9;
+
+    // encoding message
+    proto_status     = pb_encode(&stream, TelemMessage_fields, &message);
+    proto_msg_length = (uint8_t)stream.bytes_written;
+
+    if (modem_900_choice == true)
+    {
+        hw_uart_transmitPoll(modem->modem900M, &proto_msg_length, 1, 1);
+        hw_uart_transmitPoll(modem->modem900M, proto_buffer, (uint8_t)sizeof(proto_buffer), 1);
+    }
+    else
+    {
+        hw_uart_transmitPoll(modem->modem2_4G, &proto_msg_length, 1, 1);
+        hw_uart_transmitPoll(modem->modem2_4G, proto_buffer, (uint8_t)sizeof(proto_buffer), 1);
+    }
+    return true;
+}
