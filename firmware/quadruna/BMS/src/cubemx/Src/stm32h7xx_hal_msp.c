b28e6b5c89a0aaf875424b4a772210f517149a91
will-chaba <willchabaubc@gmail.com>
2024-05-02 23:24:58 -0700

    IMD duty cycle working, maybe not frequency

--- firmware/quadruna/BMS/src/cubemx/Src/stm32h7xx_hal_msp.c
+++ firmware/quadruna/BMS/src/cubemx/Src/stm32h7xx_hal_msp.c
@@ -506,25 +506,16 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim_base)
         /* Peripheral clock enable */
         __HAL_RCC_TIM1_CLK_ENABLE();
 
-        __HAL_RCC_GPIOE_CLK_ENABLE();
         __HAL_RCC_GPIOA_CLK_ENABLE();
         /**TIM1 GPIO Configuration
-        PE11     ------> TIM1_CH2
         PA8     ------> TIM1_CH1
         */
-        GPIO_InitStruct.Pin       = GPIO_PIN_11;
+        GPIO_InitStruct.Pin       = IMD_M_HS_3V3_Pin;
         GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
         GPIO_InitStruct.Pull      = GPIO_NOPULL;
         GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
         GPIO_InitStruct.Alternate = GPIO_AF1_TIM1;
-        HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
-
-        GPIO_InitStruct.Pin       = GPIO_PIN_8;
-        GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull      = GPIO_NOPULL;
-        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
-        GPIO_InitStruct.Alternate = GPIO_AF1_TIM1;
-        HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+        HAL_GPIO_Init(IMD_M_HS_3V3_GPIO_Port, &GPIO_InitStruct);
 
         /* TIM1 interrupt Init */
         HAL_NVIC_SetPriority(TIM1_CC_IRQn, 5, 0);
@@ -600,12 +591,9 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef *htim_base)
         __HAL_RCC_TIM1_CLK_DISABLE();
 
         /**TIM1 GPIO Configuration
-        PE11     ------> TIM1_CH2
         PA8     ------> TIM1_CH1
         */
-        HAL_GPIO_DeInit(GPIOE, GPIO_PIN_11);
-
-        HAL_GPIO_DeInit(GPIOA, GPIO_PIN_8);
+        HAL_GPIO_DeInit(IMD_M_HS_3V3_GPIO_Port, IMD_M_HS_3V3_Pin);
 
         /* TIM1 interrupt DeInit */
         HAL_NVIC_DisableIRQ(TIM1_CC_IRQn);
