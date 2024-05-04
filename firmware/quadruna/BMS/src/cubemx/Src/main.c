7f192431eccc615dc74350cc973c09ded16fc093
AmirTajaddodi <amir.tajadodi@gmail.com>
2024-05-04 12:24:16 -0700

    temp adc settings for bms

--- firmware/quadruna/BMS/src/cubemx/Src/main.c
+++ firmware/quadruna/BMS/src/cubemx/Src/main.c
@@ -389,7 +389,7 @@ static void MX_ADC1_Init(void)
      */
     sConfig.Channel      = ADC_CHANNEL_10;
     sConfig.Rank         = ADC_REGULAR_RANK_4;
-    sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
+    sConfig.SamplingTime = ADC_SAMPLETIME_16CYCLES_5;
     if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
     {
         Error_Handler();
