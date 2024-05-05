8d31e4cac803187a46fb8e4ee4d8fad6e4210241
AmirTajaddodi <amir.tajadodi@gmail.com>
2024-05-04 22:59:14 -0700

    hella changes

--- firmware/quadruna/VC/src/io/io_vcShdn.c
+++ firmware/quadruna/VC/src/io/io_vcShdn.c
@@ -14,7 +14,7 @@ bool io_vcShdn_TsmsFault_get(void)
 
 bool io_vcShdn_PcmFault_get(void)
 {
-    return hw_gpio_readPin(&config->pcm_gpio);
+    return (!hw_gpio_readPin(&config->pcm_gpio));
 }
 
 bool io_vcShdn_LEStopFault_get(void)
