9583fc9d5e4bb8a0c00b9d80665e359ac307d52d
Ashli <forbesashli@gmail.com>
2024-05-04 19:50:14 -0700

    general fix

--- software/tracksight/frontend/src/app/page.tsx
+++ software/tracksight/frontend/src/app/page.tsx
@@ -9,7 +9,7 @@ import Dashboard from './components/dashboard';
 import Visualize from './components/visualize';
 
 //const FLASK_URL = "http://evanyl.pythonanywhere.com";
-const FLASK_URL = "http://206.12.160.196:5000";
+const FLASK_URL = process.env.NODE_ENV == "development" ? "http://127.0.0.1:5000" : "http://206.12.68.61:5000";
 const Home = () => {
     const [componentToDisplay, setComponentToDisplay] = useState<string>('visualize'); 
     const [socketInstance, setSocketInstance] = useState<Socket | null>(null); 
