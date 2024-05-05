42c6cf64c11ab712ca62bebcc525c469519efa2e
Ashli Forbes <44144720+forbesashli@users.noreply.github.com>
2024-05-04 18:55:30 -0700

    Telem Firmware Implemented (#1246)
    ### Changelist 
    Telem proto, .h and .c files, minor changes to io_can.h in order to have
    it such that io can use can and pass tests (thanks Holly)
    
    ### Testing Done
    built and flashed onto spare board, works fine
    
    ### Resolved Tickets
    n/a

--- firmware/quadruna/VC/src/io/io_telemMessage.c
+++ firmware/quadruna/VC/src/io/io_telemMessage.c
@@ -2,8 +2,7 @@
 #include "telem.pb.h"
 #include "pb_encode.h"
 #include "pb_decode.h"
-#include "cmsis_os.h"
-#include "queue.h"
+#include "io_time.h"
 
 // create the truth table for now to decide which amount of things to use
 // create or grab the constants for the different modem and pins and such
@@ -12,13 +11,17 @@
 static bool   modem_900_choice;
 static Modem *modem;
 
+#define CAN_DATA_LENGTH 8
+#define UART_LENGTH 1
 #define QUEUE_SIZE 12
 #define QUEUE_BYTES 4 * QUEUE_SIZE // this is all temp
+static bool   modem_900_choice;
+static Modem *modem;
 
 static bool    proto_status;
 static uint8_t proto_msg_length;
 static uint8_t proto_buffer[QUEUE_SIZE]; // TODO: verify that this is the needed size (most likely can be smaller)
-TelemMessage   message = TelemMessage_init_zero;
+TelemMessage   t_message = TelemMessage_init_zero;
 
 void io_telemMessage_init(Modem *m)
 {
@@ -26,29 +29,32 @@ void io_telemMessage_init(Modem *m)
     modem            = m;
 }
 
-bool io_telemMessage_broadcast()
+bool io_telemMessage_broadcast(CanMsg *rx_msg)
 {
     // send it over the correct UART functionality
     pb_ostream_t stream = pb_ostream_from_buffer(proto_buffer, sizeof(proto_buffer));
 
     // filling in fields
-    message.can_id     = 53;
-    message.data       = 23;
-    message.time_stamp = 9;
+    t_message.can_id = (int32_t)(rx_msg->std_id);
+    for (uint8_t i = 0; i < CAN_DATA_LENGTH; i++)
+    {
+        t_message.message[i] = rx_msg->data[i];
+    }
+    t_message.time_stamp = (int32_t)io_time_getCurrentMs();
 
     // encoding message
-    proto_status     = pb_encode(&stream, TelemMessage_fields, &message);
+    proto_status     = pb_encode(&stream, TelemMessage_fields, &t_message);
     proto_msg_length = (uint8_t)stream.bytes_written;
 
     if (modem_900_choice == true)
     {
-        hw_uart_transmitPoll(modem->modem900M, &proto_msg_length, 1, 1);
-        hw_uart_transmitPoll(modem->modem900M, proto_buffer, (uint8_t)sizeof(proto_buffer), 1);
+        hw_uart_transmitPoll(modem->modem900M, &proto_msg_length, UART_LENGTH, UART_LENGTH);
+        hw_uart_transmitPoll(modem->modem900M, proto_buffer, (uint8_t)sizeof(proto_buffer), UART_LENGTH);
     }
     else
     {
-        hw_uart_transmitPoll(modem->modem2_4G, &proto_msg_length, 1, 1);
-        hw_uart_transmitPoll(modem->modem2_4G, proto_buffer, (uint8_t)sizeof(proto_buffer), 1);
+        hw_uart_transmitPoll(modem->modem2_4G, &proto_msg_length, UART_LENGTH, UART_LENGTH);
+        hw_uart_transmitPoll(modem->modem2_4G, proto_buffer, (uint8_t)sizeof(proto_buffer), UART_LENGTH);
     }
     return true;
 }
