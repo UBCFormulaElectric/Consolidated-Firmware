0c404d1b9b970dda7b73f98f05ad43da9258c8e6
AmirTajaddodi <amir.tajadodi@gmail.com>
2024-05-05 20:36:33 -0700

    efuses errors

--- firmware/quadruna/VC/src/app/app_powerManager.c
+++ firmware/quadruna/VC/src/app/app_powerManager.c
@@ -72,7 +72,7 @@ void app_powerManager_setState(PowerManagerState state)
 
     for (int efuse = 0; efuse < NUM_EFUSE_CHANNELS; efuse++)
     {
-        io_efuse_se tChannel((EfuseChannel)efuse, power_states_config[state].efuses[efuse]);
+        io_efuse_setChannel((EfuseChannel)efuse, power_states_config[state].efuses[efuse]);
     }
     io_pcm_set(power_states_config[state].pcm);
 }
