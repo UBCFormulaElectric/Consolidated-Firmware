73e06096e07d187b6701831ef51add992a949a2f
Edwin <20777515+Lucien950@users.noreply.github.com>
2024-05-16 13:49:44 -0700

    Tracksight Refactor (#1257)
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    ### Resolved Tickets
    <!-- Link any tickets that this PR resolves. -->
    
    ---------
    
    Co-authored-by: Ashli <forbesashli@gmail.com>
    Co-authored-by: Ashli Forbes <44144720+forbesashli@users.noreply.github.com>

--- software/hardware_tests/data_rate_test_rx.py
+++ software/hardware_tests/data_rate_test_rx.py
@@ -1,20 +1,22 @@
 import serial
 
+
 def receive_data():
-    ser = serial.Serial('/dev/ttyUSB0', baudrate=57600, stopbits=1, timeout=100) 
+    ser = serial.Serial("/dev/ttyUSB0", baudrate=57600, stopbits=1, timeout=100)
 
     tick_count = 0
 
     try:
         while True:
-            line = ser.readline(12).decode().strip() 
+            line = ser.readline(12).decode().strip()
             print(line)
-            tick_count+=1
+            tick_count += 1
 
     except KeyboardInterrupt:
         # Close the serial port on Ctrl+C
         ser.close()
         print(tick_count)
+
 
 if __name__ == "__main__":
 
