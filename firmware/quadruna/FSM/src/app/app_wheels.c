1b7d8851d4a73729dbef682a970bdc2f45e27130
AmirTajaddodi <amir.tajadodi@gmail.com>
2024-05-18 21:17:11 -0700

    more quick fixes

--- firmware/quadruna/FSM/src/app/app_wheels.c
+++ firmware/quadruna/FSM/src/app/app_wheels.c
@@ -19,11 +19,11 @@ void app_wheels_broadcast(void)
     RangeCheckStatusMetaData left_wheel_status =
         app_rangeCheck_getValue(&left_wheel_speed_in_range_check, left_wheel_speed);
     app_canTx_FSM_LeftWheelSpeed_set(left_wheel_status.value);
-    app_canAlerts_FSM_Warning_LeftWheelSpeedOCSC_set(left_wheel_status.status != VALUE_IN_RANGE);
+    app_canAlerts_FSM_Warning_LeftWheelSpeedOutOfRange_set(left_wheel_status.status != VALUE_IN_RANGE);
 
     float                    right_wheel_speed = io_wheels_getRightSpeedKph();
     RangeCheckStatusMetaData right_wheel_status =
         app_rangeCheck_getValue(&right_wheel_speed_in_range_check, right_wheel_speed);
     app_canTx_FSM_RightWheelSpeed_set(right_wheel_status.value);
-    app_canAlerts_FSM_Warning_RightWheelSpeedOCSC_set(right_wheel_status.status != VALUE_IN_RANGE);
+    app_canAlerts_FSM_Warning_RightWheelSpeedOutOfRange_set(right_wheel_status.status != VALUE_IN_RANGE);
 }
