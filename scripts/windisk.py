d2574b32d4bf17dbf71cbef4394a8063cf51bd97
Edwin <20777515+Lucien950@users.noreply.github.com>
2024-05-18 18:57:58 -0700

    ?

--- scripts/windisk.py
+++ scripts/windisk.py
@@ -10,20 +10,25 @@ from littlefs import LittleFS, UserContextWinDisk
 #         print("worng number")
 #     number = new_number
 
-disk_path = r"\\.\D: " # assume F drive is your storage device for littlefs
+disk_path = r"\\.\D: "  # assume F drive is your storage device for littlefs
 
 bc_16g = 30228480
 bc_2g = bc_16g // 8
-fs = LittleFS(block_size=512, block_count=1000000, mount=False ,context=UserContextWinDisk(disk_path))
+fs = LittleFS(
+    block_size=512,
+    block_count=1000000,
+    mount=False,
+    context=UserContextWinDisk(disk_path),
+)
 
 
 # fs.format()
 fs.mount()
 
-files = fs.listdir('/')
+files = fs.listdir("/")
 for file in files:
-    with fs.open(file, 'rb') as fh:
-        with open(file, 'wb+') as f:
+    with fs.open(file, "rb") as fh:
+        with open(file, "wb+") as f:
             data = fh.read()
             print(data)
             f.write(data)
