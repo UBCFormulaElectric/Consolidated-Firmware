429b123a844cf47fe0c77a6ad2e8915b0d12bf07
Jugveer-Sandher <jugveersandher@gmail.com>
2024-05-20 15:10:29 -0700

    updated cube and all states

--- firmware/quadruna/VC/src/app/states/app_allStates.c
+++ firmware/quadruna/VC/src/app/states/app_allStates.c
@@ -11,6 +11,7 @@
 #include "app_shdnLast.h"
 // io
 #include "io_sbgEllipse.h"
+#include "io_imu.h"
 
 #define IGNORE_HEARTBEAT_CYCLES 3U
 
@@ -23,6 +24,21 @@ void app_allStates_runOnTick100Hz(void)
     app_currentSensing_broadcast();
     app_efuse_broadcast();
     app_shdnLast_broadcast();
+
+    float lin_accel_x = 0;
+    float lin_accel_y = 0;
+    float lin_accel_z = 0;
+
+    bool has_lin_accel_x = io_imu_getLinearAccelerationX(&lin_accel_x);
+    bool has_lin_accel_y = io_imu_getLinearAccelerationY(&lin_accel_y);
+    bool has_lin_accel_z = io_imu_getLinearAccelerationZ(&lin_accel_z);
+
+    if (has_lin_accel_x && has_lin_accel_y && has_lin_accel_z)
+    {
+        app_canTx_VC_ImuAccelerationX_set(lin_accel_x);
+        app_canTx_VC_ImuAccelerationY_set(lin_accel_y);
+        app_canTx_VC_ImuAccelerationZ_set(lin_accel_z);
+    }
 
     app_heartbeatMonitor_checkIn();
 
