afb18a06a23cecd6f0fcac344b6a58c9de0c6c35
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2024-05-25 22:51:29 -0700

    as will requested

--- firmware/quadruna/BMS/src/app/app_accumulator.c
+++ firmware/quadruna/BMS/src/app/app_accumulator.c
@@ -4,7 +4,6 @@
 #include "app_canAlerts.h"
 #include "app_math.h"
 #include "app_timer.h"
-#include "io_charger.h"
 #include "ltc6813/io_ltc6813Shared.h"
 #include "ltc6813/io_ltc6813CellVoltages.h"
 #include "ltc6813/io_ltc6813CellTemps.h"
@@ -474,7 +473,7 @@ bool app_accumulator_checkFaults(void)
     float min_allowable_cell_temp = MIN_CELL_DISCHARGE_TEMP_DEGC;
 
     // if we are charging, max cell temp is 45C not 60C
-    if (io_charger_isConnected())
+    if (app_canRx_BRUSA_IsConnected_get())
     {
         max_allowable_cell_temp = MAX_CELL_CHARGE_TEMP_DEGC;
         min_allowable_cell_temp = MIN_CELL_CHARGE_TEMP_DEGC;
