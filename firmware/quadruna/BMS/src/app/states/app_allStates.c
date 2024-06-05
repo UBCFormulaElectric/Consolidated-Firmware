fe3221c743f44c42d91ef4a59fdbe5166f060ac1
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2024-06-05 14:45:07 -0700

    yep

--- firmware/quadruna/BMS/src/app/states/app_allStates.c
+++ firmware/quadruna/BMS/src/app/states/app_allStates.c
@@ -76,13 +76,15 @@ bool app_allStates_runOnTick100Hz(void)
                 iso_spi_state_counter = 0;
             }
 
-            const uint32_t cycles_to_measure =
-                balancing_enabled ? NUM_CYCLES_TO_MEASURE_BALANCING : NUM_CYCLES_TO_MEASURE_NOMINAL;
-            if (iso_spi_state_counter >= cycles_to_measure)
-            {
-                iso_spi_state_counter = 0;
-                iso_spi_task_state    = RUN_OPEN_WIRE_CHECK;
-            }
+            // Open wire check is mysteriously causing communication errors with the LTCs, so disabling it for now.
+            // TODO: Find out why!
+            // const uint32_t cycles_to_measure =
+            //     balancing_enabled ? NUM_CYCLES_TO_MEASURE_BALANCING : NUM_CYCLES_TO_MEASURE_NOMINAL;
+            // if (iso_spi_state_counter >= cycles_to_measure)
+            // {
+            //     iso_spi_state_counter = 0;
+            //     iso_spi_task_state    = RUN_OPEN_WIRE_CHECK;
+            // }
 
             iso_spi_state_counter++;
             break;
