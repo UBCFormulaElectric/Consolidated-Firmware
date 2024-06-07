cea553cd6baa43d5816cb8f993125d64838297e2
Lara Kawasme <113661683+larakawasme@users.noreply.github.com>
2024-06-07 14:51:54 -0700

    Send live data to local influx and send to front end (#1299)
    ### Changelist 
    Sending live data to local influx
    ### Testing Done
    it works
    ### Resolved Tickets
    OH YEA YEA OH YEA OH YEA YEA

--- software/tracksight/backend/app/signal_util.py
+++ software/tracksight/backend/app/signal_util.py
@@ -12,6 +12,7 @@ import serial
 import logging
 import time
 import pandas as pd
+from queue import Queue
 from tzlocal import get_localzone
 
 from generated import telem_pb2
@@ -23,33 +24,39 @@ sys.path.insert(
     0,
     os.path.abspath(
         os.path.join(
-            os.path.dirname(__file__), "../../../../../scripts/code_generation/"
+            os.path.dirname(__file__), "../../../../scripts/code_generation/"
         )
     ),
 )
 bus_path = os.path.abspath(
-    os.path.join(os.path.dirname(__file__), "../../../../../can_bus/quadruna")
+    os.path.join(os.path.dirname(__file__), "../../../../can_bus/quadruna")
 )
-# from jsoncan.src.json_parsing.json_can_parsing import JsonCanParser
-# from jsoncan.src.can_database import CanDatabase
+print("sys.path[0]:", sys.path[0])
+print(bus_path)
 
-VALID_PACKET_SIZES = {255, 165, 253}
+from jsoncan.src.json_parsing.json_can_parsing import JsonCanParser
+from jsoncan.src.can_database import CanDatabase
+
+INVALID_PACKET_SIZES = {255, 165, 253}
 
 
 class SignalUtil:
     available_signals = {}
     client_signals = {}
     is_setup = False
+    signal_df = pd.DataFrame(columns=['time', 'signal', 'value', 'unit'])
+    max_df_size = 1  # Define the size threshold
+
 
     @classmethod
     def setup(cls, port: str, app):
-        # cls.ser = serial.Serial(port=port, baudrate=57600, timeout=1)
-        # cls.ser.reset_input_buffer()
-        # cls.ser.reset_output_buffer()
+        cls.ser = serial.Serial(port=port, baudrate=57600, timeout=1)
+        cls.ser.reset_input_buffer()
+        cls.ser.reset_output_buffer()
         cls.is_setup = True
         cls.app = app
 
-        # can_db = JsonCanParser(bus_path).make_database()
+        cls.can_db = JsonCanParser(bus_path).make_database()
 
     @classmethod
     def read_messages(cls):
@@ -64,71 +71,87 @@ class SignalUtil:
             while True:
                 # TODO: Lara: Upload actual signals instead!
 
-                # packet_size = int.from_bytes(cls.ser.read(1), byteorder="little")
-                # logger.info(f"Received data: {packet_size}")
-                # if packet_size in VALID_PACKET_SIZES:
-                #     continue
+                packet_size = int.from_bytes(cls.ser.read(1), byteorder="little")
+                logger.info(f"Received data: {packet_size}")
+                if packet_size in INVALID_PACKET_SIZES:
+                    continue
 
-                # if (
-                #     last_bit == 0 and packet_size != 0
-                # ):  # the size will be different due to 0 not often being include
+                if (
+                    last_bit == 0 and packet_size != 0
+                ):  # the size will be different due to 0 not often being include
 
-                #     # Read in UART message and parse the protobuf
-                #     bytes_read = cls.ser.read(packet_size)
-                #     message_received = telem_pb2.TelemMessage()
-                #     message_received.ParseFromString(bytes_read)
+                    # Read in UART message and parse the protobuf
+                    bytes_read = cls.ser.read(packet_size)
+                    message_received = telem_pb2.TelemMessage()
+                    message_received.ParseFromString(bytes_read)
 
-                #     # Make data array out of ints
-                #     data_array = cls.make_bytes(message_received)
+                    # Make data array out of ints
+                    data_array = cls.make_bytes(message_received)
 
-                #     # Unpack the data and add the id and meta data
-                #     signal_list = cls.can_db.unpack(message_received.can_id, data_array)
+                    # Unpack the data and add the id and meta data
+                    signal_list = cls.can_db.unpack(message_received.can_id, data_array)
 
-                #     for single_signal in signal_list:
+                    for single_signal in signal_list:
 
-                #         # Add the time stamp
-                #         single_signal["timestamp"] = message_received.time_stamp
-                #         signal_name = single_signal["name"]
+                        # Add the time stamp and get name
+                        single_signal["timestamp"] = message_received.time_stamp
+                        signal_name = single_signal["name"] 
 
-                #         # Update the list of availble signals and add it to client signals
-                #         if signal_name not in cls.available_signals:
-                #             cls.available_signals[signal_name] = True
-                #             cls.client_signals[signal_name] = []
+                        # Update the list of availble signals and add it to client signals
+                        if signal_name not in cls.available_signals:
+                            cls.available_signals[signal_name] = True
+                            cls.client_signals[signal_name] = []
 
-                #         # Emit the message
-                #         flask_socketio.emit("signal_response", single_signal)
+                      
+                        # Ensure the value is the correct type (convert to float)
+                        value = int(single_signal["value"])
 
-                # else:
-                #     last_bit = packet_size
 
-                if "Signal" not in cls.available_signals:
-                    cls.available_signals["Signal"] = True
-                    cls.client_signals["Signal"] = []
+                        #print(single_signal) #Un comment to verify getting signals to terminal
 
-                # signal = {
-                #     # "timestamp": "2024",
-                #     "name": "Signal",
-                #     "value": 3,
-                #     "unit": "W",
-                # }
-                # with cls.app.app_context():
+                        # Create a DataFrame for the new signal
+                        new_signal_df = pd.DataFrame([{
+                            "time": pd.Timestamp.now(tz=get_localzone()),#TODO: Make time more accurate in mili since start
+                            "value": value,
+                            "unit": single_signal["unit"],
+                            "signal": single_signal["name"]
+                        }])
 
-                signals = {"time": [], "signal": [], "value": [], "unit": []}
-                for _ in range(10):
-                    timestamp = pd.Timestamp.now(tz=get_localzone())
-                    signals["time"].append(timestamp)
-                    signals["signal"].append("Test")
-                    signals["value"].append(2)
-                    signals["unit"].append("kW")
 
-                InfluxHandler.write(
-                    pd.DataFrame(data=signals),
-                    measurement="live",
-                )
+                        # Filter out empty or all-NA columns before concatenation
+                        cls.signal_df = cls.signal_df.dropna(axis=1, how='all')
+                        new_signal_df = new_signal_df.dropna(axis=1, how='all')
+                        # Concatenate the new signal DataFrame with the existing one
+                        cls.signal_df = pd.concat([cls.signal_df, new_signal_df], ignore_index=True)
 
-                time.sleep(1)
+                
+                         # Emit the message
+                        if len(cls.signal_df) >= cls.max_df_size:
+                            print(cls.signal_df)
+                            InfluxHandler.write(
+                                 cls.signal_df, measurement='live'
+                            )
+
+                            cls.signal_df = pd.DataFrame(columns=['time', 'value', 'unit', 'signal'])
+                            time.sleep(1)
+                        
+                else:
+                    last_bit = packet_size
 
         except Exception as e:
             logger.error("Error receiving/sending proto msg:", e)
         finally:
             cls.ser.close()
+
+
+
+    @classmethod    
+    def make_bytes(cls, message):
+        """
+        Make the byte array out of the messages array.
+        """
+        return bytearray([
+            message.message_0, message.message_1, message.message_2, 
+            message.message_3, message.message_4, message.message_5, 
+            message.message_6, message.message_7
+        ]) 
