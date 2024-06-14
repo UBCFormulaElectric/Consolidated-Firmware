926a75590c7b7519625545c1dd5f93062a652f39
DJ90864 <dikshitjain664@gmail.com>
2024-06-14 08:55:15 -0400

    debounce to state transition added

--- firmware/quadruna/BMS/src/app/states/app_driveState.c
+++ firmware/quadruna/BMS/src/app/states/app_driveState.c
@@ -3,8 +3,13 @@
 #include "io_airs.h"
 #include "app_airs.h"
 
+#define AIR_N_DEBOUNCE_PERIOD (500U) //ms
+
+static TimerChannel debounce_timer;
+
 static void driveStateRunOnEntry(void)
 {
+    app_timer_init(&debounce_timer, AIR_N_DEBOUNCE_PERIOD);
     app_canTx_BMS_State_set(BMS_DRIVE_STATE);
 }
 
@@ -20,7 +25,16 @@ static void driveStateRunOnTick100Hz(void)
         // if AIR- opens, go back to init state.
         const bool air_negative_opened  = !io_airs_isNegativeClosed();
         const bool is_charger_connected = app_canRx_BRUSA_IsConnected_get();
-        if (air_negative_opened || is_charger_connected)
+
+        bool air_negative_opened_debounced = false;
+
+        // app_timer_runIfCondition(&debounce_timer, air_negative_opened);
+
+        if(app_timer_runIfCondition(&debounce_timer, air_negative_opened) == TIMER_STATE_EXPIRED) {
+            air_negative_opened_debounced = true;
+        }
+
+        if (air_negative_opened_debounced || is_charger_connected)
         {
             app_stateMachine_setNextState(app_initState_get());
         }
