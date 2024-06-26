474c61d083728c9428a6c452f6719b742bc6369e
Edwin <20777515+Lucien950@users.noreply.github.com>
2024-06-26 00:08:32 -0700

    minimal C++ build

---
+++ firmware/quintuna/CRIT/src/tasks.cpp
@@ -0,0 +1,40 @@
+#include "tasks.h"
+
+void tasks_preInit() {}
+
+void tasks_init() {}
+
+void tasks_run1Hz()
+{
+    for (;;)
+    {
+    }
+}
+
+void tasks_run100Hz()
+{
+    for (;;)
+    {
+    }
+}
+
+void tasks_run1kHz()
+{
+    for (;;)
+    {
+    }
+}
+
+void tasks_runCanTx()
+{
+    for (;;)
+    {
+    }
+}
+
+void tasks_runCanRx()
+{
+    for (;;)
+    {
+    }
+}
---
+++ firmware/quintuna/CRIT/src/tasks.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#ifdef __cplusplus
+#define EXTERNC extern "C"
+#define EXTERNCNORET extern "C" [[noreturn]]
+#else
+#define EXTERNC
+#define EXTERNCNORET _Noreturn
+#endif
+
+EXTERNC void      tasks_preInit();
+EXTERNC void      tasks_init();
+EXTERNCNORET void tasks_run1Hz();
+EXTERNCNORET void tasks_run100Hz();
+EXTERNCNORET void tasks_run1kHz();
+EXTERNCNORET void tasks_runCanTx();
+EXTERNCNORET void tasks_runCanRx();
+
+#undef EXTERNC
