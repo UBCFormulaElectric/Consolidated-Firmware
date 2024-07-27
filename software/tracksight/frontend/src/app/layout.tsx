663e5db0b03efc7d241860e2f3b23bfdba1e95e2
Edwin <20777515+Lucien950@users.noreply.github.com>
2024-07-27 01:09:07 -0700

    delete

--- software/tracksight/frontend/src/app/layout.tsx
+++ software/tracksight/frontend/src/app/layout.tsx
@@ -2,7 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './navbar';
-import ThemeProvider, { ThemeContext } from './ThemeProvider';
+import ThemeProvider from './ThemeProvider';
 
 const inter = Inter({ subsets: ['latin'] })
 
