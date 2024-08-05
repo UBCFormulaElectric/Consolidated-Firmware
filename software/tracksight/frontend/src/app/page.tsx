0a582ced23bc0d721f20af4f0ea66c43c6605dcc
myung03 <matthewyungisworking@gmail.com>
2024-08-04 22:52:54 -0700

    convert to tailwind css

--- software/tracksight/frontend/src/app/page.tsx
+++ software/tracksight/frontend/src/app/page.tsx
@@ -1,67 +1,70 @@
-'use client';
-import React, { useEffect, useState } from 'react';
-import { message, Layout } from 'antd';
+"use client";
+import React, { useState } from "react";
+import { message, Layout } from "antd";
 const { Header, Content } = Layout;
-import styles from './page.module.css';
-import NavBar from './components/navbar';
-import Dashboard from './components/dashboard';
-import Visualize from './components/visualize';
+import NavBar from "./components/navbar";
+import Dashboard from "./components/dashboard";
+import Visualize from "./components/visualize";
 
 const FLASK_URL = "http://localhost:5000";
 
 const Home = () => {
-    const [componentToDisplay, setComponentToDisplay] = useState<string>('visualize');
-    const [graphs, setGraphs] = useState<number[]>([]);
-    const [liveGraphs, setLiveGraphs] = useState<number[]>([]);
-    const [messageApi, contextHolder] = message.useMessage();
-    const [theme, setTheme] = useState<boolean>(true);
+  const [componentToDisplay, setComponentToDisplay] =
+    useState<string>("visualize");
+  const [graphs, setGraphs] = useState<number[]>([]);
+  const [liveGraphs, setLiveGraphs] = useState<number[]>([]);
+  const [messageApi, contextHolder] = message.useMessage();
+  const [theme, setTheme] = useState<boolean>(true);
 
-    //add a new graph
-    const addGraph = (live: boolean) => {
-        const newGraphId = Date.now();
-        if (live) {
-            setLiveGraphs(prevGraphs => [...prevGraphs, newGraphId]);
-        } else {
-            setGraphs(prevGraphs => [...prevGraphs, newGraphId]);
-        }
-    };
+  //add a new graph
+  const addGraph = (live: boolean) => {
+    const newGraphId = Date.now();
+    if (live) {
+      setLiveGraphs((prevGraphs) => [...prevGraphs, newGraphId]);
+    } else {
+      setGraphs((prevGraphs) => [...prevGraphs, newGraphId]);
+    }
+  };
 
+  //delete a graph
+  const deleteGraph = (graphId: number, live: boolean) => {
+    if (live) {
+      setLiveGraphs((prevGraphs) => prevGraphs.filter((id) => id !== graphId));
+    } else {
+      setGraphs((prevGraphs) => prevGraphs.filter((id) => id !== graphId));
+    }
+  };
 
-    //delete a graph
-    const deleteGraph = (graphId: number, live: boolean) => {
-        if (live) {
-            setLiveGraphs(prevGraphs => prevGraphs.filter(id => id !== graphId));
-        } else {
-            setGraphs(prevGraphs => prevGraphs.filter(id => id !== graphId));
-        }
-    };
-
-    return (
-        <Layout className={`${styles.main} ${theme ? styles.dark : styles.light}`}>
-            <Header className={styles.header}>
-                <NavBar updateFunction={setComponentToDisplay} theme={theme} setTheme={setTheme} />
-            </Header>
-            <Content>
-                {contextHolder}
-                {
-                    componentToDisplay === "visualize" ? (
-                        <Visualize
-                            addGraph={addGraph}
-                            graphs={graphs}
-                            liveGraphs={liveGraphs}
-                            deleteGraph={deleteGraph}
-                            url={FLASK_URL}
-                            messageApi={messageApi}
-                        />
-                    ) : (
-                        <Dashboard
-                            deleteGraph={deleteGraph}
-                        />
-                    )
-                }
-            </Content>
-        </Layout>
-    );
-}
+  return (
+    <Layout
+      className={`flex flex-col justify-between p-0 m-0 min-h-screen ${
+        theme ? "text-white bg-black" : "text-black bg-white"
+      }`}
+    >
+      <Header className="p-0">
+        <NavBar
+          updateFunction={setComponentToDisplay}
+          theme={theme}
+          setTheme={setTheme}
+        />
+      </Header>
+      <Content>
+        {contextHolder}
+        {componentToDisplay === "visualize" ? (
+          <Visualize
+            addGraph={addGraph}
+            graphs={graphs}
+            liveGraphs={liveGraphs}
+            deleteGraph={deleteGraph}
+            url={FLASK_URL}
+            messageApi={messageApi}
+          />
+        ) : (
+          <Dashboard deleteGraph={deleteGraph} />
+        )}
+      </Content>
+    </Layout>
+  );
+};
 
 export default Home;
