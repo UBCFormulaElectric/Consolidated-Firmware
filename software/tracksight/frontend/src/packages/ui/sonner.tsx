6dc45a708e4a552c61d87a9d727f8af9a550d804
myung03 <matthewyungisworking@gmail.com>
2024-08-16 15:10:44 -0700

    set up alias imports

---
+++ software/tracksight/frontend/src/packages/ui/sonner.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface SonnerProps {
+  // Add your props here
+}
+
+const Sonner: React.FC<SonnerProps> = () => {
+  return <div>{/* Add your component code here */}</div>;
+};
+
+export default Sonner;
