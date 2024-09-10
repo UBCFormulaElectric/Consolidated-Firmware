c51973d53795d3dab729cb5da7927c42e7c46165
DJ90864 <79118005+DJ90864@users.noreply.github.com>
2024-09-10 14:51:07 -0700

    Fixed some sample code for Power Supply

--- validationtools/validationtools/powerSupply/_powerSupply.py
+++ validationtools/validationtools/powerSupply/_powerSupply.py
@@ -95,11 +95,13 @@ class PowerSupply:
 if __name__ == "__main__":
     power_supply = PowerSupply()
     print(power_supply.get_id())
-    power_supply.set_voltage(5.0)  # Set voltage to 5V
-    power_supply.set_current(1.51)  # Set current to 1.51A
-    power_supply.enable_output()
-    time.sleep(5)  # Wait for 5 seconds
-    voltage = power_supply.measure_voltage()
-    current = power_supply.measure_current()
+    # power_supply.set_voltage(5.0)  # Set voltage to 5V
+    power_supply.set_current(1.51, 2)
+    power_supply.enable_output(2)
+    
+    time.sleep(5)
+
+    current = power_supply.measure_current(2)
+    voltage = power_supply.measure_voltage(2)
     print(f"Voltage: {voltage} V, Current: {current} A")
     power_supply.disable_output()
