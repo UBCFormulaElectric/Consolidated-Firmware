f702b57bbc2f16c72d63564a0c96fb8b53dbb237
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2024-10-19 16:14:09 -0700

    Finsihed task

---
+++ firmware/quintuna/RSM/src/hw/hw_gpios.h
@@ -0,0 +1,14 @@
+#include "hw_gpio.h"
+
+namespace hw::gpio
+{
+extern const Gpio n_chimera_pin;     
+extern const Gpio led_pin;       
+extern const Gpio rad_fan_en_pin;
+extern const Gpio fr_stby_pin;
+extern const Gpio brake_light_en_pin;
+extern const Gpio acc_fan_en_pin;
+extern const Gpio n_program_pin;
+extern const Gpio acc_fan_pin;
+extern const Gpio rad_fan_pin;
+} // namespace hw::gpio
