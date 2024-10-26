45b4bd6cdc29494028f23ee46984da08bea26e8e
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2024-10-26 16:41:56 -0700

    working on pwms

---
+++ firmware/quintuna/RSM/hw/hw_pwms.h
@@ -0,0 +1,9 @@
+#pragma once
+#include "hw_pwmInput.h"
+#include "hw_pwmInputFreqOnly.h"
+#include <stdint.h>
+
+namespace hw::pwm
+{
+    extern const PwmInputFreqOnly coolant_config;
+} // namespace hw:pwm
