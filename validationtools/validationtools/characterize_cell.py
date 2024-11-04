d9b76e0b5f6382f7ed1182ed8f770fc3bd6660fc
Kelm <lkevin2003@gmail.com>
2024-11-03 20:06:59 -0800

    added the basic outline for the script to characterize the cells

--- validationtools/validationtools/characterize_cell.py
+++ validationtools/validationtools/characterize_cell.py
@@ -2,16 +2,97 @@ from loadBank._loadBank import *
 from powerSupply._powerSupply import *
 from logger.logger import *
 
-def main()->None:
-    loadbank = LoadBank()
-    powersupply = PowerSupply()
-    loggingDict = \
-    {
-        "Voltage [V]" : powersupply.measure_voltage,
-        "Current [A]" : powersupply.measure_current
-    }
+totalCellCapacity = 100000 # mAh Filler Value
+chargingRate      = 1      # 1C Rate in amps Filler Value
 
-    logger = Logger() 
+
+def getCoulombs( current, timeStepSeconds)->float:
+    coulombs = current * timeStepSeconds
+    return coulombs
+
+def getCellSOC(accumulatedCoulombs):
+    return accumulatedCoulombs / totalCellCapacity * 100 # In percent
+
+def getChargingRow(logger, powerSupply):
+    lastRow = logger.getLastRow
+    voltage = powerSupply.measure_voltage
+    current = powerSupply.measure_current
+    if lastRow is not None:
+        chargingCoulombs = getCoulombs(current, logger.getTimeIncrement()) + lastRow[5] # Charging Coulombs Col
+    else:
+        chargingCoulombs = None
+    dischargingCoulombs = None
+    soc = chargingCoulombs / totalCellCapacity * 100
+
+    row = [voltage, current, chargingCoulombs, dischargingCoulombs, soc]
+
+    return row
+
+def getDischargingRow(logger, loadBank):
+    lastRow = logger.getLastRow
+    voltage = loadBank.measure_voltage
+    current = loadBank.measure_current
+    if lastRow is not None:
+        dischargingCoulombs = getCoulombs(current, logger.getTimeIncrement()) + lastRow[6] # Charging Coulombs Col
+    else:
+        dischargingCoulombs = None
+    chargingCoulombs = None
+    soc = (1- dischargingCoulombs / totalCellCapacity) * 100
+
+    row = [voltage, current, chargingCoulombs, dischargingCoulombs, soc]
+
+    return row
+
+
+def main()->None:
+    loadBank = LoadBank()
+    powerSupply = PowerSupply()
+    soc = 0
+
+    loggingCols = \
+    [
+        "Voltage [V]",
+        "Current [A]",
+        "Charging Coulombs [Ah]",
+        "Discharging Coulombs [Ah]",
+        "SOC"
+    ]
+
+    logger = Logger(loggingCols)
+    logger.startLogging()
+
+    while(soc < 100):
+        powerSupply.set_voltage(1)
+        powerSupply.set_current(chargingRate)
+        row = getChargingRow(logger, powerSupply)
+        logger.storeRow(row)
+
+        time.sleep(10)
+        powerSupply.set_current(0)
+
+        row = getChargingRow(logger, powerSupply)
+        logger.storeRow(row)
+        time.sleep(10)
+
+        soc = row[-1]
+
+    while(soc > 0):
+        loadBank.set_voltage(1)
+        loadBank.set_current(-chargingRate)
+        row = getChargingRow(logger, loadBank)
+        logger.storeRow(row)
+
+        time.sleep(10)
+        loadBank.set_current(0)
+
+        row = getChargingRow(logger, loadBank)
+        logger.storeRow(row)
+        time.sleep(10)
+
+        soc = row[-1]
+    
+    
+     
 
 
 
--- validationtools/validationtools/logger/logger.py
+++ validationtools/validationtools/logger/logger.py
@@ -7,36 +7,53 @@ import threading
 # from powerSupply._powerSupply import *
 
 class Logger:
-    def __init__(self, logFolder: str, columnMap) -> None:
-        self.columns = ["Timestamp", "Elapsed Time [s]"] + list(columnMap.keys())
-        self.columnFuncs = [self.getTimestamp, self.getElapsedTime] + list(columnMap.values())
+    def __init__(self, logFolder: str, columns, newFile= True) -> None:
+        self.columns = ["Timestamp", "Elapsed Time [s]", "Time Increment [s]"] + columns
         self.logFileName = datetime.now().strftime("log_%Y-%m-%d_%H-%M-%S.csv")
         self.logFilePath = os.path.join(logFolder, self.logFileName)
         self.logStop = False
-    
-    def startLogging(self):
-        with open(self.logFilePath, mode= 'w', newline= '') as csvFile:
-            writer = csv.writer(csvFile, delimiter= ',', quotechar= '|', quoting= csv.QUOTE_MINIMAL)
-            writer.writerow(self.columns)
-        thread = threading.Thread(target= self.logLoop)
-        thread.start()
-    
-    def logLoop(self):
-        self.startTime = time.time()
-        self.logStop = False
-        while(not self.logStop):
-            with open(self.logFilePath, mode= 'a', newline= '') as csvFile:
+        if newFile:
+            with open(self.logFilePath, mode= 'w', newline= '') as csvFile:
                 writer = csv.writer(csvFile, delimiter= ',', quotechar= '|', quoting= csv.QUOTE_MINIMAL)
-                row = [str(func()) for func in self.columnFuncs]
+                writer.writerow(self.columns)
+    
+    # def startLogging(self):
+    #     with open(self.logFilePath, mode= 'w', newline= '') as csvFile:
+    #         writer = csv.writer(csvFile, delimiter= ',', quotechar= '|', quoting= csv.QUOTE_MINIMAL)
+    #         writer.writerow(self.columns)
+        # thread = threading.Thread(target= self.logLoop)
+        # thread.start()
+    
+    # def logLoop(self):
+    #     self.startTime = time.time()
+    #     self.logStop = False
+    #     while(not self.logStop):
+    #         with open(self.logFilePath, mode= 'a', newline= '') as csvFile:
+    #             writer = csv.writer(csvFile, delimiter= ',', quotechar= '|', quoting= csv.QUOTE_MINIMAL)
+    #             row = [str(func()) for func in self.columnFuncself.columnFuncInputs]
+    #             self.lastRow = row
                 
-                print(row)
-                writer.writerow(row)
-                csvFile.flush()
-            time.sleep(1)
+    #             print(row)
+    #             writer.writerow(row)
+    #             csvFile.flush()
+    #         time.sleep(1)
 
-    def stopLogging(self):
-        self.logStop = True
-        return
+    # def stopLogging(self):
+    #     self.logStop = True
+    #     return
+
+
+    def storeRow(self, row):
+        if self.startTime is None:
+            self.startTime = time.time()
+        self.lastRowTime = time.time()
+        with open(self.logFilePath, mode= 'a', newline= '') as csvFile:
+                writer = csv.writer(csvFile, delimiter= ',', quotechar= '|', quoting= csv.QUOTE_MINIMAL)
+                
+                writer.writerow([self.getTimestamp(), self.getElapsedTime()] + row)
+                
+    def getLastRow(self):
+        return self.lastRow
     
     def getTimestamp(self):
         return datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
@@ -48,7 +65,19 @@ class Logger:
         elapsed_time = (time.time() - self.startTime) 
         return round(elapsed_time, 3)   
     
-                
+    def getTimeIncrement(self):
+        if self.startTime is None:
+            return "Logging has not started yet."
+        elif self.getLastRow() is None:
+            return None
+        else: 
+        # Calculate elapsed time in milliseconds
+            timeIncrement = (time.time() - self.lastRowTime) 
+        return round(timeIncrement, 3)   
+    
+
+
+# TESTING #  
 def get1():
     return 1
 
