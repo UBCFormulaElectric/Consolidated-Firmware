4b74131080b239a8d6ca1a1b55f5689dfe326224
kryptoish <krish_thakur7@icloud.com>
2024-11-04 20:03:58 -0800

    CRIT and RSM fixes. FSM deinit peripherals sequence added

--- firmware/quadruna/CRIT/src/tasks.c
+++ firmware/quadruna/CRIT/src/tasks.c
@@ -43,10 +43,6 @@ extern ADC_HandleTypeDef  hadc1;
 extern TIM_HandleTypeDef  htim3;
 extern UART_HandleTypeDef huart2;
 extern CAN_HandleTypeDef  hcan1;
-extern ADC_HandleTypeDef  hadc3;
-extern UART_HandleTypeDef huart1;
-extern UART_HandleTypeDef huart3;
-extern UART_HandleTypeDef huart7;
 
 static const CanHandle can = { .can = &hcan1, .can_msg_received_callback = io_can_pushRxMsgToQueue };
 
@@ -415,25 +411,17 @@ void tasks_preInit(void)
 
 void tasks_JumpToApp(void)
 {
-    HAL_TIM_Base_Stop_IT(&htim3); // stops the timer and disables the interrupts
+    HAL_TIM_Base_Stop_IT(&htim3); // disables the interrupts for timer
     HAL_TIM_Base_DeInit(&htim3);
 
-    HAL_UART_Abort_IT(&huart1);
     HAL_UART_Abort_IT(&huart2);
-    HAL_UART_Abort_IT(&huart3); // a communication protocol used for serial communication between devices
-    HAL_UART_Abort_IT(&huart7);
-    HAL_UART_DeInit(&huart1);
     HAL_UART_DeInit(&huart2);
-    HAL_UART_DeInit(&huart3);
-    HAL_UART_DeInit(&huart7);
 
     HAL_CAN_IRQHandler(&hcan1);
     HAL_CAN_DeInit(&hcan1);
 
     HAL_ADC_Stop_IT(&hadc1);
-    HAL_ADC_Stop_IT(&hadc3);
     HAL_ADC_DeInit(&hadc1); // analog to digital signals
-    HAL_ADC_DeInit(&hadc3);
 }
 
 void tasks_init(void)
