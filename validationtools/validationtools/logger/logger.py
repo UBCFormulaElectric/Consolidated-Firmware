bd9b493ac96d56330a0d2d9b65a582415a018064
Kelm <lkevin2003@gmail.com>
2024-11-04 20:35:17 -0800

    finished basic implementation of logger

--- validationtools/validationtools/logger/logger.py
+++ validationtools/validationtools/logger/logger.py
@@ -2,70 +2,62 @@ import os
 import time
 from datetime import datetime
 import csv
-import threading
-# from loadBank._loadBank import *
-# from powerSupply._powerSupply import *
+
 
 class Logger:
-    def __init__(self, logFolder: str, columns, newFile= True) -> None:
-        self.columns = ["Timestamp", "Elapsed Time [s]", "Time Increment [s]"] + columns
+    def __init__(self, logFolder: str, columns) -> None:
+        self.columns = ["Index", "Timestamp", "Elapsed Time [s]", "Time Increment [s]"] + columns
         self.logFileName = datetime.now().strftime("log_%Y-%m-%d_%H-%M-%S.csv")
         self.logFilePath = os.path.join(logFolder, self.logFileName)
-        self.logStop = False
-        if newFile:
-            with open(self.logFilePath, mode= 'w', newline= '') as csvFile:
-                writer = csv.writer(csvFile, delimiter= ',', quotechar= '|', quoting= csv.QUOTE_MINIMAL)
-                writer.writerow(self.columns)
-    
-    # def startLogging(self):
-    #     with open(self.logFilePath, mode= 'w', newline= '') as csvFile:
-    #         writer = csv.writer(csvFile, delimiter= ',', quotechar= '|', quoting= csv.QUOTE_MINIMAL)
-    #         writer.writerow(self.columns)
-        # thread = threading.Thread(target= self.logLoop)
-        # thread.start()
-    
-    # def logLoop(self):
-    #     self.startTime = time.time()
-    #     self.logStop = False
-    #     while(not self.logStop):
-    #         with open(self.logFilePath, mode= 'a', newline= '') as csvFile:
-    #             writer = csv.writer(csvFile, delimiter= ',', quotechar= '|', quoting= csv.QUOTE_MINIMAL)
-    #             row = [str(func()) for func in self.columnFuncself.columnFuncInputs]
-    #             self.lastRow = row
-                
-    #             print(row)
-    #             writer.writerow(row)
-    #             csvFile.flush()
-    #         time.sleep(1)
+        self.startTime = None
+        self.lastRow = None
+        self.lastRowTime = None
 
-    # def stopLogging(self):
-    #     self.logStop = True
-    #     return
+        with open(self.logFilePath, mode= 'w', newline= '') as csvFile:
+            writer = csv.writer(csvFile, delimiter= ',', quotechar= '|', quoting= csv.QUOTE_MINIMAL)
+            writer.writerow(self.columns)
+
+    def getIndex(self)->int:
+        if self.lastRow is None:
+            return 0
+        else:
+            return self.lastRow["Index"] + 1
 
 
-    def storeRow(self, row):
+    def storeRow(self, row)->None:
         if self.startTime is None:
             self.startTime = time.time()
-        self.lastRowTime = time.time()
+        
         with open(self.logFilePath, mode= 'a', newline= '') as csvFile:
                 writer = csv.writer(csvFile, delimiter= ',', quotechar= '|', quoting= csv.QUOTE_MINIMAL)
+                row = [self.getIndex(), self.getTimestamp(), self.getElapsedTime(), self.getTimeIncrement()] + row
                 
-                writer.writerow([self.getTimestamp(), self.getElapsedTime()] + row)
+                writer.writerow(row)
+
+                self.lastRow = dict(zip(self.columns, row))
+
+        self.lastRowTime = time.time()
                 
-    def getLastRow(self):
+    def getLastRow(self)->dict:
+        """
+        Returns a map with the column names as keys and the last column values as values.
+
+        INPUTS:  None
+        OUTPUTS: Map with the column names as keys and the last column values as values.
+        """
         return self.lastRow
     
-    def getTimestamp(self):
+    def getTimestamp(self)->datetime:
         return datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-3]
     
-    def getElapsedTime(self):
+    def getElapsedTime(self)->float:
         if self.startTime is None:
             return "Logging has not started yet."
         # Calculate elapsed time in milliseconds
         elapsed_time = (time.time() - self.startTime) 
         return round(elapsed_time, 3)   
     
-    def getTimeIncrement(self):
+    def getTimeIncrement(self)->float:
         if self.startTime is None:
             return "Logging has not started yet."
         elif self.getLastRow() is None:
@@ -76,7 +68,6 @@ class Logger:
         return round(timeIncrement, 3)   
     
 
-
 # TESTING #  
 def get1():
     return 1
@@ -84,16 +75,16 @@ def get1():
 def get2():
     return 2
 
-
-
     
 def main():
-    file = r'C:\Users\lkevi\OneDrive\Desktop\Coding'
-    funcDict = {"One": get1, "Two": get2}
+    file = r'Your\file\path'
+    funcList = ["One", "Two", "Three Float"]
 
-    logger = Logger(file, funcDict)
+    logger = Logger(file, funcList)
+    while(1):
 
-    logger.startLogging()
+        logger.storeRow([1, 2, 3.4])
+        time.sleep(0.5)
 
 if __name__ == "__main__":
     main()
