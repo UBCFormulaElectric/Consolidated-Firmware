05540aefbaffe7e52fa7e58c8260c1a9f4401792
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2024-11-04 19:08:21 -0800

    building fine, let's test

--- firmware/shared/srcpp/io/io_stackMonitor.cpp
+++ firmware/shared/srcpp/io/io_stackMonitor.cpp
@@ -4,6 +4,8 @@ namespace io
 {
 float StackMonitor::stack_usage() const
 {
-    return static_cast<float>(osThreadGetStackSpace(handle)) / static_cast<float>(osThreadGetStackSize(handle));
+    // WARNING: osThreadGetStackSize is not implemented in FreeRTOS
+    // https://github.com/ARM-software/CMSIS-FreeRTOS/blob/dd7793adcbea0c3c0f3524f86b031ab88b9e2193/DoxyGen/General/src/cmsis_freertos.txt#L312
+    return static_cast<float>(osThreadGetStackSpace(handle)) / stack_size;
 }
 } // namespace io
--- firmware/shared/srcpp/io/io_stackMonitor.h
+++ firmware/shared/srcpp/io/io_stackMonitor.h
@@ -7,8 +7,13 @@ class StackMonitor
 {
     osThreadId_t handle; // We can't use TaskHandle as a compile-time constant,
                          // so instead we use a pointer to it.
+    const float stack_size;
   public:
-    explicit StackMonitor(osThreadId_t i_handle) : handle(i_handle) {}
-    [[nodiscard]] float    stack_usage() const;
+    explicit StackMonitor(osThreadId_t i_handle, const uint32_t i_stack_size) : handle(i_handle), stack_size(static_cast<float>(i_stack_size)) {}
+
+    /**
+    * Returns stack usage for in percentage between 0-1
+    */
+    [[nodiscard]] float stack_usage() const;
 };
 } // namespace io
