43ceb5f346d2fba7fdedd2567ece4c5ec2fbf6aa
Jack Polloway <60236839+yawollopkcaj@users.noreply.github.com>
2024-11-09 14:50:40 -0800

    added test for diagnostics temperature reading and fixed previous testing for diagnostics voltage and diagnostics enabled

--- firmware/quadruna/BMS/test/test_diagnosticsMode.cpp
+++ firmware/quadruna/BMS/test/test_diagnosticsMode.cpp
@@ -5,35 +5,82 @@ class BmsFaultTest : public BmsBaseStateMachineTest
 {
 };
 
-TEST_F(BmsFaultTest, check_all_cell_voltages_segment0)
+TEST_F(BmsFaultTest, check_cell_voltage_segment0_cell0)
 {
     /**
-     * Test that every cell voltage in Segment 0 is being properly transmitted to CAN
+     * Test cell voltage in Segment 0 is being properly transmitted to CAN.
      */
 
+    // Setting up the environment.
     TearDown();
     SetUp();
-    float fake_voltage = 3.0f;
+    float fake_voltage = 1.0f;
     fake_io_ltc6813CellVoltages_getCellVoltage_returnsForAnyArgs(fake_voltage);
+
+    // Passing time to let the message transmit.
     LetTimePass(1000);
+
+    // Reciving and transmitting the data.
+    app_canRx_Debug_BMSDiagnosticsRequest_update(true);
     app_canRx_Debug_SegmentCellVoltageRequest_update(SEG_0);
-    app_diagnosticsMode_broadcast();
+
+    // Passing time to let the message transmit.
+    LetTimePass(1000);
+
+    // Test assertion.
     ASSERT_EQ(fake_voltage, app_canTx_BMS_Seg0_Cell0_Voltage_get());
-    ASSERT_EQ(fake_voltage, app_canTx_BMS_Seg0_Cell1_Voltage_get());
 }
 
-TEST_F(BmsFaultTest, check_diagnostics_mode_is_off)
+TEST_F(BmsFaultTest, check_temperature_segment0)
 {
     /**
-     * Test that neither voltage nor temperature reading are being
-     * transmitted when diagnostics mode is off (even if individual temperature/voltage reading modes are turned on).
+     * Test temperature in segment 0 is being properly transmitted to CAN. 
      */
 
+    // Setting up the environment.
+    TearDown();
+    SetUp();
+    float fake_temperature = 2.0f;
+    fake_io_ltc6813CellTemperatures_getSpecificCellTempDegC_returnsForAnyArgs(fake_temperature);
+
+    // Passing time to let the message transmit.
+    LetTimePass(1000);
+
+    // Reciving and transmitting the data.
+    app_canRx_Debug_BMSDiagnosticsRequest_update(true);
+    app_canRx_Debug_SegmentTemperaturesRequest_update(true);
+    //app_diagnosticsMode_broadcast();
+
+    // Passing time to let the message transmit.
+    LetTimePass(1000);
+
+    // Test assertion.
+    ASSERT_EQ(fake_temperature, app_canTx_BMS_Seg0_Temp_get());
+}
+
+TEST_F(BmsFaultTest, check_diagnostics_mode_is_off_on)
+{
+    /**
+     * Test that neither voltage nor temperature reading are being transmitted when diagnostics 
+     * mode is off (even if individual temperature/voltage reading modes are turned on).
+     */
+
+    // Setting up the environment.
     TearDown();
     SetUp();
     float fake_voltage = 3.0f;
     fake_io_ltc6813CellVoltages_getCellVoltage_returnsForAnyArgs(fake_voltage);
+
+    // Passing time to let the message transmit.
     LetTimePass(1000);
+
+    // Reciving and transmitting the data.
     app_canRx_Debug_BMSDiagnosticsRequest_update(false);
+    app_canRx_Debug_SegmentCellVoltageRequest_update(SEG_0);
+
+    // Passing time to let the message transmit.
+    LetTimePass(1000);
+
+    // Test assertion.
     ASSERT_EQ(0, app_canTx_BMS_Seg0_Cell0_Voltage_get());
 }
