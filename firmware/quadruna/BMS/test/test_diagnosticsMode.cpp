69b687fb4700bd2e7151f467aec74b2b1d5626bc
Jack Polloway <60236839+yawollopkcaj@users.noreply.github.com>
2024-11-09 13:50:20 -0800

    style changes fixed

--- firmware/quadruna/BMS/test/test_diagnosticsMode.cpp
+++ firmware/quadruna/BMS/test/test_diagnosticsMode.cpp
@@ -7,7 +7,9 @@ class BmsFaultTest : public BmsBaseStateMachineTest
 
 TEST_F(BmsFaultTest, check_all_cell_voltages_segment0)
 {
-    // Test that every cell voltage in Segment 0 is being properly transmitted to CAN
+    /**
+     * Test that every cell voltage in Segment 0 is being properly transmitted to CAN
+     */ 
 
     TearDown();
     SetUp();
@@ -22,14 +24,15 @@ TEST_F(BmsFaultTest, check_all_cell_voltages_segment0)
 
 TEST_F(BmsFaultTest, check_diagnostics_mode_is_off) 
 {
-    // Test that neither voltage nor temperature reading are being transmitted when diagnostics mode is off (even if individual temperature/voltage reading modes are turned on).
+    /**
+     * Test that neither voltage nor temperature reading are being 
+     * transmitted when diagnostics mode is off (even if individual temperature/voltage reading modes are turned on).
+     */
 
     TearDown();
     SetUp();
     float fake_voltage = 3.0f;
     fake_io_ltc6813CellVoltages_getCellVoltage_returnsForAnyArgs(fake_voltage);
-
     LetTimePass(1000);
-    
     ASSERT_EQ(3.0f, app_canTx_BMS_Seg0_Cell0_Voltage_get());
 }
