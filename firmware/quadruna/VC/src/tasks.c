c7959fff3d14b6556bbfd2a14f7339f0250d0e36
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2024-11-09 14:12:30 -0800

    message for sysview conf in tasks.c

--- firmware/quadruna/VC/src/tasks.c
+++ firmware/quadruna/VC/src/tasks.c
@@ -334,7 +334,7 @@ void tasks_init(void)
 {
     // Configure and initialize SEGGER SystemView.
     // NOTE: Needs to be done after clock config!
-    SEGGER_SYSVIEW_Conf();
+    SEGGER_SYSVIEW_Conf(); // aka traceSTART apparently...
     LOG_INFO("VC reset!");
 
     __HAL_DBGMCU_FREEZE_IWDG1();
