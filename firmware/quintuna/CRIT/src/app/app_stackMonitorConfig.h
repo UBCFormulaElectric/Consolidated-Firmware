5c978234307efb735ae1296203a0af00bb0cc2df
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2024-11-09 22:56:48 -0800

    ok

--- firmware/quintuna/CRIT/src/app/app_stackMonitorConfig.h
+++ firmware/quintuna/CRIT/src/app/app_stackMonitorConfig.h
@@ -8,13 +8,13 @@ namespace app
 inline StackMonitor<5> crit_stack_monitor{
     { stack_monitor_config{ task_1_hz_stack_monitor, 0.7f, app_canTx_CRIT_StackWaterMarkHighTask1HzLevel_set,
                             app_canTx_CRIT_Warning_StackWaterMarkHighTask1Hz_set },
-      { task_100_hz_stack_monitor, 0.7f, app_canTx_CRIT_StackWaterMarkHighTask100HzLevel_set,
+      stack_monitor_config { task_100_hz_stack_monitor, 0.7f, app_canTx_CRIT_StackWaterMarkHighTask100HzLevel_set,
         app_canTx_CRIT_Warning_StackWaterMarkHighTask100Hz_set },
-      { task_1_khz_stack_monitor, 0.7f, app_canTx_CRIT_StackWaterMarkHighTask1kHzLevel_set,
+      stack_monitor_config { task_1_khz_stack_monitor, 0.7f, app_canTx_CRIT_StackWaterMarkHighTask1kHzLevel_set,
         app_canTx_CRIT_Warning_StackWaterMarkHighTask1kHz_set },
-      { task_canrx_stack_monitor, 0.7f, app_canTx_CRIT_StackWaterMarkHighTaskCanRxLevel_set,
+      stack_monitor_config { task_canrx_stack_monitor, 0.7f, app_canTx_CRIT_StackWaterMarkHighTaskCanRxLevel_set,
         app_canTx_CRIT_Warning_StackWaterMarkHighTaskCanTx_set },
-      { task_cantx_stack_monitor, 0.7f, app_canTx_CRIT_StackWaterMarkHighTaskCanTxLevel_set,
+      stack_monitor_config { task_cantx_stack_monitor, 0.7f, app_canTx_CRIT_StackWaterMarkHighTaskCanTxLevel_set,
         app_canTx_CRIT_Warning_StackWaterMarkHighTaskCanRx_set } }
 };
 }
