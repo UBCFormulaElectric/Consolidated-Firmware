f8be8d69a8b5472f40f2601399d673db30450f22
rijwal <rijwalsangey@gmail.com>
2024-11-16 13:45:21 -0800

    Initial commit

--- firmware/quadruna/RSM/src/io/io_suspension.h
+++ firmware/quadruna/RSM/src/io/io_suspension.h
@@ -5,21 +5,20 @@
  * Get the travel of the rear left suspension
  * @return The travel of the suspension in (TODO: add units)
  */
+float io_suspension_getRearLeftTravel(void);
 
-    float getRearLeftTravel(void);
+/**
+ * Get the travel of the rear right suspension
+ * @return The travel of the suspension in (TODO: add units)
+ */
+float io_suspension_getRearRightTravel(void);
 
-    /**
-     * Get the travel of the rear right suspension
-     * @return The travel of the suspension in (TODO: add units)
-     */
-    float getRearRightTravel(void);
+/**
+ * @return whether or not the left suspension sensor is open or short circuit
+ */
+bool io_suspension_leftSensorOCSC(void);
 
-    /**
-     * @return whether or not the left suspension sensor is open or short circuit
-     */
-    bool leftSensorOCSC(void);
-
-    /**
-     * @return whether or not the right suspension sensor is open or short circuit
-     */
-    bool rightSensorOCSC(void);
+/**
+ * @return whether or not the right suspension sensor is open or short circuit
+ */
+bool io_suspension_rightSensorOCSC(void);
