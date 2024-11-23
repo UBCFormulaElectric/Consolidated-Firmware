04f1c711ab96d2a3bf5d58d74f13b28ee3505854
myung03 <matthewyungisworking@gmail.com>
2024-11-23 11:49:01 -0800

    enumeration graph

--- software/tracksight/frontend/src/app/testing/page.tsx
+++ software/tracksight/frontend/src/app/testing/page.tsx
@@ -1,22 +1,57 @@
-'use client'
+"use client";
 
-import LiveFault from './components/LiveFault'
-import Timer from './components/Timer'
-import Live from './components/Live'
-
-// import MouseTest from './components/MouseTest'
+import React, { useState, useEffect } from "react";
+import LiveFault from "./components/LiveFault";
+import Timer from "./components/Timer";
+import Live from "./components/Live";
+import EnumerationGraph from "./components/Enumeration";
 
 const TestingPage = () => {
-	return (
-		<div>
-			<Live />
-			{/* <MouseTest /> */}
-			<Timer />
-			<LiveFault />
-		</div>
-	)
-}
+  const [currentTime, setCurrentTime] = useState(Math.floor(Date.now() / 1000));
+  const [currentState, setCurrentState] = useState("VC_INIT_STATE");
 
-export default TestingPage
+  const enumStates = [
+    "VC_INIT_STATE",
+    "VC_PCM_STATE",
+    "VC_HV_STATE",
+    "VC_DRIVE_STATE",
+    "VC_VD_STATE",
+  ];
+
+  useEffect(() => {
+    const timeInterval = setInterval(() => {
+      setCurrentTime(Math.floor(Date.now() / 1000));
+    }, 1000);
+
+    const stateInterval = setInterval(() => {
+      setCurrentState((prevState) => {
+        const nextIndex = Math.floor(Math.random() * enumStates.length);
+        return enumStates[nextIndex];
+      });
+    }, 10000);
+
+    return () => {
+      clearInterval(timeInterval);
+      clearInterval(stateInterval);
+    };
+  }, []);
+
+  return (
+    <div>
+      <Live />
+      {/* <MouseTest /> */}
+      <Timer />
+      <LiveFault />
+      <EnumerationGraph
+        signalName="VC_STATE"
+        currentState={currentState}
+        currentTime={currentTime}
+        enumStates={enumStates}
+      />
+    </div>
+  );
+};
+
+export default TestingPage;
 
 // TODO: Consider using ScrollArea https://ui.shadcn.com/docs/components/scroll-area
