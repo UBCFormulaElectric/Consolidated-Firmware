a2c0137fee7cda9db3c26ab5e9b6aa44af0e90aa
rijwal <rijwalsangey@gmail.com>
2024-11-25 18:12:58 -0800

    changes for review

--- firmware/quintuna/RSM/src/io/io_suspension.cpp
+++ firmware/quintuna/RSM/src/io/io_suspension.cpp
@@ -24,28 +24,5 @@ float SuspensionSensor::OCSC()
     return !(MIN_SUSPENSION_VOLTAGE <= sensorVoltage && sensorVoltage <= MAX_SUSPENSION_VOLTAGE);
 }
 
-/*
-float getRearLeftTravel(void)
-{
-    return susp_travel_rl_3v3.getVoltage();
-}
-
-float getRearRightTravel(void)
-{
-    return susp_travel_rr_3v3.getVoltage();
-}
-
-bool leftSensorOCSC(void)
-{
-    float leftSensorVoltage = susp_travel_rl_3v3.getVoltage();
-    return !(MIN_SUSPENSION_VOLTAGE <= leftSensorVoltage && leftSensorVoltage <= MAX_SUSPENSION_VOLTAGE);
-}
-
-bool rightSensorOCSC(void)
-{
-    float rightSensorVoltage = susp_travel_rr_3v3.getVoltage();
-    return !(MIN_SUSPENSION_VOLTAGE <= rightSensorVoltage && rightSensorVoltage <= MAX_SUSPENSION_VOLTAGE);
-}
-*/
 
 } // namespace io::suspension
--- firmware/quintuna/RSM/src/io/io_suspension.h
+++ firmware/quintuna/RSM/src/io/io_suspension.h
@@ -10,33 +10,15 @@
 namespace io::suspension
 {
 
-float getRearLeftTravel(void);
-
-/**
- * Get the travel of the rear right suspension
- * @return The travel of the suspension in (TODO: add units)
- */
-float getRearRightTravel(void);
-
-/**
- * @return whether or not the left suspension sensor is open or short circuit
- */
-bool leftSensorOCSC(void);
-
-/**
- * @return whether or not the right suspension sensor is open or short circuit
- */
-bool rightSensorOCSC(void);
-
 class SuspensionSensor
 {
     hw::Adc *src;
 
-  public:
-    SuspensionSensor(hw::Adc *src) { this->src = src; }
+    public:
+        SuspensionSensor(hw::Adc *src) { this->src = src; }
 
-    float getTravel();
-    float OCSC();
+        float getTravel();
+        float OCSC();
 };
 
 static extern const SuspensionSensor right;
