520276332479e2b960db36610e18393d66c78322
Liam Ilan <snowboardsheep@gmail.com>
2024-12-06 02:06:28 -0800

    Set up shared library for bulk of sil board logic.

---
+++ firmware/quadruna/sil/src/board/sil.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "io_canRx.h"
+#include "io_canTx.h"
+
+void sil_txCallback(const JsonCanMsg *msg);
+
+void sil_main(
+    void tasks_init(),
+    void tasks_1Hz(uint32_t time_ms),
+    void tasks_100Hz(uint32_t time_ms),
+    void tasks_1kHz(uint32_t time_ms));
