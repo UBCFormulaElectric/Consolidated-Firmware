1681b1a8e0469c87b0981f5cc5e9c323b5b14d09
Liam Ilan <snowboardsheep@gmail.com>
2024-12-06 18:45:05 -0800

    Clang format.

--- firmware/quadruna/sil/src/board/sil.h
+++ firmware/quadruna/sil/src/board/sil.h
@@ -6,8 +6,9 @@
 void sil_txCallback(const JsonCanMsg *msg);
 
 void sil_main(
-    int argc, char *argv[],
-    void tasks_init(),
-    void tasks_1Hz(uint32_t time_ms),
-    void tasks_100Hz(uint32_t time_ms),
-    void tasks_1kHz(uint32_t time_ms));
+    int   argc,
+    char *argv[],
+    void  tasks_init(),
+    void  tasks_1Hz(uint32_t time_ms),
+    void  tasks_100Hz(uint32_t time_ms),
+    void  tasks_1kHz(uint32_t time_ms));
