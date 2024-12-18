b8a511fe0f4f71e0bae8db508b0571b900d1827d
Liam Ilan <snowboardsheep@gmail.com>
2024-12-18 01:58:06 -0800

    Setup sil_setTime

--- firmware/quadruna/sil/src/manager/main.c
+++ firmware/quadruna/sil/src/manager/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <czmq.h>
+#include <stdatomic.h>
 
 typedef struct
 {
@@ -9,11 +10,23 @@ typedef struct
     uint32_t    timeMs;
 } Board;
 
+Board boards[] = {
+    { .name = "FSM", .binPath = "./build_fw_sil/firmware/quadruna/FSM/quadruna_FSM_sil", .timeMs = 0 },
+    { .name = "RSM", .binPath = "./build_fw_sil/firmware/quadruna/RSM/quadruna_RSM_sil", .timeMs = 0 },
+    { .name = "BMS", .binPath = "./build_fw_sil/firmware/quadruna/BMS/quadruna_BMS_sil", .timeMs = 0 },
+    { .name = "CRIT", .binPath = "./build_fw_sil/firmware/quadruna/CRIT/quadruna_CRIT_sil", .timeMs = 0 },
+    { .name = "VC", .binPath = "./build_fw_sil/firmware/quadruna/VC/quadruna_VC_sil", .timeMs = 0 },
+};
+size_t boardCount = sizeof(boards) / sizeof(boards[0]);
+
 // Store socket poll, and proxy pointers for graceful exit.
 static zactor_t  *proxy;
 static zsock_t   *socketTx;
 static zsock_t   *socketRx;
 static zpoller_t *pollerRx;
+
+// Store global reference for time.
+uint32_t timeMs = 0;
 
 // Utility function to convert char * to uint32_t.
 // Returns 0 if invalid input.
@@ -131,18 +144,89 @@ void exitHandler()
     zsock_destroy(&socketRx);
 }
 
+// Set the time to a given target.
+// Will block until the target is reached.
+// If a target lower than the current time is provided, will fail on assertion.
+void sil_setTime(uint32_t targetMs)
+{
+    assert(targetMs >= timeMs);
+    timeMs = targetMs;
+
+    // Make a request to all the boards to set the time to targetMs.
+    zsock_send(socketTx, "s4", "time_req", targetMs);
+
+    // Block until the the request is satisfied, reading all incomming messages in the process.
+    // Every loop either:
+    //  - Captures a message to update records.
+    //  - Checks the records for times matching the target.
+    for (;;)
+    {
+        // zpoller_wait returns reference to the socket that is ready to recieve, or NULL.
+        // there is only one such socket attachted to pollerRx, which is socketRx.
+        // Since zpoller_wait may technically also return a zactor,
+        // CZMQ common practice is to make this a void pointer.
+        void *socket = zpoller_wait(pollerRx, 1);
+        if (socket != NULL)
+        {
+            // Receive the message.
+            char   *topic;
+            zmsg_t *zmqMsg;
+
+            if (zsock_recv(socket, "sm", &topic, &zmqMsg) == -1)
+            {
+                perror("Error: Failed to receive on socket");
+            }
+            else if (strcmp(topic, "time_resp") == 0)
+            {
+                // time_resp topic.
+                // Message are built in the following order:
+                //  1) Name of board.
+                //  2) Time in ms.
+
+                // Extract board name.
+                char *receivedBoardName = zmsg_popstr(zmqMsg);
+                if (receivedBoardName != NULL)
+                {
+                    // If successful, extract time in ms.
+                    char *receivedTimeMsStr = zmsg_popstr(zmqMsg);
+                    if (receivedTimeMsStr != NULL)
+                    {
+                        // If successful, convert time to uint32_t.
+                        uint32_t receivedTimeMs = sil_atoiUint32(receivedTimeMsStr);
+
+                        // Update record.
+                        for (size_t boardIndex = 0; boardIndex < boardCount; boardIndex += 1)
+                            if (strcmp(receivedBoardName, boards[boardIndex].name) == 0)
+                                boards[boardIndex].timeMs = receivedTimeMs;
+
+                        free(receivedTimeMsStr);
+                    }
+
+                    free(receivedBoardName);
+                }
+            }
+
+            // Free up zmq-allocated memory.
+            free(topic);
+            zmsg_destroy(&zmqMsg);
+        }
+        else
+        {
+            // If we are not receiving messages, check if all the boards have passed the target time.
+            bool boardsHitTargetTime = true;
+            for (size_t boardIndex = 0; boardIndex < boardCount; boardIndex += 1)
+                boardsHitTargetTime &= boards[boardIndex].timeMs >= targetMs;
+
+            // If they have, break out.
+            if (boardsHitTargetTime)
+                break;
+        }
+    }
+}
+
 int main()
 {
     printf("Starting up SIL Manager\n");
-
-    Board boards[] = {
-        { .name = "FSM", .binPath = "./build_fw_sil/firmware/quadruna/FSM/quadruna_FSM_sil", .timeMs = 0 },
-        { .name = "RSM", .binPath = "./build_fw_sil/firmware/quadruna/RSM/quadruna_RSM_sil", .timeMs = 0 },
-        { .name = "BMS", .binPath = "./build_fw_sil/firmware/quadruna/BMS/quadruna_BMS_sil", .timeMs = 0 },
-        { .name = "CRIT", .binPath = "./build_fw_sil/firmware/quadruna/CRIT/quadruna_CRIT_sil", .timeMs = 0 },
-        { .name = "VC", .binPath = "./build_fw_sil/firmware/quadruna/VC/quadruna_VC_sil", .timeMs = 0 },
-    };
-    size_t boardCount = sizeof(boards) / sizeof(boards[0]);
 
     // See https://zguide.zeromq.org/docs/chapter2/#The-Dynamic-Discovery-Problem - Figure 13.
     // Normal PUB/SUB zmq architectures support one-to-many and many-to-one.
@@ -203,68 +287,8 @@ int main()
         sil_board_run(&boards[boardIndex]);
     }
 
-    // Keep track of time.
-    uint32_t timeMs = 0;
-
-    // Aim to set time to 1000 ms.
-    zsock_send(socketTx, "s4", "time_req", 1000);
-
-    // Main loop for manager.
-    for (;;)
-    {
-        // zpoller_wait returns reference to the socket that is ready to recieve, or NULL.
-        // there is only one such socket attachted to pollerRx, which is socketRx.
-        // Since zpoller_wait may technically also return a zactor,
-        // CZMQ common practice is to make this a void pointer.
-        void *socket = zpoller_wait(pollerRx, 1);
-        if (socket != NULL)
-        {
-            // Receive the message.
-            char   *topic;
-            zmsg_t *zmqMsg;
-
-            if (zsock_recv(socket, "sm", &topic, &zmqMsg) == -1)
-            {
-                perror("Error: Failed to receive on socket");
-            }
-            else if (strcmp(topic, "time_resp") == 0)
-            {
-                // time_resp topic.
-                // Message are built in the following order:
-                //  1) Name of board.
-                //  2) Time in ms.
-
-                // Extract board name.
-                char *receivedBoardName = zmsg_popstr(zmqMsg);
-                if (receivedBoardName != NULL)
-                {
-                    // If successful, extract time in ms.
-                    char *receivedTimeMsStr = zmsg_popstr(zmqMsg);
-                    if (receivedTimeMsStr != NULL)
-                    {
-                        // If successful, convert time to uint32_t.
-                        uint32_t receivedTimeMs = sil_atoiUint32(receivedTimeMsStr);
-
-                        // Update record.
-                        for (size_t boardIndex = 0; boardIndex < boardCount; boardIndex += 1)
-                            if (strcmp(receivedBoardName, boards[boardIndex].name) == 0)
-                                boards[boardIndex].timeMs = receivedTimeMs;
-
-                        free(receivedTimeMsStr);
-                    }
-
-                    free(receivedBoardName);
-                }
-            }
-
-            // Free up zmq-allocated memory.
-            free(topic);
-            zmsg_destroy(&zmqMsg);
-        }
-        else
-        {
-            for (size_t boardIndex = 0; boardIndex < boardCount; boardIndex += 1)
-                printf("%s: %d\n", boards[boardIndex].name, boards[boardIndex].timeMs);
-        }
-    }
+    sil_setTime(1000);
+    printf("\n--- Sleeping for 5 s ---\n");
+    zclock_sleep(5000);
+    sil_setTime(2000);
 }
