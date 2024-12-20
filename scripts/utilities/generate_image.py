4c1183f631a58a64c7aea26b0143a9af7789d1de
Jugveer-Sandher <jugveersandher@gmail.com>
2024-12-19 17:26:00 -0800

    h7 loading...

--- scripts/utilities/generate_image.py
+++ scripts/utilities/generate_image.py
@@ -57,13 +57,16 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--app-hex", type=str, required=True)
     parser.add_argument("--boot-hex", type=str, required=True)
+    parser.add_argument("--loader-hex", type=str, required=True)
     parser.add_argument("--app-metadata-hex-out", type=str, required=True)
     parser.add_argument("--image-hex-out", type=str, required=True)
+
     args = parser.parse_args()
 
     crc32 = Crc32()
     app_hex = intelhex.IntelHex(args.app_hex)
     boot_hex = intelhex.IntelHex(args.boot_hex)
+    loader_hex = intelhex.IntelHex(args.loader_hex)
 
     # Add checksum to app file metadata, so the bootloader can verify the chip has a valid
     # binary before booting. Our bootloader programs 8 bytes at a time (2 4-byte words), so
@@ -85,6 +88,7 @@ if __name__ == "__main__":
         app_hex.tofile(file, format="hex")
 
     # Merge app with metadata and bootloader, creating the final image.
+    app_hex.merge(loader_hex, overlap="replace")
     app_hex.merge(boot_hex, overlap="replace")
     with open(args.image_hex_out, "w") as file:
         app_hex.tofile(file, format="hex")
