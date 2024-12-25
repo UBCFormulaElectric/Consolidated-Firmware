80eb078d49b2d747c4fae0b57e9601dc388dac61
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2024-12-25 15:57:45 -0500

    ioc updates

--- firmware/quintuna/VC/boot/cubemx/Src/stm32h7xx_it.c
+++ firmware/quintuna/VC/boot/cubemx/Src/stm32h7xx_it.c
@@ -187,6 +187,21 @@ void FDCAN1_IT1_IRQHandler(void)
 
     /* USER CODE END FDCAN1_IT1_IRQn 1 */
 }
+
+/**
+ * @brief This function handles TIM6 global interrupt, DAC1_CH1 and DAC1_CH2 underrun error interrupts.
+ */
+void TIM6_DAC_IRQHandler(void)
+{
+    /* USER CODE BEGIN TIM6_DAC_IRQn 0 */
+
+    /* USER CODE END TIM6_DAC_IRQn 0 */
+    HAL_TIM_IRQHandler(&htim6);
+    /* USER CODE BEGIN TIM6_DAC_IRQn 1 */
+
+    /* USER CODE END TIM6_DAC_IRQn 1 */
+}
+
 /* USER CODE BEGIN 1 */
 
 /* USER CODE END 1 */
