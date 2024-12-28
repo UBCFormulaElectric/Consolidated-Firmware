65ea33fe27a8a5f934ad0323ebc67b0f46463c4a
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2024-12-28 00:59:04 -0500

    formatting

--- firmware/shared/src/app/app_heartbeatMonitor.h
+++ firmware/shared/src/app/app_heartbeatMonitor.h
@@ -4,19 +4,19 @@
 
 typedef struct
 {
-  HeartbeatMonitorBoard * boards;
-  uint8_t board_count;
-  bool block_faults;
-  void (*self_heartbeater)(bool);
+    HeartbeatMonitorBoard *boards;
+    uint8_t                board_count;
+    bool                   block_faults;
+    void (*self_heartbeater)(bool);
 } HeartbeatMonitor;
 
-void app_heartbeatMonitor_checkIn(const HeartbeatMonitor * hbm);
+void app_heartbeatMonitor_checkIn(const HeartbeatMonitor *hbm);
 
-void app_heartbeatMonitor_broadcastFaults(const HeartbeatMonitor* hbm);
+void app_heartbeatMonitor_broadcastFaults(const HeartbeatMonitor *hbm);
 
-bool app_heartbeatMonitor_isSendingMissingHeartbeatFault(const HeartbeatMonitor* hbm);
+bool app_heartbeatMonitor_isSendingMissingHeartbeatFault(const HeartbeatMonitor *hbm);
 
 #ifdef TARGET_TEST
-void app_heartbeatMonitor_clearFaults(const HeartbeatMonitor* hbm);
+void app_heartbeatMonitor_clearFaults(const HeartbeatMonitor *hbm);
 void app_heartbeatMonitor_blockFaults(HeartbeatMonitor *hbm, bool block_faults_init);
 #endif
