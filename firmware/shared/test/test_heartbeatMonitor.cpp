6939b2d4d85d8a24fa99c75062896a1278ca8c79
Edwin <20777515+Lucien950@users.noreply.github.com>
2024-12-28 16:15:31 -0800

    Heartbeat Refactor (#1325)
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    - Changed structure of heartbeat monitor config. (see
    `firmware/shared/src/app/app_heartbeatMonitorBoard.h`)
    - Gives sturcture towards future app level refactor
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    - fixed tests for all boards
    - adapted shared heartbeat monitor to new structure
    
    ### Resolved Tickets
    <!-- Link any tickets that this PR resolves. -->

--- firmware/shared/test/test_heartbeatMonitor.cpp
+++ firmware/shared/test/test_heartbeatMonitor.cpp
@@ -1,324 +1,85 @@
 #include <gtest/gtest.h>
-
 #include "fake_io_time.hpp"
-
 extern "C"
 {
-#include "app_heartbeatBoardsEnum.h"
-#include "app_heartbeatMonitor.h"
+#include "app_heartbeatBoard.h"
 }
 
 // fake can states
-bool E_fake_can_fault_state = false;
-bool A_fake_can_fault_state = false;
+bool           MOCK_BOARD_CAN_FAULT     = false;
+bool           MOCK_CAN_HEARTBEAT_STATE = false;
+uint8_t        timeout_ms               = 200;
+HeartbeatBoard a_hbmonitor              = {
+                 .timeout_ms = timeout_ms,
+                 .getter     = []() { return MOCK_CAN_HEARTBEAT_STATE; },
+    .resetter                           = [](bool v) { MOCK_CAN_HEARTBEAT_STATE = v; },
+    .fault_setter                       = [](bool v) { MOCK_BOARD_CAN_FAULT = v; },
+    .fault_getter                       = []() { return MOCK_BOARD_CAN_FAULT; },
+};
 
-bool E_fake_can_heartbeat_state = false;
-bool A_fake_can_heartbeat_state = false;
-bool D_fake_can_heartbeat_state = false;
-
-// fake can methods
-void E_fakeCanHeartbeatUpdater(bool val)
+class HeartbeatMonitorTest : public testing::Test
 {
-    E_fake_can_heartbeat_state = val;
-}
+  protected:
+    void SetUp() override { app_heartbeatBoard_init(&a_hbmonitor); }
+};
 
-void A_fakeCanHeartbeatUpdater(bool val)
+TEST_F(HeartbeatMonitorTest, test_create)
 {
-    A_fake_can_heartbeat_state = val;
-}
-
-void D_fakeCanHeartbeatSetter(bool val)
-{
-    D_fake_can_heartbeat_state = val;
-}
-
-void E_fakeCanFaultSetter(bool val)
-{
-    E_fake_can_fault_state = val;
-}
-
-void A_fakeCanFaultSetter(bool val)
-{
-    A_fake_can_fault_state = val;
-}
-
-bool E_fakeCanFaultGetter()
-{
-    return E_fake_can_fault_state;
-}
-
-bool A_fakeCanFaultGetter()
-{
-    return A_fake_can_fault_state;
-}
-
-bool E_fakeCanHeartbeatGetter()
-{
-    return E_fake_can_heartbeat_state;
-}
-
-bool A_fakeCanHeartbeatGetter()
-{
-    return A_fake_can_heartbeat_state;
-}
-
-// callback config structs to be passed in to methods later on
-void (*fault_can_setters[HEARTBEAT_BOARD_COUNT])(bool) = { [A_HEARTBEAT_BOARD] = &A_fakeCanFaultSetter,
-                                                           [B_HEARTBEAT_BOARD] = NULL,
-                                                           [C_HEARTBEAT_BOARD] = NULL,
-                                                           [D_HEARTBEAT_BOARD] = NULL,
-                                                           [E_HEARTBEAT_BOARD] = &E_fakeCanFaultSetter };
-
-void (*heartbeat_can_updaters[HEARTBEAT_BOARD_COUNT])(bool) = { [A_HEARTBEAT_BOARD] = &A_fakeCanHeartbeatUpdater,
-                                                                [B_HEARTBEAT_BOARD] = NULL,
-                                                                [C_HEARTBEAT_BOARD] = NULL,
-                                                                [D_HEARTBEAT_BOARD] = NULL,
-                                                                [E_HEARTBEAT_BOARD] = &E_fakeCanHeartbeatUpdater };
-
-bool (*heartbeat_can_getters[HEARTBEAT_BOARD_COUNT])() = { [A_HEARTBEAT_BOARD] = &A_fakeCanHeartbeatGetter,
-                                                           [B_HEARTBEAT_BOARD] = NULL,
-                                                           [C_HEARTBEAT_BOARD] = NULL,
-                                                           [D_HEARTBEAT_BOARD] = NULL,
-                                                           [E_HEARTBEAT_BOARD] = &E_fakeCanHeartbeatGetter };
-
-bool (*fault_can_getters[HEARTBEAT_BOARD_COUNT])() = { [A_HEARTBEAT_BOARD] = &A_fakeCanFaultGetter,
-                                                       [B_HEARTBEAT_BOARD] = NULL,
-                                                       [C_HEARTBEAT_BOARD] = NULL,
-                                                       [D_HEARTBEAT_BOARD] = NULL,
-                                                       [E_HEARTBEAT_BOARD] = &E_fakeCanFaultGetter };
-
-TEST(HeartbeatMonitor, test_check_faults)
-{
-    /* test check faults */
-    // this hypothetical (fake) board only checks the A and E, and is the D
-    bool heartbeats_to_check[HEARTBEAT_BOARD_COUNT];
-    for (bool &board : heartbeats_to_check)
-    {
-        board = false;
-    }
-
-    heartbeats_to_check[A_HEARTBEAT_BOARD] = true;
-    heartbeats_to_check[E_HEARTBEAT_BOARD] = true;
-
-    app_heartbeatMonitor_init(
-        heartbeats_to_check, heartbeat_can_getters, heartbeat_can_updaters, &D_fakeCanHeartbeatSetter,
-        fault_can_setters, fault_can_getters);
-
-    // check that there are no faults
-    E_fake_can_fault_state = false;
-    A_fake_can_fault_state = false;
-
-    ASSERT_EQ(app_heartbeatMonitor_isSendingMissingHeartbeatFault(), false);
-
-    // check that if there is a fault, it is detected
-    E_fake_can_fault_state = false;
-    A_fake_can_fault_state = true;
-
-    ASSERT_EQ(app_heartbeatMonitor_isSendingMissingHeartbeatFault(), true);
-}
-
-TEST(HeartbeatMonitor, test_create)
-{
-    /* test heartbeat monitor create */
-
-    // this hypothetical (fake) board only checks the A and E, and is the D
-    bool heartbeats_to_check[HEARTBEAT_BOARD_COUNT];
-    for (bool &board : heartbeats_to_check)
-    {
-        board = false;
-    }
-
-    heartbeats_to_check[A_HEARTBEAT_BOARD] = true;
-    heartbeats_to_check[E_HEARTBEAT_BOARD] = true;
-
-    // create and assert time fields work
-    app_heartbeatMonitor_init(
-        heartbeats_to_check, heartbeat_can_getters, heartbeat_can_updaters, &D_fakeCanHeartbeatSetter,
-        fault_can_setters, fault_can_getters);
-    HeartbeatMonitor *heartbeat_monitor = app_heartbeatMonitor_get();
-
-    // check self heartbeat setter
-    ASSERT_EQ(heartbeat_monitor->self_checkin, &D_fakeCanHeartbeatSetter);
-
     // assert status, checked in, to check, getters, updaters, and fault setters all set correctly
-    for (int board = 0; board < HEARTBEAT_BOARD_COUNT; board++)
-    {
-        ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[board]);
-        ASSERT_FALSE(heartbeat_monitor->status[board]);
-        ASSERT_EQ(heartbeat_monitor->is_watching_heartbeat_for[board], heartbeats_to_check[board]);
-
-        ASSERT_EQ(heartbeat_monitor->getters[board], heartbeat_can_getters[board]);
-        ASSERT_EQ(heartbeat_monitor->resetters[board], heartbeat_can_updaters[board]);
-        ASSERT_EQ(heartbeat_monitor->fault_setters[board], fault_can_setters[board]);
-    }
+    ASSERT_FALSE(a_hbmonitor.status);
+    ASSERT_FALSE(a_hbmonitor.heartbeat_checked_in);
+    ASSERT_EQ(a_hbmonitor.timer.state, TIMER_STATE_EXPIRED);
+    ASSERT_TRUE(a_hbmonitor.fault_getter());
 }
 
-TEST(HeartbeatMonitor, test_broadcast_faults)
+TEST_F(HeartbeatMonitorTest, test_broadcast_faults)
 {
-    /* test broadcast faults */
-    // initialize monitor
-
-    bool heartbeats_to_check[HEARTBEAT_BOARD_COUNT] = { [A_HEARTBEAT_BOARD] = true,
-                                                        [B_HEARTBEAT_BOARD] = false,
-                                                        [C_HEARTBEAT_BOARD] = false,
-                                                        [D_HEARTBEAT_BOARD] = false,
-                                                        [E_HEARTBEAT_BOARD] = true };
-
-    app_heartbeatMonitor_init(
-        heartbeats_to_check, heartbeat_can_getters, heartbeat_can_updaters, &D_fakeCanHeartbeatSetter,
-        fault_can_setters, fault_can_getters);
-    HeartbeatMonitor *heartbeat_monitor = app_heartbeatMonitor_get();
-
     // broadcast all good
-    for (bool &status : heartbeat_monitor->status)
-    {
-        status = true;
-    }
-
-    app_heartbeatMonitor_broadcastFaults();
-
-    ASSERT_FALSE(E_fake_can_fault_state);
-    ASSERT_FALSE(A_fake_can_fault_state);
-
-    // broadcast only A and E good (should be indistinguishable from all good)
-    for (bool &statu : heartbeat_monitor->status)
-    {
-        statu = false;
-    }
-    heartbeat_monitor->status[E_HEARTBEAT_BOARD] = true;
-    heartbeat_monitor->status[A_HEARTBEAT_BOARD] = true;
-    app_heartbeatMonitor_broadcastFaults();
-
-    ASSERT_FALSE(E_fake_can_fault_state);
-    ASSERT_FALSE(A_fake_can_fault_state);
-
-    // broadcast only A good (should trigger E fault)
-    for (bool &status : heartbeat_monitor->status)
-    {
-        status = false;
-    }
-    heartbeat_monitor->status[E_HEARTBEAT_BOARD] = false;
-    heartbeat_monitor->status[A_HEARTBEAT_BOARD] = true;
-    app_heartbeatMonitor_broadcastFaults();
-
-    ASSERT_TRUE(E_fake_can_fault_state);
-    ASSERT_FALSE(A_fake_can_fault_state);
-
+    a_hbmonitor.status = true;
+    app_heartbeatBoard_broadcastFaults(&a_hbmonitor);
+    ASSERT_FALSE(MOCK_BOARD_CAN_FAULT);
     // broadcast all bad
-    for (bool &status : heartbeat_monitor->status)
-    {
-        status = false;
-    }
-    app_heartbeatMonitor_broadcastFaults();
-
-    ASSERT_TRUE(E_fake_can_fault_state);
-    ASSERT_TRUE(A_fake_can_fault_state);
+    a_hbmonitor.status = false;
+    app_heartbeatBoard_broadcastFaults(&a_hbmonitor);
+    ASSERT_TRUE(MOCK_BOARD_CAN_FAULT);
 }
 
-TEST(HeartbeatMonitor, test_check_in_and_tick)
+TEST_F(HeartbeatMonitorTest, test_check_in_and_tick)
 {
     /* test tick and check in functions*/
     fake_io_time_getCurrentMs_returns(0);
 
-    // our hypothetical D board relies on A and E
-    bool heartbeats_to_check[HEARTBEAT_BOARD_COUNT];
-    for (bool &board : heartbeats_to_check)
-    {
-        board = false;
-    }
-
-    heartbeats_to_check[A_HEARTBEAT_BOARD] = true;
-    heartbeats_to_check[E_HEARTBEAT_BOARD] = true;
-
-    app_heartbeatMonitor_init(
-        heartbeats_to_check, heartbeat_can_getters, heartbeat_can_updaters, &D_fakeCanHeartbeatSetter,
-        fault_can_setters, fault_can_getters);
-    HeartbeatMonitor *heartbeat_monitor = app_heartbeatMonitor_get();
-
     // assert nothing changed (0 ms)
-    app_heartbeatMonitor_checkIn();
+    app_heartbeatBoard_checkIn(&a_hbmonitor);
+    ASSERT_FALSE(a_hbmonitor.heartbeat_checked_in);
+    ASSERT_FALSE(a_hbmonitor.status);
 
-    ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[A_HEARTBEAT_BOARD]);
-    ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[B_HEARTBEAT_BOARD]);
-    ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[C_HEARTBEAT_BOARD]);
-    ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[D_HEARTBEAT_BOARD]);
-    ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[E_HEARTBEAT_BOARD]);
+    MOCK_CAN_HEARTBEAT_STATE = true;
+    app_heartbeatBoard_checkIn(&a_hbmonitor);
+    ASSERT_TRUE(a_hbmonitor.heartbeat_checked_in);
+    ASSERT_TRUE(a_hbmonitor.status);
 
-    ASSERT_EQ(heartbeat_monitor->is_watching_heartbeat_for[A_HEARTBEAT_BOARD], heartbeats_to_check[A_HEARTBEAT_BOARD]);
-    ASSERT_EQ(heartbeat_monitor->is_watching_heartbeat_for[B_HEARTBEAT_BOARD], heartbeats_to_check[B_HEARTBEAT_BOARD]);
-    ASSERT_EQ(heartbeat_monitor->is_watching_heartbeat_for[C_HEARTBEAT_BOARD], heartbeats_to_check[C_HEARTBEAT_BOARD]);
-    ASSERT_EQ(heartbeat_monitor->is_watching_heartbeat_for[D_HEARTBEAT_BOARD], heartbeats_to_check[D_HEARTBEAT_BOARD]);
-    ASSERT_EQ(heartbeat_monitor->is_watching_heartbeat_for[E_HEARTBEAT_BOARD], heartbeats_to_check[E_HEARTBEAT_BOARD]);
+    // after a checkin, the CAN table should be reset to false
+    app_heartbeatBoard_checkIn(&a_hbmonitor);
+    ASSERT_FALSE(a_hbmonitor.heartbeat_checked_in);
+    ASSERT_TRUE(a_hbmonitor.status);
 
-    // confirm status bad for non-null heartbeat boards
-    ASSERT_FALSE(heartbeat_monitor->status[A_HEARTBEAT_BOARD]);
-    ASSERT_FALSE(heartbeat_monitor->status[E_HEARTBEAT_BOARD]);
-    ASSERT_TRUE(heartbeat_monitor->status[B_HEARTBEAT_BOARD]);
-    ASSERT_TRUE(heartbeat_monitor->status[C_HEARTBEAT_BOARD]);
-    ASSERT_TRUE(heartbeat_monitor->status[D_HEARTBEAT_BOARD]);
+    // right before the timer expires, the status should still be fine
+    fake_io_time_getCurrentMs_returns(timeout_ms - 1);
+    app_heartbeatBoard_checkIn(&a_hbmonitor);
+    ASSERT_FALSE(a_hbmonitor.heartbeat_checked_in);
+    ASSERT_TRUE(a_hbmonitor.status);
 
-    // check in A and E midway (150 ms)
-    fake_io_time_getCurrentMs_returns(150);
+    // only after the timeout, should the status be false
+    fake_io_time_getCurrentMs_returns(timeout_ms);
+    app_heartbeatBoard_checkIn(&a_hbmonitor);
+    ASSERT_FALSE(a_hbmonitor.heartbeat_checked_in);
+    ASSERT_FALSE(a_hbmonitor.status);
 
-    E_fake_can_heartbeat_state = true;
-    A_fake_can_heartbeat_state = true;
-    D_fake_can_heartbeat_state = false;
-
-    app_heartbeatMonitor_checkIn();
-
-    ASSERT_TRUE(D_fake_can_heartbeat_state);
-    ASSERT_FALSE(E_fake_can_heartbeat_state);
-    ASSERT_FALSE(A_fake_can_heartbeat_state);
-
-    ASSERT_TRUE(heartbeat_monitor->heartbeats_checked_in[A_HEARTBEAT_BOARD]);
-    ASSERT_TRUE(heartbeat_monitor->heartbeats_checked_in[E_HEARTBEAT_BOARD]);
-
-    // progress to timeout period and verify all good (300 ms)
-    fake_io_time_getCurrentMs_returns(300);
-    app_heartbeatMonitor_checkIn();
-
-    ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[A_HEARTBEAT_BOARD]);
-    ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[B_HEARTBEAT_BOARD]);
-    ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[C_HEARTBEAT_BOARD]);
-    ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[D_HEARTBEAT_BOARD]);
-    ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[E_HEARTBEAT_BOARD]);
-
-    ASSERT_TRUE(heartbeat_monitor->status[A_HEARTBEAT_BOARD]);
-    ASSERT_TRUE(heartbeat_monitor->status[B_HEARTBEAT_BOARD]);
-    ASSERT_TRUE(heartbeat_monitor->status[C_HEARTBEAT_BOARD]);
-    ASSERT_TRUE(heartbeat_monitor->status[D_HEARTBEAT_BOARD]);
-    ASSERT_TRUE(heartbeat_monitor->status[E_HEARTBEAT_BOARD]);
-
-    // check in E midway (450 ms)
-    fake_io_time_getCurrentMs_returns(450);
-
-    E_fake_can_heartbeat_state = true;
-    A_fake_can_heartbeat_state = false;
-    D_fake_can_heartbeat_state = false;
-
-    app_heartbeatMonitor_checkIn();
-
-    ASSERT_TRUE(D_fake_can_heartbeat_state);
-    ASSERT_FALSE(E_fake_can_heartbeat_state);
-    ASSERT_FALSE(A_fake_can_heartbeat_state);
-
-    ASSERT_TRUE(heartbeat_monitor->heartbeats_checked_in[E_HEARTBEAT_BOARD]);
-    ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[A_HEARTBEAT_BOARD]);
-
-    // progress to timeout period and verify A missing (600 ms)
-    fake_io_time_getCurrentMs_returns(650);
-    app_heartbeatMonitor_checkIn();
-
-    ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[A_HEARTBEAT_BOARD]);
-    ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[B_HEARTBEAT_BOARD]);
-    ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[C_HEARTBEAT_BOARD]);
-    ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[D_HEARTBEAT_BOARD]);
-    ASSERT_FALSE(heartbeat_monitor->heartbeats_checked_in[E_HEARTBEAT_BOARD]);
-
-    // A not checked in returns false
-    ASSERT_FALSE(heartbeat_monitor->status[A_HEARTBEAT_BOARD]);
-    ASSERT_TRUE(heartbeat_monitor->status[B_HEARTBEAT_BOARD]);
-    ASSERT_TRUE(heartbeat_monitor->status[C_HEARTBEAT_BOARD]);
-    ASSERT_TRUE(heartbeat_monitor->status[D_HEARTBEAT_BOARD]);
-    ASSERT_TRUE(heartbeat_monitor->status[E_HEARTBEAT_BOARD]);
+    // with one checkin, the status should be fine again
+    fake_io_time_getCurrentMs_returns(timeout_ms + 1);
+    MOCK_CAN_HEARTBEAT_STATE = true;
+    app_heartbeatBoard_checkIn(&a_hbmonitor);
+    ASSERT_TRUE(a_hbmonitor.heartbeat_checked_in);
+    ASSERT_TRUE(a_hbmonitor.status);
 }
