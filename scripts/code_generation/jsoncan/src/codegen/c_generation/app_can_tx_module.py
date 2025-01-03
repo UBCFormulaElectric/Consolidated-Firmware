b85e460eeab0173fc3458db194f31f0ca2707388
Jiajun-Huang <jhuang430@qq.com>
2025-01-02 23:58:50 -0800

    canTx header done

--- scripts/code_generation/jsoncan/src/codegen/c_generation/app_can_tx_module.py
+++ scripts/code_generation/jsoncan/src/codegen/c_generation/app_can_tx_module.py
@@ -1,4 +1,7 @@
+import jinja2 as j2
+
 from .c_writer import *
+from .utils import load_template
 
 
 class AppCanTxModule(CModule):
@@ -171,3 +174,15 @@ class AppCanTxModule(CModule):
             cw.add_line()
 
         return str(cw)
+
+    def header_template(self):
+        template = load_template("app_canTx.h.j2")
+        j2_env = j2.Environment(loader=j2.BaseLoader, extensions=['jinja2.ext.loopcontrols'])
+        template = j2_env.from_string(template)
+        return template.render(messages=self._db.tx_msgs_for_node(self._node))
+    
+    def source_template(self):
+        template = load_template("app_canTx.c.j2")
+        j2_env = j2.Environment(loader=j2.BaseLoader, extensions=['jinja2.ext.loopcontrols'])
+        template = j2_env.from_string(template)
+        return template.render(source=self.source())
