b952aa908174562fabb4504855cb285d2f723621
Jiajun-Huang <jhuang430@qq.com>
2025-01-02 21:44:07 -0800

    complete util c pack

--- scripts/code_generation/jsoncan/src/codegen/c_generation/app_can_utils_module.py
+++ scripts/code_generation/jsoncan/src/codegen/c_generation/app_can_utils_module.py
@@ -1,6 +1,8 @@
-from .utils import load_template
-from .c_writer import *
 import jinja2 as j2
+
+from .c_writer import *
+from .utils import load_template
+
 PACK_SHIFT_LEFT_FUNC = "packShiftLeft"
 PACK_SHIFT_RIGHT_FUNC = "packShiftRight"
 UNPACK_SHIFT_LEFT_FUNC = "unpackShiftLeft"
@@ -14,6 +16,34 @@ SIGNED_DECODE_MACRO = "SIGNED_DECODE"
 PACK_TEMPLATE = "out_data[{byte_index}] |= {func}({value}, {shift}, {mask});   // Packs bits {bits_comment} of byte {byte_index}"
 UNPACK_TEMPLATE = "{var_name} |= {func}({value}, {shift}, {mask});   // Unpacks bits {bits_comment} of msg byte {byte_index}"
 
+def calculate_packing_iterations(signal):
+    packed_bits = 0
+    iterations = []
+    bit_start = signal.start_bit
+    starting_byte = signal.start_bit // BYTE_SIZE
+
+    while packed_bits < signal.bits:
+        bits_to_pack = min(BYTE_SIZE - (bit_start % BYTE_SIZE), signal.bits - packed_bits)
+        shift = packed_bits - (bit_start % BYTE_SIZE)
+        mask = (1 << bits_to_pack) - 1 << (bit_start % BYTE_SIZE)
+        mask_text = f"0x{mask:X}"
+        comment_data = ["_"] * BYTE_SIZE
+        for i in range(bit_start % BYTE_SIZE, (bit_start % BYTE_SIZE) + bits_to_pack):
+            comment_data[i] = "#"
+
+        iterations.append({
+            "starting_byte": starting_byte,
+            "shift": shift,
+            "mask_text": mask_text,
+            "comment_data": "".join(reversed(comment_data)),
+            "bits_to_pack": bits_to_pack,
+        })
+
+        packed_bits += bits_to_pack
+        bit_start += bits_to_pack
+        starting_byte += 1
+
+    return iterations
 
 class AppCanUtilsModule(CModule):
     def __init__(self, db: CanDatabase, node: str):
@@ -376,7 +406,14 @@ class AppCanUtilsModule(CModule):
         return str(cw)
     
     def source_template(self):
-        pass
+        template = load_template("app_canUtils.c.j2")
+        j2_env = j2.Environment(loader=j2.BaseLoader, extensions=['jinja2.ext.loopcontrols'])
+        template = j2_env.from_string(template)
+        return template.render(
+            messages = self._db.msgs_for_node(self._node),
+            signal_placement_comment = signal_placement_comment,
+            iterations = calculate_packing_iterations
+        )
     
     def header_template(self):
         can_enums = self._db.shared_enums
@@ -384,12 +421,9 @@ class AppCanUtilsModule(CModule):
             for signal in msg.signals:
                 if signal.enum and signal.enum not in can_enums:
                     can_enums.append(signal.enum)
-        pwd = os.getcwd() 
-        files = []
+                    
         template = load_template("app_canUtils.h.j2")
         j2_env = j2.Environment(loader=j2.BaseLoader)
-        j2_env = j2.Environment(loader=j2.BaseLoader)
-        j2_env.filters['pascal_to_screaming_snake_case'] = pascal_to_screaming_snake_case
         template = j2_env.from_string(template)
         
         return template.render(
