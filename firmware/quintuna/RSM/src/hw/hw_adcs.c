31999d4e76318960230288e0282398a73ccebdc2
rijwal <rijwalsangey@gmail.com>
2025-01-06 19:04:14 -0800

    formatting

--- firmware/quintuna/RSM/src/hw/hw_adcs.c
+++ firmware/quintuna/RSM/src/hw/hw_adcs.c
@@ -6,19 +6,18 @@
 
 static uint16_t raw_adc_values[NUM_ADC_CHANNELS];
 static float    adc_voltages[NUM_ADC_CHANNELS];
-const AdcChip adc1 = { &hadc1 };
+const AdcChip   adc1 = { &hadc1 };
 
-const Adc acc_fan_i_sns        = {&adc_voltages[0]};
-const Adc rad_fan_i_sns        = {&adc_voltages[1]};
-const Adc coolantpressure1_3v3 = {&adc_voltages[2]};
-const Adc coolantpressure2_3v3 = {&adc_voltages[3]};
-const Adc lc3_out              = {&adc_voltages[4]};
-const Adc susp_travel_rr_3v3   = {&adc_voltages[5]};
-const Adc susp_travel_rl_3v3   = {&adc_voltages[6]};
-const Adc coolanttemp2_3v3     = {&adc_voltages[7]};
-const Adc coolanttemp1_3v3     = {&adc_voltages[8]};
-const Adc regen_3v3_lc3_out    = {&adc_voltages[9]};
-
+const Adc acc_fan_i_sns        = { &adc_voltages[0] };
+const Adc rad_fan_i_sns        = { &adc_voltages[1] };
+const Adc coolantpressure1_3v3 = { &adc_voltages[2] };
+const Adc coolantpressure2_3v3 = { &adc_voltages[3] };
+const Adc lc3_out              = { &adc_voltages[4] };
+const Adc susp_travel_rr_3v3   = { &adc_voltages[5] };
+const Adc susp_travel_rl_3v3   = { &adc_voltages[6] };
+const Adc coolanttemp2_3v3     = { &adc_voltages[7] };
+const Adc coolanttemp1_3v3     = { &adc_voltages[8] };
+const Adc regen_3v3_lc3_out    = { &adc_voltages[9] };
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *hadc)
 {
