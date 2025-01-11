04f8da5c497367442d75654da69e9d08149ead6a
Setare Maleki <72472685+setaremalekiii@users.noreply.github.com>
2025-01-11 11:16:20 -0800

    USB Drivers (#1404)
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    added a hw_usb layer, which contains CDC class USB calls for
    transmitting and receiving function calls.
    initialized a queue in the cdc_recieve function to place the information
    received under the hood into the queue one byte at a time
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    tested the receive by sending a message through python client
    tested the transmit function by sending a message and receiving it on
    the python end
    tested hot-plugging the USB device and seeing if the status is changing 
    
    ### Resolved Tickets
    <!-- Link any tickets that this PR resolves. -->
    
    ---------
    
    Co-authored-by: Liam Ilan <snowboardsheep@gmail.com>

---
+++ firmware/shared/src/hw/hw_usb.c
@@ -0,0 +1,95 @@
+#include "hw_usb.h"
+#include "usbd_cdc_if.h"
+#include "cmsis_os.h"
+#include "io_log.h"
+
+// setup rx queue
+static StaticQueue_t      rx_queue_control_block;
+static uint8_t            rx_queue_buf[RX_QUEUE_SIZE];
+static osMessageQueueId_t rx_queue_id = NULL;
+static USBD_HandleTypeDef hUsbDeviceFS;
+
+static const osMessageQueueAttr_t rx_queue_attr = { .name      = "USB RX Queue",
+                                                    .attr_bits = 0,
+                                                    .cb_mem    = &rx_queue_control_block,
+                                                    .cb_size   = sizeof(StaticQueue_t),
+                                                    .mq_mem    = rx_queue_buf,
+                                                    .mq_size   = sizeof(rx_queue_buf) };
+
+void hw_usb_init()
+{
+    if (rx_queue_id == NULL)
+    {
+        rx_queue_id = osMessageQueueNew(RX_QUEUE_SIZE, sizeof(uint8_t), &rx_queue_attr);
+    }
+}
+
+bool hw_usb_checkConnection()
+{
+    return hUsbDeviceFS.dev_state != USBD_STATE_SUSPENDED;
+}
+
+void hw_usb_transmit(uint8_t *msg, uint16_t len)
+{
+    CDC_Transmit_FS(msg, len);
+}
+
+uint8_t hw_usb_recieve()
+{
+    // receive pops off the byte queue and returns
+    uint8_t    res    = 0;
+    osStatus_t status = osMessageQueueGet(rx_queue_id, &res, NULL, 100);
+    if (status == osOK)
+        return res;
+    return 0;
+}
+
+void hw_usb_pushRxMsgToQueue(uint8_t *packet, uint32_t len)
+{
+    uint32_t space = osMessageQueueGetSpace(rx_queue_id);
+    if (len < space)
+    {
+        LOG_WARN("usb message queue overflow");
+    }
+
+    for (uint32_t i = 0; i < len; i += 1)
+    {
+        // write one byte at a time
+        osStatus_t status = osMessageQueuePut(rx_queue_id, &packet[i], 0, 0);
+    }
+}
+
+void hw_usb_transmit_example()
+{
+    // init usb peripheral
+    hw_usb_init();
+    osDelay(1000);
+
+    int msg_count = 0;
+    for (;;)
+    {
+        // send hello (without null terminator)
+        char     msg[]  = "hello";
+        uint8_t *packet = (uint8_t *)msg;
+        hw_usb_transmit(packet, 5);
+
+        msg_count += 1;
+        LOG_INFO("transmitted \"hello\" %d times", msg_count);
+
+        osDelay(1000);
+    }
+}
+
+void hw_usb_recieve_example()
+{
+    // init usb peripheral
+    hw_usb_init();
+
+    // dump the queue.
+    for (int i = 0; true; i += 1)
+    {
+        uint8_t result = hw_usb_recieve();
+        LOG_INFO("char %d: %c", i, (char)result);
+        osDelay(1000);
+    }
+}
