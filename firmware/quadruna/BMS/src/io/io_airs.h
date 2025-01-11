1cb6fd91930791cb83a242ae8e6c2efa9a3382a9
DJ90864 <dikshitjain664@gmail.com>
2025-01-10 18:41:10 -0800

    small io_airs fix, bug from Thruna days

--- firmware/quadruna/BMS/src/io/io_airs.h
+++ firmware/quadruna/BMS/src/io/io_airs.h
@@ -12,7 +12,6 @@ typedef struct
     const Gpio        air_p_gpio;
     const Gpio        air_n_gpio;
     const Gpio        precharge_gpio;
-    const AdcChannel *loop_isense_channel;
 } AirsConfig;
 #else
 EMPTY_STRUCT(AirsConfig);
