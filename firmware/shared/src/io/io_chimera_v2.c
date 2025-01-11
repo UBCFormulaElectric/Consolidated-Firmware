816bed4a42e2616dbfa782fc16c9823cb1b3e78f
Liam Ilan <snowboardsheep@gmail.com>
2025-01-11 11:57:09 -0800

    Clang format.

--- firmware/shared/src/io/io_chimera_v2.c
+++ firmware/shared/src/io/io_chimera_v2.c
@@ -1,24 +1,27 @@
 #include <stdlib.h>
 #include "hw_usb.h"
 
-
 /*
     CHIMERA Packet Format:
     [Non-zero indicator][ low length byte ][ high length byte ][... content bytes * length ...]
 */
 
-void io_chimera_main(uint8_t (*transmit_handle)(uint8_t *Buf, uint16_t Len), uint32_t net_name_gpio, uint32_t net_name_adc) {
-
+void io_chimera_main(
+    uint8_t (*transmit_handle)(uint8_t *Buf, uint16_t Len),
+    uint32_t net_name_gpio,
+    uint32_t net_name_adc)
+{
     // init usb peripheral.
     hw_usb_init(transmit_handle);
 
     // dump the queue.
     for (int i = 0; true; i += 1)
     {
-        if (hw_usb_recieve() == 0x0) continue;
+        if (hw_usb_recieve() == 0x0)
+            continue;
 
         // Length-delimination sent with little-endian.
-        uint8_t low_byte = hw_usb_recieve();
+        uint8_t low_byte  = hw_usb_recieve();
         uint8_t high_byte = hw_usb_recieve();
 
         // Extract length.
@@ -26,14 +29,13 @@ void io_chimera_main(uint8_t (*transmit_handle)(uint8_t *Buf, uint16_t Len), uin
 
         // Populate buffer.
         uint8_t *buf = malloc(length * sizeof(uint8_t));
-        for (int i = 0; i < length; i += 1) {
+        for (int i = 0; i < length; i += 1)
+        {
             buf[i] = hw_usb_recieve();
         }
 
         // TODO: PROCESS BUFFER
 
         free(buf);
-
     }
 };
-
--- firmware/shared/src/io/io_chimera_v2.h
+++ firmware/shared/src/io/io_chimera_v2.h
@@ -1,4 +1,7 @@
 #pragma once
 #include <stdlib.h>
 
-void io_chimera_main(uint8_t (*transmit_handle)(uint8_t *Buf, uint16_t Len), uint32_t net_name_gpio, uint32_t net_name_adc);
+void io_chimera_main(
+    uint8_t (*transmit_handle)(uint8_t *Buf, uint16_t Len),
+    uint32_t net_name_gpio,
+    uint32_t net_name_adc);
