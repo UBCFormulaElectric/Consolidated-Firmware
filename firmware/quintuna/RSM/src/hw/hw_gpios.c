c507135c5a934e648165daa8685a405df2c2caa7
rijwal <rijwalsangey@gmail.com>
2025-01-11 14:20:55 -0800

    gpios

---
+++ firmware/quintuna/RSM/src/hw/hw_gpios.c
@@ -0,0 +1,12 @@
+#include "hw_gpios.h"
+#include "main.h"
+
+const Gpio n_chimera_pin      = { NCHIMERA_GPIO_Port, NCHIMERA_Pin };
+const Gpio led_pin            = { LED_GPIO_Port, LED_Pin };
+const Gpio rad_fan_en_pin     = { RAD_FAN_EN_GPIO_Port, RAD_FAN_EN_Pin };
+const Gpio fr_stby_pin        = { FR_STBY_GPIO_Port, FR_STBY_Pin };
+const Gpio brake_light_en_pin = { BRAKE_LIGHT_EN_3V3_GPIO_Port, BRAKE_LIGHT_EN_3V3_Pin };
+const Gpio acc_fan_en_pin     = { ACC_FAN_EN_GPIO_Port, ACC_FAN_EN_Pin };
+const Gpio n_program_pin      = { NProgram_3V3_GPIO_Port, NProgram_3V3_Pin };
+const Gpio acc_fan_pin        = { ACC_FAN_EN_GPIO_Port, RAD_FAN_EN_Pin };
+const Gpio rad_fan_pin        = { RAD_FAN_EN_GPIO_Port, ACC_FAN_EN_Pin };
--- firmware/quintuna/RSM/src/hw/hw_gpios.h
+++ firmware/quintuna/RSM/src/hw/hw_gpios.h
@@ -1,7 +1,6 @@
 #include "hw_gpio.h"
+#include <stdint.h>
 
-namespace hw::gpio
-{
 extern const Gpio n_chimera_pin;
 extern const Gpio led_pin;
 extern const Gpio rad_fan_en_pin;
@@ -10,5 +9,4 @@ extern const Gpio brake_light_en_pin;
 extern const Gpio acc_fan_en_pin;
 extern const Gpio n_program_pin;
 extern const Gpio acc_fan_pin;
-extern const Gpio rad_fan_pin;
-} // namespace hw::gpio
+extern const Gpio rad_fan_pin; 
