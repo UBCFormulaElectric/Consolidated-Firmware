d51a8cafe793d32dfd77306c1226859a259bf2c7
kryptoish <krish_thakur7@icloud.com>
2025-01-13 18:38:13 -0800

    more progress on the read function might need to rethink the struct

--- firmware/quintuna/VC/src/io/io_power_monitor.c
+++ firmware/quintuna/VC/src/io/io_power_monitor.c
@@ -1,21 +1,23 @@
 #include "io_power_monitor.h"
 #include "hw_i2cs.h"
 
-enum voltage_bus {
- 	VBAT = 0x0B,
-    V_BOOST = 0x0C,
-    V_ACC = 0x0D,
-    V_EXT = 0x0E
-};
-
-bool io_power_monitor_init() {
+bool io_power_monitor_init()
+{
+    voltage_bus voltage_address = { .VBAT = 0x0B, .V_BOOST = 0x0C,
+                                    .V_ACC = 0x0D, .V_EXT = 0x0E }; //is this an ok way of doing it?
     return hw_i2c_isTargetReady(&pwr_mon);
 }
 
-int io_power_monitor_read(voltage_bus voltage) { //might change to uint8_t
-    //refresh command not needed (send byte command does thee same and changes pointer to correct register address)
-    //change the hw_i2c to accept both send byte and recieve byte (NVM MIGHT NOT BE NEEDED JUST NOTICED)
-    return 0;
+uint8_t io_power_monitor_read(voltage_bus voltage_address) // another way of doing it could be
+                                                           // where I could get all 4 bus line voltages and store them in a struct
+{
+    //might change to uint8_t
+    uint8_t buffer[1];
+    hw_i2c_transmit(&pwr_mon, 0x1F, sizeof(0x1F)); // better buffer size - refresh_V
+    hw_12c_transmit(&pwr_mon, voltage_address, sizeof(voltage_address)); // change register pointer address
+    hw_i2c_receive(&pwr_mon, buffer, sizeof(buffer)); // get raw register data and store it in buffer
+    return buffer[0];
 }
 
-// am I missing anything else for the poer monitoring chip??
+// am I missing anything else for the power monitoring chip??
+//app layer will have the processing of the raw voltage data
--- firmware/quintuna/VC/src/io/io_power_monitor.h
+++ firmware/quintuna/VC/src/io/io_power_monitor.h
@@ -1,4 +1,13 @@
-#ifndef POWER_MONITOR_H
-#define POWER_MONITOR_H
+#pragma once
 
-#endif //POWER_MONITOR_H
+typedef struct
+{
+    uint8_t VBAT;
+    uint8_t V_BOOST;
+    uint8_t V_ACC;
+    uint8_t V_EXT;
+} voltage_bus;
+
+bool io_power_monitor_init();
+
+uint8_t io_power_monitor_read(voltage_bus voltage_address);
