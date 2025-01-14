f686471ff950cd12779d132e7d9d5b2e4e8d61e7
Kelm <lkevin2003@gmail.com>
2025-01-13 17:45:57 -0800

    tested with function generator and oscilliscope. Fixed tim1frequencyy discrepency that was causing frequency to be reported double after the initial fix

--- firmware/quadruna/BMS/src/cubemx/Src/stm32h7xx_hal_msp.c
+++ firmware/quadruna/BMS/src/cubemx/Src/stm32h7xx_hal_msp.c
@@ -506,19 +506,10 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim_base)
         /* Peripheral clock enable */
         __HAL_RCC_TIM1_CLK_ENABLE();
 
-        __HAL_RCC_GPIOE_CLK_ENABLE();
         __HAL_RCC_GPIOA_CLK_ENABLE();
         /**TIM1 GPIO Configuration
-        PE11     ------> TIM1_CH2
         PA8     ------> TIM1_CH1
         */
-        GPIO_InitStruct.Pin       = GPIO_PIN_11;
-        GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull      = GPIO_NOPULL;
-        GPIO_InitStruct.Speed     = GPIO_SPEED_FREQ_LOW;
-        GPIO_InitStruct.Alternate = GPIO_AF1_TIM1;
-        HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
-
         GPIO_InitStruct.Pin       = GPIO_PIN_8;
         GPIO_InitStruct.Mode      = GPIO_MODE_AF_PP;
         GPIO_InitStruct.Pull      = GPIO_NOPULL;
@@ -600,11 +591,8 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef *htim_base)
         __HAL_RCC_TIM1_CLK_DISABLE();
 
         /**TIM1 GPIO Configuration
-        PE11     ------> TIM1_CH2
         PA8     ------> TIM1_CH1
         */
-        HAL_GPIO_DeInit(GPIOE, GPIO_PIN_11);
-
         HAL_GPIO_DeInit(GPIOA, GPIO_PIN_8);
 
         /* TIM1 interrupt DeInit */
