08c25025acbd985e33275f8ef1318692744b4a8b
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2025-01-18 21:01:14 -0800

    formatting

--- firmware/shared/src/app/app_heartbeatMonitor.h
+++ firmware/shared/src/app/app_heartbeatMonitor.h
@@ -10,7 +10,7 @@ typedef struct
     void (*own_heartbeat)(bool);
 } HeartbeatMonitor;
 
-void app_heartbeatMonitor_init(const HeartbeatMonitor* hbm);
+void app_heartbeatMonitor_init(const HeartbeatMonitor *hbm);
 
 void app_heartbeatMonitor_checkIn(const HeartbeatMonitor *hbm);
 
