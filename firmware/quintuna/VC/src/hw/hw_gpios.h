afe52c4bbb2d02c18251c28a0d25c3e618fb27df
AmirTajaddodi <amir.tajadodi@gmail.com>
2025-01-19 00:09:53 -0800

    hw layer done

---
+++ firmware/quintuna/VC/src/hw/hw_gpios.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "hw_gpio.h"
+
+extern const Gpio pgood;
+extern const Gpio l_inv_en;
+extern const Gpio r_inv_en;
+extern const Gpio dam_en;
+extern const Gpio rsm_en;
+extern const Gpio imu_int1;
+extern const Gpio imu_int2;
+extern const Gpio pcm_en;
+extern const Gpio pwr_mtr_nalert;
+extern const Gpio bat_chrg_nshdn;
+extern const Gpio bat_chrg_mode;
+extern const Gpio front_en;
+extern const Gpio fr_stby_front;
+extern const Gpio fr_stby_rear;
+extern const Gpio fr_stby_inv;
+extern const Gpio bat_mtr_alert;
+extern const Gpio bat_mtr_rst_shut;
+extern const Gpio bms_en;
