fe3e9afe5b603eae00a09f8ed4c337f5797ccbed
mattiaswong6 <151343938+mattiaswong6@users.noreply.github.com>
2025-01-20 18:48:09 -0800

    removed diagnosticsModeRequest jsoncan bit

--- firmware/quadruna/BMS/test/test_diagnosticsMode.cpp
+++ firmware/quadruna/BMS/test/test_diagnosticsMode.cpp
@@ -18,9 +18,6 @@ TEST_F(BmsFaultTest, check_cell_voltage_segment0_cell0)
     // Passing time to let the message transmit.
     LetTimePass(1000);
 
-    // Reciving and transmitting the data.
-    app_canRx_Debug_BMSDiagnosticsRequest_update(true);
-
     // Passing time to let the message transmit.
     LetTimePass(1000);
 
@@ -41,37 +38,9 @@ TEST_F(BmsFaultTest, check_temperature_segment0)
     // Passing time to let the message transmit.
     LetTimePass(1000);
 
-    // Reciving and transmitting the data.
-    app_canRx_Debug_BMSDiagnosticsRequest_update(true);
-
     // Passing time to let the message transmit.
     LetTimePass(1000);
 
     // Test assertion.
     ASSERT_EQ(fake_temperature, app_canTx_BMS_Seg0_Temp_get());
-}
-
-TEST_F(BmsFaultTest, check_diagnostics_mode_is_off_on)
-{
-    /**
-     * Test that voltage and temperature readings are negative (invalid values)
-     * when BMSDiagnosticsRequest is falseß
-     */
-
-    // Setting up the environment.
-    float fake_voltage = 3.0f;
-    fake_io_ltc6813CellVoltages_getCellVoltage_returnsForAnyArgs(fake_voltage);
-
-    // Passing time to let the message transmit.
-    LetTimePass(1000);
-
-    // Reciving and transmitting the data.
-    app_canRx_Debug_BMSDiagnosticsRequest_update(false);
-
-    // Passing time to let the message transmit.
-    LetTimePass(1000);
-
-    // Test assertion.
-    ASSERT_EQ((float)-0.1, app_canTx_BMS_Seg0_Cell0_Voltage_get());
-    ASSERT_EQ(-1, app_canTx_BMS_Seg0_Temp_get());
 }
