11c81cc5b445c56f3a506d8d69c3abecf19decfd
kryptoish <krish_thakur7@icloud.com>
2025-01-25 12:01:11 -0800

    made current reading func

--- firmware/quintuna/VC/src/io/io_power_monitor.c
+++ firmware/quintuna/VC/src/io/io_power_monitor.c
@@ -1,18 +1,21 @@
 #include "io_power_monitor.h"
 #include "hw_i2cs.h"
 
-#define VOLTAGE_TRANSFER_FACTOR 4.8e-4f;
+#define VOLTAGE_BUS_TRANSFER_FACTOR 4.8828e-4f;
+#define VOLTAGE_SENSE_TRANSFER_FACTOR 1.5259e-6f;
+#define SENSE_RESISTOR 2.0e6f;
+#define FSC 5.00e-8f // Full-Scale Current
 
-const power_rail bat_bus{
-    .address = 0x07 // VBUS1
-};
-const power_rail boost_bus = {
+//const power_rail bat_bus{
+//    .address = 0x07 // VBUS1 got removed?
+//};
+const power_rail boost = {
     .address = 0x08 // VBUS2
 };
-const power_rail acc_bus = {
+const power_rail acc = {
     .address = 0x09 // VBUS3
 };
-const power_rail ext_bus = {
+const power_rail ext = {
     .address = 0x0A // VBUS4
 };
 
@@ -33,7 +36,28 @@ float io_power_monitor_read_voltage(const power_rail *voltage_address)
 
     hw_i2c_receive(&pwr_mon, buffer, 1); // get raw register data and store it in buffer
 
-    const float voltage_buffer = (float)buffer[0] * VOLTAGE_TRANSFER_FACTOR;
+    const float voltage_buffer = (float)buffer[0] * VOLTAGE_BUS_TRANSFER_FACTOR;
 
     return voltage_buffer;
 }
+
+float io_power_monitor_read_current(const power_rail *current_address)
+{
+    // This repeat gets the Vsense value
+    uint16_t buffer[1];
+
+    buffer[0] = 0x1F;
+    hw_i2c_transmit(&pwr_mon, buffer, 1); // refresh
+
+    buffer[0] = (current_address->address) + 0x04;
+    hw_i2c_transmit(&pwr_mon, buffer, 1); // change register pointer address (Vsense)
+
+    hw_i2c_receive(&pwr_mon, buffer, 1); // get raw register data and store it in buffer
+
+    const float voltage_sense_buffer = (float)buffer[0] * VOLTAGE_SENSE_TRANSFER_FACTOR;
+
+    return FSC * (voltage_sense_buffer/65536.0f);
+}
+
+
+// Make alert detection thingy
