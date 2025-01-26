3bafe5b23f8ab90364358861da91d788b37c38a7
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2025-01-25 21:53:37 -0800

    formatting

--- firmware/quadruna/CRIT/src/io/io_taskMonitors.h
+++ firmware/quadruna/CRIT/src/io/io_taskMonitors.h
@@ -1,4 +1,5 @@
 #pragma once
 #include "io_taskMonitor.h"
 
-extern const TaskMonitor task_1hz_monitor, task_100hz_monitor, task_1khz_monitor, task_can_rx_monitor, task_can_tx_monitor;
+extern const TaskMonitor task_1hz_monitor, task_100hz_monitor, task_1khz_monitor, task_can_rx_monitor,
+    task_can_tx_monitor;
