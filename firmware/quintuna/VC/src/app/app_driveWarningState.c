bd688fbec150b2a050c165b16993ded10d742b55
AmirTajaddodi <amir.tajadodi@gmail.com>
2025-02-01 19:48:34 -0800

    added initial code for vc state machine

---
+++ firmware/quintuna/VC/src/app/app_driveWarningState.c
@@ -0,0 +1,28 @@
+#include "app_initState.h"
+#include "app_allStates.h"
+#include "app_inverterOnState.h"
+#include "app_canUtils.h"
+#include "app_canTx.h"
+#include "app_canRx.h"
+#include "app_powerManager.h"
+#include "app_pumpControl.h"
+#include "app_faultCheck.h"
+
+static void driveWarningStateRunOnEntry(void){}
+static void driveWarningStateRunOnTick1Hz(void){}
+static void driveWarningStateRunOnTick100Hz(void){}
+static void driveWarningStateRunOnExit(void) {}
+
+
+const State *app_initState_get(void)
+{
+    static State init_state = {
+        .name              = "DRIVE WARNING",
+        .run_on_entry      = driveWarningStateRunOnEntry,
+        .run_on_tick_1Hz   = driveWarningStateRunOnTick1Hz,
+        .run_on_tick_100Hz = driveWarningStateRunOnTick100Hz,
+        .run_on_exit = driveWarningStateRunOnExit
+    };
+
+    return &init_state;
+}
