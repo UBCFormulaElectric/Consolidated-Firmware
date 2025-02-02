82017ecde47af1d9fa089b64e2b39a80b3ab8a26
Jack Polloway <60236839+yawollopkcaj@users.noreply.github.com>
2025-02-02 11:53:24 -0800

    changed pin config for quintune in cubeMX and changed aa few file from cpp to c and deleted sysmem.c in attempt to get it to build

--- firmware/quintuna/VC/src/io/io_lowVoltageBattery.c
+++ firmware/quintuna/VC/src/io/io_lowVoltageBattery.c
@@ -2,23 +2,22 @@
 #include "hw_i2c.h"
 #include "hw_hal.h"
 
-const uint8_t BQ76922_I2C_ADDR = 0x10; // 7-bit I2C address
-const uint16_t CMD_DASTATUS6 = 0x0076; // Subcommand for accumulated charge
-const uint8_t COMMAND_ADDRESS = 0x3E;
-const uint8_t REG_SUBCOMMAND_LSB = 0x3E;
-const uint8_t REG_SUBCOMMAND_MSB = 0x3F;
-const uint8_t REG_DATA_BUFFER = 0x40;
-const uint8_t REG_CHECKSUM = 0x60;
-const uint8_t REG_RESPONSE_LENGTH = 0x61;
+const uint8_t  BQ76922_I2C_ADDR      = 0x10;   // 7-bit I2C address
+const uint16_t CMD_DASTATUS6         = 0x0076; // Subcommand for accumulated charge
+const uint8_t  COMMAND_ADDRESS       = 0x3E;
+const uint8_t  REG_SUBCOMMAND_LSB    = 0x3E;
+const uint8_t  REG_SUBCOMMAND_MSB    = 0x3F;
+const uint8_t  REG_DATA_BUFFER       = 0x40;
+const uint8_t  REG_CHECKSUM          = 0x60;
+const uint8_t  REG_RESPONSE_LENGTH   = 0x61;
+const uint16_t CELL0_VOLTAGE_COMMAND = 0x1514;
+const uint16_t CELL1_VOLTAGE_COMMAND = 0x1716;
+const uint16_t CELL2_VOLTAGE_COMMAND = 0x1B1A;
+const uint16_t CELL4_VOLTAGE_COMMAND = 0x1D1C;
+const uint16_t STACK_VOLTAGE_COMMAND = 0x3534;
 
-#define R_SENSE 2.0f  // Sense resistor in mΩ
-#define Q_FULL  2000.0f // Battery full charge capacity in mAh
-
-// const uint8_t CELL0_VOLTAGE_COMMAND[] = {0x14, 0x15};
-// const uint8_t CELL1_VOLTAGE_COMMAND[] = {0x16, 0x17};
-// const uint8_t CELL2_VOLTAGE_COMMAND[] = {0x1A, 0x1B};
-// const uint8_t CELL4_VOLTAGE_COMMAND[] = {0x1C, 0x1D};
-// const uint8_t STACK_VOLTAGE_COMMAND[] = {0x34, 0x35};
+#define R_SENSE 2.0f    // Sense resistor in mΩ
+#define Q_FULL 11200.0f // Battery full charge capacity in mAh
 
 extern I2C_HandleTypeDef hi2c1;
 
@@ -29,35 +28,18 @@ bool io_lowVoltageBattery_init()
     return hw_i2c_isTargetReady(&lvBatMon);
 }
 
-bool writeSubcommand(uint16_t subcommand) 
+bool writeSubcommand(uint16_t subcommand)
 {
     uint8_t data[2] = { subcommand & 0xFF, (subcommand >> 8) & 0xFF };
-    if(!hw_i2c_memWrite(&hi2c1, REG_SUBCOMMAND_LSB, data, 2))
+    if (!hw_i2c_memWrite(&hi2c1, REG_SUBCOMMAND_LSB, data, 2))
     {
         return false;
     }
-    
-    bool readDataReady = false;
-    int timeout = 1000;
-     while (!readDataReady && timeout > 0)
-    {
-        uint8_t rxBuffer[2] = {0};
 
-        if (hw_i2c_memRead(&lvBatMon, COMMAND_ADDRESS, rxBuffer, sizeof(rxBuffer)))
-        {
-            if (rxBuffer[0] == data[0] && rxBuffer[1] == data[1])
-            {
-                readDataReady = true;
-            }
-        }
-
-        timeout--;
-    }
-
-    return readDataReady;
+    return true;
 }
 
-uint8_t readResponseLength() 
+uint8_t readResponseLength()
 {
     uint8_t responseLength;
     if (!hw_i2c_memRead(&lvBatMon, REG_RESPONSE_LENGTH, &responseLength, sizeof(responseLength)))
@@ -66,7 +48,7 @@ uint8_t readResponseLength()
     }
     if (responseLength > sizeof(uint8_t))
     {
-        return 0; 
+        return 0;
     }
     return responseLength;
 }
@@ -75,56 +57,59 @@ bool readCharge(uint32_t *charge, uint16_t *time)
 {
     uint8_t responseLen = readResponseLength();
 
-    if (responseLen != 6) {
+    if (responseLen != 6)
+    {
         return false;
     }
 
     uint8_t buffer[6];
 
-    if (!hw_i2c_memRead(&hi2c1, REG_DATA_BUFFER, buffer, 6)) 
+    if (!hw_i2c_memRead(&hi2c1, REG_DATA_BUFFER, buffer, 6))
     {
         return false;
     }
 
     uint8_t checksum;
-    if (!hw_i2c_memRead(&hi2c1, REG_CHECKSUM, &checksum, 1)) {
+    if (!hw_i2c_memRead(&hi2c1, REG_CHECKSUM, &checksum, 1))
+    {
         return false;
     }
 
     uint8_t calculated_checksum = (CMD_DASTATUS6 & 0xFF) + (CMD_DASTATUS6 >> 8) + responseLen;
-    for (int i = 0; i < responseLen; i++) {
+    for (int i = 0; i < responseLen; i++)
+    {
         calculated_checksum += buffer[i];
     }
     calculated_checksum = ~calculated_checksum; // Invert bits
 
-    if (calculated_checksum != checksum) {
+    if (calculated_checksum != checksum)
+    {
         return false;
     }
 
     *charge = (buffer[0] | (buffer[1] << 8) | (buffer[2] << 16));
-    *time = (buffer[3] | (buffer[4] << 8));
+    *time   = (buffer[3] | (buffer[4] << 8));
 
     return true;
 }
 
-float calculateSOC(uint32_t charge) 
+float calculateSOC(uint32_t charge)
 {
-    float CC_GAIN = 7.4768f / R_SENSE;
+    float CC_GAIN    = 7.4768f / R_SENSE;
     float charge_mAh = (charge * CC_GAIN) / 3600.0f;
     return (charge_mAh / Q_FULL) * 100.0f;
 }
 
 bool io_lowVoltageBattery_getPackCharge(float *SOC)
 {
-
     uint32_t *charge;
     uint16_t *time;
 
-    if(!writeSubcommand(CMD_DASTATUS6))
+    if (!writeSubcommand(CMD_DASTATUS6))
     {
         return false;
     }
-    
+
     if (!readCharge(charge, time))
     {
         return false;
@@ -184,12 +169,13 @@ bool io_lowVoltageBattery_getPackVoltage(uint16_t *packVoltage)
     {
         return false;
     }
-    if (checkSum != voltageBuffer) {
+    if (checkSum != voltageBuffer)
+    {
         return false;
-    } else {
+    }
+    else
+    {
         *packVoltage = voltageBuffer;
     }
     return true;
 }
-
-
