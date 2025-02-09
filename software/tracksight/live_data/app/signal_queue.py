2a6bfbb6d1078ad0ce2411fbd6f8f32a877a824d
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2025-02-08 17:27:31 -0800

    hehe

--- software/tracksight/live_data/app/signal_queue.py
+++ software/tracksight/live_data/app/signal_queue.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+import json
 from queue import Queue
 
 # Simulating message_received object
@@ -9,5 +10,11 @@ class Signal:
 	value: any
 	unit: str
 	timestamp: str
+	def toJSON(self):
+		return json.dumps(
+            self,
+            default=lambda o: o.__dict__, 
+            sort_keys=True,
+            indent=4)
 
 signal_queue = Queue()
