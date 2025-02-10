2bb8b044bc85a621aff5a1d16bea080865dcb8b2
Liam Ilan <snowboardsheep@gmail.com>
2025-02-09 23:02:49 -0800

    io_chimera_v2 docs.

--- firmware/shared/src/io/io_chimera_v2.h
+++ firmware/shared/src/io/io_chimera_v2.h
@@ -4,5 +4,11 @@
 #include "hw_adc.h"
 #include "hw_gpio.h"
 
-// Run the main Chimera loop.
+/**
+ * @brief The main Chimera loop.
+ * @param id_to_gpio A table of Protobuf-generated net names to Gpio pins.
+ * @param id_to_adc A table of Protobuf-generated net names to ADC channels.
+ * @param gpio_tag The Protobuf-generated tag for the board's GPIO pins, defined in shared.pb.h.
+ * @param adc_tag The Protobuf-generated tag for the board's ADC channels, defined in shared.pb.h.
+ */
 void io_chimera_v2_main(const Gpio *id_to_gpio[], const AdcChannel *id_to_adc[], pb_size_t gpio_tag, pb_size_t adc_tag);
