7b401cf765f07ac0bd21bf2108555e61890b1371
DJ90864 <dikshitjain664@gmail.com>
2025-02-12 01:14:39 -0800

    hw_gpio in progress

---
+++ firmware/quintuna/BMS/src/hw/hw_gpios.c
@@ -0,0 +1,5 @@
+#include "hw_gpios.h"
+#include "main.h"
+
+const Gpio led_b_pin                        = { .port = LEDB_GPIO_Port, .pin = LEDB_Pin };
+const Gpio led_b_pin                        = { .port = LEDB_GPIO_Port, .pin = LEDB_Pin };
