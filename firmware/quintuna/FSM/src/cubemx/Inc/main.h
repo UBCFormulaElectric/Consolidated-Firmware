cb8042e9c579b04dabe2a68a2e0f3bc343ad0e4d
shayana18 <shayanajmaluni@gmail.com>
2025-02-11 18:45:57 -0800

    code gen for FreeRtos

--- firmware/quintuna/FSM/src/cubemx/Inc/main.h
+++ firmware/quintuna/FSM/src/cubemx/Inc/main.h
@@ -23,25 +23,24 @@
 #define __MAIN_H
 
 #ifdef __cplusplus
-extern "C"
-{
+extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32f4xx_hal.h"
 
-    /* Private includes ----------------------------------------------------------*/
-    /* USER CODE BEGIN Includes */
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
 
-    /* USER CODE END Includes */
+/* USER CODE END Includes */
 
-    /* Exported types ------------------------------------------------------------*/
-    /* USER CODE BEGIN ET */
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
 
-    /* USER CODE END ET */
+/* USER CODE END ET */
 
-    /* Exported constants --------------------------------------------------------*/
-    /* USER CODE BEGIN EC */
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
     extern ADC_HandleTypeDef hadc1;
     extern DMA_HandleTypeDef hdma_adc1;
     extern CAN_HandleTypeDef hcan2;
@@ -49,21 +48,36 @@ extern "C"
     extern I2C_HandleTypeDef hi2c1;
     // extern PCD_HandleTypeDef hpcd_USB_OTG_FS; needed??
 
-    /* USER CODE END EC */
+/* USER CODE END EC */
 
-    /* Exported macro ------------------------------------------------------------*/
-    /* USER CODE BEGIN EM */
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
 
-    /* USER CODE END EM */
+/* USER CODE END EM */
 
-    /* Exported functions prototypes ---------------------------------------------*/
-    void Error_Handler(void);
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define TASK_100HZ_STACK_SIZE 512
+#define TASKS_1HZ_STACK_SIZE 512
+#define TASK_1KHZ_STACK_SIZE 512
+#define TASK_CANTX_STACK_SIZE 512
+#define TASK_CANRX_STACK_SIZE 512
+#define TIM3_PRESCALER 8
+#define TIM12_PWM_MINIMUM_FREQUENCY 1
+#define ADC_FREQUENCY 1000
+#define TIMx_FREQUENCY 96000000
+#define IWDG_RESET_FREQUENCY 5
+#define TIM12_PRESCALER (TIMx_FREQUENCY/ TIM12_AUTO_RELOAD_REG/ TM12_PWM_MINIMUM_FREQUENCY)
+#define TIM12_AUTO_RELOAD_REG 0xFFFF
+#define IWDG_PRESCALER 4
+#define IWDG_WINDOW_DISABLE_VALUE 4095
+#define LSI_FREQUENCY 32000
 #define STR_ANGLE_3V3_Pin GPIO_PIN_1
 #define STR_ANGLE_3V3_GPIO_Port GPIOC
 #define APPS1_3V3_Pin GPIO_PIN_2
@@ -115,9 +129,9 @@ extern "C"
 #define nSUSP_FL_OCSC_Pin GPIO_PIN_9
 #define nSUSP_FL_OCSC_GPIO_Port GPIOB
 
-    /* USER CODE BEGIN Private defines */
+/* USER CODE BEGIN Private defines */
 
-    /* USER CODE END Private defines */
+/* USER CODE END Private defines */
 
 #ifdef __cplusplus
 }
