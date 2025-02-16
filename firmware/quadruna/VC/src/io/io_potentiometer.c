05cef50005f1855aad37017346c1b61efba15215
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2025-02-16 15:29:15 -0800

    nice

--- firmware/quadruna/VC/src/io/io_potentiometer.c
+++ firmware/quadruna/VC/src/io/io_potentiometer.c
@@ -1,14 +1,14 @@
-#include "io_i2cPotentiometer.h"
+#include "io_potentiometer.h"
 #include "hw_i2c.h"
 #include "hw_hal.h"
 #include "hw_i2cs.h"
 
-bool io_i2cPotentiometer_Init()
+bool io_potentiometer_init()
 {
     return hw_i2c_isTargetReady(HW_I2C_DEVICE_POTENTIOMETER);
 }
 
-void io_i2cPotentiometer_setWiper(uint8_t position)
+void io_potentiometer_setWiper(uint8_t position)
 {
     uint8_t buffer[1];
     buffer[0] = position;
@@ -16,7 +16,7 @@ void io_i2cPotentiometer_setWiper(uint8_t position)
     hw_i2c_transmit(HW_I2C_DEVICE_POTENTIOMETER, buffer, sizeof(buffer));
 }
 
-uint8_t io_i2cPotentiometer_readWiper()
+uint8_t io_potentiometer_readWiper()
 {
     uint8_t buffer[1];
     hw_i2c_receive(HW_I2C_DEVICE_POTENTIOMETER, buffer, sizeof(buffer));
