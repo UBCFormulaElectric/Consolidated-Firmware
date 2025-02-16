b66e10c840dcad42446b90fb6f71985aee9e86e5
kryptoish <krish_thakur7@icloud.com>
2025-02-15 16:04:56 -0800

    additions to io_apps

--- firmware/quintuna/FSM/src/io/io_apps.h
+++ firmware/quintuna/FSM/src/io/io_apps.h
@@ -1,1 +1,18 @@
 #pragma once
+
+#include "hw_adc.h"
+
+#ifdef TARGET_EMBEDDED
+
+typedef struct
+{
+    const AdcChannel *papps;
+    const AdcChannel *sapps;
+} AppsConfig;
+
+#else
+#include "app_utils.h"
+EMPTY_STRUCT(AppsConfig)
+#endif
+
+
