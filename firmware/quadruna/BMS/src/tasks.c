67f9cdd9086afe430aa59d55b352fa51341e04f3
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2025-02-16 18:39:02 -0800

    BMS hw init fix

--- firmware/quadruna/BMS/src/tasks.c
+++ firmware/quadruna/BMS/src/tasks.c
@@ -76,6 +76,7 @@ void tasks_init(void)
     hw_hardFaultHandler_init();
     hw_crc_init(&hcrc);
     hw_watchdog_init(hw_watchdogConfig_refresh, hw_watchdogConfig_timeoutCallback);
+    hw_can_init(&can1);
 
     io_tractiveSystem_init(&ts_config);
     io_ltc6813Shared_init(&ltc6813_spi);
