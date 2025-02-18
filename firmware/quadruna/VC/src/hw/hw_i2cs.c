2da338d0a8056f948238895b307fd11c2f99cef9
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2025-02-17 18:32:00 -0800

    fix

--- firmware/quadruna/VC/src/hw/hw_i2cs.c
+++ firmware/quadruna/VC/src/hw/hw_i2cs.c
@@ -4,8 +4,8 @@
 static I2cBus i2c_bus_1 = { .handle = &hi2c1 };
 static I2cBus i2c_bus_2 = { .handle = &hi2c2 };
 
-const I2cDevice imu_i2c           = { .bus = &i2c_bus_1, .target_address = 0x6B, .timeout_ms = 100 };
-const I2cDevice potentiometer_i2c = { .bus = &i2c_bus_2, .target_address = 0x2F, .timeout_ms = 100 };
+const I2cDevice imu_i2c           = { .bus = &i2c_bus_2, .target_address = 0x6B, .timeout_ms = 100 };
+const I2cDevice potentiometer_i2c = { .bus = &i2c_bus_1, .target_address = 0x2F, .timeout_ms = 100 };
 
 I2cBus *hw_i2c_getBusFromHandle(const I2C_HandleTypeDef *handle)
 {
