9fa82dfcbbc62930f8c38bcc2c352f67275d2e10
peterjinweigu <petergujinwei@gmail.com>
2025-02-22 22:51:50 -0800

    some basic changes

--- software/dimos/lib/src/app.dart
+++ software/dimos/lib/src/app.dart
@@ -2,20 +2,23 @@ import 'dart:io';
 import 'package:dimos/src/widgets/Speedometer.dart';
 import 'package:flutter/material.dart';
 import 'package:window_manager/window_manager.dart';
+import 'screen/LVScreen.dart';
+import 'widgets/NotificationBar.dart';
 
-// TODO: Stateful page + router
+// TODO: Stateful page + router 
 
-class Dimos extends StatelessWidget {
-  const Dimos({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(colorScheme: ColorScheme.dark()),
+      theme: new ThemeData(scaffoldBackgroundColor: const Color.fromARGB(255, 0, 0, 0)),
       home: Scaffold(
+        appBar: NotificationBar(),
         body: Center(
-          child: Speedometer(),
+          child: LVScreen()
         ),
       ),
     );
