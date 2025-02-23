f0e8cb892006304ce73d1fd0be65083bdb4de896
Liam Ilan <snowboardsheep@gmail.com>
2025-02-23 15:06:19 -0800

    Patchy docs fix.

--- firmware/chimera_v2/scripts/generate_docs.sh
+++ firmware/chimera_v2/scripts/generate_docs.sh
@@ -8,6 +8,15 @@ cd $CHIMERA_V2_DIR
     BRANCH="${1:-"master"}"
     GITHUB_SRC_URL="https://github.com/UBCFormulaElectric/Consolidated-Firmware/tree/$BRANCH"
     echo "Generating Chimera V2 docs using $BRANCH branch for base url"
+
+    # Reinstall this package.
+    pip uninstall -y chimera_v2
+    pip install .
+
+    # Generate docs.
     lazydocs --validate --src-base-url="$GITHUB_SRC_URL" chimera_v2
+
+    # Uninstall this package.
+    pip uninstall -y chimera_v2
     
 cd $CACHED_WORKING_DIR
