15f7961258c47ae30cdd94b3d7bea764daa7d8f2
Jiajun-Huang <jhuang430@qq.com>
2025-02-23 16:38:04 -0800

    new rx schema

--- scripts/code_generation/jsoncan/src/codegen/c_generation/io_can_rx_module.py
+++ scripts/code_generation/jsoncan/src/codegen/c_generation/io_can_rx_module.py
@@ -1,9 +1,6 @@
 import jinja2 as j2
 
 from ...can_database import *
-from ...utils import *
-from .c_config import *
-from .c_writer import *
 from .utils import load_template
 
 
@@ -12,15 +9,22 @@ class IoCanRxModule(CModule):
         self._db = db
         self._node = node
 
-
     def header_template(self):
         template = load_template("io_canRx.h.j2")
-        j2_env = j2.Environment(loader=j2.BaseLoader, extensions=['jinja2.ext.loopcontrols'])
+        j2_env = j2.Environment(
+            loader=j2.BaseLoader, extensions=["jinja2.ext.loopcontrols"]
+        )
         template = j2_env.from_string(template)
-        return template.render(messages=self._db.rx_msgs_for_node(self._node), node=self._node)
-    
+        return template.render(
+            messages=self._db.rx_msgs_for_node(self._node), node=self._node
+        )
+
     def source_template(self):
         template = load_template("io_canRx.c.j2")
-        j2_env = j2.Environment(loader=j2.BaseLoader, extensions=['jinja2.ext.loopcontrols'])
+        j2_env = j2.Environment(
+            loader=j2.BaseLoader, extensions=["jinja2.ext.loopcontrols"]
+        )
         template = j2_env.from_string(template)
-        return template.render(messages=self._db.rx_msgs_for_node(self._node), node=self._node)
+        return template.render(
+            messages=self._db.rx_msgs_for_node(self._node), node=self._node
+        )
