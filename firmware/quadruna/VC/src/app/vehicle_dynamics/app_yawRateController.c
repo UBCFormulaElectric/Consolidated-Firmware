98330a881f8b4cb5cdce0d5ca9ca3318d9554235
Aditya-Dhiman4 <aditya16dhiman@gmail.com>
2025-02-24 18:18:30 -0800

    small changes

--- firmware/quadruna/VC/src/app/vehicle_dynamics/app_yawRateController.c
+++ firmware/quadruna/VC/src/app/vehicle_dynamics/app_yawRateController.c
@@ -5,11 +5,11 @@
 
 /*
  * NOTE:
- *  
+ *
  * Untested and Untuned
  * Currently does not sent torque requests
  * Currently adapted for 2 wheel driving,
-*/
+ */
 
 void app_yawRateController_init(YawRateController *yrc, PID *pid, const YawRateController_Config *config)
 {
@@ -38,19 +38,21 @@ float app_yawRateController_pid_compute(YawRateController *yrc, float ref_yaw_ra
     return app_pid_compute(yrc->pid, ref_yaw_rate, yrc->real_yaw_rate);
 }
 
-void app_yawRateController_computeTorque(
-    YawRateController *yrc,
-    float              ref_yaw_rate,
-    float              yaw_moment)
+void app_yawRateController_computeTorque(YawRateController *yrc, float ref_yaw_rate, float yaw_moment)
 {
     float lambda;
     float yaw_rate_diff = fabsf(ref_yaw_rate) - fabsf(yrc->real_yaw_rate);
 
-    if (yaw_rate_diff <= yrc->k4) {
+    if (yaw_rate_diff <= yrc->k4)
+    {
         lambda = 0;
-    } else if (yaw_rate_diff > yrc->k4 && yaw_rate_diff < yrc->k5) {
-        lambda = fabsf(yrc->real_yaw_rate) - yrc->k4) / (yrc->k5 - yrc->k4);
-    } else if (yaw_rate_diff >= yrc->k5) {
+    }
+    else if (yaw_rate_diff > yrc->k4 && yaw_rate_diff < yrc->k5)
+    {
+        lambda = (yaw_rate_diff - yrc->k4) / (yrc->k5 - yrc->k4);
+    }
+    else if (yaw_rate_diff >= yrc->k5)
+    {
         lambda = 1;
     }
 
--- firmware/quadruna/VC/src/app/vehicle_dynamics/app_yawRateController.h
+++ firmware/quadruna/VC/src/app/vehicle_dynamics/app_yawRateController.h
@@ -31,28 +31,28 @@ typedef struct
 typedef struct
 {
     const PID_Config *pid_config;
-    float       ku;
-    float       k4;
-    float       k5;
+    float             ku;
+    float             k4;
+    float             k5;
 } YawRateController_Config;
 
 /*
  * Initializes Yaw Rate Controller
-*/
+ */
 void app_yawRateController_init(YawRateController *yrc, PID *pid, const YawRateController_Config *config);
 
 /*
  * Computes the reference yaw rate based on steering angle and velocity
-*/
+ */
 float app_yawRateController_computeRefYawRate(YawRateController *yrc);
 
 /*
  * Computes a corrective yaw moment using PID based on reference and real yaw rate
-*/
+ */
 float app_yawRateController_pid_compute(YawRateController *yrc, float ref_yaw_rate);
 
 /*
- * Computes the torques to send to the wheels based on the 
+ * Computes the torques to send to the wheels based on the
  * corrective yaw moment and reference yaw rate
-*/
+ */
 void app_yawRateController_computeTorque(YawRateController *yrc, float ref_yaw_rate, float yaw_moment);
