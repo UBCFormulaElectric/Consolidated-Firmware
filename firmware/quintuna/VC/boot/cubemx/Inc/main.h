009aa83dd2bc881e7812ba8121e3bc2a5d236ea1
Jiajun-Huang <jhuang430@qq.com>
2025-02-24 21:25:45 -0800

    config task

--- firmware/quintuna/VC/boot/cubemx/Inc/main.h
+++ firmware/quintuna/VC/boot/cubemx/Inc/main.h
@@ -37,6 +37,7 @@ extern "C"
 
     /* Exported types ------------------------------------------------------------*/
     /* USER CODE BEGIN ET */
+
     /* USER CODE END ET */
 
     /* Exported constants --------------------------------------------------------*/
