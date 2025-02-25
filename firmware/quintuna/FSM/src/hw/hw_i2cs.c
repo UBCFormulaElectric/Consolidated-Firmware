52d423e4de1785808f9d9c4ca893bc380b186cd2
kryptoish <krish_thakur7@icloud.com>
2025-02-24 18:44:36 -0800

    gpio fix

--- firmware/quintuna/FSM/src/hw/hw_i2cs.c
+++ firmware/quintuna/FSM/src/hw/hw_i2cs.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include "hw_i2cs.h"
 
-const I2cInterface imu = { .i2c_handle = &hi2c1,
-                           .target_address = 0x6B,
-                           .timeout_ms = 40 };
+const I2cDevice imu = { .bus = &hi2c1, //fix this after i2c merge
+                        .target_address = 0x6B,
+                        .timeout_ms = 40 };
--- firmware/quintuna/FSM/src/tasks.c
+++ firmware/quintuna/FSM/src/tasks.c
@@ -1,1 +1,2 @@
 #include "tasks.h"
+
