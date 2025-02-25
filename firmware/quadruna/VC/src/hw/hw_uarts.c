e83888910eaf92bca9cb263d30dfbcf30210315c
Jiajun-Huang <jhuang430@qq.com>
2025-02-24 19:09:40 -0800

    bring back VC qudruna

--- firmware/quadruna/VC/src/hw/hw_uarts.c
+++ firmware/quadruna/VC/src/hw/hw_uarts.c
@@ -1,4 +1,7 @@
 #include "hw_uarts.h"
 #include "main.h"
 
-const UART debug_uart = { .handle = &huart7 };
+const UART debug_uart    = { .handle = &huart7 };
+const UART sbg_uart      = { .handle = &huart2 };
+const UART modem2G4_uart = { .handle = &huart3 };
+const UART modem900_uart = { .handle = &huart1 };
