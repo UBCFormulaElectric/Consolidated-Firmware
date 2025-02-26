97c3633360eed963f0f09e0a3ec2e0b14512cb44
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2025-02-25 17:28:09 -0800

    yahoo

--- firmware/quintuna/CRIT/src/jobs.c
+++ firmware/quintuna/CRIT/src/jobs.c
@@ -19,7 +19,7 @@ void jobs_init(void)
 {
     // can
     io_canTx_init(canTransmit); // TODO this needs to be more sophisticated for multiple busses
-    io_canTx_enableMode_Can(CAN_MODE_DEFAULT, true);
+    io_canTx_enableMode(CAN_MODE_DEFAULT, true);
     app_canTx_init();
     app_canRx_init();
     // broadcast commit info
@@ -30,7 +30,7 @@ void jobs_init(void)
 void jobs_run1Hz_tick(void)
 {
     const bool debug_mode_enabled = app_canRx_Debug_EnableDebugMode_get();
-    io_canTx_enableMode_Can(CAN_MODE_DEBUG, debug_mode_enabled);
+    io_canTx_enableMode(CAN_MODE_DEBUG, debug_mode_enabled);
     io_canTx_enqueue1HzMsgs();
 }
 
