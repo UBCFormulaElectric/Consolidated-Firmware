b3956784e5565458010f6d3538f2712ccd7d9acd
Jack Polloway <60236839+yawollopkcaj@users.noreply.github.com>
2025-02-25 19:58:31 -0800

    resolving merge conflicts from rebase

--- firmware/quintuna/VC/src/io/io_lowVoltageBattery.h
+++ firmware/quintuna/VC/src/io/io_lowVoltageBattery.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "hw_gpio.h"
-<<<<<<< HEAD
 #include "cmsis_os2.h"
 
 extern osSemaphoreId_t bat_mtr_sem;
@@ -17,18 +16,10 @@ extern const uint16_t STACK_VOLTAGE_COMMAND;
  * @brief Initializes the low-voltage battery monitoring system.
  *
  * @return true if initialization was successful; false otherwise.
-=======
-
-/**
- * Initialize the LV battery monitor.
- * 
- * @return true if semaphore init is sucessful and false otherwise
->>>>>>> 9d1f39c9 (changes from main: added io file for VC in quintuna, added LV battery monitor file and header and had cubemx gutogenerate code for configuring an alert pin)
  */
 bool io_lowVoltageBattery_init();
 
 /**
-<<<<<<< HEAD
  * @brief Gets the battery state-of-charge (SOC) as a percentage.
  *
  * @return SOC percentage on success, or -1.0f on error.
@@ -43,9 +34,3 @@ float io_lowVoltageBattery_get_SOC();
  * @return The battery voltage on success, or -1 on error.
  */
 uint16_t io_lowVoltageBattery_get_voltage(uint16_t voltage_cmd);
-=======
- * Gets current from low voltage battery ()
- * @return current from battery
- */
-float io_lowVoltageBattery_getPackVoltage();
->>>>>>> 9d1f39c9 (changes from main: added io file for VC in quintuna, added LV battery monitor file and header and had cubemx gutogenerate code for configuring an alert pin)
