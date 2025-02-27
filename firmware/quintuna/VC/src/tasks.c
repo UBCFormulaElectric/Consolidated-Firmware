cc9fc50c7cfbdc49620ea6868b422c1fb36ff0ec
Jack Polloway <60236839+yawollopkcaj@users.noreply.github.com>
2025-02-26 19:53:38 -0800

    added code improvements

--- firmware/quintuna/VC/src/tasks.c
+++ firmware/quintuna/VC/src/tasks.c
@@ -1,6 +1,5 @@
 #include "tasks.h"
 #include "/Users/jackpolloway/Consolidated-Firmware/firmware/quintuna/VC/src/cubemx/Inc/main.h"
-#include "cmsis_os2.h"
 #include "io_lowVoltageBattery.h"
 
 void tasks_init(void)
@@ -14,8 +13,7 @@ void tasks_bat_mtr(void)
     io_lowVoltageBattery_get_voltage();
 }
 
-HAL_GPIO_EXTI_Callback(uint16_t GPIO_pin)
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_pin)
 {
-    GPIO_pin = BAT_MTR_ALERT_Pin;
-    osSemaphoreRelease(bat_mtr_sem);
+    io_lowVoltageBattery_completeAlert(GPIO_pin);
 }
