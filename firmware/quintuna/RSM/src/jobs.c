30f8d09a52e892be41a7a9079db13d4661a6aee5
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2025-02-28 11:53:26 -0800

    formatted

--- firmware/quintuna/RSM/src/jobs.c
+++ firmware/quintuna/RSM/src/jobs.c
@@ -8,8 +8,12 @@ void jobs_run100Hz_tick() {}
 
 void jobs_run1kHz_tick() {}
 
-void jobs_runCanTx_tick() {}B
+void jobs_runCanTx_tick() {}
+B
 
-void jobs_runCanRx_tick() {}
+    void
+    jobs_runCanRx_tick()
+{
+}
 
 static void jsoncan_transmit() {}
