3b21c2f3ce880e7bc6ac2b87e959bc18b9290e1f
kryptoish <krish_thakur7@icloud.com>
2025-03-01 11:04:55 -0800

    tasks additions

--- firmware/quintuna/FSM/src/tasks.c
+++ firmware/quintuna/FSM/src/tasks.c
@@ -1,2 +1,87 @@
 #include "tasks.h"
+#include "cmsis_os.h"
+#include "shared.pb.h"
+#include "jobs.h"
 
+// io
+#include "io_log.h"
+#include "io_canQueue.h"
+
+// hw
+#include "hw_utils.h"
+#include "hw_hardFaultHandler.h"
+#include "hw_can.h"
+
+void tasks_preinit(void) {}
+
+void tasks_init(void)
+{
+    // Configure and initialize SEGGER SystemView.
+    // NOTE: Needs to be done after clock config!
+    SEGGER_SYSVIEW_Conf();
+    LOG_INFO("FSM reset!");
+
+    // Re-enable watchdog.
+    __HAL_DBGMCU_FREEZE_IWDG();
+    hw_hardFaultHandler_init();
+
+    jobs_init();
+}
+
+void tasks_runCanTx(void)
+{
+    // Setup tasks.
+    for (;;)
+    {
+        CanMsg msg = io_canQueue_popTx();
+        hw_can_transmit(&can1, &msg);
+    }
+}
+
+void tasks_runCanRx(void)
+{
+    // Setup tasks.
+    for (;;)
+    {
+        jobs_runCanRx_tick();
+    }
+}
+
+void tasks_run1Hz(void)
+{
+    // Setup tasks.
+    static const TickType_t period_ms   = 1000U;
+    uint32_t                start_ticks = osKernelGetTickCount();
+    for (;;)
+    {
+        jobs_run1Hz_tick();
+        start_ticks += period_ms;
+        osDelayUntil(start_ticks);
+    }
+}
+
+void tasks_run100Hz(void)
+{
+    // Setup tasks.
+    static const TickType_t period_ms   = 10;
+    uint32_t                start_ticks = osKernelGetTickCount();
+    for (;;)
+    {
+        jobs_run100Hz_tick();
+        start_ticks += period_ms;
+        osDelayUntil(start_ticks);
+    }
+}
+
+void tasks_run1kHz(void)
+{
+    // Setup tasks.
+    static const TickType_t period_ms   = 1;
+    uint32_t                start_ticks = osKernelGetTickCount();
+    for (;;)
+    {
+        jobs_run1kHz_tick();
+        start_ticks += period_ms;
+        osDelayUntil(start_ticks);
+    }
+}
--- firmware/quintuna/FSM/src/tasks.h
+++ firmware/quintuna/FSM/src/tasks.h
@@ -1,1 +1,9 @@
 #pragma once
+
+void           tasks_preInit();
+void           tasks_init();
+_Noreturn void tasks_run1Hz();
+_Noreturn void tasks_run100Hz();
+_Noreturn void tasks_run1kHz();
+_Noreturn void tasks_runCanTx();
+_Noreturn void tasks_runCanRx();
