1436770c4615c9e6e5705ef5d56bd60a57e2bd31
Jack Polloway <60236839+yawollopkcaj@users.noreply.github.com>
2025-03-09 11:28:42 -0700

    initialized TIM15 in cube mx and set the appropriate pwm input config

--- firmware/quintuna/BMS/src/hw/hw_pwms.c
+++ firmware/quintuna/BMS/src/hw/hw_pwms.c
@@ -9,10 +9,9 @@ static const PwmInputConfig imd_pwm_input_config = {
     .falling_edge_tim_channel = TIM_CHANNEL_2,
 };
 
-/* Make changes based on EVSE protocol */
 static const PwmInputConfig evse_pwm_input_config = {
     .htim                     = &htim15,
     .timer_frequency_hz       = TIM1_FREQUENCY / TIM1_PRESCALER,
-    .rising_edge_tim_channel  = TIM_CHANNEL_1,
-    .falling_edge_tim_channel = TIM_CHANNEL_2,
+    .rising_edge_tim_channel  = TIM_CHANNEL_2,
+    .falling_edge_tim_channel = TIM_CHANNEL_1,
 };
