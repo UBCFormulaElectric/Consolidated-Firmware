a882b31fd684683e6f2548ef404931d9a10a2e39
kryptoish <krish_thakur7@icloud.com>
2025-03-10 22:29:24 -0700

    changes and fixes made, power function added. Some things unresolved

--- firmware/quintuna/VC/src/io/io_power_monitor.c
+++ firmware/quintuna/VC/src/io/io_power_monitor.c
@@ -1,28 +1,39 @@
 #include "io_power_monitor.h"
 #include "hw_i2cs.h"
 
-#define VOLTAGE_BUS_TRANSFER_FACTOR 4.8828e-4f;
-#define VOLTAGE_SENSE_TRANSFER_FACTOR 1.5259e-6f;
-#define SENSE_RESISTOR 2.0e6f;
-#define FSC 5.00e-8f // Full-Scale Current
-#define REFRESH 0x1F
-#define ALERT_STATUS 0x26
+/*
+ * For more info about this chip, the data sheet is linked below:
+ * https://ww1.microchip.com/downloads/aemDocuments/documents/MSLD/ProductDocuments/DataSheets/PAC195X-Family-Data-Sheet-DS20006539.pdf
+ *
+ * A confluence Page will also be made for how the driver code goes about
+ * using the PAC1954-E/4MX power monitoring chip
+ *
+ */
 
-const PowerRail boost = { .bus_address = 0x07, .sense_address = 0x0B };
-const PowerRail acc   = { .bus_address = 0x08, .sense_address = 0x0C };
-const PowerRail ext   = { .bus_address = 0x09, .sense_address = 0x0D };
+const PowerRail ext = { .bus_address = 0x07, .sense_address = 0x0B, .power_address = 0x17 };
+const PowerRail acc = { .bus_address = 0x08, .sense_address = 0x0C, .power_address = 0x18 };
 
 bool io_power_monitor_init()
 {
+    // Configuration of the ctrl register may be needed
     return hw_i2c_isTargetReady(&pwr_mon);
 }
+
+#define REFRESH 0x1F
+
+void io_power_monitor_refresh()
+{
+    uint8_t buffer[1] = { REFRESH };
+    hw_i2c_transmit(&pwr_mon, buffer, 1);
+}
+
+#define VOLTAGE_BUS_TRANSFER_FACTOR 4.8828e-4f;
 
 float io_power_monitor_read_voltage(const PowerRail *voltage_address)
 {
     uint8_t buffer[2];
 
-    buffer[0] = REFRESH;
-    hw_i2c_transmit(&pwr_mon, buffer, 1); // refresh
+    io_power_monitor_refresh(); // timing problems potentially?
 
     buffer[0] = voltage_address->bus_address;
     hw_i2c_transmit(&pwr_mon, buffer, 1); // change register pointer address
@@ -34,12 +45,14 @@ float io_power_monitor_read_voltage(const PowerRail *voltage_address)
     return voltage_buffer;
 }
 
+#define VOLTAGE_SENSE_TRANSFER_FACTOR 1.5259e-6f;
+#define SENSE_RESISTOR 3.0e6f;
+
 float io_power_monitor_read_current(const PowerRail *current_address)
 {
     uint8_t buffer[2];
 
-    buffer[0] = REFRESH;
-    hw_i2c_transmit(&pwr_mon, buffer, 1); // refresh
+    io_power_monitor_refresh();
 
     buffer[0] = current_address->sense_address;
     hw_i2c_transmit(&pwr_mon, buffer, 1); // change register pointer address (Vsense)
@@ -48,15 +61,36 @@ float io_power_monitor_read_current(const PowerRail *current_address)
 
     const float voltage_sense_buffer = (float)((buffer[0] << 8) | buffer[1]) * VOLTAGE_SENSE_TRANSFER_FACTOR;
 
-    return FSC * (voltage_sense_buffer / 65536.0f); // FSC * (Vsense/denom)
+    return voltage_sense_buffer / SENSE_RESISTOR; // FSC * (Vsense/denom)
 }
+
+#define VOLTAGE_POWER_TRANSFER_FACTOR 9.3132e-10f;
+#define POWER_FSR 1.0666e-6f;
+
+float io_power_monitor_read_power(const PowerRail *power_address)
+{
+    uint8_t buffer[2];
+
+    io_power_monitor_refresh();
+
+    buffer[0] = power_address->power_address;
+    hw_i2c_transmit(&pwr_mon, buffer, 1); // change register pointer address
+
+    hw_i2c_receive(&pwr_mon, buffer, 2); // get raw register data and store it in buffer
+
+    const float voltage_power_buffer = (float)((buffer[0] << 8) | buffer[1]) * VOLTAGE_POWER_TRANSFER_FACTOR;
+
+    return voltage_power_buffer * POWER_FSR;
+}
+
+#define ALERT_STATUS 0x26
 
 uint32_t io_power_monitor_alert_status()
 {
     uint8_t buffer[3]; // Does not require a REFRESH
 
-    buffer[0] = ALERT_STATUS; // ALERT_STATUS address
-    hw_i2c_transmit(&pwr_mon, buffer, 1);
+    buffer[0] = ALERT_STATUS;             // ALERT_STATUS address
+    hw_i2c_transmit(&pwr_mon, buffer, 1); // change register pointer to alert_status
 
     hw_i2c_receive(&pwr_mon, buffer, 3); // Gets all the alert statuses
 
--- firmware/quintuna/VC/src/io/io_power_monitor.h
+++ firmware/quintuna/VC/src/io/io_power_monitor.h
@@ -7,6 +7,7 @@ typedef struct
 {
     const uint8_t bus_address;
     const uint8_t sense_address;
+    const uint8_t power_address;
 } PowerRail;
 
 #else
@@ -14,12 +15,18 @@ typedef struct
 EMPTY_STRUCT(PowerRail)
 #endif
 
-extern const PowerRail boost;
-extern const PowerRail acc;
 extern const PowerRail ext;
+extern const PowerRail acc;
 
+// TODO: add comments and decriptions for how deez work
 bool io_power_monitor_init();
 
+void io_power_monitor_refresh();
+
 float io_power_monitor_read_voltage(const PowerRail *voltage_address);
+
+float io_power_monitor_read_current(const PowerRail *current_address);
+
+float io_power_monitor_read_power(const PowerRail *power_address);
 
 uint32_t io_power_monitor_alert_status();
