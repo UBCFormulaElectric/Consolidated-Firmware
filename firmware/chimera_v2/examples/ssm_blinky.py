4fb9a931b6f1b36fa13033ac02d83a32ddfb39fe
Liam Ilan <snowboardsheep@gmail.com>
2025-03-12 02:00:15 -0700

    Fix ssm blinky chimera example.

--- firmware/chimera_v2/examples/ssm_blinky.py
+++ firmware/chimera_v2/examples/ssm_blinky.py
@@ -1,25 +1,20 @@
 import time
 import chimera_v2
 
-ssm_usb_device = chimera_v2._UsbDevice("ssm")
+# Config.
+RATE_HZ = 10.0
+DUTY_CYCLE = 0.5
+LED_NET_NAME = "DEBUG_LED"
+
+# Init device.
+chimera_device = chimera_v2.SSM()
+
+# Main loop.
 while True:
-    time.sleep(1)
-    ssm_usb_device.read(5)
+    print("On")
+    chimera_device.gpio_write(LED_NET_NAME, True)
+    time.sleep(DUTY_CYCLE / RATE_HZ)
 
-# # Config.
-# RATE_HZ = 10.0
-# DUTY_CYCLE = 0.5
-# LED_NET_NAME = "DEBUG_LED"
-
-# # Init device.
-# chimera_device = chimera_v2.SSM()
-
-# # Main loop.
-# while True:
-#     print("On")
-#     chimera_device.gpio_write(LED_NET_NAME, True)
-#     time.sleep(DUTY_CYCLE / RATE_HZ)
-
-#     print("Off")
-#     chimera_device.gpio_write(LED_NET_NAME, False)
-#     time.sleep((1.0 - DUTY_CYCLE) / RATE_HZ)
+    print("Off")
+    chimera_device.gpio_write(LED_NET_NAME, False)
+    time.sleep((1.0 - DUTY_CYCLE) / RATE_HZ)
