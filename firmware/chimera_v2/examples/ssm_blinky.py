f4cd71a3c45a64f54d732df2a773d6a091b9dbb9
Liam Ilan <liamilanyvr@gmail.com>
2025-03-12 01:39:12 -0700

    Chimera Over USB (Chimera V2) (#1420)
    > Note to reviewers - This PR isn't small... Highly recommend reading
    through
    [`firmware/chimera_v2/README.md`](https://github.com/UBCFormulaElectric/Consolidated-Firmware/blob/liam-ilan/chimera_v2/firmware/chimera_v2/README.md),
    it'll take you across the codebase to the parts that really matter.
    
    ### Changelist 
    This PR Implements Chimera V2 (aka. Chimera over USB).
    
    To allow larger messages than 64 bytes (the max CDC USB packet size,
    definitely overkill 😅), `hw_usb` dumps any received data over usb into
    an `osMessageQueue`, composed of the bytes received over USB. Since we
    have no indicator for the start and end of a stream, and all bytes are
    valid in RPC, we define a length-delimited format. See the diagram
    below.
    
    ![chimera_v2_high_res
    drawio](https://github.com/user-attachments/assets/a1979946-6a53-4e6c-a2ef-a3a65eaf67b6)
    
    ### Testing Done
    - [x] Tested blinky on the F4 Dev Board (GPIO Output).
    - [x] Tested on Windows.
    - [x] Tested GPIO Read.
    - [ ] Tested ADC Read.
    - [ ] Tested with IWDG active.
    - [ ] Tested I2C Peripherals.
    
    ### For Future PRs
    When boards arrive, we might want to swap from the single
    `chimera_v2_main` to a dedicated task.
    
    ### Note About Install
    Until this PR is merged, Chimera V2 is installed with
    ```sh
    pip install 'git+https://github.com/UBCFormulaElectric/Consolidated-Firmware.git@liam-ilan/chimera_v2#egg=chimera-v2&subdirectory=firmware/chimera_v2'
    ```

---
+++ firmware/chimera_v2/examples/ssm_blinky.py
@@ -0,0 +1,25 @@
+import time
+import chimera_v2
+
+ssm_usb_device = chimera_v2._UsbDevice("ssm")
+while True:
+    time.sleep(1)
+    ssm_usb_device.read(5)
+
+# # Config.
+# RATE_HZ = 10.0
+# DUTY_CYCLE = 0.5
+# LED_NET_NAME = "DEBUG_LED"
+
+# # Init device.
+# chimera_device = chimera_v2.SSM()
+
+# # Main loop.
+# while True:
+#     print("On")
+#     chimera_device.gpio_write(LED_NET_NAME, True)
+#     time.sleep(DUTY_CYCLE / RATE_HZ)
+
+#     print("Off")
+#     chimera_device.gpio_write(LED_NET_NAME, False)
+#     time.sleep((1.0 - DUTY_CYCLE) / RATE_HZ)
--- firmware/quintuna/CRIT/src/cubemx/Inc/main.h
+++ firmware/quintuna/CRIT/src/cubemx/Inc/main.h
@@ -33,6 +33,7 @@ extern "C"
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include "cmsis_os.h"
+#include "hw_error.h"
     /* USER CODE END Includes */
 
     /* Exported types ------------------------------------------------------------*/
