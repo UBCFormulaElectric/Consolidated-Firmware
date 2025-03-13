7228913d45bd926e80b9eccbff63a1f3aae51c8c
Edwin <20777515+Lucien950@users.noreply.github.com>
2025-03-12 19:40:23 -0700

    Revert MessageStreams (#1489)
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    ### Resolved Tickets
    <!-- Link any tickets that this PR resolves. -->
    
    ---------
    
    Co-authored-by: Jiajun-Huang <jhuang430@qq.com>

--- firmware/shared/src/io/io_canQueue.c
+++ firmware/shared/src/io/io_canQueue.c
@@ -7,21 +7,29 @@
 
 #include "FreeRTOS.h"
 #include "message_buffer.h"
+#include "cmsis_os.h"
 #define TX_QUEUE_SIZE 128
 #define RX_QUEUE_SIZE 128
 #define TX_QUEUE_BYTES sizeof(CanMsg) * TX_QUEUE_SIZE
 #define RX_QUEUE_BYTES sizeof(CanMsg) * RX_QUEUE_SIZE
 
-char                        *tx_name;
-char                        *rx_name;
-MessageBufferHandle_t        tx_queue_id;
-MessageBufferHandle_t        rx_queue_id;
-StaticQueue_t                tx_queue_control_block;
-StaticQueue_t                rx_queue_control_block;
-uint8_t                      tx_queue_buf[TX_QUEUE_BYTES];
-uint8_t                      rx_queue_buf[RX_QUEUE_BYTES];
+char                      *tx_name;
+char                      *rx_name;
+osMessageQueueId_t         tx_queue_id;
+MessageBufferHandle_t      rx_queue_id;
+StaticQueue_t              tx_queue_control_block;
+StaticQueue_t              rx_queue_control_block;
+uint8_t                    tx_queue_buf[TX_QUEUE_BYTES];
+uint8_t                    rx_queue_buf[RX_QUEUE_BYTES];
+const osMessageQueueAttr_t tx_queue_attr = {
+    .name      = "CAN TX Queue",
+    .attr_bits = 0,
+    .cb_mem    = &tx_queue_control_block,
+    .cb_size   = sizeof(StaticQueue_t),
+    .mq_mem    = tx_queue_buf,
+    .mq_size   = TX_QUEUE_BYTES,
+};
 static StaticMessageBuffer_t rx_buffer_control_block;
-static StaticMessageBuffer_t tx_buffer_control_block;
 
 static bool init_complete = false;
 
@@ -34,7 +42,7 @@ void io_canQueue_init()
 {
     // Initialize CAN queues.
     assert(!init_complete);
-    tx_queue_id   = xMessageBufferCreateStatic(TX_QUEUE_BYTES, tx_queue_buf, &tx_buffer_control_block);
+    tx_queue_id   = osMessageQueueNew(TX_QUEUE_SIZE, sizeof(CanMsg), &tx_queue_attr);
     rx_queue_id   = xMessageBufferCreateStatic(RX_QUEUE_BYTES, rx_queue_buf, &rx_buffer_control_block);
     init_complete = true;
 
@@ -44,11 +52,12 @@ void io_canQueue_init()
 void io_canQueue_pushTx(const CanMsg *tx_msg)
 {
     assert(init_complete);
-    static uint32_t tx_overflow_count = 0;
-    size_t          bytes_sent        = xMessageBufferSend(tx_queue_id, tx_msg, sizeof(CanMsg), 0);
+    static uint32_t  tx_overflow_count = 0;
+    const osStatus_t s                 = osMessageQueuePut(tx_queue_id, tx_msg, 0, 0);
 
-    if (bytes_sent != sizeof(CanMsg))
+    if (s != osOK)
     {
+        // If pushing to the queue failed, the queue is full. Discard the tx_msg and invoke the TX overflow callback.
         canTxQueueOverflowCallBack(++tx_overflow_count);
     }
     else
@@ -62,9 +71,11 @@ CanMsg io_canQueue_popTx()
     assert(init_complete);
     CanMsg msg;
     // Pop a msg of the TX queue
-    size_t bytes_received = xMessageBufferReceive(tx_queue_id, &msg, sizeof(CanMsg), portMAX_DELAY);
+    // size_t bytes_received = xMessageBufferReceive(tx_queue_id, &msg, sizeof(CanMsg), portMAX_DELAY);
+    // assert(bytes_received == sizeof(CanMsg));
+    const osStatus_t s = osMessageQueueGet(tx_queue_id, &msg, NULL, osWaitForever);
+    assert(s == osOK);
 
-    assert(bytes_received == sizeof(CanMsg));
     return msg;
 }
 
