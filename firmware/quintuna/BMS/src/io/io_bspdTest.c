9756774dd274a0876ac70607f39348df9d21aa00
mattiaswong6 <151343938+mattiaswong6@users.noreply.github.com>
2025-03-15 16:40:39 -0700

    io_bspdTest implementation

---
+++ firmware/quintuna/BMS/src/io/io_bspdTest.c
@@ -0,0 +1,12 @@
+#include "io_bspdTest.h"
+#include "hw_gpios.h"
+
+void io_bspdTest_enable(const bool enable)
+{
+    hw_gpio_writePin(&bspd_test_enable_pin, enable);
+}
+
+bool io_bspdTest_isCurrentThresholdExceeded(void)
+{
+    return !hw_gpio_readPin(&n_high_current_bspd_pin);
+}
