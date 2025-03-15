18ef15c6ddb672d568291393e500a0bd93c18351
Jack Polloway <jack_polloway@icloud.com>
2025-03-15 11:59:45 -0700

    changed TIM15 in cubemx to setup EVSE PWM

--- firmware/quintuna/BMS/src/cubemx/Src/main.c
+++ firmware/quintuna/BMS/src/cubemx/Src/main.c
@@ -903,9 +903,9 @@ static void MX_TIM15_Init(void)
 
     /* USER CODE END TIM15_Init 1 */
     htim15.Instance               = TIM15;
-    htim15.Init.Prescaler         = TIM1_PRESCALER;
+    htim15.Init.Prescaler         = TIM15_PRESCALER;
     htim15.Init.CounterMode       = TIM_COUNTERMODE_UP;
-    htim15.Init.Period            = TIM1_AUTO_RELOAD_REG;
+    htim15.Init.Period            = TIM15_AUTO_RELOAD_REG;
     htim15.Init.ClockDivision     = TIM_CLOCKDIVISION_DIV1;
     htim15.Init.RepetitionCounter = 0;
     htim15.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
