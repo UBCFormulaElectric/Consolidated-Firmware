7fc4e0481c0522b3898ffa384e691f79457af975
DJ90864 <dikshitjain664@gmail.com>
2025-03-15 16:43:59 -0700

    chimera WIP

--- firmware/chimera_v2/proto_autogen/ssm_pb2.py
+++ firmware/chimera_v2/proto_autogen/ssm_pb2.py
@@ -4,7 +4,7 @@ from google.protobuf import descriptor_pool as _descriptor_pool
 from google.protobuf import runtime_version as _runtime_version
 from google.protobuf import symbol_database as _symbol_database
 from google.protobuf.internal import builder as _builder
-_runtime_version.ValidateProtobufRuntimeVersion(_runtime_version.Domain.PUBLIC, 5, 28, 2, '', 'ssm.proto')
+_runtime_version.ValidateProtobufRuntimeVersion(_runtime_version.Domain.PUBLIC, 5, 29, 0, '-rc2', 'ssm.proto')
 _sym_db = _symbol_database.Default()
 DESCRIPTOR = _descriptor_pool.Default().AddSerializedFile(b'\n\tssm.proto\x12\x03ssm*z\n\x0bGpioNetName\x12\x1d\n\x19GPIO_NET_NAME_UNSPECIFIED\x10\x00\x12\x0f\n\x0bINDICATOR_1\x10\x01\x12\x0f\n\x0bINDICATOR_2\x10\x02\x12\x0f\n\x0bINDICATOR_3\x10\x03\x12\n\n\x06DOUT_4\x10\x04\x12\r\n\tDEBUG_LED\x10\x05**\n\nAdcNetName\x12\x1c\n\x18ADC_NET_NAME_UNSPECIFIED\x10\x00**\n\nI2cNetName\x12\x1c\n\x18I2C_NET_NAME_UNSPECIFIED\x10\x00b\x06proto3')
 _globals = globals()
