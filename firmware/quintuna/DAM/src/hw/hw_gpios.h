7b9f0579b657ba68cd95e5db95fda0e8e9e5c765
Jiajun-Huang <jhuang430@qq.com>
2025-03-15 12:28:39 -0700

    firmware config chimera config

--- firmware/quintuna/DAM/src/hw/hw_gpios.h
+++ firmware/quintuna/DAM/src/hw/hw_gpios.h
@@ -5,7 +5,7 @@
 extern const Gpio telem_pwr_en_pin;
 extern const Gpio buzzer_pwr_en_pin;
 extern const Gpio tsim_red_en_pin;
-extern const Gpio ntsim_green_en_pi;
+extern const Gpio ntsim_green_en_pin;
 extern const Gpio r_shdn_sense_pin;
 extern const Gpio l_shdn_sense_pin;
 extern const Gpio led_pin;
@@ -13,3 +13,4 @@ extern const Gpio boot_pin;
 extern const Gpio sd_present;
 extern const Gpio sd_fail_pin;
 extern const Gpio n_rtc_int1_pin;
+extern const Gpio _900m_pin;
--- firmware/quintuna/DAM/src/io/io_chimeraConfig_v2.c
+++ firmware/quintuna/DAM/src/io/io_chimeraConfig_v2.c
@@ -1,4 +1,4 @@
-#include "ssm.pb.h"
+#include "dam.pb.h"
 #include "hw_gpio.h"
 #include "hw_gpios.h"
 #include "hw_adc.h"
@@ -7,13 +7,23 @@
 #include "hw_i2cs.h"
 
 const Gpio *id_to_gpio[] = {
-    [ssm_GpioNetName_INDICATOR_1] = &indicator_1, [ssm_GpioNetName_INDICATOR_2] = &indicator_2,
-    [ssm_GpioNetName_INDICATOR_3] = &indicator_3, [ssm_GpioNetName_DOUT_4] = &dout_4,
-    [ssm_GpioNetName_DEBUG_LED] = &debug_led,
+    [DAM_GpioNetName_GPIO_NET_NAME_UNSPECIFIED] = NULL,
+    [DAM_GpioNetName_TELEM_PWR_EN]              = &telem_pwr_en_pin,
+    [DAM_GpioNetName_BUZZER_PWR_EN]             = &buzzer_pwr_en_pin,
+    [DAM_GpioNetName_TSIM_RED_EN]               = &tsim_red_en_pin,
+    [DAM_GpioNetName_NTSIM_GRN_EN]              = &ntsim_green_en_pin,
+    [DAM_GpioNetName_R_SHDN_SENSE]              = &r_shdn_sense_pin,
+    [DAM_GpioNetName_L_SHDN_SENSE]              = &l_shdn_sense_pin,
+    [DAM_GpioNetName_LED]                       = &led_pin,
+    [DAM_GpioNetName_BOOT]                      = &boot_pin,
+    [DAM_GpioNetName_SD_PRESENT]                = &sd_present,
+    [DAM_GpioNetName_N_RTC_INT1]                = &n_rtc_int1_pin,
+    [DAM_GpioNetName__900M]                     = &_900m_pin,
+    [DAM_GpioNetName_SD_FAIL]                   = &sd_fail_pin,
 };
 
 // TODO: Configure adcs.
-const AdcChannel *id_to_adc[] = { [ssm_AdcNetName_ADC_NET_NAME_UNSPECIFIED] = NULL };
+const AdcChannel *id_to_adc[] = { [DAM_AdcNetName_ADC_NET_NAME_UNSPECIFIED] = NULL };
 
 // TODO: Configure I2Cs.
-const I2cDevice *id_to_i2c[] = { [ssm_I2cNetName_I2C_NET_NAME_UNSPECIFIED] = NULL };
+const I2cDevice *id_to_i2c[] = { [DAM_I2cNetName_I2C_NET_NAME_UNSPECIFIED] = NULL };
