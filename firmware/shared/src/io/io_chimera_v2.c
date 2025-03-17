4c148c2865b1b4d0e8c1ed40f86a696d39a75dba
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2025-03-17 16:00:43 -0700

    chimera v2

--- firmware/shared/src/io/io_chimera_v2.c
+++ firmware/shared/src/io/io_chimera_v2.c
@@ -44,6 +44,8 @@ static const Gpio *io_chimera_v2_getGpio(const GpioNetName *net_name)
         return id_to_gpio[net_name->name.ssm_net_name];
     if (net_name->which_name == GpioNetName_crit_net_name_tag)
         return id_to_gpio[net_name->name.crit_net_name];
+    if (net_name->which_name == GpioNetName_rsm_net_name_tag)
+        return id_to_gpio[net_name->name.rsm_net_name];
 
     LOG_ERROR("Chimera: Received GPIO pin from unsupported board.");
     return NULL;
@@ -64,6 +66,8 @@ static const AdcChannel *io_chimera_v2_getAdc(const AdcNetName *net_name)
         return id_to_adc[net_name->name.ssm_net_name];
     if (net_name->which_name == AdcNetName_crit_net_name_tag)
         return id_to_adc[net_name->name.crit_net_name];
+    if (net_name->which_name == AdcNetName_rsm_net_name_tag)
+        return id_to_adc[net_name->name.rsm_net_name];
 
     LOG_ERROR("Chimera: Received ADC channel from unsupported board.");
     return NULL;
@@ -84,6 +88,8 @@ static const I2cDevice *io_chimera_v2_getI2c(const I2cNetName *net_name)
         return id_to_i2c[net_name->name.ssm_net_name];
     if (net_name->which_name == I2cNetName_crit_net_name_tag)
         return id_to_i2c[net_name->name.crit_net_name];
+    if (net_name->which_name == I2cNetName_rsm_net_name_tag)
+        return id_to_i2c[net_name->name.rsm_net_name];
 
     LOG_ERROR("Chimera: Received I2C device from unsupported board.");
     return 0;
