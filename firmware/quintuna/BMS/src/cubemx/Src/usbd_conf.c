bca91c5b62973b2d1b9d6fef92f2e910075b9a13
Liam Ilan <snowboardsheep@gmail.com>
2025-03-18 02:17:08 -0700

    BMS USB -> HSI48.

--- firmware/quintuna/BMS/src/cubemx/Src/usbd_conf.c
+++ firmware/quintuna/BMS/src/cubemx/Src/usbd_conf.c
@@ -75,7 +75,7 @@ void HAL_PCD_MspInit(PCD_HandleTypeDef *pcdHandle)
         /** Initializes the peripherals clock
          */
         PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USB;
-        PeriphClkInitStruct.UsbClockSelection    = RCC_USBCLKSOURCE_PLL;
+        PeriphClkInitStruct.UsbClockSelection    = RCC_USBCLKSOURCE_HSI48;
         if (HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct) != HAL_OK)
         {
             Error_Handler();
