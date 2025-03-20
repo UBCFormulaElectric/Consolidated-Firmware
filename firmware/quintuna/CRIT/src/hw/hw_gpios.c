97b0da65733183320b70d7bb87beb616a9a6be25
Liam Ilan <liamilanyvr@gmail.com>
2025-03-19 22:18:01 -0700

    SRCK to RCK on CRIT (#1512)
    - Fix a CRIT hw GPIO misconfiguration.

--- firmware/quintuna/CRIT/src/hw/hw_gpios.c
+++ firmware/quintuna/CRIT/src/hw/hw_gpios.c
@@ -12,6 +12,6 @@ const Gpio launch_control_sig   = { .port = LAUNCH_CONTROL_SIG_GPIO_Port, .pin =
 const Gpio torque_vectoring_sig = { .port = TORQUE_VECTORING_SIG_GPIO_Port, .pin = TORQUE_VECTORING_SIG_Pin };
 const Gpio regen_sig            = { .port = REGEN_SIG_GPIO_Port, .pin = REGEN_SIG_Pin };
 const Gpio led_rck              = { .port = LED_RCK_GPIO_Port, .pin = LED_RCK_Pin };
-const Gpio seven_seg_rck        = { .port = SEVEN_SEG_SRCK_GPIO_Port, .pin = SEVEN_SEG_SRCK_Pin };
+const Gpio seven_seg_rck        = { .port = SEVEN_SEG_RCK_GPIO_Port, .pin = SEVEN_SEG_RCK_Pin };
 const Gpio seven_seg_dimming    = { .port = SEVEN_SEG_DIMMING_GPIO_Port, .pin = SEVEN_SEG_DIMMING_Pin };
 const Gpio led_dimming          = { .port = LED_DIMMING_GPIO_Port, .pin = LED_DIMMING_Pin };
