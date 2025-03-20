cd7f8ecbf7564c278ed263202b2568489b8c527d
Jiajun-Huang <92061424+Jiajun-Huang@users.noreply.github.com>
2025-03-19 17:14:10 -0700

    dam hw layer (#1470)
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    ### Resolved Tickets
    <!-- Link any tickets that this PR resolves. -->

--- firmware/quintuna/DAM/src/cubemx/Src/usbd_conf.c
+++ firmware/quintuna/DAM/src/cubemx/Src/usbd_conf.c
@@ -78,7 +78,7 @@ void HAL_PCD_MspInit(PCD_HandleTypeDef *pcdHandle)
         PeriphClkInitStruct.PLL3.PLL3M           = 1;
         PeriphClkInitStruct.PLL3.PLL3N           = 24;
         PeriphClkInitStruct.PLL3.PLL3P           = 2;
-        PeriphClkInitStruct.PLL3.PLL3Q           = 2;
+        PeriphClkInitStruct.PLL3.PLL3Q           = 4;
         PeriphClkInitStruct.PLL3.PLL3R           = 2;
         PeriphClkInitStruct.PLL3.PLL3RGE         = RCC_PLL3VCIRANGE_3;
         PeriphClkInitStruct.PLL3.PLL3VCOSEL      = RCC_PLL3VCOWIDE;
