85778cf7e45a0b8cbd83ad3f92fd1e1518d72742
Liam Ilan <snowboardsheep@gmail.com>
2025-03-19 23:52:02 -0700

    Fix.

--- firmware/quintuna/CRIT/src/cubemx/Src/main.c
+++ firmware/quintuna/CRIT/src/cubemx/Src/main.c
@@ -331,7 +331,7 @@ static void MX_SPI2_Init(void)
     hspi2.Init.CLKPolarity       = SPI_POLARITY_LOW;
     hspi2.Init.CLKPhase          = SPI_PHASE_1EDGE;
     hspi2.Init.NSS               = SPI_NSS_SOFT;
-    hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+    hspi2.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
     hspi2.Init.FirstBit          = SPI_FIRSTBIT_MSB;
     hspi2.Init.TIMode            = SPI_TIMODE_DISABLE;
     hspi2.Init.CRCCalculation    = SPI_CRCCALCULATION_DISABLE;
@@ -367,7 +367,7 @@ static void MX_SPI3_Init(void)
     hspi3.Init.CLKPolarity       = SPI_POLARITY_LOW;
     hspi3.Init.CLKPhase          = SPI_PHASE_1EDGE;
     hspi3.Init.NSS               = SPI_NSS_SOFT;
-    hspi3.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_2;
+    hspi3.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_16;
     hspi3.Init.FirstBit          = SPI_FIRSTBIT_MSB;
     hspi3.Init.TIMode            = SPI_TIMODE_DISABLE;
     hspi3.Init.CRCCalculation    = SPI_CRCCALCULATION_DISABLE;
