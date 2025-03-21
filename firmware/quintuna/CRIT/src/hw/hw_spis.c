76232efca0320cb9fa6190e790f2a8cffc627d12
DJ90864 <dikshitjain664@gmail.com>
2025-03-21 16:05:08 -0700

    formatting

--- firmware/quintuna/CRIT/src/hw/hw_spis.c
+++ firmware/quintuna/CRIT/src/hw/hw_spis.c
@@ -5,12 +5,8 @@
 static SpiBus led_spi_bus       = { .handle = &hspi2 };
 static SpiBus seven_seg_spi_bus = { .handle = &hspi3 };
 
-const SpiDevice led_spi       = { .bus        = &led_spi_bus,
-                                  .nss_pin    = &led_serin,
-                                  .timeout_ms = 10u };
-const SpiDevice seven_seg_spi = { .bus        = &seven_seg_spi_bus,
-                                  .nss_pin    = &seven_seg_serin,
-                                  .timeout_ms = 10u };
+const SpiDevice led_spi       = { .bus = &led_spi_bus, .nss_pin = &led_serin, .timeout_ms = 10u };
+const SpiDevice seven_seg_spi = { .bus = &seven_seg_spi_bus, .nss_pin = &seven_seg_serin, .timeout_ms = 10u };
 
 SpiBus *hw_spi_getBusFromHandle(const SPI_HandleTypeDef *handle)
 {
