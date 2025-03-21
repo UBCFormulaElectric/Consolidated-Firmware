a9c0d64990d5694bf4dec40c2937049a50544030
DJ90864 <dikshitjain664@gmail.com>
2025-03-21 16:04:30 -0700

    spi gpio referencing bug fix

--- firmware/quintuna/CRIT/src/hw/hw_spis.c
+++ firmware/quintuna/CRIT/src/hw/hw_spis.c
@@ -1,14 +1,15 @@
 #include "hw_spis.h"
+#include "hw_gpios.h"
 #include "main.h"
 
 static SpiBus led_spi_bus       = { .handle = &hspi2 };
 static SpiBus seven_seg_spi_bus = { .handle = &hspi3 };
 
 const SpiDevice led_spi       = { .bus        = &led_spi_bus,
-                                  .nss_pin    = { .port = LED_SERIN_GPIO_Port, .pin = LED_SERIN_Pin },
+                                  .nss_pin    = &led_serin,
                                   .timeout_ms = 10u };
 const SpiDevice seven_seg_spi = { .bus        = &seven_seg_spi_bus,
-                                  .nss_pin    = { .port = SEVEN_SEG_SERIN_GPIO_Port, .pin = SEVEN_SEG_SERIN_Pin },
+                                  .nss_pin    = &seven_seg_serin,
                                   .timeout_ms = 10u };
 
 SpiBus *hw_spi_getBusFromHandle(const SPI_HandleTypeDef *handle)
