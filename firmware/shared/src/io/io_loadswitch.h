78bf185cb370e8b1db81ded88b5d2afced8c5bd3
AmirTajaddodi <amir.tajadodi@gmail.com>
2025-03-22 18:10:19 -0700

    init

---
+++ firmware/shared/src/io/io_loadswitch.h
@@ -0,0 +1,61 @@
+#pragma once
+
+#include <stdbool.h>
+
+#ifdef TARGET_EMBEDDED
+#include "hw_gpios.h"
+#include "hw_adcs.h"
+#else
+#include "app_utils.h"
+EMPTY_STRUCT(LoadSwitch)
+EMPTY_STRUCT(Efuse)
+#endif
+
+typedef struct
+{
+    const Gpio       *enable_gpio;
+    const AdcChannel *sns_adc_channel;
+} Efuse;
+
+typedef struct
+{
+    const Efuse *efuse1;
+    const Efuse *efuse2;
+    const Gpio  *stby_reset_gpio;
+} ST_LoadSwitch;
+
+typedef struct
+{
+    const Efuse *efuse;
+    const Gpio  *pgood;
+} TI_LoadSwitch;
+
+/**
+ * Enable or disable the provided loadswitch channel.
+ * @param channel Channel to enable/disable
+ * @param enabled Enable if enabled is true, disable if false
+ */
+void io_loadswitch_setChannel(const Efuse *channel, const bool enabled);
+/**
+ * Check of provided loadswitch channel is enabled
+ * @param channel Channel to enable/disable
+ * @return If loadswitch channel is enabled
+ */
+bool io_loadswitch_isChannelEnabled(const Efuse *channel);
+/**
+ * Get the current read from the provided channel
+ * @param channel Channel to enable/disable
+ * @return The current read from the provided channel, in A
+ */
+float io_loadswitch_getChannelCurrent(const Efuse *channel);
+/**
+ * Reset the hardfault set by the loadswitch
+ * @param loadswitch Reset the hardfault set by loadswitch
+ */
+void io_STloadswitch_Reset(const ST_LoadSwitch *loadswitch);
+
+/**
+ * Check if pgood is ok and if not reset the loadswitch
+ * @param loadswitch Reset the hardfault set by loadswitch
+ */
+void io_TILoadswitch_Reset(const TI_LoadSwitch *loadSwitch);
