1c679b25e663aef0e4ab6602291814a14eae9c4d
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2025-03-29 11:07:37 -0700

    changes

--- firmware/quintuna/RSM/src/io/io_rPump.c
+++ firmware/quintuna/RSM/src/io/io_rPump.c
@@ -6,13 +6,13 @@ bool io_rPump_isPumpReady(const Pump *pump){
     return hw_i2c_isTargetReady(pump->src);
 }
 
-void io_rPump_writeSmth(const Pump *pump, uint8_t data) {
+void io_rPump_write(const Pump *pump, uint8_t data) {
     uint8_t buffer[1];
     buffer[0] = data;
     hw_i2c_transmit(pump->src, buffer, sizeof(buffer));
 }
 
-uint8_t io_rPump_readSmth(const Pump *pump) {
+uint8_t io_rPump_read(const Pump *pump) {
     uint8_t buffer[1];
     hw_i2c_receive(pump->src, buffer, sizeof(buffer));
     return buffer[0];
