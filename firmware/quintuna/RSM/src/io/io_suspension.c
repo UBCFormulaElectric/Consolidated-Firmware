1fb80a4786bb919bb4544863fb4075c3a24ede4c
rijwal <rijwalsangey@gmail.com>
2025-03-29 11:35:06 -0700

    suspensions done except read data from elec for m and b

--- firmware/quintuna/RSM/src/io/io_suspension.c
+++ firmware/quintuna/RSM/src/io/io_suspension.c
@@ -2,23 +2,22 @@
 #include "hw_adcs.h"
 #include "hw_gpios.h"
 
-// CHECK MIN/MAX VALUES FOR RSM
-
-#define MIN_SUSPENSION_VOLTAGE (0.2f)
-#define MAX_SUSPENSION_VOLTAGE (3.5f)
+#define M (0.0f)
+#define B (0.0f) // GET M AND B FROM RSM DATA
 
 const Suspension right = { .src = &susp_travel_rr_3v3 };
 const Suspension left  = { .src = &susp_travel_rl_3v3 };
 
-/*static float travelFromVoltage(float voltage)
+float travelFromVoltage(float voltage) 
 {
-    // TODO: implement
-    return voltage;
-}*/
+    float travel_mm = (voltage - B) / M;
+    return travel_mm;
+}
 
 float io_suspension_getTravel(const Suspension *suspension)
 {
-    return hw_adc_getVoltage(suspension->src);
+    float voltage = hw_adc_getVoltage(suspension->src);
+    return travelFromVoltage(voltage);
 }
 
 bool io_suspension_rr_OCSC(void)
