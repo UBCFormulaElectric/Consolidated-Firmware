e766940c567e0f40780d36291e89f1860de1c195
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2025-03-29 12:34:39 -0700

    seems like uart for sbg is working ok

--- firmware/shared/src/hw/hw_uart.c
+++ firmware/shared/src/hw/hw_uart.c
@@ -28,12 +28,6 @@ static void transactionCompleteHandler(UART_HandleTypeDef *handle, bool is_tx_tr
         return;
     }
 
-    if (device->task_in_progress == NULL)
-    {
-        // No task in progress, abort.
-        return;
-    }
-
     if (device->is_callback_transaction)
     {
         if (is_tx_transaction)
@@ -49,6 +43,12 @@ static void transactionCompleteHandler(UART_HandleTypeDef *handle, bool is_tx_tr
     }
     else
     {
+        if (device->task_in_progress == NULL)
+        {
+            // No task in progress, abort.
+            return;
+        }
+
         // Notify the task that started the UART transaction, if there is a transaction in progress.
         BaseType_t higher_priority_task_woken = pdFALSE;
         vTaskNotifyGiveFromISR(device->task_in_progress, &higher_priority_task_woken);
@@ -111,7 +111,7 @@ bool hw_uart_transmitCallback(UartDevice *device, uint8_t *data, uint16_t size)
     }
 
     // Save current task before starting a UART transaction.
-    device->task_in_progress        = xTaskGetCurrentTaskHandle();
+    device->task_in_progress        = NULL;
     device->is_callback_transaction = true;
 
     if (device->config.callback_dma)
@@ -126,7 +126,7 @@ bool hw_uart_transmitCallback(UartDevice *device, uint8_t *data, uint16_t size)
 
 bool hw_uart_receiveCallback(UartDevice *device, uint8_t *data, uint16_t size)
 {
-    assert(device->config.transmit_callback != NULL);
+    assert(device->config.receive_callback != NULL);
 
     if (device->task_in_progress != NULL)
     {
@@ -135,7 +135,7 @@ bool hw_uart_receiveCallback(UartDevice *device, uint8_t *data, uint16_t size)
     }
 
     // Save current task before starting a UART transaction.
-    device->task_in_progress        = xTaskGetCurrentTaskHandle();
+    device->task_in_progress        = NULL;
     device->is_callback_transaction = true;
 
     if (device->config.callback_dma)
