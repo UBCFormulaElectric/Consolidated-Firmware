89f317223293c5bb76148a7de2f44aec62433a33
Jack Polloway <jack_polloway@icloud.com>
2025-03-29 12:12:58 -0700

    name changes

--- firmware/quintuna/BMS/src/io/io_bmsShdn.h
+++ firmware/quintuna/BMS/src/io/io_bmsShdn.h
@@ -4,6 +4,6 @@
 
 #define BMS_SHDN_NODE_COUNT 2
 
-bool io_bmsShdn_TS_ILCK_OK_get(void);
+bool io_bmsShdn_shdn_en_pin_get(void);
 
-bool io_bmsShdn_HVD_OK_get(void);
+bool io_bmsShdn_hv_p_intlck_sns_pin_get(void);
