b09bdcf4f4ff631ce4fa49603cb8d0ff89c6075d
rijwal <rijwalsangey@gmail.com>
2025-03-29 16:49:38 -0700

    pls bruh

--- firmware/quintuna/RSM/src/cubemx/Src/main.c
+++ firmware/quintuna/RSM/src/cubemx/Src/main.c
@@ -190,10 +190,6 @@ int main(void)
     }
     MX_TIM2_Init();
     /* USER CODE BEGIN 2 */
->>>>>>> master
-=======
-    /* USER CODE BEGIN 2 */
-
     tasks_init();
     /* USER CODE END 2 */
 
@@ -324,17 +320,10 @@ static void MX_ADC1_Init(void)
     hadc1.Init.ClockPrescaler        = ADC_CLOCK_SYNC_PCLK_DIV4;
     hadc1.Init.Resolution            = ADC_RESOLUTION_12B;
     hadc1.Init.ScanConvMode          = ENABLE;
-<<<<<<< HEAD
-    hadc1.Init.ContinuousConvMode    = ENABLE;
-    hadc1.Init.DiscontinuousConvMode = DISABLE;
-    hadc1.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_RISING;
-    hadc1.Init.ExternalTrigConv      = ADC_EXTERNALTRIGCONV_T2_CC2;
-=======
     hadc1.Init.ContinuousConvMode    = DISABLE;
     hadc1.Init.DiscontinuousConvMode = DISABLE;
     hadc1.Init.ExternalTrigConvEdge  = ADC_EXTERNALTRIGCONVEDGE_RISING;
     hadc1.Init.ExternalTrigConv      = ADC_EXTERNALTRIGCONV_T2_TRGO;
->>>>>>> pranay-rijwal/RSM_io
     hadc1.Init.DataAlign             = ADC_DATAALIGN_RIGHT;
     hadc1.Init.NbrOfConversion       = 4;
     hadc1.Init.DMAContinuousRequests = ENABLE;
