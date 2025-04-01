46a43cc57bbfdaca641230360746c785d97d170a
kryptoish <krish_thakur7@icloud.com>
2025-03-31 17:48:02 -0700

    googly boogly merger

--- firmware/quintuna/VC/src/cubemx/Inc/main.h
+++ firmware/quintuna/VC/src/cubemx/Inc/main.h
@@ -42,7 +42,7 @@ extern "C"
 
     /* Exported constants --------------------------------------------------------*/
     /* USER CODE BEGIN EC */
-    extern I2C_HandleTypeDef hi2c1;
+
     /* USER CODE END EC */
 
     /* Exported macro ------------------------------------------------------------*/
--- firmware/quintuna/VC/src/hw/hw_i2cs.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "hw_i2c.h"
-
-extern I2cDevice pwr_mon_i2c;
