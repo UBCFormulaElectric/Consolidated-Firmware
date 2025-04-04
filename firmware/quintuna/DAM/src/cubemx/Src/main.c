f5fad52d94d159842fd8d95071643f8e362e9717
Jiajun-Huang <jhuang430@qq.com>
2025-04-03 18:06:27 -0700

    rtc set and read working

--- firmware/quintuna/DAM/src/cubemx/Src/main.c
+++ firmware/quintuna/DAM/src/cubemx/Src/main.c
@@ -412,7 +412,7 @@ static void MX_I2C1_Init(void)
 
     /* USER CODE END I2C1_Init 1 */
     hi2c1.Instance              = I2C1;
-    hi2c1.Init.Timing           = 0x60404E72;
+    hi2c1.Init.Timing           = 0xC0202FA1;
     hi2c1.Init.OwnAddress1      = 0;
     hi2c1.Init.AddressingMode   = I2C_ADDRESSINGMODE_7BIT;
     hi2c1.Init.DualAddressMode  = I2C_DUALADDRESS_DISABLE;
