9a2cafa77f6d74fb778cac146ebdca5839a0a8f5
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2025-04-05 11:42:35 -0700

    document

--- firmware/quintuna/RSM/src/io/io_coolants.h
+++ firmware/quintuna/RSM/src/io/io_coolants.h
@@ -2,7 +2,24 @@
 
 #include <stdbool.h>
 
+/**
+ * Initialize the flow meter by configuring PWM input 
+ */
 void  io_coolant_init();
+
+/**
+ * Update the flow meter frequency by reading the timer value.
+ */
 void  io_coolant_inputCaptureCallback();
+
+/**
+ * Check if the flow meter is active. If it is not, set the frequency to 0.
+ */
 void  io_coolant_checkIfFlowMeterActive();
+
+/**
+ * Get the flow rate in liters per minute.
+ * @return The flow rate in liters per minute.
+ * @note The flow rate is calculated based on the frequency of the PWM signal
+ */
 float io_coolant_getFlowRate();
--- firmware/quintuna/RSM/src/io/io_rPump.c
+++ firmware/quintuna/RSM/src/io/io_rPump.c
@@ -1,28 +1,29 @@
 #include "hw_i2cs.h"
 
+//SOURCE: https://datasheet.ciiva.com/41459/001083254-da-01-en-ic-dgtl-pot-mcp4661-502e-st-tssop-14-mcp-41459017.pdf?src-supplier=Conrad
 #define MAX_WIPER_VALUE (256.0f)
 #define MIN_WIPER_VALUE (0.0f)
 
-bool io_rPump_isPumpReady(const I2cDevice *device)
+bool io_rPump_isPumpReady()
 {
-    return hw_i2c_isTargetReady(device);
+    return hw_i2c_isTargetReady(&r_pump_i2c);
 }
 
-void io_rPump_write(const I2cDevice *device, uint8_t data)
+void io_rPump_write(uint8_t data)
 {
     uint8_t buffer[1];
     buffer[0] = data;
-    hw_i2c_memoryWrite(device, 0x02, buffer, sizeof(buffer));
+    hw_i2c_memoryWrite(&r_pump_i2c, 0x02, buffer, sizeof(buffer));
 }
 
-uint8_t io_rPump_read(const I2cDevice *device)
+uint8_t io_rPump_read()
 {
     uint8_t buffer[1];
-    hw_i2c_memoryRead(device, 0x02, buffer, sizeof(buffer));
+    hw_i2c_memoryRead(&r_pump_i2c, 0x02, buffer, sizeof(buffer));
     return buffer[0];
 }
 
-void io_rPump_setPercentage(const I2cDevice *device, float percentage)
+void io_rPump_setPercentage(float percentage)
 {
     if (percentage > 100.0f)
     {
@@ -33,12 +34,12 @@ void io_rPump_setPercentage(const I2cDevice *device, float percentage)
         percentage = 0.0f;
     }
     uint8_t data = (uint8_t)((1.0f - (percentage / 100.0f)) * MAX_WIPER_VALUE);
-    io_rPump_write(device, data);
+    io_rPump_write(data);
 }
 
-float io_rPump_readPercentage(const I2cDevice *device)
+float io_rPump_readPercentage()
 {
-    uint8_t data       = io_rPump_read(device);
+    uint8_t data       = io_rPump_read();
     float   percentage = (1.0f - ((float)data / MAX_WIPER_VALUE)) * 100.0f;
     return percentage;
 }
