108fe23469f708f95a91357c5b42592909f03f13
kryptoish <krish_thakur7@icloud.com>
2025-04-04 20:29:34 -0700

    fixes related to review

--- firmware/quintuna/FSM/src/io/io_brake.c
+++ firmware/quintuna/FSM/src/io/io_brake.c
@@ -10,9 +10,6 @@
 // Psi per Volt: (Max Pressure - Min Pressure) / (Max Input Voltage - Min Input Voltage)
 #define BRAKE_PSI_PER_VOLT (2500.0f / (BRAKE_PRESSURE_SC_THRESHOLD_V - BRAKE_PRESSURE_OC_THRESHOLD_V))
 
-// Unused Macros Removed:
-// - BRAKE_PEDAL_MIN_VOLTAGE, BRAKE_PEDAL_MAX_VOLTAGE, and BRAKE_PRESSURE_SENSOR_MAX_V
-
 static const BrakeConfig *config = NULL;
 
 // Checks if a given pressure sensor voltage is out-of-spec
@@ -36,14 +33,12 @@ void io_brake_init(const BrakeConfig *brake_config)
 
 bool io_brake_isActuated(void)
 {
-    return !hw_gpio_readPin(
-        config->nbspd_brake_pressed); // Brake is actuated when the brake pressed signal is low (active low)
+    return !hw_gpio_readPin(config->nbspd_brake_pressed);
 }
 
 float io_brake_getFrontPressurePsi(void)
 {
-    return pressureFromVoltage(
-        hw_adc_getVoltage(config->front_brake)); // Get the front brake sensor voltage and convert to pressure in Psi
+    return pressureFromVoltage(hw_adc_getVoltage(config->front_brake));
 }
 
 bool io_brake_frontPressureSensorOCSC(void)
@@ -52,8 +47,7 @@ bool io_brake_frontPressureSensorOCSC(void)
     return pressureSensorOCSC(front_pressure_voltage);
 }
 
-bool io_brake_hwOCSC(void) // idk really know wot this does
+bool io_brake_hwOCSC(void)
 {
-    return hw_gpio_readPin(
-        config->brake_hardware_ocsc); // Return the hardware overcurrent/short-circuit status for the brake system
+    return hw_gpio_readPin(config->brake_hardware_ocsc);
 }
--- firmware/quintuna/FSM/src/io/io_imu_config.h
+++ firmware/quintuna/FSM/src/io/io_imu_config.h
@@ -1,13 +1,4 @@
 #pragma once
 #include "io_imu.h"
-typedef struct
-{
-    float long_accel;
-    float lat_accel;
-    float z_accel;
-    float yaw_rate;
-    float pitch_rate;
-    float roll_rate;
-} ImuData;
 
 extern const imuConfig imu_config;
