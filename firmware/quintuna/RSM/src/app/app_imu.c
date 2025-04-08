fbdf0d2886749c43d0b71256250c698a3ea7dd97
rijwal <rijwalsangey@gmail.com>
2025-04-07 20:12:51 -0700

    can for brake shi

--- firmware/quintuna/RSM/src/app/app_imu.c
+++ firmware/quintuna/RSM/src/app/app_imu.c
@@ -1,5 +1,7 @@
 #include "io_imus.h"
 #include "app_imu.h"
+#include "app_canTx.h"
+
 
 void app_imu_broadcast(void) {
     float x_lin_accel, y_lin_accel, z_lin_accel;
@@ -8,5 +10,10 @@ void app_imu_broadcast(void) {
     io_imu_getLinAccel(&x_lin_accel, &y_lin_accel, &z_lin_accel);
     io_imu_getAngVel(&roll_ang_vel, &pitch_ang_vel, &yaw_ang_vel);
 
-
+    app_canTx_RSM_LinearAccelerationInX_set(x_lin_accel);
+    app_canTx_RSM_LinearAccelerationInY_set(y_lin_accel);
+    app_canTx_RSM_LinearAccelerationInZ_set(z_lin_accel);
+    app_canTx_RSM_RollAngularAcceleration_set(roll_ang_vel);
+    app_canTx_RSM_PitchAngularAcceleration_set(pitch_ang_vel);
+    app_canTx_RSM_YawAngularAcceleration_set(yaw_ang_vel);
 }
