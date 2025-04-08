96f891af6f20efcf58d02684c690fd1bbe4b3931
Kelm <lkevin2003@gmail.com>
2025-04-07 21:21:50 -0700

    just started data processing

--- validationtools/validationtools/process_cell_data.py
+++ validationtools/validationtools/process_cell_data.py
@@ -6,6 +6,8 @@ path = r"C:\Users\lkevi\OneDrive\Desktop\Coding\UBCFE\Data\CellCharData"
 def main():
     for cellFileName in os.listdir(path):
         cellFilePath = os.path.join(path, cellFileName)
+        rawCellData = pd.read_excel(cellFilePath, sheet_name=None)
+        
 
 
 if __name__ == "__main__":
