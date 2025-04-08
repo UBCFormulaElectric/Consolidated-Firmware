a562e5c4510b262ebe260d64cf3eece97cb86ae7
Kelm <lkevin2003@gmail.com>
2025-04-07 18:32:33 -0700

    removed taskmonitor

--- firmware/quintuna/BMS/src/io/io_taskMonitors.c
+++
@@ -1,8 +0,0 @@
-#include "io_taskMonitors.h"
-#include "main.h"
-
-const TaskMonitor task_1_hz_monitor   = { .handle = &Task1HzHandle, .attributes = &Task1Hz_attributes },
-                  task_100_hz_monitor = { .handle = &Task100HzHandle, .attributes = &Task100Hz_attributes },
-                  task_1_khz_monitor  = { .handle = &Task1kHzHandle, .attributes = &Task1kHz_attributes },
-                  task_can_rx_monitor = { .handle = &TaskCanRxHandle, .attributes = &TaskCanRx_attributes },
-                  task_can_tx_monitor = { .handle = &TaskCanTxHandle, .attributes = &TaskCanTx_attributes };
