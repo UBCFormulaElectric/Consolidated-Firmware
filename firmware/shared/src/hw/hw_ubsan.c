65eb308b6dde6a1c605c6cae69d660a1907a344c
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2025-04-27 17:57:16 -0700

    proper error handling on detection

--- firmware/shared/src/hw/hw_ubsan.c
+++ firmware/shared/src/hw/hw_ubsan.c
@@ -1,13 +1,48 @@
+#include "hw_error.h"
+
 // NOLINTBEGIN(bugprone-reserved-identifier)
-void __ubsan_handle_type_mismatch_v1(void *a, void *b) {}
-void __ubsan_handle_pointer_overflow(void *a, void *b, void *c) {}
-void __ubsan_handle_load_invalid_value(void *a, void *b) {}
-void __ubsan_handle_nonnull_arg(void *a) {}
-void __ubsan_handle_out_of_bounds(void *a, void *b) {}
-void __ubsan_handle_add_overflow(void *a, void *b, void *c) {}
-void __ubsan_handle_sub_overflow(void *a, void *b, void *c) {}
-void __ubsan_handle_vla_bound_not_positive(void *a, void *b) {}
-void __ubsan_handle_mul_overflow(void *a, void *b, void *c) {}
-void __ubsan_handle_negate_overflow(void *a, void *b) {}
-void __ubsan_handle_shift_out_of_bounds(void *a, void *b, void *c) {}
+void __ubsan_handle_type_mismatch_v1(void *a, void *b)
+{
+    Error_Handler();
+}
+void __ubsan_handle_pointer_overflow(void *a, void *b, void *c)
+{
+    Error_Handler();
+}
+void __ubsan_handle_load_invalid_value(void *a, void *b)
+{
+    Error_Handler();
+}
+void __ubsan_handle_nonnull_arg(void *a)
+{
+    Error_Handler();
+}
+void __ubsan_handle_out_of_bounds(void *a, void *b)
+{
+    Error_Handler();
+}
+void __ubsan_handle_add_overflow(void *a, void *b, void *c)
+{
+    Error_Handler();
+}
+void __ubsan_handle_sub_overflow(void *a, void *b, void *c)
+{
+    Error_Handler();
+}
+void __ubsan_handle_vla_bound_not_positive(void *a, void *b)
+{
+    Error_Handler();
+}
+void __ubsan_handle_mul_overflow(void *a, void *b, void *c)
+{
+    Error_Handler();
+}
+void __ubsan_handle_negate_overflow(void *a, void *b)
+{
+    Error_Handler();
+}
+void __ubsan_handle_shift_out_of_bounds(void *a, void *b, void *c)
+{
+    Error_Handler();
+}
 // NOLINTEND(bugprone-reserved-identifier)
