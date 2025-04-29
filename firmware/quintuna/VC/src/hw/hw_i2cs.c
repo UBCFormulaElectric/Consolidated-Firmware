4973fa8dd1246839727e021ca2f2161bdfc7e342
AmirTajaddodi <amir.tajadodi@gmail.com>
2025-04-29 13:19:12 -0700

    some small changes

--- firmware/quintuna/VC/src/hw/hw_i2cs.c
+++ firmware/quintuna/VC/src/hw/hw_i2cs.c
@@ -9,7 +9,7 @@ static I2cBus   i2c_bus_4 = { .handle = &hi2c5 };
 const I2cDevice pwr_mtr   = { .bus = &i2c_bus_4, .target_address = 0x01, .timeout_ms = 100 };
 
 const I2cDevice imu     = { .bus = &i2c_bus_2, .target_address = 0x6B, .timeout_ms = 100 };
-const I2cDevice bat_mtr = { .bus = &i2c_bus_1, .target_address = 0x01, .timeout_ms = 100 };
+const I2cDevice bat_mtr = { .bus = &i2c_bus_1, .target_address = 0x10, .timeout_ms = 100 };
 const I2cDevice pumps   = { .bus = &i2c_bus_3, .target_address = 0x2D, .timeout_ms = 100 };
 
 I2cBus *hw_i2c_getBusFromHandle(const I2C_HandleTypeDef *handle)
