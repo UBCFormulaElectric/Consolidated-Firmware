11cfffdeb588ef6306f4aaf127546b9d464e3929
Jiajun-Huang <jhuang430@qq.com>
2025-04-30 05:39:32 -0700

    take boolean tsim

--- firmware/quintuna/DAM/src/io/io_tsim.c
+++ firmware/quintuna/DAM/src/io/io_tsim.c
@@ -1,22 +1,11 @@
 #include "io_tsim.h"
 #include "hw_gpios.h"
 
-void io_tsim_set_red_high(void)
+void io_tsim_set_red(bool state)
 {
-    hw_gpio_writePin(&tsim_red_en_pin, true);
+    hw_gpio_writePin(&tsim_red_en_pin, state);
 }
-
-void io_tsim_set_red_low(void)
+void io_tsim_set_green(bool state)
 {
-    hw_gpio_writePin(&tsim_red_en_pin, false);
-}
-
-void io_tsim_set_green_high(void)
-{
-    hw_gpio_writePin(&ntsim_green_en_pin, true);
-}
-
-void io_tsim_set_green_low(void)
-{
-    hw_gpio_writePin(&ntsim_green_en_pin, false);
+    hw_gpio_writePin(&ntsim_green_en_pin, state);
 }
--- firmware/quintuna/DAM/src/io/io_tsim.h
+++ firmware/quintuna/DAM/src/io/io_tsim.h
@@ -3,22 +3,5 @@
 #include <stdbool.h>
 #include "app_utils.h"
 
-/**
- * Turn on the red light on the TSIM
- */
-void io_tsim_set_red_high(void);
-
-/**
- * Turn off the red light on the TSIM
- */
-void io_tsim_set_red_low(void);
-
-/**
- * Turn on the green light on the TSIM
- */
-void io_tsim_set_green_high(void);
-
-/**
- * Turn off the green light on the TSIM
- */
-void io_tsim_set_green_low(void);
+void io_tsim_set_red(bool state);
+void io_tsim_set_green(bool state);
