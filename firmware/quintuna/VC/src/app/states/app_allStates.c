a69a7c16131caecd0b3f105319252bf5c1e7f8e7
Aditya-Dhiman4 <aditya16dhiman@gmail.com>
2025-04-30 22:16:38 -0700

    added sbg to quintuna vc, needs testing

--- firmware/quintuna/VC/src/app/states/app_allStates.c
+++ firmware/quintuna/VC/src/app/states/app_allStates.c
@@ -1,5 +1,11 @@
 #include "app_allStates.h"
+#include "app_sbgEllipse.h"
+#include "io_sbgEllipse.h"
 
-void app_allStates_runOnTick100Hz(void) {}
+void app_allStates_runOnTick100Hz(void)
+{
+    io_sbgEllipse_handleLogs();
+    app_sbgEllipse_broadcast();
+}
 
 void app_allStates_runOnTick1Hz(void) {}
