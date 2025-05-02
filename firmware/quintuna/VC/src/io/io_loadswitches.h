96dbf8dd8bf329aad9cd8bfa001b2e5701897ad6
AmirTajaddodi <amir.tajadodi@gmail.com>
2025-05-02 15:48:51 -0700

    basic io stuf

---
+++ firmware/quintuna/VC/src/io/io_loadswitches.h
@@ -0,0 +1,12 @@
+#pragma once 
+
+#include "io_loadswitch.h"
+
+extern const ST_LoadSwitch inv_rsm_loadswitch;
+extern const ST_LoadSwitch inv_bms_loadswitch;
+extern const ST_LoadSwitch front_loadswitch;
+extern const TI_LoadSwitch rl_pump_loadswitch;
+extern const TI_LoadSwitch rr_pump_loadswitch;
+extern const TI_LoadSwitch f_pump_loadswitch;
+extern const ST_LoadSwitch rad_fan_loadswitch;
+ 
