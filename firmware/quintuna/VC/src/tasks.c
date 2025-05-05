833d8ddc77c501df7d002919d305d1507307a0a7
Amir Tajaddoditalab <amir.tajadodi@gmail.com>
2025-05-04 18:59:09 -0700

    added a little cheeky CAN

--- firmware/quintuna/VC/src/tasks.c
+++ firmware/quintuna/VC/src/tasks.c
@@ -13,6 +13,7 @@
 #include "hw_usb.h"
 #include "hw_resetReason.h"
 #include "hw_hardFaultHandler.h"
+#include "hw_cans.h"
 
 // chimera
 #include "hw_chimeraConfig_v2.h"
@@ -120,20 +121,25 @@ _Noreturn void tasks_run1kHz(void)
 
 _Noreturn void tasks_runCanTx(void)
 {
-    osDelay(osWaitForever);
-
     for (;;)
     {
-        // CanMsg tx_msg = io_canQueue_popTx();
-        // LOG_IF_ERR(hw_can_transmit(&can1, &tx_msg));
+        CanMsg tx_msg = io_canQueue_popTx();
+        if (tx_msg.is_fd)
+        {
+            hw_fdcan_transmit(&can1, &tx_msg);
+        }
+        else
+        {
+            //this logic is going to be changed once we have the 
+            //json to can code merged in
+            hw_can_transmit(&can2, &tx_msg);
+            hw_can_transmit(&can3,  &tx_msg);
+        }
     }
 }
 
 _Noreturn void tasks_runCanRx(void)
 {
-    // io_chimera_sleepTaskIfEnabled();
-    osDelay(osWaitForever);
-
     for (;;)
     {
         jobs_runCanRx_tick();
