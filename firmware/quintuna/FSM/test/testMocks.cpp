e0c44d1b98aef79cdbed20e4613bd4f9158d44c6
kryptoish <krish_thakur7@icloud.com>
2025-05-05 19:25:41 -0700

    format

--- firmware/quintuna/FSM/test/testMocks.cpp
+++ firmware/quintuna/FSM/test/testMocks.cpp
@@ -1,6 +1,8 @@
-extern "C" {
-    #include "io_canTx.h"
-    void io_canTx_init(void (*transmit_tx_msg_func)(const JsonCanMsg*)) {
+extern "C"
+{
+#include "io_canTx.h"
+    void io_canTx_init(void (*transmit_tx_msg_func)(const JsonCanMsg *))
+    {
         (void)transmit_tx_msg_func;
     }
 }
