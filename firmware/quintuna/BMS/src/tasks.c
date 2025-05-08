a16bcc033416069a3e805b520ee6bbe084a86922
Amir Tajaddoditalab <96522772+AmirTajaddodi@users.noreply.github.com>
2025-05-07 21:07:23 -0700

    Amir/bms can and jobs config (#1584)
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    ### Resolved Tickets
    <!-- Link any tickets that this PR resolves. -->

--- firmware/quintuna/BMS/src/tasks.c
+++ firmware/quintuna/BMS/src/tasks.c
@@ -16,6 +16,7 @@
 #include "hw_chimeraConfig_v2.h"
 #include "hw_chimera_v2.h"
 #include "hw_resetReason.h"
+#include "jobs.h"
 #include "shared.pb.h"
 
 void tasks_runChimera(void)
@@ -34,7 +35,11 @@ void tasks_init(void)
     hw_usb_init();
     hw_adcs_chipsInit();
     hw_pwms_init();
+    hw_can_init(&can1);
+    hw_can_init(&can2);
     hw_watchdog_init(hw_watchdogConfig_refresh, hw_watchdogConfig_timeoutCallback);
+
+    jobs_init();
 
     app_canTx_BMS_ResetReason_set((CanResetReason)hw_resetReason_get());
 }
@@ -45,6 +50,10 @@ void tasks_run1Hz(void)
     uint32_t                start_ticks = osKernelGetTickCount();
     for (;;)
     {
+        if (!hw_chimera_v2_enabled)
+        {
+            jobs_run1Hz_tick();
+        }
         start_ticks += period_ms;
         osDelayUntil(start_ticks);
     }
@@ -56,6 +65,10 @@ void tasks_run100Hz(void)
     uint32_t                start_ticks = osKernelGetTickCount();
     for (;;)
     {
+        if (!hw_chimera_v2_enabled)
+        {
+            jobs_run100Hz_tick();
+        }
         start_ticks += period_ms;
         osDelayUntil(start_ticks);
     }
@@ -79,7 +92,7 @@ void tasks_runCanTx(void)
     for (;;)
     {
         CanMsg tx_msg = io_canQueue_popTx();
-        hw_can_transmit(&can1, &tx_msg);
+        hw_fdcan_transmit(&can1, &tx_msg);
     }
 }
 
@@ -87,7 +100,6 @@ void tasks_runCanRx(void)
 {
     for (;;)
     {
-        const CanMsg rx_msg = io_canQueue_popRx();
-        UNUSED(rx_msg);
+        jobs_runCanRx_tick();
     }
 }
