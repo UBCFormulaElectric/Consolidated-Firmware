b0b5a2ae9fdfb1a425b01848338dd5138d21d368
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2025-05-08 15:38:21 -0500

    big one

---
+++ firmware/shared/src/io/io_potentiometer.c
@@ -0,0 +1,40 @@
+#include "io_potentiometer.h"
+#include "hw_utils.h"
+#include "hw_i2cs.h"
+#include <stdbool.h>
+
+#define MAX_WIPER_VALUE (256) 
+
+typedef enum {
+    POTENTIOMETER_WRITE_CMD = 0x00,
+    POTENTIOMETER_INC_CMD = 0x01,
+    POTENTIOMETER_DEC_CMD = 0x02,
+    POTENTIOMETER_READ_CMD = 0x03,
+} POTENTIOMETER_CMD;
+
+typedef enum {
+    POTENTIOMETER_WIPER_0_ADD = 0x00,
+    POTENTIOMETER_WIPER_1_ADD = 0x01,
+} POTENTIOMETER_ADD;
+
+ExitCode io_potentiometer_readPercentage(Potentiometer *potentiometer, POTENTIOMETER_ADD wiper, uint8_t *dest){
+    uint8_t data;
+    RETURN_IF_ERR(io_rPump_read(potentiometer->i2c_handle, wiper, &data));
+    *dest = data/MAX_WIPER_VALUE * 100;
+    return EXIT_CODE_OK;
+}
+
+ExitCode io_potentiometer_writePercentage(Potentiometer *potentiometer, POTENTIOMETER_ADD wiper, uint8_t percentage){
+    return io_potentiometer_writeWiper(potentiometer->i2c_handle, wiper, CLAMP(percentage, 0, 100) * MAX_WIPER_VALUE / 100);
+}
+
+ExitCode io_potentiometer_readWiper(Potentiometer *potentiometer, POTENTIOMETER_ADD wiper, uint8_t *dest) {
+    return hw_i2c_memoryRead(potentiometer->i2c_handle, wiper, dest, sizeof(dest));
+}
+
+ExitCode io_potentiometer_writeWiper(Potentiometer *potentiometer, POTENTIOMETER_ADD wiper, uint8_t data) {
+
+}
+
+
+
---
+++ firmware/shared/src/io/io_potentiometer.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#ifdef TARGET_EMBEDDED
+
+#include "hw_i2cs.h"
+
+typedef struct {
+    const I2cDevice *i2c_handle;
+} Potentiometer
+
+#else
+#include "app_utils.h"
+EMPTY_STRUCT(Potentiometer)
+#endif
+
+/**
+ * Reads the potentiometer wiper value as a percentage (0 to 100) of its maximum range.
+ * @param potentiometer Pointer to the potentiometer object
+ * @param wiper The wiper to read
+ * @param dest Pointer to store the read percentage
+ * @return If read was successful
+ */
+ExitCode io_potentiometer_readPercentage(Potentiometer *potentiometer, POTENTIOMETER_ADD wiper, uint8_t *dest);
+
+/**
+ * Writes the potentiometer wiper value as a percentage (0 to 100) of its maximum range.
+ * @param potentiometer Pointer to the potentiometer object
+ * @param wiper The wiper to write
+ * @param percentage The percentage to write to the potentiometer
+ * @return If write was successful
+ */
+ExitCode io_potentiometer_writePercentage(Potentiometer *potentiometer, POTENTIOMETER_ADD wiper, uint8_t percentage);
+
+/**
+ * Reads the potentiometer wiper value.
+ * @param potentiometer Pointer to the potentiometer object
+ * @param wiper The wiper to read
+ * @param dest Pointer to store the read wiper value
+ * @return If read was successful
+ */
+ExitCode io_potentiometer_readWiper(Potentiometer *potentiometer, POTENTIOMETER_ADD wiper, uint8_t *dest);
+
+/**
+ * Writes the potentiometer wiper value.
+ * @param potentiometer Pointer to the potentiometer object
+ * @param wiper The wiper to write
+ * @param data The wiper value to write to the potentiometer
+ * @return If write was successful
+ */
+ExitCode io_potentiometer_writeWiper(Potentiometer *potentiometer, POTENTIOMETER_ADD wiper, uint8_t data);
