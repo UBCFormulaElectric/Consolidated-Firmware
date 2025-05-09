4bb809f5d94bfbd94d6870b020d215cff5d47195
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2025-05-09 14:10:37 -0700

    w data capture module

--- scripts/code_generation/jsoncan/src/codegen/c_generation/app_can_data_capture_module.py
+++ scripts/code_generation/jsoncan/src/codegen/c_generation/app_can_data_capture_module.py
@@ -7,14 +7,14 @@ from .cmodule import CModule
 
 class AppCanDataCaptureModule(CModule):
     def __init__(self, db: CanDatabase):
-        self._db = db
+        self._messages = list(db.msgs.values())
 
     def header_template(self):
         j2_env = j2.Environment(loader=j2.BaseLoader(), extensions=["jinja2.ext.loopcontrols"])
         template = j2_env.from_string(load_template("app_canDataCapture.h.j2"))
-        return template.render(messages=list(self._db.msgs.values()))
+        return template.render(messages=self._messages)
 
     def source_template(self):
         j2_env = j2.Environment(loader=j2.BaseLoader(), extensions=["jinja2.ext.loopcontrols"])
         template = j2_env.from_string(load_template("app_canDataCapture.c.j2"))
-        return template.render(messages=list(self._db.msgs.values()))
+        return template.render(messages=self._messages)
