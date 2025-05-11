888096aaa9c366279bedbeedea70ee2a5906fad2
Amir Tajaddoditalab <amir.tajadodi@gmail.com>
2025-05-11 16:49:37 -0700

    added shutdown node detection

--- firmware/quintuna/VC/src/app/states/app_inverterOnState.c
+++ firmware/quintuna/VC/src/app/states/app_inverterOnState.c
@@ -5,24 +5,22 @@
 #include <app_canUtils.h>
 #include <stdbool.h>
 
-
 static const PowerState power_manager_shutdown_inverter_on = {
-    .efuses = {
-    [EFUSE_CHANNEL_F_INV] = true,
-    [EFUSE_CHANNEL_RSM] = true,
-    [EFUSE_CHANNEL_BMS] = true,
-    [EFUSE_CHANNEL_R_INV] = true,
-    [EFUSE_CHANNEL_DAM] = true,
-    [EFUSE_CHANNEL_FRONT] = true,
-    [EFUSE_CHANNEL_RL_PUMP] = false,
-    [EFUSE_CHANNEL_RR_PUMP] = false,
-    [EFUSE_CHANNEL_F_PUMP] = false,
-    [EFUSE_CHANNEL_L_RAD] = false,
-    [EFUSE_CHANNEL_R_RAD] = false
-    },
+    .efuses = { [EFUSE_CHANNEL_F_INV]   = true,
+                [EFUSE_CHANNEL_RSM]     = true,
+                [EFUSE_CHANNEL_BMS]     = true,
+                [EFUSE_CHANNEL_R_INV]   = true,
+                [EFUSE_CHANNEL_DAM]     = true,
+                [EFUSE_CHANNEL_FRONT]   = true,
+                [EFUSE_CHANNEL_RL_PUMP] = false,
+                [EFUSE_CHANNEL_RR_PUMP] = false,
+                [EFUSE_CHANNEL_F_PUMP]  = false,
+                [EFUSE_CHANNEL_L_RAD]   = false,
+                [EFUSE_CHANNEL_R_RAD]   = false },
 };
 
-static void inverterOnStateRunOnEntry(void) {
+static void inverterOnStateRunOnEntry(void)
+{
     app_canTx_VC_State_set(VC_INVERTER_ON_STATE);
     app_powerManager_updateConfig(power_manager_shutdown_inverter_on);
 }
