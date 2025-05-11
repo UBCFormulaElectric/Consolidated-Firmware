8381b75439b5e75a96b4ff67b36b3bd0e32ae636
AmirTajaddodi <amir.tajadodi@gmail.com>
2025-05-10 22:45:43 -0700

    added can messages

--- firmware/quintuna/VC/src/app/states/app_bmsOnState.c
+++ firmware/quintuna/VC/src/app/states/app_bmsOnState.c
@@ -1,7 +1,31 @@
-#include "app_allStates.h"
 #include "app_states.h"
+#include "app_loadswitches.h"
+#include "app_powerManager.h"
+#include <app_canTx.h>
+#include <app_canUtils.h>
+#include <stdbool.h>
 
-static void bmsOnStateRunOnEntry(void) {}
+static const PowerState power_manager_shutdown_bms_on = {
+    .efuses = {
+    [EFUSE_CHANNEL_F_INV] = true,
+    [EFUSE_CHANNEL_RSM] = true,
+    [EFUSE_CHANNEL_BMS] = true,
+    [EFUSE_CHANNEL_R_INV] = true,
+    [EFUSE_CHANNEL_DAM] = true,
+    [EFUSE_CHANNEL_FRONT] = true,
+    [EFUSE_CHANNEL_RL_PUMP] = false,
+    [EFUSE_CHANNEL_RR_PUMP] = false,
+    [EFUSE_CHANNEL_F_PUMP] = false,
+    [EFUSE_CHANNEL_L_RAD] = false,
+    [EFUSE_CHANNEL_R_RAD] = false
+    },
+};
+
+
+static void bmsOnStateRunOnEntry(void) {
+    app_canTx_VC_State_set(VC_BMS_ON_STATE);
+    app_powerManager_updateConfig(power_manager_shutdown_bms_on);
+}
 static void bmsOnStateRunOnTick1Hz(void) {}
 static void bmsOnStateRunOnTick100Hz(void) {}
 static void bmsOnStateRunOnExit(void) {}
