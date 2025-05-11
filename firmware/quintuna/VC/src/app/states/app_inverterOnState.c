e7a7dfbf75f0b69266cc5ad2345dea2ee8a33e69
AmirTajaddodi <amir.tajadodi@gmail.com>
2025-05-10 22:45:43 -0700

    added can messages

--- firmware/quintuna/VC/src/app/states/app_inverterOnState.c
+++ firmware/quintuna/VC/src/app/states/app_inverterOnState.c
@@ -1,6 +1,32 @@
 #include "app_states.h"
+#include "app_loadswitches.h"
+#include "app_powerManager.h"
+#include <app_canTx.h>
+#include <app_canUtils.h>
+#include <stdbool.h>
 
-static void inverterOnStateRunOnEntry(void) {}
+
+static const PowerState power_manager_shutdown_inverter_on = {
+    .efuses = {
+    [EFUSE_CHANNEL_F_INV] = true,
+    [EFUSE_CHANNEL_RSM] = true,
+    [EFUSE_CHANNEL_BMS] = true,
+    [EFUSE_CHANNEL_R_INV] = true,
+    [EFUSE_CHANNEL_DAM] = true,
+    [EFUSE_CHANNEL_FRONT] = true,
+    [EFUSE_CHANNEL_RL_PUMP] = false,
+    [EFUSE_CHANNEL_RR_PUMP] = false,
+    [EFUSE_CHANNEL_F_PUMP] = false,
+    [EFUSE_CHANNEL_L_RAD] = false,
+    [EFUSE_CHANNEL_R_RAD] = false
+    },
+};
+
+static void inverterOnStateRunOnEntry(void) {
+    app_canTx_VC_State_set(VC_INVERTER_ON_STATE);
+    app_powerManager_updateConfig(power_manager_shutdown_inverter_on);
+}
+
 static void inverterOnStateRunOnTick1Hz(void) {}
 static void inverterOnStateRunOnTick100Hz(void) {}
 static void inverterOnStateRunOnExit(void) {}
