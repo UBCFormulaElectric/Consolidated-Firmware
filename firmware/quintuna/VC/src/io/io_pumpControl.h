9721e282455a7ce468c9f0e60cabae89d05f19e3
AmirTajaddodi <amir.tajadodi@gmail.com>
2025-05-12 21:40:24 -0700

    added pumps

---
+++ firmware/quintuna/VC/src/io/io_pumpControl.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "app_utils.h"
+#include "hw_i2cs.h"
+#include "io_potentiometer.h"
+#include "app_uitls.h"
+#include <stdint.h>
+
+typedef enum{
+    RR_PUMP = 0,
+    F_PUMP = 1
+} PUMPS;
+
+
+ExitCode io_pumpControl_setPercentage(uint8_t percentage, PUMPS pump);
+
+ExitCode io_pumpControl_readPercentage(uint8_t *percentage, PUMPS pump);
