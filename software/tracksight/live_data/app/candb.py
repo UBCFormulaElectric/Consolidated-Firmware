ebff12c59fcc05955452e42569514c23cbe9ef3a
Jiajun-Huang <jhuang430@qq.com>
2025-05-17 12:38:55 -0700

    change how it start json can

--- software/tracksight/live_data/app/candb.py
+++ software/tracksight/live_data/app/candb.py
@@ -1,10 +1,12 @@
-from concurrent.futures import ThreadPoolExecutor
 import os
+from concurrent.futures import ThreadPoolExecutor
+
 import requests
 
 _dockerized = os.environ.get("DOCKERIZED") == "1"
 if not _dockerized:
     import sys
+
     sys.path.insert(
         0,
         os.path.abspath(
@@ -13,6 +15,7 @@ if not _dockerized:
             )
         ),
     )
+
 
 def _download_file(commit_sha, file, folder_path, save_dir):
     file_url = f"https://raw.githubusercontent.com/UBCFormulaElectric/Consolidated-Firmware/{commit_sha}/{file['path']}"
@@ -25,16 +28,19 @@ def _download_file(commit_sha, file, folder_path, save_dir):
                 f.write(chunk)
     print(f"Downloaded: {file['path']}")
 
+
 _car = os.environ.get("CAR_NAME") or "quintuna"
 bus_configs_path = os.path.abspath(
     os.path.join(os.path.dirname(__file__), "../bus_configs", _car)
 )
 _cached_commit_sha: str | None = None
-def fetch_jsoncan_configs(commit_sha: str, force = False) -> str:
+
+
+def fetch_jsoncan_configs(commit_sha: str, force=False) -> str:
     """
     Fetches the jsoncan configs for a given sha
     RETURNS: the path of the folder containing the jsoncan configs for car/ecu
-    NOTE: this will 
+    NOTE: this will
     NOTE: this determines which car you are driving with an environment variable.
     """
     global _cached_commit_sha
@@ -51,13 +57,19 @@ def fetch_jsoncan_configs(commit_sha: str, force = False) -> str:
     response.raise_for_status()
 
     # Filter for files in the specified folder
-    files: list[str] = [file for file in response.json().get("tree", []) if file["path"].startswith(folder_path) and file["type"] == "blob"]
+    files: list[str] = [
+        file
+        for file in response.json().get("tree", [])
+        if file["path"].startswith(folder_path) and file["type"] == "blob"
+    ]
 
     # DISK MUTATIONS HERE AND BELOW
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
     with ThreadPoolExecutor(max_workers=46) as executor:
-        executor.map(lambda file: _download_file(commit_sha, file, folder_path, save_dir), files)
+        executor.map(
+            lambda file: _download_file(commit_sha, file, folder_path, save_dir), files
+        )
     _cached_commit_sha = commit_sha
     return save_dir
 
@@ -67,4 +79,19 @@ def fetch_jsoncan_configs(commit_sha: str, force = False) -> str:
 # from jsoncan.src.can_database import CanDatabase
 # can_db = CanDatabase()
 from jsoncan.src.json_parsing.json_can_parsing import JsonCanParser
-live_can_db = JsonCanParser(os.path.join(bus_configs_path, "e12121d")).make_database()
+from settings import CAR_NAME
+
+json_can_config_root = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "..",
+    "..",
+    "..",
+    "..",
+    "can_bus",
+    CAR_NAME,
+)
+
+if(not os.path.lexists(json_can_config_root)):
+    raise Exception("json can path does not exist, did you pass correct CAN_NAME")
+
+live_can_db = JsonCanParser(json_can_config_root).make_database()
