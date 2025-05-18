b5d1308fc24907c81ba014c1eb4ef336778bf458
DJ90864 <dikshitjain664@gmail.com>
2025-05-18 16:02:59 -0700

    formatting

--- firmware/quintuna/BMS/src/app/app_shdnNode.c
+++ firmware/quintuna/BMS/src/app/app_shdnNode.c
@@ -3,9 +3,9 @@
 #include "io_bmsShdn.h"
 #include "app_canTx.h"
 
-static const BoardShdnNode msd_ok_node      = { &io_bmsShdn_msd_shdn_sns_pin_get,    &app_canTx_BMS_MSDShdnOKStatus_set };
-static const BoardShdnNode hv_p_ok_node     = { &io_bmsShdn_hv_p_intlck_sns_pin_get, &app_canTx_BMS_HVPShdnOKStatus_set };
-static const BoardShdnNode hv_n_ok_node     = { &io_bmsShdn_hv_n_intlck_sns_pin_get, &app_canTx_BMS_HVNShdnOKStatus_set };
+static const BoardShdnNode msd_ok_node  = { &io_bmsShdn_msd_shdn_sns_pin_get, &app_canTx_BMS_MSDShdnOKStatus_set };
+static const BoardShdnNode hv_p_ok_node = { &io_bmsShdn_hv_p_intlck_sns_pin_get, &app_canTx_BMS_HVPShdnOKStatus_set };
+static const BoardShdnNode hv_n_ok_node = { &io_bmsShdn_hv_n_intlck_sns_pin_get, &app_canTx_BMS_HVNShdnOKStatus_set };
 
 void app_shdnLoop_broadcast(void)
 {
