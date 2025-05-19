3599cc9b1991525f86ea1cdca64f7f5158ef74c9
AmirTajaddodi <amir.tajadodi@gmail.com>
2025-05-18 21:33:24 -0700

    added pump control

--- firmware/quintuna/VC/src/app/states/app_allStates.c
+++ firmware/quintuna/VC/src/app/states/app_allStates.c
@@ -1,10 +1,11 @@
 #include "app_allStates.h"
+#include "app_pumpControl.h"
 #include "app_powerManager.h"
 
 void app_allStates_runOnTick100Hz(void)
 {
     app_powerManager_EfuseProtocolTick_100Hz();
-    app_powerManger_MonitorPumps();
+    app_pumpControl_MonitorPumps();
 }
 
 void app_allStates_runOnTick1Hz(void) {}
