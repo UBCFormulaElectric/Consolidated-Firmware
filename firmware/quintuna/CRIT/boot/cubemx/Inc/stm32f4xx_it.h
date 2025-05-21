68f30de78fcb8aa18e700e67da62624e65243d24
Jugveer-Sandher <62398344+Jugveer-Sandher@users.noreply.github.com>
2025-05-20 21:41:15 -0700

    Quintuna F4 Bootloader (#1521)
    ### Changelist 
    Bootloader compatible with quintuna F4 boards and yeeted quadruna
    bootloader configs from `canup`.
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    ### Resolved Tickets
    <!-- Link any tickets that this PR resolves. -->
    
    ---------
    
    Co-authored-by: Edwin <20777515+Lucien950@users.noreply.github.com>
    Co-authored-by: AmirTajaddodi <amir.tajadodi@gmail.com>

--- firmware/quintuna/CRIT/boot/cubemx/Inc/stm32f4xx_it.h
+++ firmware/quintuna/CRIT/boot/cubemx/Inc/stm32f4xx_it.h
@@ -53,9 +53,10 @@ extern "C"
     void BusFault_Handler(void);
     void UsageFault_Handler(void);
     void DebugMon_Handler(void);
-    void CAN1_RX0_IRQHandler(void);
-    void CAN1_RX1_IRQHandler(void);
     void TIM6_IRQHandler(void);
+    void CAN2_TX_IRQHandler(void);
+    void CAN2_RX0_IRQHandler(void);
+    void CAN2_RX1_IRQHandler(void);
     /* USER CODE BEGIN EFP */
 
     /* USER CODE END EFP */
