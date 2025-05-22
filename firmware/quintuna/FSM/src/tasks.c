f89434ed73cef17e29a16c08b6c38297793fc5a6
Edwin <20777515+Lucien950@users.noreply.github.com>
2025-05-21 19:02:43 -0700

    Smart Chimera Enable (#1589)
    ### Changelist 
    This PR develops a mechanism to enable and disable chimera through USB
    connection and disconnection interrupts.
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    ### Resolved Tickets
    <!-- Link any tickets that this PR resolves. -->

--- firmware/quintuna/FSM/src/tasks.c
+++ firmware/quintuna/FSM/src/tasks.c
@@ -40,7 +40,7 @@ void tasks_init(void)
     // Re-enable watchdog.
     __HAL_DBGMCU_FREEZE_IWDG();
     hw_hardFaultHandler_init();
-    hw_usb_init();
+    ASSERT_EXIT_OK(hw_usb_init());
     hw_adcs_chipsInit();
     hw_can_init(&can);
 
@@ -61,7 +61,8 @@ void tasks_run1Hz(void)
     uint32_t                start_ticks = osKernelGetTickCount();
     for (;;)
     {
-        jobs_run1Hz_tick();
+        if (!hw_chimera_v2_enabled)
+            jobs_run1Hz_tick();
 
         start_ticks += period_ms;
         osDelayUntil(start_ticks);
--- firmware/shared/src/hw/hw_usb.c
+++ firmware/shared/src/hw/hw_usb.c
@@ -1,6 +1,10 @@
 #include "hw_usb.h"
+
+#include <cmsis_os2.h>
+#include <FreeRTOS.h>
+#include <task.h>
+
 #include "usbd_cdc_if.h"
-#include "cmsis_os.h"
 #include "io_log.h"
 
 // Select between FS or HS methods.
@@ -22,7 +26,7 @@ extern USBD_HandleTypeDef hUsbDeviceHS;
 #endif
 
 // Setup the rx queue.
-#define RX_QUEUE_SIZE (2048)
+#define RX_QUEUE_SIZE (20)
 static StaticQueue_t              rx_queue_control_block;
 static uint8_t                    rx_queue_buffer[RX_QUEUE_SIZE];
 static const osMessageQueueAttr_t rx_queue_attr = { .name      = "USB RX Queue",
@@ -33,24 +37,18 @@ static const osMessageQueueAttr_t rx_queue_attr = { .name      = "USB RX Queue",
                                                     .mq_size   = sizeof(rx_queue_buffer) };
 static osMessageQueueId_t         rx_queue_id   = NULL;
 
-void hw_usb_init()
+ExitCode hw_usb_init()
 {
     if (rx_queue_id == NULL)
     {
         rx_queue_id = osMessageQueueNew(RX_QUEUE_SIZE, sizeof(uint8_t), &rx_queue_attr);
         if (rx_queue_id == NULL)
         {
-            LOG_ERROR("USB: Failed to init RX queue.");
-        }
-        else
-        {
-            LOG_INFO("USB: Initialized RX queue.");
+            LOG_ERROR("USB: Failed to create RX queue.");
+            return EXIT_CODE_ERROR;
         }
     }
-    else
-    {
-        LOG_WARN("USB: RX queue already exists when attempting to init USB driver.");
-    }
+    return EXIT_CODE_OK;
 }
 
 bool hw_usb_checkConnection()
@@ -58,67 +56,39 @@ bool hw_usb_checkConnection()
     return USB_DEVICE_HANDLER.dev_state == USBD_STATE_CONFIGURED;
 }
 
-bool hw_usb_transmit(uint8_t *msg, uint16_t len)
+ExitCode hw_usb_transmit(uint8_t *msg, const uint16_t len)
 {
-    uint8_t status = TRANSMIT(msg, len);
+    const uint8_t status = TRANSMIT(msg, len);
     if (status != USBD_OK)
     {
         LOG_WARN("USB: Transmit handle returned %d status code instead of USBD_OK.", status);
-        return false;
+        return EXIT_CODE_ERROR;
     }
-
-    return true;
+    return EXIT_CODE_OK;
 }
 
-bool hw_usb_receive(uint8_t *dest, uint32_t len, uint32_t timeout_ms)
+ExitCode hw_usb_receive(uint8_t *dest, const uint32_t timeout_ms)
 {
     if (rx_queue_id == NULL)
     {
         LOG_ERROR("USB: Peripheral not initialized before attempting receive from RX queue.");
-        return false;
+        return EXIT_CODE_OK;
     }
-
-    if (len > RX_QUEUE_SIZE)
+    const osStatus_t status = osMessageQueueGet(rx_queue_id, dest, NULL, timeout_ms);
+    if (status != osOK)
     {
-        LOG_ERROR("USB: Requested to receive %d messages from RX queue, but size of queue is %d.", len, RX_QUEUE_SIZE);
-
-        return false;
-    }
-
-    // Wait either until we timeout, or when the queue is full enough.
-    uint32_t start_ms   = osKernelGetTickCount();
-    uint32_t queued_len = osMessageQueueGetCount(rx_queue_id);
-    while (osKernelGetTickCount() - start_ms <= timeout_ms && queued_len < len)
-    {
-        queued_len = osMessageQueueGetCount(rx_queue_id);
-        osDelay(5);
-    }
-
-    // Check that we have enough messages in the queue to populate the dest buffer.
-    if (queued_len < len)
-    {
-        return false;
-        LOG_WARN("USB: Receive timed out.");
-    }
-
-    // Loop through every index in the buffer.
-    for (uint32_t i = 0; i < len; i += 1)
-    {
-        // Dump the byte.
-        osStatus_t status = osMessageQueueGet(rx_queue_id, &dest[i], NULL, osWaitForever);
-
-        // Check success.
-        if (status != osOK)
+        if (status == osErrorTimeout)
         {
-            LOG_WARN("USB: Queue pop returned non-ok status %d", status);
-            return false;
+            LOG_WARN("USB: Timeout occurred while waiting for RX queue.");
+            return EXIT_CODE_TIMEOUT;
         }
+        LOG_WARN("USB: Queue pop returned non-ok status %d", status);
+        return EXIT_CODE_ERROR;
     }
-
-    return true;
+    return EXIT_CODE_OK;
 }
 
-bool hw_usb_pushRxMsgToQueue(uint8_t *msg, uint32_t len)
+bool hw_usb_pushRxMsgToQueue(const uint8_t *msg, const uint32_t len)
 {
     if (rx_queue_id == NULL)
     {
@@ -126,29 +96,72 @@ bool hw_usb_pushRxMsgToQueue(uint8_t *msg, uint32_t len)
         return false;
     }
 
-    uint32_t space = osMessageQueueGetSpace(rx_queue_id);
+    const uint32_t space = osMessageQueueGetSpace(rx_queue_id);
     if (len > space)
     {
         LOG_ERROR("USB: Receiving message that will overflow RX queue.");
         return false;
     }
-
     for (uint32_t i = 0; i < len; i += 1)
     {
         // Note: timeout cannot be non-zero in an IRQ.
-        osStatus_t status = osMessageQueuePut(rx_queue_id, &msg[i], 0, 0);
+        const osStatus_t status = osMessageQueuePut(rx_queue_id, &msg[i], 0, 0);
         if (status != osOK)
         {
             LOG_ERROR(
                 "USB: Error status encountered when pushing to RX queue, "
-                "osMessageQueuePut returned %d insteado of osOK.",
+                "osMessageQueuePut returned %d instead of osOK.",
                 status);
             return false;
         }
     }
-
     return true;
 }
+
+// Connection handling
+
+static bool         usb_connected = false;
+static TaskHandle_t usb_task      = NULL;
+
+void hw_usb_connect_callback()
+{
+    usb_connected = true;
+    if (usb_task == NULL)
+    {
+        LOG_WARN("USB: No task to notify.");
+        return;
+    }
+    BaseType_t higherPriorityTaskWoken = pdFALSE;
+    // notify the task which is waiting for the USB connection
+    vTaskNotifyGiveFromISR(usb_task, &higherPriorityTaskWoken);
+    portYIELD_FROM_ISR(higherPriorityTaskWoken);
+}
+
+void hw_usb_disconnect_callback()
+{
+    usb_connected = false;
+}
+
+bool hw_usb_connected()
+{
+    return usb_connected;
+}
+
+void hw_usb_waitForConnected()
+{
+    if (usb_connected)
+        return;
+
+    assert(usb_task == NULL);
+    // save which task we need to notify
+    usb_task = xTaskGetCurrentTaskHandle();
+    // block this task until notification comes
+    const uint32_t num_notifications = ulTaskNotifyTake(pdTRUE, portMAX_DELAY);
+    UNUSED(num_notifications);
+    usb_task = NULL;
+}
+
+// EXAMPLES
 
 void hw_usb_transmit_example()
 {
@@ -157,9 +170,9 @@ void hw_usb_transmit_example()
     for (;;)
     {
         // Send hello (without null terminator).
-        char     msg[]  = "hello";
-        uint8_t *packet = (uint8_t *)msg;
-        hw_usb_transmit(packet, 5);
+        const char msg[]  = "hello";
+        uint8_t   *packet = (uint8_t *)msg;
+        ASSERT_EXIT_OK(hw_usb_transmit(packet, 5));
 
         msg_count += 1;
         LOG_INFO("transmitted \"hello\" %d times", msg_count);
@@ -174,7 +187,7 @@ void hw_usb_receive_example()
     for (;;)
     {
         uint8_t result = 0;
-        if (hw_usb_receive(&result, 1, 100))
+        if (IS_EXIT_OK(hw_usb_receive(&result, 100)))
             LOG_PRINTF("%c", result);
         osDelay(100);
     }
