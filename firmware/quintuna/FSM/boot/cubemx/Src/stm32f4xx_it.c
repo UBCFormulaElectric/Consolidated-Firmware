676d407865764553a011cb447bde930c61c8ebfd
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2025-05-21 17:15:11 -0700

    hardfaulthandler

--- firmware/quintuna/FSM/boot/cubemx/Src/stm32f4xx_it.c
+++ firmware/quintuna/FSM/boot/cubemx/Src/stm32f4xx_it.c
@@ -23,6 +23,7 @@
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
 #include <SEGGER_SYSVIEW_FreeRTOS.h>
+#include "hw_hardFaultHandler.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -87,7 +88,7 @@ void NMI_Handler(void)
 void HardFault_Handler(void)
 {
     /* USER CODE BEGIN HardFault_IRQn 0 */
-
+    hw_hardFaultHandler_handleFault();
     /* USER CODE END HardFault_IRQn 0 */
     while (1)
     {
