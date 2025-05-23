974c73a507c35be99efcc7c3f146ba750b03265c
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2025-05-22 18:09:12 -0700

    sequencing test

--- firmware/quintuna/VC/test/test_powerManager.cpp
+++ firmware/quintuna/VC/test/test_powerManager.cpp
@@ -104,12 +104,119 @@ TEST_F(VCPowerManagerTest, test_sequencingStateMachine)
     LetTimePass(10);
     ASSERT_EQ(app_stateMachine_getCurrentState(), &bmsOn_state)
         << app_stateMachine_getCurrentState()->name << " != bmsOn_state";
+    check_efuses(
+        {
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            false,
+            false,
+            false,
+            false,
+            false,
+        });
 
     // turn on the bms
     app_canRx_BMS_State_update(BMS_DRIVE_STATE);
     LetTimePass(10);
     ASSERT_EQ(app_stateMachine_getCurrentState(), &pcmOn_state)
         << app_stateMachine_getCurrentState()->name << " != pcmOn_state";
+    check_efuses(
+        {
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            false,
+            false,
+            false,
+            false,
+            false,
+        });
+
+    // todo whatever gets us out of PCM on state
+    LetTimePass(10);
+    ASSERT_EQ(app_stateMachine_getCurrentState(), &hvInit_state)
+        << app_stateMachine_getCurrentState()->name << " != hvInit_state";
+    check_efuses(
+        {
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            false,
+            false,
+            false,
+            false,
+        });
+    LetTimePass(200);
+    check_efuses(
+        {
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            false,
+            false,
+            false,
+        });
+    LetTimePass(200);
+    check_efuses(
+        {
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            false,
+            false,
+        });
+    LetTimePass(200);
+    check_efuses(
+        {
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            false,
+        });
+    LetTimePass(200);
+    check_efuses(
+        {
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true,
+        });
 }
 
 TEST_F(VCPowerManagerTest, test_steadyStateFailure)
