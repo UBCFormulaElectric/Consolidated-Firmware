3c03648cf1b2c9f91746678dd806730e9f30e8d2
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2025-05-23 16:43:14 -0700

    fixed

--- firmware/dev/h7dev/src/fdcan_spam.c
+++ firmware/dev/h7dev/src/fdcan_spam.c
@@ -5,16 +5,13 @@
 
 void fd_can_demo_tick()
 {
-    static uint64_t i = 0;
-    uint8_t         i8[8];
-    *(uint64_t *)&i8 = i;
-
-    const CanMsg msg = {
-        .std_id    = 0,
-        .dlc       = 8,
-        .timestamp = 0,
-        .data      = { i8[7], i8[6], i8[5], i8[4], i8[3], i8[2], i8[1], i8[0] },
-        .bus       = 0,
+    static uint64_t i   = 0;
+    const CanMsg    msg = {
+           .std_id    = 0,
+           .dlc       = 8,
+           .timestamp = 0,
+           .data      = { .data64 = { i } },
+           .bus       = 0,
     };
     io_canQueue_pushTx(&msg);
     i++;
