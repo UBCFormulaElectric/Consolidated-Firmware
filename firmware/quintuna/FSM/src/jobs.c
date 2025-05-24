0f2af0df9a717d2eaaf1189139b7dedc7fa45ffd
Krish Thakur <69884464+kryptoish@users.noreply.github.com>
2025-05-23 22:40:11 -0700

    FSM IO implementation (#1518)
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    - io layer stuff made:
    - apps, brake, fsmshdn, imu, steering, suspension
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    - looks working but no proper tests :thumbsup:
    
    ### Resolved Tickets
    <!-- Link any tickets that this PR resolves. -->
    
    - [FSM IO - FIRM-335](https://ubcformulaelectric.atlassian.net/browse/FIRM-335)

--- firmware/quintuna/FSM/src/jobs.c
+++ firmware/quintuna/FSM/src/jobs.c
@@ -3,11 +3,19 @@
 #include "app_commitInfo.h"
 #include "app_canTx.h"
 #include "app_canRx.h"
+#include "app_jsoncan.h"
+#include "app_imu.h"
+#include "app_apps.h"
+#include "app_brake.h"
+#include "app_steering.h"
+#include "app_shdnLoop.h"
+#include "app_suspension.h"
 // io
 #include "io_time.h"
 #include "io_canTx.h"
 #include "io_canQueue.h"
-#include "app_jsoncan.h"
+#include "io_imu.h"
+#include "io_log.h"
 
 static void canTransmit(const JsonCanMsg *msg)
 {
@@ -24,6 +32,10 @@ void jobs_init(void)
     app_canTx_init();
     app_canRx_init();
 
+    LOG_ERROR_IF(io_imu_init());
+
+    app_apps_init();
+
     // broadcast commit info
     app_canTx_FSM_Hash_set(GIT_COMMIT_HASH);
     app_canTx_FSM_Clean_set(GIT_COMMIT_CLEAN);
@@ -31,29 +43,23 @@ void jobs_init(void)
 
 void jobs_run1Hz_tick(void)
 {
-    // debug mode
-    const bool debug_mode_enabled = app_canRx_Debug_EnableDebugMode_get();
-    io_canTx_enableMode_can2(CAN2_MODE_DEBUG, debug_mode_enabled);
-    // cantx
     io_canTx_enqueue1HzMsgs();
 }
 
 void jobs_run100Hz_tick(void)
 {
-    // cantx
+    app_apps_broadcast();
+    app_brake_broadcast();
+    app_steering_broadcast();
+    app_shdnLoop_broadcast();
+    app_suspension_broadcast();
+    app_imu_broadcast();
+
     io_canTx_enqueue100HzMsgs();
 }
 
 void jobs_run1kHz_tick(void)
 {
-    // cantx
-    const uint32_t task_start_ms = io_time_getCurrentMs();
-    io_canTx_enqueueOtherPeriodicMsgs(task_start_ms);
-
-    // Watchdog check-in must be the last function called before putting the
-    // task to sleep. Prevent check in if the elapsed period is greater or
-    // equal to the period ms
-    // if (io_time_getCurrentMs() - task_start_ms <= 1)
-    // {
-    // }
+    io_canTx_enableMode_can2(CAN2_MODE_DEBUG, app_canRx_Debug_EnableDebugMode_get());
+    io_canTx_enqueueOtherPeriodicMsgs(io_time_getCurrentMs());
 }
