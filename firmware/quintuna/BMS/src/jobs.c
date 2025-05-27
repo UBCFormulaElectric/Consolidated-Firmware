c5f0bea5f3e7a0bad4cfe752611dafb88eaf2db6
Jack Polloway <jack_polloway@icloud.com>
2025-05-27 14:46:22 -0700

    moved allstates to jobs

--- firmware/quintuna/BMS/src/jobs.c
+++ firmware/quintuna/BMS/src/jobs.c
@@ -6,6 +6,7 @@
 
 #include "io_bootHandler.h"
 #include "io_canTx.h"
+#include "io_faultLatch.h"
 
 #include "io_canQueue.h"
 #include "app_jsoncan.h"
@@ -38,6 +39,14 @@ void jobs_init()
 void jobs_run1Hz_tick(void)
 {
     io_canTx_enqueue1HzMsgs();
+
+    /**
+     * add charger connection status CAN tx once charging implementation branch is merged
+     */
+
+     /**
+      * add reset SOC from min cell voltage if soc corrupt and voltage readings settled after soc app is finished
+      */
 }
 
 void jobs_run100Hz_tick(void)
@@ -47,6 +56,33 @@ void jobs_run100Hz_tick(void)
 
     io_canTx_enqueue100HzMsgs();
     app_shdnLoop_broadcast();
+
+    app_canTx_BMS_Heartbeat_set(true);
+
+     /**
+     * Update CAN signals for BMS latch statuses
+     */
+    app_canTx_BMS_BmsOk_set(io_faultLatch_getCurrentStatus(&bms_ok_latch));
+    app_canTx_BMS_ImdOk_set(io_faultLatch_getCurrentStatus(&imd_ok_latch));
+    app_canTx_BMS_BspdOk_set(io_faultLatch_getCurrentStatus(&bspd_ok_latch));
+    app_canTx_BMS_BmsLatchedFault_set(io_faultLatch_getLatchedStatus(&bms_ok_latch));
+    app_canTx_BMS_ImdLatchedFault_set(io_faultLatch_getLatchedStatus(&imd_ok_latch));
+    app_canTx_BMS_BspdLatchedFault_set(io_faultLatch_getLatchedStatus(&bspd_ok_latch));
+    /**
+     * app_canTx_BMS_Soc_set(app_soc_getMinSocPercent()); add once soc app is finished
+     */
+
+    /**
+     * add cell balancing check once cell balancing is enabled
+     */
+
+    /**
+     * add accumulator tasks once accumulator app is finished
+     */
+
+    /**
+     * add soc stat update once app soc is finished
+     */
 }
 
 void jobs_run1kHz_tick(void) {}
