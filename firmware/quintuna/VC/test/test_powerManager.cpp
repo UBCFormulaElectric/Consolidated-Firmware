757d7a59ea199a5de3f7afe336c1fe9f614162d8
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2025-05-31 13:38:55 -0700

    passing unit tests

--- firmware/quintuna/VC/test/test_powerManager.cpp
+++ firmware/quintuna/VC/test/test_powerManager.cpp
@@ -40,20 +40,18 @@ struct efuse_expected_state
     bool l_rad_fan_efuse;
     bool r_rad_fan_efuse;
 };
-static void check_efuses(const efuse_expected_state s)
-{
-    ASSERT_EQ(inv_rsm_loadswitch.efuse1->enabled, s.f_inv_efuse);
-    ASSERT_EQ(inv_rsm_loadswitch.efuse2->enabled, s.rsm_efuse);
-    ASSERT_EQ(inv_bms_loadswitch.efuse1->enabled, s.bms_efuse);
-    ASSERT_EQ(inv_bms_loadswitch.efuse2->enabled, s.r_inv_efuse);
-    ASSERT_EQ(front_loadswitch.efuse1->enabled, s.dam_efuse);
-    ASSERT_EQ(front_loadswitch.efuse2->enabled, s.front_efuse);
-    ASSERT_EQ(rl_pump_loadswitch.efuse->enabled, s.rl_pump_efuse);
-    ASSERT_EQ(rr_pump_loadswitch.efuse->enabled, s.rr_pump_efuse);
-    ASSERT_EQ(f_pump_loadswitch.efuse->enabled, s.f_pump_efuse);
-    ASSERT_EQ(rad_fan_loadswitch.efuse1->enabled, s.l_rad_fan_efuse);
-    ASSERT_EQ(rad_fan_loadswitch.efuse2->enabled, s.r_rad_fan_efuse);
-}
+#define check_efuses(f_inv_efuse, rsm_efuse, bms_efuse, r_inv_efuse, dam_efuse, front_efuse, rl_pump_efuse, rr_pump_efuse, f_pump_efuse, l_rad_fan_efuse, r_rad_fan_efuse) \
+    ASSERT_EQ(inv_rsm_loadswitch.efuse1->enabled, f_inv_efuse); \
+    ASSERT_EQ(inv_rsm_loadswitch.efuse2->enabled, rsm_efuse); \
+    ASSERT_EQ(inv_bms_loadswitch.efuse1->enabled, bms_efuse); \
+    ASSERT_EQ(inv_bms_loadswitch.efuse2->enabled, r_inv_efuse); \
+    ASSERT_EQ(front_loadswitch.efuse1->enabled, dam_efuse); \
+    ASSERT_EQ(front_loadswitch.efuse2->enabled, front_efuse); \
+    ASSERT_EQ(rl_pump_loadswitch.efuse->enabled, rl_pump_efuse); \
+    ASSERT_EQ(rr_pump_loadswitch.efuse->enabled, rr_pump_efuse); \
+    ASSERT_EQ(f_pump_loadswitch.efuse->enabled, f_pump_efuse); \
+    ASSERT_EQ(rad_fan_loadswitch.efuse1->enabled, l_rad_fan_efuse); \
+    ASSERT_EQ(rad_fan_loadswitch.efuse2->enabled, r_rad_fan_efuse); \
 
 TEST_F(VCPowerManagerTest, test_sequencingStateMachine)
 {
@@ -61,7 +59,7 @@ TEST_F(VCPowerManagerTest, test_sequencingStateMachine)
     ASSERT_EQ(app_stateMachine_getCurrentState(), &init_state)
         << app_stateMachine_getCurrentState()->name << " != init_state";
     LetTimePass(10);
-    check_efuses({
+    check_efuses(
         false,
         true,
         true,
@@ -72,15 +70,15 @@ TEST_F(VCPowerManagerTest, test_sequencingStateMachine)
         false,
         false,
         false,
-        false,
-    });
+        false
+    );
 
     // close ir negative
     app_canRx_BMS_IrNegative_update(CONTACTOR_STATE_CLOSED);
     LetTimePass(10);
     ASSERT_EQ(app_stateMachine_getCurrentState(), &inverterOn_state)
         << app_stateMachine_getCurrentState()->name << " != inverterOn_state";
-    check_efuses({
+    check_efuses(
         true,
         true,
         true,
@@ -91,8 +89,8 @@ TEST_F(VCPowerManagerTest, test_sequencingStateMachine)
         false,
         false,
         false,
-        false,
-    });
+        false
+    );
 
     // turn on inverters
     app_canRx_INVFL_bSystemReady_update(true);
@@ -102,7 +100,7 @@ TEST_F(VCPowerManagerTest, test_sequencingStateMachine)
     LetTimePass(10);
     ASSERT_EQ(app_stateMachine_getCurrentState(), &bmsOn_state)
         << app_stateMachine_getCurrentState()->name << " != bmsOn_state";
-    check_efuses({
+    check_efuses(
         true,
         true,
         true,
@@ -113,15 +111,15 @@ TEST_F(VCPowerManagerTest, test_sequencingStateMachine)
         false,
         false,
         false,
-        false,
-    });
+        false
+    );
 
     // turn on the bms
     app_canRx_BMS_State_update(BMS_DRIVE_STATE);
     LetTimePass(10);
     ASSERT_EQ(app_stateMachine_getCurrentState(), &pcmOn_state)
         << app_stateMachine_getCurrentState()->name << " != pcmOn_state";
-    check_efuses({
+    check_efuses(
         true,
         true,
         true,
@@ -132,8 +130,8 @@ TEST_F(VCPowerManagerTest, test_sequencingStateMachine)
         false,
         false,
         false,
-        false,
-    });
+        false
+    );
 
     // // todo whatever gets us out of PCM on state
     // LetTimePass(10);
