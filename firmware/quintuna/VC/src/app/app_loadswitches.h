7cdd138690c3ea7c77406b9d48934c319971d1e2
Amir Tajaddoditalab <96522772+AmirTajaddodi@users.noreply.github.com>
2025-05-31 13:42:46 -0700

    VC Bringup (#1580)
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    ### Resolved Tickets
    <!-- Link any tickets that this PR resolves. -->
    
    ---------
    
    Co-authored-by: Edwin Z <20777515+Lucien950@users.noreply.github.com>

---
+++ firmware/quintuna/VC/src/app/app_loadswitches.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <stdbool.h>
+#include "io_loadswitches.h"
+
+void app_efuse_broadcast(void);
---
+++ firmware/quintuna/VC/src/app/app_pumpControl.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "io_pumpControl.h"
+#include <stdint.h>
+
+void app_pumpControl_MonitorPumps(void);
