12f362601ff83df276f3190d621eec75bbb27e05
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2025-06-02 10:22:14 -0700

    oopsies x2

--- firmware/quintuna/VC/src/app/states/app_initState.c
+++ firmware/quintuna/VC/src/app/states/app_initState.c
@@ -17,13 +17,13 @@ static PowerManagerConfig power_manager_state = {
                        [EFUSE_CHANNEL_R_RAD]   = { .efuse_enable = false, .timeout = 200, .max_retry = 5 } }
 };
 
-static void runOnEntry(void)
+static void initStateRunOnEntry(void)
 {
     app_canTx_VC_State_set(VC_INIT_STATE);
     app_powerManager_updateConfig(power_manager_state);
 }
 static void initStateRunOnTick1Hz(void) {}
-static void runOnTick100Hz(void)
+static void initStateRunOnTick100Hz(void)
 {
     const ContactorState air_minus_closed = app_canRx_BMS_IrNegative_get();
     if (air_minus_closed == CONTACTOR_STATE_CLOSED)
@@ -34,7 +34,7 @@ static void runOnTick100Hz(void)
 static void initStateRunOnExit(void) {}
 
 State init_state = { .name              = "INIT",
-                     .run_on_entry      = runOnEntry,
+                     .run_on_entry      = initStateRunOnEntry,
                      .run_on_tick_1Hz   = initStateRunOnTick1Hz,
-                     .run_on_tick_100Hz = runOnTick100Hz,
+                     .run_on_tick_100Hz = initStateRunOnTick100Hz,
                      .run_on_exit       = initStateRunOnExit };
