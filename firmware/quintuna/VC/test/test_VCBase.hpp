9a5e5533fa56bd2c8d73c137bc1f2fb7fc74c14d
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2025-06-03 00:04:23 -0700

    1

--- firmware/quintuna/VC/test/test_VCBase.hpp
+++ firmware/quintuna/VC/test/test_VCBase.hpp
@@ -6,12 +6,14 @@ extern "C"
 {
 #include "jobs.h"
 #include "io_loadswitches.h"
+#include "app_canRx.h"
 }
 
 class VCBaseTest : public EcuTestBase
 {
     void board_setup() override
     {
+        suppress_heartbeat = false;
         // micro startup simulation
         for (const TI_LoadSwitch &tils : { rl_pump_loadswitch, rr_pump_loadswitch, f_pump_loadswitch })
         {
@@ -26,6 +28,18 @@ class VCBaseTest : public EcuTestBase
         jobs_init();
     }
     void board_teardown() override {}
-    void tick_100hz() override { jobs_run100Hz_tick(); }
+    void tick_100hz() override
+    {
+        if (!suppress_heartbeat)
+        {
+            app_canRx_BMS_Heartbeat_update(true);
+            app_canRx_FSM_Heartbeat_update(true);
+            app_canRx_RSM_Heartbeat_update(true);
+        }
+        jobs_run100Hz_tick();
+    }
     void tick_1hz() override { jobs_run1Hz_tick(); }
+
+  public:
+    bool suppress_heartbeat = false;
 };
--- firmware/quintuna/VC/test/test_stateMachine.cpp
+++ firmware/quintuna/VC/test/test_stateMachine.cpp
@@ -1,41 +1,121 @@
 #include "test_VCBase.hpp"
 
+extern "C"
+{
+#include "states/app_states.h"
+#include "app_canRx.h"
+#include "app_canTx.h"
+#include "app_canAlerts.h"
+}
+
 class VCStateMachineTest : public VCBaseTest
 {
 };
 
-TEST_F(VCStateMachineTest, test_SetStateToDrive) {}
+TEST_F(VCStateMachineTest, starts_in_init_state_contactors_open)
+{
+    ASSERT_EQ(app_canRx_BMS_IrNegative_get(), CONTACTOR_STATE_OPEN);
+    ASSERT_EQ(app_stateMachine_getCurrentState(), &init_state);
+}
 
-TEST_F(VCStateMachineTest, check_init_transitions_to_drive_if_conditions_met_and_start_switch_pulled_up) {}
+TEST_F(VCStateMachineTest, air_minus_close_to_inv_on_state)
+{
+    app_stateMachine_setNextState(&init_state);
+    app_canRx_BMS_IrNegative_update(CONTACTOR_STATE_CLOSED);
+    LetTimePass(10);
+    ASSERT_EQ(app_stateMachine_getCurrentState(), &inverterOn_state);
+}
 
-TEST_F(VCStateMachineTest, check_init_state_is_broadcasted_over_can) {}
+TEST_F(VCStateMachineTest, inverter_on_leave_condition_test)
+{
+    app_stateMachine_setNextState(&inverterOn_state);
+    app_canRx_BMS_IrNegative_update(CONTACTOR_STATE_CLOSED);
 
-TEST_F(VCStateMachineTest, check_state_transition_from_init_to_inverter_on) {}
+    app_canRx_INVFL_bSystemReady_update(true);
+    LetTimePass(10);
+    ASSERT_EQ(app_stateMachine_getCurrentState(), &inverterOn_state);
 
-TEST_F(VCStateMachineTest, check_drive_state_is_broadcasted_over_can) {}
+    app_canRx_INVFR_bSystemReady_update(true);
+    LetTimePass(10);
+    ASSERT_EQ(app_stateMachine_getCurrentState(), &inverterOn_state);
 
-TEST_F(VCStateMachineTest, check_inverter_on_state_is_broadcasted_over_can) {}
+    app_canRx_INVRL_bSystemReady_update(true);
+    LetTimePass(10);
+    ASSERT_EQ(app_stateMachine_getCurrentState(), &inverterOn_state);
 
-TEST_F(VCStateMachineTest, disable_inverters_in_init_state) {}
+    app_canRx_INVRR_bSystemReady_update(true);
+    LetTimePass(10);
+    ASSERT_EQ(app_stateMachine_getCurrentState(), &bmsOn_state);
+}
 
-TEST_F(VCStateMachineTest, start_switch_off_transitions_drive_state_to_inverter_on_state) {}
+TEST_F(VCStateMachineTest, bms_drive_state_transition)
+{
+    app_stateMachine_setNextState(&bmsOn_state);
+    app_canRx_BMS_IrNegative_update(CONTACTOR_STATE_CLOSED);
+    LetTimePass(100);
+    ASSERT_EQ(app_stateMachine_getCurrentState(), &bmsOn_state);
+    app_canRx_BMS_State_update(BMS_DRIVE_STATE);
+    LetTimePass(10);
+    ASSERT_EQ(app_stateMachine_getCurrentState(), &pcmOn_state);
+}
 
-TEST_F(VCStateMachineTest, check_if_buzzer_stays_on_for_two_seconds_only_after_entering_drive_state) {}
+TEST_F(VCStateMachineTest, air_minus_open_in_all_states_to_init)
+{
+    app_stateMachine_setNextState(&inverterOn_state);
+    app_canRx_BMS_IrNegative_update(CONTACTOR_STATE_OPEN);
+    LetTimePass(10);
+    ASSERT_EQ(app_stateMachine_getCurrentState(), &init_state);
 
-TEST_F(VCStateMachineTest, no_torque_requests_when_accelerator_pedal_is_not_pressed) {}
+    app_stateMachine_setNextState(&bmsOn_state);
+    app_canRx_BMS_IrNegative_update(CONTACTOR_STATE_OPEN);
+    LetTimePass(10);
+    ASSERT_EQ(app_stateMachine_getCurrentState(), &init_state);
 
-TEST_F(VCStateMachineTest, exit_drive_state_on_left_inverter_fault) {}
+    // etc.
+}
 
-TEST_F(VCStateMachineTest, exit_drive_state_on_right_inverter_fault) {}
+TEST_F(VCStateMachineTest, torque_request_zero_when_leave_drive) {}
 
-TEST_F(VCStateMachineTest, exit_drive_state_on_bms_fault) {}
+TEST_F(VCStateMachineTest, values_reset_when_no_heartbeat)
+{
+    app_canRx_BMS_IrNegative_update(CONTACTOR_STATE_CLOSED);
+    ASSERT_FALSE(app_canAlerts_VC_Fault_MissingBMSHeartbeat_get());
+    ASSERT_FALSE(app_canAlerts_VC_Fault_MissingFSMHeartbeat_get());
+    ASSERT_FALSE(app_canAlerts_VC_Fault_MissingRSMHeartbeat_get());
+    LetTimePass(100);
 
-TEST_F(VCStateMachineTest, exit_drive_state_on_fsm_fault) {}
+    suppress_heartbeat = true;
+    LetTimePass(10);
 
-TEST_F(VCStateMachineTest, exit_drive_state_on_VC_fault) {}
+    // tests
+    ASSERT_EQ(app_canRx_BMS_IrNegative_get(), CONTACTOR_STATE_OPEN);
+    ASSERT_EQ(app_stateMachine_getCurrentState(), &fault_state);
+    ASSERT_TRUE(app_canAlerts_VC_Fault_MissingBMSHeartbeat_get());
+    ASSERT_TRUE(app_canAlerts_VC_Fault_MissingFSMHeartbeat_get());
+    ASSERT_TRUE(app_canAlerts_VC_Fault_MissingRSMHeartbeat_get());
+    // TODO more concequences of heartbeat failure
+}
 
-TEST_F(VCStateMachineTest, exit_drive_state_on_CRIT_fault) {}
-
-TEST_F(VCStateMachineTest, BMS_causes_drive_to_inverter_on) {}
-
-TEST_F(VCStateMachineTest, BMS_causes_drive_to_inverter_on_to_init) {}
+// TEST_F(VCStateMachineTest, test_SetStateToDrive) {}
+//
+// TEST_F(VCStateMachineTest, check_init_transitions_to_drive_if_conditions_met_and_start_switch_pulled_up) {}
+//
+// TEST_F(VCStateMachineTest, check_init_state_is_broadcasted_over_can) {}
+//
+// TEST_F(VCStateMachineTest, check_state_transition_from_init_to_inverter_on) {}
+//
+// TEST_F(VCStateMachineTest, check_drive_state_is_broadcasted_over_can) {}
+//
+// TEST_F(VCStateMachineTest, check_inverter_on_state_is_broadcasted_over_can) {}
+//
+// TEST_F(VCStateMachineTest, disable_inverters_in_init_state) {}
+//
+// TEST_F(VCStateMachineTest, start_switch_off_transitions_drive_state_to_inverter_on_state) {}
+//
+// TEST_F(VCStateMachineTest, check_if_buzzer_stays_on_for_two_seconds_only_after_entering_drive_state) {}
+//
+// TEST_F(VCStateMachineTest, no_torque_requests_when_accelerator_pedal_is_not_pressed) {}
+//
+// TEST_F(VCStateMachineTest, BMS_causes_drive_to_inverter_on) {}
+//
+// TEST_F(VCStateMachineTest, BMS_causes_drive_to_inverter_on_to_init) {}
