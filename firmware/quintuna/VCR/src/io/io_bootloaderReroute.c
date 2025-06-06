a5b145f70aea5970ab517cafde4672e2e1a43beb
AmirTajaddodi <amir.tajadodi@gmail.com>
2025-06-05 22:17:24 -0700

    made some slight changes

--- firmware/quintuna/VCR/src/io/io_bootloaderReroute.c
+++ firmware/quintuna/VCR/src/io/io_bootloaderReroute.c
@@ -1,5 +1,7 @@
 #include "io_bootloaderReroute.h"
 #include "io_canMsg.h"
+#include <stdint.h>
+#include <string.h>
 
 static void (*transmit_func_can1)(const CanMsg *tx_msg);
 static void (*transmit_func_can2)(const CanMsg *tx_msg);
@@ -20,15 +22,26 @@ void io_bootloadeReroute_init(
     transmit_func_can3 = transmit_can3_msg_func;
 }
 
-void io_bootloaderReroute_can1(CanMsg *msg){
+void io_bootloaderReroute_reRoute(const CanMsg *msg){
     CanMsg new_msg;
     memset(&new_msg, 0, sizeof(CanMsg));
 
     new_msg.std_id = msg->std_id;
     new_msg.dlc = 8;
-    
+
+    memcpy(&(new_msg.data), &(msg->data), sizeof(uint64_t));
+
     switch ((msg->std_id & 0x1FFFFFFC)) {
         case(FSM_BOOTCONFIG):
-
+            transmit_func_can2(&new_msg);
+            break;
+        case(RSM_BOOTCONFIG):
+            transmit_func_can2(&new_msg);
+            break;
+        case(CRIT_BOOTCONFIG):
+            transmit_func_can2(&new_msg);
+            break;
+        default:
+            break;
     }
 }
