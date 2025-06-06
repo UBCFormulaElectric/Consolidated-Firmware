aa568b96e0f567da302fa6d2b641fc6c181aa182
Kelm <lkevin2003@gmail.com>
2025-06-06 02:09:44 -0300

    added init state files

---
+++ firmware/quintuna/BMS/src/app/states/app_initState.c
@@ -0,0 +1,87 @@
+#include "states/app_allStates.h"
+#include "app_imd.h"
+#include "app_soc.h"
+#include "io_faultLatch.h"
+#include "io_airs.h"
+#include "io_charger.h"
+#include "app_inverterOnState.h"
+#include "app_heartbeatMonitors.h"
+#include "app_tractiveSystem.h"
+
+#define TS_DISCHARGED_THRESHOLD_V (10.0f)
+
+static void initStateRunOnEntry(void)
+{
+    app_canTx_BMS_State_set(BMS_INIT_STATE);
+    io_faultLatch_setCurrentStatus(&bms_ok_latch, true);
+
+    // AIR+ opens upon entering init state
+    // Should always be opened at this point from other states, this is only for redundancy since we really don't want
+    // AIR+ closed in init
+    io_airs_openPositive();
+
+}
+
+static void initStateRunOnTick1Hz(void)
+{
+
+    // // ONLY RUN THIS WHEN CELLS HAVE HAD TIME TO SETTLE
+    if (app_canRx_Debug_ResetSoc_MinCellV_get())
+    {
+        app_soc_resetSocFromVoltage();
+    }
+    else if (app_canRx_Debug_ResetSoc_CustomEnable_get())
+    {
+        app_soc_resetSocCustomValue(app_canRx_Debug_ResetSoc_CustomVal_get());
+    }
+}
+
+static void initStateRunOnTick100Hz(void)
+{
+    const bool air_negative_closed = io_airs_isNegativeClosed();
+    const bool ts_discharged       = app_tractiveSystem_getVoltage() < TS_DISCHARGED_THRESHOLD_V;
+    const bool missing_hb          = app_heartbeatMonitor_isSendingMissingHeartbeatFault(&hb_monitor);
+
+    if (air_negative_closed && ts_discharged)
+    {
+        const bool charger_connected     = io_charger_getStatus() == EVSE_CONNECTED;
+        const bool charger_disconnected  = io_charger_getStatus() == EVSE_DISCONNECTED;
+        const bool cell_balancing_enabled    = app_canRx_Debug_CellBalancingRequest_get();
+        const bool external_charging_request = app_canRx_Debug_StartCharging_get();
+        //TODO: Re-enable if elcon has specific CAN field to clear latched faults
+        // const bool clear_elcon_latch         = app_canRx_Debug_ClearChargerLatchedFault_get();
+
+        // app_canTx_BMS_ClearLatch_set(clear_elcon_latch);
+
+        const bool precharge_for_charging = charger_connected && external_charging_request;
+        // TODO: Update state change condition 
+        const bool precharge_for_driving  = !charger_connected && !cell_balancing_enabled && !missing_hb;
+        // const bool precharge_for_driving  = app_canRx_VC_State_get() == BMS_On  && charger_disconnected && !missing_hb;
+
+        if (precharge_for_charging)
+        {
+            app_stateMachine_setNextState(app_prechargeState_get());
+        }
+        else if (precharge_for_driving)
+        {
+            app_stateMachine_setNextState(app_inverterOnState_get());
+        }
+        else if (cell_balancing_enabled)
+        {
+            app_stateMachine_setNextState(app_balancingState_get());
+        }
+    }
+}
+
+const State *app_initState_get(void)
+{
+    static State init_state = {
+        .name              = "INIT",
+        .run_on_entry      = initStateRunOnEntry,
+        .run_on_tick_1Hz   = initStateRunOnTick1Hz,
+        .run_on_tick_100Hz = initStateRunOnTick100Hz,
+        .run_on_exit       = NULL,
+    };
+
+    return &init_state;
+}
