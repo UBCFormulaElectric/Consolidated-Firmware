e16233771709d2f412bea250cdd13d2fd21ca92b
AmirTajaddodi <amir.tajadodi@gmail.com>
2025-06-06 00:17:32 -0700

    added bit switching

--- firmware/quintuna/VCR/src/io/io_bootloaderReroute.c
+++ firmware/quintuna/VCR/src/io/io_bootloaderReroute.c
@@ -11,7 +11,6 @@ static void (*transmit_func_can3)(const CanMsg *tx_msg);
 #define RSM_BOOTCONFIG 0x14000000
 #define CRIT_BOOTCONFIG 0x18000000
 
-
 void io_bootloadeReroute_init(
     void (*transmit_can1_msg_func)(const CanMsg *),
     void (*transmit_can2_msg_func)(const CanMsg *),
@@ -22,23 +21,25 @@ void io_bootloadeReroute_init(
     transmit_func_can3 = transmit_can3_msg_func;
 }
 
-void io_bootloaderReroute_reRoute(const CanMsg *msg){
+void io_bootloaderReroute_reRoute(const CanMsg *msg)
+{
     CanMsg new_msg;
     memset(&new_msg, 0, sizeof(CanMsg));
 
     new_msg.std_id = msg->std_id;
-    new_msg.dlc = 8;
+    new_msg.dlc    = 8;
 
     memcpy(&(new_msg.data), &(msg->data), sizeof(uint64_t));
 
-    switch ((msg->std_id & 0x1FFFFFFC)) {
-        case(FSM_BOOTCONFIG):
+    switch ((msg->std_id & 0x1FFFFFFC))
+    {
+        case (FSM_BOOTCONFIG):
             transmit_func_can2(&new_msg);
             break;
-        case(RSM_BOOTCONFIG):
+        case (RSM_BOOTCONFIG):
             transmit_func_can2(&new_msg);
             break;
-        case(CRIT_BOOTCONFIG):
+        case (CRIT_BOOTCONFIG):
             transmit_func_can2(&new_msg);
             break;
         default:
