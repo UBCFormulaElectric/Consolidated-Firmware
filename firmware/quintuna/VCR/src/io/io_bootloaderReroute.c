29ca1d38acbbf7eb3d435da95e41d2a241d5d63c
shayana18 <shayanajmaluni@gmail.com>
2025-06-06 23:24:53 -0700

    fucked up my commits, resetting

--- firmware/quintuna/VCR/src/io/io_bootloaderReroute.c
+++ firmware/quintuna/VCR/src/io/io_bootloaderReroute.c
@@ -3,44 +3,29 @@
 #include <stdint.h>
 #include <string.h>
 
-static void (*transmit_func_can1)(const CanMsg *tx_msg);
 static void (*transmit_func_can2)(const CanMsg *tx_msg);
-static void (*transmit_func_can3)(const CanMsg *tx_msg);
 
 #define FSM_BOOTCONFIG 0x10000001
 #define RSM_BOOTCONFIG 0x14000000
 #define CRIT_BOOTCONFIG 0x18000000
 
 void io_bootloadeReroute_init(
-    void (*transmit_can1_msg_func)(const CanMsg *),
-    void (*transmit_can2_msg_func)(const CanMsg *),
-    void (*transmit_can3_msg_func)(const CanMsg *))
-{
-    transmit_func_can1 = transmit_can1_msg_func;
+    void (*transmit_can2_msg_func)(const CanMsg *)){
     transmit_func_can2 = transmit_can2_msg_func;
-    transmit_func_can3 = transmit_can3_msg_func;
 }
 
 void io_bootloaderReroute_reRoute(const CanMsg *msg)
 {
-    CanMsg new_msg;
-    memset(&new_msg, 0, sizeof(CanMsg));
-
-    new_msg.std_id = msg->std_id;
-    new_msg.dlc    = 8;
-
-    memcpy(&(new_msg.data), &(msg->data), sizeof(uint64_t));
-
     switch ((msg->std_id & 0x1FFFFFFC))
     {
         case (FSM_BOOTCONFIG):
-            transmit_func_can2(&new_msg);
+            transmit_func_can2(msg);
             break;
         case (RSM_BOOTCONFIG):
-            transmit_func_can2(&new_msg);
+            transmit_func_can2(msg);
             break;
         case (CRIT_BOOTCONFIG):
-            transmit_func_can2(&new_msg);
+            transmit_func_can2(msg);
             break;
         default:
             break;
