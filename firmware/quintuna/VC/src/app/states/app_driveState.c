b23e4ff965455456250996b1ea64b81da835d5f3
Phi Lam <lamyenphi14@gmail.com>
2025-06-09 12:29:48 -0700

    change regen to new pedal remap

--- firmware/quintuna/VC/src/app/states/app_driveState.c
+++ firmware/quintuna/VC/src/app/states/app_driveState.c
@@ -105,15 +105,15 @@ static void driveStateRunOnTick100Hz(void)
         return;
     }
 
-    // regen switched pedal percentage from [0.0, 1.0] to[-0.3, 0.7] and
-    // then scaled to[-1, 1] if (regen_switch_is_on)
+    // regen switches pedal percentage from [0.0f, 1.0f] to [-0.2f, 0.8f] and then scaled to [-1.0f, 1.0f]
+    if (regen_switch_is_on)
     {
-        apps_pedal_percentage  = (float)app_regen_pedalRemapping(apps_pedal_percentage);
-        sapps_pedal_percentage = (float)app_regen_pedalRemapping(sapps_pedal_percentage);
+        apps_pedal_percentage  = app_regen_pedalRemapping(apps_pedal_percentage);
+        sapps_pedal_percentage = app_regen_pedalRemapping(sapps_pedal_percentage);
     }
 
-    app_canTx_VC_MappedPedalPercentage_set((float)apps_pedal_percentage);
-    runDrivingAlgorithm((float)apps_pedal_percentage, (float)sapps_pedal_percentage);
+    app_canTx_VC_MappedPedalPercentage_set(apps_pedal_percentage * 100.0f);
+    runDrivingAlgorithm(apps_pedal_percentage, sapps_pedal_percentage);
 }
 
 static void driveStateRunOnExit(void)
@@ -315,85 +315,3 @@ State drive_state = {
     .run_on_tick_100Hz = driveStateRunOnTick100Hz,
     .run_on_exit       = driveStateRunOnExit,
 };
-
-bool driveStatePassPreCheck()
-{
-    // All states module checks for faults, and returns whether or not a fault was detected.
-    const bool any_board_has_fault = app_faultCheck_checkBoards();
-    const bool inverter_has_fault  = app_faultCheck_checkInverters();
-    const bool all_states_ok       = !(any_board_has_fault || inverter_has_fault);
-
-    const bool start_switch_off          = app_canRx_CRIT_StartSwitch_get() == SWITCH_OFF;
-    const bool bms_not_in_drive          = app_canRx_BMS_State_get() != BMS_DRIVE_STATE;
-    bool       exit_drive_to_init        = bms_not_in_drive;
-    bool       exit_drive_to_inverter_on = !all_states_ok || start_switch_off;
-
-    // Make sure you can only turn on VD in init and not during drive, only able to turn off
-    bool prev_regen_switch_val = regen_switch_is_on;
-    regen_switch_is_on         = app_canRx_CRIT_RegenSwitch_get() == SWITCH_ON && prev_regen_switch_val;
-
-    bool prev_torque_vectoring_switch_val = torque_vectoring_switch_is_on;
-    regen_switch_is_on = app_canRx_CRIT_TorqueVecSwitch_get() == SWITCH_ON && prev_torque_vectoring_switch_val;
-
-    /* TODO: Vehicle dyanmics people need to make sure to do a check if sensor init failed
-        or not before using closed loop features */
-
-    // Update Regen + TV LEDs
-    if (!regen_switch_is_on)
-    {
-        app_canTx_VC_RegenEnabled_set(false);
-        app_canTx_VC_Warning_RegenNotAvailable_set(true);
-    }
-
-    if (!torque_vectoring_switch_is_on)
-    {
-        app_canTx_VC_TorqueVectoringEnabled_set(true);
-    }
-
-    if (exit_drive_to_init)
-    {
-        app_stateMachine_setNextState(app_initState_get());
-        return false;
-    }
-    else if (exit_drive_to_inverter_on)
-    {
-        app_stateMachine_setNextState(app_inverterOnState_get());
-        return false;
-    }
-
-    return true;
-}
-
-void app_regularDrive_run(float apps_pedal_percentage)
-{
-    // TODO: Use power limiting in regular drive
-    const float bms_available_power         = (float)app_canRx_BMS_AvailablePower_get();
-    const float right_front_motor_speed_rpm = (float)app_canRx_INVR_MotorSpeed_get();
-    const float right_back_motor_speed_rpm  = (float)app_canRx_INVR_MotorSpeed_get();
-    const float left_front_motor_speed_rpm  = (float)app_canRx_INVL_MotorSpeed_get();
-    const float left_back_motor_speed_rpm   = (float)app_canRx_INVL_MotorSpeed_get();
-    float       bms_torque_limit            = MAX_TORQUE_REQUEST_NM;
-
-    if ((right_front_motor_speed_rpm + right_back_motor_speed_rpm + left_front_motor_speed_rpm +
-         left_back_motor_speed_rpm) > 0.0f)
-    {
-        // Estimate the maximum torque request to draw the maximum power available from the BMS
-        const float available_output_power_w = bms_available_power * EFFICIENCY_ESTIMATE;
-        const float combined_motor_speed_rads =
-            RPM_TO_RADS(right_front_motor_speed_rpm) + RPM_TO_RADS(right_back_motor_speed_rpm) +
-            RPM_TO_RADS(left_front_motor_speed_rpm) + RPM_TO_RADS(left_back_motor_speed_rpm);
-        bms_torque_limit = MIN(available_output_power_w / combined_motor_speed_rads, MAX_TORQUE_REQUEST_NM);
-    }
-
-    // Calculate the maximum torque request, according to the BMS available power
-    const float max_bms_torque_request = apps_pedal_percentage * bms_torque_limit;
-
-    // Calculate the actual torque request to transmit
-    const float torque_request = MIN(max_bms_torque_request, MAX_TORQUE_REQUEST_NM);
-
-    // Transmit torque command to both inverters
-    app_canTx_VC_RightFrontInverterTorqueCommand_set(torque_request);
-    app_canTx_VC_RightBackInverterTorqueCommand_set(torque_request);
-    app_canTx_VC_LeftFrontInverterTorqueCommand_set(torque_request);
-    app_canTx_VC_LeftBackInverterTorqueCommand_set(torque_request);
-}
