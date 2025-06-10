426724985d392b911b9a5a524e53f72c9522fa9f
AmirTajaddodi <amir.tajadodi@gmail.com>
2025-06-09 19:25:28 -0700

    small current changes

--- firmware/quintuna/RSM/src/cubemx/Src/stm32f4xx_hal_msp.c
+++ firmware/quintuna/RSM/src/cubemx/Src/stm32f4xx_hal_msp.c
@@ -400,6 +400,7 @@ void HAL_I2C_MspDeInit(I2C_HandleTypeDef *hi2c)
  */
 void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim_base)
 {
+    GPIO_InitTypeDef GPIO_InitStruct = { 0 };
     if (htim_base->Instance == TIM2)
     {
         /* USER CODE BEGIN TIM2_MspInit 0 */
@@ -414,18 +415,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim_base)
 
         /* USER CODE END TIM2_MspInit 1 */
     }
-}
-
-/**
- * @brief TIM_IC MSP Initialization
- * This function configures the hardware resources used in this example
- * @param htim_ic: TIM_IC handle pointer
- * @retval None
- */
-void HAL_TIM_IC_MspInit(TIM_HandleTypeDef *htim_ic)
-{
-    GPIO_InitTypeDef GPIO_InitStruct = { 0 };
-    if (htim_ic->Instance == TIM4)
+    else if (htim_base->Instance == TIM4)
     {
         /* USER CODE BEGIN TIM4_MspInit 0 */
 
@@ -472,17 +462,7 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef *htim_base)
 
         /* USER CODE END TIM2_MspDeInit 1 */
     }
-}
-
-/**
- * @brief TIM_IC MSP De-Initialization
- * This function freeze the hardware resources used in this example
- * @param htim_ic: TIM_IC handle pointer
- * @retval None
- */
-void HAL_TIM_IC_MspDeInit(TIM_HandleTypeDef *htim_ic)
-{
-    if (htim_ic->Instance == TIM4)
+    else if (htim_base->Instance == TIM4)
     {
         /* USER CODE BEGIN TIM4_MspDeInit 0 */
 
