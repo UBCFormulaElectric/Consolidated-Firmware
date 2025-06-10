d2892fc7752e96f9a3886b4ac2598e51ee46c108
Kelm <lkevin2003@gmail.com>
2025-06-10 03:31:07 -0300

    removed precharge_ok variable from fault state

--- firmware/quintuna/BMS/src/app/states/app_faultState.c
+++ firmware/quintuna/BMS/src/app/states/app_faultState.c
@@ -16,10 +16,9 @@ static void app_faultStateRunOnEntry(void)
 static void app_faultStateRunOnTick100Hz(void)
 {
     const bool acc_fault_cleared = true; //! app_accumulator_checkFaults();
-    const bool precharge_ok      = !app_precharge_limitExceeded();
     const bool bms_fault_cleared = io_faultLatch_getLatchedStatus(&bms_ok_latch);
 
-    if (acc_fault_cleared && precharge_ok && bms_fault_cleared)
+    if (acc_fault_cleared && bms_fault_cleared)
     {
         app_stateMachine_setNextState(&init_state);
     }
