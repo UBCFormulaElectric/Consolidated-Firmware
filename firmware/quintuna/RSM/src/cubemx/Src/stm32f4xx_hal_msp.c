8fafa2580a710291f9befdb98324ff3a1948fcd6
AmirTajaddodi <amir.tajadodi@gmail.com>
2025-06-09 22:17:41 -0700

    some small chantes

--- firmware/quintuna/RSM/src/cubemx/Src/stm32f4xx_hal_msp.c
+++ firmware/quintuna/RSM/src/cubemx/Src/stm32f4xx_hal_msp.c
@@ -434,6 +434,9 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim_base)
         GPIO_InitStruct.Alternate = GPIO_AF2_TIM4;
         HAL_GPIO_Init(FLOW_METER_5V_GPIO_Port, &GPIO_InitStruct);
 
+        /* TIM4 interrupt Init */
+        HAL_NVIC_SetPriority(TIM4_IRQn, 5, 0);
+        HAL_NVIC_EnableIRQ(TIM4_IRQn);
         /* USER CODE BEGIN TIM4_MspInit 1 */
 
         /* USER CODE END TIM4_MspInit 1 */
@@ -475,6 +478,8 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef *htim_base)
         */
         HAL_GPIO_DeInit(FLOW_METER_5V_GPIO_Port, FLOW_METER_5V_Pin);
 
+        /* TIM4 interrupt DeInit */
+        HAL_NVIC_DisableIRQ(TIM4_IRQn);
         /* USER CODE BEGIN TIM4_MspDeInit 1 */
 
         /* USER CODE END TIM4_MspDeInit 1 */
