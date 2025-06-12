566cd0246cd17b0702c043dcdf57f8793bb2d957
Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>
2025-06-11 21:02:19 -0700

    whoops

--- firmware/quintuna/BMS/src/app/app_precharge.c
+++ firmware/quintuna/BMS/src/app/app_precharge.c
@@ -44,7 +44,8 @@ void app_precharge_restart(void)
 
 PrechargeState app_precharge_poll(bool precharge_for_charging)
 {
-    float       ts_voltage   = app_tractiveSystem_getVoltage();
+    float ts_voltage = app_tractiveSystem_getVoltage();
+    // TODO: Change to actual pack voltage.
     const float pack_voltage = 300.0f;
     // float threshold_voltage = app_segments_getPackVoltage() * PRECHARGE_ACC_V_THRESHOLD;
     float threshold_voltage = pack_voltage * PRECHARGE_ACC_V_THRESHOLD;
