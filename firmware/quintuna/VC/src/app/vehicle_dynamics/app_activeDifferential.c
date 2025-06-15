d2adc9c937142ff229c3b7d7cfb9a74675f0bb7e
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2025-06-15 09:53:47 -0700

    cleanup job

--- firmware/quintuna/VC/src/app/vehicle_dynamics/app_activeDifferential.c
+++ firmware/quintuna/VC/src/app/vehicle_dynamics/app_activeDifferential.c
@@ -3,26 +3,28 @@
 #include <math.h>
 #include "app_units.h"
 #include "app_utils.h"
-#include <stdlib.h>
+
+static float powerToTorque(
+    const float power_kW,
+    const float motor_speed_fl_rpm,
+    const float motor_speed_fr_rpm,
+    const float motor_speed_rl_rpm,
+    const float motor_speed_rr_rpm,
+    const float cl,
+    const float cr)
+{
+    // TODO: Change conversion factor for AMK inverters
+    return POWER_TO_TORQUE_CONVERSION_FACTOR * power_kW /
+           ((motor_speed_fl_rpm + motor_speed_fr_rpm) * cl + (motor_speed_rl_rpm + motor_speed_rr_rpm) * cr +
+            SMALL_EPSILON);
+}
 
 void app_activeDifferential_computeTorque(ActiveDifferential_Inputs *inputs, ActiveDifferential_Outputs *outputs)
 {
     const float Delta = app_activeDifferential_wheelAngleToSpeedDelta(inputs->wheel_angle_deg);
-    const float cl, cr;
+    const float cl = inputs->is_regen_mode ? 1 - Delta : 1 + Delta, cr = inputs->is_regen_mode ? 1 + Delta : 1 - Delta;
 
-    // TODO: Someone help me double check this pls
-    if (inputs->is_regen_mode)
-    {
-        cl = 1 - Delta;
-        cr = 1 + Delta;
-    }
-    else
-    {
-        cl = 1 + Delta;
-        cr = 1 - Delta;
-    }
-
-    const float torque_lim_Nm = app_activeDifferential_powerToTorque(
+    const float torque_lim_Nm = powerToTorque(
         inputs->power_max_kW, inputs->motor_speed_fl_rpm, inputs->motor_speed_fr_rpm, inputs->motor_speed_rl_rpm,
         inputs->motor_speed_rr_rpm, cl, cr);
 
@@ -31,13 +33,13 @@ void app_activeDifferential_computeTorque(ActiveDifferential_Inputs *inputs, Act
     const float torque_max_Nm          = fmaxf(torque_left_Nm, torque_right_Nm);
     const float torque_negative_max_Nm = fminf(torque_left_Nm, torque_right_Nm);
 
-    const float scale = CLAMP_TO_ONE(inputs->derating_value);
-  
-    if (inputs->is_regen_mode && (torque_negative_max_Nm < -torque_lim_Nm))
+    float scale = CLAMP_TO_ONE(inputs->derating_value);
+
+    if (inputs->is_regen_mode && torque_negative_max_Nm < -torque_lim_Nm)
     {
         scale *= -torque_lim_Nm / torque_negative_max_Nm;
     }
-    else if (!inputs->is_regen_mode && (torque_max_Nm > torque_lim_Nm))
+    else if (!inputs->is_regen_mode && torque_max_Nm > torque_lim_Nm)
     {
         scale = torque_lim_Nm / torque_max_Nm;
     }
@@ -54,19 +56,4 @@ float app_activeDifferential_wheelAngleToSpeedDelta(const float wheel_angle_deg)
     // angle > 0 = right, angle < 0 = left
 
     return TRACK_WIDTH_mm * tanf(DEG_TO_RAD(wheel_angle_deg)) / (2 * WHEELBASE_mm);
-}
-
-float app_activeDifferential_powerToTorque(
-    const float power_kW,
-    const float motor_speed_fl_rpm,
-    const float motor_speed_fr_rpm,
-    const float motor_speed_rl_rpm,
-    const float motor_speed_rr_rpm,
-    const float cl,
-    const float cr)
-{
-    // TODO: Change conversion factor for AMK inverters
-    return (POWER_TO_TORQUE_CONVERSION_FACTOR * power_kW) /
-           ((motor_speed_fl_rpm + motor_speed_fr_rpm) * cl + (motor_speed_rl_rpm + motor_speed_rr_rpm) * cr +
-            SMALL_EPSILON);
 }
--- firmware/quintuna/VC/src/app/vehicle_dynamics/app_activeDifferential.h
+++ firmware/quintuna/VC/src/app/vehicle_dynamics/app_activeDifferential.h
@@ -4,13 +4,4 @@
 
 void app_activeDifferential_computeTorque(ActiveDifferential_Inputs *inputs, ActiveDifferential_Outputs *outputs);
 
-float app_activeDifferential_wheelAngleToSpeedDelta(float wheel_angle);
-
-float app_activeDifferential_powerToTorque(
-    float power_kW,
-    float motor_speed_rr_rpm,
-    float motor_speed_rl_rpm,
-    float motor_speed_fr_rpm,
-    float motor_speed_fl_rpm,
-    float cl,
-    float cr);
+float app_activeDifferential_wheelAngleToSpeedDelta(float wheel_angle_deg);
