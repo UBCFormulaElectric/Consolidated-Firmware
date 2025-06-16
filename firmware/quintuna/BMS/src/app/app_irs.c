8fb2721cdc0970539b49912750b26e5248bae7d1
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2025-06-16 13:20:21 -0700

    precharge

--- firmware/quintuna/BMS/src/app/app_irs.c
+++ firmware/quintuna/BMS/src/app/app_irs.c
@@ -8,7 +8,7 @@ void app_irs_broadcast()
     const ContactorState negative_closed  = io_irs_negativeState();
     const ContactorState positive_closed  = io_irs_positiveState();
     const ContactorState precharge_closed = io_irs_prechargeState();
-    app_canTx_BMS_IrNegative_set((ContactorState)negative_closed);
-    app_canTx_BMS_IrPositive_set((ContactorState)positive_closed);
-    app_canTx_BMS_PrechargeRelay_set((ContactorState)precharge_closed);
+    app_canTx_BMS_IrNegative_set(negative_closed);
+    app_canTx_BMS_IrPositive_set(positive_closed);
+    app_canTx_BMS_PrechargeRelay_set(precharge_closed);
 }
