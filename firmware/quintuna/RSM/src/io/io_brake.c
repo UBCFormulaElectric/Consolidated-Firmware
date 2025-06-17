7a081d4c429659df698662201eaab4e1b2cd87ae
Aditya-Dhiman4 <aditya16dhiman@gmail.com>
2025-06-17 03:37:33 -0400

    i am trusting joe herer

--- firmware/quintuna/RSM/src/io/io_brake.c
+++ firmware/quintuna/RSM/src/io/io_brake.c
@@ -3,12 +3,12 @@
 
 #define BRAKE_PRESSURE_OC_THRESHOLD_V (0.33f) // Under-voltage threshold (indicative of open circuit)
 #define BRAKE_PRESSURE_SC_THRESHOLD_V (3.0f)  // Over-voltage threshold (indicative of short circuit)
-#define BRAKE_ACTUATED_THRESHOLD_PSI (8.0f)   // Minimum pressure (in Psi) to consider the brake actuated
+#define BRAKE_ACTUATED_THRESHOLD_PSI (200.0f)   // Minimum pressure (in Psi) to consider the brake actuated
 
 // Maximum measurable pressure (in Psi) over the sensor's operating range
 // Psi per Volt: (Max Pressure - Min Pressure) / (Max Input Voltage - Min Input Voltage)
 // https://www.cdiweb.com/products/detail/m3031000005100pg-te-connectivity/327606
-#define BRAKE_PSI_PER_VOLT (100.0f / (BRAKE_PRESSURE_SC_THRESHOLD_V - BRAKE_PRESSURE_OC_THRESHOLD_V))
+#define BRAKE_PSI_PER_VOLT (2500.0f / (BRAKE_PRESSURE_SC_THRESHOLD_V - BRAKE_PRESSURE_OC_THRESHOLD_V))
 
 // Converts an ADC voltage reading to brake pressure (in Psi)
 static float pressureFromVoltage(float voltage)
