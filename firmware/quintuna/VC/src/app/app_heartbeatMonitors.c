54a57558366734b759d4e9a9feca124bbc8aa9f3
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2025-06-17 23:25:19 -0400

    finalize

--- firmware/quintuna/VC/src/app/app_heartbeatMonitors.c
+++ firmware/quintuna/VC/src/app/app_heartbeatMonitors.c
@@ -2,10 +2,9 @@
 
 // CAN
 #include "app_canRx.h"
-#include "app_canTx.h"
 #include "app_canAlerts.h"
 
-static HeartbeatBoard heartbeat_boards[5] = {
+static HeartbeatBoard heartbeat_boards[6] = {
     // bms
     { .getter       = app_canRx_BMS_Heartbeat_get,
       .resetter     = app_canRx_BMS_Heartbeat_update,
@@ -15,8 +14,8 @@ static HeartbeatBoard heartbeat_boards[5] = {
     // rsm
     { .getter       = app_canRx_RSM_Heartbeat_get,
       .resetter     = app_canRx_RSM_Heartbeat_update,
-      .fault_setter = app_canAlerts_VC_Warning_MissingRSMHeartbeat_set,
-      .fault_getter = app_canAlerts_VC_Warning_MissingRSMHeartbeat_get,
+      .fault_setter = app_canAlerts_VC_Info_MissingRSMHeartbeat_set,
+      .fault_getter = app_canAlerts_VC_Info_MissingRSMHeartbeat_get,
       .timeout_ms   = 200 },
     // fsm
     { .getter       = app_canRx_FSM_Heartbeat_get,
@@ -33,9 +32,15 @@ static HeartbeatBoard heartbeat_boards[5] = {
     // vcr
     { .getter       = app_canRx_VCR_Heartbeat_get,
       .resetter     = app_canRx_VCR_Heartbeat_update,
-      .fault_setter = app_canAlerts_VC_Warning_MissingVCRHeartbeat_set,
-      .fault_getter = app_canAlerts_VC_Warning_MissingVCRHeartbeat_get,
+      .fault_setter = app_canAlerts_VC_Info_MissingVCRHeartbeat_set,
+      .fault_getter = app_canAlerts_VC_Info_MissingVCRHeartbeat_get,
+      .timeout_ms   = 200 },
+    // DAM
+    { .getter       = app_canRx_DAM_Heartbeat_get,
+      .resetter     = app_canRx_DAM_Heartbeat_update,
+      .fault_setter = app_canAlerts_VC_Info_MissingDAMHeartbeat_set,
+      .fault_getter = app_canAlerts_VC_Info_MissingDAMHeartbeat_get,
       .timeout_ms   = 200 }
 };
 
-const HeartbeatMonitor hb_monitor = { .boards = heartbeat_boards, .board_count = 5, .block_faults = false };
+const HeartbeatMonitor hb_monitor = { .boards = heartbeat_boards, .board_count = 6, .block_faults = false };
