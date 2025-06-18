659efad192ba2bee54a0cfa6bb1ed1b6529923c5
Aditya-Dhiman4 <87353212+Aditya-Dhiman4@users.noreply.github.com>
2025-06-17 20:55:57 -0700

    VC Control Rear Pump (#1669)
    ### Changelist 
    added app layer pumps in rsm
    added can msg in app layer pumps in vc that controls rsm pump
    
    ### Testing Done
    
    ### Resolved Tickets
    
    ---------
    
    Co-authored-by: Edwin Z <20777515+Lucien950@users.noreply.github.com>

--- firmware/quintuna/VC/src/app/app_pumpControl.c
+++ firmware/quintuna/VC/src/app/app_pumpControl.c
@@ -11,14 +11,15 @@ static uint16_t time             = 0;
 
 #define CURRENT_THRESH 0.025f
 
-static inline void pumpControl_rampUp(void)
+static void pumpControl_rampUp(void)
 {
     // if we are done ramping up note that we are not ramping up
+
     if (finished_ramp_up)
     {
         return;
     }
-    // calcualte percentage based on defined slope above
+    // calculate percentage based on defined slope above
     uint8_t percentage = (uint8_t)(SLOPE * time);
     app_canTx_VC_PumpRampUpSetPoint_set((uint32_t)percentage);
 
@@ -29,7 +30,7 @@ static inline void pumpControl_rampUp(void)
     }
 }
 
-static inline void pumpControl_stopFlow(void)
+static void pumpControl_stopFlow(void)
 {
     app_canTx_VC_PumpFailure_set(true);
     finished_ramp_up = false;
@@ -38,12 +39,17 @@ static inline void pumpControl_stopFlow(void)
 
 void app_pumpControl_MonitorPumps(void)
 {
+    time += 10;
     const bool pumps_ok = io_TILoadswitch_pgood(&rl_pump_loadswitch);
 
     const bool pumps_enabled = io_loadswitch_isChannelEnabled(efuse_channels[EFUSE_CHANNEL_RL_PUMP]);
 
-    if (pumps_ok && pumps_enabled)
+    bool ramp_up_pumps = pumps_ok && pumps_enabled;
+
+    if (ramp_up_pumps)
         pumpControl_rampUp();
     else
         pumpControl_stopFlow();
+
+    app_canTx_VC_RsmTurnOnPump_set(ramp_up_pumps);
 }
