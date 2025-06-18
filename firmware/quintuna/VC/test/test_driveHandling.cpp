e341bf681dff3a450bbd1cd32276e17c730d0572
Aditya-Dhiman4 <aditya16dhiman@gmail.com>
2025-06-18 15:21:27 -0400

    fixed drive handling test

--- firmware/quintuna/VC/test/test_driveHandling.cpp
+++ firmware/quintuna/VC/test/test_driveHandling.cpp
@@ -1,4 +1,5 @@
-#include "app_canUtils.h"
+#include <gtest/gtest.h>
+
 #include "test_VCBase.hpp"
 
 extern "C"
@@ -8,18 +9,23 @@ extern "C"
 #include "app_canRx.h"
 #include "app_canTx.h"
 #include "app_canAlerts.h"
-#include "io_pcm.h"
 #include "app_vehicleDynamicsConstants.h"
+#include "app_driveHandling.h"
+#include "app_canUtils.h"
 }
 
 class DriveHandlingTest : public VCBaseTest
 {
 };
 
-TEST_F(DriveHandlingTest, DriveModeVanillaNotPowerLimiting)
+TEST_F(DriveHandlingTest, DriveModeVanillaNotPowerLimiting_FullPower_LowRPM)
 {
+    // assumes full power
     uint32_t bms_avail_power_w = 80000;
     uint32_t inverter_rpm      = 1000;
+    const float apps              = 1.0f;
+
+    TorqueAllocationOutputs torqueOutputToMotors;
 
     app_canRx_BMS_AvailablePower_update(bms_avail_power_w);
     app_canRx_INVFR_ActualVelocity_update(inverter_rpm);
@@ -27,8 +33,69 @@ TEST_F(DriveHandlingTest, DriveModeVanillaNotPowerLimiting)
     app_canRx_INVRR_ActualVelocity_update(inverter_rpm);
     app_canRx_INVRL_ActualVelocity_update(inverter_rpm);
 
-    float torque_limit     = (float)(bms_avail_power_w / (4 * inverter_rpm));
-    float torque_to_motors = fminf(torque_limit, MAX_TORQUE_REQUEST_NM);
+    app_vanillaDrive_run(apps, &torqueOutputToMotors);
 
-    ASSERT_TRUE(torque_to_motors == MAX_TORQUE_REQUEST_NM);
+    ASSERT_TRUE(torqueOutputToMotors.front_left_torque == MAX_TORQUE_REQUEST_NM);
+}
+
+TEST_F(DriveHandlingTest, DriveModeVanillaWithPowerLimiting_FullPower_HighRPM)
+{
+    // Full power
+    uint32_t bms_avail_power_w = 80000;
+    // Higher RPM will enable power limiting
+    uint32_t inverter_rpm      = 10000;
+    const float apps              = 1.0f;
+
+    TorqueAllocationOutputs torqueOutputToMotors;
+
+    app_canRx_BMS_AvailablePower_update(bms_avail_power_w);
+    app_canRx_INVFR_ActualVelocity_update(inverter_rpm);
+    app_canRx_INVFL_ActualVelocity_update(inverter_rpm);
+    app_canRx_INVRR_ActualVelocity_update(inverter_rpm);
+    app_canRx_INVRL_ActualVelocity_update(inverter_rpm);
+
+    app_vanillaDrive_run(apps, &torqueOutputToMotors);
+
+    ASSERT_TRUE(torqueOutputToMotors.front_left_torque < MAX_TORQUE_REQUEST_NM);
+}
+
+TEST_F(DriveHandlingTest, DriveModeVanillaWithPowerLimiting_LessPower_LessRPM)
+{
+    // less power will enable power limiting
+    uint32_t bms_avail_power_w = 70000;
+    uint32_t inverter_rpm      = 1000;
+    const float apps              = 1.0f;
+
+    TorqueAllocationOutputs torqueOutputToMotors;
+
+    app_canRx_BMS_AvailablePower_update(bms_avail_power_w);
+    app_canRx_INVFR_ActualVelocity_update(inverter_rpm);
+    app_canRx_INVFL_ActualVelocity_update(inverter_rpm);
+    app_canRx_INVRR_ActualVelocity_update(inverter_rpm);
+    app_canRx_INVRL_ActualVelocity_update(inverter_rpm);
+
+    app_vanillaDrive_run(apps, &torqueOutputToMotors);
+
+    ASSERT_TRUE(torqueOutputToMotors.front_left_torque == MAX_TORQUE_REQUEST_NM);
+}
+
+TEST_F(DriveHandlingTest, DriveModeVanillaWithPowerLimiting_LessPower_HighRPM)
+{
+    // less power will enable power limiting
+    uint32_t bms_avail_power_w = 70000;
+    // high rpm wll also enable power limiting
+    uint32_t inverter_rpm      = 10000;
+    const float apps              = 1.0f;
+
+    TorqueAllocationOutputs torqueOutputToMotors;
+
+    app_canRx_BMS_AvailablePower_update(bms_avail_power_w);
+    app_canRx_INVFR_ActualVelocity_update(inverter_rpm);
+    app_canRx_INVFL_ActualVelocity_update(inverter_rpm);
+    app_canRx_INVRR_ActualVelocity_update(inverter_rpm);
+    app_canRx_INVRL_ActualVelocity_update(inverter_rpm);
+
+    app_vanillaDrive_run(apps, &torqueOutputToMotors);
+
+    ASSERT_TRUE(torqueOutputToMotors.front_left_torque < MAX_TORQUE_REQUEST_NM);
 }
