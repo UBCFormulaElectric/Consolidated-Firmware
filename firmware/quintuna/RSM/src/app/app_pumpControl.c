36673535971ac59458a0503dd016b0c8eed176f0
Edwin Z <20777515+Lucien950@users.noreply.github.com>
2025-06-17 23:50:42 -0400

    fixed

--- firmware/quintuna/RSM/src/app/app_pumpControl.c
+++ firmware/quintuna/RSM/src/app/app_pumpControl.c
@@ -1,8 +1,7 @@
 #include "app_pumpControl.h"
+
 #include "io_rPump.h"
-#include "io_time.h"
-#include <app_canTx.h>
-#include <app_canRx.h>
+#include "app_canRx.h"
 
 void app_pumpControl_MonitorPumps(void)
 {
