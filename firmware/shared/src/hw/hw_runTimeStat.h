e61b553aeb5faff98d937cd88d0ced4292977fed
AmirTajaddodi <amir.tajadodi@gmail.com>
2025-07-13 21:14:18 -0700

    added concept of max cpu usage

--- firmware/shared/src/hw/hw_runTimeStat.h
+++ firmware/shared/src/hw/hw_runTimeStat.h
@@ -5,8 +5,9 @@
 
 typedef struct
 {
-    float cpu_usage;
-    float stack_usage;
+    float cpu_curr_usage;
+    float cpu_max_usage;
+    float stack_usage_max;
 } RunTimeStats;
 
 void hw_runTimeStat_init(TIM_HandleTypeDef *htim);
