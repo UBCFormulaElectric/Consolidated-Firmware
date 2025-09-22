1513a899aede151bf8731e8a415a65913a184dcf
Jugveer-Sandher <jugveersandher@gmail.com>
2025-09-22 16:13:25 -0700

    enabled freeRTOS

--- firmware/hexray/BMS/src/cubemx/Inc/FreeRTOSConfig.h
+++ firmware/hexray/BMS/src/cubemx/Inc/FreeRTOSConfig.h
@@ -51,12 +51,13 @@
 #if defined(__ICCARM__) || defined(__CC_ARM) || defined(__GNUC__)
 #include <stdint.h>
 extern uint32_t SystemCoreClock;
+void            xPortSysTickHandler(void);
 #endif
 #ifndef CMSIS_device_header
 #define CMSIS_device_header "stm32h7xx.h"
 #endif /* CMSIS_device_header */
 
-#define configENABLE_FPU 1
+#define configENABLE_FPU 0
 #define configENABLE_MPU 0
 
 #define configUSE_PREEMPTION 1
@@ -92,9 +93,6 @@ extern uint32_t SystemCoreClock;
 #define configTIMER_TASK_PRIORITY (2)
 #define configTIMER_QUEUE_LENGTH 10
 #define configTIMER_TASK_STACK_DEPTH 256
-
-/* The following flag must be enabled only when using newlib */
-#define configUSE_NEWLIB_REENTRANT 1
 
 /* CMSIS-RTOS V2 flags */
 #define configUSE_OS2_THREAD_SUSPEND_RESUME 1
--- firmware/hexray/VC/src/cubemx/Inc/FreeRTOSConfig.h
+++ firmware/hexray/VC/src/cubemx/Inc/FreeRTOSConfig.h
@@ -51,6 +51,7 @@
 #if defined(__ICCARM__) || defined(__CC_ARM) || defined(__GNUC__)
 #include <stdint.h>
 extern uint32_t SystemCoreClock;
+void            xPortSysTickHandler(void);
 #endif
 #ifndef CMSIS_device_header
 #define CMSIS_device_header "stm32h7xx.h"
@@ -92,9 +93,6 @@ extern uint32_t SystemCoreClock;
 #define configTIMER_TASK_PRIORITY (2)
 #define configTIMER_QUEUE_LENGTH 10
 #define configTIMER_TASK_STACK_DEPTH 256
-
-/* The following flag must be enabled only when using newlib */
-#define configUSE_NEWLIB_REENTRANT 1
 
 /* CMSIS-RTOS V2 flags */
 #define configUSE_OS2_THREAD_SUSPEND_RESUME 1
@@ -171,7 +169,7 @@ standard names. */
 /* IMPORTANT: After 10.3.1 update, Systick_Handler comes from NVIC (if SYS timebase = systick), otherwise from
  * cmsis_os2.c */
 
-#define USE_CUSTOM_SYSTICK_HANDLER_IMPLEMENTATION 0
+#define USE_CUSTOM_SYSTICK_HANDLER_IMPLEMENTATION 1
 
 /* USER CODE BEGIN Defines */
 #include "hw_freeRtosConfigs.h"
