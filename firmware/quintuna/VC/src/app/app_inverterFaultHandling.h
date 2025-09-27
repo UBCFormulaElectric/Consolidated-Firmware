75d3eedaf79781beb80e035a635acc50e680c33b
Setare <setimalek@gmail.com>
2025-09-26 20:27:30 -0700

    adding a fault handling state to the sm

--- firmware/quintuna/VC/src/app/app_inverterFaultHandling.h
+++ firmware/quintuna/VC/src/app/app_inverterFaultHandling.h
@@ -1,2 +1,6 @@
 #include "app_canUtils.h"
 #include <stdint.h>
+
+void hardware_reset (inv_type inverter);
+void inverter_retry (inv_type inverter);
+fault_handler invfr_error_handling (uint32_t *INVFR_ErrorInfo_val, uint32_t *INVFL_ErrorInfo_val, uint32_t *INVRR_ErrorInfo_val, uint32_t *INVRL_ErrorInfo_val);
