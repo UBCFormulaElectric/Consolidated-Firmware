33892f0db75d82ce96dc42a6a6197563d6e97ace
jackyao <jackyao2004@gmail.com>
2025-10-02 16:54:05 -0700

    influx connection

--- software/tracksight/live_data/app/settings.py
+++ software/tracksight/live_data/app/settings.py
@@ -1,6 +1,5 @@
 import os
 from pathlib import Path
-from typing import LiteralString
 
 from dotenv import load_dotenv
 
@@ -19,9 +18,7 @@ def str2bool(v: str | None):
 DOCKERIZED: bool = os.environ.get("DOCKERIZED") == "1"
 
 INFLUX_BUCKET: str = os.environ.get("INFLUXDB_BUCKET", "can_data")
-INFLUX_URL: LiteralString = os.environ.get(
-    "INFLUXDB_URL", f"http://{'influx' if DOCKERIZED else 'localhost'}:8086"
-)
+INFLUX_URL: str | None = os.environ.get("INFLUXDB_URL")
 INFLUX_ORG: str | None = os.environ.get("INFLUXDB_ORG")
 INFLUX_TOKEN: str | None = os.environ.get("ADMIN_TOKEN")
 
