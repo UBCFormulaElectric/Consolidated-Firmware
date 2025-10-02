8f6dddecb3f72c444a8bf8a10a16c26523d5e816
Edwin <20777515+Lucien950@users.noreply.github.com>
2025-10-01 20:50:25 -0700

    DAM New TSIM Logic (#1733)
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    ### Resolved Tickets
    <!-- Link any tickets that this PR resolves. -->
    
    Co-authored-by: Gus Tahara-Edmonds <gtaharaedmonds@gmail.com>

--- firmware/quintuna/DAM/src/cubemx/Src/main.c
+++ firmware/quintuna/DAM/src/cubemx/Src/main.c
@@ -666,10 +666,10 @@ static void MX_GPIO_Init(void)
     HAL_GPIO_WritePin(GPIOE, TELEM_PWR_EN_Pin | BUZZER_PWR_EN_Pin, GPIO_PIN_RESET);
 
     /*Configure GPIO pin Output Level */
-    HAL_GPIO_WritePin(nTSIM_GRN_EN_GPIO_Port, nTSIM_GRN_EN_Pin, GPIO_PIN_RESET);
+    HAL_GPIO_WritePin(nTSIM_GRN_EN_GPIO_Port, nTSIM_GRN_EN_Pin, GPIO_PIN_SET);
 
     /*Configure GPIO pin Output Level */
-    HAL_GPIO_WritePin(TSIM_RED_EN_GPIO_Port, TSIM_RED_EN_Pin, GPIO_PIN_SET);
+    HAL_GPIO_WritePin(TSIM_RED_EN_GPIO_Port, TSIM_RED_EN_Pin, GPIO_PIN_RESET);
 
     /*Configure GPIO pins : LED_Pin BOOT_LED_Pin SD_FAIL_Pin */
     GPIO_InitStruct.Pin   = LED_Pin | BOOT_LED_Pin | SD_FAIL_Pin;
