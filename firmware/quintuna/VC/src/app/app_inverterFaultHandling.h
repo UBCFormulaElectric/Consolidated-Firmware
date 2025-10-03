22a34e442a23e045d435768042df51ea363eb493
Setare <setimalek@gmail.com>
2025-10-03 16:36:47 -0700

    clean up for build

--- firmware/quintuna/VC/src/app/app_inverterFaultHandling.h
+++ firmware/quintuna/VC/src/app/app_inverterFaultHandling.h
@@ -1,6 +1,11 @@
+typedef int make_iso_compilers_happy;
+
+#if 0
 #include "app_canUtils.h"
 #include <stdint.h>
 
-void hardware_reset (inv_type inverter);
-void inverter_retry (inv_type inverter);
+
+void hardware_reset (InverterConfig inverter);
+void inverter_retry (InverterConfig inverter);
 fault_handler inv_error_handling (uint32_t *INVFR_ErrorInfo_val, uint32_t *INVFL_ErrorInfo_val, uint32_t *INVRR_ErrorInfo_val, uint32_t *INVRL_ErrorInfo_val);
+#endif
