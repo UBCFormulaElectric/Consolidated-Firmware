55ccefb3c436bf0f62609efb8e923860d3935dcb
Setare <setimalek@gmail.com>
2025-10-03 01:03:11 -0700

    cleaning up

--- firmware/quintuna/VC/src/app/states/app_faultHandlingState.c
+++ firmware/quintuna/VC/src/app/states/app_faultHandlingState.c
@@ -1,32 +1,19 @@
+#include "app_stateMachine.h"
 #include "app_states.h"
-
+#include "app_powerManager.h"
+#include "app_timer.h"
+#include "app_canAlerts.h"
+#include "app_warningHandling.h"
+#include "io_loadswitches.h"
 #include "app_canTx.h"
 #include "app_canRx.h"
+#include "app_canUtils.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include "io_log.h"
+#include "app_inverterBringup.h"
 
-#include <app_utils.h>
-#include "app_canAlerts.h"
-#include "app_powerManager.h"
-#include "app_warningHandling.h"
-#include "app_inverterFaultHandling.h"
-
-#include "app_regen.h"
-#include "app_vehicleDynamicsConstants.h"
-#include "app_torqueVectoring.h"
-#include "app_vehicleDynamics.h"
-#include "app_torqueDistribution.h"
-#include "app_driveHandling.h"
-#include "app_startSwitch.h"
-#include "app_inverterFaultHandling.h"
-
-
-#define OFF 0
-#define 
-
-static bool                    launch_control_switch_is_on;
-static bool                    regen_switch_is_on;
-static TorqueAllocationOutputs torqueOutputToMotors;
 static VCInverterFaults current_inverter_fault_state;
-
 
 static PowerManagerConfig power_manager_state = {
     .efuse_configs = { [EFUSE_CHANNEL_F_INV]   = { .efuse_enable = true, .timeout = 0, .max_retry = 5 },
@@ -42,23 +29,25 @@ static PowerManagerConfig power_manager_state = {
 
 typedef enum { INVERTER_FL, INVERTER_FR, INVERTER_RL, INVERTER_RR, NUM_INVERTERS } InverterConfig;
 extern InverterWarningHandling inverter_reset_handle[NUM_INVERTERS];
-typedef enum {
-    259 = 0,
-    1342 = 1,
-    2311 = 2,
-} uint32_t LockoutErrorCodes;
+
+/*Lockout error just means don't retry you need to power cycle
+add any new exceptions error codes here if you don't want retry*/
+static inline bool is_lockout_code(uint32_t code)
+{
+    switch (code) {
+    case 259u:
+    case 1342u:
+    case 2311u:
+        return true;
+    default:
+        return false;
+    }
+}
 
 
+static inline bool inv_bError(InverterConfig inv)
 {
-    void (*can_enable_inv)(bool);
-    void (*can_invOn)(bool);
-    void (*can_dcOn)(bool);
-    uint32_t (*can_error_info)(void);
-    void (*error_reset)(bool);
-} InverterWarningHandling;
-static inline bool inv_bError(InverterConfig i)
-{
-    switch (i) {
+    switch (inv) {
     case INVERTER_FL: return app_canRx_INVFL_bError_get();
     case INVERTER_FR: return app_canRx_INVFR_bError_get();
     case INVERTER_RL: return app_canRx_INVRL_bError_get();
@@ -67,42 +56,71 @@ static inline bool inv_bError(InverterConfig i)
     }
 }
 
-
 static void faultHandlingStateRunOnEntry(void)
 {
     app_canTx_VC_State_set(VC_FAULT_STATE);
     app_canAlerts_VC_Info_InverterRetry_set(true);
     app_powerManager_updateConfig(power_manager_state);
     current_inverter_fault_state = INV_FAULT_RETRY;
-    //app_timer_init(&start_up_timer, FAULT_TIMEOUT_MS);
+    /*may not need this unless we wanna transition time out faults here too*/
+    app_timer_init(&start_up_timer, FAULT_TIMEOUT_MS);
 }
 
 static void FaultHandlingStateRunOnTick100Hz(void)
 {
-    switch (current_inverter_fault_state)
-    {
-        case INV_FAULT_RETRY:
-            inv_error_handling();
-            app_warningHandling_inverterReset();
-            current_inverter_fault_state = HW_RESET;
-            break;
+   switch (current_inverter_fault_state) {
+    case INV_FAULT_RETRY: {
+        bool any_faulted = false;
+        bool any_lockout = false;
 
-        case INV_FAULT_LOCKOUT:
-            // Stay in this state until a manual reset is done
-            break;
+        for (int i = 0; i < NUM_INVERTERS; ++i) {
 
-        case INV_FAULT_RECOVERED:
-        {
-            app_canAlerts_VC_info_InverterRetry_set(false);
-            app_stateMachine_setNextState(&hvInit_state);
+            retry_one_inverter((InverterConfig)->i);
+            any_faulted |= inv_bError((InverterConfig)->i);
+            any_lockout |= is_lockout_code(inverter_reset_handle[i].can_error_info());
+            if any_faulted{            
+            inverter_reset_handle[i].can_invOn(false);
+            inverter_reset_handle[i].can_dcOn(false);
+            inverter_reset_handle[i].can_enable_inv(false);
+            inverter_reset_handle[i].error_reset(true);
+            }
+            if (any_lockout) {
+            app_canAlerts_VC_Warning_InverterHwLockout_set(true);
+            s_all_clear_ticks = 0;
+            current_inverter_fault_state = INV_FAULT_LOCKOUT;
             break;
         }
-        default:
-            break;
 
-    app_canTx_VC_InverterState_set(current_inverter_state);
+        }
+
+
+        if (!any_faulted) {
+            if (++s_all_clear_ticks >= TICKS(CLEAR_STABLE_MS)) {
+                current_inverter_fault_state = INV_FAULT_RECOVERED;
+            }
+        } else {
+            s_all_clear_ticks = 0;
+        }
+        break;
     }
-}
+
+    case INV_FAULT_LOCKOUT:
+        // Do nothing here: no retry by design; wait for manual action or power cycle.
+        // (Outputs are already kept off in retry_one_inverter())
+        break;
+
+    case INV_FAULT_RECOVERED:
+        // Leave with reset lines LOW and return to bring-up
+        for (int i = 0; i < NUM_INVERTERS; ++i)
+            inverter_reset_handle[i].error_reset(false);
+
+        app_canAlerts_VC_Info_InverterRetry_set(false);
+        app_stateMachine_setNextState(&hvInit_state);   // redo Hvinit instead of everything 
+        break;
+
+    default:
+        break;
+    }}
 
 static void faultHandlingStateRunOnExit(void)
 {
