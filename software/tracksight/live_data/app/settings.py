83189049321d36da0fa8112caa205d9e35a0fa42
jackyao <jackyao2004@gmail.com>
2025-10-04 13:47:44 -0700

    templates

--- software/tracksight/live_data/app/settings.py
+++ software/tracksight/live_data/app/settings.py
@@ -3,19 +3,19 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
-_DOTENV_PATH = Path(__file__).resolve().parent.parent / ".env"
-if _DOTENV_PATH.exists():
+def get_env_bool(e: str):
+    val = os.environ.get(e)
+    return False if val is None else val.lower() in ("yes", "true", "t", "1")
+
+DOCKERIZED: bool = get_env_bool("DOCKERIZED")
+
+# App running on docker should have existing environment variables set up
+_DOTENV_PATH = Path(__file__).resolve().parent.parent / "live_data.env"
+if not DOCKERIZED and _DOTENV_PATH.exists():
     load_dotenv(_DOTENV_PATH)
 else:
     raise FileNotFoundError(f"Environment file not found at {_DOTENV_PATH}")
 
-
-def str2bool(v: str | None):
-    assert v is not None, "Environment variable cannot be None"
-    return v.lower() in ("yes", "true", "t", "1")
-
-
-DOCKERIZED: bool = os.environ.get("DOCKERIZED") == "1"
 
 INFLUX_BUCKET: str = os.environ.get("INFLUXDB_BUCKET", "can_data")
 INFLUX_URL: str | None = os.environ.get("INFLUXDB_URL")
@@ -31,7 +31,7 @@ DATA_SOURCE = os.environ.get("DATA_SOURCE", "mock").upper()
 
 SERIAL_PORT = os.environ.get("SERIAL_PORT")
 DATA_FILE = os.environ.get("DATA_FILE")
-DEBUG = str2bool(os.environ.get("DEBUG"))
+DEBUG = get_env_bool("DEBUG")
 
 SERVER_IP = os.environ.get("SERVER_IP", None)
 SERVER_DOMAIN_NAME = os.environ.get("SERVER_DOMAIN_NAME", "telem")
