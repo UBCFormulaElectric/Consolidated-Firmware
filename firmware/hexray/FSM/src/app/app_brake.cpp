2a460ba8e4fe8d6b84d00e50ec0e7159503dcdae
acp02 <aujames430@gmail.com>
2025-10-04 15:29:43 -0700

    temp commit

---
+++ firmware/hexray/FSM/src/app/app_brake.cpp
@@ -0,0 +1,20 @@
+#include "app_brake.hpp"
+
+#include "io_brake.hpp"
+//#include "app_canTx.h"    ***********Implement later***********
+//#include "app_canAlerts.h"    ***********Implement later***********
+
+#include <math.h>
+
+void app_brake_broadcast(void)
+{
+    const bool  brake_pressed             = io_brake_isActuated();
+    const float front_pressure            = io_brake_getFrontPressurePsi();
+    const bool  front_brake_pressure_ocsc = io_brake_OCSC();
+
+    /*app_canTx_FSM_BrakeActuated_set(brake_pressed);                         ***********Implement later***********
+    app_canTx_FSM_FrontBrakePressure_set((uint32_t)roundf(front_pressure));
+    app_canAlerts_FSM_Info_FrontBrakePressureOCSC_set(front_brake_pressure_ocsc);
+    app_canTx_FSM_Info_FrontBrakePressureOutOfRange_set(
+        (front_pressure >= MAX_BRAKE_PRESSURE_PSI) || (front_pressure < MIN_BRAKE_PRESSURE_PSI));*/
+}
--- firmware/hexray/FSM/src/jobs.cpp
+++ firmware/hexray/FSM/src/jobs.cpp
@@ -1,6 +1,68 @@
 #include "jobs.hpp"
+// app
+#include "app_commitInfo.h"
+#include "app_canTx.h"
+#include "app_canRx.h"
+#include "app_jsoncan.h"
+#include "app_imu.h"
+#include "app_apps.h"
+#include "app_brake.h"
+#include "app_steering.h"
+#include "app_shdnLoop.h"
+#include "app_suspension.h"
+#include "app_utils.h"
+// io
+#include "io_time.h"
+#include "io_canTx.h"
+#include "io_canQueue.h"
+#include "io_imu.h"
+#include "io_log.h"
+#include "app_jsoncan.h"
 
-void jobs_init() {}
-void jobs_run1Hz_tick() {}
-void jobs_run100Hz_tick() {}
-void jobs_run1kHz_tick() {}
+CanTxQueue can_tx_queue;
+
+static void canTransmit(const JsonCanMsg *msg)
+{
+    const CanMsg m = app_jsoncan_copyToCanMsg(msg);
+    io_canQueue_pushTx(&can_tx_queue, &m);
+}
+
+void jobs_init() 
+{
+    // can
+    io_canTx_init(canTransmit);
+    io_canTx_enableMode_can2(CAN2_MODE_DEFAULT, true);
+    io_canQueue_initRx();
+    io_canQueue_initTx(&can_tx_queue);
+    app_canTx_init();
+    app_canRx_init();
+    LOG_IF_ERR(io_imu_init());
+    app_apps_init();
+
+    app_canTx_FSM_Hash_set(GIT_COMMIT_HASH);
+    app_canTx_FSM_Clean_set(GIT_COMMIT_CLEAN);
+    app_canTx_FSM_Heartbeat_set(true);
+}
+
+void jobs_run1Hz_tick() 
+{
+    io_canTx_enqueue1HzMsgs();
+}
+
+void jobs_run100Hz_tick() 
+{
+    app_apps_broadcast();
+    app_brake_broadcast();
+    app_steering_broadcast();
+    app_shdnLoop_broadcast();
+    app_suspension_broadcast();
+    app_imu_broadcast();
+
+    io_canTx_enqueue100HzMsgs();
+}
+
+void jobs_run1kHz_tick() 
+{
+    io_canTx_enableMode_can2(CAN2_MODE_DEBUG, app_canRx_Debug_EnableDebugMode_get());
+    io_canTx_enqueueOtherPeriodicMsgs(io_time_getCurrentMs());
+}
