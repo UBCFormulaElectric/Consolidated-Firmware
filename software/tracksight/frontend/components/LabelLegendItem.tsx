befba7aeeee327aa9abff73f4a2092e9bfe2bf31
Eroxl <InfinityWaffles@outlook.com>
2025-10-07 17:24:29 -0700

    add demo stuff

---
+++ software/tracksight/frontend/components/LabelLegendItem.tsx
@@ -0,0 +1,30 @@
+import type { LegendItemRenderer } from "./Legend";
+
+const LabelLegendItem: LegendItemRenderer = (props) => {
+    const {
+        content,
+        color
+    } = props;
+
+    return (
+        <div
+            className="w-min flex gap-2 items-center"
+            style={{
+                borderColor: color,
+            }}
+        >
+            <div
+                className="size-4" 
+                style={{
+                    backgroundColor: color,
+                }}
+            />
+
+            <p className="text-primary-900 font-extralight">
+                {content}
+            </p>
+        </div>
+    );
+}
+
+export default LabelLegendItem;
