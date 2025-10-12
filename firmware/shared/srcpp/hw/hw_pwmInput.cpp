1ce6e528b2eda93b32148dc07c3aca740a0c0d9c
AmirTajaddodi <amir.tajadodi@gmail.com>
2025-10-12 15:56:07 -0700

    adding changes for pwm mode to accunt for frequency only and full pwmmode

--- firmware/shared/srcpp/hw/hw_pwmInput.cpp
+++ firmware/shared/srcpp/hw/hw_pwmInput.cpp
@@ -4,18 +4,11 @@
 #include <stm32f4xx_hal_tim.h>
 #include <tuple>
 
-
 namespace hw
 {
-void PwmInput::init(){
+void PwmInput::init()
+{
     assert(htim != NULL);
-
-    frequency_hz = 0;
-    first_tick = false;
-    curr_rising_edge = 0;
-    prev_rising_edge = 0;
-    duty_cycle = 0;
-    frequency_hz = 0;
 
     HAL_TIM_IC_Start_IT(htim, rising_edge_tim_channel);
     HAL_TIM_IC_Start_IT(htim, falling_edge_tim_channel);
@@ -23,30 +16,37 @@ void PwmInput::init(){
     HAL_TIM_Base_Start_IT(htim);
 }
 
-float PwmInput::get_frequency() const {
+float PwmInput::get_frequency(void) const
+{
     return frequency_hz;
-} 
+}
 
-float PwmInput::get_dutyCycle() const
+float PwmInput::get_dutyCycle(void) const
 {
     return duty_cycle;
 }
-TIM_HandleTypeDef *PwmInput::get_timer_handle() const{
+TIM_HandleTypeDef *PwmInput::get_timer_handle(void) const
+{
     return htim;
 }
 
-HAL_TIM_ActiveChannel PwmInput::get_timer_activeChannel() const{
+HAL_TIM_ActiveChannel PwmInput::get_timer_activeChannel(void) const;
+{
     return tim_active_channel;
 }
 
-void PwmInput::tick(){
-
+void PwmInput::tick(void)
+{
+    // Reset the timer overflow count to indicate that the PWM signal is active
     tim_overflow_count = 0;
 
+    // We store the counter values captured during two most recent rising edges.
+    // The difference between these two counter values is used to compute the
+    // frequency of the PWM input signal.
     if (first_tick)
     {
         curr_rising_edge = HAL_TIM_ReadCapturedValue(htim, rising_edge_tim_channel);
-        first_tick = false;
+        first_tick       = false;
     }
     else
     {
@@ -61,35 +61,43 @@ void PwmInput::tick(){
             rising_edge_detlta = curr_rising_edge - prev_rising_edge;
             PwmInput::setFrequency(tim_frequency_hz / (float)rising_edge_detlta);
         }
-        else if(curr_rising_edge < prev_rising_edge)
+        else if (curr_rising_edge < prev_rising_edge)
         {
+            // Occurs when the counter rolls over
             rising_edge_detlta = tim_auto_reload_reg - prev_rising_edge + curr_rising_edge;
             PwmInput::setFrequency(tim_frequency_hz / (float)rising_edge_detlta);
         }
         else
         {
+            // Occurs when the counter rolls over (i.e. The PWM frequency being
+            // measured is too low), or either when a tick arrives before the
+            // counter can upcount (i.e. The PWM frequency being measured is
+            // too high)
             PwmInput::setFrequency(0);
         }
     }
 
-    //Calculating duty cycle
-    if (curr_rising_edge != 0)
-    {
-        uint32_t falling_edge = HAL_TIM_ReadCapturedValue(htim, falling_edge_tim_channel);
+    if (mode == PwmMode::PWMINPUT){
+        // Calculating duty cycle
+        if (curr_rising_edge != 0)
+        {
+            uint32_t falling_edge = HAL_TIM_ReadCapturedValue(htim, falling_edge_tim_channel);
 
-        duty_cycle = 100.0f * (float) falling_edge/ (float) curr_rising_edge;
+            duty_cycle = 100.0f * (float)falling_edge / (float)curr_rising_edge;
+        }
     }
 }
 
-
-//change this to exit codes
-bool PwmInput::pwm_isActive() {
-
+//TODO: change this to exit codes
+bool PwmInput::pwm_isActive(void)
+{
+    // If the timer overflows twice without a rising edge, the PWM signal is
+    // likely inactive (i.e. DC signal) and its frequency can't be computed
     if (++tim_overflow_count == 2U)
     {
         PwmInput::setFrequency(0);
-        return false
+        return false;
     }
     return true;
 }
-}
+} // namespace hw
