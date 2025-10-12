c8dc6596f42b70247e54680527d999e283140bbf
Jugveer-Sandher <jugveersandher@gmail.com>
2025-10-12 12:48:38 -0700

    boot code

--- firmware/dev/h5dev/src/tasks.cpp
+++ firmware/dev/h5dev/src/tasks.cpp
@@ -5,9 +5,12 @@
 
 extern "C"
 {
+#include "app_jsoncan.h"
 #include "io_canQueue.h"
 #include "hw_fdcan.h"
 #include "hw_cans.h"
+#include "hw_bootup.h"
+#include "hw_resetReason.h"
 }
 
 static CanTxQueue can_tx_queue;
@@ -18,7 +21,32 @@ void tasks_init()
 {
     SEGGER_SYSVIEW_Conf();
     LOG_INFO("h5dev reset!");
+
+    __HAL_DBGMCU_FREEZE_IWDG();
+
     hw_can_init(&fdcan);
+    // LOG_IF_ERR(hw_usb_init());
+
+    // Check for watchdog timeout on a previous boot cycle.
+    const ResetReason reset_reason = hw_resetReason_get();
+    if (reset_reason == RESET_REASON_WATCHDOG)
+    {
+        LOG_WARN("Detected watchdog timeout on the previous boot cycle!");
+    }
+
+    BootRequest boot_request = hw_bootup_getBootRequest();
+    if (boot_request.context != BOOT_CONTEXT_NONE)
+    {
+        if (boot_request.context == BOOT_CONTEXT_STACK_OVERFLOW)
+        {
+            LOG_WARN("Detected stack overflow on the previous boot cycle!");
+        }
+
+        // Clear stack overflow bootup.
+        boot_request.context       = BOOT_CONTEXT_NONE;
+        boot_request.context_value = 0;
+        hw_bootup_setBootRequest(boot_request);
+    }
 }
 
 void tasks_run1Hz()
@@ -39,14 +67,19 @@ void tasks_run1kHz()
 }
 void tasks_runCanFDTx()
 {
-    forever {}
+    forever
+    {
+        CanMsg msg = io_canQueue_popTx(&can_tx_queue);
+        LOG_IF_ERR(hw_can_transmit(&fdcan, &msg));
+    }
 }
 void tasks_runCanRx()
 {
     forever
     {
-        const CanMsg msg = io_canQueue_popRx();
-        UNUSED(msg);
+        CanMsg     rx_msg         = io_canQueue_popRx();
+        JsonCanMsg jsoncan_rx_msg = app_jsoncan_copyFromCanMsg(&rx_msg);
+        io_canRx_updateRxTableWithMessage(&jsoncan_rx_msg);
     }
 }
 void tasks_runChimera()
