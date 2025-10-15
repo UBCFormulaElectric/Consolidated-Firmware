0d4960b7560c4faf3c90d8f3f92d3bbd28d1b296
Eroxl <InfinityWaffles@outlook.com>
2025-10-15 12:13:51 -0700

    add functionality to signal dropdown

--- software/tracksight/frontend/components/widgets/StateTimelineEditor.tsx
+++ software/tracksight/frontend/components/widgets/StateTimelineEditor.tsx
@@ -3,16 +3,21 @@ import getWidgetSchema from "@/lib/getWidgetSchema";
 import { useSignalMetadata } from "@/lib/contexts/SignalsContext";
 import SignalSelector from "../SignalSelector";
 import isEnumSignal from "@/lib/isEnumSignal";
+import { useRef } from "react";
+import { ChevronDown } from "lucide-react";
 
 const StateTimelineEditor: Widget<"stateTimeline"> = (props) => {
   const {
     signals,
     options,
     type,
+    editWidget
   } = props;
 
   const signalMetadata = useSignalMetadata(signals[0]);
   const enumOptions = Object.values(signalMetadata?.enum || {});
+
+  const signalDropdownRef = useRef<HTMLDivElement>(null) as React.RefObject<HTMLDivElement>;
 
   const {
     colorPalette
@@ -28,13 +33,22 @@ const StateTimelineEditor: Widget<"stateTimeline"> = (props) => {
       <div className="ml-2 pl-2 border-l-2 border-gray-200">
         <div className="font-medium text-lg flex flex-row gap-2 items-center">
           Signal:
-          <SignalSelector
-            filter={isEnumSignal}
-            selectedSignal={signalMetadata || null}
-            onSelect={(signal) => {
-              console.log("Selected signal:", signal);
-            }}
-          />
+          <div className="font-normal text-base flex flex-row gap-2 relative select-none hover:cursor-pointer" ref={signalDropdownRef}>
+            {signalMetadata ? signalMetadata.name : "No signal selected"}
+            <ChevronDown size={16} />
+            <SignalSelector
+              filter={isEnumSignal}
+              selectedSignal={signalMetadata || null}
+              onSelect={(signal) => {
+                if (!editWidget) return;
+
+                editWidget({
+                  signals: [signal.name]
+                })
+              }}
+              buttonElement={signalDropdownRef}
+            />
+          </div>
         </div>
       </div>
     </div>
