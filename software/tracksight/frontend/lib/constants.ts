a49c92ca9b3e13be27dd9253fd36f73e72864bf3
Eroxl <InfinityWaffles@outlook.com>
2025-10-16 22:28:06 -0700

    start implementing state for signals

--- software/tracksight/frontend/lib/constants.ts
+++ software/tracksight/frontend/lib/constants.ts
@@ -13,6 +13,9 @@ const WIDGET_SCHEMAS = [
     }
 ] as const;
 
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:5001/";
+
 export {
-    WIDGET_SCHEMAS
+    WIDGET_SCHEMAS,
+    API_BASE_URL,
 };
