97c15af1b35e029306a2d048e160d9ff256deac3
Edwin <20777515+Lucien950@users.noreply.github.com>
2025-10-20 17:43:37 -0700

    boothandler fix

--- firmware/shared/src/io/io_bootHandler.h
+++ firmware/shared/src/io/io_bootHandler.h
@@ -1,5 +1,4 @@
 #pragma once
 #include "io_canMsg.h"
-#include "app_utils.h"
 
-NORET void io_bootHandler_processBootRequest(const CanMsg *msg);
+void io_bootHandler_processBootRequest(const CanMsg *msg);
