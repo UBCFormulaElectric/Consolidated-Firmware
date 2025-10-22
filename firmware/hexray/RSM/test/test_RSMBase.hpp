b30ad9109bf2c21de4a0579f33941a770618a2f7
Shun Akiyama <shunakiyama@dhcp-206-12-183-72.ubcsecure.wireless.ubc.ca>
2025-10-21 22:14:27 -0700

    unit test continue

---
+++ firmware/hexray/RSM/test/test_RSMBase.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+class RSMBaseTest
+{
+    void board_setup() override
+    {
+
+    }
+
+    void board_teardown() override {}
+
+    void tick_100hz() override
+    {
+
+    }
+
+};
--- firmware/hexray/RSM/test/test_fakes.cpp
+++ firmware/hexray/RSM/test/test_fakes.cpp
@@ -1,5 +1,47 @@
+#include "test_fakes.hpp"
+#include "app_brake.hpp"
+#include "app_tireTemp.hpp"
 
-namespace fakes::io::brake
+namespace fakes::io
 {
-   
+   namespace brake
+   {    
+        static float rearPressurePsi = 0.0f;
+
+        void setRearPressurePsi(const float rear_PressurePsi)
+        {
+            rearPressurePsi = rear_PressurePsi;
+        }
+   }
+
+   namespace tireTemp
+   {
+        static float temperature = 0.0f;
+
+        void set(const float temp)
+        {
+            io::tireTemp::temperature = temp;
+        }
+
+        float get()
+        {
+            return(io::tireTemp::temperature);
+        }
+   }
+
+}
+
+namespace io {
+    namespace brake{
+        float getRearPressurePsi()
+        {
+            return fakes::io::brake::rearPressurePsi;
+        }
+
+        bool isActuated()
+        {
+        #define BRAKE_ACTUATED_THRESHOLD_PSI 200.0f
+            return fakes::io::brake::getRearPressurePsi() > BRAKE_ACTUATED_THRESHOLD_PSI;
+        }
+    }
 }
