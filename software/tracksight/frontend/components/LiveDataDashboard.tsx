6e7789c76d01bf306bb9c9cfa68812bbc9cc7209
Eroxl <InfinityWaffles@outlook.com>
2025-10-21 20:15:23 -0700

    editable card titles

--- software/tracksight/frontend/components/LiveDataDashboard.tsx
+++ software/tracksight/frontend/components/LiveDataDashboard.tsx
@@ -1,55 +1,30 @@
 "use client";
 
-import React, { useCallback } from "react";
-
-import type { Card as CardData } from "@/lib/types/DashboardCard";
-import type { WIDGET_TYPE, Widget as WidgetData } from "@/lib/types/Widget";
+import React from "react";
 
 import Card from "@/components/common/Card";
 import Widget from "@/components/widgets/Widget";
 import { useDashboardLayout } from "@/lib/contexts/DashboardLayout";
 import { useEditMode } from "@/lib/contexts/EditModeContext";
+import EditableDataDashboard from "./EditableDataDashboard";
 
 const LiveDataDashboard: React.FC = () => {
-  const { cards, editWidget, addWidget } = useDashboardLayout();
+  const { cards, addWidget, addCard } = useDashboardLayout();
   const { isEditMode } = useEditMode();
 
-  const createEditWidgetFunction = useCallback(
-    (parent: CardData, widget: WidgetData<WIDGET_TYPE>) => {
-      return (newWidgetData: Partial<WidgetData<WIDGET_TYPE>>) => {
-        editWidget(parent, widget, newWidgetData);
-      };
-    },
-    [editWidget]
-  );
+  if (isEditMode) {
+    return <EditableDataDashboard />;
+  }
 
   return (
-    <div className="flex h-full w-full flex-col">
+    <div className="flex h-full w-full flex-col gap-4">
       {cards.map((card) => (
-        <Card title={card.title}>
+        <Card
+          title={card.title || "Card"}
+        >
           {card.widgets.map((widget) => (
-            <Widget {...widget} editWidget={createEditWidgetFunction(card, widget)} />
+            <Widget {...widget} />
           ))}
-          {
-            isEditMode && (
-              <div className="w-full flex justify-center py-5">
-                <button
-                  className="mt-2 rounded bg-green-400 px-4 py-2 text-white hover:bg-green-500 hover:cursor-pointer"
-                  onClick={() => {
-                    addWidget(card, {
-                      type: "stateTimeline",
-                      signals: [],
-                      options: {
-                        colorPalette: ["#3b82f6", "#10b981", "#f59e0b", "#ef4444"],
-                      },
-                    })
-                  }}
-                >
-                  Add Widget
-                </button>
-              </div>
-            )
-          }
         </Card>
       ))}
     </div>
