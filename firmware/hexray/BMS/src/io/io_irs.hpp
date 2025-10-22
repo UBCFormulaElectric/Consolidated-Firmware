49a63d93d7e56f2313624eedeb643989912dc83a
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2025-10-21 21:03:37 -0700

    irs done

--- firmware/hexray/BMS/src/io/io_irs.hpp
+++ firmware/hexray/BMS/src/io/io_irs.hpp
@@ -7,27 +7,27 @@ namespace io::irs {
     * Check if the IR- is closed
     * @returns the state of the IR- relay.
     */
-    ContactorState io_irs_negativeState(void);
+    ContactorState negativeState(void);
 
     /**
     * Sets the IR- relay to the desired state
     */
-    void io_irs_setPositive(ContactorState state);
+    void setPositive(ContactorState state);
 
     /**
     * Check if the IR+ is closed
     * @returns the state of the IR+ relay.
     */
-    ContactorState io_irs_positiveState(void);
+    ContactorState positiveState(void);
 
     /**
     * Set the precharge relay to the desired state
     */
-    void io_irs_setPrecharge(ContactorState state);
+    void setPrecharge(ContactorState state);
 
     /**
     * Check if the precharge relay is closed
     * @returns the state of the precharge relay.
     */
-    ContactorState io_irs_prechargeState(void);
+    ContactorState prechargeState(void);
 }
