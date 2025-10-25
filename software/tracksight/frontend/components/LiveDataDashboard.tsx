f02955ed400e22706a3ce62f164fcaa6e542a991
Eroxl <InfinityWaffles@outlook.com>
2025-10-25 14:42:42 -0700

    use ref instead of global id

--- software/tracksight/frontend/components/LiveDataDashboard.tsx
+++ software/tracksight/frontend/components/LiveDataDashboard.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import type { FC } from "react";
+import { useRef } from "react";
 
 import Widget from "@/components/widgets/Widget";
 import { useDashboardLayout } from "@/lib/contexts/DashboardLayout";
@@ -8,10 +9,12 @@ import { useDashboardLayout } from "@/lib/contexts/DashboardLayout";
 const LiveDataDashboard: FC = () => {
   const { widgets } = useDashboardLayout();
 
+  const containerRef = useRef<HTMLDivElement>(null);
+
   return (
     <div
       className="flex h-full min-w-[200vw] flex-col gap-16"
-      id="data-dashboard-container"
+      ref={containerRef}
     >
       {widgets.map((widget, index) => (
         <div className="sticky left-0 w-screen" style={{ zIndex: widgets.length - index }}>
