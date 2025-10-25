e277538b533624a8068d14893b85497287ecbb89
Rijwal Sangey <rijwalsangey@gmail.com>
2025-10-25 16:28:28 -0700

    negative chnages

---
+++ firmware/hexray/VC/src/hw/hw_adcs.cpp
@@ -0,0 +1,12 @@
+#include "hw_adcs.hpp"
+
+namespace hw::adcs 
+{
+    static uint16_t adc1_raw_adc_values[6];
+    static uint16_t adc2_raw_adc_values[5];
+    static float    adc1_adc_voltages[6];
+    static float    adc2_adc_voltages[5];
+
+    
+}
+
