daf7efe9ce49c781039f81fe02d2736c6b330528
Aditya-Dhiman4 <aditya16dhiman@gmail.com>
2025-10-25 20:46:56 -0700

    renamed efuses and implemented ok function

--- firmware/shared/srcpp/io/efuse/io_efuse_TI_TPS2.cpp
+++ firmware/shared/srcpp/io/efuse/io_efuse_TI_TPS2.cpp
@@ -1,23 +1,23 @@
 #include <assert.h>
-#include "io_efuseTI.hpp"
+#include "io_efuse_TI_TPS2.hpp"
 #include "hw_gpio.hpp"
 
-namespace io::efuse
+namespace io
 {
-    TI_Efuse::TI_Efuse(const hw::Gpio &enable_gpio, const hw::Adc &sns_adc_channel, const hw::Gpio& pgood)
+    ST_TPS2_Efuse::ST_TPS2_Efuse(const hw::Gpio &enable_gpio, const hw::Adc &sns_adc_channel, const hw::Gpio& pgood)
     : Efuse(enable_gpio, sns_adc_channel) , pgood_gpio(pgood) {}
 
-    void        TI_Efuse::reset() {
+    void        ST_TPS2_Efuse::reset() {
         this->enable_gpio.writePin(false);
         this->enable_gpio.writePin(true);
         this->enable_gpio.writePin(false);
     }
 
-    const bool  TI_Efuse::pgood() {
+    const bool  ST_TPS2_Efuse::pgood() {
         return this->pgood_gpio.readPin();
     }
 
-    const bool  TI_Efuse::ok() {
+    const bool  ST_TPS2_Efuse::ok() {
         return this->pgood();
     }
 }
