01eab425cd53a76e14532225672cf3a70cea5fbf
shayana18 <shayanajmaluni@gmail.com>
2025-10-28 22:37:19 -0700

    initial architecture

---
+++ firmware/quintuna/VC/src/app/torque_vectoring/app_torque_vecotring.c
@@ -0,0 +1,6 @@
+// 1. call state estimation 
+// 2. call yaw rate control
+// 3. call torque allocator 
+// 4. call power limiting
+
+//
---
+++ firmware/quintuna/VC/src/app/torque_vectoring/controllers/torque_allocator/app_torque_allocator.c
@@ -0,0 +1,4 @@
+// yaw rate should be an input here
+// slip should be calculated and used as a constriant here 
+// load transfer should be applied here 
+// output torques to be used by power limiting
