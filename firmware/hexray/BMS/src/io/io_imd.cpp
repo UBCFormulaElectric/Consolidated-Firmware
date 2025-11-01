98884fc655dfbaff45802276f6784a32b3893609
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2025-11-01 16:23:31 -0700

    io stuff

--- firmware/hexray/BMS/src/io/io_imd.cpp
+++ firmware/hexray/BMS/src/io/io_imd.cpp
@@ -1,5 +1,6 @@
 #include "io_imd.hpp"
 #include "io_time.hpp"
+#include "hw_pwms.hpp"
 
 constexpr uint8_t PWM_TICKS_MAX = 255;
 
@@ -7,11 +8,26 @@ static uint8_t pwm_counter = 0;
 
 namespace io::imd
 {
-uint8_t pwmCounterTick(void) {}
+uint8_t pwmCounterTick() {
+    if (pwm_counter != PWM_TICKS_MAX) {
+        pwm_counter++;
+        return pwm_counter;
+    }
+    return pwm_counter;
+}
 
-float getFrequency(void) {}
+float getFrequency() {
+    return hw::PwmInput::getFrequency(&hw::pwms::imd_pwm_input);
+}
 
-float getDutyCycle(void) {}
+float getDutyCycle() {
+    return hw::PwmInput::getDutyCycle(&hw::pwms::imd_pwm_input);
+}
 
-uint32_t getTimeSincePowerOn(void) {}
+uint32_t getTimeSincePowerOn() {
+    // The IMD shares the same power rail as the BMS, so we assume that the IMD
+    // and BMS boot up at the same time. In other words, the IMD has been ON
+    // for just as long as the BMS.
+    return io::time::getCurrentMs();
+}
 } // namespace io::imd
--- firmware/hexray/BMS/src/io/io_irs.cpp
+++ firmware/hexray/BMS/src/io/io_irs.cpp
@@ -1,3 +1,4 @@
+#include "hw_gpios.hpp"
 #include "io_irs.hpp"
 
 namespace io::irs
@@ -10,13 +11,23 @@ constexpr float POSITIVE_CLOSED_THRESH_A  = 0.5f;
 constexpr float LOOP_GAIN                 = (1.0f / 20.0f);
 constexpr float LOOP_SHUNT_RES            = (1.0f / 75.0e-3f);
 
-ContactorState negativeState() {}
+ContactorState negativeState() {
+    return hw::Gpio::readPin(&hw::gpios::msd_shdn_sns_pin) ? CONTACTOR_STATE_CLOSED : CONTACTOR_STATE_OPEN;
+}
 
-void setPositive(const ContactorState state) {}
+void setPositive(const ContactorState state) {
+    hw::Gpio::writePin(&hw::gpios::ir_p_en_pin, state == CONTACTOR_STATE_CLOSED);
+}
 
-ContactorState positiveState() {}
+ContactorState positiveState() {
+    return hw::Gpio::readPin(&hw::gpios::ir_p_en_pin) ? CONTACTOR_STATE_CLOSED : CONTACTOR_STATE_OPEN;
+}
 
-void setPrecharge(const ContactorState state) {}
+void setPrecharge(const ContactorState state) {
+    hw::Gpio::writePin(&hw::gpios::precharge_en_pin, state == CONTACTOR_STATE_CLOSED);
+}
 
-ContactorState prechargeState() {}
+ContactorState prechargeState() {
+    return hw::Gpio::readPin(&hw::gpios::precharge_en_pin) ? CONTACTOR_STATE_CLOSED : CONTACTOR_STATE_OPEN;
+}
 } // namespace io::irs
