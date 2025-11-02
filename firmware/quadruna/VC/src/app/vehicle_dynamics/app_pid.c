11bf590fc69c588b95a10bf64d0cbc6f7b1f267b
tumi! <94874741+efabiyi@users.noreply.github.com>
2025-11-01 18:53:08 -0700

    Updated PID Library with C/C++ implementations of Feedforward, back calculation and derivative filtering

--- firmware/quadruna/VC/src/app/vehicle_dynamics/app_pid.c
+++ firmware/quadruna/VC/src/app/vehicle_dynamics/app_pid.c
@@ -3,21 +3,17 @@
 
 void app_pid_init(PID *pid, const PID_Config *conf)
 {
-    pid->Kp          = conf->Kp;
-    pid->Ki          = conf->Ki;
-    pid->Kd          = conf->Kd;
-    pid->Kb          = conf->Kb;
-    pid->prev_input  = 0.0f;
-    pid->prev_output = 0.0f;
-    pid->integral    = 0.0f;
-    pid->out_min     = conf->out_min;
-    pid->out_max     = conf->out_max;
-    pid->back_calculation = conf->back_calculation;
-    pid->prev_time   = io_time_getCurrentMs() - conf->sample_time;
+    pid->Kp         = conf->Kp;
+    pid->Ki         = conf->Ki;
+    pid->Kd         = conf->Kd;
+    pid->prev_input = 0.0f;
+    pid->integral   = 0.0f;
+    pid->out_min    = conf->out_min;
+    pid->out_max    = conf->out_max;
 }
 
 /**
- * PID controller effort implementation with derivative-on-measurement and discrete time handling
+ * PID controller effort implementation with derivative-on-measurement
  * @param pid data store
  * @param setpoint setpoint
  * @param input also "measurement"/"process variable"
@@ -25,44 +21,14 @@ void app_pid_init(PID *pid, const PID_Config *conf)
  */
 float app_pid_compute(PID *pid, const float setpoint, const float input)
 {
-    uint32_t current_time = io_time_getCurrentMs();
-    float dt = current_time - pid->prev_time;
-    pid->prev_time = current_time;
-    bool integrate =  true;
+    pid->error = setpoint - input;
+    pid->integral += pid->error;
+    pid->derivative = (input - pid->prev_input);
+    pid->prev_input = input;
+    float output    = pid->Kp * pid->error + pid->Ki * pid->integral - pid->Kd * pid->derivative;
 
-    if (dt >= pid->sample_time | pid->prev_output == 0)
-    {       
-        pid->error = setpoint - input;
-        
-        if(integrate){
-            pid->integral += pid->error * dt;
-        }else{
-            pid->integral = 0;
-        }
-
-        float raw_derivative = (input - pid->prev_input) / dt;
-        pid->prev_input = input;        
-
-        float u_calc = pid->Kp * pid->error + pid->Ki * pid->integral - pid->Kd * pid->filtered_derivative;
-
-        float u_actual  = CLAMP(u_calc, pid->out_min, pid->out_max);
-
-        //Saturation check (is ucalc - u_actual = 0?)
-        if(u_calc != u_actual){
-            integrate = false;  
-        }else{
-            integrate = true;
-        }
-
-        // Update integral with back calculation
-        if (pid->back_calculation){
-            pid-> integral = pid->integral + pid->Kb*(u_actual - u_calc);
-        }
-
-        return u_actual;
-    } else {
-        return pid->prev_output;
-    }
+    output = CLAMP(output, pid->out_min, pid->out_max);
+    return output;
 }
 
 void app_pid_requestReset(PID *pid)
--- firmware/quadruna/VC/src/app/vehicle_dynamics/app_pid.h
+++ firmware/quadruna/VC/src/app/vehicle_dynamics/app_pid.h
@@ -1,23 +1,16 @@
 #pragma once
-#include <stdint.h>
-#include <stdbool.h>
 
 typedef struct
 {
     float Kp;
     float Ki;
     float Kd;
-    float Kb;
     float error;
-    float filtered_derivative;
+    float derivative;
     float integral;
     float prev_input;
-    float prev_output;
     float out_min;
     float out_max;
-    bool back_calculation;
-    uint32_t prev_time;
-    uint32_t sample_time;
 } PID;
 
 typedef struct
@@ -25,11 +18,8 @@ typedef struct
     const float Kp;
     const float Ki;
     const float Kd;
-    const float Kb;
     const float out_min;
     const float out_max;
-    const bool back_calculation;
-    const uint32_t sample_time;
 } PID_Config;
 
 void app_pid_init(PID *pid, const PID_Config *conf);
