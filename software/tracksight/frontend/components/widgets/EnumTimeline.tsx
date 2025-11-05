e066aa20b28d01227b0d4b2e3d4d25e3228f310f
Eroxl <InfinityWaffles@outlook.com>
2025-11-04 19:18:51 -0800

    add basic error state

--- software/tracksight/frontend/components/widgets/EnumTimeline.tsx
+++ software/tracksight/frontend/components/widgets/EnumTimeline.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import LabelLegend from "@/components/legends/LabelLegend";
-import { IS_VERBOSE_DEBUG } from "@/lib/constants";
 import { useSignalMetadata } from "@/lib/contexts/SignalsMetadataContext";
-import { useDataVersion, useSignalDataRef } from "@/lib/signalData";
 import { WidgetRenderer } from "@/lib/types/Widget";
-import { useEffect } from "react";
 import EnumSignalSelector from "@/components/widgets/EnumSignalSelector";
 import { useDashboardLayout } from "@/lib/contexts/DashboardLayout";
 
@@ -16,24 +13,19 @@ const EnumTimeline: WidgetRenderer<"enumTimeline"> = (props) => {
 
   const { editWidget } = useDashboardLayout();
 
-  const signalsDataRef = useSignalDataRef(signals);
-  const dataVersion = useDataVersion(signals);
-
   const signalMetadata = useSignalMetadata(signals[0]);
 
   const enumOptions = Object.values(signalMetadata?.enum?.items || {});
 
-  useEffect(() => {
-    if (!IS_VERBOSE_DEBUG) return;
-
-    console.groupCollapsed("%c[StateTimeline]: Receieved new Data", "color: #81a1c1;");
-    console.log("%cSignals: %o", "color: #d08770;", signals);
-    console.log("%cData Version: %d", "color: #d08770;", dataVersion);
-    console.log("%cSignal Data Ref: %o", "color: #d08770;", signalsDataRef);
-    console.groupEnd();
-  }, [dataVersion]);
-
-  if (!signalMetadata) return null;
+  if (!signalMetadata) {
+    return (
+      <div className="flex flex-col items-center">
+        <p className="p-4 text-center text-sm">
+          Signal metadata not found.
+        </p>
+      </div>
+    )
+  }
 
   // TODO(evan): Implement this when the renderer is done
   return (
