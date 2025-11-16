96e31eac2e9d1faea093e67b56bedf8b8532c10b
Kelm <lkevin2003@gmail.com>
2025-11-15 16:20:12 -0800

    fixed shutdownnode syntaxes I think

--- firmware/hexray/BMS/src/app/app_shdnLoop.cpp
+++ firmware/hexray/BMS/src/app/app_shdnLoop.cpp
@@ -11,5 +11,5 @@ constexpr node hv_n_ok_node(hv_n_intlck_sns_pin, app_canTx_BMS_HVNShdnOKStatus_s
 
 namespace app::shdn
 {
-constexpr shdnLoop<2> bms_shdnLoop{ std::array<const io::shdn::node *, 2>{ &hv_p_ok_node, &hv_n_ok_node } };
+const shdnLoop<2> bms_shdnLoop{ std::array<const io::shdn::node *const, 2>{ &hv_p_ok_node, &hv_n_ok_node } };
 }
