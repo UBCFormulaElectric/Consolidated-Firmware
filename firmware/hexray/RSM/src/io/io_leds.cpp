bc946e62e78f24a77ecdcc67c63e29f3f4e49c42
Shun Akiyama <shunakiyama@dhcp-206-12-183-75.ubcsecure.wireless.ubc.ca>
2025-11-18 21:56:30 -0800

    sdf

--- firmware/hexray/RSM/src/io/io_leds.cpp
+++ firmware/hexray/RSM/src/io/io_leds.cpp
@@ -1,4 +1,4 @@
 #include "io_leds.hpp"
-// #include "hw_gpios.hpp" 
+// #include "hw_gpios.hpp"
 
 // const Binary brake_light = { .gpio = &brake_light_en_pin } const Binary led = { .gpio = &led_pin }
