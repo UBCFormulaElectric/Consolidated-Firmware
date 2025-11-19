c241a6bb3151f4d6f1b29b052879fd1e344a311a
Shun Akiyama <shunakiyama@dhcp-206-12-183-75.ubcsecure.wireless.ubc.ca>
2025-11-18 21:55:20 -0800

    fixing

--- firmware/hexray/RSM/src/io/io_leds.cpp
+++ firmware/hexray/RSM/src/io/io_leds.cpp
@@ -1,6 +1,4 @@
 #include "io_leds.hpp"
-#include "hw_gpios.hpp" //Not yet created
+// #include "hw_gpios.hpp" 
 
-const Binary brake_light = { .gpio = &brake_light_en_pin } const Binary led = { .gpio = &led_pin }
-
-// Not sure why error
+// const Binary brake_light = { .gpio = &brake_light_en_pin } const Binary led = { .gpio = &led_pin }
