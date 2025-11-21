056ae169846b8f11ce630708e3c5f61c34ed14fa
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2025-11-20 17:23:03 -0800

    some tests

--- firmware/hexray/BMS/test/test_stateMachine.cpp
+++ firmware/hexray/BMS/test/test_stateMachine.cpp
@@ -16,10 +16,9 @@ class BmsStateMachineTest : public BMSBaseTest
 {
 };
 
-// Precharge tests
+// Precharge drive tests
 
-TEST_F(BmsStateMachineTest, enter_precharge_drive_when_vc_bms_on_and_negative_closed)
-{
+TEST_F(BmsStateMachineTest, enter_precharge_drive) {
     ASSERT_STATE_EQ(init_state);
 
     fakes::irs::setNegativeState(CONTACTOR_STATE_CLOSED);
@@ -29,12 +28,13 @@ TEST_F(BmsStateMachineTest, enter_precharge_drive_when_vc_bms_on_and_negative_cl
     LetTimePass(20);
 
     ASSERT_STATE_EQ(precharge_drive_state);
+    ASSERT_EQ(io_irs_prechargeState(), CONTACTOR_STATE_CLOSED);
+    ASSERT_EQ(io_irs_positiveState(), CONTACTOR_STATE_OPEN);
+    ASSERT_EQ(io_irs_negativeState(), CONTACTOR_STATE_CLOSED);
 }
 
 TEST_F(BmsStateMachineTest, no_precharge_drive_if_negative_open)
 {
-    ASSERT_STATE_EQ(init_state);
-
     app_canRx_VC_State_update(VC_BMS_ON_STATE);
     fakes::irs::setNegativeState(CONTACTOR_STATE_OPEN);
     LetTimePass(20);
@@ -44,8 +44,6 @@ TEST_F(BmsStateMachineTest, no_precharge_drive_if_negative_open)
 
 TEST_F(BmsStateMachineTest, no_precharge_drive_if_vc_state_not_bms_on)
 {
-    ASSERT_STATE_EQ(init_state);
-
     app_canRx_VC_State_update(VC_INIT_STATE);
     fakes::irs::setNegativeState(CONTACTOR_STATE_CLOSED);
     LetTimePass(20);
@@ -55,8 +53,6 @@ TEST_F(BmsStateMachineTest, no_precharge_drive_if_vc_state_not_bms_on)
 
 TEST_F(BmsStateMachineTest, no_precharge_drive_if_charging_requested)
 {
-    ASSERT_STATE_EQ(init_state);
-
     app_canRx_VC_State_update(VC_BMS_ON_STATE);
     fakes::irs::setNegativeState(CONTACTOR_STATE_CLOSED);
     app_canRx_Debug_StartCharging_update(true);
@@ -64,3 +60,9 @@ TEST_F(BmsStateMachineTest, no_precharge_drive_if_charging_requested)
 
     ASSERT_STATE_EQ(init_state);
 }
+
+static constexpr int precharge_too_fast_time = 20; // ms
+static constexpr int precharge_just_good_time = 1220; // ms
+static constexpr int precharge_too_slow_time = 7290; // ms
+
+static constexpr int precharge_cooldown_time = 1000; // ms
