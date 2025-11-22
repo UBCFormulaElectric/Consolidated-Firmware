86679ca8ef75b438033ba4d2d9e0ee8326f031ea
Setare <setimalek@gmail.com>
2025-11-21 18:44:23 -0800

    making stack words part of class init

--- firmware/shared/src/hw/hw_rtosTaskHandler.hpp
+++ firmware/shared/src/hw/hw_rtosTaskHandler.hpp
@@ -7,23 +7,24 @@
 #include "timers.h"
 namespace hw::rtos
 {
+template <size_t StackWords>
 class StaticTask
 {
   public:
     constexpr explicit StaticTask(
-        uint32_t      *stack_mem,
-        size_t         stack_words,
         osPriority_t   priority,
         const char    *name,
         osThreadFunc_t func)
-      : size_(stack_mem), stack_(stack_words), prio_(priority), name_(name), fn_(func)
+      // : size_(stack_mem), stack_(stack_words), prio_(priority), name_(name), fn_(func)
+      : prio_(priority), name_(name), fn_(func)
+
     {
         // Runtime osThreadAttr_t assignments
         attr_.name       = name_;
         attr_.cb_mem     = &cb_;
         attr_.cb_size    = sizeof(cb_);
-        attr_.stack_mem  = size_;
-        attr_.stack_size = stack_ * sizeof(uint32_t);
+        attr_.stack_mem  = stack_;
+        attr_.stack_size = StackWords * sizeof(uint32_t);;
         attr_.priority   = prio_;
     }
 
@@ -35,13 +36,15 @@ class StaticTask
     osThreadId_t id() const { return id_; }
 
   private:
-    uint32_t *const    size_;
-    const size_t       stack_;
+    // uint32_t *const    size_;
+    // const size_t       stack_;
     const osPriority_t prio_;
     const char        *name_;
     osThreadFunc_t     fn_;
     osThreadId_t       id_{};
     StaticTask_t       cb_{};
     osThreadAttr_t     attr_{};
+    alignas(8) uint32_t stack_[StackWords];
+
 };
 } // namespace  hw::rtos
