f5e2afc2cf996e42f8ab8b5878322fcdaa1c6137
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2025-11-29 14:08:18 -0800

    stuff

--- firmware/hexray/BMS/src/io/io_imd.cpp
+++ firmware/hexray/BMS/src/io/io_imd.cpp
@@ -2,12 +2,9 @@
 #include "io_time.hpp"
 #include "hw_pwms.hpp"
 
-constexpr uint8_t PWM_TICKS_MAX = 255;
-
-static uint8_t pwm_counter = 0;
-
 namespace io::imd
 {
+<<<<<<< Updated upstream
 uint8_t pwmCounterTick()
 {
     if (pwm_counter != PWM_TICKS_MAX)
@@ -21,6 +18,14 @@ uint8_t pwmCounterTick()
 float getFrequency()
 {
     return hw::PwmInput::getFrequency(&hw::pwms::imd_pwm_input);
+=======
+float getFrequency() {
+    if (!hw::pwms::imd_pwm_input.pwm_isActive()) {
+        return 0.0f;
+    }
+    
+    return hw::pwms::imd_pwm_input.get_frequency();
+>>>>>>> Stashed changes
 }
 
 float getDutyCycle()
@@ -28,11 +33,16 @@ float getDutyCycle()
     return hw::PwmInput::getDutyCycle(&hw::pwms::imd_pwm_input);
 }
 
+<<<<<<< Updated upstream
 uint32_t getTimeSincePowerOn()
 {
     // The IMD shares the same power rail as the BMS, so we assume that the IMD
     // and BMS boot up at the same time. In other words, the IMD has been ON
     // for just as long as the BMS.
+=======
+uint32_t getTimeSincePowerOn() {
+    // IMD and BMS share the same power rail, so uptime = BMS uptime
+>>>>>>> Stashed changes
     return io::time::getCurrentMs();
 }
 } // namespace io::imd
