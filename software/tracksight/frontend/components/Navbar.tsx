50437ebcf06ca46156202c4ba74211a67e2bbca5
Edwin <20777515+Lucien950@users.noreply.github.com>
2025-11-28 20:38:49 -0800

    cleanup navbar

--- software/tracksight/frontend/components/Navbar.tsx
+++ software/tracksight/frontend/components/Navbar.tsx
@@ -2,10 +2,10 @@ import { Calendar } from "lucide-react";
 
 function Navbar() {
   return (
-    <nav className="border-primary-300 bg-primary-100 fixed top-0 left-0 z-50 flex h-min w-screen flex-col">
-      <div className="flex flex-row items-center justify-between border-b-2 border-inherit px-10 py-5 select-none">
+    <nav className="fixed top-0 left-0 z-50 h-min w-screen">
+      <div className="flex flex-row items-center justify-between select-none px-8 py-4">
         {/** TODO(evan): Make this dynamic and a selector **/}
-        <div className="hover:text-primary-900 flex items-center gap-4 rounded-md px-2 py-1 transition-colors hover:cursor-pointer">
+        <div className="hover:bg-gray-300 flex items-center gap-4 rounded-lg px-4 py-4 transition-colors hover:cursor-pointer">
           <Calendar className="stroke-current" size={24} />
           <div className="flex flex-col items-start">
             <span className="text font-semibold">September 28th 2025</span>
@@ -15,7 +15,6 @@ function Navbar() {
 
         {/** TODO(evan): Add pause / play button here */}
       </div>
-
       {/** TODO(evan): Add the universal timeline here */}
     </nav>
   );
