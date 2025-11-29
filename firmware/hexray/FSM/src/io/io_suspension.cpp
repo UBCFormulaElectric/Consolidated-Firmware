99f3483fad5c07bb5145fb2f6a7ac08b56daaf37
acp02 <aujames430@gmail.com>
2025-11-29 15:06:12 -0800

    Tried fixing 22

--- firmware/hexray/FSM/src/io/io_suspension.cpp
+++ firmware/hexray/FSM/src/io/io_suspension.cpp
@@ -1,28 +1,28 @@
-#include "io_suspension.hpp"
-#include "hw_adcs.hpp"
-#include "hw_gpios.hpp"
+// #include "io_suspension.hpp"
+// #include "hw_adcs.hpp"
+// #include "hw_gpios.hpp"
 
-namespace io::suspension
-{
-float getLeftTravel(void)
-{
-    // Return left suspension travel based on the ADC voltage.
-    return hw_adc_getVoltage(&susp_fl);
-}
+// namespace io::suspension
+// {
+// float getLeftTravel(void)
+// {
+//     // Return left suspension travel based on the ADC voltage.
+//     return hw_adc_getVoltage(&susp_fl);
+// }
 
-float getRightTravel(void)
-{
-    // Return right suspension travel based on the ADC voltage.
-    return hw_adc_getVoltage(&susp_fr);
-}
+// float getRightTravel(void)
+// {
+//     // Return right suspension travel based on the ADC voltage.
+//     return hw_adc_getVoltage(&susp_fr);
+// }
 
-bool leftSensorOCSC(void)
-{
-    return !hw_gpio_readPin(&nsusp_fl_ocsc);
-}
+// bool leftSensorOCSC(void)
+// {
+//     return !hw_gpio_readPin(&nsusp_fl_ocsc);
+// }
 
-bool rightSensorOCSC(void)
-{
-    return !hw_gpio_readPin(&nsusp_fr_ocsc);
-}
-} // namespace io::suspension
+// bool rightSensorOCSC(void)
+// {
+//     return !hw_gpio_readPin(&nsusp_fr_ocsc);
+// }
+// } // namespace io::suspension
