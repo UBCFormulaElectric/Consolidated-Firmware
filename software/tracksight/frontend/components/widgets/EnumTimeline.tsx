1ecf1e09c0b8dae23b016e02dd14d42e8163d829
Eroxl <InfinityWaffles@outlook.com>
2025-12-05 16:57:41 -0800

    fixs

--- software/tracksight/frontend/components/widgets/EnumTimeline.tsx
+++ software/tracksight/frontend/components/widgets/EnumTimeline.tsx
@@ -5,6 +5,9 @@ import { WidgetData } from "@/lib/types/Widget";
 import EnumSignalSelector from "@/components/widgets/EnumSignalSelector";
 import { useDashboardLayout } from "@/lib/contexts/DashboardLayout";
 import useSignalMetadata from "@/lib/hooks/useSignalMetadata";
+import { useSignalDataStore } from "@/lib/contexts/SignalDataStore";
+import { useEffect, useState } from "react";
+import { isEnumSignalMetadata } from "@/lib/types/Signal";
 
 function EnumTimeline(props: WidgetData<"enumTimeline">) {
   const { signals, options } = props;
@@ -12,6 +15,15 @@ function EnumTimeline(props: WidgetData<"enumTimeline">) {
   const { colorPalette } = options;
 
   const { editWidget } = useDashboardLayout();
+
+  const [dataVersion, setDataVersion] = useState(0);
+  const data = useSignalDataStore(signals[0]);
+
+  useEffect(() => {
+    setInterval(() => {
+      setDataVersion((prev) => prev + 1);
+    }, 100);
+  }, []);
 
   const signalMetadata = useSignalMetadata(signals[0] ?? "");
 
@@ -46,9 +58,19 @@ function EnumTimeline(props: WidgetData<"enumTimeline">) {
     )
   }
 
-  const enumOptions = Object.values(signalMetadata.data.enum?.items || {});
+  if (!isEnumSignalMetadata(signalMetadata.data)) {
+    return (
+      <div className="flex flex-col items-center">
+        Invalid signal type.
+        <p className="p-4 text-center text-sm">
+          The signal "{signals[0]}" is not an enum signal. Please select a valid enum signal.
+        </p>
+      </div>
+    );
+  }
 
-  // TODO(evan): Implement this when the renderer is done
+  const enumOptions = Object.values(signalMetadata.data.enum.items);
+
   return (
     <div className="flex flex-col gap-5">
       <div className="px-8">
@@ -63,14 +85,45 @@ function EnumTimeline(props: WidgetData<"enumTimeline">) {
       </div>
 
       <div className="relative flex h-6 w-full">
-        {signals.map((_, i) => (
-          <div
-            className="w-full"
-            style={{
-              backgroundColor: colorPalette[i],
-            }}
-          />
-        ))}
+        {
+          // NOTE(evan): This is just here for my personal testing ignore how horrible
+          Array.from({ length: data.data.values.size() }, (_, i) => {
+            const previousPoint = data.data.values.get(i - 1);
+            const lengthPerElement = 100 / Math.max(data.data.values.size(), 1);
+            let widthPercent = lengthPerElement;
+
+            if (previousPoint === undefined || previousPoint != data.data.values.get(i)) {
+              let lengthOfSegment = 1;
+
+              while (data.data.values.get(i + lengthOfSegment) === data.data.values.get(i)) {
+                lengthOfSegment += 1;
+              }
+
+              widthPercent = lengthOfSegment * lengthPerElement;
+            } else {
+              return null;
+            }
+
+            const value = data.data.values.get(i);
+
+            if (value === undefined) return null;
+
+            const enumOption = enumOptions[value];
+            const color = colorPalette[value % colorPalette.length];
+
+            return (
+              <div
+                key={i}
+                className="h-full"
+                style={{
+                  width: `${widthPercent}%`,
+                  backgroundColor: color,
+                  display: "inline-block",
+                }}
+              />
+            );
+          })
+        }
       </div>
 
       <LabelLegend signals={enumOptions} colorPalette={colorPalette} />
