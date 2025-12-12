6a2dfd73c9af50056904f07fbaba9b5953f408bc
Setare <setimalek@gmail.com>
2025-12-11 23:32:09 -0800

    applying comments

--- firmware/quintuna/VC/src/app/states/app_InverterfaultHandlingState.c
+++ firmware/quintuna/VC/src/app/states/app_InverterfaultHandlingState.c
@@ -8,6 +8,7 @@
 #include "app_canAlerts.h"
 #include "io_log.h"
 
+
 // Retry window time
 #define TIMEOUT 1000u
 
@@ -67,7 +68,8 @@ static void InverterFaultHandlingStateRunOnTick100Hz(void)
     {
         case INV_FAULT_RETRY:
         {
-            // Lockout check, if ANY lockout, stop retrying and remain faulted
+            // Lockout check, if ANY lockout, stop retrying and remain faulted.
+            // This is the most safety critical check so it has highest priority
             const bool any_lockout = is_lockout_code(inverter_handle_FL.can_error_info()) ||
                                      is_lockout_code(inverter_handle_FR.can_error_info()) ||
                                      is_lockout_code(inverter_handle_RL.can_error_info()) ||
@@ -87,6 +89,7 @@ static void InverterFaultHandlingStateRunOnTick100Hz(void)
             }
 
             // If all inverters are clear, we’re done
+            // Second priority because we want to recover as quickly as possible
             const bool fl_fault = inverter_handle_FL.can_error_bit();
             const bool fr_fault = inverter_handle_FR.can_error_bit();
             const bool rl_fault = inverter_handle_RL.can_error_bit();
@@ -99,7 +102,7 @@ static void InverterFaultHandlingStateRunOnTick100Hz(void)
                 break;
             }
 
-            // Start a new retry cycle is fault persist after 1000 ms of retrying
+            // Now start a new retry cycle is fault persist after TIMEOUT = 1000 ms of retrying
             if (!retry_cycle_active)
             {
                 if (fl_fault)
@@ -110,40 +113,21 @@ static void InverterFaultHandlingStateRunOnTick100Hz(void)
                     inverter_start_retry_routine(inverter_handle_RL);
                 if (rr_fault)
                     inverter_start_retry_routine(inverter_handle_RR);
-
                 app_timer_restart(&retry_timer);
                 retry_cycle_active = true;
                 break;
             }
 
-            // timing of a cycle
+            // Check to see if the timer has expired yet
             const TimerState timer_state = app_timer_updateAndGetState(&retry_timer);
-
             if (retry_cycle_active && timer_state == TIMER_STATE_EXPIRED)
             {
-                // First end the retry cycle
+                // First end the retry cycle so we can reset it again
                 end_retry_cycle(inverter_handle_FL);
                 end_retry_cycle(inverter_handle_FR);
                 end_retry_cycle(inverter_handle_RL);
                 end_retry_cycle(inverter_handle_RR);
-
-                // Check the error bits
-                const bool fl_now = inverter_handle_FL.can_error_bit();
-                const bool fr_now = inverter_handle_FR.can_error_bit();
-                const bool rl_now = inverter_handle_RL.can_error_bit();
-                const bool rr_now = inverter_handle_RR.can_error_bit();
-
-                // If there are faults wait to
-                if (!fl_now && !fr_now && !rl_now && !rr_now)
-                {
-                    LOG_INFO("retry -> recovered after pulse window");
-                    current_inverter_fault_state = INV_FAULT_RECOVERED;
-                }
-                else
-                {
-                    // Some inverter still faulted keep retrying in next cycle
-                    retry_cycle_active = false;
-                }
+                retry_cycle_active = false;
             }
             break;
         }
@@ -152,7 +136,9 @@ static void InverterFaultHandlingStateRunOnTick100Hz(void)
         {
             // No retries needs hw reset
             app_canAlerts_VC_Info_InverterRetry_set(false);
-            // Ensuring we are in a hard fault state so the BMS contactors open
+
+            // Ensuring we are in a hard fault state and open BMS contactors
+            //app_canTx_VC_State_set(VC_INV_LOCKOUT_STATE);
             app_canTx_VC_State_set(VC_FAULT_STATE);
             return;
         }
