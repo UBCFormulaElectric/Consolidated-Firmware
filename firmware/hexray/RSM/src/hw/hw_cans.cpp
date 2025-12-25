506e6b2ca0ee277c08470b302a8ab618dbd86616
Shun <shunaki0214@gmail.com>
2025-12-24 20:19:35 -0800

    hw_cans complete

--- firmware/hexray/RSM/src/hw/hw_cans.cpp
+++ firmware/hexray/RSM/src/hw/hw_cans.cpp
@@ -2,8 +2,18 @@
 #include "hw_can.hpp"
 #include "main.h"
 #include "tasks.h"
-
+#include <cassert>
+//main.h not linking check cmake?
+//is bus number arbitrary?
 namespace hw
 {
-    can(&hcan1, 0,  ...)
+    //no tasks_runCanRxCallback yet in tasks.c (need bootloader stuff)
+    can fdcan1(hfdcan1, 0, tasks_runCanRxCallback);
+    //should I change type to FDCAN_HandleTypeDef in hw_can.hpp?
+    const can &hw_can_getHandle(const FDCAN_HandleTypeDef *hcan)
+    {
+        assert(hcan == fdcan1.getHcan());     
+        return fdcan1;
+    }
+    
 }
---
+++ firmware/hexray/RSM/src/hw/hw_cans.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include "hw_can.hpp"
+
+namespace hw
+{
+    extern can fdcan1; 
+    // do I need to include the function or no?
+    // const can &hw_can_getHandle(const FDCAN_HandleTypeDef *hcan);
+}
