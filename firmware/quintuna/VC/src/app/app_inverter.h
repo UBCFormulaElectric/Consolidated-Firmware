bb77bcfd61992e1884f658c39858372c9f194cc2
Setare Maleki <72472685+setaremalekiii@users.noreply.github.com>
2026-01-04 12:46:05 -0800

    inverter retry logic for fault handling  (#1716)
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    ### Resolved Tickets
    <!-- Link any tickets that this PR resolves. -->
    [FIRM-449](https://ubcformulaelectric.atlassian.net/browse/FIRM-449)
    
    [FIRM-449]:
    https://ubcformulaelectric.atlassian.net/browse/FIRM-449?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

---
+++ firmware/quintuna/VC/src/app/app_inverter.h
@@ -0,0 +1,45 @@
+#pragma once
+#include "stdbool.h"
+#include <stdint.h>
+#include "app_stateMachine.h"
+
+extern const State *state_to_recover_after_fault;
+typedef enum
+{
+    CAN_ISSUES = 3587,
+    DC_BUS_ISSUES,
+} Inverter_Fault_Info;
+typedef enum
+{
+    INVERTER_FL,
+    INVERTER_FR,
+    INVERTER_RL,
+    INVERTER_RR,
+    NUM_INVERTERS
+} InverterConfig;
+
+typedef enum
+{
+    INV_FAULT_RETRY,
+    INV_FAULT_LOCKOUT,
+    INV_FAULT_RECOVERED,
+} VCInverterFaults;
+typedef struct
+{
+    void (*can_enable_inv)(bool);
+    void (*can_invOn)(bool);
+    void (*can_dcOn)(bool);
+    uint32_t (*can_error_info)(void);
+    void (*error_reset)(bool);
+    bool (*can_error_bit)(void);
+    void (*can_inv_warning)(bool);
+} InverterHandle;
+
+extern const InverterHandle inverter_handle_FL;
+extern const InverterHandle inverter_handle_FR;
+extern const InverterHandle inverter_handle_RL;
+extern const InverterHandle inverter_handle_RR;
+
+// inverters
+bool app_inverter_inverterStatus(void);
+void app_stateMachine_inverterFaultHandling(void);
--- firmware/quintuna/VC/src/app/app_warningHandling.c
+++ firmware/quintuna/VC/src/app/app_warningHandling.c
@@ -1,81 +1,20 @@
 #include "app_warningHandling.h"
-
 #include "app_signal.h"
-
 #include "app_canAlerts.h"
 #include "app_canRx.h"
 #include "app_canTx.h"
-
-#include <stdbool.h>
-#include <stdint.h>
-
 #define APPS_BRAKE_DISAGREEMENT_TIME_TO_FAULT (10u)
 #define APPS_BRAKE_DISAGREEMENT_TIME_TO_CLEAR (10u)
 
 static Signal apps_brake_disagreement_signal;
-typedef enum
-{
-    INVERTER_FL,
-    INVERTER_FR,
-    INVERTER_RL,
-    INVERTER_RR,
-    NUM_INVERTERS
-} InverterConfig;
 
-bool app_warningHandling_boardWarningCheck(void)
+bool app_warningHandlin_boardWarningCheck(void)
 {
     return app_canAlerts_AnyBoardHasWarning();
 }
 
-typedef struct
-{
-    void (*can_enable_inv)(bool);
-    void (*can_invOn)(bool);
-    void (*can_dcOn)(bool);
-    uint32_t (*can_error_info)(void);
-    void (*error_reset)(bool);
-} InverterWarningHandling;
-
-static InverterWarningHandling inverter_reset_handle[NUM_INVERTERS] = {
-    [INVERTER_FL] = { .can_enable_inv = app_canTx_VC_INVFLbEnable_set,
-                      .can_invOn      = app_canTx_VC_INVFLbInverterOn_set,
-                      .can_dcOn       = app_canTx_VC_INVFLbDcOn_set,
-                      .can_error_info = app_canRx_INVFL_ErrorInfo_get,
-                      .error_reset    = app_canTx_VC_INVFLbErrorReset_set },
-    [INVERTER_FR] = { .can_enable_inv = app_canTx_VC_INVFRbEnable_set,
-                      .can_invOn      = app_canTx_VC_INVFRbInverterOn_set,
-                      .can_dcOn       = app_canTx_VC_INVFRbDcOn_set,
-                      .can_error_info = app_canRx_INVFR_ErrorInfo_get,
-                      .error_reset    = app_canTx_VC_INVFRbErrorReset_set },
-    [INVERTER_RL] = { .can_enable_inv = app_canTx_VC_INVRLbEnable_set,
-                      .can_invOn      = app_canTx_VC_INVRLbInverterOn_set,
-                      .can_dcOn       = app_canTx_VC_INVRLbDcOn_set,
-                      .can_error_info = app_canRx_INVRL_ErrorInfo_get,
-                      .error_reset    = app_canTx_VC_INVRLbErrorReset_set },
-    [INVERTER_RR] = { .can_enable_inv = app_canTx_VC_INVRRbEnable_set,
-                      .can_invOn      = app_canTx_VC_INVRRbInverterOn_set,
-                      .can_dcOn       = app_canTx_VC_INVRRbDcOn_set,
-                      .can_error_info = app_canRx_INVRR_ErrorInfo_get,
-                      .error_reset    = app_canTx_VC_INVRRbErrorReset_set },
-};
-
-bool app_warningHandling_inverterStatus(void)
-{
-    const bool invrr_error = app_canRx_INVRR_bError_get() == true;
-    const bool invrl_error = app_canRx_INVRL_bError_get() == true;
-    const bool invfl_error = app_canRx_INVFL_bError_get() == true;
-    const bool invfr_error = app_canRx_INVFR_bError_get() == true;
-
-    app_canAlerts_VC_Warning_RearLeftInverterFault_set(invrl_error);
-    app_canAlerts_VC_Warning_FrontRightInverterFault_set(invrr_error);
-    app_canAlerts_VC_Warning_FrontLeftInverterFault_set(invfl_error);
-    app_canAlerts_VC_Warning_RearRightInverterFault_set(invfr_error);
-
-    return invfl_error || invrl_error || invrr_error || invfr_error;
-}
-
 // TODO: integrate with warnings (will have to make compatible with regen)
-bool app_warningHandling_checkSoftwareBspd(float papps_pedal_percentage)
+bool app_warningHandlin_checkSoftwareBspd(float papps_pedal_percentage)
 {
     // Accelerator Brake Plausibility (bad user input safety issues)
     // Protect against brake/apps active at same time
@@ -95,16 +34,6 @@ bool app_warningHandling_checkSoftwareBspd(float papps_pedal_percentage)
     return apps_brake_disagreement_active;
 }
 
-void app_warningHandling_inverterReset(void)
-{
-    for (uint8_t inverter = 0; inverter < NUM_INVERTERS; inverter++)
-    {
-        inverter_reset_handle[inverter].can_invOn(false);
-        inverter_reset_handle[inverter].can_dcOn(false);
-        inverter_reset_handle[inverter].can_enable_inv(false);
-        inverter_reset_handle[inverter].error_reset(true);
-    }
-}
 void app_softwareBspd_init(void)
 {
     app_signal_init(
