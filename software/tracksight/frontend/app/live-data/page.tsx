6daa1109eae03ab19fe7d23d8a0d1703cc80aa06
Eroxl <InfinityWaffles@outlook.com>
2026-01-10 12:01:18 -0800

    re-add alert panel

--- software/tracksight/frontend/app/live-data/page.tsx
+++ software/tracksight/frontend/app/live-data/page.tsx
@@ -1,12 +1,16 @@
+import AlertBar from "@/components/AlertBar";
 import LiveDataDashboard from "@/components/LiveDataDashboard";
 import { DashboardLayoutProvider } from "@/lib/contexts/DashboardLayout";
 
+
 export default function LiveDataPage() {
   return (
-    <div className="w-screen">
+    <div className="w-screen pb-20">
       <DashboardLayoutProvider>
         <LiveDataDashboard />
+        <AlertBar />
       </DashboardLayoutProvider>
     </div>
   );
 }
+
