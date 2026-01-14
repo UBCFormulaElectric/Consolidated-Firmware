b01da228af019b53030d51038f5e21cc818aa548
jackyao <jackyao2004@gmail.com>
2026-01-13 19:05:50 -0800

    update versions and stuff, wip

--- software/tracksight/rust_backend/src/tasks/can_data/live_data_handler.rs
+++ software/tracksight/rust_backend/src/tasks/can_data/live_data_handler.rs
@@ -3,13 +3,15 @@ use influxdb2::Client;
 use tokio::select;
 
 use crate::config::CONFIG;
-use crate::tasks::telem_message::CanMessage;
+use crate::tasks::telem_message::CanPayload;
+
+use jsoncan_rust::can_database::CanMessage;
 
 /**
  * After serial_handler parses the can messages,
  * this task will handle "forwarding" live data to clients via sockets
  */
-pub async fn run_live_data_handler(mut shutdown_signal: Receiver<()>, mut can_queue_receiver: Receiver<CanMessage>) {
+pub async fn run_live_data_handler(mut shutdown_signal: Receiver<()>, mut can_signals_rx: Receiver<()>) {
     println!("Live data task started.");
 
     loop {
@@ -18,9 +20,8 @@ pub async fn run_live_data_handler(mut shutdown_signal: Receiver<()>, mut can_qu
                 println!("Shutting down live data handler task.");
                 break;
             }
-            Ok(can) = can_queue_receiver.recv() => {
+            _ = can_signals_rx.recv() => {
                 // todo should also probably check for closed channels and close thread
-                let CanMessage { can_id, can_time_offset, can_payload } = can;
                 // todo handle
             }
         }
--- software/tracksight/rust_backend/src/tasks/can_data_handler.rs
+++ software/tracksight/rust_backend/src/tasks/can_data_handler.rs
@@ -4,6 +4,8 @@ use tokio::{select, spawn};
 
 use super::can_data::influx_handler::run_influx_handler;
 use super::can_data::live_data_handler::run_live_data_handler;
+use super::telem_message::CanPayload;
+use crate::config::CONFIG;
 
 use jsoncan_rust::parsing::JsonCanParser;
 use jsoncan_rust::can_database::CanDatabase;
@@ -13,22 +15,23 @@ use jsoncan_rust::can_database; // todo fix the naming conflict with canmessage 
  * Consumes from serial handler
  * Uses JsonCan config to parse CAN messages and broadcasts to other tasks
  */
-pub async fn run_can_data_handler(mut shutdown_rx: broadcast::Receiver<()>, mut can_queue_receiver: broadcast::Receiver<CanMessage>) {
+pub async fn run_can_data_handler(mut shutdown_rx: broadcast::Receiver<()>, mut can_queue_rx: broadcast::Receiver<CanPayload>) {
     println!("CAN data handler task started.");
 
-    let parser = JsonCanParser::new(CONFIG.jsoncan_config_path);
+    let parser = JsonCanParser::new(CONFIG.jsoncan_config_path.clone());
     let can_db = match CanDatabase::from(parser) {
         Ok(db) => db,
         Err(e) => {
-            eprintln!("Failed to load CAN database: {}", e);
+            eprintln!("Failed to load CAN database: {:?}", e);
             return;
         }
     };
 
-    let (parsed_can_tx, _) = broadcast::channel::<can_database::CanMessage>(32);
+    // let (parsed_can_tx, _) = broadcast::channel::<can_database::CanMessage>(32);
 
-    let influx_handler_task = spawn(run_influx_handler(shutdown_rx.resubscribe(), parsed_can_tx.subscribe()));
-    let live_data_handler_task = spawn(run_live_data_handler(shutdown_rx.resubscribe(), parsed_can_tx.subscribe()));
+    // parsed can signal consumers
+    // let influx_handler_task = spawn(run_influx_handler(shutdown_rx.resubscribe(), parsed_can_tx.subscribe()));
+    // let live_data_handler_task = spawn(run_live_data_handler(shutdown_rx.resubscribe(), parsed_can_tx.subscribe()));
     
     loop {
         select! {
@@ -36,13 +39,12 @@ pub async fn run_can_data_handler(mut shutdown_rx: broadcast::Receiver<()>, mut 
                 println!("Shutting down CAN data handler task.");
                 break;
             }
-            Ok(can) = can_queue_receiver.recv() => {
-                
+            Ok(can_payload) = can_queue_rx.recv() => {
             }
         }
     }
 
-    influx_handler_task.await;
-    live_data_handler_task.await;
+    // influx_handler_task.await;
+    // live_data_handler_task.await;
 }
 
