85ee70d62cd83d02ab95cbcc42201ba1b06ef54d
Setare <setimalek@gmail.com>
2026-01-13 20:45:51 -0800

    namepace

--- firmware/hexray/VC/src/app/app_faultHandling.hpp
+++ firmware/hexray/VC/src/app/app_faultHandling.hpp
@@ -2,4 +2,9 @@
 #include <iostream>
 
 // Fault handling is dedicated to faults that take the VC state machine out of drive state
+namespace vc::app::fault_handling{
+// Check if the air minus contactor is closed
 bool app_faultHandling_air_minus_closed(void);
+// Function ensuring the BMS hearbeat is valid for VC 
+bool app_faultHandling_fsm_bms_HeartbeartChecks(void);
+}
