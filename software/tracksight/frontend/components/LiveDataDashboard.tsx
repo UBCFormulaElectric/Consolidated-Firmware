b369133031d6099e60e4ebd1cc28d500ea60333d
Eroxl <InfinityWaffles@outlook.com>
2026-01-17 11:39:16 -0800

    undo

--- software/tracksight/frontend/components/LiveDataDashboard.tsx
+++ software/tracksight/frontend/components/LiveDataDashboard.tsx
@@ -4,28 +4,26 @@ import { useRef } from "react";
 
 import Widget from "@/components/widgets/Widget";
 import { useDashboardLayout } from "@/lib/contexts/DashboardLayout";
-import { LiveSignalDataStoreProvider } from "@/lib/contexts/LiveSignalDataStore";
 
+// TODO(evan): Rename to just DataDashboard since it can be used for both live and historical data
 function LiveDataDashboard() {
   const { widgets } = useDashboardLayout();
 
   const containerRef = useRef<HTMLDivElement>(null);
 
   return (
-    <LiveSignalDataStoreProvider>
-      <div className="w-full h-fit overflow-x-scroll scrollbar-hidden overscroll-x-none">
-        <div
-          className="flex h-full min-w-[200vw] flex-col gap-16"
-          ref={containerRef}
-        >
-          {widgets.map((widget, index) => (
-            <div className="sticky left-0 w-screen" style={{ zIndex: widgets.length - index }}>
-              <Widget {...widget} />
-            </div>
-          ))}
-        </div>
+    <div className="w-full h-fit overflow-x-scroll scrollbar-hidden overscroll-x-none">
+      <div
+        className="flex h-full min-w-[200vw] flex-col gap-16"
+        ref={containerRef}
+      >
+        {widgets.map((widget, index) => (
+          <div className="sticky left-0 w-screen" style={{ zIndex: widgets.length - index }}>
+            <Widget {...widget} />
+          </div>
+        ))}
       </div>
-    </LiveSignalDataStoreProvider>
+    </div>
   );
 }
 
