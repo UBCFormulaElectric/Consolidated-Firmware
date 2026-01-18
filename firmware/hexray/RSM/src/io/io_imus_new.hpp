358da733e5e0c6d59f7d33dc9973d889ab3f84f0
Shun <shunaki0214@gmail.com>
2026-01-17 16:43:45 -0800

    changed imu app and io

---
+++ firmware/hexray/RSM/src/io/io_imus_new.hpp
@@ -0,0 +1,17 @@
+#pragma once
+#include "util_errorCodes.hpp"
+
+namespace io::imus 
+{
+    extern io::imu::Imu imu_config();
+}
+
+typedef struct
+{
+    ExitCode accel_x_fault;
+    ExitCode accel_y_fault;
+    ExitCode accel_z_fault;
+    ExitCode gyro_x_fault;
+    ExitCode gyro_y_fault;
+    ExitCode gyro_z_fault;
+} RSMImuFaults;
