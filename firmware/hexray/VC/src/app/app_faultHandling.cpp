5f75f63c469e0be064726d471482727d205c4691
Setare Maleki <setimalek@gmail.com>
2026-01-18 12:39:21 -0800

    heart beat and fault handling

--- firmware/hexray/VC/src/app/app_faultHandling.cpp
+++ firmware/hexray/VC/src/app/app_faultHandling.cpp
@@ -7,19 +7,20 @@
 
 #define CONTACTOR_STATE_CLOSED 1
 
-namespace vc::app::fault_handling{
-    bool app_faultHandling_air_minus_closed(void)
-    {
-        // return (CONTACTOR_STATE_CLOSED == app_canRx_BMS_IrNegative_get());
-        //TODO: CanRx functions are not implemented build sys yet
-        return (CONTACTOR_STATE_CLOSED == 1);
-    }
+namespace vc::app::fault_handling
+{
+bool app_faultHandling_air_minus_closed(void)
+{
+    // return (CONTACTOR_STATE_CLOSED == app_canRx_BMS_IrNegative_get());
+    // TODO: CanRx functions are not implemented build sys yet
+    return (CONTACTOR_STATE_CLOSED == 1);
+}
 
-    bool app_faultHandling_fsm_bms_HeartbeartChecks(void)
-    {
-        //TODO: CanAlerts not implemented in cpp yet
-        //return app_canAlerts_BoardHasFault(VC_NODE);
-        return 1;
-    }
+bool app_faultHandling_fsm_bms_HeartbeartChecks(void)
+{
+    // TODO: CanAlerts not implemented in cpp yet
+    // return app_canAlerts_BoardHasFault(VC_NODE);
+    return 1;
+}
 
-}  //namespace vc::app::fault_handling
+} // namespace vc::app::fault_handling
