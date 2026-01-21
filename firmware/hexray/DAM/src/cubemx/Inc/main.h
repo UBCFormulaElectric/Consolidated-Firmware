cb57a3af24fe57fab3c2e7a31f2cefb5e4d41a1e
emilysxng <song.emily2004@gmail.com>
2026-01-20 20:15:17 -0800

    cmake

--- firmware/hexray/DAM/src/cubemx/Inc/main.h
+++ firmware/hexray/DAM/src/cubemx/Inc/main.h
@@ -52,7 +52,6 @@ extern "C"
 
     /* Exported functions prototypes ---------------------------------------------*/
     void Error_Handler(void);
-
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
