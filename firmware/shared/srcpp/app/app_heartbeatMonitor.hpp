919328e390c502b2faff7c01695268f1b42c4875
AmirTajaddodi <amir.tajadodi@gmail.com>
2026-01-25 11:08:55 -0800

    heartbeat monitoring ci/cd fix

--- firmware/shared/srcpp/app/app_heartbeatMonitor.hpp
+++ firmware/shared/srcpp/app/app_heartbeatMonitor.hpp
@@ -4,6 +4,7 @@
 #include "app_timer.hpp"
 #include <cassert>
 #include <array>
+#include <cstddef>
 
 namespace app::heartbeat
 {
@@ -30,7 +31,7 @@ template <size_t NODE_COUNT> class monitor
       : heartbeat_nodes(std::move(in_heartbeat_nodes)), checkin_self(in_checkin_self)
     {
         init();
-    };
+    }
     explicit monitor(
         void (*const in_checkin_self)(bool),
         const std::array<io::heartbeat::node *const, NODE_COUNT> in_heartbeat_nodes,
@@ -38,7 +39,7 @@ template <size_t NODE_COUNT> class monitor
       : block_faults(in_block_faults), heartbeat_nodes(std::move(in_heartbeat_nodes)), checkin_self(in_checkin_self)
     {
         init();
-    };
+    }
 
     /**
      * Populates heartbeats_checked_in
