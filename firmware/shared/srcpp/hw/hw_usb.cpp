80e1492432bec610c78654e38fb0f86210c407c1
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-01-28 01:25:43 -0800

    more?

--- firmware/shared/srcpp/hw/hw_usb.cpp
+++ firmware/shared/srcpp/hw/hw_usb.cpp
@@ -16,21 +16,24 @@
 // and the TRANSMIT macro (CDC_Transmit_FS or CDC_Transmit_HS).
 // Everything is derived from usbd_cdc_if.h.
 #if defined(STM32H562xx)
+#define FS
+#elif defined(STM32H733xx)
+#define HS
+#else
+#error Either STM32H733xx or STM32F412Rx must be defined, \
+        so that hw_usb knows which handlers to use.
+#endif
+
+#ifdef FS
 #define TRANSMIT(buf, len) (CDC_Transmit_FS(buf, static_cast<uint16_t>(len)))
 extern USBD_HandleTypeDef hUsbDeviceFS;
 #define USB_DEVICE_HANDLER (hUsbDeviceFS)
 #define DEVICE_FS 0
-extern USBD_DescriptorsTypeDef CDC_Desc;
-#elif defined(STM32H733xx)
+#elif HS
 #define TRANSMIT(buf, len) (CDC_Transmit_HS(buf, len))
-extern USBD_HandleTypeDef      hUsbDeviceHS;
+extern USBD_HandleTypeDef hUsbDeviceHS;
 #define USB_DEVICE_HANDLER (hUsbDeviceHS)
-extern USBD_DescriptorsTypeDef HS_Desc;
-extern USBD_DescriptorsTypeDef CDC_Desc;
 #define DEVICE_HS 1
-#else
-#error Either STM32H733xx or STM32F412Rx must be defined, \
-        so that hw_usb knows which handlers to use.
 #endif
 
 static constexpr size_t                      APP_RX_DATA_SIZE = 2048;
@@ -43,8 +46,8 @@ static std::array<uint8_t, APP_TX_DATA_SIZE> UserTxBufferFS;
  */
 static int8_t CDC_Init_FS()
 {
-    USBD_CDC_SetTxBuffer(&hUsbDeviceFS, UserTxBufferFS.data(), 0);
-    USBD_CDC_SetRxBuffer(&hUsbDeviceFS, UserRxBufferFS.data());
+    USBD_CDC_SetTxBuffer(&USB_DEVICE_HANDLER, UserTxBufferFS.data(), 0);
+    USBD_CDC_SetRxBuffer(&USB_DEVICE_HANDLER, UserRxBufferFS.data());
     return USBD_OK;
 }
 /**
@@ -54,7 +57,9 @@ static int8_t CDC_Init_FS()
  * @param  length: Number of data to be sent (in bytes)
  * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL
  */
-static int8_t CDC_Control_FS(const uint8_t cmd, uint8_t *const pbuf, const uint16_t length)
+// ReSharper disable once CppParameterMayBeConstPtrOrRef
+static int8_t
+    CDC_Control_FS(const uint8_t cmd, uint8_t *const pbuf, const uint16_t length) // NOLINT(*-non-const-parameter)
 {
     UNUSED(pbuf);
     UNUSED(length);
@@ -106,11 +111,11 @@ static int8_t CDC_Control_FS(const uint8_t cmd, uint8_t *const pbuf, const uint1
  * @param  Len: Number of data received (in bytes)
  * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAILL
  */
-static int8_t CDC_Receive_FS(uint8_t *Buf, uint32_t *Len)
+static int8_t CDC_Receive_FS(uint8_t *Buf, uint32_t *Len) // NOLINT(*-non-const-parameter)
 {
     hw::usb::receive({ Buf, *Len });
-    USBD_CDC_SetRxBuffer(&hUsbDeviceFS, &Buf[0]);
-    USBD_CDC_ReceivePacket(&hUsbDeviceFS);
+    USBD_CDC_SetRxBuffer(&USB_DEVICE_HANDLER, &Buf[0]);
+    USBD_CDC_ReceivePacket(&USB_DEVICE_HANDLER);
     return USBD_OK;
 }
 /**
@@ -122,10 +127,10 @@ static int8_t CDC_Receive_FS(uint8_t *Buf, uint32_t *Len)
  */
 static uint8_t CDC_Transmit_FS(uint8_t *Buf, const uint16_t len)
 {
-    if (static_cast<const USBD_CDC_HandleTypeDef *>(hUsbDeviceFS.pClassData)->TxState != 0)
+    if (static_cast<const USBD_CDC_HandleTypeDef *>(USB_DEVICE_HANDLER.pClassData)->TxState != 0)
         return USBD_BUSY;
-    USBD_CDC_SetTxBuffer(&hUsbDeviceFS, Buf, len);
-    return USBD_CDC_TransmitPacket(&hUsbDeviceFS);
+    USBD_CDC_SetTxBuffer(&USB_DEVICE_HANDLER, Buf, len);
+    return USBD_CDC_TransmitPacket(&USB_DEVICE_HANDLER);
 }
 /**
  * @brief  CDC_TransmitCplt_FS, Data transmitted callback
@@ -137,7 +142,9 @@ static uint8_t CDC_Transmit_FS(uint8_t *Buf, const uint16_t len)
  * @param epnum
  * @retval Result of the operation: USBD_OK if all operations are OK else USBD_FAIL
  */
-static int8_t CDC_TransmitCplt_FS(uint8_t *const Buf, uint32_t *const Len, const uint8_t epnum)
+// ReSharper disable once CppParameterMayBeConstPtrOrRef
+static int8_t
+    CDC_TransmitCplt_FS(uint8_t *const Buf, uint32_t *const Len, const uint8_t epnum) // NOLINT(*-non-const-parameter)
 {
     constexpr int8_t result = USBD_OK;
     const std::span  buffer(Buf, *Len);
@@ -149,7 +156,7 @@ static USBD_CDC_ItfTypeDef USBD_Interface_fops_FS = {
     CDC_Init_FS, [] {return static_cast<int8_t>(USBD_OK); }, CDC_Control_FS, CDC_Receive_FS, CDC_TransmitCplt_FS,
 };
 
-extern char *USBD_PRODUCT_STRING_FS;
+extern char *USBD_PRODUCT_STRING_FS; // TODO make clear this must be configured per board
 
 static constexpr uint8_t USB_SIZ_STRING_SERIAL          = 0x1AU;
 static constexpr int     USBD_VID                       = 1155;
@@ -199,25 +206,25 @@ static __ALIGN_BEGIN uint8_t USBD_StringSerial[USB_SIZ_STRING_SERIAL] __ALIGN_EN
     USB_DESC_TYPE_STRING,
 };
 
-static uint8_t *USBD_FS_DeviceDescriptor(const USBD_SpeedTypeDef speed, uint16_t *length)
+static uint8_t *USBD_DeviceDescriptor(const USBD_SpeedTypeDef speed, uint16_t *length)
 {
     UNUSED(speed);
     *length = sizeof(USBD_FS_DeviceDesc);
     return USBD_FS_DeviceDesc;
 }
-static uint8_t *USBD_FS_LangIDStrDescriptor(const USBD_SpeedTypeDef speed, uint16_t *length)
+static uint8_t *USBD_LangIDStrDescriptor(const USBD_SpeedTypeDef speed, uint16_t *length)
 {
     UNUSED(speed);
     *length = sizeof(USBD_LangIDDesc);
     return USBD_LangIDDesc;
 }
-static uint8_t *USBD_FS_ManufacturerStrDescriptor(const USBD_SpeedTypeDef speed, uint16_t *length)
+static uint8_t *USBD_ManufacturerStrDescriptor(const USBD_SpeedTypeDef speed, uint16_t *length)
 {
     UNUSED(speed);
     USBD_GetString(reinterpret_cast<uint8_t *>(USBD_MANUFACTURER_STRING), USBD_StrDesc, length);
     return USBD_StrDesc;
 }
-static uint8_t *USBD_FS_ProductStrDescriptor(const USBD_SpeedTypeDef speed, uint16_t *length)
+static uint8_t *USBD_ProductStrDescriptor(const USBD_SpeedTypeDef speed, uint16_t *length)
 {
     UNUSED(speed);
     USBD_GetString(reinterpret_cast<uint8_t *>(USBD_PRODUCT_STRING_FS), USBD_StrDesc, length);
@@ -259,7 +266,7 @@ static void Get_SerialNum()
         IntToUnicode(deviceserial1, &USBD_StringSerial[18], 4);
     }
 }
-static uint8_t *USBD_FS_SerialStrDescriptor(const USBD_SpeedTypeDef speed, uint16_t *length)
+static uint8_t *USBD_SerialStrDescriptor(const USBD_SpeedTypeDef speed, uint16_t *length)
 {
     UNUSED(speed);
     *length = USB_SIZ_STRING_SERIAL;
@@ -272,43 +279,223 @@ static uint8_t *USBD_FS_SerialStrDescriptor(const USBD_SpeedTypeDef speed, uint1
     /* USER CODE END USBD_FS_SerialStrDescriptor */
     return USBD_StringSerial;
 }
-static uint8_t *USBD_FS_ConfigStrDescriptor(const USBD_SpeedTypeDef speed, uint16_t *length)
+static uint8_t *USBD_GetConfigurationStrDescriptor(const USBD_SpeedTypeDef speed, uint16_t *length)
 {
     UNUSED(speed);
     USBD_GetString(reinterpret_cast<uint8_t *>(USBD_CONFIGURATION_STRING_FS), USBD_StrDesc, length);
     return USBD_StrDesc;
 }
-static uint8_t *USBD_FS_InterfaceStrDescriptor(const USBD_SpeedTypeDef speed, uint16_t *length)
+static uint8_t *USBD_InterfaceStrDescriptor(const USBD_SpeedTypeDef speed, uint16_t *length)
 {
     UNUSED(speed);
     USBD_GetString(reinterpret_cast<uint8_t *>(USBD_INTERFACE_STRING_FS), USBD_StrDesc, length);
     return USBD_StrDesc;
 }
 
-static USBD_DescriptorsTypeDef FS_Desc = { USBD_FS_DeviceDescriptor,
-                                           USBD_FS_LangIDStrDescriptor,
-                                           USBD_FS_ManufacturerStrDescriptor,
-                                           USBD_FS_ProductStrDescriptor,
-                                           USBD_FS_SerialStrDescriptor,
-                                           USBD_FS_ConfigStrDescriptor,
-                                           USBD_FS_InterfaceStrDescriptor
+static USBD_DescriptorsTypeDef desc = { USBD_DeviceDescriptor,
+                                        USBD_LangIDStrDescriptor,
+                                        USBD_ManufacturerStrDescriptor,
+                                        USBD_ProductStrDescriptor,
+                                        USBD_SerialStrDescriptor,
+                                        USBD_GetConfigurationStrDescriptor,
+                                        USBD_InterfaceStrDescriptor
 #if (USBD_LPM_ENABLED == 1)
-                                           ,
-                                           USBD_FS_USR_BOSDescriptor
+                                        ,
+                                        USBD_FS_USR_BOSDescriptor
 #endif
 };
+
+extern "C"
+{
+    static USBD_StatusTypeDef USBD_Get_USB_Status(const HAL_StatusTypeDef hal_status)
+    {
+        USBD_StatusTypeDef usb_status = USBD_OK;
+        switch (hal_status)
+        {
+            case HAL_OK:
+                usb_status = USBD_OK;
+                break;
+            case HAL_ERROR:
+                usb_status = USBD_FAIL;
+                break;
+            case HAL_BUSY:
+                usb_status = USBD_BUSY;
+                break;
+            case HAL_TIMEOUT:
+            default:
+                usb_status = USBD_FAIL;
+                break;
+        }
+        return usb_status;
+    }
+    void HAL_PCD_SetupStageCallback(PCD_HandleTypeDef *hpcd)
+    {
+        USBD_LL_SetupStage(static_cast<USBD_HandleTypeDef *>(hpcd->pData), reinterpret_cast<uint8_t *>(hpcd->Setup));
+    }
+    void HAL_PCD_DataOutStageCallback(PCD_HandleTypeDef *hpcd, const uint8_t epnum)
+    {
+        USBD_LL_DataOutStage(static_cast<USBD_HandleTypeDef *>(hpcd->pData), epnum, hpcd->OUT_ep[epnum].xfer_buff);
+    }
+    void HAL_PCD_DataInStageCallback(PCD_HandleTypeDef *hpcd, const uint8_t epnum)
+    {
+        USBD_LL_DataInStage(static_cast<USBD_HandleTypeDef *>(hpcd->pData), epnum, hpcd->IN_ep[epnum].xfer_buff);
+    }
+    void HAL_PCD_SOFCallback(PCD_HandleTypeDef *hpcd)
+    {
+        USBD_LL_SOF(static_cast<USBD_HandleTypeDef *>(hpcd->pData));
+    }
+    void HAL_PCD_ResetCallback(PCD_HandleTypeDef *hpcd)
+    {
+        constexpr USBD_SpeedTypeDef speed = USBD_SPEED_FULL;
+        if (hpcd->Init.speed != PCD_SPEED_FULL)
+        {
+            Error_Handler();
+        }
+        /* Set Speed. */
+        USBD_LL_SetSpeed(static_cast<USBD_HandleTypeDef *>(hpcd->pData), speed);
+        /* Reset Device. */
+        USBD_LL_Reset(static_cast<USBD_HandleTypeDef *>(hpcd->pData));
+    }
+    void HAL_PCD_SuspendCallback(PCD_HandleTypeDef *hpcd)
+    {
+        USBD_LL_Suspend(static_cast<USBD_HandleTypeDef *>(hpcd->pData));
+#ifdef __HAL_PCD_GATE_PHYCLOCK
+        __HAL_PCD_GATE_PHYCLOCK(hpcd);
+#endif
+        hw::usb::disconnect_callback();
+        if (hpcd->Init.low_power_enable)
+        {
+            SCB->SCR |= (uint32_t)(SCB_SCR_SLEEPDEEP_Msk | SCB_SCR_SLEEPONEXIT_Msk);
+        }
+    }
+    void HAL_PCD_ResumeCallback(PCD_HandleTypeDef *hpcd)
+    {
+        hw::usb::connect_callback();
+        USBD_LL_Resume(static_cast<USBD_HandleTypeDef *>(hpcd->pData));
+    }
+    void HAL_PCD_ConnectCallback(PCD_HandleTypeDef *hpcd)
+    {
+        USBD_LL_DevConnected(static_cast<USBD_HandleTypeDef *>(hpcd->pData));
+    }
+    void HAL_PCD_DisconnectCallback(PCD_HandleTypeDef *hpcd)
+    {
+        USBD_LL_DevDisconnected(static_cast<USBD_HandleTypeDef *>(hpcd->pData));
+    }
+    USBD_StatusTypeDef USBD_LL_Init(USBD_HandleTypeDef *pdev)
+    {
+        /* Link USB Device <-> PCD */
+        pdev->pData           = &hpcd_USB_DRD_FS;
+        hpcd_USB_DRD_FS.pData = pdev;
+
+        /* Initialize LL Driver */
+        // MX_USB_PCD_Init(); // TODO dawg...
+
+        /* PMA buffer configuration */
+#ifdef FS
+        HAL_PCDEx_PMAConfig(&hpcd_USB_DRD_FS, 0x00, PCD_SNG_BUF, 0x20); /* EP0 OUT */
+        HAL_PCDEx_PMAConfig(&hpcd_USB_DRD_FS, 0x80, PCD_SNG_BUF, 0x60); /* EP0 IN */
+        HAL_PCDEx_PMAConfig(&hpcd_USB_DRD_FS, CDC_OUT_EP, PCD_SNG_BUF, 0xA0);
+        HAL_PCDEx_PMAConfig(&hpcd_USB_DRD_FS, CDC_IN_EP, PCD_SNG_BUF, 0xE0);
+        HAL_PCDEx_PMAConfig(&hpcd_USB_DRD_FS, CDC_CMD_EP, PCD_SNG_BUF, 0x120);
+#endif
+#ifdef HS
+        HAL_PCDEx_SetRxFiFo(&hpcd_USB_OTG_HS, 0x200);
+        HAL_PCDEx_SetTxFiFo(&hpcd_USB_OTG_HS, 0, 0x80);
+        HAL_PCDEx_SetTxFiFo(&hpcd_USB_OTG_HS, 1, 0x174);
+#endif
+        return USBD_OK;
+    }
+    USBD_StatusTypeDef USBD_LL_DeInit(USBD_HandleTypeDef *pdev)
+    {
+        return USBD_Get_USB_Status(HAL_PCD_DeInit(static_cast<PCD_HandleTypeDef *>(pdev->pData)));
+    }
+    USBD_StatusTypeDef USBD_LL_Start(USBD_HandleTypeDef *pdev)
+    {
+        return USBD_Get_USB_Status(HAL_PCD_Start(static_cast<PCD_HandleTypeDef *>(pdev->pData)));
+    }
+    USBD_StatusTypeDef USBD_LL_Stop(USBD_HandleTypeDef *pdev)
+    {
+        return USBD_Get_USB_Status(HAL_PCD_Stop(static_cast<PCD_HandleTypeDef *>(pdev->pData)));
+    }
+    USBD_StatusTypeDef
+        USBD_LL_OpenEP(USBD_HandleTypeDef *pdev, const uint8_t ep_addr, const uint8_t ep_type, const uint16_t ep_mps)
+    {
+        return USBD_Get_USB_Status(
+            HAL_PCD_EP_Open(static_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr, ep_mps, ep_type));
+    }
+    USBD_StatusTypeDef USBD_LL_CloseEP(USBD_HandleTypeDef *pdev, const uint8_t ep_addr)
+    {
+        return USBD_Get_USB_Status(HAL_PCD_EP_Close(static_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr));
+    }
+    USBD_StatusTypeDef USBD_LL_FlushEP(USBD_HandleTypeDef *pdev, const uint8_t ep_addr)
+    {
+        return USBD_Get_USB_Status(HAL_PCD_EP_Flush(static_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr));
+    }
+    USBD_StatusTypeDef USBD_LL_StallEP(USBD_HandleTypeDef *pdev, const uint8_t ep_addr)
+    {
+        return USBD_Get_USB_Status(HAL_PCD_EP_SetStall(static_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr));
+    }
+    USBD_StatusTypeDef USBD_LL_ClearStallEP(USBD_HandleTypeDef *pdev, const uint8_t ep_addr)
+    {
+        return USBD_Get_USB_Status(HAL_PCD_EP_ClrStall(static_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr));
+    }
+    uint8_t USBD_LL_IsStallEP(USBD_HandleTypeDef *pdev, const uint8_t ep_addr)
+    {
+        const PCD_HandleTypeDef *hpcd = static_cast<PCD_HandleTypeDef *>(pdev->pData);
+        if ((ep_addr & 0x80) == 0x80)
+        {
+            return hpcd->IN_ep[ep_addr & 0x7F].is_stall;
+        }
+        return hpcd->OUT_ep[ep_addr & 0x7F].is_stall;
+    }
+    USBD_StatusTypeDef USBD_LL_SetUSBAddress(USBD_HandleTypeDef *pdev, const uint8_t dev_addr)
+    {
+        return USBD_Get_USB_Status(HAL_PCD_SetAddress(static_cast<PCD_HandleTypeDef *>(pdev->pData), dev_addr));
+    }
+    USBD_StatusTypeDef
+        USBD_LL_Transmit(USBD_HandleTypeDef *pdev, const uint8_t ep_addr, uint8_t *pbuf, const uint32_t size)
+    {
+        return USBD_Get_USB_Status(
+            HAL_PCD_EP_Transmit(static_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr, pbuf, size));
+    }
+    USBD_StatusTypeDef
+        USBD_LL_PrepareReceive(USBD_HandleTypeDef *pdev, const uint8_t ep_addr, uint8_t *pbuf, const uint32_t size)
+    {
+        return USBD_Get_USB_Status(
+            HAL_PCD_EP_Receive(static_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr, pbuf, size));
+    }
+    uint32_t USBD_LL_GetRxDataSize(USBD_HandleTypeDef *pdev, const uint8_t ep_addr)
+    {
+        return HAL_PCD_EP_GetRxCount(static_cast<PCD_HandleTypeDef *>(pdev->pData), ep_addr);
+    }
+    void *USBD_static_malloc(const uint32_t size)
+    {
+        UNUSED(size);
+        static uint32_t mem[sizeof(USBD_CDC_HandleTypeDef) / 4 + 1]; /* On 32-bit boundary */
+        return mem;
+    }
+    // ReSharper disable once CppParameterMayBeConstPtrOrRef
+    void USBD_static_free(void *const p)
+    {
+        UNUSED(p);
+    }
+    void USBD_LL_Delay(const uint32_t Delay)
+    {
+        HAL_Delay(Delay);
+    }
+}
 
 namespace hw::usb
 {
 ExitCode init()
 {
-    if (USBD_Init(&hUsbDeviceFS, &FS_Desc, DEVICE_FS) != USBD_OK)
+    if (USBD_Init(&USB_DEVICE_HANDLER, &desc, DEVICE_FS) != USBD_OK)
         return ExitCode::EXIT_CODE_ERROR;
-    if (USBD_RegisterClass(&hUsbDeviceFS, &USBD_CDC) != USBD_OK)
+    if (USBD_RegisterClass(&USB_DEVICE_HANDLER, &USBD_CDC) != USBD_OK)
         return ExitCode::EXIT_CODE_ERROR;
-    if (USBD_CDC_RegisterInterface(&hUsbDeviceFS, &USBD_Interface_fops_FS) != USBD_OK)
+    if (USBD_CDC_RegisterInterface(&USB_DEVICE_HANDLER, &USBD_Interface_fops_FS) != USBD_OK)
         return ExitCode::EXIT_CODE_ERROR;
-    if (USBD_Start(&hUsbDeviceFS) != USBD_OK)
+    if (USBD_Start(&USB_DEVICE_HANDLER) != USBD_OK)
         return ExitCode::EXIT_CODE_ERROR;
     HAL_PWREx_EnableUSBVoltageDetector();
     return ExitCode::EXIT_CODE_OK;
