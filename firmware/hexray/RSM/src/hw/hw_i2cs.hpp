16ebb3175a5cd64b9e5476dd530c09b0d1e16db2
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-01-30 21:48:45 -0800

    formatting

--- firmware/hexray/RSM/src/hw/hw_i2cs.hpp
+++ firmware/hexray/RSM/src/hw/hw_i2cs.hpp
@@ -4,7 +4,7 @@
 
 namespace hw::i2c
 {
-extern I2CBus i2c_bus_2;
+extern I2CBus    i2c_bus_2;
 extern I2CDevice r_pump;
-I2CBus       &getBusFromHandle(const I2C_HandleTypeDef *handle);
+I2CBus          &getBusFromHandle(const I2C_HandleTypeDef *handle);
 } // namespace hw::i2c
