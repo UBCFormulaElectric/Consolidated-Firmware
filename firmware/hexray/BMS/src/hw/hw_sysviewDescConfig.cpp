395da27e75c34e796a0a08c09ec619cdd140f5eb
Jack Polloway <jack_polloway@icloud.com>
2026-01-30 20:11:20 -0800

    sysviewjksdhfljksflks done

---
+++ firmware/hexray/BMS/src/hw/hw_sysviewDescConfig.cpp
@@ -0,0 +1,9 @@
+#include "SEGGER_SYSVIEW.h" // Note: Are we planning on making an hpp version?
+
+namespace hw::sysviewDescConfig
+{
+    void sendSystemDesc(void)
+    {
+        SEGGER_SYSVIEW_SendSysDesc("N=BMS,D=Cortex-M7,O=FreeRTOS");
+    }
+} // namespace hw::sysviewDescConfig
