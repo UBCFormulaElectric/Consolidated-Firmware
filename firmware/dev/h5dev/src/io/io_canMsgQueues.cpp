83479298933c8e95550cc3e03f39bf7403e45510
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-01-30 23:33:47 -0800

    queue

--- firmware/dev/h5dev/src/io/io_canMsgQueues.cpp
+++ firmware/dev/h5dev/src/io/io_canMsgQueues.cpp
@@ -1,5 +1,4 @@
 #include "io_canMsgQueues.hpp"
-#include <string>
 
 void tx_overflow_callback(const uint32_t overflow_count)
 {
@@ -11,8 +10,8 @@ void rx_overflow_callback(const uint32_t overflow_count)
     UNUSED(overflow_count);
     LOG_INFO("CAN RX OVERFLOW");
 }
-void tx_overflow_clear_callback(void) {}
-void rx_overflow_clear_callback(void) {}
+void tx_overflow_clear_callback() {}
+void rx_overflow_clear_callback() {}
 
-io::CanMsgQueue<> can_tx_queue("CanTXQueue", tx_overflow_callback, tx_overflow_clear_callback);
-io::CanMsgQueue<> can_rx_queue("CanRXQueue", rx_overflow_callback, rx_overflow_clear_callback);
+io::queue<io::CanMsg, 128> can_tx_queue("CanTXQueue", tx_overflow_callback, tx_overflow_clear_callback);
+io::queue<io::CanMsg, 128> can_rx_queue("CanRXQueue", rx_overflow_callback, rx_overflow_clear_callback);
