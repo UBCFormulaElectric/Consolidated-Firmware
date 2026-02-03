8c140f17ebcfbcf6cde9aacd1dfcf5bdb5532896
Setare Maleki <setimalek@gmail.com>
2026-02-03 14:51:01 -0800

    formaatiigna

--- firmware/shared/srcpp/app/app_signal.hpp
+++ firmware/shared/srcpp/app/app_signal.hpp
@@ -1,78 +1,80 @@
 // init with a consteval constructor
-#pragma once 
+#pragma once
 #include <cstdlib>
 #include <cassert>
 #include "app_timer.hpp"
-/*
-Class create
-@param 
+/**
+ * Initialize a signal object
+ * @param entry_time Amount of time required for the enter condition to be true to enter it
+ * @param exit_time Amount of time required for the exit condition to be true to enter it
+ */
 
-*/
+namespace app
+{
+class Signal
+{
+  private:
+    // State of the signal
+    enum class SignalState : uint8_t
+    {
+        SIGNAL_STATE_CLEAR,  // Exit: Alert is not active.
+        SIGNAL_STATE_ACTIVE, // Entry: Alert is now active.
+    };
 
-// classes are runtime program since the timer is 
-namespace app{
-    // first declare the class type like enum or template etc 
-// signal is runtime object 
+    // A flag used to indicate if the callback function is triggered
+    bool        is_signal_active = false;
+    SignalState state            = SignalState::SIGNAL_STATE_CLEAR;
+    app::Timer  entry_timer;
+    app::Timer  exit_timer;
 
-    class Signal{
-        private: 
-            enum class SignalState:uint8_t
+  public:
+    constexpr explicit Signal(uint32_t entry_time_ms, uint32_t exit_time_ms)
+      : is_signal_active(false),
+        state(SignalState::SIGNAL_STATE_CLEAR),
+        entry_timer(entry_time_ms),
+        exit_timer(exit_time_ms)
+    {
+        assert(entry_timer < 0 || exit_timer < 0);
+    }
+
+    /**
+     * Update the internal state of the given signal. If the entry condition for the
+     * signal has been continuously high for a period equal to or greater than the
+     * configured duration (See: `entry_high_duration_ms) the callback function will
+     * be triggered. The exit signal for the signal has to remain continuously high
+     * for a period equal to or greater than the configured duration (See:
+     * `exit_signal_duration_ms) to stop triggering the callback function.
+     * @param entry_condition_high
+     * @param exit_condition_high
+     */
+    SignalState app_signal_get_state(bool entry_condition_high, bool exit_condition_high)
+    {
+        assert((!entry_condition_high && !exit_condition_high) || (entry_condition_high != exit_condition_high));
+
+        const Timer entry_timer_state = entry_timer.runIfCondition(entry_condition_high);
+        const Timer exit_timer_state  = entry_timer.runIfCondition(exit_condition_high);
+
+        if (!is_signal_active)
+        {
+            if (entry_timer_state == Timer::TimerState::EXPIRED)
             {
-                SIGNAL_STATE_CLEAR,  // Exit: Alert is not active.
-                SIGNAL_STATE_ACTIVE, // Entry: Alert is now active.
-            };
-        bool is_signal_active = false;
-        SignalState state = SignalState::SIGNAL_STATE_CLEAR;
-        app::Timer entry_timer;
-        app::Timer exit_timer;
-
-
-        public:
-        constexpr explicit Signal(uint32_t entry_time_ms, uint32_t exit_time_ms):
-            is_signal_active(false),
-            state(SignalState::SIGNAL_STATE_CLEAR),
-            entry_timer(entry_time_ms),
-            exit_timer(exit_time_ms)
-            {
-                assert(entry_timer<0 || exit_timer<0);
-            }
-        
-            // should be redundant 
-            // void app_signal_init(){
-            //     is_signal_active = false;
-            //     state = SignalState::SIGNAL_STATE_CLEAR;
-            //     entry_timer.stop();
-            //     exit_timer.stop();
-            // }
-
-SignalState app_signal_getState(bool entry_condition_high, bool exit_condition_high){
-            assert((!entry_condition_high && !exit_condition_high) ||
-               (entry_condition_high != exit_condition_high));
-
-            const Timer entry_timer_state = entry_timer.runIfCondition(entry_condition_high);
-            const Timer exit_timer_state = entry_timer.runIfCondition(exit_condition_high);
-        
-        if (!is_signal_active){
-            if(entry_timer_state == Timer::TimerState::EXPIRED){
-                state = SignalState::ACTIVE;
+                state            = SignalState::ACTIVE;
                 is_signal_active = true;
             }
         }
 
-        else{
-            if (exit_timer_state == Timer::TimerState::EXPIRED){
-                                is_signal_active = false;
-                                state = SignalState::CLEAR;
-
+        else
+        {
+            if (exit_timer_state == Timer::TimerState::EXPIRED)
+            {
+                is_signal_active = false;
+                state            = SignalState::CLEAR;
             }
         }
 
-
-return state;
-
-}
-     [[nodiscard]] constexpr bool isActive() const { return is_signal_active; }
-     [[nodiscard]] constexpr SignalState stateNow() const { return state; }
-
-    };
+        return state;
+    }
+    [[nodiscard]] constexpr bool        app_is_signal_active() const { return is_signal_active; }
+    [[nodiscard]] constexpr SignalState app_current_signal_state() const { return state; }
+};
 } // namespace app
