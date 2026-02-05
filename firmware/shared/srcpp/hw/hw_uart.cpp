c8f8429e6f2c6ad19051527fa91924dcf5b8f333
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-02-05 14:44:57 -0800

    uart cleanup

--- firmware/shared/srcpp/hw/hw_uart.cpp
+++ firmware/shared/srcpp/hw/hw_uart.cpp
@@ -13,16 +13,16 @@ std::expected<void, ErrorCode> Uart::receiveDma(std::span<uint8_t> data) const
     return hw_utils_convertHalStatus(HAL_UART_Receive_DMA(handle, data.data(), static_cast<uint16_t>(data.size())));
 }
 
-std::expected<void, ErrorCode> Uart::transmitPoll(const std::span<const uint8_t> data, const uint32_t timeout) const
+std::expected<void, ErrorCode> Uart::transmitPoll(const std::span<const uint8_t> tx_data, const uint32_t timeout) const
 {
     return hw_utils_convertHalStatus(
-        HAL_UART_Transmit(handle, data.data(), static_cast<uint16_t>(data.size()), timeout));
+        HAL_UART_Transmit(handle, tx_data.data(), static_cast<uint16_t>(tx_data.size()), timeout));
 }
 
-std::expected<void, ErrorCode> Uart::receivePoll(std::span<uint8_t> data, const uint32_t timeout) const
+std::expected<void, ErrorCode> Uart::receivePoll(std::span<uint8_t> rx_data, const uint32_t timeout) const
 {
     return hw_utils_convertHalStatus(
-        HAL_UART_Receive(handle, data.data(), static_cast<uint16_t>(data.size()), timeout));
+        HAL_UART_Receive(handle, rx_data.data(), static_cast<uint16_t>(rx_data.size()), timeout));
 }
 
 std::expected<void, ErrorCode> Uart::transmitIt(const std::span<const uint8_t> data) const
@@ -30,8 +30,8 @@ std::expected<void, ErrorCode> Uart::transmitIt(const std::span<const uint8_t> d
     return hw_utils_convertHalStatus(HAL_UART_Transmit_IT(handle, data.data(), static_cast<uint16_t>(data.size())));
 }
 
-std::expected<void, ErrorCode> Uart::receiveIt(std::span<uint8_t> data) const
+std::expected<void, ErrorCode> Uart::receiveIt(std::span<uint8_t> rx_data) const
 {
-    return hw_utils_convertHalStatus(HAL_UART_Receive_IT(handle, data.data(), static_cast<uint16_t>(data.size())));
+    return hw_utils_convertHalStatus(HAL_UART_Receive_IT(handle, rx_data.data(), static_cast<uint16_t>(rx_data.size())));
 }
 } // namespace hw
--- firmware/shared/srcpp/hw/hw_uart.hpp
+++ firmware/shared/srcpp/hw/hw_uart.hpp
@@ -23,49 +23,43 @@ class Uart
   public:
     /**
      * Transmits an amount of data in DMA mode (non-blocking).
-     * @param pData Pointer to data buffer (u8 or u16 data elements).
-     * @param size Amount of data elements (u8 or u16) to be transmitted.
+     * @param tx_data Span of data to transmit.
      */
-    std::expected<void, ErrorCode> transmitDma(std::span<const uint8_t>) const;
+    std::expected<void, ErrorCode> transmitDma(std::span<const uint8_t> tx_data) const;
 
     /**
      * Receives an amount of data in DMA mode (non-blocking).
-     * @param pData Pointer to data buffer (u8 or u16 data elements).
-     * @param size Amount of data elements (u8 or u16) to be received.
+     * @param rx_data Span to store received data.
      */
-    std::expected<void, ErrorCode> receiveDma(std::span<uint8_t>) const;
+    std::expected<void, ErrorCode> receiveDma(std::span<uint8_t> rx_data) const;
 
     /**
      * Transmits an amount of data in polling mode (blocking).
-     * @param pData Pointer to data buffer (u8 or u16 data elements).
-     * @param size Amount of data elements (u8 or u16) to be transmitted.
+     * @param tx_data Span of data to transmit.
      * @param timeout Timeout duration
      */
     std::expected<void, ErrorCode>
-        transmitPoll(std::span<const uint8_t>, uint32_t timeout = std::numeric_limits<uint32_t>::max()) const;
+        transmitPoll(std::span<const uint8_t> tx_data, uint32_t timeout = std::numeric_limits<uint32_t>::max()) const;
 
     /**
      * Receives an amount of data in polling mode (blocking).
-     * @param pData Pointer to data buffer (u8 or u16 data elements).
-     * @param size Amount of data elements (u8 or u16) to be received.
+     * @param rx_data Span to store received data.
      * @param timeout Timeout duration
      */
     std::expected<void, ErrorCode>
-        receivePoll(std::span<uint8_t>, uint32_t timeout = std::numeric_limits<uint32_t>::max()) const;
+        receivePoll(std::span<uint8_t> rx_data, uint32_t timeout = std::numeric_limits<uint32_t>::max()) const;
 
     /**
      * Receives an amount of data in interrupt mode (non-blocking).
-     * @param pData Pointer to data buffer (u8 or u16 data elements).
-     * @param size Amount of data elements (u8 or u16) to be received.
+     * @param tx_data Span of data to transmit.
      */
-    std::expected<void, ErrorCode> transmitIt(std::span<const uint8_t>) const;
+    std::expected<void, ErrorCode> transmitIt(std::span<const uint8_t> tx_data) const;
 
     /**
      * Receives an amount of data in interrupt mode (non-blocking).
-     * @param pData Pointer to data buffer (u8 or u16 data elements).
-     * @param size Amount of data elements (u8 or u16) to be received.
+     * @param rx_data Span to store received data.
      */
-    std::expected<void, ErrorCode> receiveIt(std::span<uint8_t>) const;
+    std::expected<void, ErrorCode> receiveIt(std::span<uint8_t> rx_data) const;
 
     [[nodiscard]] const UART_HandleTypeDef *getHandle() const { return handle; }
 };
