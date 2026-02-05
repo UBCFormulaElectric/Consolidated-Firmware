7deb80a1a5ca403b8552182f18a90446aa79969a
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-02-05 14:55:21 -0800

    format diff

--- firmware/shared/srcpp/hw/hw_uart.cpp
+++ firmware/shared/srcpp/hw/hw_uart.cpp
@@ -32,6 +32,7 @@ std::expected<void, ErrorCode> Uart::transmitIt(const std::span<const uint8_t> d
 
 std::expected<void, ErrorCode> Uart::receiveIt(std::span<uint8_t> rx_data) const
 {
-    return hw_utils_convertHalStatus(HAL_UART_Receive_IT(handle, rx_data.data(), static_cast<uint16_t>(rx_data.size())));
+    return hw_utils_convertHalStatus(
+        HAL_UART_Receive_IT(handle, rx_data.data(), static_cast<uint16_t>(rx_data.size())));
 }
 } // namespace hw
