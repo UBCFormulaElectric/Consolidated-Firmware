68c82524ca264042db6126a8bf3626a91c5f61a0
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-02-05 14:39:09 -0800

    builds now

--- firmware/shared/srcpp/hw/hw_uart.cpp
+++ firmware/shared/srcpp/hw/hw_uart.cpp
@@ -3,33 +3,35 @@
 
 namespace hw
 {
-std::expected<void, ErrorCode> Uart::transmitDma(std::span<const uint8_t> data) const
+std::expected<void, ErrorCode> Uart::transmitDma(const std::span<const uint8_t> data) const
 {
-    return hw_utils_convertHalStatus(HAL_UART_Transmit_DMA(handle, data.data(), data.size()));
+    return hw_utils_convertHalStatus(HAL_UART_Transmit_DMA(handle, data.data(), static_cast<uint16_t>(data.size())));
 }
 
 std::expected<void, ErrorCode> Uart::receiveDma(std::span<uint8_t> data) const
 {
-    return hw_utils_convertHalStatus(HAL_UART_Receive_DMA(handle, data.data(), data.size()));
+    return hw_utils_convertHalStatus(HAL_UART_Receive_DMA(handle, data.data(), static_cast<uint16_t>(data.size())));
 }
 
-std::expected<void, ErrorCode> Uart::transmitPoll(std::span<const uint8_t> data, uint32_t timeout) const
+std::expected<void, ErrorCode> Uart::transmitPoll(const std::span<const uint8_t> data, const uint32_t timeout) const
 {
-    return hw_utils_convertHalStatus(HAL_UART_Transmit(handle, data.data(), data.size(), timeout));
+    return hw_utils_convertHalStatus(
+        HAL_UART_Transmit(handle, data.data(), static_cast<uint16_t>(data.size()), timeout));
 }
 
-std::expected<void, ErrorCode> Uart::receivePoll(std::span<uint8_t> data, uint32_t timeout) const
+std::expected<void, ErrorCode> Uart::receivePoll(std::span<uint8_t> data, const uint32_t timeout) const
 {
-    return hw_utils_convertHalStatus(HAL_UART_Receive(handle, data.data(), data.size(), timeout));
+    return hw_utils_convertHalStatus(
+        HAL_UART_Receive(handle, data.data(), static_cast<uint16_t>(data.size()), timeout));
 }
 
-std::expected<void, ErrorCode> Uart::transmitIt(std::span<const uint8_t> data) const
+std::expected<void, ErrorCode> Uart::transmitIt(const std::span<const uint8_t> data) const
 {
-    return hw_utils_convertHalStatus(HAL_UART_Transmit_IT(handle, data.data(), data.size()));
+    return hw_utils_convertHalStatus(HAL_UART_Transmit_IT(handle, data.data(), static_cast<uint16_t>(data.size())));
 }
 
 std::expected<void, ErrorCode> Uart::receiveIt(std::span<uint8_t> data) const
 {
-    return hw_utils_convertHalStatus(HAL_UART_Receive_IT(handle, data.data(), data.size()));
+    return hw_utils_convertHalStatus(HAL_UART_Receive_IT(handle, data.data(), static_cast<uint16_t>(data.size())));
 }
 } // namespace hw
