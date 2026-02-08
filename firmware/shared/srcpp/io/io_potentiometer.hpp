eb98bd4e2ac84e44b6b32b969ecb4152aa95584d
Shun <shunaki0214@gmail.com>
2026-02-07 18:07:49 -0800

    read functions completed

--- firmware/shared/srcpp/io/io_potentiometer.hpp
+++ firmware/shared/srcpp/io/io_potentiometer.hpp
@@ -2,6 +2,12 @@
 
 #include <cstdint>
 #include <io_potentiometer.h>
+#include "util_errorCodes.hpp"
+
+constexpr float MAX_WIPER_VALUE = 256.0f;
+constexpr float MIN_WIPER_VALUE = 0.0f;
+constexpr uint8_t POT_WIPER0_REGISTER = 0x0;
+constexpr uint8_t POT_WIPER1_REGISTER = 0x1;
 
 #ifdef TARGET_EMBEDDED
 
@@ -12,7 +18,7 @@ namespace io
     class Potentiometer
     {
         private:
-            const hw::i2c::I2CDevice handle;
+            const hw::i2c::I2CDevice device;
         
             enum class POTENTIOMETER_WIPER
             {
@@ -37,11 +43,32 @@ namespace io
                 // byte 2
                 uint8_t data;
             }
-        
+
         public:
-            Potentiometer()
+            consteval explicit Potentiometer(
+                hw::i2c::I2CDevice device,
+                POTENTIOMETER_WIPER wiper
+            )
+
+            std::expected<void, ErrorCode> readWiper(std::array<uint8_t, 2> &dest) const
+            {
+                std::array<uint8_t, 1> read_cmd {static_cast<uint8_t>((((wiper == WIPER0) ? POT_WIPER0_REGISTER : POT_WIPER1_REGISTER) << 4) |
+                                       (static_cast<uint8_t>(POTENTIOMETER_READ_CMD) << 2))};
+                RETURN_IF_ERR(device.transmit(read_cmd));
+                RETURN_IF_ERR(device.receive(dest));
+                return {};
+            }
+
+            std::expected<void, ErrorCode> readPercentage(uint8_t &dest) const
+            {
+                std::array<uint8_t, 2> data {0};
+                RETURN_IF_ERR(readWiper(data));
+                uint16_t read_data {static_cast<uint16_t>(data[0] << 8 | data [1])};
+                dest = static_cast<uint8_t>((read_data * 100)/ MAX_WIPER_VALUE);
+                return {};
+            }
     }
 }
 
-
+//Rpump is connected to wiper0 not wiper1 this year
 
