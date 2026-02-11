f6ae623fb51b7ab57d48341c81ec9c82c3fc8878
Aditya-Dhiman4 <aditya16dhiman@gmail.com>
2026-02-10 23:12:44 -0800

    ops

--- firmware/hexray/VC/src/app/torque_vectoring/controllers/yaw_rate_control/controllers_dyrc.cpp
+++ firmware/hexray/VC/src/app/torque_vectoring/controllers/yaw_rate_control/controllers_dyrc.cpp
@@ -7,14 +7,16 @@ namespace app::tv::controllers::dyrc
 {
 [[nodiscard]] inline float DirectYawRateControl::computeRefYawRate(float steer_ang_rad, float body_velx_mps) const
 {
-    return (body_velx_mps * steer_ang_rad) / (WHEELBASE_m * (1.0f + ku * body_velx_mps * body_velx_mps));
+    ref_yaw_rate_rad = (body_velx_mps * steer_ang_rad) / (WHEELBASE_m * (1.0f + ku * body_velx_mps * body_velx_mps));
+    return ref_yaw_rate_rad;
 }
 
 [[nodiscard]] inline float
     DirectYawRateControl::computeYawMoment(float r_actual_rad, float steer_ang_rad, float body_velx_mps) const
 {
     float r_ref_rad = computeRefYawRate(steer_ang_rad, body_velx_mps);
-    return pid.compute(r_ref_rad, r_actual_rad, 0.0f);
+    yaw_moment_Nm = pid.compute(r_ref_rad, r_actual_rad, 0.0f);
+    return yaw_moment_Nm;
 }
 
 [[nodiscard]] float getYawMoment() const
