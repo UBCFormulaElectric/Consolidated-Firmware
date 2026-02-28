3376ca864cda8138b4e5f4811b75eba090f5eb45
Rijwal Sangey <rijwalsangey@gmail.com>
2026-02-28 12:03:35 -0800

    rando

--- firmware/hexray/VC/src/io/io_imus.cpp
+++ firmware/hexray/VC/src/io/io_imus.cpp
@@ -1,7 +1,7 @@
 #include "io_imus.hpp"
 #include "hw_spis.hpp"
 
-ImuFilterConfig filter = {};
-io::Imu IMU1(hw::spi::imu1, filter);
-io::Imu IMU2(hw::spi::imu2, filter);
-io::Imu IMU3(hw::spi::imu3, filter);
+//ImuFilterConfig filter = {};
+io::Imu IMU1(hw::spi::imu1);
+io::Imu IMU2(hw::spi::imu2);
+io::Imu IMU3(hw::spi::imu3);
