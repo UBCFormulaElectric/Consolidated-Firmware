a0006f645f37b790620ceb28bdc25f6279913f08
SAki2007 <144624706+SAki2007@users.noreply.github.com>
2026-02-28 17:31:44 -0800

    RSM IO Hexray Bringup (#1850)
    ### Changelist 
    Creating RSM IO layer for Hexray
    
    ### Resolved Tickets
    https://ubcformulaelectric.atlassian.net/browse/FIRM-582
    https://ubcformulaelectric.atlassian.net/browse/FIRM-581
    https://ubcformulaelectric.atlassian.net/browse/FIRM-580
    https://ubcformulaelectric.atlassian.net/browse/FIRM-579
    https://ubcformulaelectric.atlassian.net/browse/FIRM-578
    https://ubcformulaelectric.atlassian.net/browse/FIRM-577
    
    ---------
    
    Co-authored-by: Edwin <20777515+Lucien950@users.noreply.github.com>

---
+++ firmware/hexray/RSM/src/hw/hw_sysviewDescConfig.cpp
@@ -0,0 +1,13 @@
+extern "C"
+{
+#include "SEGGER_SYSVIEW.h"
+}
+
+namespace hw::sysviewConfig
+{
+void sendSystemDesc(void)
+{
+    SEGGER_SYSVIEW_SendSysDesc("N=RSM,D=Cortex-M33,O=FreeRTOS,D=STM32H562");
+    SEGGER_SYSVIEW_SendSysDesc("I#15=SysTick,I#34=ADC,I#72=DMA1_Stream0");
+}
+} // namespace hw::sysviewConfig
