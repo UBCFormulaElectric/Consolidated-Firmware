1dec70211d44b1e26b0d1911435ef6e288144807
Setare Maleki <setimalek@gmail.com>
2026-03-07 11:10:18 -0800

    format

--- firmware/hexray/VC/src/app/app_loadswitches.cpp
+++ firmware/hexray/VC/src/app/app_loadswitches.cpp
@@ -6,51 +6,52 @@
 Setting CAN signals for the efuse status aka turning efuse on or off and setting the
 current we want to send over CAN for each efuse channel.
 */
-struct CanSetters{
+struct CanSetters
+{
     void *setter;
     void *getter;
 }
 
 namespace app::loadswitches
 {
-// array passed in will be the efuse_channels which will have all the efuses. 
-static void (*const efuse_enabled_can_setters[NUM_EFUSE_CHANNELS])(bool) = {
-    [F_INV_Efuse]   = app::can_tx::VC_FrontInvertersStatus_set,
-    [RSM_Efuse]     = app::can_tx::VC_RSMStatus_set,
-    [BMS_Efuse]     = app::can_tx::VC_BMSStatus_set,
-    [R_INV_Efuse]   = app::can_tx::VC_RearInvertersStatus_set,
-    [DAM_Efuse]     = app::can_tx::VC_DAMStatus_set,
-    [FRONT_Efuse]   = app::can_tx::VC_FrontStatus_set,
-    [RL_PUMP_Efuse] = app::can_tx::VC_RearLeftPumpStatus_set,
-    [R_RAD_Efuse]   = app::can_tx::VC_RightRadiatorFanStatus_set,
-    [RR_PUMP_Efuse] = app::can_tx::VC_RearRightPumpStatus_set,
-    [L_RAD_Efuse]   = app::can_tx::VC_LeftRadiatorFanStatus_set,
-};
+    // array passed in will be the efuse_channels which will have all the efuses.
+    static void (*const efuse_enabled_can_setters[NUM_EFUSE_CHANNELS])(bool) = {
+        [F_INV_Efuse]   = app::can_tx::VC_FrontInvertersStatus_set,
+        [RSM_Efuse]     = app::can_tx::VC_RSMStatus_set,
+        [BMS_Efuse]     = app::can_tx::VC_BMSStatus_set,
+        [R_INV_Efuse]   = app::can_tx::VC_RearInvertersStatus_set,
+        [DAM_Efuse]     = app::can_tx::VC_DAMStatus_set,
+        [FRONT_Efuse]   = app::can_tx::VC_FrontStatus_set,
+        [RL_PUMP_Efuse] = app::can_tx::VC_RearLeftPumpStatus_set,
+        [R_RAD_Efuse]   = app::can_tx::VC_RightRadiatorFanStatus_set,
+        [RR_PUMP_Efuse] = app::can_tx::VC_RearRightPumpStatus_set,
+        [L_RAD_Efuse]   = app::can_tx::VC_LeftRadiatorFanStatus_set,
+    };
 
-static void (*const efuse_current_can_setters[NUM_EFUSE_CHANNELS])(float) = {
-    [F_INV_Efuse]   = app::can_tx::VC_FrontInvertersCurrent_set,
-    [RSM_Efuse]     = app::can_tx::VC_RSMCurrent_set,
-    [BMS_Efuse]     = app::can_tx::VC_BMSCurrent_set,
-    [R_INV_Efuse]   = app::can_tx::VC_RearInvertersCurrent_set,
-    [DAM_Efuse]     = app::can_tx::VC_DAMCurrent_set,
-    [FRONT_Efuse]   = app::can_tx::VC_FrontCurrent_set,
-    [RL_PUMP_Efuse] = app::can_tx::VC_RearLeftPumpCurrent_set,
-    [R_RAD_Efuse]   = app::can_tx::VC_RightRadiatorFanCurrent_set,
-    [RR_PUMP_Efuse] = app::can_tx::VC_RearRightPumpCurrent_set,
-    [L_RAD_Efuse]   = app::can_tx::VC_LeftRadiatorFanCurrent_set,
-};
+    static void (*const efuse_current_can_setters[NUM_EFUSE_CHANNELS])(float) = {
+        [F_INV_Efuse]   = app::can_tx::VC_FrontInvertersCurrent_set,
+        [RSM_Efuse]     = app::can_tx::VC_RSMCurrent_set,
+        [BMS_Efuse]     = app::can_tx::VC_BMSCurrent_set,
+        [R_INV_Efuse]   = app::can_tx::VC_RearInvertersCurrent_set,
+        [DAM_Efuse]     = app::can_tx::VC_DAMCurrent_set,
+        [FRONT_Efuse]   = app::can_tx::VC_FrontCurrent_set,
+        [RL_PUMP_Efuse] = app::can_tx::VC_RearLeftPumpCurrent_set,
+        [R_RAD_Efuse]   = app::can_tx::VC_RightRadiatorFanCurrent_set,
+        [RR_PUMP_Efuse] = app::can_tx::VC_RearRightPumpCurrent_set,
+        [L_RAD_Efuse]   = app::can_tx::VC_LeftRadiatorFanCurrent_set,
+    };
 
-void efuse_broadcast(void)
-{
-    // run through each efuse, and broadcast the channel status and current
-    for (int i = 0; i < NUM_EFUSE_CHANNELS; i++)
+    void efuse_broadcast(void)
     {
-        assert(efuse_channels[i] != nullptr);
+        // run through each efuse, and broadcast the channel status and current
+        for (int i = 0; i < NUM_EFUSE_CHANNELS; i++)
+        {
+            assert(efuse_channels[i] != nullptr);
 
-        const bool  enabled = efuse_channels[i]->isChannelEnabled();
-        const float current = efuse_channels[i]->getChannelCurrent();
-        efuse_enabled_can_setters[i](enabled);
-        efuse_current_can_setters[i](current);
+            const bool  enabled = efuse_channels[i]->isChannelEnabled();
+            const float current = efuse_channels[i]->getChannelCurrent();
+            efuse_enabled_can_setters[i](enabled);
+            efuse_current_can_setters[i](current);
+        }
     }
-}
 } // namespace app::loadswitches
--- firmware/hexray/VC/src/app/app_loadswitches.hpp
+++ firmware/hexray/VC/src/app/app_loadswitches.hpp
@@ -5,16 +5,8 @@
 using namespace io;
 #define NUM_EFUSE_CHANNELS 10
 std::array<io::Efuse *const, [NUM_EFUSE_CHANNELS]> efuse_channels = {
-    &F_INV_Efuse,
-    &RSM_Efuse,
-    &BMS_Efuse,
-    &R_INV_Efuse,
-    &DAM_Efuse,
-    &FRONT_Efuse,
-    &RL_PUMP_Efuse,
-    &R_RAD_Efuse,
-    &RR_PUMP_Efuse,
-    &L_RAD_Efuse,
+    &F_INV_Efuse, &RSM_Efuse,     &BMS_Efuse,   &R_INV_Efuse,   &DAM_Efuse,
+    &FRONT_Efuse, &RL_PUMP_Efuse, &R_RAD_Efuse, &RR_PUMP_Efuse, &L_RAD_Efuse,
 };
 
 namespace app::loadswitches
