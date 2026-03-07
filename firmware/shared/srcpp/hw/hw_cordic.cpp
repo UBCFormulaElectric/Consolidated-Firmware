3ca6ee0e9324b4d4f401b2f486e02e2474b38ed7
Aditya-Dhiman4 <aditya16dhiman@gmail.com>
2026-03-07 14:11:43 -0800

    stuff

--- firmware/shared/srcpp/hw/hw_cordic.cpp
+++ firmware/shared/srcpp/hw/hw_cordic.cpp
@@ -1,7 +1,9 @@
 #include "hw_cordic.hpp"
+#include "util_errorCodes.hpp"
 
 namespace hw::cordic
 {
+inline constexpr uint32_t CORDIC_TIMEOUT = 10U;
 
 /**
  * @brief Configuret the CORDIC peripheral to:
@@ -11,14 +13,42 @@ namespace hw::cordic
  * - Number of 32 bit R/W expected before and after calculation
  * - Precision: 1 - 15 cycles (Higher -> More Precision)
  */
-static inline ErrorCode configure()
+static inline std::expected<void, ErrorCode> configure(
+    CORDIC_Function          func,
+    CORDIC_Scale             scale,
+    CORDIC_Nb_Write          nbwrite,
+    std::span<const int32_t> args,
+    std::span<int32_t>       res)
 {
-    // CORDIC_ConfigTypeDef config;
-    // config.Function = ;
-    // config.Scale = ;
-    // config.InSize = ;
-    // config.OutSize = ;
-    // config.NbRead = ;
-    // config.Precision = ;
+    config.Function  = func;
+    config.Scale     = scale;
+    config.InSize    = CORDIC_INSIZE_32BITS;
+    config.OutSize   = CORDIC_OUTSIZE_32BITS;
+    config.NbRead    = CORDIC_NBREAD_1;
+    config.NbWrite   = nbwrite;
+    config.Precision = CORDIC_PRECISION_15CYCLES;
+
+    RETURN_IF_ERR(hw_utils_convertHalStatus(HAL_CORDIC_Configure(handle, &config)));
+    return {};
 }
+static inline std::expected<void, ErrorCode> configure(CORDIC_Nb_Write nbwrite, std::span<const int32_t> args)
+{
+    RETURN_IF_ERR(
+        hw_utils_convertHalStatus(HAL_CORDIC_Calculate(handle, args.data(), res.data(), nbwrite, CORDIC_TIMEOUT)));
+    return {};
+}
+
+inline float sin(float angle, float hyp = 1.0f)
+{
+    int32_t fp_angle = static_cast<int32_t>(angle * (1 << 31));
+}
+inline float cos(float angle, float hyp = 1.0f) {}
+inline float phase(float x, float y) {}
+inline float mod(float x, float y) {}
+inline float atan(float x) {}
+inline float sinh(float angle) {}
+inline float cosh(float angle) {}
+inline float atanh(float angle) {}
+inline float ln(float x) {}
+inline float sqrt(float x) {}
 }; // namespace hw::cordic
