c1fc0558cd8bf014b6ccfe8b443b8e154b48e527
acp02 <aujames430@gmail.com>
2026-03-11 03:30:25 -0700

    no extern c in assert

--- firmware/shared/srcpp/hw/hw_assert.cpp
+++ firmware/shared/srcpp/hw/hw_assert.cpp
@@ -5,7 +5,7 @@
 
 static char buffer[100];
 
-[[noreturn]] extern "C" void __assert_func(const char *file, int line, const char *func, const char *failedexpr)
+[[noreturn]] void __assert_func(const char *file, int line, const char *func, const char *failedexpr)
 {
     // Store the message into a buffer so we can easily inspect it using a
     // debugger even without SEGGER RTT set up
