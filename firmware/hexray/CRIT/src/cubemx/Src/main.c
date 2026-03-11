00c89267a22e096f7df8a5bdb3c8a47d9bc323e0
acp02 <aujames430@gmail.com>
2026-03-11 03:25:26 -0700

    syntax

--- firmware/hexray/CRIT/src/cubemx/Src/main.c
+++ firmware/hexray/CRIT/src/cubemx/Src/main.c
@@ -84,7 +84,7 @@ static void MX_USB_PCD_Init(void);
 int main(void)
 {
     /* USER CODE BEGIN 1 */
-    tasks_preInit()
+    tasks_preInit();
         /* USER CODE END 1 */
 
         /* MCU Configuration--------------------------------------------------------*/
