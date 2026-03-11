88acb0191e16e3530d78e793a6bcada064d8bf56
Rijwal Sangey <rijwalsangey@gmail.com>
2026-03-10 19:33:03 -0700

    merge boy

--- firmware/hexray/VC/src/io/io_efuses.hpp
+++ firmware/hexray/VC/src/io/io_efuses.hpp
@@ -1,15 +1,22 @@
 #pragma once
 
 #include "efuse/io_efuse.hpp"
+#include "efuse/io_efuse_TI_TPS25.hpp"
+#include "efuse/io_efuse_TI_TPS28.hpp"
 
-constexpr int                                            NUM_EFUSE_CHANNELS = 10; // change to 11 with misc fuse
-extern std::reference_wrapper<const io::Efuse> efuses;
+#include <array>
+#include <functional>
 
-extern const io::Efuse f_inv_efuse;
-extern const io::Efuse rsm_efuse;
-extern const io::Efuse bms_efuse;
-extern const io::Efuse r_inv_efuse;
-extern const io::Efuse dam_efuse;
-extern const io::Efuse front_efuse;
-extern const io::Efuse rl_pump_efuse;
-extern const io::Efuse r_rad_fan_efuse;
+constexpr int NUM_EFUSE_CHANNELS = 10; // change to 11 with misc fuse
+extern std::array<std::reference_wrapper<io::Efuse>, NUM_EFUSE_CHANNELS> efuses;
+
+extern io::TI_TPS28_Efuse f_inv_efuse;
+extern io::TI_TPS28_Efuse rsm_efuse;
+extern io::TI_TPS28_Efuse bms_efuse;
+extern io::TI_TPS28_Efuse r_inv_efuse;
+extern io::TI_TPS28_Efuse dam_efuse;
+extern io::TI_TPS28_Efuse front_efuse;
+extern io::TI_TPS28_Efuse l_rad_fan_efuse;
+extern io::TI_TPS28_Efuse r_rad_fan_efuse;
+extern io::TI_TPS25_Efuse rl_pump_efuse;
+extern io::TI_TPS25_Efuse rr_pump_efuse;
