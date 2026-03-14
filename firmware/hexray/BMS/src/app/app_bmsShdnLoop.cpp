7e9604f60ed783cb31c648f83f8f8617b8e46bd6
Kelm <lkevin2003@gmail.com>
2026-03-14 05:56:21 -0700

    builds binaries with lots of warnings too

--- firmware/hexray/BMS/src/app/app_bmsShdnLoop.cpp
+++ firmware/hexray/BMS/src/app/app_bmsShdnLoop.cpp
@@ -3,12 +3,14 @@
 #include "app_bmsShdnLoop.hpp"
 #include "io_shdnLoopNode.hpp"
 #include "app_canTx.hpp"
+#ifdef TARGET_EMBEDDED
+#include "hw_gpios.hpp"
+#endif
 
 using namespace io::shdn;
 namespace
 {
 #ifdef TARGET_EMBEDDED
-#include "hw_gpios.hpp"
 node hv_p_ok_node(hv_p_intlck_sns, app::can_tx::BMS_HVPShdnOKStatus_set);
 node hv_n_ok_node(hv_n_intlck_sns, app::can_tx::BMS_HVNShdnOKStatus_set);
 #elif TARGET_TEST
