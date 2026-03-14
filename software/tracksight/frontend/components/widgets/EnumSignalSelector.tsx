ce7ecb8382a750bc163996bbb94fb4143f6f38bb
Evan <evan@erox.one>
2026-03-14 15:48:03 -0700

    Tracksight State Integration (#1888)
    Continuation of
    https://github.com/UBCFormulaElectric/Consolidated-Firmware/pull/1802,
    fixed which branch
    
    # Description
    
    Moves code over into the new state type, had to split up the canvas
    renderer into two seperate ones for enum and numerical to preserve type
    safety. Moved out mock data into a different data store, will need a new
    paradigm for storing signals.
    
    Regression: I don't think the local storage hook uses the right format
    right now.
    
    ---------
    
    Co-authored-by: Edwin <20777515+Lucien950@users.noreply.github.com>

---
+++ software/tracksight/frontend/components/widgets/EnumSignalSelector.tsx
@@ -0,0 +1,51 @@
+import { ChevronDown } from "lucide-react";
+import React, { RefObject, useRef } from "react";
+
+import SignalSelector from "@/components/SignalSelector";
+import useSignalMetadata from "@/lib/hooks/useSignalMetadata";
+import { isEnumSignalMetadata } from "@/lib/types/Signal";
+
+type EnumSignalSelectorProps = {
+  currentSignal: string;
+  onSignalChange?: (signalName: string) => void;
+};
+
+function EnumSignalSelector(props: EnumSignalSelectorProps) {
+  const { currentSignal, onSignalChange } = props;
+
+  const signalMetadata = useSignalMetadata(currentSignal);
+  const signalDropdownRef = useRef<HTMLDivElement>(null) as RefObject<HTMLDivElement>;
+
+  if (signalMetadata.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (signalMetadata.error) {
+    return <div>Error loading signal metadata.</div>;
+  }
+
+  return (
+    <div className="flex flex-row items-center gap-2 text-lg font-medium">
+      Signal:
+      <div
+        className="relative flex flex-row gap-2 text-base font-normal select-none hover:cursor-pointer"
+        ref={signalDropdownRef}
+      >
+        {signalMetadata ? signalMetadata.data?.name : "Select Signal"}
+        <ChevronDown size={16} />
+        <SignalSelector
+          filter={isEnumSignalMetadata}
+          selectedSignal={signalMetadata.data || null}
+          onSelect={(signal) => {
+            if (!onSignalChange) return;
+
+            onSignalChange(signal.name);
+          }}
+          buttonElement={signalDropdownRef}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default EnumSignalSelector;
