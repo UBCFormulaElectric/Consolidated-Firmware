bb4e1bba89503bac5f926030648479ec42d0c9ac
Setare Maleki <setimalek@gmail.com>
2026-03-14 17:09:00 -0700

    bug fix

--- firmware/hexray/FSM/src/app/app_imu.cpp
+++ firmware/hexray/FSM/src/app/app_imu.cpp
@@ -34,23 +34,5 @@ void broadcast()
     can_tx::FSM_GyroX_set(imu_results.gyro_x_res.value_or(0.0f));
     can_tx::FSM_GyroY_set(imu_results.gyro_y_res.value_or(0.0f));
     can_tx::FSM_GyroZ_set(imu_results.gyro_z_res.value_or(0.0f));
-
-    accel_x = imu_faults.accel_x_fault ? accel_x : 0.0f;
-    accel_y = imu_faults.accel_y_fault ? accel_y : 0.0f;
-    accel_z = imu_faults.accel_z_fault ? accel_z : 0.0f;
-    gyro_x  = imu_faults.gyro_x_fault ? gyro_x : 0.0f;
-    gyro_y  = imu_faults.gyro_y_fault ? gyro_y : 0.0f;
-    gyro_z  = imu_faults.gyro_z_fault ? gyro_z : 0.0f;
-
-    io::Imu::AccelData imu_accel_data = { accel_x, accel_y, accel_z };
-    io::Imu::GyroData  imu_gyro_data  = { gyro_x, gyro_y, gyro_z };
-
-    app_canTx_FSM_LinearAccelerationInX_set(imu_accel_data.x);
-    app_canTx_FSM_LinearAccelerationInY_set(imu_accel_data.y);
-    app_canTx_FSM_LinearAccelerationInZ_set(imu_accel_data.z);
-    app_canTx_FSM_RollAngularAcceleration_set(imu_gyro_data.x);
-    app_canTx_FSM_PitchAngularAcceleration_set(imu_gyro_data.y);
-    app_canTx_FSM_YawAngularAcceleration_set(imu_gyro_data.z);
-#endif // TARGET_TEST
 }
 } // namespace app::imu
