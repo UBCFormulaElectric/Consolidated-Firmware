f5e42977fa4d5fa1af9da8b798167b533e262890
Setare Maleki <setimalek@gmail.com>
2026-03-14 17:03:14 -0700

    bruh formatting

--- firmware/hexray/VC/src/io/io_powerMonitoring.hpp
+++ firmware/hexray/VC/src/io/io_powerMonitoring.hpp
@@ -12,7 +12,7 @@ std::expected<void, ErrorCode> read_register(std::uint16_t reg, std::span<uint8_
 std::expected<void, ErrorCode> write_register(std::uint16_t reg, std::span<const uint8_t> data);
 
 void refresh(void);
-  
+
 void read_voltage(uint8_t ch, float *voltage);
 void read_current(uint8_t ch, float *current);
 void read_power(uint8_t ch, float *power);
