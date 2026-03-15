d5e2109ce0e8f1ffdd9af52fdb0e42522f51a2d4
Setare Maleki <setimalek@gmail.com>
2026-03-14 18:22:50 -0700

    move shdnloop

--- firmware/hexray/VC/src/io/io_shdnLoop.hpp
+++
@@ -1,4 +0,0 @@
-hw::Gpio sb_shdn_3v3_out;
-hw::Gpio rr_ilck_3v3_out;
-hw::Gpio tsms_3v3_out;
-hw::Gpio inertia_3v3_out;
--- firmware/quintuna/VC/src/app/app_heartbeatMonitors.h
+++ firmware/quintuna/VC/src/app/app_heartbeatMonitors.h
@@ -1,11 +1,3 @@
 #pragma once
-#ifdef __cplusplus
-extern "C"
-{
-#endif
 #include "app_heartbeatMonitor.h"
-
-    extern const HeartbeatMonitor hb_monitor;
-#ifdef __cplusplus
-}
-#endif
+extern const HeartbeatMonitor hb_monitor;
