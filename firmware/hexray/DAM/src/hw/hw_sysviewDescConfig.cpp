3b77980a0a2714f6d2dd8a38dffd773ba3d49158
Rijwal Sangey <rijwalsangey@gmail.com>
2026-03-21 15:27:13 -0700

    fixed dam

--- firmware/hexray/DAM/src/hw/hw_sysviewDescConfig.cpp
+++ firmware/hexray/DAM/src/hw/hw_sysviewDescConfig.cpp
@@ -1,6 +1,6 @@
 #include "SEGGER_SYSVIEW.h"
 
-void hw_sysviewConfig_sendSystemDesc()
+extern "C" void hw_sysviewConfig_sendSystemDesc()
 {
     SEGGER_SYSVIEW_SendSysDesc("N=DAM,C=Cortex-M7,O=FreeRTOS,D=STM32H562RI");
     SEGGER_SYSVIEW_SendSysDesc("I#15=SysTick");
