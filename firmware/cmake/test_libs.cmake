0c3dcd88573872a11011a77a080080875b8f96aa
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-03-22 18:41:43 -0700

    come on

--- firmware/cmake/test_libs.cmake
+++ firmware/cmake/test_libs.cmake
@@ -1,34 +1,6 @@
 message("")
 message("⚙️ [test_libs.cmake] Configuring library wrappers")
 
-message("  🔃 Registered jsoncan_library() function")
-function(jsoncan_library BOARD CAR JSONCAN_DIR)
-    set(JSONCAN_LIB "${CAR}_${BOARD}_jsoncan")
-    set(JSONCAN_FAKES_LIB "${CAR}_${BOARD}_jsoncan_fakes")
-
-    jsoncan_sources(
-            ${BOARD}
-            ${JSONCAN_DIR}
-            FALSE
-            "${CAN_DIR}/dbcs/${CAR}.dbc"
-            "${CAN_DIR}/${CAR}"
-    )
-    add_library(${JSONCAN_LIB} INTERFACE)
-    target_sources(${JSONCAN_LIB} INTERFACE ${CAN_SRCS})
-    no_checks("${CAN_SRCS}")
-    target_include_directories(${JSONCAN_LIB} INTERFACE "${CAN_INCLUDE_DIRS}")
-
-    set(HEADERS_TO_FAKE
-            "${JSONCAN_DIR}/io/io_canTx.h"
-            "${JSONCAN_DIR}/io/io_canRx.h"
-    )
-    create_fake_library(
-            "${JSONCAN_FAKES_LIB}"
-            "${HEADERS_TO_FAKE}"
-    )
-    target_link_libraries(${JSONCAN_LIB} INTERFACE "${JSONCAN_FAKES_LIB}")
-endfunction()
-
 message("  🔃 Registered jsoncan_library_cpp() function")
 function(jsoncan_library_cpp BOARD CAR JSONCAN_DIR)
     set(JSONCAN_LIB "${CAR}_${BOARD}_jsoncan")
