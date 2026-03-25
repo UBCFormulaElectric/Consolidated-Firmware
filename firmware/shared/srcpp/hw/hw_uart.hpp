e6d5d2635ad573e2db06c9c8595324cf9a54ac03
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-03-24 20:13:29 -0700

    Async UART (#1923)
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    ### Resolved Tickets
    <!-- Link any tickets that this PR resolves. -->

--- firmware/shared/srcpp/hw/hw_uart.hpp
+++ firmware/shared/srcpp/hw/hw_uart.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
 #include "util_errorCodes.hpp"
-#include <span>
-
-#ifdef TARGET_EMBEDDED
 #include "hw_hal.hpp"
-#endif
+#include "FreeRTOS.h"
+#include "task.h"
+
+#include <span>
 
 // HAL UART type documentation pg 1161:
 // https://www.st.com/resource/en/user_manual/um1725-description-of-stm32f4-hal-and-lowlayer-drivers-stmicroelectronics.pdf
@@ -16,11 +16,41 @@ class Uart
 {
 #ifdef TARGET_EMBEDDED
   private:
-    UART_HandleTypeDef *const handle; // pointer to structure containing UART module configuration information
+    mutable TaskHandle_t taskInProgress;
+    UART_HandleTypeDef  &handle; // pointer to structure containing UART module configuration information
   public:
-    explicit consteval Uart(UART_HandleTypeDef *in_handle) : handle(in_handle) {}
+    explicit consteval Uart(UART_HandleTypeDef &in_handle) : taskInProgress(nullptr), handle(in_handle) {}
 #endif
+  private:
+    /**
+     * @param timeoutMs
+     * @return
+     */
+    std::expected<void, ErrorCode> waitForNotification(uint32_t timeoutMs) const;
+
   public:
+    /**
+     *
+     */
+    void onTransactionCompleteFromISR() const;
+
+    /**
+     * Transmits an amount of data in polling mode (blocking).
+     * @param tx Span of data to transmit.
+     * @param timeout Timeout duration
+     */
+    std::expected<void, ErrorCode>
+        transmit(std::span<const uint8_t> tx, uint32_t timeout = std::numeric_limits<uint32_t>::max()) const;
+
+    /**
+     * Receives an amount of data in polling mode (blocking).
+     * @param rx Span to store received data.
+     * @param timeout Timeout duration
+     */
+    std::expected<void, ErrorCode>
+        receive(std::span<uint8_t> rx, uint32_t timeout = std::numeric_limits<uint32_t>::max()) const;
+
+    // TODO
     /**
      * Transmits an amount of data in DMA mode (non-blocking).
      * @param tx_data Span of data to transmit.
@@ -33,36 +63,9 @@ class Uart
      */
     std::expected<void, ErrorCode> receiveDma(std::span<uint8_t> rx_data) const;
 
-    /**
-     * Transmits an amount of data in polling mode (blocking).
-     * @param tx_data Span of data to transmit.
-     * @param timeout Timeout duration
-     */
-    std::expected<void, ErrorCode>
-        transmitPoll(std::span<const uint8_t> tx_data, uint32_t timeout = std::numeric_limits<uint32_t>::max()) const;
+    void deinit() const;
 
-    /**
-     * Receives an amount of data in polling mode (blocking).
-     * @param rx_data Span to store received data.
-     * @param timeout Timeout duration
-     */
-    std::expected<void, ErrorCode>
-        receivePoll(std::span<uint8_t> rx_data, uint32_t timeout = std::numeric_limits<uint32_t>::max()) const;
-
-    /**
-     * Receives an amount of data in interrupt mode (non-blocking).
-     * @param tx_data Span of data to transmit.
-     */
-    std::expected<void, ErrorCode> transmitIt(std::span<const uint8_t> tx_data) const;
-
-    /**
-     * Receives an amount of data in interrupt mode (non-blocking).
-     * @param rx_data Span to store received data.
-     */
-    std::expected<void, ErrorCode> receiveIt(std::span<uint8_t> rx_data) const;
-
-    [[nodiscard]] const UART_HandleTypeDef *getHandle() const { return handle; }
+    [[nodiscard]] const UART_HandleTypeDef &getHandle() const { return handle; }
 };
-
 [[nodiscard]] const Uart &getUartFromHandle(const UART_HandleTypeDef *handle);
 } // namespace hw
