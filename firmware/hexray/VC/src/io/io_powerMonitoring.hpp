8e5bcf4adae76ec579bb08ade296dfcf1aa127b3
Rijwal Sangey <rijwalsangey@gmail.com>
2026-03-28 12:18:09 -0700

    testing

--- firmware/hexray/VC/src/io/io_powerMonitoring.hpp
+++ firmware/hexray/VC/src/io/io_powerMonitoring.hpp
@@ -11,9 +11,9 @@ namespace io::powerMonitoring
 std::expected<void, ErrorCode> read_register(std::uint16_t reg, std::span<uint8_t> data);
 std::expected<void, ErrorCode> write_register(std::uint16_t reg, std::span<const uint8_t> data);
 
-void io_power_monitoring_refresh(void);
-
-void read_voltage(uint8_t ch, float *voltage);
-void read_current(uint8_t ch, float *current);
-void read_power(uint8_t ch, float *power);
+void refresh(void);
+void init(void);
+float read_voltage(uint8_t ch, float *voltage);
+float read_current(uint8_t ch, float *current);
+float read_power(uint8_t ch, float *power);
 } // namespace io::powerMonitoring
