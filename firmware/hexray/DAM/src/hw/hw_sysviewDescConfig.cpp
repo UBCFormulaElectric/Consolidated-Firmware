857577da82b6b2f950a48f1f21ab13cf3dcd7b41
Rijwal Sangey <rijwalsangey@gmail.com>
2026-03-28 01:21:27 -0700

    formatting

--- firmware/hexray/DAM/src/hw/hw_sysviewDescConfig.cpp
+++ firmware/hexray/DAM/src/hw/hw_sysviewDescConfig.cpp
@@ -6,7 +6,7 @@ extern "C" void hw_sysviewConfig_sendSystemDesc()
     SEGGER_SYSVIEW_SendSysDesc("N=DAM,O=FreeRTOS,D=STM32H562RI,C=Cortex-M33");
     SEGGER_SYSVIEW_SendSysDesc("I#15=SysTick");
 
-    // SEGGER_SYSVIEW_SendSysDesc("I#35=FDCAN1_IT0,I#70=UART7_IRQ,I#143=AD3_IRQ,I#27=ADC1_DMA"); 
+    // SEGGER_SYSVIEW_SendSysDesc("I#35=FDCAN1_IT0,I#70=UART7_IRQ,I#143=AD3_IRQ,I#27=ADC1_DMA");
 
     // I'd clarify the values you have commented above they are lowk suspicious
 }
