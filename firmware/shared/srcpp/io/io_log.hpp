bceb6663401f96f1c5f3beafcb8166114ed7b780
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-04-01 13:30:40 -0700

    hehe

--- firmware/shared/srcpp/io/io_log.hpp
+++ firmware/shared/srcpp/io/io_log.hpp
@@ -2,6 +2,11 @@
 
 #include "util_utils.hpp"
 
+#include <source_location>
+#include <string_view>
+#include <array>
+#include <charconv>
+
 /*
 This logging module users SEGGER Real-Time Transfer (RTT) for printf-style debugging.
 The SEGGER_RTT_printf function has the following spec:
@@ -24,23 +29,80 @@ Notes
 
 */
 
-#define ANSI_BOLD_GREEN "\x1B[1;32m"
-#define ANSI_BOLD_YELLOW "\x1B[1;33m"
-#define ANSI_BOLD_RED "\x1B[1;31m"
-#define ANSI_RESET "\x1B[1;0m"
+inline constexpr std::string_view ANSI_BOLD_GREEN  = "\x1B[1;32m";
+inline constexpr std::string_view ANSI_BOLD_YELLOW = "\x1B[1;33m";
+inline constexpr std::string_view ANSI_BOLD_RED    = "\x1B[1;31m";
+inline constexpr std::string_view ANSI_RESET       = "\x1B[1;0m";
 
 // Do not use these macros directly!
 #ifdef TARGET_EMBEDDED
 #include "SEGGER_RTT.h"
-#define _LOG_PRINTF(format, ...) SEGGER_RTT_printf(0, format, ##__VA_ARGS__)
 #else
 #include <cstdio>
 #define _LOG_PRINTF(format, ...) printf(format, ##__VA_ARGS__)
 #endif
 
-#define _LOG(level, format, ...) \
-    _LOG_PRINTF("[" level ANSI_RESET "] (%s:%u) " format "\n", __BASENAME__(__FILE__), __LINE__, ##__VA_ARGS__)
+template <std::string_view const &...Strs> struct join
+{
+    // Join all strings into a single std::array of chars
+    static constexpr auto impl() noexcept
+    {
+        constexpr std::size_t     len = (Strs.size() + ... + 0);
+        std::array<char, len + 1> arr{};
+        auto                      append = [i = 0, &arr](auto const &s) mutable
+        {
+            for (auto c : s)
+                arr[i++] = c;
+        };
+        (append(Strs), ...);
+        arr[len] = 0;
+        return arr;
+    }
+    // Give the joined string static storage
+    static constexpr auto arr = impl();
+    // View as a std::string_view
+    static constexpr std::string_view value{ arr.data(), arr.size() - 1 };
+};
+// Helper to get the value out
+template <std::string_view const &...Strs> static constexpr auto join_v = join<Strs...>::value;
 
-#define LOG_INFO(format, ...) _LOG(ANSI_BOLD_GREEN "INFO ", format, ##__VA_ARGS__)
-#define LOG_WARN(format, ...) _LOG(ANSI_BOLD_YELLOW "WARN ", format, ##__VA_ARGS__)
-#define LOG_ERROR(format, ...) _LOG(ANSI_BOLD_RED "ERROR", format, ##__VA_ARGS__)
+struct LogContext
+{
+    const char          *fmt;
+    std::source_location loc;
+
+    // The magic: default argument captures the caller's info
+    template <typename T>
+    // ReSharper disable once CppNonExplicitConvertingConstructor
+    LogContext(T &&s, const std::source_location l = std::source_location::current()) : fmt(std::forward<T>(s)), loc(l)
+    {
+    }
+};
+
+template <typename... Args> void LOG(const std::string_view level, const LogContext &ctx, Args &&...args)
+{
+    SEGGER_RTT_printf(0, "[%s] %s:%u: ", level.data(), ctx.loc.file_name(), ctx.loc.line());
+    SEGGER_RTT_printf(0, ctx.fmt, std::forward<Args>(args)...);
+    SEGGER_RTT_printf(0, "\r\n");
+}
+
+template <typename... Args> void LOG_INFO(const LogContext ctx, Args &&...args)
+{
+    static constexpr std::string_view level      = "INFO";
+    static constexpr std::string_view full_level = join_v<ANSI_BOLD_GREEN, level, ANSI_RESET>;
+    LOG(full_level, ctx, std::forward<Args>(args)...);
+}
+
+template <typename... Args> void LOG_WARN(const LogContext ctx, Args &&...args)
+{
+    static constexpr std::string_view level      = "WARN";
+    static constexpr std::string_view full_level = join_v<ANSI_BOLD_YELLOW, level, ANSI_RESET>;
+    LOG(full_level, ctx, std::forward<Args>(args)...);
+}
+
+template <typename... Args> void LOG_ERROR(const LogContext ctx, Args &&...args)
+{
+    static constexpr std::string_view level      = "ERROR";
+    static constexpr std::string_view full_level = join_v<ANSI_BOLD_RED, level, ANSI_RESET>;
+    LOG(full_level, ctx, std::forward<Args>(args)...);
+}
--- firmware/shared/srcpp/util/util_errorCodes.hpp
+++ firmware/shared/srcpp/util/util_errorCodes.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "io_log.hpp"
 #include <expected>
+#include <source_location>
 
 enum class ErrorCode
 {
@@ -15,6 +16,33 @@ enum class ErrorCode
     ERROR_INDETERMINATE, // use this for when you don't know what the exit code is YET
     NUM_EXIT_CODES,
 };
+
+constexpr std::string_view error_code_to_string(const ErrorCode code)
+{
+    switch (code)
+    {
+        case ErrorCode::INVALID_ARGS:
+            return "Invalid arguments";
+        case ErrorCode::OUT_OF_RANGE:
+            return "Out of range";
+        case ErrorCode::TIMEOUT:
+            return "Timeout";
+        case ErrorCode::ERROR:
+            return "Error";
+        case ErrorCode::BUSY:
+            return "Busy";
+        case ErrorCode::UNIMPLEMENTED:
+            return "Unimplemented";
+        case ErrorCode::RETRY_FAILED:
+            return "Retry failed";
+        case ErrorCode::CHECKSUM_FAIL:
+            return "Checksum fail";
+        case ErrorCode::ERROR_INDETERMINATE:
+            return "Indeterminate error";
+        default:
+            return "Unknown error code";
+    }
+}
 
 #define RETURN_IF_ERR(err_expr)                                                \
     {                                                                          \
@@ -33,11 +61,17 @@ enum class ErrorCode
         }                                        \
     }
 
-template <typename T> void _log_if_err(std::expected<T, ErrorCode> out, const char *err_expr)
+template <typename T>
+void _log_if_err(
+    const std::expected<T, ErrorCode> out,
+    const char                       *err_expr,
+    const std::source_location        loc = std::source_location::current())
 {
     if (not out)
     {
-        LOG_ERROR("%s exited with an error: %d", err_expr, static_cast<int>(out.error()));
+        LOG_ERROR("%s exited with an error: %s", err_expr, error_code_to_string(out.error()), loc);
     }
 }
+// this needs to be a macro because it takes an expression and converts it to string
+// i think at some point we will have strong enough reflection to handle this, but not yet
 #define LOG_IF_ERR(err_expr) _log_if_err(err_expr, #err_expr)
