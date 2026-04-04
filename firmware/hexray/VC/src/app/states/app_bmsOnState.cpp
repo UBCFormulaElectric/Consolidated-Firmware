b18bb3638adb28909f105c3d49426673f7a7c707
AmirTajaddodi <amir.tajadodi@gmail.com>
2026-04-04 16:40:29 -0700

    vc_state machine

--- firmware/hexray/VC/src/app/states/app_bmsOnState.cpp
+++ firmware/hexray/VC/src/app/states/app_bmsOnState.cpp
@@ -2,7 +2,7 @@
 #include "app_states.hpp"
 #include "app_canTx.hpp"
 #include "app_canRx.hpp"
-#include <app_canUtils.hpp>
+#include "app_canUtils.hpp"
 
 using namespace app::can_utils;
 
---
+++ firmware/hexray/VC/src/app/states/app_pcmOnState.cpp
@@ -0,0 +1,13 @@
+#include "app_stateMachine.hpp"
+#include "app_canTx.hpp"
+#include "app_states.hpp"
+#include "app_canRx.hpp"
+#include "app_canUtils.hpp"
+#include "app_canAlerts.hpp"
+
+#include "io_pcm.hpp"
+
+using namespace app::can_utils;
+
+
+
