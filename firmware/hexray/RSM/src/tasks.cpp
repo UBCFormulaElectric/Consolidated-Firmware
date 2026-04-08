bd59b233ed5e91f2a5a678c7384733ca510be09f
Shun <shunaki0214@gmail.com>
2026-04-07 20:23:17 -0700

    configured cubeMX

--- firmware/hexray/RSM/src/tasks.cpp
+++ firmware/hexray/RSM/src/tasks.cpp
@@ -104,7 +104,7 @@ static void RSM_StartAllTasks()
 
 void tasks_preInit()
 {
-    hw::bootup::enableInterruptsForApp();
+    // hw::bootup::enableInterruptsForApp();
     hw_hardFaultHandler_init();
 }
 
@@ -113,18 +113,18 @@ void tasks_init()
     hw::adcs::chipsInit();
     hw::can::can1.init();
 
-    hw::bootup::BootRequest boot_request = hw::bootup::getBootRequest();
-    if (boot_request.context != hw::bootup::BootContext::BOOT_CONTEXT_NONE)
-    {
-        if (boot_request.context == hw::bootup::BootContext::BOOT_CONTEXT_STACK_OVERFLOW)
-        {
-            LOG_WARN("Detected stack overflow on the previous boot cycle!");
-        }
+    // hw::bootup::BootRequest boot_request = hw::bootup::getBootRequest();
+    // if (boot_request.context != hw::bootup::BootContext::BOOT_CONTEXT_NONE)
+    // {
+    //     if (boot_request.context == hw::bootup::BootContext::BOOT_CONTEXT_STACK_OVERFLOW)
+    //     {
+    //         LOG_WARN("Detected stack overflow on the previous boot cycle!");
+    //     }
 
-        const_cast<hw::bootup::BootRequest &> (boot_request).context = hw::bootup::BootContext::BOOT_CONTEXT_NONE;
-        const_cast<hw::bootup::BootRequest &> (boot_request).context_value = 0;
-        hw::bootup::setBootRequest(boot_request);
-    }
+    //     const_cast<hw::bootup::BootRequest &> (boot_request).context = hw::bootup::BootContext::BOOT_CONTEXT_NONE;
+    //     const_cast<hw::bootup::BootRequest &> (boot_request).context_value = 0;
+    //     hw::bootup::setBootRequest(boot_request);
+    // }
     
 
     jobs_init();
