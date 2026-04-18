8b1979885f60b5513305249e416b63c6e75efb8b
jackyao <jackyao2004@gmail.com>
2026-04-18 15:34:23 -0700

    sd test

--- software/tracksight/rust_backend/src/bin/sd_test.rs
+++ software/tracksight/rust_backend/src/bin/sd_test.rs
@@ -1,10 +1,25 @@
-use std::fs;
+use std::{fs, path::Path, sync::Arc};
 
-use logfs::{self, LogFsFileCfg};
+use logfs::{LogFsOpenFlags_LOGFS_OPEN_RD_WR, logfs::*};
 
 fn main() {
+    // test 1: check for sd card ✅
     let drives = find_detachable_drives();
     println!("Found drives: {:?}", drives);
+
+    // test 2: format sd card with logfs
+    let sd_card_dir = "/dev/sdb";
+    let disk = Arc::new(LogFsUnixDisk::new(512, 1024 * 1024 * 15, Path::new(sd_card_dir)).unwrap());
+    let mut logfs: LogFs = LogFs::new(512, 1024 * 1024 * 15, disk, 0, false);
+
+    logfs.format().unwrap();
+    logfs.mount().unwrap();
+    let mut file = logfs.open("/.txt", LogFsOpenFlags_LOGFS_OPEN_RD_WR).unwrap();
+    file.write(b"bruh").unwrap();
+
+    let read = file.read(None).unwrap();
+
+    println!("read from file: {}", read.iter().map(|&b| b as char).collect::<String>());
 }
 
 fn find_detachable_drives() -> Vec<String> {
