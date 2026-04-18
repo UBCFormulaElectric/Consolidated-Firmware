d46b2a326aac9bdd74f49a9682e54f91093d2b05
jackyao <jackyao2004@gmail.com>
2026-04-18 16:06:33 -0700

    link functions

--- firmware/logfs/rust/build.rs
+++ firmware/logfs/rust/build.rs
@@ -2,10 +2,23 @@ use bindgen::Builder;
 use std::path::PathBuf;
 
 fn main() {
-    let logfs_root = PathBuf::from(env!("CARGO_MANIFEST_DIR")).parent().unwrap().to_path_buf();
+    let logfs_root = PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .parent()
+        .unwrap()
+        .to_path_buf();
+
     let src_dir = logfs_root.join("src");
 
     println!("cargo:rerun-if-changed={}", src_dir.display());
+
+    cc::Build::new()
+        .include(&src_dir)
+        .files([
+            src_dir.join("logfs.c"),
+            src_dir.join("disk.c"),
+            src_dir.join("crc.c"),
+        ])
+        .compile("logfs");
 
     let bindings = Builder::default()
         .header(src_dir.join("logfs.h").to_str().unwrap())
