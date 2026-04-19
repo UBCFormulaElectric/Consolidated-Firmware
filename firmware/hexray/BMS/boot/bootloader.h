6574abd2934519ec830b16e69740946574c60da7
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2026-04-19 16:29:49 -0700

    ao

--- firmware/hexray/BMS/boot/bootloader.h
+++ firmware/hexray/BMS/boot/bootloader.h
@@ -1,8 +1,5 @@
 #pragma once
 #include "util_utils.hpp"
 
-CFUNC void bootloader_preinit();
-CFUNC void bootloader_init();
-CFUNC NORET void bootloader_runInterfaceTask(void);
-CFUNC NORET void bootloader_runTickTask(void);
-CFUNC NORET void bootloader_runCanTxTask(void);
+CFUNC void       bootloader_preinit();
+CFUNC NORET void bootloader_init();
