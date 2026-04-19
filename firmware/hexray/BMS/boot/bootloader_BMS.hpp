10f958f7a30ef71927feeeec061f6f59c9ca394e
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2026-04-19 16:29:49 -0700

    aq

--- firmware/hexray/BMS/boot/bootloader_BMS.hpp
+++ firmware/hexray/BMS/boot/bootloader_BMS.hpp
@@ -7,6 +7,6 @@ extern "C"
 #include "app_commitInfo.h"
 }
 
-constexpr uint32_t board_highbits       = 0x1C000000;
+constexpr uint32_t board_highbits       = 0x04000000;
 constexpr uint32_t git_commit_has_val   = GIT_COMMIT_HASH;
 constexpr bool     git_commit_clean_val = GIT_COMMIT_CLEAN;
