2824d9eb44bf130b736d8a76f7f388a2a7167c2c
Aditya-Dhiman4 <aditya16dhiman@gmail.com>
2026-04-22 22:54:25 -0700

    format

--- firmware/hexray/CRIT/src/app/app_switches.cpp
+++ firmware/hexray/CRIT/src/app/app_switches.cpp
@@ -4,12 +4,12 @@
 
 namespace app::switches
 {
-    void broadcast(void)
-    {
-        // update the state from the switches
-        app::can_tx::CRIT_TorqueVectoringSwitch_set(io::switches::torque_vectoring_sw.isClosed());
-        app::can_tx::CRIT_LaunchControlSwitch_set(io::switches::launch_control_sw.isClosed());
-        app::can_tx::CRIT_RegenSwitch_set(io::switches::regen_sw.isClosed());
-        app::can_tx::CRIT_StartButton_set(io::switches::start_sw.isClosed());
-    }
+void broadcast(void)
+{
+    // update the state from the switches
+    app::can_tx::CRIT_TorqueVectoringSwitch_set(io::switches::torque_vectoring_sw.isClosed());
+    app::can_tx::CRIT_LaunchControlSwitch_set(io::switches::launch_control_sw.isClosed());
+    app::can_tx::CRIT_RegenSwitch_set(io::switches::regen_sw.isClosed());
+    app::can_tx::CRIT_StartButton_set(io::switches::start_sw.isClosed());
+}
 } // namespace app::switches
