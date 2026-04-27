058c90129f2ce5adf42c246763452b097b68cb8c
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-04-27 15:50:07 -0700

    step 1

--- firmware/hexray/BMS/src/app/app_heartbeatMonitors.cpp
+++ firmware/hexray/BMS/src/app/app_heartbeatMonitors.cpp
@@ -2,13 +2,18 @@
 #include "app_canRx.hpp"
 #include "app_canTx.hpp"
 #include "app_canAlerts.hpp"
+#include <functional>
 
 static io::heartbeat::node vc_hb_node(
     app::can_rx::VC_Heartbeat_get,
     app::can_rx::VC_Heartbeat_update,
     app::can_tx::BMS_Info_MissingVCHeartbeat_get,
-    app::can_alerts::infos::MissingVCHeartbeat_set);
+    [](const bool fault)
+    {
+        if (fault)
+            app::can_rx::clear_board_rx_table(app::can_utils::CanNode::VC_NODE);
+        app::can_alerts::infos::MissingVCHeartbeat_set(fault);
+    });
 
-static std::array<io::heartbeat::node *const, 1> hb_nodes = { { &vc_hb_node } };
-
-const app::heartbeat::monitor<1> hb_monitor(app::can_tx::BMS_Heartbeat_set, hb_nodes);
+static std::array<std::reference_wrapper<io::heartbeat::node>, 1> hb_nodes{ { vc_hb_node } };
+const app::heartbeat::monitor<1>                                  hb_monitor(app::can_tx::BMS_Heartbeat_set, hb_nodes);
