3775682e7dd475c4b634d36651f7b43107a5517a
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-04-27 12:16:34 -0700

    remove freertos from everything

--- firmware/dev/f4dev/src/cubemx/Src/main.c
+++ firmware/dev/f4dev/src/cubemx/Src/main.c
@@ -19,7 +19,6 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
-#include "cmsis_os.h"
 #include "usb_device.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -54,13 +53,6 @@ SD_HandleTypeDef hsd;
 
 UART_HandleTypeDef huart2;
 
-/* Definitions for defaultTask */
-osThreadId_t         defaultTaskHandle;
-const osThreadAttr_t defaultTask_attributes = {
-    .name       = "defaultTask",
-    .stack_size = 512 * 4,
-    .priority   = (osPriority_t)osPriorityNormal,
-};
 /* USER CODE BEGIN PV */
 /* USER CODE END PV */
 
@@ -73,8 +65,6 @@ static void MX_ADC1_Init(void);
 static void MX_CAN2_Init(void);
 static void MX_USART2_UART_Init(void);
 static void MX_I2C3_Init(void);
-void        StartDefaultTask(void *argument);
-
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -115,46 +105,12 @@ int main(void)
     MX_ADC1_Init();
     MX_CAN2_Init();
     MX_USART2_UART_Init();
+    MX_USB_DEVICE_Init();
     MX_I2C3_Init();
     /* USER CODE BEGIN 2 */
 
     /* USER CODE END 2 */
 
-    /* Init scheduler */
-    osKernelInitialize();
-
-    /* USER CODE BEGIN RTOS_MUTEX */
-    /* add mutexes, ... */
-    /* USER CODE END RTOS_MUTEX */
-
-    /* USER CODE BEGIN RTOS_SEMAPHORES */
-    /* add semaphores, ... */
-    /* USER CODE END RTOS_SEMAPHORES */
-
-    /* USER CODE BEGIN RTOS_TIMERS */
-    /* start timers, add new ones, ... */
-    /* USER CODE END RTOS_TIMERS */
-
-    /* USER CODE BEGIN RTOS_QUEUES */
-    tasks_init();
-    /* USER CODE END RTOS_QUEUES */
-
-    /* Create the thread(s) */
-    /* creation of defaultTask */
-    defaultTaskHandle = osThreadNew(StartDefaultTask, NULL, &defaultTask_attributes);
-
-    /* USER CODE BEGIN RTOS_THREADS */
-    /* add threads, ... */
-    /* USER CODE END RTOS_THREADS */
-
-    /* USER CODE BEGIN RTOS_EVENTS */
-    /* add events, ... */
-    /* USER CODE END RTOS_EVENTS */
-
-    /* Start scheduler */
-    osKernelStart();
-
-    /* We should never get here as control is now taken by the scheduler */
     /* Infinite loop */
     /* USER CODE BEGIN WHILE */
     while (1)
@@ -490,22 +446,6 @@ static void MX_GPIO_Init(void)
 /* USER CODE BEGIN 4 */
 
 /* USER CODE END 4 */
-
-/* USER CODE BEGIN Header_StartDefaultTask */
-/**
- * @brief  Function implementing the defaultTask thread.
- * @param  argument: Not used
- * @retval None
- */
-/* USER CODE END Header_StartDefaultTask */
-void StartDefaultTask(void *argument)
-{
-    /* init code for USB_DEVICE */
-    MX_USB_DEVICE_Init();
-    /* USER CODE BEGIN 5 */
-    tasks_default();
-    /* USER CODE END 5 */
-}
 
 /**
  * @brief  Period elapsed callback in non blocking mode
--- firmware/dev/f4dev/src/cubemx/Src/stm32f4xx_hal_msp.c
+++ firmware/dev/f4dev/src/cubemx/Src/stm32f4xx_hal_msp.c
@@ -71,8 +71,6 @@ void HAL_MspInit(void)
     __HAL_RCC_PWR_CLK_ENABLE();
 
     /* System interrupt init*/
-    /* PendSV_IRQn interrupt configuration */
-    HAL_NVIC_SetPriority(PendSV_IRQn, 15, 0);
 
     /* USER CODE BEGIN MspInit 1 */
 
@@ -196,9 +194,9 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef *hcan)
         HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
         /* CAN1 interrupt Init */
-        HAL_NVIC_SetPriority(CAN1_RX0_IRQn, 5, 0);
+        HAL_NVIC_SetPriority(CAN1_RX0_IRQn, 0, 0);
         HAL_NVIC_EnableIRQ(CAN1_RX0_IRQn);
-        HAL_NVIC_SetPriority(CAN1_RX1_IRQn, 5, 0);
+        HAL_NVIC_SetPriority(CAN1_RX1_IRQn, 0, 0);
         HAL_NVIC_EnableIRQ(CAN1_RX1_IRQn);
         /* USER CODE BEGIN CAN1_MspInit 1 */
 
@@ -230,9 +228,9 @@ void HAL_CAN_MspInit(CAN_HandleTypeDef *hcan)
         HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
         /* CAN2 interrupt Init */
-        HAL_NVIC_SetPriority(CAN2_RX0_IRQn, 5, 0);
+        HAL_NVIC_SetPriority(CAN2_RX0_IRQn, 0, 0);
         HAL_NVIC_EnableIRQ(CAN2_RX0_IRQn);
-        HAL_NVIC_SetPriority(CAN2_RX1_IRQn, 5, 0);
+        HAL_NVIC_SetPriority(CAN2_RX1_IRQn, 0, 0);
         HAL_NVIC_EnableIRQ(CAN2_RX1_IRQn);
         /* USER CODE BEGIN CAN2_MspInit 1 */
 
