3ab7dc9110fba26e2e49a8ffbfa104a3e7b76503
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-04-29 14:29:47 -0700

    inshallah all fixed

--- firmware/hexray/RSM/src/io/io_coolant.cpp
+++ firmware/hexray/RSM/src/io/io_coolant.cpp
@@ -1,4 +1,5 @@
 #include "io_coolant.hpp"
+#include "hw_pwms.hpp"
 
 constexpr float FREQ_TO_LITERS_PER_MINUTE = 7.5f;
 
@@ -6,17 +7,17 @@ namespace io::coolant
 {
 void init()
 {
-    LOG_IF_ERR(hw::pwm::flow_meter_config.init());
+    LOG_IF_ERR(flow_meter_config.init());
 }
 
 float getFlowRate()
 {
-    float frequency = hw::pwm::flow_meter_config.get_frequency();
+    const float frequency = flow_meter_config.get_frequency();
     return frequency * FREQ_TO_LITERS_PER_MINUTE;
 }
 
 std::expected<void, ErrorCode> checkIfFlowMeterActive()
 {
-    return hw::pwm::flow_meter_config.pwm_isActive();
+    return flow_meter_config.pwm_isActive();
 }
 } // namespace io::coolant
