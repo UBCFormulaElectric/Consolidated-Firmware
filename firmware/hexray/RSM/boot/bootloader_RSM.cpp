5a9c3ac509eb69aab4f14533cc8c762ac49a4f81
Shun <shunaki0214@gmail.com>
2026-04-28 19:04:42 -0700

    updated bootloader rsm

--- firmware/hexray/RSM/boot/bootloader_RSM.cpp
+++ firmware/hexray/RSM/boot/bootloader_RSM.cpp
@@ -3,6 +3,8 @@
 #include "bootloader.h"
 #include "main.h"
 #include "io_log.hpp"
+#include "hw_rtosTaskHandler.hpp"
+#include <cassert>
 
 void tx_overflow_callback(const uint32_t overflow_count)
 {
@@ -19,12 +21,20 @@ void rx_overflow_callback(const uint32_t overflow_count)
 void tx_overflow_clear_callback(){};
 void rx_overflow_clear_callback(){};
 
+
+static_assert(sizeof(hw::CanMsg) == 72);
 io::queue<hw::CanMsg, 256> boot_can_tx_queue{ "CanTxQueue", tx_overflow_callback, tx_overflow_clear_callback };
 io::queue<hw::CanMsg, 256> boot_can_rx_queue{ "CanRxQueue", rx_overflow_callback, rx_overflow_clear_callback };
 
 namespace hw::cans
 {
 fdcan fdcan1(hfdcan1, [](const hw::CanMsg &msg) { (void)boot_can_rx_queue.push(msg); });
+}
+
+const hw::fdcan &hw::fdcan_getHandle(const FDCAN_HandleTypeDef *hfdcan)
+{
+    assert(hfdcan == hw::cans::fdcan1.getHfdcan());
+    return hw::cans::fdcan1;
 }
 
 bootloader::config RSM_boot_config(
@@ -35,17 +45,27 @@ bootloader::config RSM_boot_config(
     git_commit_hash_val,
     git_commit_clean_val);
 
-void bootloader_preInit()
+
+static hw::rtos::StaticTask<1024>
+    bootInterfaceTask(osPriorityRealtime, "BootIntf", [](void *) { bootloader::runInterfaceTask(RSM_boot_config); });
+static hw::rtos::StaticTask<1024>
+    bootTickTask(osPriorityRealtime, "BootTick", [](void *) { bootloader::runTickTask(RSM_boot_config); });
+static hw::rtos::StaticTask<1024>
+    bootCanTxTask(osPriorityRealtime, "BootCanTxTask", [](void *) { bootloader::runCanTxTask(RSM_boot_config); });
+
+CFUNC void bootloader_preInit()
 {
     bootloader::preInit();
 }
 
-void bootloader_init()
+[[noreturn]] void bootloader_init()
 {
+    HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
+    osKernelInitialize();
     bootloader::init(RSM_boot_config);
-}
-
-const hw::fdcan &hw::fdcan_getHandle(const FDCAN_HandleTypeDef *hfdcan)
-{
-    return hw::cans::fdcan1;
+    bootInterfaceTask.start();
+    bootTickTask.start();
+    bootCanTxTask.start();
+    osKernelStart();
+    forever {};
 }
