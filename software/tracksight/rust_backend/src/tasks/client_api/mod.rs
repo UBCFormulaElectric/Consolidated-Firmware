450ab73d15f9dda509b2f7fa8a485146d1db89c5
jackyao <jackyao2004@gmail.com>
2026-04-29 00:14:11 -0700

    frontend to DAM channel

--- software/tracksight/rust_backend/src/tasks/client_api/mod.rs
+++ software/tracksight/rust_backend/src/tasks/client_api/mod.rs
@@ -3,18 +3,20 @@ pub mod subtable_api_handler;
 pub mod signal_api_handler;
 pub mod signal_tile;
 pub mod sd_api_handler;
+pub mod transmit_handler;
 
 
 use std::sync::Arc;
-use tokio::sync::RwLock;
+use tokio::sync::{RwLock, broadcast};
 use jsoncan_rust::can_database::CanDatabase;
-use crate::tasks::client_api::{subtable_clients::Clients, signal_tile::SignalTileCache};
+use crate::tasks::{client_api::{signal_tile::SignalTileCache, subtable_clients::Clients}, telem_message::TelemetryOutgoingMessage};
 
 #[derive(Clone)]
 pub struct AppState {
     pub can_db: Arc<CanDatabase>,
     pub clients: Arc<RwLock<Clients>>,
     pub influx_client: Arc<influxdb2::Client>,
+    pub client_out_msg_tx: broadcast::Sender<TelemetryOutgoingMessage>,
 
     pub signal_tile_cache: SignalTileCache,
 }
