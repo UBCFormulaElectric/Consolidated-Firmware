9cd2e1101f4ac3c7a03cf512b7e490db9b69d7d7
Aditya-Dhiman4 <aditya16dhiman@gmail.com>
2026-04-30 00:42:29 -0700

    a

---
+++ firmware/hexray/CRIT/src/app/app_leds.hpp
@@ -0,0 +1,3 @@
+#include "io_leds.hpp"
+
+extern 
--- firmware/hexray/CRIT/src/app/app_switches.cpp
+++ firmware/hexray/CRIT/src/app/app_switches.cpp
@@ -2,6 +2,7 @@
 #include "app_canUtils.hpp"
 #include "app_switches.hpp"
 #include "io_switches.hpp"
+#include "io_leds.hpp"
 
 using namespace app::can_utils;
 
@@ -22,10 +23,16 @@ bool getDebouncedstate(const bool raw, Signal &signal)
 
 void broadcast(void)
 {
+    // TODo: enable leds 
+    const bool tv_switch    = getDebouncedstate(io::switches::torque_vectoring_get(), tv_signal);
+    const bool launch_switch = getDebouncedstate(io::switches::launch_control_get(), launch_signal);
+    const bool regen_switch = getDebouncedstate(io::switches::regen_get(), regen_signal);
+    const bool start_button = getDebouncedstate(io::switches::start_get(), start_signal);
+
     // update the state from the switches
-    app::can_tx::CRIT_TorqueVectoringSwitch_set(static_cast<SwitchState>(getDebouncedstate(io::switches::torque_vectoring_get(), tv_signal)));
-    app::can_tx::CRIT_LaunchControlSwitch_set(static_cast<SwitchState>(getDebouncedstate(io::switches::launch_control_get(), launch_signal)));
-    app::can_tx::CRIT_RegenSwitch_set(static_cast<SwitchState>(getDebouncedstate(io::switches::regen_get(), regen_signal)));
-    app::can_tx::CRIT_StartButton_set(static_cast<SwitchState>(getDebouncedstate(io::switches::start_get(), start_signal)));
+    app::can_tx::CRIT_TorqueVectoringSwitch_set(static_cast<SwitchState>(tv_switch));
+    app::can_tx::CRIT_LaunchControlSwitch_set(static_cast<SwitchState>(launch_switch));
+    app::can_tx::CRIT_RegenSwitch_set(static_cast<SwitchState>(regen_switch));
+    app::can_tx::CRIT_StartButton_set(static_cast<SwitchState>(start_button));    
 }
 } // namespace app::switches
