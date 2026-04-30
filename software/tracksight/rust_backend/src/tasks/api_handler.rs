e80caedaf309df8d483a18a053b0fba71e27d135
jackyao <jackyao2004@gmail.com>
2026-04-29 22:27:26 -0700

    mock

--- software/tracksight/rust_backend/src/tasks/api_handler.rs
+++ software/tracksight/rust_backend/src/tasks/api_handler.rs
@@ -19,7 +19,7 @@ use crate::tasks::client_api::AppState;
 use crate::tasks::client_api::subtable_clients::Clients;
 use crate::tasks::client_api::signal_api_handler::get_signal_router;
 use crate::tasks::client_api::subtable_api_handler::get_subtable_router;
-use crate::tasks::client_api::sd_api_handler::get_sd_router;
+use crate::tasks::client_api::sd_api_handler::{get_sd_router, get_sd_router_mock};
 use crate::vprintln;
 
 pub async fn run_api_handler(
@@ -97,6 +97,7 @@ pub async fn run_api_handler(
         .nest("/api/v1/", get_subtable_router())
         .nest("/api/v1/", get_signal_router())
         .nest("/api/v1/", get_sd_router())
+        .nest("/api/mock/", get_sd_router_mock())
         .nest("/api/v1/", get_transmit_router())
         .with_state(app_state)
         .layer(cors)
