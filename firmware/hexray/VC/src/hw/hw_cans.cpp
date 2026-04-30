fbbfa41457c8d9deae277e77997ac5296e56af1b
AmirTajaddodi <amir.tajadodi@gmail.com>
2026-04-30 00:57:56 -0700

    vc_can reroute

--- firmware/hexray/VC/src/hw/hw_cans.cpp
+++ firmware/hexray/VC/src/hw/hw_cans.cpp
@@ -1,10 +1,45 @@
 #include "hw_cans.hpp"
-#include "io_canReroutes.hpp"
 #include "main.h"
 #include <cassert>
+#ifndef USE_CHIMERA
 
-const hw::fdcan fdcan1(hfdcan1, io::canReroute::handleCallback);
-const hw::fdcan invcan(hfdcan3, io::canReroute::handleCallback);
+#include "app_canUtils.hpp"
+#include "app_jsoncan.hpp"
+#include "io_canReroute.hpp"
+#include "app_canUtils.hpp"
+#include "io_canQueues.hpp"
+
+void handleCallback(const hw::CanMsg &hw_rx_msg)
+{
+    io::CanMsg io_rx_msg{ hw_rx_msg.std_id, hw_rx_msg.dlc, hw_rx_msg.data, true,
+                          static_cast<app::can_utils::BusEnum>(0) };
+
+    JsonCanMsg json_can_msg = app::jsoncan::copyFromCanMsg(io_rx_msg);
+
+    io::can_reroute::reroute_InvCAN(json_can_msg);
+    io::can_reroute::reroute_FDCAN(json_can_msg);
+
+    can_rx_queue.push(io_rx_msg);
+}
+#endif
+
+//Chimera cannot interact with app and io layer
+const hw::fdcan fdcan1(hfdcan1, 
+
+#ifndef USE_CHIMERA
+    handleCallback
+#else
+[](const hw::CanMsg &msg) { UNUSED(msg); } 
+#endif
+);
+
+const hw::fdcan invcan(hfdcan3, 
+#ifndef USE_CHIMERA
+    handleCallback
+#else
+[](const hw::CanMsg &msg) { UNUSED(msg); } 
+#endif
+);
 
 const hw::fdcan &hw::fdcan_getHandle(const FDCAN_HandleTypeDef *hfdcan)
 {
--- firmware/hexray/VC/src/io/io_canReroutes.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "hw_can.hpp"
-#include "io_canReroute.hpp"
-
-namespace io::canReroute
-{
-void handleCallback(const hw::CanMsg &hw_rx_msg);
-}
