61aa043b9f2bb5ac716392229978ba3f63710957
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-05-03 20:22:08 -0700

    ugh

--- firmware/hexray/CRIT/src/app/app_switches.cpp
+++ firmware/hexray/CRIT/src/app/app_switches.cpp
@@ -2,46 +2,47 @@
 #include "app_canUtils.hpp"
 #include "app_switches.hpp"
 #include "io_switches.hpp"
-#include "io_leds.hpp"
+#include "app_signal.hpp"
 
 using namespace app::can_utils;
 
 namespace app::switches
 {
 static constexpr uint32_t DEBOUNCE_TIME = 10U;
-
-static Signal tv_signal(DEBOUNCE_TIME, DEBOUNCE_TIME);
-static Signal launch_signal(DEBOUNCE_TIME, DEBOUNCE_TIME);
-static Signal regen_signal(DEBOUNCE_TIME, DEBOUNCE_TIME);
-static Signal start_signal(DEBOUNCE_TIME, DEBOUNCE_TIME);
-
-bool getDebouncedstate(const bool raw, Signal &signal)
+namespace
 {
-    const Signal::SignalState state = signal.get_updated_state(raw, !raw);
-    return (state == Signal::SignalState::ACTIVE) ? true : false;
-}
+    bool getDebouncedstate(const bool raw, Signal &signal)
+    {
+        const Signal::SignalState state = signal.get_updated_state(raw, !raw);
+        return state == Signal::SignalState::ACTIVE;
+    }
 
-bool torque_vectoring_get()
-{
-    return getDebouncedstate(io::switches::torque_vectoring_get(), tv_signal);
-}
+    bool torque_vectoring_get()
+    {
+        static Signal tv_signal(DEBOUNCE_TIME, DEBOUNCE_TIME);
+        return getDebouncedstate(io::switches::torque_vectoring_get(), tv_signal);
+    }
 
-bool launch_control_get()
-{
-    return getDebouncedstate(io::switches::launch_control_get(), launch_signal);
-}
+    bool launch_control_get()
+    {
+        static Signal launch_signal(DEBOUNCE_TIME, DEBOUNCE_TIME);
+        return getDebouncedstate(io::switches::launch_control_get(), launch_signal);
+    }
 
-bool regen_get()
-{
-    return getDebouncedstate(io::switches::regen_get(), regen_signal);
-}
+    bool regen_get()
+    {
+        static Signal regen_signal(DEBOUNCE_TIME, DEBOUNCE_TIME);
+        return getDebouncedstate(io::switches::regen_get(), regen_signal);
+    }
 
-bool start_get()
-{
-    return getDebouncedstate(io::switches::start_get(), start_signal);
-}
+    bool start_get()
+    {
+        static Signal start_signal(DEBOUNCE_TIME, DEBOUNCE_TIME);
+        return getDebouncedstate(io::switches::start_get(), start_signal);
+    }
+} // namespace
 
-void broadcast(void)
+void broadcast()
 {
     // update the state from the switches
     app::can_tx::CRIT_TorqueVectoringSwitch_set(static_cast<SwitchState>(torque_vectoring_get()));
--- firmware/hexray/CRIT/src/app/app_switches.hpp
+++ firmware/hexray/CRIT/src/app/app_switches.hpp
@@ -1,13 +1,6 @@
 #pragma once
 
-#include "app_signal.hpp"
-
 namespace app::switches
 {
-bool torque_vectoring_get();
-bool launch_control_get();
-bool regen_get();
-bool start_get();
-
 void broadcast();
 } // namespace app::switches
