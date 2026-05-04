db43fa6bb0fc107b3ff2557c4318ab679fd1e17a
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-05-04 13:53:34 -0700

    lmao

--- firmware/hexray/VC/src/app/app_shdnLast.cpp
+++ firmware/hexray/VC/src/app/app_shdnLast.cpp
@@ -1,7 +1,6 @@
 #include "app_shdnLast.hpp"
 #include "app_canTx.hpp"
 #include "app_canRx.hpp"
-#include "io_shdnLoopNode.hpp"
 #include "app_canUtils.hpp"
 
 namespace app::shdnLast
@@ -11,36 +10,36 @@ static ShutdownNode get_first_shutdown()
 {
     // The shutdowns in the accumulator
     if (!app::can_rx::BMS_BmsLatchOk_get())
-        return ShutdownNode::SHDN_BMS_OK;
+        return ShutdownNode::BMS_OK;
     if (!app::can_rx::BMS_ImdLatchOk_get())
-        return ShutdownNode::SHDN_IMD_OK;
+        return ShutdownNode::IMD_OK;
     if (!app::can_rx::BMS_BspdLatchOk_get())
-        return ShutdownNode::SHDN_BSPD_OK;
+        return ShutdownNode::BSPD_OK;
     if (!app::can_rx::BMS_HVPShdnOKStatus_get())
-        return ShutdownNode::SHDN_HV_P_ILCK;
+        return ShutdownNode::HV_P_ILCK;
     if (!app::can_rx::BMS_HVNShdnOKStatus_get())
-        return ShutdownNode::SHDN_HV_N_ILCK;
+        return ShutdownNode::HV_N_ILCK;
     if (!app::can_tx::VC_MSDOrEMeterOKStatus_get())
-        return ShutdownNode::SHDN_MSD_EMETER_ILCK;
+        return ShutdownNode::MSD_EMETER_ILCK;
     if (!app::can_rx::RSM_RearLeftMotorInterlock_get())
-        return ShutdownNode::SHDN_RL_ILCK;
+        return ShutdownNode::RL_ILCK;
     if (!app::can_tx::VC_RearRightMotorInterlock_get())
-        return ShutdownNode::SHDN_RR_ILCK;
+        return ShutdownNode::RR_ILCK;
     if (!app::can_rx::DAM_REStopOKStatus_get())
-        return ShutdownNode::SHDN_R_EStop;
+        return ShutdownNode::R_EStop;
     if (!app::can_rx::DAM_LEStopOKStatus_get())
-        return ShutdownNode::SHDN_L_EStop;
+        return ShutdownNode::L_EStop;
     if (!app::can_rx::FSM_COCKPITOKStatus_get())
-        return ShutdownNode::SHDN_Cockpit_EStop;
+        return ShutdownNode::Cockpit_EStop;
     if (!app::can_rx::FSM_BOTSOKStatus_get())
-        return ShutdownNode::SHDN_BOTS;
+        return ShutdownNode::BOTS;
     if (!app::can_rx::FSM_FrontLeftILCKInertiaOKStatus_get())
-        return ShutdownNode::SHDN_FL_INERTIA_ILCK;
+        return ShutdownNode::FL_INERTIA_ILCK;
     if (!app::can_rx::FSM_FrontRightILCKOKStatus_get())
-        return ShutdownNode::SHDN_FR_ILCK;
+        return ShutdownNode::FR_ILCK;
     if (!app::can_tx::VC_TSMSOKStatus_get())
-        return ShutdownNode::SHDN_TSMS;
-    return ShutdownNode::SHDN_OK;
+        return ShutdownNode::TSMS;
+    return ShutdownNode::OK;
 }
 
 void broadcast()
