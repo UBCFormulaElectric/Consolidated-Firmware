7bbcabc14e36a274549eb639f6664ce6e0924012
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-05-04 11:47:36 -0700

    small fix

--- firmware/hexray/CRIT/src/app/app_switches.cpp
+++ firmware/hexray/CRIT/src/app/app_switches.cpp
@@ -16,31 +16,31 @@ namespace
         const Signal::SignalState state = signal.get_updated_state(raw, !raw);
         return state == Signal::SignalState::ACTIVE;
     }
-
-    bool torque_vectoring_get()
-    {
-        static Signal tv_signal(DEBOUNCE_TIME, DEBOUNCE_TIME);
-        return getDebouncedstate(io::switches::torque_vectoring_get(), tv_signal);
-    }
-
-    bool launch_control_get()
-    {
-        static Signal launch_signal(DEBOUNCE_TIME, DEBOUNCE_TIME);
-        return getDebouncedstate(io::switches::launch_control_get(), launch_signal);
-    }
-
-    bool regen_get()
-    {
-        static Signal regen_signal(DEBOUNCE_TIME, DEBOUNCE_TIME);
-        return getDebouncedstate(io::switches::regen_get(), regen_signal);
-    }
-
-    bool start_get()
-    {
-        static Signal start_signal(DEBOUNCE_TIME, DEBOUNCE_TIME);
-        return getDebouncedstate(io::switches::start_get(), start_signal);
-    }
 } // namespace
+
+bool torque_vectoring_get()
+{
+    static Signal tv_signal(DEBOUNCE_TIME, DEBOUNCE_TIME);
+    return getDebouncedstate(io::switches::torque_vectoring_get(), tv_signal);
+}
+
+bool launch_control_get()
+{
+    static Signal launch_signal(DEBOUNCE_TIME, DEBOUNCE_TIME);
+    return getDebouncedstate(io::switches::launch_control_get(), launch_signal);
+}
+
+bool regen_get()
+{
+    static Signal regen_signal(DEBOUNCE_TIME, DEBOUNCE_TIME);
+    return getDebouncedstate(io::switches::regen_get(), regen_signal);
+}
+
+bool start_get()
+{
+    static Signal start_signal(DEBOUNCE_TIME, DEBOUNCE_TIME);
+    return getDebouncedstate(io::switches::start_get(), start_signal);
+}
 
 void broadcast()
 {
--- firmware/hexray/CRIT/src/app/app_switches.hpp
+++ firmware/hexray/CRIT/src/app/app_switches.hpp
@@ -2,5 +2,10 @@
 
 namespace app::switches
 {
+bool torque_vectoring_get();
+bool launch_control_get();
+bool regen_get();
+bool start_get();
+
 void broadcast();
 } // namespace app::switches
