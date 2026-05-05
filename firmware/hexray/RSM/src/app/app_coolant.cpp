37f47e9cfe36cbc8b436d477d939e36598961800
acp02 <aujames430@gmail.com>
2026-05-05 02:24:53 -0700

    rangecheck W

--- firmware/hexray/RSM/src/app/app_coolant.cpp
+++ firmware/hexray/RSM/src/app/app_coolant.cpp
@@ -1,10 +1,7 @@
 #include "app_coolant.hpp"
 #include "app_canTx.hpp"
 #include "app_canAlerts.hpp"
-extern "C"
-{
-#include "app_rangeCheck.h"
-}
+#include "app_rangeCheck.hpp"
 #include "app_signal.hpp"
 
 #include "io_coolant.hpp"
@@ -12,19 +9,19 @@ extern "C"
 static constexpr float MIN_FLOW_RATE_L_PER_MIN = 1.0f;
 static constexpr float MAX_FLOW_RATE_L_PER_MIN = 30.0f;
 
-static constexpr uint32_t   FLOW_METER_TIME_TO_FAULT = 1000U;
-static constexpr uint32_t   FLOW_METER_TIME_TO_CLEAR = 1000U;
-static constexpr RangeCheck flow_rate_in_range_check = { nullptr, MIN_FLOW_RATE_L_PER_MIN, MAX_FLOW_RATE_L_PER_MIN };
+static constexpr uint32_t FLOW_METER_TIME_TO_FAULT = 1000U;
+static constexpr uint32_t FLOW_METER_TIME_TO_CLEAR = 1000U;
 
 namespace app::coolant
 {
-static Signal flow_in_range_signal(FLOW_METER_TIME_TO_FAULT, FLOW_METER_TIME_TO_CLEAR);
+static Signal               flow_in_range_signal(FLOW_METER_TIME_TO_FAULT, FLOW_METER_TIME_TO_CLEAR);
+static constexpr RangeCheck flow_rate_in_range_check = RangeCheck(MIN_FLOW_RATE_L_PER_MIN, MAX_FLOW_RATE_L_PER_MIN);
 
 void broadcast()
 {
-    const float flow_val               = io::coolant::getFlowRate();
-    const auto [status, clamped_value] = app_rangeCheck_getValue(&flow_rate_in_range_check, flow_val);
-    can_tx::RSM_CoolantFlowRate_set(flow_val);
-    can_alerts::infos::FlowRateOutOfRange_set(status != VALUE_IN_RANGE);
+    const float flow_val = io::coolant::getFlowRate();
+    can_tx::RSM_CoolantFlowRate_set(flow_rate_in_range_check.get_clamped_value(flow_val));
+    can_alerts::infos::FlowRateOutOfRange_set(
+        flow_rate_in_range_check.get_status(flow_val) != RangeCheck::Status::VALUE_IN_RANGE);
 }
 } // namespace app::coolant
--- firmware/shared/srcpp/app/app_rangeCheck.cpp
+++
@@ -1,6 +0,0 @@
-#include "app_rangeCheck.hpp"
-
-namespace app::range 
-{
-
-}
