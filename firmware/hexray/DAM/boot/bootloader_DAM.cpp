ecc43bce2f1e3d776100314dd3f3c727132a54a3
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-05-06 15:57:42 -0700

    how did i forgor

--- firmware/hexray/DAM/boot/bootloader_DAM.cpp
+++ firmware/hexray/DAM/boot/bootloader_DAM.cpp
@@ -41,21 +41,34 @@ CFUNC void bootloader_preInit()
     bootloader::preInit();
 }
 
-static hw::rtos::StaticTask<1024>
-    bootInterfaceTask(osPriorityRealtime, "BootIntf", [](void *) { bootloader::runInterfaceTask(dam_boot_config); });
-static hw::rtos::StaticTask<1024>
-    bootTickTask(osPriorityRealtime, "BootTick", [](void *) { bootloader::runTickTask(dam_boot_config); });
-static hw::rtos::StaticTask<1024>
-    bootCanTxTask(osPriorityRealtime, "BootCanTx", [](void *) { bootloader::runCanTxTask(dam_boot_config); });
+static hw::rtos::StaticTask::StaticTaskStack<1024> bootInterfaceStack;
+static hw::rtos::StaticTask::StaticTaskStack<1024> bootTickStack;
+static hw::rtos::StaticTask::StaticTaskStack<1024> bootCanTxStack;
+
+static hw::rtos::StaticTask bootInterfaceTask(
+    osPriorityRealtime,
+    "BootIntf",
+    [](void *) { bootloader::runInterfaceTask(dam_boot_config); },
+    bootInterfaceStack);
+static hw::rtos::StaticTask bootTickTask(
+    osPriorityRealtime,
+    "BootTick",
+    [](void *) { bootloader::runTickTask(dam_boot_config); },
+    bootTickStack);
+static hw::rtos::StaticTask bootCanTxTask(
+    osPriorityRealtime,
+    "BootCanTx",
+    [](void *) { bootloader::runCanTxTask(dam_boot_config); },
+    bootCanTxStack);
 
 [[noreturn]] void bootloader_init()
 {
     HAL_GPIO_WritePin(BOOT_LED_GPIO_Port, BOOT_LED_Pin, GPIO_PIN_SET);
     osKernelInitialize();
     bootloader::init(dam_boot_config);
-    bootInterfaceTask.start();
-    bootTickTask.start();
-    bootCanTxTask.start();
+    UNUSED(bootInterfaceTask.start());
+    UNUSED(bootTickTask.start());
+    UNUSED(bootCanTxTask.start());
     osKernelStart();
     forever{};
 }
--- firmware/hexray/VC/boot/bootloader_vc.cpp
+++ firmware/hexray/VC/boot/bootloader_vc.cpp
@@ -41,21 +41,34 @@ CFUNC void bootloader_preinit()
     bootloader::preInit();
 }
 
-static hw::rtos::StaticTask<1024>
-    bootInterfaceTask(osPriorityRealtime, "BootIntf", [](void *) { bootloader::runInterfaceTask(vcboot_config); });
-static hw::rtos::StaticTask<1024>
-    bootTickTask(osPriorityRealtime, "BootTick", [](void *) { bootloader::runTickTask(vcboot_config); });
-static hw::rtos::StaticTask<1024>
-    bootCanTxTask(osPriorityRealtime, "BootCanTx", [](void *) { bootloader::runCanTxTask(vcboot_config); });
+static hw::rtos::StaticTask::StaticTaskStack<1024> bootInterfaceStack;
+static hw::rtos::StaticTask::StaticTaskStack<1024> bootTickStack;
+static hw::rtos::StaticTask::StaticTaskStack<1024> bootCanTxStack;
+
+static hw::rtos::StaticTask bootInterfaceTask(
+    osPriorityRealtime,
+    "BootIntf",
+    [](void *) { bootloader::runInterfaceTask(vcboot_config); },
+    bootInterfaceStack);
+static hw::rtos::StaticTask bootTickTask(
+    osPriorityRealtime,
+    "BootTick",
+    [](void *) { bootloader::runTickTask(vcboot_config); },
+    bootTickStack);
+static hw::rtos::StaticTask bootCanTxTask(
+    osPriorityRealtime,
+    "BootCanTx",
+    [](void *) { bootloader::runCanTxTask(vcboot_config); },
+    bootCanTxStack);
 
 [[noreturn]] void bootloader_init()
 {
     HAL_GPIO_WritePin(BOOT_GPIO_Port, BOOT_Pin, GPIO_PIN_SET);
     osKernelInitialize();
     bootloader::init(vcboot_config);
-    bootInterfaceTask.start();
-    bootTickTask.start();
-    bootCanTxTask.start();
+    UNUSED(bootInterfaceTask.start());
+    UNUSED(bootTickTask.start());
+    UNUSED(bootCanTxTask.start());
     osKernelStart();
     forever {}
 }
