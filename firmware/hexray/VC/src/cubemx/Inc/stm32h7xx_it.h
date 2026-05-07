0ba40658103ff88a706626b56fb7711a74a27e51
Rijwal Sangey <rijwalsangey@gmail.com>
2026-05-06 21:13:07 -0700

    changes

--- firmware/hexray/VC/src/cubemx/Inc/stm32h7xx_it.h
+++ firmware/hexray/VC/src/cubemx/Inc/stm32h7xx_it.h
@@ -53,8 +53,6 @@ extern "C"
     void BusFault_Handler(void);
     void UsageFault_Handler(void);
     void DebugMon_Handler(void);
-    void DMA1_Stream0_IRQHandler(void);
-    void DMA1_Stream1_IRQHandler(void);
     void FDCAN1_IT0_IRQHandler(void);
     void FDCAN1_IT1_IRQHandler(void);
     void TIM6_DAC_IRQHandler(void);
--- firmware/hexray/VC/src/cubemx/Src/stm32h7xx_it.c
+++ firmware/hexray/VC/src/cubemx/Src/stm32h7xx_it.c
@@ -165,32 +165,6 @@ void DebugMon_Handler(void)
 /******************************************************************************/
 
 /**
- * @brief This function handles DMA1 stream0 global interrupt.
- */
-void DMA1_Stream0_IRQHandler(void)
-{
-    /* USER CODE BEGIN DMA1_Stream0_IRQn 0 */
-
-    /* USER CODE END DMA1_Stream0_IRQn 0 */
-    /* USER CODE BEGIN DMA1_Stream0_IRQn 1 */
-
-    /* USER CODE END DMA1_Stream0_IRQn 1 */
-}
-
-/**
- * @brief This function handles DMA1 stream1 global interrupt.
- */
-void DMA1_Stream1_IRQHandler(void)
-{
-    /* USER CODE BEGIN DMA1_Stream1_IRQn 0 */
-
-    /* USER CODE END DMA1_Stream1_IRQn 0 */
-    /* USER CODE BEGIN DMA1_Stream1_IRQn 1 */
-
-    /* USER CODE END DMA1_Stream1_IRQn 1 */
-}
-
-/**
  * @brief This function handles FDCAN1 interrupt 0.
  */
 void FDCAN1_IT0_IRQHandler(void)
