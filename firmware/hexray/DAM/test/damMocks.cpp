5caab106a89294998a20670e02b96cb54d441c00
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-05-08 21:50:54 -0700

    L mocks

--- firmware/hexray/DAM/test/damMocks.cpp
+++ firmware/hexray/DAM/test/damMocks.cpp
@@ -4,16 +4,17 @@ io::queue<io::CanMsg, 128> can_tx_queue{ "" };
 io::queue<io::CanMsg, 128> can_rx_queue{ "" };
 
 #include "io_filesystem.hpp"
-std::expected<uint32_t, io::FileSystemError> io::FileSystem::open(const char *path)
+std::expected<uint32_t, io::FileSystem::FileSystemError> io::FileSystem::open(const char *path)
 {
     return {};
 }
-std::expected<void, io::FileSystemError>
-    io::FileSystem::readMetadata(uint32_t fd, std::span<uint8_t> buf, size_t size, uint32_t &num_read)
+std::expected<void, io::FileSystem::FileSystemError>
+    io::FileSystem::readMetadata(uint32_t fd, std::span<uint8_t> buf, uint32_t &num_read)
 {
     return {};
 }
-std::expected<void, io::FileSystemError> io::FileSystem::writeMetadata(uint32_t fd, std::span<uint8_t> buf, size_t size)
+std::expected<void, io::FileSystem::FileSystemError>
+    io::FileSystem::writeMetadata(uint32_t fd, std::span<const uint8_t> buf)
 {
     return {};
 }
