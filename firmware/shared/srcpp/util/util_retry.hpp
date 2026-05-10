3f587c36bbab1107b56f9fcb94e34089101869e2
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-05-09 19:48:20 -0700

    lmao

--- firmware/shared/srcpp/util/util_retry.hpp
+++ firmware/shared/srcpp/util/util_retry.hpp
@@ -14,9 +14,25 @@ template <typename T, typename E> struct expected_traits<std::expected<T, E>>
     using error_type = E;
 };
 
-template <typename T, typename E>
-std::expected<T, E> retry(std::function<std::expected<T, E>()> k, const uint32_t retry_count)
+// template <typename T, typename E>
+// std::expected<T, E> retry(std::function<std::expected<T, E>()> k, const uint32_t retry_count)
+// {
+// }
+//
+// template <typename E>
+// std::expected<void, E> retry(std::function<std::expected<void, E>()> k, const uint32_t retry_count)
+// {
+//     return retry<void, E>(k, retry_count);
+// }
+
+// Overload that deduces T and E from callable return type
+template <typename Callable> auto retry(Callable &&k, const uint32_t retry_count) -> decltype(k())
 {
+    using result_type = decltype(k());
+    using traits      = expected_traits<result_type>;
+    using T           = traits::value_type;
+    using E           = traits::error_type;
+
     std::expected<T, E> result;
     for (uint32_t i = 0; i < retry_count; i++)
     {
@@ -27,22 +43,5 @@ std::expected<T, E> retry(std::function<std::expected<T, E>()> k, const uint32_t
         }
     }
     return result;
-}
-
-template <typename E>
-std::expected<void, E> retry(std::function<std::expected<void, E>()> k, const uint32_t retry_count)
-{
-    return retry<void, E>(k, retry_count);
-}
-
-// Overload that deduces T and E from callable return type
-template <typename Callable> auto retry(Callable &&k, const uint32_t retry_count) -> decltype(k())
-{
-    using result_type = decltype(k());
-    using traits      = expected_traits<result_type>;
-    using T           = traits::value_type;
-    using E           = traits::error_type;
-
-    return retry<T, E>(std::function<result_type()>(std::forward<Callable>(k)), retry_count);
 }
 } // namespace util
