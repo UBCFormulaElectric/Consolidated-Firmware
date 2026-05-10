6c9ecb402183c9e6002970c34e01c8dca714d8d7
emily song <108087950+emilysxng@users.noreply.github.com>
2026-05-09 18:43:43 -0700

    DAM IO SD Card Bringup (#1963)
    https://ubcformulaelectric.atlassian.net/jira/software/c/projects/FIRM/boards/37/backlog?selectedIssue=FIRM-570
    
    ---------
    
    Co-authored-by: Edwin <20777515+Lucien950@users.noreply.github.com>

---
+++ firmware/shared/srcpp/io/io_filesystem.hpp
@@ -0,0 +1,153 @@
+#pragma once
+
+#include <array>
+#include <cstddef>
+#include <cstdint>
+#include <cstring>
+#include <expected>
+#include <span>
+
+#ifdef TARGET_EMBEDDED
+#include "hw_sd.hpp"
+#include "logfs.h"
+#elif TARGET_TEST
+#include <unordered_map>
+#include <unordered_set>
+#include <fstream>
+#endif
+
+namespace io
+{
+
+class FileSystem
+{
+  public:
+    static constexpr int         NUM_MOUNT_ATTEMPTS = 3;
+    static constexpr int         RETRY_COUNT        = 3;
+    static constexpr uint32_t    MAX_WRITE_CYCLES   = 1000;
+    static constexpr std::size_t MAX_FILE_NUMBER    = 3;
+    enum class FileSystemError
+    {
+        OK            = 0,
+        ERROR         = -1, // general error
+        CORRUPTED     = -2, // file system corrupted
+        NOT_FOUND     = -3, // file not found
+        NO_SPACE      = -4, // no space left
+        ERROR_IO      = -5, // io error
+        ERROR_BAD_ARG = -6, // Invalid argument passed in
+        MOUNT_FAILED  = -7  // Initial mount failed so filesystem is unavailable for this boot cycle
+    };
+
+#ifdef TARGET_EMBEDDED
+    /* Constructor*/
+    explicit FileSystem(const hw::SdCard &sd_card) : fs(), sd(sd_card) {}
+#else
+    /* Constructor for non-embedded targets, just to make testing easier. */
+    FileSystem() = default;
+#endif
+
+    /**
+     * init, config, and mount the file system.
+     * does not create the bootcount file
+     * @return FileSystemError status
+     */
+    std::expected<void, FileSystemError> init();
+
+    /**
+     * open a file and return the file descriptor
+     * Note: Don't try opening the same file twice!
+     * @param path path in question
+     * @return a file descriptor
+     */
+    std::expected<uint32_t, FileSystemError> open(const char *path);
+
+    /**
+     * reads from file descriptor for some size into the buffer
+     * @param fd file descriptor in question
+     * @param buf buffer in question
+     * @param size size in question
+     * @return FileSystemError status
+     */
+    std::expected<void, FileSystemError> read(uint32_t fd, std::span<uint8_t> buf, std::size_t size);
+
+    /**
+     * writes to file descriptor for some size into the buffer
+     * @param fd file descriptor in question
+     * @param buf buffer in question
+     * @param size size in question
+     * @return FileSystemError status
+     */
+    std::expected<void, FileSystemError> write(uint32_t fd, std::span<uint8_t> buf, std::size_t size);
+
+    /**
+     * Reads metadata from a file
+     * @param fd file descriptor
+     * @param buf destination buffer
+     * @param num_read actual bytes read
+     * @return FileSystemError status
+     */
+    std::expected<void, FileSystemError> readMetadata(uint32_t fd, std::span<uint8_t> buf, uint32_t &num_read);
+
+    /**
+     * Writes metadata to a file
+     * @param fd file descriptor
+     * @param buf source buffer
+     * @return FileSystemError status
+     */
+    std::expected<void, FileSystemError> writeMetadata(uint32_t fd, std::span<const uint8_t> buf);
+
+    /**
+     * Closes file given file descriptor
+     * @param fd file descriptor in question
+     * @return FileSystemError status
+     */
+    std::expected<void, FileSystemError> close(uint32_t fd);
+
+    /**
+     * Do the concrete write operation to the hardware
+     * @param fd on this file descriptor
+     * @return FileSystemError status
+     */
+    std::expected<void, FileSystemError> sync(uint32_t fd);
+
+  private:
+    bool mount_failed = false;
+
+#ifdef TARGET_EMBEDDED
+    static LogFsErr logfsCfgRead(const LogFsCfg *cfg, uint32_t block, void *buf);
+    static LogFsErr logfsCfgWrite(const LogFsCfg *cfg, uint32_t block, void *buf);
+    LogFsCfg        fs_cfg{
+               .context      = this,
+               .block_size   = HW_DEVICE_SECTOR_SIZE,
+               .block_count  = 1024 * 1024 * 15, // ~7.5 GB
+               .read         = logfsCfgRead,
+               .write        = logfsCfgWrite,
+               .cache        = cache.data(),
+               .write_cycles = MAX_WRITE_CYCLES,
+               .rd_only      = false,
+    };
+    LogFs fs;
+
+    const hw::SdCard                                                    &sd; // sd card instance
+    std::array<uint8_t, HW_DEVICE_SECTOR_SIZE>                           cache{};
+    std::array<LogFsFileCfg, MAX_FILE_NUMBER>                            files_cfg{};
+    std::array<LogFsFile, MAX_FILE_NUMBER>                               files{};
+    std::array<std::array<char, HW_DEVICE_SECTOR_SIZE>, MAX_FILE_NUMBER> files_cache{};
+#else
+    std::unordered_map<size_t, std::fstream> files;
+    std::unordered_set<std::string>          open_file_names;
+#endif
+    std::array<bool, MAX_FILE_NUMBER> files_opened{};
+
+    /**
+     * Checks the file descriptor to make sure it's valid
+     * @param fd file descriptor in question
+     */
+    bool checkFileDescriptor(const uint32_t fd) const { return fd < MAX_FILE_NUMBER and files_opened[fd]; }
+
+    /**
+     * Checks if the filesystem has been mounted successfully.
+     */
+    bool checkMount() const { return !mount_failed; }
+}; // class FileSystem
+} // namespace io
