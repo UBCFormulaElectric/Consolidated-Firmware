f8023344e8c071685bb7cfca2103571181a6436c
Kelm <lkevin2003@gmail.com>
2026-05-10 16:06:17 -0700

    rebase pull

--- firmware/hexray/BMS/src/jobs.cpp
+++ firmware/hexray/BMS/src/jobs.cpp
@@ -128,19 +128,19 @@ void jobs_run100Hz_tick()
     bms_ok_latch.setCurrentStatus(acc_fault ? FaultLatchState::FAULT : FaultLatchState::OK);
 
     // Update CAN signals for BMS latch statuses.
-    const bool imd_latch_curr_ok  = imd_ok_latch.getCurrentStatus() == FaultLatchState::OK;
-    const bool bspd_latch_curr_ok = bspd_ok_latch.getCurrentStatus() == FaultLatchState::OK;
-    const bool bms_latch_curr_ok  = bms_ok_latch.getCurrentStatus() == FaultLatchState::OK;
+    const bool imd_latched_status  = imd_ok_latch.getLatchedStatus() == FaultLatchState::OK;
+    const bool bspd_latched_status = bspd_ok_latch.getLatchedStatus() == FaultLatchState::OK;
+    const bool bms_latched_status  = bms_ok_latch.getLatchedStatus() == FaultLatchState::OK;
 
-    app::can_alerts::faults::ImdNotOk_set(not imd_latch_curr_ok);
-    app::can_alerts::faults::HardwareBspd_set(not bspd_latch_curr_ok);
+    app::can_alerts::faults::ImdLatched_set(not imd_latched_status);
+    app::can_alerts::faults::HardwareBspdLatched_set(not bspd_latched_status);
 
-    app::can_tx::BMS_BmsCurrentlyOk_set(bms_latch_curr_ok);
-    app::can_tx::BMS_ImdCurrentlyOk_set(imd_latch_curr_ok);
-    app::can_tx::BMS_BspdCurrentlyOk_set(bspd_latch_curr_ok);
-    app::can_tx::BMS_BmsLatchOk_set(bms_ok_latch.getLatchedStatus() == FaultLatchState::OK);
-    app::can_tx::BMS_ImdLatchOk_set(imd_ok_latch.getLatchedStatus() == FaultLatchState::OK);
-    app::can_tx::BMS_BspdLatchOk_set(bspd_ok_latch.getLatchedStatus() == FaultLatchState::OK);
+    app::can_tx::BMS_BmsCurrentlyOk_set(bms_ok_latch.getLatchedStatus() == FaultLatchState::OK);
+    app::can_tx::BMS_ImdCurrentlyOk_set(imd_ok_latch.getLatchedStatus() == FaultLatchState::OK);
+    app::can_tx::BMS_BspdCurrentlyOk_set(bspd_ok_latch.getLatchedStatus() == FaultLatchState::OK);
+    app::can_tx::BMS_BmsLatchOk_set(bms_latched_status);
+    app::can_tx::BMS_ImdLatchOk_set(imd_latched_status);
+    app::can_tx::BMS_BspdLatchOk_set(bspd_latched_status);
 
     app::can_tx::BMS_BSPDBrakePressureThresholdExceeded_set(io::bspdtest::isBrakePressureThresholdExceeded());
     app::can_tx::BMS_BSPDAccelBrakeOk_set(io::bspdtest::isAccelBrakeOk());
@@ -150,7 +150,7 @@ void jobs_run100Hz_tick()
     {
         app::StateMachine::set_next_state(&app::states::fault_state);
     }
-    if (ir_negative_opened_debounced && not app::can_alerts::AnyBoardHasFault())
+    else if (ir_negative_opened_debounced && not app::can_alerts::AnyBoardHasFault())
     {
         app::StateMachine::set_next_state(&app::states::init_state);
     }
