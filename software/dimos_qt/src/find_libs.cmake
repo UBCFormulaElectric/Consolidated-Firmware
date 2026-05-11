616da856a2f29b2284e1af66c63bb4041aa3b506
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-05-11 15:55:53 -0700

    onetap

--- software/dimos_qt/src/find_libs.cmake
+++ software/dimos_qt/src/find_libs.cmake
@@ -23,7 +23,6 @@ message("📚✔️ Added JsonCAN library")
 
 # commit_info
 commit_info_generate_sources(${EXECUTABLE_NAME} "${CMAKE_CURRENT_BINARY_DIR}/commit_info")
-add_library(dimos_commit_info STATIC ${COMMIT_INFO_SRC})
 target_include_directories(dimos_commit_info PUBLIC ${COMMIT_INFO_INCLUDE_DIR})
 list(APPEND REQUIRED_LIBRARIES "dimos_commit_info")
 message("📚✔️ Added commit_info library")
