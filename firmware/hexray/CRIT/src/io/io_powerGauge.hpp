5823d2c98ed3ad384f6601d1ec536c33ca65c4df
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-05-17 19:10:36 -0700

    powergague

--- firmware/hexray/CRIT/src/io/io_powerGauge.hpp
+++ firmware/hexray/CRIT/src/io/io_powerGauge.hpp
@@ -1,22 +1,37 @@
 #pragma once
 #include "util_errorCodes.hpp"
 
-#include <array>
+#include <span>
+#include <numeric>
 
 namespace io::power_gauge
 {
 inline constexpr std::size_t LEDS = 8;
 struct led_frame
 {
-    uint8_t _s : 3 = 0b111;
     uint8_t _brightness : 5;
-    uint8_t _r;
-    uint8_t _g;
+    uint8_t _s : 3 = 0b111;
     uint8_t _b;
+    uint8_t _g;
+    uint8_t _r;
     led_frame(const uint8_t brightness, const uint8_t r, const uint8_t g, const uint8_t b)
-      : _brightness(brightness & 0b11111), _r(r), _g(g), _b(b)
+      : _brightness(brightness & 0b11111), _b(b), _g(g), _r(r)
+    {
+    }
+
+    /**
+     *
+     * @param brightness 0-1
+     * @param r 0-1
+     * @param g 0-1
+     * @param b 0-1
+     */
+    led_frame(const double brightness, const uint8_t r, const uint8_t g, const uint8_t b)
+      : _brightness(static_cast<uint8_t>(brightness * 0b11111) & 0b11111), _b(b), _g(g), _r(r)
     {
     }
 };
-std::expected<void, ErrorCode> update(std::array<led_frame, LEDS> data);
+static_assert(sizeof(led_frame) == 4);
+
+std::expected<void, ErrorCode> update(std::span<led_frame, LEDS> data);
 } // namespace io::power_gauge
--- firmware/hexray/CRIT/src/jobs.cpp
+++ firmware/hexray/CRIT/src/jobs.cpp
@@ -8,12 +8,16 @@
 #include "screens/app_screens.hpp"
 #include "app_heartbeatMonitors.hpp"
 #include "app_brightness.hpp"
+#include "app_rgbhsv.hpp"
 
 #include "io_canTx.hpp"
 #include "io_time.hpp"
 #include "io_canQueues.hpp"
 #include "io_leds.hpp"
+#include "io_powerGauge.hpp"
 #include "io_sevenSeg.hpp"
+
+#include <cmath>
 
 void jobs_init()
 {
@@ -45,7 +49,22 @@ void jobs_run100Hz_tick()
     app::leds::setLeds();
     app::screens::tick();
 
-    // io::power_gauge::update({});
+    const double h = std::fmod(static_cast<double>(io::time::getCurrentMs()) / 10, 360);
+    auto [r, g, b] = app::rgb::from({ h, 1.0, 0.5 });
+    const auto r_8 = static_cast<uint8_t>(r * static_cast<double>(std::numeric_limits<uint8_t>::max()));
+    const auto g_8 = static_cast<uint8_t>(g * static_cast<double>(std::numeric_limits<uint8_t>::max()));
+    const auto b_8 = static_cast<uint8_t>(b * static_cast<double>(std::numeric_limits<uint8_t>::max()));
+    std::array<io::power_gauge::led_frame, io::power_gauge::LEDS> data{ {
+        { static_cast<double>(app::brightness) / 100, r_8, g_8, b_8 },
+        { static_cast<double>(app::brightness) / 100, r_8, g_8, b_8 },
+        { static_cast<double>(app::brightness) / 100, r_8, g_8, b_8 },
+        { static_cast<double>(app::brightness) / 100, r_8, g_8, b_8 },
+        { static_cast<double>(app::brightness) / 100, r_8, g_8, b_8 },
+        { static_cast<double>(app::brightness) / 100, r_8, g_8, b_8 },
+        { static_cast<double>(app::brightness) / 100, r_8, g_8, b_8 },
+        { static_cast<double>(app::brightness) / 100, r_8, g_8, b_8 },
+    } };
+    LOG_IF_ERR(io::power_gauge::update(data));
 
     app::switches::broadcast();
     // app::driveModes::broadcast();
