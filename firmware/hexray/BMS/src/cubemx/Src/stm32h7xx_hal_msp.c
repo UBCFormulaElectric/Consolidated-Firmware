89a8633db8474097fe7dc0bb1c94fa01b049e4b7
Kelm <lkevin2003@gmail.com>
2026-05-20 00:31:05 -0700

    fixing stm configs. active pwm works :))

--- firmware/hexray/BMS/src/cubemx/Src/stm32h7xx_hal_msp.c
+++ firmware/hexray/BMS/src/cubemx/Src/stm32h7xx_hal_msp.c
@@ -630,6 +630,8 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim_base)
         HAL_GPIO_Init(IMD_M_HS_3V3_GPIO_Port, &GPIO_InitStruct);
 
         /* TIM1 interrupt Init */
+        HAL_NVIC_SetPriority(TIM1_UP_IRQn, 5, 0);
+        HAL_NVIC_EnableIRQ(TIM1_UP_IRQn);
         HAL_NVIC_SetPriority(TIM1_CC_IRQn, 5, 0);
         HAL_NVIC_EnableIRQ(TIM1_CC_IRQn);
         /* USER CODE BEGIN TIM1_MspInit 1 */
@@ -714,6 +716,7 @@ void HAL_TIM_Base_MspDeInit(TIM_HandleTypeDef *htim_base)
         HAL_GPIO_DeInit(IMD_M_HS_3V3_GPIO_Port, IMD_M_HS_3V3_Pin);
 
         /* TIM1 interrupt DeInit */
+        HAL_NVIC_DisableIRQ(TIM1_UP_IRQn);
         HAL_NVIC_DisableIRQ(TIM1_CC_IRQn);
         /* USER CODE BEGIN TIM1_MspDeInit 1 */
 
