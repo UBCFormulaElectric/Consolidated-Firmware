5aa00572b4e1b9d853a828ebdf40652849b23cf8
Rijwal Sangey <rijwalsangey@gmail.com>
2026-05-30 16:39:43 -0700

    can this work

--- firmware/hexray/VC/test/vc_fakes.cpp
+++ firmware/hexray/VC/test/vc_fakes.cpp
@@ -1,6 +1,8 @@
 #include "vc_fakes.hpp"
 #include "app_canTx.hpp"
 #include "io_imus.hpp"
+#include "io_batteryCharging.hpp"
+#include "io_batteryMonitoring.hpp"
 #include "io_pumpControl.hpp"
 #include "io_sbgEllipse.hpp"
 #include "io_powerMonitoring.hpp"
@@ -32,6 +34,93 @@ const io::shdn::node rear_right_motor_interlock_node(false, app::can_tx::VC_Rear
 const io::shdn::node splitter_box_interlock_node(false, app::can_tx::VC_MSDOrEMeterOKStatus_set);
 namespace io // Define the mocked functions here
 {
+namespace batteryMonitoring
+{
+    result<void> init()
+    {
+        return {};
+    }
+    result<float> get_voltage_cell(CellReading)
+    {
+        return 0.0f;
+    }
+    result<float> get_voltage_system(SystemReading)
+    {
+        return 0.0f;
+    }
+    result<float> get_current()
+    {
+        return 0.0f;
+    }
+    result<uint64_t> get_integrated_charge()
+    {
+        return 0ULL;
+    }
+    result<float> get_temperatureIC()
+    {
+        return 0.0f;
+    }
+    result<SafetyStatusA> get_safety_alert_a()
+    {
+        return SafetyStatusA{ .raw_status = 0 };
+    }
+    result<SafetyStatusB> get_safety_alert_b()
+    {
+        return SafetyStatusB{ .raw_status = 0 };
+    }
+    result<void> send_balancing_subcommand(CellBalance_BitMask)
+    {
+        return {};
+    }
+    result<void> stop_balancing_subcommand()
+    {
+        return {};
+    }
+    result<uint16_t> read_balancing_subcommand()
+    {
+        return 0U;
+    }
+    result<uint16_t> balacing_time_elapsed()
+    {
+        return 0U;
+    }
+    result<std::array<uint16_t, 5>> get_voltage_OV(uint16_t)
+    {
+        return std::array<uint16_t, 5>{};
+    }
+    result<std::array<uint16_t, 5>> get_voltage_UV(uint16_t)
+    {
+        return std::array<uint16_t, 5>{};
+    }
+    void alert_handler()
+    {
+    }
+    bool consume_alert_pending()
+    {
+        return false;
+    }
+    result<AlertStatus> read_alarm_status()
+    {
+        return AlertStatus{ .raw_value = 0 };
+    }
+    result<int16_t> read_currentcc1()
+    {
+        return 0;
+    }
+} // namespace batteryMonitoring
+namespace batteryCharging
+{
+    void charger_enable(void)
+    {
+    }
+    void charger_disable(void)
+    {
+    }
+    bool check_status(void)
+    {
+        return false;
+    }
+} // namespace batteryCharging
 namespace imus
 {
     result<void> initAll()
