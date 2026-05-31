833157126b5116bacb12554469454249d1be5082
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2026-05-30 17:07:48 -0700

    BMS Shdn Node

--- firmware/hexray/BMS/src/io/io_bmsShdn.cpp
+++ firmware/hexray/BMS/src/io/io_bmsShdn.cpp
@@ -5,4 +5,5 @@ namespace io::shdn
 {
 node hv_p_ok_node(hv_p_intlck_sns, app::can_tx::BMS_HVPShdnOKStatus_set);
 node hv_n_ok_node(hv_n_intlck_sns, app::can_tx::BMS_HVNShdnOKStatus_set);
+node msd_shdn_ok_node(msd_shdn_sns, app::can_tx::BMS_MSDShdnOKStatus_set);
 } // namespace io::shdn
--- firmware/hexray/BMS/src/io/io_bmsShdn.hpp
+++ firmware/hexray/BMS/src/io/io_bmsShdn.hpp
@@ -5,4 +5,5 @@ namespace io::shdn
 {
 extern node hv_p_ok_node;
 extern node hv_n_ok_node;
+extern node msd_shdn_ok_node;
 } // namespace io::shdn
