4b26a482d93afdd8eb8ac254b866a93c5c13f1c4
Kevin <63944867+KelmLelm@users.noreply.github.com>
2026-06-01 18:03:03 -0700

    PWM isActive Hotfix (#2017)
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    - Changed to add a minimum expected freq for hw pwm class 
    - Changed pwm class to include support for timers configured with slave
    reset mode.
    - Added callbacks for timer overflows and a counter for counting consecutive arr overflow events to detect pwm signal not active.
    - **NOTE** RSM pwm needs to be tested.
    build.
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    - Tested with IMD10Hz signal and was able to get correct frequency.
    - Same test setup was able to get a duty cycle value, not able to verify
    if it is correct.
    - After more adjustments, tested with EVSE signal. Can support 1khz
    ### Resolved Tickets
    <!-- Link any tickets that this PR resolves. -->
    [FIRM-640](https://ubcformulaelectric.atlassian.net/browse/FIRM-640)
    
    [FIRM-640]:
    https://ubcformulaelectric.atlassian.net/browse/FIRM-640?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    [FIRM-660](https://ubcformulaelectric.atlassian.net/browse/FIRM-660)
    
    ---------
    
    Co-authored-by: Edwin <20777515+Lucien950@users.noreply.github.com>

--- firmware/hexray/RSM/src/hw/hw_pwms.cpp
+++ firmware/hexray/RSM/src/hw/hw_pwms.cpp
@@ -5,10 +5,13 @@
 // constexpr uint32_t TIM1_FREQUENCY = 100;
 // constexpr uint16_t TIM1_PRESCALER = 64;
 // constexpr uint16_t TIM1_ARR = 1000;
+constexpr bool TIMER_RESET_MODE  = false;
+constexpr bool MIN_EXPECTED_FREQ = 10U;
 
-constexpr hw::PwmInput flow_meter_config{ htim1,          HAL_TIM_ACTIVE_CHANNEL_1,
-                                          TIM1_FREQUENCY, TIM_CHANNEL_1,
-                                          TIM_CHANNEL_2,  TIM1_ARR };
+constexpr hw::PwmInput flow_meter_config{
+    htim1,    HAL_TIM_ACTIVE_CHANNEL_1, TIM1_FREQUENCY,   TIM_CHANNEL_1, TIM_CHANNEL_2,
+    TIM1_ARR, TIMER_RESET_MODE,         MIN_EXPECTED_FREQ
+};
 
 extern "C"
 {
