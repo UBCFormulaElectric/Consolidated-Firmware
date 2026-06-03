9e767d765578959ca57f04b57913146cc477039e
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-06-03 13:55:20 -0700

    yoosh

--- firmware/hexray/VC/src/app/app_heartbeatMonitors.cpp
+++ firmware/hexray/VC/src/app/app_heartbeatMonitors.cpp
@@ -4,42 +4,42 @@
 #include "app_canTx.hpp"
 
 static io::heartbeat::node bms_hb{ app::can_rx::BMS_Heartbeat_get, app::can_rx::BMS_Heartbeat_update,
-                                   [](const bool b)
+                                   [](const bool fault)
                                    {
-                                       app::can_alerts::warnings::MissingBMSHeartbeat_set(b);
-                                       if (not b)
+                                       app::can_alerts::warnings::MissingBMSHeartbeat_set(fault);
+                                       if (fault)
                                            app::can_rx::clear_board_rx_table(app::can_utils::CanNode::BMS_NODE);
                                    } };
 
 static io::heartbeat::node rsm_hb{ app::can_rx::RSM_Heartbeat_get, app::can_rx::RSM_Heartbeat_update,
-                                   [](const bool b)
+                                   [](const bool fault)
                                    {
-                                       app::can_alerts::infos::MissingRSMHeartbeat_set(b);
-                                       if (not b)
+                                       app::can_alerts::infos::MissingRSMHeartbeat_set(fault);
+                                       if (fault)
                                            app::can_rx::clear_board_rx_table(app::can_utils::CanNode::RSM_NODE);
                                    } };
 
 static io::heartbeat::node fsm_hb{ app::can_rx::FSM_Heartbeat_get, app::can_rx::FSM_Heartbeat_update,
-                                   [](const bool b)
+                                   [](const bool fault)
                                    {
-                                       app::can_alerts::warnings::MissingFSMHeartbeat_set(b);
-                                       if (not b)
+                                       app::can_alerts::warnings::MissingFSMHeartbeat_set(fault);
+                                       if (fault)
                                            app::can_rx::clear_board_rx_table(app::can_utils::CanNode::FSM_NODE);
                                    } };
 
 static io::heartbeat::node crit_hb{ app::can_rx::CRIT_Heartbeat_get, app::can_rx::CRIT_Heartbeat_update,
-                                    [](const bool b)
+                                    [](const bool fault)
                                     {
-                                        app::can_alerts::warnings::MissingCRITHeartbeat_set(b);
-                                        if (not b)
+                                        app::can_alerts::warnings::MissingCRITHeartbeat_set(fault);
+                                        if (fault)
                                             app::can_rx::clear_board_rx_table(app::can_utils::CanNode::CRIT_NODE);
                                     } };
 
 static io::heartbeat::node dam_hb{ app::can_rx::DAM_Heartbeat_get, app::can_rx::DAM_Heartbeat_update,
-                                   [](const bool b)
+                                   [](const bool fault)
                                    {
-                                       app::can_alerts::infos::MissingDAMHeartbeat_set(b);
-                                       if (not b)
+                                       app::can_alerts::infos::MissingDAMHeartbeat_set(fault);
+                                       if (fault)
                                            app::can_rx::clear_board_rx_table(app::can_utils::CanNode::DAM_NODE);
                                    } };
 
