b63700f3441f0d4cd5a836f9f80ce23d910955da
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-06-02 20:49:44 -0700

    calibrated

--- firmware/hexray/FSM/src/io/io_steering.cpp
+++ firmware/hexray/FSM/src/io/io_steering.cpp
@@ -5,13 +5,13 @@
 namespace io::steering
 {
 // Voltage thresholds for the steering sensor (in Volts)
-constexpr float MIN_STEERING_VOLTAGE = 0.0693040267f;                           // collected empirically
-constexpr float MAX_STEERING_VOLTAGE = 1.47150183f;                             // collected empirically
-constexpr float MIN_STEERING_ANGLE   = -100.0f;                                 // Minimum steering angle in degrees
-constexpr float MAX_STEERING_ANGLE   = 80.0f;                                   // Maximum steering angle in degrees
-constexpr float LOCK_TO_LOCK         = MAX_STEERING_ANGLE - MIN_STEERING_ANGLE; // Total range of steering angles
+constexpr float MIN_STEERING_VOLTAGE = 0.0693040267f; // collected empirically
+constexpr float MAX_STEERING_VOLTAGE = 1.47150183f;   // collected empirically
+constexpr float MIN_STEERING_ANGLE   = -100.0f;       // Minimum steering angle in degrees
+constexpr float MAX_STEERING_ANGLE   = 80.0f;         // Maximum steering angle in degrees
 
 // Constants for steering angle calculation
+constexpr float LOCK_TO_LOCK    = MAX_STEERING_ANGLE - MIN_STEERING_ANGLE; // Total range of steering angles
 constexpr float DEGREE_PER_VOLT = LOCK_TO_LOCK / (MAX_STEERING_VOLTAGE - MIN_STEERING_VOLTAGE);
 
 float getAngleDegrees()
