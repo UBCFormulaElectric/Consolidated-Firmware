dfc659212d68c99103b8841bbcd97f52c56276b5
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-06-02 20:48:57 -0700

    FSM steering calibration

--- firmware/hexray/FSM/src/io/io_steering.cpp
+++ firmware/hexray/FSM/src/io/io_steering.cpp
@@ -5,18 +5,20 @@
 namespace io::steering
 {
 // Voltage thresholds for the steering sensor (in Volts)
-constexpr float MIN_STEERING_VOLTAGE = 0.2f;
-constexpr float MAX_STEERING_VOLTAGE = 3.2f; // TODO: need to be validated
+constexpr float MIN_STEERING_VOLTAGE = 0.0693040267f;                           // collected empirically
+constexpr float MAX_STEERING_VOLTAGE = 1.47150183f;                             // collected empirically
+constexpr float MIN_STEERING_ANGLE   = -100.0f;                                 // Minimum steering angle in degrees
+constexpr float MAX_STEERING_ANGLE   = 80.0f;                                   // Maximum steering angle in degrees
+constexpr float LOCK_TO_LOCK         = MAX_STEERING_ANGLE - MIN_STEERING_ANGLE; // Total range of steering angles
 
 // Constants for steering angle calculation
-constexpr float STEERING_ANGLE_VOLTAGE_OFFSET = 2.21f; // TODO: still needs to be validated
-constexpr float DEGREE_PER_VOLT               = 360.0f / (MAX_STEERING_VOLTAGE - MIN_STEERING_VOLTAGE);
+constexpr float DEGREE_PER_VOLT = LOCK_TO_LOCK / (MAX_STEERING_VOLTAGE - MIN_STEERING_VOLTAGE);
 
 float getAngleDegrees()
 {
     const float steering_voltage = str_angle.getVoltage(); // Get the ADC voltage for the steering sensor.
-    return DEGREE_PER_VOLT *
-           (steering_voltage - STEERING_ANGLE_VOLTAGE_OFFSET); // Calculate and return the steering angle in degrees.
+    return DEGREE_PER_VOLT * (steering_voltage - MIN_STEERING_VOLTAGE) +
+           MIN_STEERING_ANGLE; // Calculate and return the steering angle in degrees.
 }
 
 bool sensorOCSC()
