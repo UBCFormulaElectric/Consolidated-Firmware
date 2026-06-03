52f5ceddfc2cd1abfdd67e710289dc33b2b0d861
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-06-03 16:32:05 -0700

    last shutdown loop

--- firmware/hexray/FSM/src/jobs.cpp
+++ firmware/hexray/FSM/src/jobs.cpp
@@ -10,6 +10,7 @@
 #include "app_canTx.hpp"
 #include "app_heartbeatMonitors.hpp"
 #include "app_commitInfo.hpp"
+#include "app_fsmShdnLoop.hpp"
 
 #include "io_canQueues.hpp"
 #include "io_time.hpp"
@@ -46,6 +47,7 @@ void jobs_run100Hz_tick()
     // app::shdnLoop::broadcast();
     app::steering::broadcast();
     app::suspension::broadcast();
+    fsm_shdnLoop.broadcast();
 
     hb_monitor.checkIn();
     hb_monitor.broadcastFaults();
