64f35d8fd866bc824602508f2216387536896c05
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-06-07 16:22:04 -0700

    warning

--- firmware/hexray/VC/src/io/io_pumps.cpp
+++ firmware/hexray/VC/src/io/io_pumps.cpp
@@ -3,5 +3,4 @@
 #include "io_pumps.hpp"
 
 const io::Potentiometer rr_pot{ rr_pump_i2c, io::POTENTIOMETER_WIPER::WIPER1 };
-// MAKE SURE THAT YOU ARE GRABBING THE LOCK FOR THIS
-const io::pump rr_pump{ rr_pot, false };
+const io::pump          rr_pump{ rr_pot, false };
--- firmware/hexray/VC/src/io/io_pumps.hpp
+++ firmware/hexray/VC/src/io/io_pumps.hpp
@@ -1,4 +1,5 @@
 #pragma once
 #include "io_pump.hpp"
 
+// MAKE SURE THAT YOU ARE GRABBING THE LOCK FOR THIS
 extern const io::pump rr_pump;
