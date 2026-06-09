8f1805cdf18127ac21b8665ebb9d3814b825efb9
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2026-06-08 23:28:23 -0700

    done

--- validationtools/validationtools/powerSupply/_powerSupply.py
+++ validationtools/validationtools/powerSupply/_powerSupply.py
@@ -7,7 +7,7 @@ import pandas as pd
 import logging
 
 # Configure logging
-logging.basicConfig(level=logging.WARNING,
+logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
 
