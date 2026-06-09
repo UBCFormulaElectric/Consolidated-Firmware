479abc9604a6ac8905c0e1f9d6d1dfc0f66e7ef1
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-06-08 21:29:10 -0700

    builds RSM

--- firmware/hexray/RSM/src/app/app_pumpControl.cpp
+++ firmware/hexray/RSM/src/app/app_pumpControl.cpp
@@ -16,11 +16,11 @@ void broadcast()
 {
     if (const result<uint8_t> percentage = rl_pump.getPercentage())
     {
-        can_tx::RSM_RPumpPercentage_set(percentage.value());
+        can_tx::RSM_RearLeftPumpPercentage_set(percentage.value());
     }
     else
     {
-        can_tx::RSM_RPumpPercentage_set(255);
+        can_tx::RSM_RearLeftPumpPercentage_set(255);
     }
 }
 
