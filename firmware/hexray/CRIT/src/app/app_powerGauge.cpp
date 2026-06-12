0db134601c99db0261786568b62b28b4b850c27e
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-06-11 23:29:51 -0700

    something to this effect

--- firmware/hexray/CRIT/src/app/app_powerGauge.cpp
+++ firmware/hexray/CRIT/src/app/app_powerGauge.cpp
@@ -1,6 +1,7 @@
 #include "app_powerGauge.hpp"
 #include "app_rgbhsv.hpp"
 #include "app_brightness.hpp"
+#include "app_canRx.hpp"
 
 #include "io_powerGauge.hpp"
 #include "io_time.hpp"
@@ -11,18 +12,27 @@ void app::power_gauge::update()
 {
     const double h = std::fmod(static_cast<double>(io::time::getCurrentMs()) / 10, 360);
     auto [r, g, b] = rgb::from({ h, 1.0, 0.5 });
-    const auto r_8 = static_cast<uint8_t>(r * static_cast<double>(std::numeric_limits<uint8_t>::max()));
-    const auto g_8 = static_cast<uint8_t>(g * static_cast<double>(std::numeric_limits<uint8_t>::max()));
-    const auto b_8 = static_cast<uint8_t>(b * static_cast<double>(std::numeric_limits<uint8_t>::max()));
-    std::array<io::power_gauge::led_frame, io::power_gauge::LEDS> data{ {
-        { static_cast<double>(brightness) / 100, r_8, g_8, b_8 },
-        { static_cast<double>(brightness) / 100, r_8, g_8, b_8 },
-        { static_cast<double>(brightness) / 100, r_8, g_8, b_8 },
-        { static_cast<double>(brightness) / 100, r_8, g_8, b_8 },
-        { static_cast<double>(brightness) / 100, r_8, g_8, b_8 },
-        { static_cast<double>(brightness) / 100, r_8, g_8, b_8 },
-        { static_cast<double>(brightness) / 100, r_8, g_8, b_8 },
-        { static_cast<double>(brightness) / 100, r_8, g_8, b_8 },
-    } };
+    // const auto r_8 = static_cast<uint8_t>(r * static_cast<double>(std::numeric_limits<uint8_t>::max()));
+    // const auto g_8 = static_cast<uint8_t>(g * static_cast<double>(std::numeric_limits<uint8_t>::max()));
+    // const auto b_8 = static_cast<uint8_t>(b * static_cast<double>(std::numeric_limits<uint8_t>::max()));
+    // std::array<io::power_gauge::led_frame, io::power_gauge::LEDS> data{ {
+    //     { static_cast<double>(brightness) / 100, r_8, g_8, b_8 },
+    //     { static_cast<double>(brightness) / 100, r_8, g_8, b_8 },
+    //     { static_cast<double>(brightness) / 100, r_8, g_8, b_8 },
+    //     { static_cast<double>(brightness) / 100, r_8, g_8, b_8 },
+    //     { static_cast<double>(brightness) / 100, r_8, g_8, b_8 },
+    //     { static_cast<double>(brightness) / 100, r_8, g_8, b_8 },
+    //     { static_cast<double>(brightness) / 100, r_8, g_8, b_8 },
+    //     { static_cast<double>(brightness) / 100, r_8, g_8, b_8 },
+    // } };
+    std::array<io::power_gauge::led_frame, io::power_gauge::LEDS> data{};
+
+    static constexpr uint32_t MAX_CURRENT = 120;
+    for (size_t i = 0; i < data.size(); i++)
+    {
+        io::power_gauge::led_frame &d = data[i];
+        d._r = d._g = d._b =
+            app::can_rx::BMS_TractiveSystemCurrent_get() > static_cast<float>(120u * i / data.size()) ? 255 : 0;
+    }
     LOG_IF_ERR(io::power_gauge::update(data));
 }
