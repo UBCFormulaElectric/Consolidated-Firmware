c69411e819c6756ba0118e15a0fb077de02659c6
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2026-06-13 05:22:20 -0700

    BMS Efuse Retry (#2079)
    ### Changelist <!-- Give a list of the changes covered in this PR. This
    will help both you and the reviewer keep this PR within scope. -->
    
    ### Testing Done <!-- Outline the testing that was done to demonstrate
    the changes are solid. This could be unit tests, integration tests,
    testing on the car, etc. Include relevant code snippets, screenshots,
    etc as needed. -->
    
    ### Resolved Tickets <!-- Link any tickets that this PR resolves. -->
    
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    ### Resolved Tickets
    <!-- Link any tickets that this PR resolves. -->

--- firmware/hexray/BMS/src/io/io_fans.cpp
+++ firmware/hexray/BMS/src/io/io_fans.cpp
@@ -1,30 +1,55 @@
 #include "io_fans.hpp"
 #include "hw_gpios.hpp"
+#include "app_timer.hpp"
 #include <cstdint>
 
-constexpr uint8_t MAX_RETRIES = 5;
 namespace io::fans
 {
+namespace
+{
+    constexpr uint8_t  MAX_RETRIES    = 5;
+    constexpr uint32_t SETTLE_TIME_MS = 100;
+
+    uint8_t    num_retries = 0;
+    bool       fan_on      = false;
+    app::Timer retry_timer{ SETTLE_TIME_MS };
+} // namespace
 
 void tick(const bool enable)
 {
-    fan_en.writePin(enable);
-    // if (enable)
-    // {
-    //
-    //     if (!fan_pgood.readPin())
-    //     {
-    //         static uint8_t num_retries = 0;
-    //         if (num_retries < MAX_RETRIES)
-    //         {
-    //             num_retries++;
-    //             fan_en.writePin(false);
-    //         }
-    //     }
-    // }
-    // else
-    // {
-    //     fan_en.writePin(false);
-    // }
+    if (!enable)
+    {
+        fan_en.writePin(false);
+        fan_on      = false;
+        num_retries = 0;
+        retry_timer.stop();
+        return;
+    }
+
+    if (retry_timer.updateAndGetState() == app::Timer::TimerState::RUNNING)
+        return;
+
+    if (!fan_on)
+    {
+        if (num_retries < MAX_RETRIES)
+        {
+            fan_en.writePin(true);
+            fan_on = true;
+            retry_timer.restart();
+        }
+        return;
+    }
+
+    if (!fan_pgood.readPin())
+    {
+        num_retries++;
+        fan_en.writePin(false);
+        fan_on = false;
+        retry_timer.restart();
+    }
+    else
+    {
+        num_retries = 0;
+    }
 }
 } // namespace io::fans
