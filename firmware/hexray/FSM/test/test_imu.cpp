41f4dc5e4b9a347aeda5c4b2af227cf024959e02
Bhatt <bhattmaneet6@gmail.com>
2026-06-13 00:41:50 -0700

    fix tests

--- firmware/hexray/FSM/test/test_imu.cpp
+++ firmware/hexray/FSM/test/test_imu.cpp
@@ -11,18 +11,18 @@ class FSMImuTest : public FSMBaseTest
 
 TEST_F(FSMImuTest, Accel_Gyro_Test)
 {
-    io::imus::imu_front.set_AccelX(10.0f);
-    io::imus::imu_front.set_AccelY(10.0f);
-    io::imus::imu_front.set_AccelZ(10.0f);
+    io::imus::imu_front.set_AccelX(4.0f);
+    io::imus::imu_front.set_AccelY(4.0f);
+    io::imus::imu_front.set_AccelZ(4.0f);
     io::imus::imu_front.set_GyroRoll(10.0f);
     io::imus::imu_front.set_GyroPitch(10.0f);
     io::imus::imu_front.set_GyroYaw(10.0f);
 
     LetTimePass(100);
 
-    ASSERT_FLOAT_EQ(10.0f, app::can_tx::FSM_AccelX_get());
-    ASSERT_FLOAT_EQ(10.0f, app::can_tx::FSM_AccelY_get());
-    ASSERT_FLOAT_EQ(10.0f, app::can_tx::FSM_AccelZ_get());
+    ASSERT_FLOAT_EQ(4.0f, app::can_tx::FSM_AccelX_get());
+    ASSERT_FLOAT_EQ(4.0f, app::can_tx::FSM_AccelY_get());
+    ASSERT_FLOAT_EQ(4.0f, app::can_tx::FSM_AccelZ_get());
     ASSERT_FLOAT_EQ(10.0f, app::can_tx::FSM_GyroX_get());
     ASSERT_FLOAT_EQ(10.0f, app::can_tx::FSM_GyroY_get());
     ASSERT_FLOAT_EQ(10.0f, app::can_tx::FSM_GyroZ_get());
--- firmware/hexray/RSM/test/test_imu.cpp
+++ firmware/hexray/RSM/test/test_imu.cpp
@@ -12,18 +12,18 @@ class RSMImuTest : public RSMBaseTest
 
 TEST_F(RSMImuTest, Accel_Gyro_Test)
 {
-    io::imus::imu_rear.set_AccelX(10.0f);
-    io::imus::imu_rear.set_AccelY(10.0f);
-    io::imus::imu_rear.set_AccelZ(10.0f);
+    io::imus::imu_rear.set_AccelX(4.0f);
+    io::imus::imu_rear.set_AccelY(4.0f);
+    io::imus::imu_rear.set_AccelZ(4.0f);
     io::imus::imu_rear.set_GyroRoll(10.0f);
     io::imus::imu_rear.set_GyroPitch(10.0f);
     io::imus::imu_rear.set_GyroYaw(10.0f);
 
     app::imu::broadcast();
 
-    ASSERT_FLOAT_EQ(10.0f, app::can_tx::RSM_AccelX_get());
-    ASSERT_FLOAT_EQ(10.0f, app::can_tx::RSM_AccelY_get());
-    ASSERT_FLOAT_EQ(10.0f, app::can_tx::RSM_AccelZ_get());
+    ASSERT_FLOAT_EQ(4.0f, app::can_tx::RSM_AccelX_get());
+    ASSERT_FLOAT_EQ(4.0f, app::can_tx::RSM_AccelY_get());
+    ASSERT_FLOAT_EQ(4.0f, app::can_tx::RSM_AccelZ_get());
     ASSERT_FLOAT_EQ(10.0f, app::can_tx::RSM_GyroX_get());
     ASSERT_FLOAT_EQ(10.0f, app::can_tx::RSM_GyroY_get());
     ASSERT_FLOAT_EQ(10.0f, app::can_tx::RSM_GyroZ_get());
