764ca5a2705f20e6fefd9e869664a646dff2d057
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-06-13 05:48:42 -0700

    semaphore chungus moment

--- firmware/shared/srcpp/io/io_semaphore.cpp
+++ firmware/shared/srcpp/io/io_semaphore.cpp
@@ -23,16 +23,16 @@ io::semaphore::semaphore(const bool priority_inheritance_protocol_on, const uint
 
 void io::semaphore::give() const
 {
-    if (osKernelGetState() != taskSCHEDULER_RUNNING)
-        return;
+    // if (osKernelGetState() != taskSCHEDULER_RUNNING)
+    //     return;
     xSemaphoreGive(freertos_semaphore_);
     if (_sysview_id != 0)
         SEGGER_SYSVIEW_MarkStop(_sysview_id);
 }
 void io::semaphore::take(const uint32_t timeout) const
 {
-    if (osKernelGetState() != taskSCHEDULER_RUNNING)
-        return;
+    // if (osKernelGetState() != taskSCHEDULER_RUNNING)
+    //     return;
     const auto take_success = xSemaphoreTake(freertos_semaphore_, timeout);
     assert(take_success == pdTRUE);
     if (_sysview_id != 0)
