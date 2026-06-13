743c65a912b7fcda7038f5b458ab7667054d8a5c
Rijwal Sangey <rijwalsangey@gmail.com>
2026-06-12 17:57:37 -0700

    bruh

--- firmware/shared/srcpp_fake/io_pumpFake.hpp
+++ firmware/shared/srcpp_fake/io_pumpFake.hpp
@@ -6,6 +6,6 @@ namespace fakes::io::rPump
 {
 extern uint8_t percentage;
 
-void    set_readPercentage(uint8_t value);
+void    set_readPercentage(const uint8_t value);
 uint8_t get_readPercentage();
 } // namespace fakes::io::rPump
