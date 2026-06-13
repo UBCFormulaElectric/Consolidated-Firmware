2547838a210c7eca9f3c6d4fcfd8c8aa74942e5d
Pranay Oza <105095960+pranay-o@users.noreply.github.com>
2026-06-12 18:59:26 -0700

    quickfix

--- firmware/hexray/BMS/src/jobs.cpp
+++ firmware/hexray/BMS/src/jobs.cpp
@@ -123,19 +123,6 @@ void jobs_run100Hz_tick()
     using FaultLatchState = io::FaultLatch::FaultLatchState;
     bms_ok_latch.setCurrentStatus(acc_fault ? FaultLatchState::FAULT : FaultLatchState::OK);
     app::latches::broadcast();
-
-    // Update CAN signals for BMS latch statuses.
-    const bool imd_latched_ok  = imd_ok_latch.getLatchedStatus() == FaultLatchState::OK;
-    const bool bspd_latched_ok = bspd_ok_latch.getLatchedStatus() == FaultLatchState::OK;
-    const bool bms_latched_ok  = bms_ok_latch.getLatchedStatus() == FaultLatchState::OK;
-
-    app::can_tx::BMS_BmsCurrentlyOk_set(bms_ok_latch.getCurrentStatus() == FaultLatchState::OK);
-    app::can_tx::BMS_ImdCurrentlyOk_set(imd_ok_latch.getCurrentStatus() == FaultLatchState::OK);
-    app::can_tx::BMS_BspdCurrentlyOk_set(bspd_ok_latch.getCurrentStatus() == FaultLatchState::OK);
-    app::can_tx::BMS_BmsLatchOk_set(bms_latched_ok);
-    app::can_tx::BMS_ImdLatchOk_set(imd_latched_ok);
-    app::can_tx::BMS_BspdLatchOk_set(bspd_latched_ok);
-
     app::can_tx::BMS_BSPDBrakePressureThresholdExceeded_set(io::bspdtest::isBrakePressureThresholdExceeded());
     app::can_tx::BMS_BSPDAccelBrakeOk_set(io::bspdtest::isAccelBrakeOk());
 
