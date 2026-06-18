828c986ef1d7a879f239d7b7c61f6633bd1baf50
Edwin <20777515+Lucien950@users.noreply.github.com>
2026-06-18 07:59:47 -0700

    CRIT Warning Override (#2097)
    ### Changelist 
    <!-- Give a list of the changes covered in this PR. This will help both
    you and the reviewer keep this PR within scope. -->
    
    ### Testing Done
    <!-- Outline the testing that was done to demonstrate the changes are
    solid. This could be unit tests, integration tests, testing on the car,
    etc. Include relevant code snippets, screenshots, etc as needed. -->
    
    ### Resolved Tickets
    <!-- Link any tickets that this PR resolves. -->

--- firmware/hexray/CRIT/src/app/app_powerGauge.cpp
+++ firmware/hexray/CRIT/src/app/app_powerGauge.cpp
@@ -27,12 +27,25 @@ void app::power_gauge::update()
     // } };
     std::array<io::power_gauge::led_frame, io::power_gauge::LEDS> data{};
 
+    const float current = -app::can_rx::BMS_TractiveSystemCurrent_get();
+
+    static constexpr uint32_t r = 0xF54927FF;
+    static constexpr uint32_t y = 0xF5B027FF;
+    static constexpr uint32_t g = 0x27F549FF;
+
+    io::power_gauge::led_frame c;
+    if (current < 80)
+        c = io::power_gauge::led_frame{ g };
+    else if (current < 100)
+        c = io::power_gauge::led_frame{ y };
+    else
+        c = io::power_gauge::led_frame{ r };
+
     static constexpr uint32_t MAX_CURRENT = 120;
     for (size_t i = 0; i < data.size(); i++)
     {
-        io::power_gauge::led_frame &d = data[i];
-        d._r = d._g = d._b =
-            app::can_rx::BMS_TractiveSystemCurrent_get() > static_cast<float>(MAX_CURRENT * i) / data.size() ? 255 : 0;
+        const bool on = current > static_cast<float>(MAX_CURRENT * i) / data.size();
+        data[i]       = on ? c : io::power_gauge::led_frame{ 0x0 };
     }
     LOG_IF_ERR(io::power_gauge::update(data));
 }
