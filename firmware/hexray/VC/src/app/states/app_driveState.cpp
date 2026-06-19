c8e6e1ca7bd41ff6f59fa9cd6b3b9f0100138c0b
Rijwal Sangey <rijwalsangey@gmail.com>
2026-06-19 15:20:05 -0400

    legit

--- firmware/hexray/VC/src/app/states/app_driveState.cpp
+++ firmware/hexray/VC/src/app/states/app_driveState.cpp
@@ -65,7 +65,7 @@ static void driveStateRunOnTick100Hz()
 
     const auto apps = can_rx::FSM_PappsMappedPedalPercentage_get();
 
-    if (can_alerts::AnyBoardHasWarning() and app::can_rx::CRIT_LaunchControlSwitch_get() != SwitchState::ON)
+    if if (app::can_rx::CRIT_LaunchControlSwitch_get() != SwitchState::ON || app::bspdWarning::checkSoftwareBspd(apps))
     {
         send_torque(NO_TORQUE_Nm, NO_TORQUE_Nm, NO_TORQUE_Nm, NO_TORQUE_Nm);
         return;
