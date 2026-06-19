4b12b69e73c929482de8e2bb3ef8aabba574f414
Setare Maleki <setimalek@gmail.com>
2026-06-19 07:07:38 -0700

    test fix

--- firmware/hexray/CRIT/test/critTestBase.hpp
+++ firmware/hexray/CRIT/test/critTestBase.hpp
@@ -1,8 +1,13 @@
 #pragma once
 #include "ecuTestBase.hpp"
+#include "jobs.hpp"
 
 class CRITBaseTest : public EcuTestBase
 {
-    void board_setup() override{};
+    void board_setup() override{
+        
+        register_task(jobs_run10Hz_tick, 1000);
+
+    };
     void board_teardown() override{};
 };
