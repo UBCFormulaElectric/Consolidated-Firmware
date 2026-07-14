893da4be0f16713daec37ad03c87afcc0118a214
Rijwal Sangey <rijwalsangey@gmail.com>
2026-07-14 01:02:49 -0700

    done

--- firmware/hexray/PCM/src/cubemx/Src/main.c
+++ firmware/hexray/PCM/src/cubemx/Src/main.c
@@ -220,7 +220,7 @@ static void MX_GPIO_Init(void)
 
     /*Configure GPIO pin : PCM_EN_Pin */
     GPIO_InitStruct.Pin  = PCM_EN_Pin;
-    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+    GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING_FALLING;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(PCM_EN_GPIO_Port, &GPIO_InitStruct);
 
